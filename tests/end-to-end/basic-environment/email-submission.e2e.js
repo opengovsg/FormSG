6cb76662fa889b816f94c493265ce60fa4a8b36a
arshadali172 <arshad172ali@gmail.com>
2020-07-30 11:35:57 +0800

    Move out common email submissions funcs to helper file

--- tests/end-to-end/basic-environment/email-submission.e2e.js
+++ tests/end-to-end/basic-environment/email-submission.e2e.js
@@ -2,25 +2,14 @@ const {
   makeMongooseFixtures,
   makeModel,
   deleteDocById,
-  getSubmission,
   createFormFromTemplate,
   createForm,
-  expectStartPage,
-  fillInForm,
-  submitForm,
-  expectEndPage,
-  appUrl,
   getOptionalVersion,
   getBlankVersion,
-  getSubstringBetween,
-  decodeHtmlEntities,
-  expectContains,
-  getResponseArray,
-  getResponseTitle,
-  expectSpcpLogin,
-  getAuthFields,
   verifySubmissionDisabled,
 } = require('../helpers/util')
+
+const { verifySubmissionE2e } = require('../helpers/email-mode')
 const { allFields } = require('../helpers/all-fields')
 const { templateFields } = require('../helpers/template-fields')
 const {
@@ -152,73 +141,6 @@ test.before(async (t) => {
   await verifySubmissionE2e(t, t.ctx.form, t.ctx.formData)
 })
 
-// Checks that an attachment field's attachment is contained in the email.
-const expectAttachment = async (t, field, attachments) => {
-  // Attachments can only be in fields that are visible AND either required
-  // or optional but filled
-  if (field.isVisible && (field.required || !field.isLeftBlank)) {
-    const attachment = attachments.find((att) => att.fileName === field.val)
-    // Check that attachment exists
-    await t.expect(attachment).ok()
-    // Check that contents match
-    await t.expect(attachment.content).eql(field.content)
-  }
-}
-
-// Grab submission email and verify its contents
-const verifySubmission = async (t, testFormData, authData) => {
-  let { user, formFields, formOptions } = testFormData
-  const authType = formOptions ? formOptions.authType : 'NIL'
-  // Add verified authentication data (NRIC/UEN/UID) at the end
-  formFields = formFields.concat(getAuthFields(authType, authData))
-  formFields = formFields.filter((f) => f.fieldType !== 'section')
-
-  const title = formOptions ? formOptions.title : ''
-  const { html, subject, to, from, attachments } = await getSubmission(title)
-  // Verify recipient of email
-  await t.expect(to).eql(user.email)
-
-  // Verify sender of email
-  await t.expect(from).eql('FormSG <donotreply@mail.form.gov.sg>')
-
-  // Verify subject of email
-  await t.expect(subject).contains(`formsg-auto: ${title} (Ref:`)
-  // Verify form content in email
-  for (let field of formFields) {
-    const contained = [
-      getResponseTitle(field, false, 'email'),
-      ...getResponseArray(field, 'email'),
-    ]
-    await expectContains(t, html, contained)
-    if (field.fieldType === 'attachment') {
-      await expectAttachment(t, field, attachments)
-    }
-  }
-
-  // Verify JSON for data collation
-  const emailJSONStr = getSubstringBetween(
-    html,
-    '<p>-- Start of JSON --</p>',
-    '<p>-- End of JSON --</p>',
-  )
-  await t.expect(emailJSONStr).notEql(null)
-  const emailJSON = JSON.parse(decodeHtmlEntities(emailJSONStr))
-  const response = emailJSON.filter(
-    ({ question }) => !/(Reference Number|Timestamp)/.test(question),
-  )
-  await t.expect(response.length).notEql(0)
-  let rowIdx = 0 // A table could result in multiple rows of answers, so there might be more answers than form fields
-  for (let field of formFields) {
-    await t
-      .expect(response[rowIdx].question)
-      .eql(getResponseTitle(field, true, 'email'))
-    for (let expectedAnswer of getResponseArray(field, 'email')) {
-      await t.expect(response[rowIdx].answer).eql(expectedAnswer)
-      rowIdx++
-    }
-  }
-}
-
 // Creates an object with default form options, with optional modifications.
 const getDefaultFormOptions = async ({
   title = 'Submission e2e Form',
@@ -242,20 +164,4 @@ const getDefaultFormOptions = async ({
       esrvcId,
     },
   }
-}
-
-// Open form, fill it in, submit and verify submission
-const verifySubmissionE2e = async (t, form, formData, authData) => {
-  // Verify that form can be accessed
-  await expectStartPage(t, form, formData, appUrl)
-  if (authData) {
-    await expectSpcpLogin(t, formData.formOptions.authType, authData)
-  }
-  // Fill in form=
-  await fillInForm(t, form, formData)
-  await submitForm(t)
-  // Verify that end page is shown
-  await expectEndPage(t)
-  // Verify that submission is as expected
-  await verifySubmission(t, formData, authData)
 }
