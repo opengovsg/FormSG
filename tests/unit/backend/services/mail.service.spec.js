158bd22eb810fef56cb44ba5bbcf4bfe7a1d231f
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-05 11:13:09 +0800

    test(MailService): add tests for sendVerificationOtp

--- tests/unit/backend/services/mail.service.spec.js
+++ tests/unit/backend/services/mail.service.spec.js
@@ -3,18 +3,23 @@ const {
 } = require('../../../../dist/backend/app/services/mail.service')
 const { merge } = require('lodash')
 
+const MOCK_VALID_EMAIL = 'to@example.com'
+const MOCK_SENDER_EMAIL = 'from@example.com'
+const MOCK_APP_NAME = 'mockApp'
+const MOCK_MAIL_PARAMS = {
+  to: MOCK_VALID_EMAIL,
+  from: MOCK_SENDER_EMAIL,
+  subject: 'send node mail in tests',
+  html: `<p>You are currently submitting a form.</p>`,
+}
+
 fdescribe('mail.service', () => {
   const mockTransporter = jasmine.createSpyObj('transporter', ['sendMail'])
   const mailService = new MailService({
     transporter: mockTransporter,
+    senderEmail: MOCK_SENDER_EMAIL,
+    appName: MOCK_APP_NAME,
   })
-
-  const MOCK_MAIL_PARAMS = {
-    to: 'to@example.com',
-    from: 'from@example.com',
-    subject: 'send node mail in tests',
-    html: `<p>You are currently submitting a form.</p>`,
-  }
 
   describe('sendNodeMail', () => {
     it('should receive correct response when mail is sent successfully', async () => {
@@ -59,5 +64,52 @@ fdescribe('mail.service', () => {
     })
   })
 
-  describe('sendVerificationOtp', () => {})
+  describe('sendVerificationOtp', () => {
+    const MOCK_OTP = '123456'
+
+    it('should send verification otp successfully', async () => {
+      // Arrange
+      const sendSpy = spyOn(mailService, 'sendNodeMail').and.callThrough()
+      const mockedResponse = 'mockedSuccessResponse'
+      mockTransporter.sendMail.and.callFake(() => mockedResponse)
+
+      const expectedArguments = [
+        {
+          to: MOCK_VALID_EMAIL,
+          from: MOCK_SENDER_EMAIL,
+          subject: `Your OTP for submitting a form on ${MOCK_APP_NAME}`,
+          // Can't use dedent here, original seems to work a little differently due to TypeScript compilation.
+          html: `<p>You are currently submitting a form on ${MOCK_APP_NAME}.</p>
+<p>
+  Your OTP is <b>${MOCK_OTP}</b>. 
+  It will expire in ${10} minutes. 
+  Please use this to verify your submission.
+</p>
+<p>If your OTP does not work, please request for a new OTP.</p>`,
+          headers: {
+            // Hardcode in tests in case something changes this.
+            'X-Formsg-Email-Type': 'Verification OTP',
+          },
+        },
+        { mailId: 'verify' },
+      ]
+
+      // Act + Assert
+      await expectAsync(
+        mailService.sendVerificationOtp(MOCK_VALID_EMAIL, MOCK_OTP),
+      ).toBeResolvedTo(mockedResponse)
+      // Check arguments passed to sendNodeMail
+      expect(sendSpy).toHaveBeenCalledTimes(1)
+      expect(sendSpy).toHaveBeenCalledWith(...expectedArguments)
+    })
+
+    it('should reject with error when email is invalid', async () => {
+      // Arrange
+      const invalidEmail = 'notAnEmail'
+      // Act + Assert
+      await expectAsync(
+        mailService.sendVerificationOtp(invalidEmail, MOCK_OTP),
+      ).toBeRejectedWithError(`${invalidEmail} is not a valid email`)
+    })
+  })
 })
