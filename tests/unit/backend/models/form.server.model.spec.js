0084a8f2f05f926c05d0931bbfc737f8cd44f170
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-07 11:18:12 +0800

    test(FormModel): add getQuestion field instance method tests

--- tests/unit/backend/models/form.server.model.spec.js
+++ tests/unit/backend/models/form.server.model.spec.js
@@ -4,6 +4,8 @@ const { ObjectId } = require('bson-ext')
 
 const dbHandler = require('../helpers/db-handler')
 const Form = spec('dist/backend/app/models/form.server.model').default(mongoose)
+const EncryptedForm = mongoose.model('encrypt')
+const EmailForm = mongoose.model('email')
 const Agency = spec('dist/backend/app/models/agency.server.model').default(
   mongoose,
 )
@@ -16,6 +18,14 @@ const MOCK_FORM_PARAMS = {
   title: 'Test Form',
   admin: MOCK_ADMIN_OBJ_ID,
 }
+const MOCK_ENCRYPTED_FORM_PARAMS = merge({}, MOCK_FORM_PARAMS, {
+  publicKey: 'mockPublicKey',
+  responseMode: 'encrypt',
+})
+const MOCK_EMAIL_FORM_PARAMS = merge({}, MOCK_FORM_PARAMS, {
+  emails: ['test@example.com'],
+  responseMode: 'email',
+})
 
 const FORM_DEFAULTS = {
   authType: 'NIL',
@@ -217,14 +227,10 @@ describe('Form Model', () => {
     })
 
     describe('Encrypted form schema', () => {
-      const EncryptedForm = mongoose.model('encrypt')
       const ENCRYPT_FORM_DEFAULTS = merge(
         { responseMode: 'encrypt' },
         FORM_DEFAULTS,
       )
-      const MOCK_ENCRYPTED_FORM_PARAMS = merge({}, MOCK_FORM_PARAMS, {
-        publicKey: 'mockPublicKey',
-      })
 
       it('should create and save successfully', async () => {
         // Arrange + Act
@@ -419,14 +425,10 @@ describe('Form Model', () => {
     })
 
     describe('Email form schema', () => {
-      const EmailForm = mongoose.model('email')
       const EMAIL_FORM_DEFAULTS = merge(
         { responseMode: 'email' },
         FORM_DEFAULTS,
       )
-      const MOCK_EMAIL_FORM_PARAMS = merge({}, MOCK_FORM_PARAMS, {
-        emails: ['test@example.com'],
-      })
 
       it('should create and save successfully', async () => {
         // Arrange + Act
@@ -726,6 +728,68 @@ describe('Form Model', () => {
       })
     })
   })
+
+  describe('Form field discriminators', () => {
+    beforeEach(async () => await preloadUserAndAgency())
+
+    describe('Methods', () => {
+      describe('getQuestion', () => {
+        it('should return field title when field type is not a table field', async () => {
+          const {
+            BasicFieldType,
+          } = require('../../../../dist/backend/types/field/fieldTypes')
+
+          // Arrange
+          // Get all field types
+          const fieldTypes = Object.values(BasicFieldType)
+
+          // Asserts
+          fieldTypes.forEach(async (type) => {
+            // Skip table field.
+            if (type === 'table') return
+            const fieldTitle = `test ${type} field title`
+            const field = await createAndReturnFormField({
+              fieldType: type,
+              title: fieldTitle,
+            })
+
+            // Assert
+            expect(field.getQuestion()).toEqual(fieldTitle)
+          })
+        })
+
+        it('should return table title concatenated with all column titles when field type is a table field', async () => {
+          // Arrange
+          const tableFieldParams = {
+            title: 'testTableTitle',
+            minimumRows: 1,
+            columns: [
+              {
+                title: 'Test Column Title 1',
+                required: true,
+                columnType: 'textfield',
+              },
+              {
+                title: 'Test Column Title 2',
+                required: true,
+                columnType: 'dropdown',
+              },
+            ],
+            fieldType: 'table',
+          }
+
+          // Act
+          const tableField = await createAndReturnFormField(tableFieldParams)
+
+          // Assert
+          const expectedQuestionString = `${
+            tableFieldParams.title
+          } (${tableFieldParams.columns.map((col) => col.title).join(', ')})`
+          expect(tableField.getQuestion()).toEqual(expectedQuestionString)
+        })
+      })
+    })
+  })
 })
 
 const preloadUserAndAgency = async () => {
@@ -743,4 +807,34 @@ const preloadUserAndAgency = async () => {
   })
 
   return { agency, admin }
+}
+
+const createAndReturnFormField = async (formFieldParams, formType) => {
+  const baseParams = formType
+    ? formType === 'email'
+      ? MOCK_EMAIL_FORM_PARAMS
+      : MOCK_ENCRYPTED_FORM_PARAMS
+    : MOCK_FORM_PARAMS
+
+  // Insert required params if they do not exist.
+  if (formFieldParams.fieldType === 'attachment') {
+    formFieldParams = { attachmentSize: 3, ...formFieldParams }
+  }
+  if (formFieldParams.fieldType === 'image') {
+    formFieldParams = {
+      url: 'http://example.com',
+      fileMd5Hash: 'some hash',
+      name: 'test image name',
+      size: 'some size',
+      ...formFieldParams,
+    }
+  }
+
+  const formParam = {
+    ...baseParams,
+    form_fields: [formFieldParams],
+  }
+  const form = await new Form(formParam).save()
+
+  return form.form_fields[0]
 }
