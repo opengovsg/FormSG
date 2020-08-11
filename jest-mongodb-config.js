9f8d86b87d112bc980d82e05d4e241b6c3e509d2
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-11 09:58:30 +0800

    fix: check mongoose MD5 binary when starting jest tests

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -2,7 +2,7 @@ module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
       version: process.env.MONGO_BINARY_VERSION || '3.6.12',
-      skipMD5: true,
+      checkMD5: true,
     },
     instance: {
       dbName: 'jest',
