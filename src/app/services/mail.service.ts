260650b5dfd5174118173d70584c3b79ee3d3141
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-11 18:36:53 +0800

    refactor: use # modifier for sendNodeMail

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -102,11 +102,7 @@ export class MailService {
    * @param mail Mail data to send with
    * @param sendOptions Extra options to better identify mail, such as form or mail id.
    */
-  // Private method used instead of # modifier for ease of testing
-  private sendNodeMail = async (
-    mail: Mail.Options,
-    sendOptions?: SendMailOptions,
-  ) => {
+  #sendNodeMail = async (mail: Mail.Options, sendOptions?: SendMailOptions) => {
     const emailLogString = `mailId: ${sendOptions?.mailId}\t Email from:${mail?.from}\t subject:${mail?.subject}\t formId: ${sendOptions?.formId}`
 
     // Guard against missing mail info.
@@ -178,7 +174,7 @@ export class MailService {
       },
     }
     // Error gets caught in getNewOtp
-    return this.sendNodeMail(mail, { mailId: 'verify' })
+    return this.#sendNodeMail(mail, { mailId: 'verify' })
   }
 
   /**
@@ -202,7 +198,7 @@ export class MailService {
       },
     }
 
-    return this.sendNodeMail(mail, { mailId: 'OTP' })
+    return this.#sendNodeMail(mail, { mailId: 'OTP' })
   }
 
   /**
@@ -245,7 +241,7 @@ export class MailService {
       replyTo: replyToEmails?.join(', '),
     }
 
-    return this.sendNodeMail(mail, {
+    return this.#sendNodeMail(mail, {
       mailId: submission.id,
       formId: String(form._id),
     })
@@ -303,7 +299,7 @@ export class MailService {
       },
     }
 
-    return this.sendNodeMail(mail, {
+    return this.#sendNodeMail(mail, {
       mailId: `${submission.id}-${index}`,
       formId: form._id,
     })
