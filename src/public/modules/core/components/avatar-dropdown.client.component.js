9fa5391b387bdf7b906cec6655262a98b65d05f1
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-12 19:07:15 +0800

    feat: open initial edit contact number modal on edit click

--- src/public/modules/core/components/avatar-dropdown.client.component.js
+++ src/public/modules/core/components/avatar-dropdown.client.component.js
@@ -3,20 +3,31 @@ const get = require('lodash/get')
 angular.module('core').component('avatarDropdownComponent', {
   templateUrl: 'modules/core/componentViews/avatar-dropdown.html',
   bindings: {},
-  controller: ['$state', 'Auth', avatarDropdownController],
+  controller: ['$state', '$uibModal', 'Auth', avatarDropdownController],
   controllerAs: 'vm',
 })
 
-function avatarDropdownController($state, Auth) {
+function avatarDropdownController($state, $uibModal, Auth) {
   const vm = this
 
   // Redirect to signin if unable to get user
   vm.user = Auth.getUser() || $state.go('signin')
+  vm.user.contact = '+65 83221353'
   vm.avatarText = generateAvatarText()
 
   vm.isDropdownOpen = false
 
   vm.signOut = () => Auth.signOut()
+
+  vm.openContactNumberModal = () => {
+    $uibModal.open({
+      animation: false,
+      template: '<div>Hello world</div>',
+      windowClass: 'full-screen-modal-window',
+      controller: 'EditContactNumberModalController',
+      controllerAs: 'vm',
+    }).result.finally(angular.noop).then(angular.noop, angular.noop)
+  }
 
   function generateAvatarText() {
     const userEmail = get(vm.user, 'email')
