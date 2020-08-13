968c50a0822bd1203aefdc4977bcdf70f6388905
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-13 14:08:55 +0800

    feat: add initial logic to display verification block on verify number

--- src/public/modules/core/controllers/edit-contact-number-modal.client.controller.js
+++ src/public/modules/core/controllers/edit-contact-number-modal.client.controller.js
@@ -2,15 +2,65 @@ angular
   .module('core')
   .controller('EditContactNumberModalController', [
     '$uibModalInstance',
-    '$timeout',
+    'Auth',
     EditContactNumberModalController,
   ])
 
-function EditContactNumberModalController(
-  $uibModalInstance,
-  $timeout,
-) {
+function EditContactNumberModalController($uibModalInstance, Auth) {
   const vm = this
+
+  // The various states of verification
+  const VERIFY_STATE = {
+    IDLE: 'IDLE',
+    AWAIT: 'AWAITING',
+    SUCCESS: 'SUCCESS',
+  }
+
+  vm.isFetching = false
+
+  // Expose so template can use it too.
+  vm.VERIFY_STATE = VERIFY_STATE
+
+  // Redirect to signin if unable to get user
+  vm.user = Auth.getUser() || $state.go('signin')
+
+  vm.vfnState = VERIFY_STATE.IDLE
+
+  vm.contact = {
+    number: '',
+    signature: undefined,
+  }
+
+  vm.lastVerifiedContact = vm.user.contact
+
+  vm.resetVfnState = () => {
+    // Reset to success state if the number is what is currently stored.
+    if (vm.contact.number === vm.lastVerifiedContact) {
+      vm.vfnState = VERIFY_STATE.SUCCESS
+      return
+    }
+
+    // Reset to idle state otherwise so user can verify another number.
+    if (vm.vfnState !== VERIFY_STATE.IDLE) {
+      vm.vfnState = VERIFY_STATE.IDLE
+    }
+  }
+
+  vm.verifyNumber = async () => {
+    const userId = vm.user._id
+    vm.isFetching = true
+    try {
+      // await Verification.sendEmergencyContactOtp(
+      //   userId,
+      //   vm.contact.number,
+      // );
+      vm.vfnState = VERIFY_STATE.AWAIT
+    } catch (err) {
+      // Show error message
+    } finally {
+      vm.isFetching = false
+    }
+  }
 
   vm.closeModal = function () {
     $uibModalInstance.close()
