f34fe0bcbda3dd6bc9e98a25f025a1b085b869cf
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-13 17:46:51 +0800

    Revert "feat: rip out all emergency verification ui"
    This reverts commit 0df2fe0abd9b977563dccf0f665a9a78ad2d4cf6.

--- src/public/modules/core/controllers/edit-contact-number-modal.client.controller.js
+++ src/public/modules/core/controllers/edit-contact-number-modal.client.controller.js
@@ -10,36 +10,75 @@ angular
 function EditContactNumberModalController($scope, $uibModalInstance, Auth) {
   const vm = this
 
-  // Various states of submission.
-  vm.isLoading = false
-  vm.isSuccess = false
-  vm.error = null
+  // The various states of verification
+  const VERIFY_STATE = {
+    IDLE: 'IDLE',
+    AWAIT: 'AWAITING',
+    SUCCESS: 'SUCCESS',
+  }
+
+  vm.isFetching = false
+
+  // Expose so template can use it too.
+  vm.VERIFY_STATE = VERIFY_STATE
 
   // Redirect to signin if unable to get user
   vm.user = Auth.getUser() || $state.go('signin')
-  vm.contact = vm.user.contact || ''
 
+  vm.vfnState = vm.user.contact ? VERIFY_STATE.SUCCESS : VERIFY_STATE.IDLE
 
-  vm.resetSubmissionState = () => {
-    // Reset to success state if the number is what is currently stored.
-    vm.isSuccess = vm.contact && vm.contact === vm.user.contact
+  vm.contact = {
+    number: vm.user.contact || '',
   }
 
-  vm.updateContactNumber = async () => {
+  vm.otp = {
+    value: '',
+    countdown: 0
+  }
+
+  vm.lastVerifiedContact = vm.user.contact
+
+  vm.resetVfnState = () => {
+    // Reset to success state if the number is what is currently stored.
+    if (vm.contact.number && vm.contact.number === vm.lastVerifiedContact) {
+      vm.vfnState = VERIFY_STATE.SUCCESS
+      return
+    }
+
+    // Reset to idle state otherwise so user can verify another number.
+    if (vm.vfnState !== VERIFY_STATE.IDLE) {
+      vm.vfnState = VERIFY_STATE.IDLE
+    }
+  }
+
+  vm.sendOtp = async () => {
     const userId = vm.user._id
-    vm.isLoading = true
+    vm.isFetching = true
     try {
-      // await Auth.updateEmergencyContactOtp(
+      // await Verification.sendEmergencyContactOtp(
       //   userId,
       //   vm.contact.number,
       // );
-      vm.isSuccess = true
-      vm.user.contact = vm.contact
+      vm.vfnState = VERIFY_STATE.AWAIT
     } catch (err) {
       // Show error message
-      vm.error = err
     } finally {
-      vm.isLoading = false
+      vm.isFetching = false
+    }
+  }
+
+  vm.verifyOtp = async () => {
+    vm.isFetching = true
+    try {
+      // Check with backend if the otp is correct
+      vm.vfnState = VERIFY_STATE.SUCCESS
+    } catch (err) {
+      // Show error message
+    } finally {
+      vm.isFetching = false
+      vm.otp.value = ''
+      $scope.otpForm.otp.$setPristine()
+      $scope.otpForm.otp.$setUntouched()
     }
   }
 
