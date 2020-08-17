3fd263ad9604f8ec72e8e1500c2c1383ea39bb2b
Antariksh Mahajan <antarikshmahajan@gmail.com>
2020-08-17 16:24:42 +0800

    refactor: use logger.info and add IP to admin mail log

--- src/app/controllers/email-submissions.server.controller.js
+++ src/app/controllers/email-submissions.server.controller.js
@@ -552,25 +552,22 @@ exports.saveMetadataToDb = function (req, res, next) {
 
   // Create submission hash
   let concatenatedResponse = concatResponse(formData, attachments)
-  let submissionLog
 
   createHash(concatenatedResponse)
     .then((result) => {
       submission.responseHash = result.hash
       submission.responseSalt = result.salt
-      submissionLog = {
-        message: 'Saving submission to MongoDB',
+      // Save submission to database
+      return submission.save()
+    })
+    .then((submission) => {
+      logger.info({
+        message: 'Saved submission to MongoDB',
         submissionId: submission.id,
         formId: form._id,
         ip: getRequestIp(req),
         responseHash: submission.responseHash,
-      }
-      // Save submission to database
-      logger.profile(submissionLog)
-      return submission.save()
-    })
-    .then((submission) => {
-      logger.profile(submissionLog)
+      })
       req.submission = submission
       return next()
     })
@@ -650,8 +647,13 @@ exports.sendAdminEmail = async function (req, res, next) {
     if (replyTo) mailOptions.replyTo = replyTo
   }
 
-  let adminLogstring = `Sending admin mail submissionId=${submission.id} formId=${form._id} submissionHash=${submission.responseHash}`
-  logger.profile(adminLogstring)
+  logger.info({
+    message: 'Sending admin mail',
+    submissionId: submission.id,
+    formId: form._id,
+    ip: getRequestIp(req),
+    submissionHash: submission.responseHash,
+  })
 
   // Send mail
   try {
