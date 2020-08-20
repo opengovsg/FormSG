b9d31c60cdcdec56fb3c603b1bc308f50d44b3ce
Arshad Ali <arshadali@Arshads-MacBook-Pro.local>
2020-08-20 23:09:01 +0800

    Reorganise based on order in documentation

--- src/config/config.ts
+++ src/config/config.ts
@@ -22,6 +22,13 @@ const isDev =
   process.env.NODE_ENV === Environment.Test
 const nodeEnv = isDev ? Environment.Dev : Environment.Prod
 
+// nodeEnv: {
+//   doc: 'Express environment mode',
+//   format: 'int',
+//   default: 0,
+//   env: 'NODE_ENV',
+// }
+
 convict.addFormat(require('convict-format-with-validator').url)
 convict.addFormat(require('convict-format-with-validator').email)
 
@@ -57,37 +64,6 @@ const validateBucketUrl = (val, { isDev, hasTrailingSlash }) => {
 }
 
 const configuration = convict({
-  sessionSecret: {
-    doc: 'Session Secret',
-    format: String,
-    default: defaults.app.sessionSecret,
-    env: 'SESSION_SECRET',
-  },
-  port: {
-    doc: 'Application Port',
-    format: 'port',
-    default: defaults.app.port,
-    env: 'PORT',
-  },
-  otpLifeSpan: {
-    doc:
-      'OTP Life Span for Login. (Should be in miliseconds, e.g. 1000 * 60 * 15 = 15 mins)',
-    format: 'int',
-    default: defaults.login.otpLifeSpan,
-    env: 'OTP_LIFE_SPAN',
-  },
-  bounceLifeSpan: {
-    doc: 'TTL of bounce documents in milliseconds',
-    format: 'int',
-    default: defaults.bounce.bounceLifeSpan,
-    env: 'BOUNCE_LIFE_SPAN',
-  },
-  submissionsTopUp: {
-    doc: 'Number of submissions to top up submissions statistic by',
-    format: 'int',
-    default: 0,
-    env: 'SUBMISSIONS_TOP_UP',
-  },
   appConfig: {
     title: {
       format: String,
@@ -120,72 +96,44 @@ const configuration = convict({
       env: 'APP_IMAGES',
     },
   },
-  formsgSdkMode: {
-    doc:
-      'Inform SDK which public keys are to be used to sign, encrypt, or decrypt data that is passed to it',
-    format: ['staging', 'production', 'development', 'test'],
-    default: 'production' as PackageMode,
-    env: 'FORMSG_SDK_MODE',
-  },
-  awsConfig: {
-    imageS3Bucket: {
-      doc: 'S3 Bucket to upload images to',
+  core: {
+    sessionSecret: {
+      doc: 'Session Secret',
       format: String,
-      default: null,
-      env: 'IMAGE_S3_BUCKET',
+      default: defaults.app.sessionSecret,
+      env: 'SESSION_SECRET',
     },
-    logoS3Bucket: {
-      doc: 'S3 Bucket to upload logos to',
+    dbHost: {
+      doc: 'Database URI',
       format: String,
-      default: null,
-      env: 'LOGO_S3_BUCKET',
+      default: '',
+      env: 'DB_HOST',
     },
-    attachmentS3Bucket: {
-      doc: 'S3 Bucket to upload encrypted attachments to',
+    port: {
+      doc: 'Application Port',
+      format: 'port',
+      default: defaults.app.port,
+      env: 'PORT',
+    },
+    otpLifeSpan: {
+      doc:
+        'OTP Life Span for Login. (Should be in miliseconds, e.g. 1000 * 60 * 15 = 15 mins)',
+      format: 'int',
+      default: defaults.login.otpLifeSpan,
+      env: 'OTP_LIFE_SPAN',
+    },
+    submissionsTopUp: {
+      doc: 'Number of submissions to top up submissions statistic by',
+      format: 'int',
+      default: 0,
+      env: 'SUBMISSIONS_TOP_UP',
+    },
+    cspReportUri: {
+      doc: 'Endpoint for content security policy reporting',
       format: String,
-      default: null,
-      env: 'ATTACHMENT_S3_BUCKET',
+      default: 'undefined', // HelmetJS reportUri param requires non-empty string
+      env: 'CSP_REPORT_URI',
     },
-    region: {
-      doc: 'Region that S3 bucket is located in',
-      format: Object.keys(awsInfo.data.regions),
-      default: defaults.aws.region,
-      env: 'AWS_REGION',
-    },
-    attachmentBucketUrl: {
-      format: (val) =>
-        validateBucketUrl(val, { isDev, hasTrailingSlash: true }),
-      default: null,
-    },
-    logoBucketUrl: {
-      format: (val) =>
-        validateBucketUrl(val, { isDev, hasTrailingSlash: false }),
-      default: null,
-    },
-    imageBucketUrl: {
-      format: (val) =>
-        validateBucketUrl(val, { isDev, hasTrailingSlash: false }),
-      default: null,
-    },
-  },
-  cspReportUri: {
-    doc: 'Endpoint for content security policy reporting',
-    format: String,
-    default: 'undefined', // HelmetJS reportUri param requires non-empty string
-    env: 'CSP_REPORT_URI',
-  },
-  chromiumBin: {
-    doc: 'Path to chromium executable for PDF generation',
-    format: String,
-    default: null,
-    env: 'CHROMIUM_BIN',
-  },
-  customCloudWatchGroup: {
-    doc:
-      'Name of CloudWatch log group to store short-term logs. Log streams are separated by date.',
-    format: String,
-    default: '',
-    env: 'CUSTOM_CLOUDWATCH_LOG_GROUP',
   },
   banner: {
     isGeneralMaintenance: {
@@ -214,11 +162,60 @@ const configuration = convict({
       env: 'ADMIN_BANNER_CONTENT',
     },
   },
-  dbHost: {
-    doc: 'Database URI',
-    format: String,
-    default: '',
-    env: 'DB_HOST',
+  awsConfig: {
+    imageS3Bucket: {
+      doc: 'S3 Bucket to upload images to',
+      format: String,
+      default: null,
+      env: 'IMAGE_S3_BUCKET',
+    },
+    logoS3Bucket: {
+      doc: 'S3 Bucket to upload logos to',
+      format: String,
+      default: null,
+      env: 'LOGO_S3_BUCKET',
+    },
+    attachmentS3Bucket: {
+      doc: 'S3 Bucket to upload encrypted attachments to',
+      format: String,
+      default: null,
+      env: 'ATTACHMENT_S3_BUCKET',
+    },
+    region: {
+      doc: 'Region that S3 bucket is located in',
+      format: Object.keys(awsInfo.data.regions),
+      default: defaults.aws.region,
+      env: 'AWS_REGION',
+    },
+    customCloudWatchGroup: {
+      doc:
+        'Name of CloudWatch log group to store short-term logs. Log streams are separated by date.',
+      format: String,
+      default: '',
+      env: 'CUSTOM_CLOUDWATCH_LOG_GROUP',
+    },
+    attachmentBucketUrl: {
+      format: (val) =>
+        validateBucketUrl(val, { isDev, hasTrailingSlash: true }),
+      default: null,
+    },
+    logoBucketUrl: {
+      format: (val) =>
+        validateBucketUrl(val, { isDev, hasTrailingSlash: false }),
+      default: null,
+    },
+    imageBucketUrl: {
+      format: (val) =>
+        validateBucketUrl(val, { isDev, hasTrailingSlash: false }),
+      default: null,
+    },
+  },
+  formsgSdkMode: {
+    doc:
+      'Inform SDK which public keys are to be used to sign, encrypt, or decrypt data that is passed to it',
+    format: ['staging', 'production', 'development', 'test'],
+    default: 'production' as PackageMode,
+    env: 'FORMSG_SDK_MODE',
   },
   mail: {
     from: {
@@ -240,8 +237,18 @@ const configuration = convict({
       default: false,
       env: 'MAIL_DEBUG',
     },
-  },
-  ses: {
+    bounceLifeSpan: {
+      doc: 'TTL of bounce documents in milliseconds',
+      format: 'int',
+      default: defaults.bounce.bounceLifeSpan,
+      env: 'BOUNCE_LIFE_SPAN',
+    },
+    chromiumBin: {
+      doc: 'Path to chromium executable for PDF generation',
+      format: String,
+      default: null,
+      env: 'CHROMIUM_BIN',
+    },
     maxMessages: {
       doc:
         'Nodemailer config to help to keep the connection up-to-date for long-running messaging',
@@ -331,7 +338,7 @@ configuration.validate({ allowed: 'strict' })
 const logger = createLoggerWithLabel(module)
 
 const dbConfig: DbConfig = {
-  uri: configuration.get('dbHost'),
+  uri: configuration.get('core.dbHost'),
   options: {
     user: '',
     pass: '',
@@ -371,9 +378,9 @@ const mailConfig: MailConfig = (function () {
       // Options as advised from https://nodemailer.com/usage/bulk-mail/
       // pool connections instead of creating fresh one for each email
       pool: true,
-      maxMessages: configuration.get('ses.maxMessages'),
-      maxConnections: configuration.get('ses.maxConnections'),
-      socketTimeout: configuration.get('ses.socketTimeout'),
+      maxMessages: configuration.get('mail.maxMessages'),
+      maxConnections: configuration.get('mail.maxConnections'),
+      socketTimeout: configuration.get('mail.socketTimeout'),
       // If set to true then logs to console. If value is not set or is false
       // then nothing is logged.
       logger: configuration.get('mail.logger'),
@@ -455,15 +462,15 @@ const config: Config = {
   cookieSettings,
   isDev,
   nodeEnv,
-  port: configuration.get('port'),
-  customCloudWatchGroup: configuration.get('customCloudWatchGroup'),
-  sessionSecret: configuration.get('sessionSecret'),
-  otpLifeSpan: configuration.get('otpLifeSpan'),
-  bounceLifeSpan: configuration.get('bounceLifeSpan'),
   formsgSdkMode: configuration.get('formsgSdkMode'),
-  chromiumBin: configuration.get('chromiumBin'),
-  cspReportUri: configuration.get('cspReportUri'),
-  submissionsTopUp: configuration.get('submissionsTopUp'),
+  customCloudWatchGroup: configuration.get('awsConfig.customCloudWatchGroup'),
+  bounceLifeSpan: configuration.get('mail.bounceLifeSpan'),
+  chromiumBin: configuration.get('mail.chromiumBin'),
+  port: configuration.get('core.port'),
+  sessionSecret: configuration.get('core.sessionSecret'),
+  otpLifeSpan: configuration.get('core.otpLifeSpan'),
+  cspReportUri: configuration.get('core.cspReportUri'),
+  submissionsTopUp: configuration.get('core.submissionsTopUp'),
   isGeneralMaintenance: configuration.get('banner.isGeneralMaintenance'),
   isLoginBanner: configuration.get('banner.isLoginBanner'),
   siteBannerContent: configuration.get('banner.siteBannerContent'),
