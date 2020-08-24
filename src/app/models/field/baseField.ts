ae5aa8f2c02a5865c9f2a8441d1d866e00b6f6be
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-24 17:02:20 +0800

    feat: remove beta field validations (#194)
    * refactor: migrate beta-permissions util to Typescript
    
    * fix(BaseFieldSchema): correct validation control flow
    
    * feat(BetaPermUtil): remove mobile field from beta
    
    * feat: remove checking of User beta permissions validation
    
    Since there is no more beta flags

--- src/app/models/field/baseField.ts
+++ src/app/models/field/baseField.ts
@@ -1,10 +1,6 @@
-import { Mongoose, Schema } from 'mongoose'
+import { Schema } from 'mongoose'
 import UIDGenerator from 'uid-generator'
 
-import {
-  isBetaField,
-  userCanCreateField,
-} from '../../../app/utils/beta-permissions'
 import {
   AuthType,
   BasicField,
@@ -14,7 +10,6 @@ import {
   MyInfoAttribute,
   ResponseMode,
 } from '../../../types'
-import getUserModel from '../user.server.model'
 
 const uidgen3 = new UIDGenerator(256, UIDGenerator.BASE62)
 
@@ -42,108 +37,75 @@ export const MyInfoSchema = new Schema<IMyInfoSchema>(
   },
 )
 
-const createBaseFieldSchema = (db: Mongoose) => {
-  const User = getUserModel(db)
-
-  const FieldSchema = new Schema<IFieldSchema>(
-    {
-      globalId: String,
-      title: {
-        type: String,
-        trim: true,
-        default: '',
-      },
-      description: {
-        type: String,
-        default: '',
-      },
-      required: {
-        type: Boolean,
-        default: true,
-      },
-      disabled: {
-        type: Boolean,
-        default: false,
-      },
-      fieldType: {
-        type: String,
-        enum: Object.values(BasicField),
-        required: true,
-      },
+export const BaseFieldSchema = new Schema<IFieldSchema>(
+  {
+    globalId: String,
+    title: {
+      type: String,
+      trim: true,
+      default: '',
     },
-    {
-      discriminatorKey: 'fieldType',
+    description: {
+      type: String,
+      default: '',
     },
-  )
+    required: {
+      type: Boolean,
+      default: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    fieldType: {
+      type: String,
+      enum: Object.values(BasicField),
+      required: true,
+    },
+  },
+  {
+    discriminatorKey: 'fieldType',
+  },
+)
 
-  // Hooks
-  FieldSchema.pre<IFieldSchema>('validate', function (next) {
-    // Required due to inner scopes
-    const thisField = this
-    // Invalid field types
-    if (!VALID_FIELD_TYPES.includes(thisField.fieldType)) {
-      return next(Error('Field type is incorrect or unspecified'))
-    }
-
-    const { responseMode, admin } = thisField.parent()
-
-    // Prevent MyInfo fields from being set in encrypt mode.
-    if (responseMode === ResponseMode.Encrypt) {
-      if (thisField.myInfo?.attr) {
-        return next(
-          Error('MyInfo fields are not allowed for storage mode forms'),
-        )
-      }
-    }
-
-    // Check if user is allowed to add this field.
-    if (isBetaField(thisField)) {
-      User.findById(admin, function (err, user) {
-        if (err) {
-          return next(
-            Error(
-              `Error validating user permissions for ${thisField.fieldType} fields`,
-            ),
-          )
-        }
-        if (!userCanCreateField(user, thisField)) {
-          return next(
-            Error(
-              `User is not allowed to access the beta feature - ${this.fieldType} fields`,
-            ),
-          )
-        }
-      })
-    }
-
-    return next()
-  })
-
-  FieldSchema.pre<IFieldSchema>('save', function (next) {
-    if (!this.globalId) {
-      this.globalId = uidgen3.generateSync()
-    }
-    return next()
-  })
-
-  // Instance methods
-  FieldSchema.methods.getQuestion = function (this: IFieldSchema) {
-    // Return concatenation of all column titles as question string.
-    if (isTableField(this)) {
-      const columnTitles = this.columns.map((col) => col.title)
-      return `${this.title} (${columnTitles.join(', ')})`
-    }
-
-    // Default question is the field title.
-    return this.title
+// Hooks
+BaseFieldSchema.pre<IFieldSchema>('validate', function (next) {
+  // Invalid field types
+  if (!VALID_FIELD_TYPES.includes(this.fieldType)) {
+    return next(Error('Field type is incorrect or unspecified'))
   }
 
-  return FieldSchema
+  // Prevent MyInfo fields from being set in encrypt mode.
+  if (this.parent().responseMode === ResponseMode.Encrypt) {
+    if (this.myInfo?.attr) {
+      return next(Error('MyInfo fields are not allowed for storage mode forms'))
+    }
+  }
+
+  // No errors.
+  return next()
+})
+
+BaseFieldSchema.pre<IFieldSchema>('save', function (next) {
+  if (!this.globalId) {
+    this.globalId = uidgen3.generateSync()
+  }
+  return next()
+})
+
+// Instance methods
+BaseFieldSchema.methods.getQuestion = function (this: IFieldSchema) {
+  // Return concatenation of all column titles as question string.
+  if (isTableField(this)) {
+    const columnTitles = this.columns.map((col) => col.title)
+    return `${this.title} (${columnTitles.join(', ')})`
+  }
+
+  // Default question is the field title.
+  return this.title
 }
 
 // Typeguards
 const isTableField = (field: IFieldSchema): field is ITableFieldSchema => {
   return field.fieldType === BasicField.Table
 }
-
-export default createBaseFieldSchema
