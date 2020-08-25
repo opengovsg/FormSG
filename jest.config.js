e2909d8cdc887a7a4f7a5c6266d10e9a72939bdb
liangyuanruo <liangyuanruo@gmail.com>
2020-08-25 10:23:08 +0800

    fix(jest): only collect coverage from TS files, bump threshold to 14%

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,11 @@ module.exports = {
     // Needed to use @shelf/jest-mongodb preset.
     ...tsJestPreset.transform,
   },
-  collectCoverageFrom: ['./src/**/*.{ts,js}'],
+  collectCoverageFrom: ['./src/**/*.{ts}'],
   coveragePathIgnorePatterns: ['./node_modules/', './tests'],
   coverageThreshold: {
     global: {
-      statements: 12, // Increase this percentage as test coverage improves
+      statements: 14, // Increase this percentage as test coverage improves
     },
   },
 }
