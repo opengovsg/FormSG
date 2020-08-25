cbb927e1e80718a103f2e2628669d37b926ffe0c
Antariksh Mahajan <antarikshmahajan@gmail.com>
2020-08-25 17:14:11 +0800

    test: test everything else

--- tests/unit/backend/utils/attachment.spec.ts
+++ tests/unit/backend/utils/attachment.spec.ts
@@ -1,7 +1,19 @@
 import { ObjectId } from 'bson'
 import { readFileSync } from 'fs'
+import { cloneDeep, merge } from 'lodash'
 
-import { attachmentsAreValid } from 'src/app/utils/attachment'
+import {
+  addAttachmentToResponses,
+  areAttachmentsMoreThan7MB,
+  attachmentsAreValid,
+  handleDuplicatesInAttachments,
+  mapAttachmentsFromParsedResponses,
+} from 'src/app/utils/attachment'
+import {
+  BasicField,
+  IAttachmentResponse,
+  ISingleAnswerResponse,
+} from 'src/types'
 
 const validSingleFile = {
   filename: 'govtech.jpg',
@@ -56,6 +68,8 @@ const zipOnlyValid = {
 //   content: readFileSync('./tests/unit/backend/resources/repeated.zip'),
 //   fieldId: String(new ObjectId()),
 // }
+
+const MOCK_ANSWER = 'mockAnswer'
 
 describe('attachmentsAreValid', () => {
   test('returns true for a single valid file', async () => {
@@ -123,4 +137,133 @@ describe('attachmentsAreValid', () => {
   test('returns true for nested zips with invalid filetypes', async () => {
     return expect(attachmentsAreValid([zipNestedInvalid])).resolves.toBe(false)
   })
+})
+
+describe('addAttachmentToResponses', () => {
+  test('adds attachments to responses correctly', async () => {
+    const attachments = [validSingleFile, zipOnlyValid]
+    const responses = [
+      getResponse(attachments[0].fieldId, attachments[0].filename),
+      getResponse(attachments[1].fieldId, attachments[1].filename),
+    ]
+    addAttachmentToResponses(responses, attachments)
+    expect(responses[0].answer).toBe(attachments[0].filename)
+    expect((responses[0] as IAttachmentResponse).filename).toBe(
+      attachments[0].filename,
+    )
+    expect((responses[0] as IAttachmentResponse).content).toEqual(
+      attachments[0].content,
+    )
+    expect(responses[1].answer).toBe(attachments[1].filename)
+    expect((responses[1] as IAttachmentResponse).filename).toBe(
+      attachments[1].filename,
+    )
+    expect((responses[1] as IAttachmentResponse).content).toEqual(
+      attachments[1].content,
+    )
+  })
+
+  test('overwrites answer with filename', async () => {
+    const attachments = [validSingleFile]
+    const responses = [getResponse(attachments[0].fieldId, MOCK_ANSWER)]
+    addAttachmentToResponses(responses, attachments)
+    expect(responses[0].answer).toBe(attachments[0].filename)
+    expect((responses[0] as IAttachmentResponse).filename).toBe(
+      attachments[0].filename,
+    )
+    expect((responses[0] as IAttachmentResponse).content).toEqual(
+      attachments[0].content,
+    )
+  })
+
+  test('does nothing for empty responses', async () => {
+    const responses = []
+    addAttachmentToResponses(responses, [validSingleFile])
+    expect(responses).toEqual([])
+  })
+
+  test('does nothing when there are no attachments', async () => {
+    const responses = [getResponse(validSingleFile.fieldId, MOCK_ANSWER)]
+    addAttachmentToResponses(responses, [])
+    expect(responses).toEqual([
+      getResponse(validSingleFile.fieldId, MOCK_ANSWER),
+    ])
+  })
+})
+
+describe('areAttachmentsMoreThan7MB', () => {
+  test('passes attachments smaller than 7MB', async () => {
+    expect(areAttachmentsMoreThan7MB([validSingleFile, zipOnlyValid])).toBe(
+      false,
+    )
+  })
+
+  test('fails a single attachment larger than 7MB', async () => {
+    const modifiedBigFile = cloneDeep(validSingleFile)
+    modifiedBigFile.content = Buffer.alloc(7000001)
+    expect(areAttachmentsMoreThan7MB([modifiedBigFile])).toBe(true)
+  })
+
+  test('fails attachments which add up to more than 7MB', async () => {
+    const modifiedBigFile1 = cloneDeep(validSingleFile)
+    const modifiedBigFile2 = cloneDeep(validSingleFile)
+    modifiedBigFile1.content = Buffer.alloc(3500000)
+    modifiedBigFile2.content = Buffer.alloc(3500001)
+    expect(
+      areAttachmentsMoreThan7MB([modifiedBigFile1, modifiedBigFile2]),
+    ).toBe(true)
+  })
+})
+
+// Note that if e.g. you have three attachments called abc.txt, abc.txt
+// and 1-abc.txt, they will not be given unique names, i.e. one of the abc.txt
+// will be renamed to 1-abc.txt so you end up with abc.txt, 1-abc.txt and 1-abc.txt.
+describe('handleDuplicatesInAttachments', () => {
+  test('makes filenames unique by appending count', async () => {
+    const attachments = [
+      cloneDeep(validSingleFile),
+      cloneDeep(validSingleFile),
+      cloneDeep(validSingleFile),
+    ]
+    handleDuplicatesInAttachments(attachments)
+    const newFilenames = attachments.map((att) => att.filename)
+    // Expect uniqueness
+    expect(newFilenames.length).toBe(new Set(newFilenames).size)
+    expect(newFilenames).toContain(validSingleFile.filename)
+    expect(newFilenames).toContain(`1-${validSingleFile.filename}`)
+    expect(newFilenames).toContain(`2-${validSingleFile.filename}`)
+  })
+})
+
+describe('mapAttachmentsFromParsedResponses', () => {
+  test('filters out non-attachment fields', async () => {
+    const response = getResponse(String(new ObjectId()), MOCK_ANSWER)
+    response.fieldType = BasicField.YesNo
+    expect(mapAttachmentsFromParsedResponses([response])).toEqual([])
+  })
+
+  test('correctly extracts filename and content', async () => {
+    const attachments = [validSingleFile, zipOnlyValid]
+    const responses = [
+      merge(getResponse(attachments[0].fieldId, MOCK_ANSWER), attachments[0]),
+      merge(getResponse(attachments[1].fieldId, MOCK_ANSWER), attachments[1]),
+    ]
+    const result = mapAttachmentsFromParsedResponses(responses)
+    expect(result.length).toBe(responses.length)
+    expect(result[0]).toEqual({
+      filename: attachments[0].filename,
+      content: attachments[0].content,
+    })
+    expect(result[1]).toEqual({
+      filename: attachments[1].filename,
+      content: attachments[1].content,
+    })
+  })
+})
+
+const getResponse = (_id: string, answer: string): ISingleAnswerResponse => ({
+  _id,
+  fieldType: BasicField.Attachment,
+  question: 'mockQuestion',
+  answer,
 })
