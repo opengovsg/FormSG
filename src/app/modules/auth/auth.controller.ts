286f208b7330aeb0d7664b27a246c6557e2cfc1d
Kar Rui Lau <karrui.lau@gmail.com>
2020-08-26 12:10:44 +0800

    refactor(AuthMiddleware): extract validateDomain from AuthService

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,46 +1,10 @@
-import to from 'await-to-js'
 import { RequestHandler } from 'express'
 import HttpStatus from 'http-status-codes'
 
-import defaults from '../../../config/defaults'
-import { createLoggerWithLabel } from '../../../config/logger'
-import { getRequestIp } from '../../utils/request'
-
-import { InvalidDomainError } from './auth.errors'
-import * as AuthService from './auth.service'
-
-const logger = createLoggerWithLabel('auth.controller')
-
-export const handleCheckUser: RequestHandler<
-  {},
-  {},
-  { email: string }
-> = async (req, res) => {
-  // Joi validation ensures existence.
-  const { email } = req.body
-
-  const [validationError] = await to(AuthService.validateDomain(email))
-
-  if (validationError) {
-    logger.error({
-      message: 'Domain validation error',
-      meta: {
-        action: 'handleCheckUser',
-        ip: getRequestIp(req),
-        email,
-      },
-      error: validationError,
-    })
-    if (validationError instanceof InvalidDomainError) {
-      return res.status(validationError.status).send(validationError.message)
-    }
-    return res
-      .status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .send(
-        `Unable to validate email domain. If this issue persists, please submit a Support Form at (${defaults.links.supportFormLink}).`,
-      )
-  }
-
-  // No error.
+/**
+ * Precondition: AuthMiddlewares.validateDomain must precede this handler.
+ * @returns 200 regardless, assumed to have passed domain validation.
+ */
+export const handleCheckUser: RequestHandler = async (_req, res) => {
   return res.sendStatus(HttpStatus.OK)
 }
---
+++ src/app/modules/auth/auth.middlewares.ts
@@ -0,0 +1,45 @@
+import to from 'await-to-js'
+import { RequestHandler } from 'express'
+import HttpStatus from 'http-status-codes'
+
+import defaults from '../../../config/defaults'
+import { createLoggerWithLabel } from '../../../config/logger'
+import { getRequestIp } from '../../utils/request'
+
+import { InvalidDomainError } from './auth.errors'
+import * as AuthService from './auth.service'
+
+const logger = createLoggerWithLabel('auth.middlewares')
+
+export const validateDomain: RequestHandler<{}, {}, { email: string }> = async (
+  req,
+  res,
+  next,
+) => {
+  // Joi validation ensures existence.
+  const { email } = req.body
+
+  const [validationError] = await to(AuthService.validateDomain(email))
+
+  if (validationError) {
+    logger.error({
+      message: 'Domain validation error',
+      meta: {
+        action: 'validateDomain',
+        ip: getRequestIp(req),
+        email,
+      },
+      error: validationError,
+    })
+    if (validationError instanceof InvalidDomainError) {
+      return res.status(validationError.status).send(validationError.message)
+    }
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .send(
+        `Unable to validate email domain. If this issue persists, please submit a Support Form at (${defaults.links.supportFormLink}).`,
+      )
+  }
+
+  return next()
+}
