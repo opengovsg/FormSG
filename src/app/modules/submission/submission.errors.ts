6640ec7d193cc66e24b6001966a0f797cb84538d
Arshad Ali <arshadali@Arshads-MacBook-Pro.local>
2020-08-27 12:32:01 +0800

    JSDoc the custom errors

--- src/app/modules/submission/submission.errors.ts
+++ src/app/modules/submission/submission.errors.ts
@@ -2,6 +2,10 @@ import HttpStatus from 'http-status-codes'
 
 import { ApplicationError } from '../core/core.errors'
 
+/**
+ * A custom error class thrown by the submission controllers
+ * when some form fields are missing from the submission
+ */
 export class ConflictError extends ApplicationError {
   constructor(message: string, meta?: string) {
     super(message, HttpStatus.CONFLICT, meta)
--- src/app/modules/webhooks/webhook.errors.ts
+++ src/app/modules/webhooks/webhook.errors.ts
@@ -2,6 +2,10 @@ import HttpStatus from 'http-status-codes'
 
 import { ApplicationError } from '../core/core.errors'
 
+/**
+ * A custom error class thrown by the webhook server controller
+ * if the submissionWebhookView is null or the webhookUrl is an invalid URL
+ */
 export class WebhookValidationError extends ApplicationError {
   constructor(message: string, meta?: string) {
     super(message, HttpStatus.UNPROCESSABLE_ENTITY, meta)
