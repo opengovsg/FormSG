6549ed6770c44436cb42723894de5a7b233e7676
arshadali172 <arshad172ali@gmail.com>
2020-09-07 11:50:21 +0800

    Revert "revert(convict): "refactor: use convict for configuration (#190)" (#285)"
    This reverts commit 3e61acae4d43c314d8759b4a9be96f953f60fd2b.

--- init-localstack.sh
+++ init-localstack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
-until $(curl --output /dev/null --silent --head --fail http://localhost:4572); do
+until $(curl --output /dev/null --silent --head --fail $AWS_ENDPOINT); do
   printf 'Waiting for Localstack to be ready...'
   sleep 5
 done
-awslocal --endpoint-url=http://localhost:4572 s3 mb s3://$IMAGE_S3_BUCKET
-awslocal --endpoint-url=http://localhost:4572 s3 mb s3://$LOGO_S3_BUCKET
-awslocal --endpoint-url=http://localhost:4572 s3 mb s3://$ATTACHMENT_S3_BUCKET
+awslocal --endpoint-url=$AWS_ENDPOINT s3 mb s3://$IMAGE_S3_BUCKET
+awslocal --endpoint-url=$AWS_ENDPOINT s3 mb s3://$LOGO_S3_BUCKET
+awslocal --endpoint-url=$AWS_ENDPOINT s3 mb s3://$ATTACHMENT_S3_BUCKET
 set +x
