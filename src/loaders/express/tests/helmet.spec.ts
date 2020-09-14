48870257db588ad88ae9cbd4f07f6fe059399cc0
shuli-ogp <shuli@open.gov.sg>
2020-09-14 16:11:48 +0800

    chore: simplify csp tests and make them stricter

--- src/loaders/express/tests/helmet.spec.ts
+++ src/loaders/express/tests/helmet.spec.ts
@@ -47,115 +47,81 @@ describe('helmetMiddlewares', () => {
     expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalled()
   })
 
-  it('should call helmet.contentSecurityPolicy() with the correct cspCoreDirectives', () => {
-    const cspCoreDirectives = {
-      defaultSrc: ["'self'"],
-      imgSrc: [
-        "'self'",
-        'data:',
-        'https://www.googletagmanager.com/',
-        'https://www.google-analytics.com/',
-        `https://s3-${config.aws.region}.amazonaws.com/agency.form.sg/`,
-        config.aws.imageBucketUrl,
-        config.aws.logoBucketUrl,
-        '*',
-      ],
-      fontSrc: ["'self'", 'data:', 'https://fonts.gstatic.com/'],
-      scriptSrc: [
-        "'self'",
-        'https://www.googletagmanager.com/',
-        'https://ssl.google-analytics.com/',
-        'https://www.google-analytics.com/',
-        'https://www.tagmanager.google.com/',
-        'https://www.google.com/recaptcha/',
-        'https://www.recaptcha.net/recaptcha/',
-        'https://www.gstatic.com/recaptcha/',
-        'https://www.gstatic.cn/',
-        'https://www.google-analytics.com/',
-      ],
-      connectSrc: [
-        "'self'",
-        'https://www.google-analytics.com/',
-        'https://ssl.google-analytics.com/',
-        'https://sentry.io/api/',
-        config.aws.attachmentBucketUrl,
-        config.aws.imageBucketUrl,
-        config.aws.logoBucketUrl,
-      ],
-      frameSrc: [
-        "'self'",
-        'https://www.google.com/recaptcha/',
-        'https://www.recaptcha.net/recaptcha/',
-      ],
-      objectSrc: ["'none'"],
-      styleSrc: [
-        "'self'",
-        'https://www.google.com/recaptcha/',
-        'https://www.recaptcha.net/recaptcha/',
-        'https://www.gstatic.com/recaptcha/',
-        'https://www.gstatic.cn/',
-        // For inline styles in angular-sanitize.js
-        "'sha256-b3IrgBVvuKx/Q3tmAi79fnf6AFClibrz/0S5x1ghdGU='",
-      ],
-      formAction: ["'self'"],
-    }
-    helmetMiddlewares()
-    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        directives: expect.objectContaining({
-          ...cspCoreDirectives,
-        }),
-      }),
-    )
-  })
+  const cspCoreDirectives = {
+    defaultSrc: ["'self'"],
+    imgSrc: [
+      "'self'",
+      'data:',
+      'https://www.googletagmanager.com/',
+      'https://www.google-analytics.com/',
+      `https://s3-${config.aws.region}.amazonaws.com/agency.form.sg/`,
+      config.aws.imageBucketUrl,
+      config.aws.logoBucketUrl,
+      '*',
+    ],
+    fontSrc: ["'self'", 'data:', 'https://fonts.gstatic.com/'],
+    scriptSrc: [
+      "'self'",
+      'https://www.googletagmanager.com/',
+      'https://ssl.google-analytics.com/',
+      'https://www.google-analytics.com/',
+      'https://www.tagmanager.google.com/',
+      'https://www.google.com/recaptcha/',
+      'https://www.recaptcha.net/recaptcha/',
+      'https://www.gstatic.com/recaptcha/',
+      'https://www.gstatic.cn/',
+      'https://www.google-analytics.com/',
+    ],
+    connectSrc: [
+      "'self'",
+      'https://www.google-analytics.com/',
+      'https://ssl.google-analytics.com/',
+      'https://sentry.io/api/',
+      config.aws.attachmentBucketUrl,
+      config.aws.imageBucketUrl,
+      config.aws.logoBucketUrl,
+    ],
+    frameSrc: [
+      "'self'",
+      'https://www.google.com/recaptcha/',
+      'https://www.recaptcha.net/recaptcha/',
+    ],
+    objectSrc: ["'none'"],
+    styleSrc: [
+      "'self'",
+      'https://www.google.com/recaptcha/',
+      'https://www.recaptcha.net/recaptcha/',
+      'https://www.gstatic.com/recaptcha/',
+      'https://www.gstatic.cn/',
+      // For inline styles in angular-sanitize.js
+      "'sha256-b3IrgBVvuKx/Q3tmAi79fnf6AFClibrz/0S5x1ghdGU='",
+    ],
+    formAction: ["'self'"],
+  }
 
-  it('should call helmet.contentSecurityPolicy() with the correct reportUri if cspReportUri', () => {
+  it('should call helmet.contentSecurityPolicy() with the correct directives if cspReportUri and !isDev', () => {
     mockFeatureManager.props = jest
       .fn()
       .mockReturnValue({ cspReportUri: 'value' })
-    helmetMiddlewares()
-    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        directives: expect.objectContaining({
-          reportUri: ['value'],
-        }),
-      }),
-    )
-  })
-
-  it('should call helmet.contentSecurityPolicy() without a reportUri if !cspReportUri', () => {
-    helmetMiddlewares()
-    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        directives: expect.not.objectContaining({
-          reportUri: expect.anything(),
-        }),
-      }),
-    )
-  })
-
-  it('should call helmet.contentSecurityPolicy() with upgradeInsecureRequests if !isDev', () => {
     mockConfig.isDev = false
     helmetMiddlewares()
-    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        directives: expect.objectContaining({
-          upgradeInsecureRequests: [],
-        }),
-      }),
-    )
+    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith({
+      directives: {
+        ...cspCoreDirectives,
+        reportUri: ['value'],
+        upgradeInsecureRequests: [],
+      },
+    })
   })
 
-  it('should call helmet.contentSecurityPolicy() without upgradeInsecureRequests if isDev', () => {
+  it('should call helmet.contentSecurityPolicy() with the correct directives if !cspReportUri and isDev', () => {
     mockConfig.isDev = true
     helmetMiddlewares()
-    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        directives: expect.not.objectContaining({
-          upgradeInsecureRequests: expect.anything(),
-        }),
-      }),
-    )
+    expect(mockHelmet.contentSecurityPolicy).toHaveBeenCalledWith({
+      directives: {
+        ...cspCoreDirectives,
+      },
+    })
   })
 
   it('should return the correct values from helmet', () => {
