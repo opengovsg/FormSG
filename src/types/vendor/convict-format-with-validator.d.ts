afe173267b86fa5d1a56ffca1003381a39f5dbd4
Antariksh Mahajan <antarikshmahajan@gmail.com>
2020-09-14 13:31:44 +0800

    fix(schema): fix errors from strict typing

--- src/types/vendor/convict-format-with-validator.d.ts
+++ src/types/vendor/convict-format-with-validator.d.ts
@@ -2,27 +2,21 @@ declare module 'convict-format-with-validator' {
   function coerce(v): string
   function validate(x): void
 
-  const url = {
+  export const url = {
     name: 'url' as const,
     coerce,
     validate,
   }
 
-  const ipaddress = {
+  export const ipaddress = {
     name: 'ipaddress' as const,
     coerce,
     validate,
   }
 
-  const email = {
+  export const email = {
     name: 'email' as const,
     coerce,
     validate,
-  }
-
-  export default {
-    url,
-    ipaddress,
-    email,
   }
 }
