c63face26c8c53e1c7dc93b101d9f43d7ed0fbcf
Antariksh Mahajan <antarikshmahajan@gmail.com>
2020-09-16 12:01:03 +0800

    refactor: rename snsService to BounceService

--- src/app/modules/bounce/bounce.controller.ts
+++ src/app/modules/bounce/bounce.controller.ts
@@ -5,7 +5,7 @@ import { StatusCodes } from 'http-status-codes'
 import { createLoggerWithLabel } from '../../../config/logger'
 import { ISnsNotification } from '../../../types'
 
-import * as snsService from './bounce.service'
+import * as BounceService from './bounce.service'
 
 const logger = createLoggerWithLabel(module)
 /**
@@ -23,11 +23,11 @@ export const handleSns: RequestHandler<
   // so we never fail on malformed input. The response code is meaningless since
   // it is meant to go back to AWS.
   try {
-    const isValid = await snsService.isValidSnsRequest(req.body)
+    const isValid = await BounceService.isValidSnsRequest(req.body)
     if (!isValid) {
       return res.sendStatus(StatusCodes.FORBIDDEN)
     }
-    await snsService.updateBounces(req.body)
+    await BounceService.updateBounces(req.body)
     return res.sendStatus(StatusCodes.OK)
   } catch (err) {
     logger.warn({
