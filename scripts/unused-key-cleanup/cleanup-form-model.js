777e0e424ed609295a8b54bdb6440a76e3125b2d
Arshad Ali <arshadali@Arshads-MacBook-Pro.local>
2020-09-23 14:55:11 +0800

    Account for ; in emails and use bulk write

--- scripts/unused-key-cleanup/cleanup-form-model.js
+++ scripts/unused-key-cleanup/cleanup-form-model.js
@@ -9,7 +9,13 @@ db.getCollection('forms').find({ responseMode: 'email' }).count()
 
 // Check total forms count with responseMode email and emails as an array 
 // ~ Should be the same number as the total number of email mode forms
-db.getCollection('forms').find({ responseMode: 'email', "emails" : { $type : "array" } }).count()
+db.getCollection('forms').find({ responseMode: 'email', "emails": { $type: "array" } }).count()
+
+// Check total forms count with responseMode email and emails with delimiter ; 
+db.getCollection('forms').find({ emails: { $regex: /;/ }, responseMode: 'email' }).count()
+
+// Check total forms count with responseMode email and emails with delimiter ,
+db.getCollection('forms').find({ emails: { $regex: /,/ }, responseMode: 'email' }).count()
 
 // !!!! MAIN UPDATE SCRIPT !!!!
 
@@ -18,12 +24,34 @@ db.getCollection('forms').find({ responseMode: 'email', "emails" : { $type : "ar
 // ['test@hotmail.com', 'test@gmail.com'] => ['test@hotmail.com', 'test@gmail.com'] ~ unchanged
 // ['test@hotmail.com, test@gmail.com'] => ['test@hotmail.com', 'test@gmail.com']
 // ['test@hotmail.com, test@gmail.com', 'test@yahoo.com'] => ['test@hotmail.com', 'test@gmail.com', 'test@yahoo.com']
+// ['test@hotmail.com; test@gmail.com; test@yahoo.com'] => ['test@hotmail.com', 'test@gmail.com', 'test@yahoo.com']
+// ['test@hotmail.com; test@gmail.com; test@yahoo.com;'] => ['test@hotmail.com', 'test@gmail.com', 'test@yahoo.com']
+// ['wee_ching_ni@pa.gov.sg;'] => ['wee_ching_ni@pa.gov.sg']
 
 // Update structure of emails key
-db.getCollection('forms').find({ responseMode: 'email' }).forEach((form) => { 
-    let parsedEmails = form.emails.join(',').split(',').map(item => item.trim())
-    db.getCollection('forms').findOneAndUpdate({ _id: form._id }, { $set: { emails: parsedEmails } }) 
+let requests = []
+db.getCollection('forms').find({ responseMode: 'email' }).forEach((form) => {
+  let parsedEmails = form.emails
+    .join(',')
+    .replace(/;/g, ',')
+    .split(',')
+    .map(item => item.trim())
+    .filter((email) => email.includes('@')) // remove ""
+  requests.push({
+    updateOne: {
+      filter: { _id: form._id },
+      update: { $set: { emails: parsedEmails } }
+    }
+  })
+  if (requests.length === 500) {
+    //Execute per 500 operations and re-init
+    db.collection.bulkWrite(requests);
+    requests = [];
+  }
 })
+if (requests.length > 0) {
+  db.collection.bulkWrite(requests);
+}
 
 // !!!! END MAIN UPDATE SCRIPT !!!!
 
@@ -33,4 +61,12 @@ db.getCollection('forms').find({ responseMode: 'email' }).count()
 
 // Check total forms count with responseMode email and emails as an array 
 // ~ Should be the same number as the total number of email mode forms
-db.getCollection('forms').find({ responseMode: 'email', "emails" : { $type : "array" } }).count()
+db.getCollection('forms').find({ responseMode: 'email', "emails": { $type: "array" } }).count()
+
+// Check total forms count with responseMode email and emails with delimiter ; 
+// ~ should be zero
+db.getCollection('forms').find({ emails: { $regex: /;/ }, responseMode: 'email' }).count()
+
+// Check total forms count with responseMode email and emails with delimiter ,
+// ~ should be zero
+db.getCollection('forms').find({ emails: { $regex: /,/ }, responseMode: 'email' }).count()
