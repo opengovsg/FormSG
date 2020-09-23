fc79596c4aca91f383d0ce9487df7c990a783086
JIA YEE <jiayeerawr@gmail.com>
2020-09-23 20:51:52 +0800

    feat: trigger toast when isVerifiable is switched off

--- src/public/modules/forms/admin/directives/is-verifiable-save-interceptor.directive.js
+++ src/public/modules/forms/admin/directives/is-verifiable-save-interceptor.directive.js
@@ -2,14 +2,20 @@
 
 angular
   .module('forms')
-  .directive('isVerifiableSaveInterceptor', isVerifiableSaveInterceptor)
+  .directive('isVerifiableSaveInterceptor', [
+    'Toastr',
+    isVerifiableSaveInterceptor,
+  ])
 
-function isVerifiableSaveInterceptor() {
+function isVerifiableSaveInterceptor(Toastr) {
   return {
     require: 'ngModel',
     link: (scope, elem, attr, ngModel) => {
       ngModel.$parsers.push((inputValue) => {
         if (!inputValue) {
+          Toastr.error(
+            'Turn on OTP verification again if you wish to restrict email domains.',
+          )
           scope.vm.field.hasAllowedEmailDomains = false
         }
         return inputValue
