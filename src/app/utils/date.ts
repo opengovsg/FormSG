4465ad4522885fac1063099018bb33d5d5505f2f
Kar Rui Lau <karrui.lau@gmail.com>
2020-09-28 16:35:02 +0800

    feat(DateUtils): add formatToRelativeString fn

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -19,3 +19,20 @@ export const createQueryWithDateParam = (
     return {}
   }
 }
+
+/**
+ * Function to format given date to a relative string representation.
+ * @param date the date to format to its relative string representation
+ * @returns the relative string representation of the given date
+ */
+export const formatToRelativeString = (date: Date): string => {
+  const timeDiffDays = moment().diff(date, 'days')
+
+  if (timeDiffDays <= 1) {
+    return 'less than 1 day ago'
+  } else if (timeDiffDays < 30) {
+    return `${timeDiffDays} days ago`
+  } else {
+    return moment(date).format('D MMM, YYYY')
+  }
+}
