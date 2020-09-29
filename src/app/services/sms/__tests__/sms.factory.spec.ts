46c5457b85dd3432a2e9112b8d7049c90542ed77
Kar Rui Lau <karrui.lau@gmail.com>
2020-09-29 23:37:47 +0800

    test(SmsFactory): add tests for sms feature enabled state

--- src/app/services/sms/__tests__/sms.factory.spec.ts
+++ src/app/services/sms/__tests__/sms.factory.spec.ts
@@ -1,3 +1,5 @@
+import Twilio from 'twilio'
+
 import {
   FeatureNames,
   ISms,
@@ -5,8 +7,23 @@ import {
 } from 'src/config/feature-manager'
 
 import { createSmsFactory } from '../sms.factory'
+import * as SmsService from '../sms.service'
+import { TwilioConfig } from '../sms.types'
+
+// This is hoisted and thus a const cannot be passed in.
+jest.mock('twilio', () =>
+  jest.fn().mockImplementation(() => ({
+    mocked: 'this is mocked',
+  })),
+)
+
+const MOCKED_TWILIO = ({
+  mocked: 'this is mocked',
+} as unknown) as Twilio.Twilio
 
 describe('sms.factory', () => {
+  beforeEach(() => jest.clearAllMocks())
+
   describe('sms feature disabled', () => {
     const MOCK_DISABLED_SMS_FEATURE: RegisteredFeature<FeatureNames.Sms> = {
       isEnabled: false,
@@ -34,6 +51,76 @@ describe('sms.factory', () => {
       // Assert
       expect(invocation).toThrowError(
         'sendVerificationOtp: SMS feature must be enabled in Feature Manager first',
+      )
+    })
+  })
+
+  describe('sms feature enabled', () => {
+    const MOCK_ENABLED_SMS_FEATURE: RegisteredFeature<FeatureNames.Sms> = {
+      isEnabled: true,
+      props: {
+        twilioAccountSid: 'ACrandomTwilioSid',
+        twilioApiKey: 'SKrandomTwilioAPIKEY',
+        twilioApiSecret: 'this is a super secret',
+        twilioMsgSrvcSid: 'formsg-is-great-pleasehelpme',
+      },
+    }
+
+    const SmsFactory = createSmsFactory(MOCK_ENABLED_SMS_FEATURE)
+
+    it('should call SmsService counterpart when invoking sendAdminContactOtp', async () => {
+      // Arrange
+      const serviceContactSpy = jest
+        .spyOn(SmsService, 'sendAdminContactOtp')
+        .mockResolvedValue(true)
+
+      const mockArguments: Parameters<typeof SmsFactory.sendAdminContactOtp> = [
+        'mockRecipient',
+        'mockOtp',
+        'mockFormId',
+      ]
+
+      // Act
+      await SmsFactory.sendAdminContactOtp(...mockArguments)
+
+      // Assert
+      const expectedTwilioConfig: TwilioConfig = {
+        msgSrvcSid: MOCK_ENABLED_SMS_FEATURE.props.twilioMsgSrvcSid,
+        client: MOCKED_TWILIO,
+      }
+
+      expect(serviceContactSpy).toHaveBeenCalledTimes(1)
+      expect(serviceContactSpy).toHaveBeenCalledWith(
+        ...mockArguments,
+        expectedTwilioConfig,
+      )
+    })
+
+    it('should call SmsService counterpart when invoking sendVerificationOtp', async () => {
+      // Arrange
+      const serviceVfnSpy = jest
+        .spyOn(SmsService, 'sendVerificationOtp')
+        .mockResolvedValue(true)
+
+      const mockArguments: Parameters<typeof SmsFactory.sendAdminContactOtp> = [
+        'mockRecipient',
+        'mockOtp',
+        'mockUserId',
+      ]
+
+      // Act
+      await SmsFactory.sendVerificationOtp(...mockArguments)
+
+      // Assert
+      const expectedTwilioConfig: TwilioConfig = {
+        msgSrvcSid: MOCK_ENABLED_SMS_FEATURE.props.twilioMsgSrvcSid,
+        client: MOCKED_TWILIO,
+      }
+
+      expect(serviceVfnSpy).toHaveBeenCalledTimes(1)
+      expect(serviceVfnSpy).toHaveBeenCalledWith(
+        ...mockArguments,
+        expectedTwilioConfig,
       )
     })
   })
