098f73afc2eb1a8688a5bbd239774ff3136ac299
Kar Rui Lau <karrui.lau@gmail.com>
2020-09-29 21:30:38 +0800

    feat: add more info to SmsSendError for Twilio error codes and status

--- src/app/services/sms/sms.errors.ts
+++ src/app/services/sms/sms.errors.ts
@@ -1,9 +1,16 @@
 import { ApplicationError } from '../../modules/core/core.errors'
 
 export class SmsSendError extends ApplicationError {
+  code?: number
+  sendStatus: unknown
+
   constructor(
     message = 'Error sending OTP. Please try again later and if the problem persists, contact us.',
+    code?: number,
+    sendStatus?: unknown,
   ) {
     super(message)
+    this.code = code
+    this.sendStatus = sendStatus
   }
 }
