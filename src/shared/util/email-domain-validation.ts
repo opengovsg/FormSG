1f53060766bc8cf209d8fb3993890805c4df049e
JIA YEE <jiayeerawr@gmail.com>
2020-09-29 22:00:43 +0800

    docs: explain why we need to prepend "bob" to email domains during validation

--- src/shared/util/email-domain-validation.ts
+++ src/shared/util/email-domain-validation.ts
@@ -2,6 +2,8 @@ import { isEmpty } from 'lodash'
 import validator from 'validator'
 
 export const validateEmailDomains = (emailDomains: string[]): boolean => {
+  // We need to prepend "bob" to the email domain so that it can form an email address and can be
+  // validated by validator.isEmail.
   return (
     isEmpty(emailDomains) ||
     (new Set(emailDomains).size === emailDomains.length &&
