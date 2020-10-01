823f4b8dd6554e77aa9230652b903f53270bffd0
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-01 14:21:59 +0800

    feat(AnalyticsService): add getSubmissionsCount

--- src/app/modules/analytics/analytics.service.ts
+++ src/app/modules/analytics/analytics.service.ts
@@ -2,9 +2,11 @@ import mongoose from 'mongoose'
 import { ResultAsync } from 'neverthrow'
 
 import { createLoggerWithLabel } from '../../../config/logger'
+import getSubmissionModel from '../../models/submission.server.model'
 import getUserModel from '../../models/user.server.model'
 import { DatabaseError } from '../core/core.errors'
 
+const SubmissionModel = getSubmissionModel(mongoose)
 const UserModel = getUserModel(mongoose)
 const logger = createLoggerWithLabel(module)
 
@@ -21,6 +23,23 @@ export const getUsersCount = (): ResultAsync<number, DatabaseError> => {
         message: 'Database error when retrieving user collection count',
         meta: {
           action: 'getUsersCount',
+        },
+        error,
+      })
+
+      return new DatabaseError()
+    },
+  )
+}
+
+export const getSubmissionsCount = (): ResultAsync<number, DatabaseError> => {
+  return ResultAsync.fromPromise(
+    SubmissionModel.estimatedDocumentCount().exec(),
+    (error) => {
+      logger.error({
+        message: 'Database error when retrieving submission collection count',
+        meta: {
+          action: 'getSubmissionsCount',
         },
         error,
       })
