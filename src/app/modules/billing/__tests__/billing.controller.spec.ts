85945a9deca4186fd8d81d8700ae915a54e0265b
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-01 12:37:11 +0800

    test(BillingCtl): add tests

---
+++ src/app/modules/billing/__tests__/billing.controller.spec.ts
@@ -0,0 +1,114 @@
+import moment from 'moment-timezone'
+import { errAsync, okAsync } from 'neverthrow'
+import expressHandler from 'tests/unit/backend/helpers/jest-express'
+import { mocked } from 'ts-jest/utils'
+
+import { AuthType, LoginStatistic } from 'src/types'
+
+import { DatabaseError } from '../../core/core.errors'
+import * as BillingController from '../billing.controller'
+import * as BillingService from '../billing.service'
+
+jest.mock('../billing.service')
+const MockBillingService = mocked(BillingService)
+
+describe('billing.controller', () => {
+  beforeEach(() => jest.clearAllMocks())
+
+  describe('handleGetBillInfo', () => {
+    const MOCK_REQ_QUERY = {
+      esrvcId: 'some-esrvc-id',
+      yr: '2020',
+      mth: '10',
+    }
+
+    const MOCK_REQ = expressHandler.mockRequest({
+      query: MOCK_REQ_QUERY,
+      session: {
+        user: 'exists',
+      },
+    })
+
+    const EXPECTED_SERVICE_CALL_ARGS = (() => {
+      const expectedStartArg = moment
+        .tz(
+          [parseInt(MOCK_REQ_QUERY.yr), parseInt(MOCK_REQ_QUERY.mth)],
+          'Asia/Singapore',
+        )
+        .startOf('month')
+      const expectedEndArg = moment(expectedStartArg).endOf('month')
+
+      return [
+        MOCK_REQ_QUERY.esrvcId,
+        expectedStartArg.toDate(),
+        expectedEndArg.toDate(),
+      ]
+    })()
+
+    it('should return 200 with login statistics successfully', async () => {
+      // Arrange
+      const mockLoginStats: LoginStatistic[] = [
+        {
+          adminEmail: 'mockemail@example.com',
+          authType: AuthType.CP,
+          formId: 'mock form id',
+          formName: 'some form name',
+          total: 100,
+        },
+      ]
+      const mockRes = expressHandler.mockResponse()
+      // Mock BillingService to return valid statistics
+      MockBillingService.getSpLoginStats.mockReturnValueOnce(
+        okAsync(mockLoginStats),
+      )
+
+      // Act
+      await BillingController.handleGetBillInfo(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert
+      expect(MockBillingService.getSpLoginStats).toHaveBeenCalledWith(
+        ...EXPECTED_SERVICE_CALL_ARGS,
+      )
+      expect(mockRes.json).toBeCalledWith({ loginStats: mockLoginStats })
+    })
+
+    it('should return 401 when user is not logged in', async () => {
+      // Arrange
+      const mockReqWithoutSession = expressHandler.mockRequest({
+        query: MOCK_REQ_QUERY,
+      })
+      const mockRes = expressHandler.mockResponse()
+
+      // Act
+      await BillingController.handleGetBillInfo(
+        mockReqWithoutSession,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      expect(MockBillingService.getSpLoginStats).not.toHaveBeenCalled()
+      expect(mockRes.status).toBeCalledWith(401)
+      expect(mockRes.json).toBeCalledWith('User is unauthorized.')
+    })
+
+    it('should return 500 when database error occurs', async () => {
+      // Arrange
+      const mockRes = expressHandler.mockResponse()
+      // Mock BillingService to return DatabaseError
+      MockBillingService.getSpLoginStats.mockReturnValueOnce(
+        errAsync(new DatabaseError()),
+      )
+
+      // Act
+      await BillingController.handleGetBillInfo(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert
+      expect(MockBillingService.getSpLoginStats).toHaveBeenCalledWith(
+        ...EXPECTED_SERVICE_CALL_ARGS,
+      )
+      expect(mockRes.status).toBeCalledWith(500)
+      expect(mockRes.json).toBeCalledWith('Error in retrieving billing records')
+    })
+  })
+})
