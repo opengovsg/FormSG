0d8fa04a090991e0de3892e187b5e8b41f80efe6
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-01 14:22:37 +0800

    feat(AnalyticsCtl): add handler for /submissions endpoint

--- src/app/modules/analytics/analytics.controller.ts
+++ src/app/modules/analytics/analytics.controller.ts
@@ -1,15 +1,16 @@
 import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
+import { submissionsTopUp } from '../../../config/config'
 import { createLoggerWithLabel } from '../../../config/logger'
 import { getRequestIp } from '../../utils/request'
 
-import { getUsersCount } from './analytics.service'
+import { getSubmissionsCount, getUsersCount } from './analytics.service'
 
 const logger = createLoggerWithLabel(module)
 
 /**
- * Handler for GET /anayltics/users
+ * Handler for GET /analytics/users
  * @route GET /analytics/users
  * @returns 200 with the number of users building forms
  * @returns 500 when database error occurs whilst retrieving user count
@@ -35,4 +36,39 @@ export const handleGetUserAnalytics: RequestHandler = async (req, res) => {
   }
 
   return res.json(countResult.value)
+}
+
+/**
+ * Handler for GET /analytics/submissions
+ * @route GET /analytics/submissions
+ * @returns 200 with the number of submissions across forms
+ * @returns 500 when database error occurs whilst retrieving submissions count
+ */
+export const handleGetSubmissionsAnalytics: RequestHandler = async (
+  req,
+  res,
+) => {
+  const countResult = await getSubmissionsCount()
+
+  if (countResult.isErr()) {
+    logger.error({
+      message: 'Mongo submissions count error',
+      meta: {
+        action: 'handleGetSubmissionsAnalytics',
+        ip: getRequestIp(req),
+        url: req.url,
+        headers: req.headers,
+      },
+      error: countResult.error,
+    })
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json('Unable to retrieve number of submissions from the database')
+  }
+
+  // Top up submissions from config file that tracks submissions that has been
+  // archived (and thus deleted from the database).
+  const totalProperCount = countResult.value + submissionsTopUp
+  return res.json(totalProperCount)
 }
