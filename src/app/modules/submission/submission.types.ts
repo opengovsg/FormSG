0a1e7f603e47d9240b0aaff8266f6ce1134b9cdc
liangyuanruo <liangyuanruo@gmail.com>
2020-10-03 11:30:11 +0800

    refactor: ProcessedFieldResponse answer property is always a string if it exists

--- src/app/modules/submission/submission.types.ts
+++ src/app/modules/submission/submission.types.ts
@@ -3,5 +3,5 @@ import { FieldResponse } from 'src/types'
 export type ProcessedFieldResponse = FieldResponse & {
   isVisible?: boolean
   isUserVerified?: boolean
-  answer?: string | undefined | null
+  answer?: string
 }
