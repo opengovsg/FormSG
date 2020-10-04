644325489f145421aa06213bcceaaba2fe5e0d32
liangyuanruo <liangyuanruo@gmail.com>
2020-10-04 14:30:42 +0800

    refactor(sectionValidator): compose type sectionValidatorConstructor for consistency

--- src/app/utils/field-validation/validators/sectionValidator.ts
+++ src/app/utils/field-validation/validators/sectionValidator.ts
@@ -1,14 +1,15 @@
-import { Either, left, right } from 'fp-ts/lib/Either'
+import { left, right } from 'fp-ts/lib/Either'
 
-import { ProcessedFieldResponse } from 'src/app/modules/submission/submission.types'
 import { ResponseValidator } from 'src/types/field/utils/validation'
+
+type sectionValidatorConstructor = () => ResponseValidator
 
 /**
  * A function that returns a validation function for a section field when called.
  */
-const constructSectionValidator = (): ResponseValidator => (
-  response: ProcessedFieldResponse,
-): Either<string, ProcessedFieldResponse> => {
+const constructSectionValidator: sectionValidatorConstructor = () => (
+  response,
+) => {
   return response.answer === '' ? right(response) : left('Answer not allowed')
 }
 
