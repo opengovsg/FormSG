d50426c964e3aafdb068c7b7b5b2f1b3d11b9ee7
liangyuanruo <liangyuanruo@gmail.com>
2020-10-05 10:50:40 +0800

    refactor: extract common type guard from single answer response

--- src/types/response/guards.ts
+++ src/types/response/guards.ts
@@ -1,7 +1,25 @@
-import { ProcessedSingleAnswerResponse } from 'src/app/modules/submission/submission.types'
+import {
+  ProcessedFieldResponse,
+  ProcessedSingleAnswerResponse,
+} from 'src/app/modules/submission/submission.types'
+
+const isProcessedFieldResponse = (
+  response: any,
+): response is ProcessedFieldResponse => {
+  return (
+    'fieldType' in response &&
+    typeof response.fieldType === 'string' &&
+    'isVisible' in response &&
+    typeof response.isVisible === 'boolean'
+  )
+}
 
 export const isProcessedSingleAnswerResponse = (
   response: any,
 ): response is ProcessedSingleAnswerResponse => {
-  return response && typeof response.answer === 'string'
+  return (
+    'answer' in response &&
+    typeof response.answer === 'string' &&
+    isProcessedFieldResponse(response)
+  )
 }
