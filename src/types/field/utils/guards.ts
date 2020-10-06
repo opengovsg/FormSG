f4c2b204edee0b79f339d9ffc71eb950c0094f03
liangyuanruo <liangyuanruo@gmail.com>
2020-10-06 18:27:20 +0800

    refactor: update type guards to use enums

--- src/types/field/utils/guards.ts
+++ src/types/field/utils/guards.ts
@@ -5,21 +5,22 @@ import {
   IShortTextField,
 } from 'src/types/field'
 import { IField } from 'src/types/field/baseField'
+import { BasicField } from 'src/types/field/fieldTypes'
 
 export const isSectionField = (
   formField: IField,
 ): formField is ISectionFieldSchema => {
-  return formField.fieldType === 'section'
+  return formField.fieldType === BasicField.Section
 }
 
 export const isShortTextField = (
   formField: IField,
 ): formField is IShortTextField => {
-  return formField.fieldType === 'textfield'
+  return formField.fieldType === BasicField.ShortText
 }
 
 export const isLongTextField = (
   formField: IField,
 ): formField is ILongTextField => {
-  return formField.fieldType === 'textarea'
+  return formField.fieldType === BasicField.LongText
 }
