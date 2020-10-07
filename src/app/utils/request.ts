f7789c9aa79b9a3ee758195c6f0ecf3d2a652c40
shuli-ogp <shuli@open.gov.sg>
2020-10-07 19:46:43 +0800

    chore: create utility function, extend Request interface globally

--- src/app/utils/request.ts
+++ src/app/utils/request.ts
@@ -1,14 +1,19 @@
 import { Request } from 'express'
+import { IncomingHttpHeaders } from 'http'
 
-interface IRequestWithId extends Request {
-  // Need to extend Request interface as id is not present in native express
-  id?: string
-}
-
-export const getRequestIp = (req: IRequestWithId) => {
+export const getRequestIp = (req: Request) => {
   return req.get('cf-connecting-ip') ?? req.ip
 }
 
-export const getTrace = (req: IRequestWithId) => {
+export const getTrace = (req: Request) => {
   return req.get('cf-ray') ?? req.id // trace using cloudflare cf-ray header, with x-request-id header as backup
+}
+
+export const getMeta = (req: Request) => {
+  return {
+    ip: req.get('cf-connecting-ip') ?? req.ip,
+    trace: req.get('cf-ray') ?? req.id, // trace using cloudflare cf-ray header, with x-request-id header as backup
+    url: req.url,
+    headers: req.headers,
+  }
 }
