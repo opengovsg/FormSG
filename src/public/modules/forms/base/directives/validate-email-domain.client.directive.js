9e60f1dfe89615c032ee0a57970ee04487cb3c03
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-08 12:27:39 +0800

    fix: early return when validating empty email string

--- src/public/modules/forms/base/directives/validate-email-domain.client.directive.js
+++ src/public/modules/forms/base/directives/validate-email-domain.client.directive.js
@@ -12,6 +12,10 @@ function validateEmailDomain() {
           ? new Set(scope.vm.field.allowedEmailDomains)
           : new Set()
       ngModel.$validators.emailDomainValidator = (emailAddress) => {
+        // Early return, do not even check domains if string is empty.
+        if (!emailAddress) {
+          return true
+        }
         if (allowedEmailDomains.size) {
           const emailDomain = emailAddress.split('@').pop()
           return allowedEmailDomains.has('@' + emailDomain)
