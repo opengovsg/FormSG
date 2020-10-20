fca5dc3a130eec969505d222bc999fbaf40dabb0
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-20 10:32:43 +0800

    fix: add additional startsWith('@') check when validating domain (#487)
    * fix: add additional startsWith('@') check when validating domain
    
    * feat: add more explicit copy for allowed domains error

--- src/shared/util/email-domain-validation.ts
+++ src/shared/util/email-domain-validation.ts
@@ -2,13 +2,14 @@ import { isEmpty } from 'lodash'
 import validator from 'validator'
 
 export const validateEmailDomains = (emailDomains: string[]): boolean => {
-  // We need to prepend "bob" to the email domain so that it can form an email address and can be
-  // validated by validator.isEmail.
   return (
     isEmpty(emailDomains) ||
     (new Set(emailDomains).size === emailDomains.length &&
-      emailDomains.every((emailDomain) =>
-        validator.isEmail('bob' + emailDomain),
+      emailDomains.every(
+        (emailDomain) =>
+          // We need to prepend "bob" to the email domain so that it can form an
+          // email address and can be validated by validator.isEmail.
+          emailDomain.startsWith('@') && validator.isEmail('bob' + emailDomain),
       ))
   )
 }
