29004de4e8e581ae86c49c2aa8e14bd61656ba98
shuli-ogp <63710093+shuli-ogp@users.noreply.github.com>
2020-10-28 22:16:38 +0800

    feat: stealth textfield prefill feature from query parameters (#524)
    * chore: allow query params in shared link url
    
    * chore: add tests for redirect
    
    * feat: implement stealth prefill feature on frontend
    
    * chore: relax joi validation for redirect endpoint
    
    * chore: address duplicate query param + sanitize
    
    * chore: use req.query instead of splitting url
    
    * chore(deps): install he
    
    * chore: support only unique params
    
    * refactor: shift res.redirect to catch block
    
    * doc: comment on using $sanitize
    
    * chore: add tests
    
    * chore: reapply $sanitize
    
    * chore: check query.length
    
    * chore: add - and _ to test
    
    * fix: check for queryParams

--- src/public/modules/forms/base/directives/field.client.directive.js
+++ src/public/modules/forms/base/directives/field.client.directive.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const { get } = require('lodash')
+const he = require('he')
+const querystring = require('querystring')
 
 angular
   .module('forms')
-  .directive('fieldDirective', ['FormFields', fieldDirective])
+  .directive('fieldDirective', [
+    'FormFields',
+    '$location',
+    '$sanitize',
+    fieldDirective,
+  ])
 
-function fieldDirective(FormFields) {
+function fieldDirective(FormFields, $location, $sanitize) {
   return {
     restrict: 'E',
     templateUrl:
@@ -23,6 +30,30 @@ function fieldDirective(FormFields) {
       isValidateDate: '<',
     },
     link: function (scope) {
+      // Stealth prefill feature
+      // If a query parameter is provided to a form URL in the form ?<fieldId1>=<value1>&<fieldId2>=<value2>...
+      // And if the fieldIds are valid mongoose object IDs and refer to a short text field,
+      // Then prefill and disable editing the corresponding form field on the frontend
+
+      const decodedUrl = he.decode($location.url()) // tech debt; after redirect, & is encoded as &amp; in the query string
+      const query = decodedUrl.split('?')
+      const queryParams =
+        query.length > 1 ? querystring.parse(query[1]) : undefined
+
+      if (
+        queryParams &&
+        scope.field._id in queryParams &&
+        scope.field.fieldType === 'textfield'
+      ) {
+        const prefillValue = queryParams[scope.field._id]
+        if (typeof prefillValue === 'string') {
+          // Only support unique query params. If query params are duplicated,
+          // none of the duplicated keys will be prefilled
+          scope.field.fieldValue = $sanitize(prefillValue) // $sanitize as a precaution to prevent xss
+          scope.field.disabled = true
+        }
+      }
+
       if ((scope.isadminpreview || scope.isTemplate) && scope.field.myInfo) {
         // Determine whether to disable field in preview
         if (
