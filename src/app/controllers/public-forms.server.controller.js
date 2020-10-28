0f547bc4982821a22ef81fe4912d9f5e80aa0abc
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-28 15:28:50 +0800

    feat: replace old public-forms controller redirect with new method

--- src/app/controllers/public-forms.server.controller.js
+++ src/app/controllers/public-forms.server.controller.js
@@ -1,12 +1,6 @@
 'use strict'
 
-const mongoose = require('mongoose')
 const { StatusCodes } = require('http-status-codes')
-
-const { createReqMeta } = require('../utils/request')
-const logger = require('../../config/logger').createLoggerWithLabel(module)
-const getFormModel = require('../models/form.server.model').default
-const Form = getFormModel(mongoose)
 
 /**
  * Checks if form is public
@@ -27,62 +21,4 @@ exports.isFormPublic = function (req, res, next) {
         formTitle: req.form.title,
       })
   }
-}
-
-/**
- * Used to redirect forms without hashbang to that with hashbang
- * This allows form links to be free of hashbangs and can thus be shared
- * via QR codes or url shorteners. Also handles requests from web crawlers
- * for the generation of rich link previews, renders index with the relevant
- * metatags if a crawler's user agent string is detected.
- * @param  {Object} req - Express request object
- * @param {String} req.params.Id - id of the form being accessed
- * @param {String} req.params.state - state of the req (preview, template or use-template)
- * @param  {Object} res - Express response object
- */
-exports.redirect = async function (req, res) {
-  let redirectPath = req.params.state
-    ? req.params.Id + '/' + req.params.state
-    : req.params.Id
-  try {
-    const metaTags = await fetchMetatags(req)
-    return res.render('index', {
-      ...metaTags,
-      redirectPath,
-    })
-  } catch (err) {
-    logger.error({
-      message: 'Error fetching metatags',
-      meta: {
-        action: 'redirect',
-        ...createReqMeta(req),
-      },
-      error: err,
-    })
-  }
-  res.redirect('/#!/' + redirectPath)
-}
-
-/**
- * Fetches the form object and the form details are used to populate the page's metatags.
- * @param {Object} req
- * @param {String} req.params.id - id of the form
- * @param {String} req.protocol - whether it's using http or https
- * @param {String} req.host - the host/domain of the request
- * @returns {Object} containing the form details that will be used in the page metatags
- */
-const fetchMetatags = async function (req) {
-  const result = await Form.findById(req.params.Id)
-  const baseUrl = req.protocol + '://' + req.get('host')
-  const fullUrl = baseUrl + req.originalUrl
-  const metaTags = {
-    title: result.title,
-    description: result.startPage.paragraph,
-    appUrl: fullUrl,
-    images: `${baseUrl}/public/modules/core/img/og/img_metatag.png, ${baseUrl}/public/modules/core/img/og/logo-vertical-color.png`.split(
-      ',',
-    ),
-    twitterImage: `${baseUrl}/public/modules/core/img/og/logo-vertical-color.png`,
-  }
-  return metaTags
 }
