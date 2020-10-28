e1bc304fa4645239aa112ede23209ddaf8fc9e6d
Kar Rui Lau <karrui.lau@gmail.com>
2020-10-28 17:06:32 +0800

    test(PublicFormCtl): add tests for handleRedirect handler

--- src/app/modules/form/public-form/__tests__/public-form.controller.spec.ts
+++ src/app/modules/form/public-form/__tests__/public-form.controller.spec.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from 'bson-ext'
+import { merge } from 'lodash'
 import { err, errAsync, ok, okAsync } from 'neverthrow'
 import { mocked } from 'ts-jest/utils'
 
@@ -13,8 +14,9 @@ import {
   PrivateFormError,
 } from '../../form.errors'
 import * as FormService from '../../form.service'
-import { handleSubmitFeedback } from '../public-form.controller'
+import * as PublicFormController from '../public-form.controller'
 import * as PublicFormService from '../public-form.service'
+import { Metatags } from '../public-form.types'
 
 jest.mock('../../form.service')
 jest.mock('../public-form.service')
@@ -55,7 +57,11 @@ describe('public-form.controller', () => {
       )
 
       // Act
-      await handleSubmitFeedback(MOCK_REQ, mockRes, jest.fn())
+      await PublicFormController.handleSubmitFeedback(
+        MOCK_REQ,
+        mockRes,
+        jest.fn(),
+      )
 
       // Assert
       // Check args of mocked services.
@@ -84,7 +90,11 @@ describe('public-form.controller', () => {
       )
 
       // Act
-      await handleSubmitFeedback(MOCK_REQ, mockRes, jest.fn())
+      await PublicFormController.handleSubmitFeedback(
+        MOCK_REQ,
+        mockRes,
+        jest.fn(),
+      )
 
       // Assert
       // Check args of mocked services.
@@ -113,7 +123,11 @@ describe('public-form.controller', () => {
       )
 
       // Act
-      await handleSubmitFeedback(MOCK_REQ, mockRes, jest.fn())
+      await PublicFormController.handleSubmitFeedback(
+        MOCK_REQ,
+        mockRes,
+        jest.fn(),
+      )
 
       // Assert
       // Check args of mocked services.
@@ -144,7 +158,11 @@ describe('public-form.controller', () => {
       )
 
       // Act
-      await handleSubmitFeedback(MOCK_REQ, mockRes, jest.fn())
+      await PublicFormController.handleSubmitFeedback(
+        MOCK_REQ,
+        mockRes,
+        jest.fn(),
+      )
 
       // Assert
       // Check args of mocked services.
@@ -173,7 +191,11 @@ describe('public-form.controller', () => {
       )
 
       // Act
-      await handleSubmitFeedback(MOCK_REQ, mockRes, jest.fn())
+      await PublicFormController.handleSubmitFeedback(
+        MOCK_REQ,
+        mockRes,
+        jest.fn(),
+      )
 
       // Assert
       // Check args of mocked services.
@@ -190,6 +212,134 @@ describe('public-form.controller', () => {
       expect(mockRes.json).toHaveBeenCalledWith({
         message: mockErrorString,
       })
+    })
+  })
+
+  describe('handleRedirect', () => {
+    const MOCK_FORM_ID = new ObjectId().toHexString()
+    const MOCK_REQ = expressHandler.mockRequest({
+      params: { Id: MOCK_FORM_ID },
+      others: {
+        protocol: 'https',
+        hostname: 'mockHostName',
+        originalUrl: '/some-url',
+      },
+    })
+    const EXPECTED_METATAG_ARGS = {
+      formId: MOCK_FORM_ID,
+      appUrl: `${MOCK_REQ.protocol}://${MOCK_REQ.hostname}${MOCK_REQ.originalUrl}`,
+      imageBaseUrl: `${MOCK_REQ.protocol}://${MOCK_REQ.hostname}`,
+    }
+
+    it('should return index render with redirectPath when metatags are created successfully', async () => {
+      // Arrange
+      const mockMetatags: Metatags = {
+        title: 'mock tag title',
+        appUrl: 'some://mock-app.url',
+        images: ['some-image-link-1', 'some-image-link-2'],
+        twitterImage: 'some-twitter-link',
+        description: 'mock tag description',
+      }
+      const mockRes = expressHandler.mockResponse()
+      MockPublicFormService.createMetatags.mockReturnValueOnce(
+        okAsync(mockMetatags),
+      )
+
+      // Act
+      await PublicFormController.handleRedirect(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert
+      const expectedRedirectPath = MOCK_FORM_ID
+      expect(MockPublicFormService.createMetatags).toHaveBeenCalledWith(
+        EXPECTED_METATAG_ARGS,
+      )
+      expect(mockRes.render).toHaveBeenCalledWith('index', {
+        ...mockMetatags,
+        redirectPath: expectedRedirectPath,
+      })
+    })
+
+    it('should return index render with redirectPath with retained state when metatags are created successfully', async () => {
+      // Arrange
+      const stateParam = 'use-template' as const
+      const mockReqWithState = merge({}, MOCK_REQ, {
+        params: { state: stateParam },
+      })
+      const mockMetatags: Metatags = {
+        title: 'mock tag title',
+        appUrl: 'some://mock-app.url',
+        images: ['some-image-link-1', 'some-image-link-2'],
+        twitterImage: 'some-twitter-link',
+        description: 'mock tag description',
+      }
+      const mockRes = expressHandler.mockResponse()
+      MockPublicFormService.createMetatags.mockReturnValueOnce(
+        okAsync(mockMetatags),
+      )
+
+      // Act
+      await PublicFormController.handleRedirect(
+        mockReqWithState,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      // Should have state param affixed.
+      const expectedRedirectPath = `${MOCK_FORM_ID}/${stateParam}`
+      expect(MockPublicFormService.createMetatags).toHaveBeenCalledWith(
+        EXPECTED_METATAG_ARGS,
+      )
+      expect(mockRes.render).toHaveBeenCalledWith('index', {
+        ...mockMetatags,
+        redirectPath: expectedRedirectPath,
+      })
+    })
+
+    it('should return 302 redirect to hashbang fallback when metatag creation fails due to invalid formId', async () => {
+      // Arrange
+      const stateParam = 'preview' as const
+      const mockReqWithState = merge({}, MOCK_REQ, {
+        params: { state: stateParam },
+      })
+      const mockRes = expressHandler.mockResponse()
+      // Mock form not found error.
+      MockPublicFormService.createMetatags.mockReturnValueOnce(
+        errAsync(new FormNotFoundError()),
+      )
+
+      // Act
+      await PublicFormController.handleRedirect(
+        mockReqWithState,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      const expectedRedirectPath = `/#!/${MOCK_FORM_ID}/${stateParam}`
+      expect(MockPublicFormService.createMetatags).toHaveBeenCalledWith(
+        EXPECTED_METATAG_ARGS,
+      )
+      expect(mockRes.redirect).toHaveBeenCalledWith(expectedRedirectPath)
+    })
+
+    it('should return 302 redirect to hashbang fallback when metatag creation fails due to database error', async () => {
+      // Arrange
+      const mockRes = expressHandler.mockResponse()
+      // Mock database error.
+      MockPublicFormService.createMetatags.mockReturnValueOnce(
+        errAsync(new DatabaseError()),
+      )
+
+      // Act
+      await PublicFormController.handleRedirect(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert
+      const expectedRedirectPath = `/#!/${MOCK_FORM_ID}`
+      expect(MockPublicFormService.createMetatags).toHaveBeenCalledWith(
+        EXPECTED_METATAG_ARGS,
+      )
+      expect(mockRes.redirect).toHaveBeenCalledWith(expectedRedirectPath)
     })
   })
 })
