9933e1f490453cd59603933e06a5a7d9f0fd8e1f
shuli-ogp <shuli@open.gov.sg>
2020-10-29 20:04:27 +0800

    fix(prefill): allow prefill only if scope.field.allowPrefill is true

--- src/public/modules/forms/base/directives/field.client.directive.js
+++ src/public/modules/forms/base/directives/field.client.directive.js
@@ -41,6 +41,8 @@ function fieldDirective(FormFields, $location, $sanitize) {
         query.length > 1 ? querystring.parse(query[1]) : undefined
 
       if (
+        scope.field.allowPrefill &&
+        scope.field.allowPrefill === true &&
         queryParams &&
         scope.field._id in queryParams &&
         scope.field.fieldType === 'textfield'
