59264d5c20eeb04fc15e231808d00e3754c9c98c
Kar Rui Lau <karrui.lau@gmail.com>
2020-11-02 17:47:01 +0800

    test(AuthMdw): add unit tests for withUserAuthenticated

--- src/app/modules/auth/auth.middlewares.ts
+++ src/app/modules/auth/auth.middlewares.ts
@@ -3,6 +3,12 @@ import { StatusCodes } from 'http-status-codes'
 
 import { isUserInSession } from './auth.utils'
 
+/**
+ * Middleware that only allows authenticated users to pass through to the next
+ * handler.
+ * @returns next if user exists in session
+ * @returns 401 if user does not exist in session
+ */
 export const withUserAuthentication: RequestHandler = (req, res, next) => {
   if (isUserInSession(req.session)) {
     return next()
