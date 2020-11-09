f5abde6dcc2c05f6b3c8adde9a3b9ec7d70a679d
Kar Rui Lau <karrui.lau@gmail.com>
2020-11-09 13:15:39 +0800

    test(EncryptSubModel): add findMetadataById tests

---
+++ tests/unit/backend/models/encrypt-submission.server.model.spec.ts
@@ -0,0 +1,100 @@
+import { ObjectId } from 'bson-ext'
+import moment from 'moment-timezone'
+import mongoose from 'mongoose'
+
+import getSubmissionModel, {
+  getEncryptSubmissionModel,
+} from 'src/app/models/submission.server.model'
+import {
+  IEncryptedSubmissionSchema,
+  SubmissionMetadata,
+  SubmissionType,
+} from 'src/types'
+
+import dbHandler from '../helpers/jest-db'
+
+const Submission = getSubmissionModel(mongoose)
+const EncryptSubmission = getEncryptSubmissionModel(mongoose)
+
+describe('Encrypt Submission Model', () => {
+  beforeAll(async () => await dbHandler.connect())
+  afterEach(async () => await dbHandler.clearDatabase())
+  afterAll(async () => await dbHandler.closeDatabase())
+
+  const MOCK_ENCRYPTED_CONTENT = 'abcdefg encryptedContent'
+
+  describe('Statics', () => {
+    describe('findMetadataById', () => {
+      it('should return submission metadata', async () => {
+        // Arrange
+        const validFormId = new ObjectId().toHexString()
+        const createdDate = new Date()
+        // Add valid encrypt submission.
+        const validSubmission = await Submission.create<
+          IEncryptedSubmissionSchema
+        >({
+          form: validFormId,
+          myInfoFields: [],
+          // Email type.
+          submissionType: SubmissionType.Encrypt,
+          encryptedContent: MOCK_ENCRYPTED_CONTENT,
+          version: 1,
+          created: createdDate,
+        })
+
+        // Act
+        const result = await EncryptSubmission.findMetadataById(
+          validFormId,
+          validSubmission._id,
+        )
+
+        // Assert
+        const expected: SubmissionMetadata = {
+          number: 1,
+          refNo: validSubmission._id,
+          submissionTime: moment(createdDate)
+            .tz('Asia/Singapore')
+            .format('Do MMM YYYY, h:mm:ss a'),
+        }
+        expect(result).toEqual(expected)
+      })
+
+      it('should return null when submission is of SubmissionType.Email', async () => {
+        // Arrange
+        const validFormId = new ObjectId().toHexString()
+        // Add email submission.
+        const validSubmission = await Submission.create({
+          form: validFormId,
+          myInfoFields: [],
+          // Email type.
+          submissionType: SubmissionType.Email,
+          responseHash: 'hash',
+          responseSalt: 'salt',
+        })
+
+        // Act
+        const result = await EncryptSubmission.findMetadataById(
+          validFormId,
+          validSubmission._id,
+        )
+
+        // Assert
+        expect(result).toBeNull()
+      })
+
+      it('should return null if no submission metadata is retrieved', async () => {
+        // Arrange
+        const validFormId = new ObjectId().toHexString()
+        const invalidSubmissionId = new ObjectId().toHexString()
+        // Act
+        const result = await EncryptSubmission.findMetadataById(
+          validFormId,
+          invalidSubmissionId,
+        )
+
+        // Assert
+        expect(result).toBeNull()
+      })
+    })
+  })
+})
