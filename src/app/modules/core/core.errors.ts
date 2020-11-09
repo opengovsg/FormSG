e773d5504459c192e489ee0df7a7a4af49f9b25d
Antariksh <antarikshmahajan@gmail.com>
2020-11-09 17:34:52 +0800

    fix: extend ApplicationError

--- src/app/modules/core/core.errors.ts
+++ src/app/modules/core/core.errors.ts
@@ -45,7 +45,7 @@ export class ExternalError extends ApplicationError {
  * Error thrown when feature-specific functions are called
  * despite those features not being activated.
  */
-export class MissingFeatureError extends Error {
+export class MissingFeatureError extends ApplicationError {
   constructor(missingFeature: FeatureNames) {
     super(
       `${missingFeature} is not activated, but a feature-specific function was called.`,
