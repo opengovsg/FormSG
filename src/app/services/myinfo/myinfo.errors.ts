04b100e4f2b7169e60818bdc7e8d7edea2c36320
Antariksh <antarikshmahajan@gmail.com>
2020-11-11 17:16:08 +0800

    ref: make error messages optional

--- src/app/services/myinfo/myinfo.errors.ts
+++ src/app/services/myinfo/myinfo.errors.ts
@@ -1,31 +1,31 @@
 import { ApplicationError } from '../../modules/core/core.errors'
 
 export class CircuitBreakerError extends ApplicationError {
-  constructor(message: string) {
-    super(message)
+  constructor(message?: string) {
+    super(message ?? 'Circuit breaker tripped')
   }
 }
 
 export class FetchMyInfoError extends ApplicationError {
-  constructor(message: string) {
-    super(message)
+  constructor(message?: string) {
+    super(message ?? 'Error while requesting MyInfo data')
   }
 }
 
 export class HashingError extends ApplicationError {
-  constructor(message: string) {
-    super(message)
+  constructor(message?: string) {
+    super(message ?? 'Error occurred while hashing data')
   }
 }
 
 export class MissingHashError extends ApplicationError {
-  constructor(message: string) {
-    super(message)
+  constructor(message?: string) {
+    super(message ?? 'Requested hashes not found in database')
   }
 }
 
 export class HashDidNotMatchError extends ApplicationError {
-  constructor(message: string) {
-    super(message)
+  constructor(message?: string) {
+    super(message ?? 'Responses did not match hashed values')
   }
 }
