3d86d0653c8cb5c32226a1da9db49ac6fdcdc7a7
Kar Rui Lau <karrui.lau@gmail.com>
2020-11-13 14:41:20 +0800

    test(EncryptSubSvc): add tests for transformAttachmentMetaStream

--- src/app/modules/submission/encrypt-submission/__tests__/encrypt-submission.service.spec.ts
+++ src/app/modules/submission/encrypt-submission/__tests__/encrypt-submission.service.spec.ts
@@ -1,10 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { ObjectId } from 'bson-ext'
+import { clone } from 'lodash'
 import mongoose from 'mongoose'
+import { PassThrough, Transform } from 'stream'
 
 import { getEncryptSubmissionModel } from 'src/app/models/submission.server.model'
 import { MalformedParametersError } from 'src/app/modules/core/core.errors'
+import { aws } from 'src/config/config'
+import { SubmissionCursorData } from 'src/types'
 
-import { getSubmissionCursor } from '../encrypt-submission.service'
+import {
+  getSubmissionCursor,
+  transformAttachmentMetaStream,
+} from '../encrypt-submission.service'
 
 const EncryptSubmission = getEncryptSubmissionModel(mongoose)
 
@@ -100,4 +108,213 @@ describe('encrypt-submission.service', () => {
       )
     })
   })
+
+  describe('transformAttachmentMetaStream', () => {
+    const MOCK_SUB_CURSOR_DATA_1: Partial<SubmissionCursorData> = {
+      attachmentMetadata: {
+        mockId1: 'mock metadata 1',
+      },
+    }
+    const MOCK_SUB_CURSOR_DATA_2: Partial<SubmissionCursorData> = {
+      attachmentMetadata: {
+        mockId2: 'mock metadata 2',
+      },
+    }
+
+    const EMPTY_METADATA = {
+      attachmentMetadata: {},
+    }
+
+    beforeEach(() => jest.resetAllMocks())
+
+    it('should successfully transform metadata to signed URL', async () => {
+      // Arrange
+      const mockFirstS3Url =
+        'http://localhost:4566/local-attachment-bucket/path/to/attachment/1?AWSAccessKeyId=fakeKey&Expires=1605248842&Signature=some_signature'
+      const mockSecondS3Url =
+        'http://localhost:4566/local-attachment-bucket/path/to/attachment/2?AWSAccessKeyId=fakeKey2&Expires=1605248842&Signature=some_signature'
+      const mockInput = new PassThrough()
+      const expectedExpiry = 20
+
+      const actualTransformedData: any[] = []
+
+      const awsSpy = jest
+        .spyOn(aws.s3, 'getSignedUrl')
+        // @ts-ignore
+        .mockImplementationOnce((_operation, _params, callback) =>
+          callback(null, mockFirstS3Url),
+        )
+        // @ts-ignore
+        .mockImplementationOnce((_operation, _params, callback) =>
+          callback(null, mockSecondS3Url),
+        )
+
+      // Act
+      // Build pipeline.
+      mockInput
+        .pipe(
+          transformAttachmentMetaStream({
+            enabled: true,
+            urlValidDuration: expectedExpiry,
+          }),
+        )
+        .pipe(stringify())
+        .on('data', (data) => {
+          actualTransformedData.push(JSON.parse(data.toString()))
+        })
+
+      // Emit events.
+      mockInput.emit('data', clone(MOCK_SUB_CURSOR_DATA_1))
+      mockInput.emit('data', clone(MOCK_SUB_CURSOR_DATA_2))
+
+      // Assert
+      // Data from end of pipe should have their attachment metadatas replaced
+      // by s3 urls.
+      expect(actualTransformedData).toEqual([
+        {
+          attachmentMetadata: {
+            mockId1: mockFirstS3Url,
+          },
+        },
+        {
+          attachmentMetadata: {
+            mockId2: mockSecondS3Url,
+          },
+        },
+      ])
+      // Check external service calls.
+      expect(awsSpy).toHaveBeenNthCalledWith(
+        1,
+        'getObject',
+        {
+          Bucket: aws.attachmentS3Bucket,
+          Key: MOCK_SUB_CURSOR_DATA_1.attachmentMetadata!['mockId1'],
+          Expires: expectedExpiry,
+        },
+        expect.any(Function),
+      )
+      expect(awsSpy).toHaveBeenNthCalledWith(
+        2,
+        'getObject',
+        {
+          Bucket: aws.attachmentS3Bucket,
+          Key: MOCK_SUB_CURSOR_DATA_2.attachmentMetadata!['mockId2'],
+          Expires: expectedExpiry,
+        },
+        expect.any(Function),
+      )
+    })
+
+    it('should return empty data.attachmentMetadata when disabled', async () => {
+      // Arrange
+      const mockInput = new PassThrough()
+      const expectedExpiry = 400
+      const actualTransformedData: any[] = []
+      const awsSpy = jest.spyOn(aws.s3, 'getSignedUrl')
+
+      // Act
+      // Build pipeline.
+      mockInput
+        .pipe(
+          transformAttachmentMetaStream({
+            // Disabled pipeline.
+            enabled: false,
+            urlValidDuration: expectedExpiry,
+          }),
+        )
+        .pipe(stringify())
+        .on('data', (data) => {
+          actualTransformedData.push(JSON.parse(data.toString()))
+        })
+
+      // Emit events.
+      mockInput.emit('data', clone(MOCK_SUB_CURSOR_DATA_1))
+
+      // Assert
+      expect(actualTransformedData).toEqual([EMPTY_METADATA])
+      expect(awsSpy).not.toHaveBeenCalled()
+    })
+
+    it('should return empty data.attachmentMetadata when nothing to transform', async () => {
+      // Arrange
+      const mockInput = new PassThrough()
+      const awsSpy = jest.spyOn(aws.s3, 'getSignedUrl')
+      const expectedExpiry = 1
+
+      const actualTransformedData: any[] = []
+
+      // Act
+      // Build pipeline.
+      mockInput
+        .pipe(
+          transformAttachmentMetaStream({
+            enabled: true,
+            urlValidDuration: expectedExpiry,
+          }),
+        )
+        .pipe(stringify())
+        .on('data', (data) => {
+          actualTransformedData.push(JSON.parse(data.toString()))
+        })
+
+      // Emit events.
+      mockInput.emit('data', EMPTY_METADATA)
+
+      // Assert
+      expect(actualTransformedData).toEqual([EMPTY_METADATA])
+      expect(awsSpy).not.toHaveBeenCalled()
+    })
+
+    it('should return error when stream errors occurs', async () => {
+      // Arrange
+      const expectedError = new Error('streams are being crossed right now!!!!')
+      const expectedExpiry = 1000
+      // Mock AWS S3 error.
+      const awsSpy = jest
+        .spyOn(aws.s3, 'getSignedUrl')
+        // @ts-ignore
+        .mockImplementationOnce((_operation, _params, callback) =>
+          callback(expectedError),
+        )
+      const mockInput = new PassThrough()
+      const actualErrors: any[] = []
+
+      // Act
+      // Build pipeline.
+      mockInput
+        .pipe(
+          transformAttachmentMetaStream({
+            enabled: true,
+            urlValidDuration: expectedExpiry,
+          }),
+        )
+        .on('error', (error) => actualErrors.push(error))
+
+      // Emit events.
+      mockInput.emit('data', clone(MOCK_SUB_CURSOR_DATA_2))
+
+      // Assert
+      expect(awsSpy).toHaveBeenCalledWith(
+        'getObject',
+        {
+          Bucket: aws.attachmentS3Bucket,
+          Key: MOCK_SUB_CURSOR_DATA_2.attachmentMetadata!['mockId2'],
+          Expires: expectedExpiry,
+        },
+        expect.any(Function),
+      )
+      expect(actualErrors).toEqual([expectedError])
+    })
+  })
 })
+
+/**
+ * Helper function to create a transform stream that converts Buffers to strings.
+ */
+const stringify = () =>
+  new Transform({
+    objectMode: true,
+    transform: function (chunk, _encoding, callback) {
+      return callback(null, JSON.stringify(chunk, null, 4) + '\n')
+    },
+  })
