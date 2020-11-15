e56910e9b0e010a852bec08911587e0df37d8529
Antariksh <antarikshmahajan@gmail.com>
2020-11-15 20:40:23 +0800

    ref: delete old redirect code

--- src/app/routes/spcp.server.routes.js
+++ src/app/routes/spcp.server.routes.js
@@ -8,28 +8,6 @@ const spcpFactory = require('../factories/spcp.factory')
 
 module.exports = function (app) {
   /**
-   * @typedef RedirectURL
-   * @property {string} redirectURL.required - the SingPass/CorpPass IDP URL to redirect to
-   */
-
-  /**
-   * Provide a URL that web agents are obliged to redirect users to.
-   * Due to cross-origin restrictions in place by SingPass/CorpPass,
-   * this endpoint cannot and should not issue a 302 redirect
-   * @route GET /spcp/redirect
-   * @group SPCP - SingPass/CorpPass logins for form-fillers
-   * @param {string} target.query.required - the destination URL after login
-   * @param {string} authType.query.required - `SP` for SingPass or `CP` for CorpPass
-   * @param {string} esrvcId.query.required - e-service id
-   * @produces application/json
-   * @returns {RedirectURL.model} 200 - contains the URL to redirect to
-   * @returns {string} 400 - the redirect URL will be malformed due to missing parameters
-   */
-  app
-    .route('/spcp/redirect')
-    .get(spcpFactory.createSpcpRedirectURL, spcpFactory.returnSpcpRedirectURL)
-
-  /**
    * Gets the spcp redirect URL and parses the returned page to check for error codes
    * @route GET /spcp/validate
    * @group SPCP - SingPass/CorpPass logins for form-fillers
