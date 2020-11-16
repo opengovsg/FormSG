5764290d7e341a486aa048ad65a325e7a5817faa
Antariksh <antarikshmahajan@gmail.com>
2020-11-16 10:20:15 +0800

    ref: initialise message in error ctor

--- src/app/modules/spcp/spcp.errors.ts
+++ src/app/modules/spcp/spcp.errors.ts
@@ -3,7 +3,7 @@ import { ApplicationError } from '../../modules/core/core.errors'
  * Error while creating redirect URL
  */
 export class CreateRedirectUrlError extends ApplicationError {
-  constructor(message?: string) {
-    super(message ?? 'Error while creating redirect URL')
+  constructor(message = 'Error while creating redirect URL') {
+    super(message)
   }
 }
