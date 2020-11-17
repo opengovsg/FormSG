df943e226b55809c5e1e3ced3ada20697ad20590
Antariksh <antarikshmahajan@gmail.com>
2020-11-17 10:51:19 +0800

    ref: extract redirect params validation

--- src/app/modules/spcp/spcp.routes.ts
+++ src/app/modules/spcp/spcp.routes.ts
@@ -1,9 +1,7 @@
-import { celebrate, Joi, Segments } from 'celebrate'
 import { Router } from 'express'
 
-import { AuthType } from '../../../types'
-
 import * as SpcpController from './spcp.controller'
+import { redirectParamsMiddleware } from './spcp.middlewares'
 // Shared routes for Singpass and Corppass
 export const SpcpRouter = Router()
 
@@ -23,12 +21,6 @@ export const SpcpRouter = Router()
  */
 SpcpRouter.get(
   '/redirect',
-  celebrate({
-    [Segments.QUERY]: Joi.object({
-      target: Joi.string().required(),
-      authType: Joi.string().required().valid(AuthType.SP, AuthType.CP),
-      esrvcId: Joi.string().required(),
-    }),
-  }),
+  redirectParamsMiddleware,
   SpcpController.handleRedirect,
 )
