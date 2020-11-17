169f107863ee7cb58870b2ac7f50da80f56f7b6b
Antariksh <antarikshmahajan@gmail.com>
2020-11-17 12:06:31 +0800

    ref: implement handleValidate

--- src/app/modules/spcp/spcp.controller.ts
+++ src/app/modules/spcp/spcp.controller.ts
@@ -1,8 +1,10 @@
 import { RequestHandler } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
 import { StatusCodes } from 'http-status-codes'
 
 import { createLoggerWithLabel } from '../../../config/logger'
 import { AuthType } from '../../../types'
+import { createReqMeta } from '../../utils/request'
 
 import { SpcpFactory } from './spcp.factory'
 import { mapRouteError } from './spcp.util'
@@ -15,8 +17,8 @@ const logger = createLoggerWithLabel(module)
  * @param res - Express response object
  */
 export const handleRedirect: RequestHandler<
-  unknown,
-  unknown,
+  ParamsDictionary,
+  { redirectURL: string } | { message: string },
   unknown,
   { authType: AuthType; target: string; esrvcId: string }
 > = (req, res) => {
@@ -30,6 +32,48 @@ export const handleRedirect: RequestHandler<
         message: 'Error while creating redirect URL',
         meta: {
           action: 'handleRedirect',
+          ...createReqMeta(req),
+          authType,
+          target,
+          esrvcId,
+        },
+        error,
+      })
+      const { statusCode, errorMessage } = mapRouteError(error)
+      return res.status(statusCode).json({ message: errorMessage })
+    })
+}
+
+/**
+ * Validates the given e-service ID.
+ * @param req - Express request object
+ * @param res - Express response object
+ */
+export const handleValidate: RequestHandler<
+  ParamsDictionary,
+  | { isValid: true }
+  | { isValid: false; errorCode: string }
+  | { message: string },
+  unknown,
+  { authType: AuthType; target: string; esrvcId: string }
+> = (req, res) => {
+  const { target, authType, esrvcId } = req.query
+  SpcpFactory.createRedirectUrl(authType, target, esrvcId)
+    .asyncAndThen(SpcpFactory.fetchLoginPage)
+    .andThen(SpcpFactory.validateLoginPage)
+    .map((errorCode) => {
+      if (!errorCode) {
+        return res.status(StatusCodes.OK).json({ isValid: true })
+      } else {
+        return res.status(StatusCodes.OK).json({ isValid: false, errorCode })
+      }
+    })
+    .mapErr((error) => {
+      logger.error({
+        message: 'Error while validating e-service ID',
+        meta: {
+          action: 'handleValidate',
+          ...createReqMeta(req),
           authType,
           target,
           esrvcId,
