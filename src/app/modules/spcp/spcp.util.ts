6e128246a943dc57f5b88976d4e5f9a4ff9dd4eb
Antariksh <antarikshmahajan@gmail.com>
2020-11-18 21:47:09 +0800

    ref: implement extractUserInfo

--- src/app/modules/spcp/spcp.util.ts
+++ src/app/modules/spcp/spcp.util.ts
@@ -13,7 +13,13 @@ import {
   LoginPageValidationError,
   VerifyJwtError,
 } from './spcp.errors'
-import { JwtName, JwtPayload, SpcpCookies } from './spcp.types'
+import {
+  CorppassAttributes,
+  JwtName,
+  JwtPayload,
+  SingpassAttributes,
+  SpcpCookies,
+} from './spcp.types'
 
 const logger = createLoggerWithLabel(module)
 const destinationRegex = /^\/([\w]+)\/?/
@@ -59,6 +65,23 @@ export const extractFormId = (relayState: string): string => {
   // Assume that the destination has already been validated
   const regexSplit = destinationRegex.exec(destination)!
   return regexSplit[1]
+}
+
+export const extractUserInfo = (
+  attributes: Record<string, unknown>,
+  authType: AuthType.SP | AuthType.CP,
+): {
+  userName?: string
+  userInfo?: string
+} => {
+  if (authType === AuthType.SP) {
+    const userName = (attributes as SingpassAttributes).UserName
+    return userName && typeof userName === 'string' ? { userName } : {}
+  } else {
+    const userName = (attributes as CorppassAttributes)?.UserInfo?.CPEntID
+    const userInfo = (attributes as CorppassAttributes)?.UserInfo?.CPUID
+    return userName && userInfo ? { userName, userInfo } : {}
+  }
 }
 
 export const getAttributesPromise = (
