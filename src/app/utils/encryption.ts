656bd264fc93c04fbf7c773a88af1c633a41cb0d
shuli-ogp <shuli@open.gov.sg>
2020-11-23 12:41:59 +0800

    refactor: neverthrow for checkIsEncryptedEncoding

--- src/app/utils/encryption.ts
+++ src/app/utils/encryption.ts
@@ -1,23 +1,28 @@
 import { decode as decodeBase64 } from '@stablelib/base64'
+import { err, ok, Result } from 'neverthrow'
 
-export const checkIsEncryptedEncoding = (encryptedStr: string): boolean => {
+export const checkIsEncryptedEncoding = (
+  encryptedStr: string,
+): Result<boolean, Error> => {
   // TODO (#42): Remove this type check once whole backend is in TypeScript.
   if (typeof encryptedStr !== 'string') {
-    throw new Error('encryptedStr is not of type `string`')
+    return err(new Error('encryptedStr is not of type `string`'))
   }
 
   const [submissionPublicKey, nonceEncrypted] = encryptedStr.split(';')
   const [nonce, encrypted] = nonceEncrypted.split(':')
   if (!submissionPublicKey || !nonce || !encrypted) {
-    throw new Error('Missing data')
+    return err(new Error('Missing data'))
   }
 
   try {
+    // decode throws error if incorrect characters for decoding
+    // see https://github.com/StableLib/stablelib/blob/2f3b21e8fcee4aaa77872282fd4ac7a7ff1633f5/packages/base64/base64.ts#L111
     decodeBase64(submissionPublicKey)
     decodeBase64(nonce)
     decodeBase64(encrypted)
-    return true
+    return ok(true)
   } catch (err) {
-    return false
+    return err(false)
   }
 }
