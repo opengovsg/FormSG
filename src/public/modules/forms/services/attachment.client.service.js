425f4b46f1f8c983ee133c816f0facb87fc9d032
tshuli <63710093+tshuli@users.noreply.github.com>
2020-11-23 19:37:09 +0800

    feat: Increase attachment size options (#692)
    * feat: expand attachment size choice up to 20mb step 1mb
    
    * feat: limit attachment size choice up to 7mb for email mode
    
    * chore: slides up to 10mb
    
    * refactor: rename EMAIL_MODE_ALLOWED_SIZES
    
    * feat: limit attachment size intervals to 1,2,3,7,10,20
    
    * feat: shift size guidance to tooltip
    
    * chore: update attachment size tooltip copy

--- src/public/modules/forms/services/attachment.client.service.js
+++ src/public/modules/forms/services/attachment.client.service.js
@@ -3,13 +3,9 @@ const { zipWith } = require('lodash')
 angular.module('forms').service('Attachment', [Attachment])
 
 function Attachment() {
-  this.sizes = ['1', '3', '7']
+  this.sizes = ['1', '2', '3', '7', '10', '20']
 
-  this.descriptions = [
-    '1 MB (e.g. Pictures)',
-    '3 MB (e.g. PDF files)',
-    '7 MB (e.g. Slides)',
-  ]
+  this.descriptions = ['1 MB', '2 MB', '3 MB', '7 MB', '10 MB', '20 MB']
 
   this.dropdown = zipWith(this.descriptions, this.sizes, (desc, size) => {
     return { name: desc, value: size }
