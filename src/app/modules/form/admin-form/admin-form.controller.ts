ef8cd282d4ff81f352e97427b3807fc16f7f46d5
Kar Rui Lau <karrui.lau@gmail.com>
2020-11-25 21:50:29 +0800

    feat: remove unused code and add jsdoc for handler

--- src/app/modules/form/admin-form/admin-form.controller.ts
+++ src/app/modules/form/admin-form/admin-form.controller.ts
@@ -264,6 +264,17 @@ export const handleCountFormFeedback: RequestHandler<{
   )
 }
 
+/**
+ * Handler for GET /{formId}/adminform/feedback/download.
+ * @security session
+ *
+ * @returns 200 with feedback stream
+ * @returns 403 when user does not have permissions to access form
+ * @returns 404 when form cannot be found
+ * @returns 410 when form is archived
+ * @returns 422 when user in session cannot be retrieved from the database
+ * @returns 500 when database or stream error occurs
+ */
 export const handleStreamFormFeedback: RequestHandler<{
   formId: string
 }> = async (req, res) => {
