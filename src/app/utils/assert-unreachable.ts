3860e347daab1defc986dc90eadff674fe54170e
Kar Rui Lau <karrui.lau@gmail.com>
2020-11-25 13:45:26 +0800

    feat(utils): add exhaustive switch case typeguard

---
+++ src/app/utils/assert-unreachable.ts
@@ -0,0 +1,10 @@
+/**
+ * Typescript type guard that asserts that all switch cases are exhaustive.
+ * Use to get compile-time safety for making sure all the cases are handled.
+ *
+ * See:
+ * https://stackoverflow.com/questions/39419170/how-do-i-check-that-a-switch-block-is-exhaustive-in-typescript
+ */
+export const assertUnreachable = (switchCase: never): never => {
+  throw new Error(`This should never be reached in TypeScript: "${switchCase}"`)
+}
