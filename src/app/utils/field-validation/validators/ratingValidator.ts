5fe84a97650d24b71e66e90197652f8dff6474aa
shuli-ogp <shuli@open.gov.sg>
2020-11-25 11:06:36 +0800

    refactor: migrate ratingValidator to ts

---
+++ src/app/utils/field-validation/validators/ratingValidator.ts
@@ -0,0 +1,37 @@
+import { chain, left, right } from 'fp-ts/lib/Either'
+import { pipe } from 'fp-ts/lib/function'
+import isInt from 'validator/lib/isInt'
+
+import { IRatingField } from 'src/types/field'
+import { ResponseValidator } from 'src/types/field/utils/validation'
+import { ISingleAnswerResponse } from 'src/types/response'
+
+import { notEmptySingleAnswerResponse } from './common'
+
+type RatingValidator = ResponseValidator<ISingleAnswerResponse>
+type RatingValidatorConstructor = (ratingField: IRatingField) => RatingValidator
+
+const ratingValidator: RatingValidatorConstructor = (ratingField) => (
+  response,
+) => {
+  const { answer } = response
+  const { steps } = ratingField.ratingOptions
+
+  const isValid = isInt(answer, {
+    min: 1,
+    max: steps,
+    allow_leading_zeroes: false,
+  })
+
+  return isValid
+    ? right(response)
+    : left(`RatingValidator:\t answer is not a valid rating`)
+}
+
+export const constructRatingValidator: RatingValidatorConstructor = (
+  ratingField,
+) => (response) =>
+  pipe(
+    notEmptySingleAnswerResponse(response),
+    chain(ratingValidator(ratingField)),
+  )
