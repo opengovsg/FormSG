14698c4198df36efcc7e1c0452b4ee0a7f61dc8c
Antariksh <antarikshmahajan@gmail.com>
2020-11-27 19:01:55 +0800

    ref: implement middleware skeleton

--- src/app/modules/submission/email-submission/email-submission.middleware.ts
+++ src/app/modules/submission/email-submission/email-submission.middleware.ts
@@ -1,11 +1,16 @@
 import { RequestHandler } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
+import { merge } from 'lodash'
 
+import { createLoggerWithLabel } from '../../../../config/logger'
 import { ResWithHashedFields } from '../../../../types'
 import { ProcessedFieldResponse } from '../submission.types'
 
+import * as EmailSubmissionReceiver from './email-submission.receiver'
 import * as EmailSubmissionService from './email-submission.service'
 import { WithEmailData } from './email-submission.types'
+
+const logger = createLoggerWithLabel(module)
 
 /**
  * Construct autoReply data and data to send admin from responses submitted
@@ -29,4 +34,50 @@ export const prepareEmailSubmission: RequestHandler<
   ;(req as WithEmailData<typeof req>).jsonData = emailData.jsonData
   ;(req as WithEmailData<typeof req>).formData = emailData.formData
   return next()
+}
+
+/**
+ * Parses multipart-form data request. Parsed attachments are
+ * placed into req.attachments and parsed fields are placed into
+ * req.body.
+ *
+ * @param req - Express request object
+ * @param res - Express response object
+ * @param next - Express next middleware function
+ */
+export const receiveEmailSubmission: RequestHandler = (req, res, next) => {
+  EmailSubmissionReceiver.createMultipartReceiver(req.headers)
+    .map((receiver) => {
+      // Configuration is event-based so we have to use a callback
+      EmailSubmissionReceiver.configureMultipartReceiver(receiver, (result) => {
+        result
+          .map((parsed) => {
+            merge(req, parsed)
+            return next()
+          })
+          .mapErr((error) => {
+            logger.error({
+              message: 'Error while receiving multipart data',
+              meta: {
+                action: 'receiveEmailSubmission',
+              },
+              error,
+            })
+            // const { errorMessage, statusCode } = mapRouteError(error)
+            // return res.status(statusCode).json({ message: errorMessage })
+          })
+      })
+      req.pipe(receiver)
+    })
+    .mapErr((error) => {
+      logger.error({
+        message: 'Error while receiving multipart data',
+        meta: {
+          action: 'receiveEmailSubmission',
+        },
+        error,
+      })
+      // const { errorMessage, statusCode } = mapRouteError(error)
+      // return res.status(statusCode).json({ message: errorMessage })
+    })
 }
