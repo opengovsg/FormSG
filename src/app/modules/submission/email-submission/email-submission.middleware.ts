e826325ad1836896baba62efc4e7e77e800c475b
Antariksh <antarikshmahajan@gmail.com>
2020-11-30 12:54:05 +0800

    ref: implement new validateEmailSubmission

--- src/app/modules/submission/email-submission/email-submission.middleware.ts
+++ src/app/modules/submission/email-submission/email-submission.middleware.ts
@@ -1,9 +1,13 @@
 import { RequestHandler } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
+import { StatusCodes } from 'http-status-codes'
 import { merge } from 'lodash'
 
 import { createLoggerWithLabel } from '../../../../config/logger'
-import { ResWithHashedFields } from '../../../../types'
+import { FieldResponse, ResWithHashedFields, WithForm } from '../../../../types'
+import { createReqMeta } from '../../../utils/request'
+import { ConflictError } from '../submission.errors'
+import { getProcessedResponses } from '../submission.service'
 import { ProcessedFieldResponse } from '../submission.types'
 
 import * as EmailSubmissionReceiver from './email-submission.receiver'
@@ -68,5 +72,56 @@ export const receiveEmailSubmission: RequestHandler = (req, res, next) => {
       })
       // const { errorMessage, statusCode } = mapRouteError(error)
       // return res.status(statusCode).json({ message: errorMessage })
+    })
+}
+
+/**
+ * Extracts relevant fields, injects questions, verifies visibility of field and validates answers
+ * to produce req.body.parsedResponses
+ *
+ * @param req - Express request object
+ * @param res - Express response object
+ * @param next - Express next middleware function
+ */
+export const validateEmailSubmission: RequestHandler<
+  ParamsDictionary,
+  unknown,
+  { responses?: FieldResponse[]; parsedResponses: ProcessedFieldResponse[] }
+> = (req, res, next) => {
+  const { form } = req as WithForm<typeof req>
+
+  if (!req.body.responses) {
+    return res.sendStatus(StatusCodes.BAD_REQUEST)
+  }
+
+  getProcessedResponses(form, req.body.responses)
+    .map((parsedResponses) => {
+      req.body.parsedResponses = parsedResponses
+      delete req.body.responses // Prevent downstream functions from using responses by deleting it
+      return next()
+    })
+    .mapErr((error) => {
+      logger.error({
+        message:
+          error instanceof ConflictError
+            ? 'Conflict - Form has been updated'
+            : 'Error processing responses',
+        meta: {
+          action: 'validateEmailSubmission',
+          ...createReqMeta(req),
+          formId: form._id,
+        },
+        error,
+      })
+      if (error instanceof ConflictError) {
+        return res.status(error.status).json({
+          message:
+            'The form has been updated. Please refresh and submit again.',
+        })
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message:
+          'There is something wrong with your form submission. Please check your responses and try again. If the problem persists, please refresh the page.',
+      })
     })
 }
