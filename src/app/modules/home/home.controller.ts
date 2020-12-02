56c08fb22c919f5f6d046d289cde2042d5760215
Yuanruo Liang <yuanruo@open.gov.sg>
2020-12-02 17:25:56 +0800

    refactor: migrate home controller, routes to TS

---
+++ src/app/modules/home/home.controller.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from 'express'
+
+/**
+ * Renders the root page of the application
+ * @param  {Object} req - Express request object
+ * @param  {Object} res - Express response object
+ */
+export const home: RequestHandler = (req, res) => {
+  res.render('index', {
+    user: JSON.stringify(req.session?.user) || 'null',
+  })
+  return
+}
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,6 +1,5 @@
 module.exports = [
   require('./admin-forms.server.routes.js'),
-  require('./core.server.routes.js'),
   require('./frontend.server.routes.js'),
   require('./public-forms.server.routes.js'),
 ]
