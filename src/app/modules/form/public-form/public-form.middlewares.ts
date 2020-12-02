8e0ef9eaed86126360cfad741a21add1bb1793e0
Yuanruo Liang <yuanruo@open.gov.sg>
2020-12-02 19:54:37 +0800

    refactor: convert file to TypeScript

--- src/app/modules/form/public-form/public-form.middlewares.ts
+++ src/app/modules/form/public-form/public-form.middlewares.ts
@@ -1,24 +1,27 @@
 'use strict'
 
-const { StatusCodes } = require('http-status-codes')
+import { RequestHandler } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
+import { WithForm } from '../../../../types'
 /**
  * Checks if form is public
  * @param  {Object} req - Express request object
  * @param  {Object} res - Express response object
  * @param  {Object} next - Express next middleware function
  */
-exports.isFormPublic = function (req, res, next) {
-  switch (req.form.status) {
+export const isFormPublic: RequestHandler = (req, res, next) => {
+  const { form } = req as WithForm<typeof req>
+  switch (form.status) {
     case 'PUBLIC':
       return next()
     case 'ARCHIVED':
       return res.sendStatus(StatusCodes.GONE)
     default:
       return res.status(StatusCodes.NOT_FOUND).json({
-        message: req.form.inactiveMessage,
+        message: form.inactiveMessage,
         isPageFound: true, // Flag to prevent default 404 subtext ("please check link") from showing
-        formTitle: req.form.title,
+        formTitle: form.title,
       })
   }
 }
