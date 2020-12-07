e93d2fba060d8d949b3ca04a1b05d2b5b7d6594b
shuli-ogp <shuli@open.gov.sg>
2020-12-07 20:55:54 +0800

    fix: wrap initDate in a new Date object to avoid reference to minDate

--- src/public/modules/forms/base/components/field-date.client.component.js
+++ src/public/modules/forms/base/components/field-date.client.component.js
@@ -58,8 +58,17 @@ function dateFieldComponentController() {
       }
 
       // If minDate is set, default view upon opening datepicker to be month of minDate
+      // Setting vm.dateOptions.initDate = vm.dateOptions.minDate directly leads to a bug
+      // whereby after a date is selected on the datepicker,minDate is set to the start of
+      // the month of the selected date.
+      // This is possibly because init date is automatically updated to the month of
+      // the latest date selection (did not find more details in documentation);
+      // since object properties are passed by reference, this led to dateOptions.minDate
+      // also being updated to the start of the selected month
       if (vm.dateOptions.minDate) {
-        vm.dateOptions.initDate = vm.dateOptions.minDate
+        vm.dateOptions.initDate = new Date(vm.dateOptions.minDate)
+      } else {
+        vm.dateOptions.initDate = null
       }
     }
   }
