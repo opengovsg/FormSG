aee09d55620552951b3cb6297e66438354a3ce44
shuli-ogp <shuli@open.gov.sg>
2020-12-07 20:09:47 +0800

    chore: remove export

--- src/app/utils/field-validation/validators/dateValidator.ts
+++ src/app/utils/field-validation/validators/dateValidator.ts
@@ -17,7 +17,7 @@ type DateValidatorConstructor = (dateField: IDateField) => DateValidator
  * @param date
  * @returns a moment with the date in the format 'DD MMM YYYY'
  */
-export const createMomentFromDateString = (date: string): moment.Moment => {
+const createMomentFromDateString = (date: string): moment.Moment => {
   const DATE_FORMAT = 'DD MMM YYYY'
 
   return moment(date, DATE_FORMAT, true)
