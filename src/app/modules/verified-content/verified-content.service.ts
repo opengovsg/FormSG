30ef55da88ddbca5bc1acd7884dc8423ecdbcd5f
Kar Rui Lau <karrui.lau@gmail.com>
2020-12-22 19:07:27 +0800

    feat(VerifiedContentSvc): add encryptVerifiedContent service fn

--- src/app/modules/verified-content/verified-content.service.ts
+++ src/app/modules/verified-content/verified-content.service.ts
@@ -1,9 +1,14 @@
+import { err, ok, Result } from 'neverthrow'
 import { UnreachableCaseError } from 'ts-essentials'
 
+import formsgSdk from '../../../config/formsg-sdk'
+import { createLoggerWithLabel } from '../../../config/logger'
 import { AuthType } from '../../../types'
 
+import { EncryptVerifiedContentError } from './verified-content.errors'
 import {
   CpVerifiedContent,
+  EncryptVerificationContentParams,
   GetVerifiedContentParams,
   SpVerifiedContent,
   VerifiedContentResult,
@@ -13,6 +18,8 @@ import {
   assertSpVerifiedContentShape,
   mapDataToKey,
 } from './verified-content.utils'
+
+const logger = createLoggerWithLabel(module)
 
 export const getVerifiedContent = ({
   type,
@@ -28,5 +35,32 @@ export const getVerifiedContent = ({
       return assertCpVerifiedContentShape(processedVerifiedContent)
     default:
       throw new UnreachableCaseError(type)
+  }
+}
+
+export const encryptVerifiedContent = ({
+  verifiedContent,
+  formPublicKey,
+  signingSecretKey,
+}: EncryptVerificationContentParams & {
+  signingSecretKey: string
+}): Result<string, EncryptVerifiedContentError> => {
+  try {
+    const encryptedContent = formsgSdk.crypto.encrypt(
+      verifiedContent,
+      formPublicKey,
+      signingSecretKey,
+    )
+    return ok(encryptedContent)
+  } catch (error) {
+    logger.error({
+      message: 'Unable to encrypt verified content',
+      meta: {
+        action: 'encryptVerifiedContent',
+      },
+      error,
+    })
+
+    return err(new EncryptVerifiedContentError())
   }
 }
