abff77489a1f08267a670689567c4b1dd38414d4
Antariksh <antarikshmahajan@gmail.com>
2020-12-22 19:51:16 +0800

    ref: add logging for each error

--- src/app/modules/submission/email-submission/email-submission.controller.ts
+++ src/app/modules/submission/email-submission/email-submission.controller.ts
@@ -1,11 +1,14 @@
-import { RequestHandler } from 'express'
+import { Request, RequestHandler } from 'express'
 
-import { CaptchaFactory } from 'src/app/services/captcha/captcha.factory'
-
+import { createLoggerWithLabel } from '../../../../config/logger'
 import { FieldResponse } from '../../../../types'
+import { CaptchaFactory } from '../../../services/captcha/captcha.factory'
+import { createReqMeta } from '../../../utils/request'
 import * as FormService from '../../form/form.service'
 
 import { mapRouteError } from './email-submission.util'
+
+const logger = createLoggerWithLabel(module)
 
 export const handleEmailSubmission: RequestHandler<
   { formId: string },
@@ -13,9 +16,19 @@ export const handleEmailSubmission: RequestHandler<
   { responses: FieldResponse[] },
   { captchaResponse: string | null }
 > = async (req, res) => {
+  const logMeta = {
+    action: 'handleEmailSubmission',
+    ...createReqMeta(req as Request),
+    formId: req.params.formId,
+  }
   // Retrieve form
   const formResult = await FormService.retrieveFullFormById(req.params.formId)
   if (formResult.isErr()) {
+    logger.error({
+      message: 'Error while retrieving form from database',
+      meta: logMeta,
+      error: formResult.error,
+    })
     const { errorMessage, statusCode } = mapRouteError(formResult.error)
     return res.status(statusCode).json({ message: errorMessage })
   }
@@ -24,6 +37,11 @@ export const handleEmailSubmission: RequestHandler<
   // Check that form is public
   const formPublicResult = FormService.isFormPublic(form)
   if (formPublicResult.isErr()) {
+    logger.warn({
+      message: 'Attempt to submit non-public form',
+      meta: logMeta,
+      error: formPublicResult.error,
+    })
     const { errorMessage, statusCode } = mapRouteError(formPublicResult.error)
     return res.status(statusCode).json({ message: errorMessage })
   }
@@ -35,6 +53,11 @@ export const handleEmailSubmission: RequestHandler<
       req.connection.remoteAddress,
     )
     if (captchaResult.isErr()) {
+      logger.error({
+        message: 'Error while verifying captcha',
+        meta: logMeta,
+        error: captchaResult.error,
+      })
       const { errorMessage, statusCode } = mapRouteError(captchaResult.error)
       return res.status(statusCode).json({ message: errorMessage })
     }
