c8647c80e51ce4f3e475a1a66340ead7461e309e
Yuanruo Liang <yuanruo@open.gov.sg>
2020-12-24 13:56:08 +0800

    fix: assert-unreachable

--- src/app/utils/assert-unreachable.ts
+++ src/app/utils/assert-unreachable.ts
@@ -6,5 +6,6 @@
  * https://stackoverflow.com/questions/39419170/how-do-i-check-that-a-switch-block-is-exhaustive-in-typescript
  */
 export const assertUnreachable = (switchCase: never): never => {
+  // eslint-disable-next-line
   throw new Error(`This should never be reached in TypeScript: "${switchCase}"`)
 }
