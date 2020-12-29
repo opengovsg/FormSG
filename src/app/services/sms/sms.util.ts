01d05838504b0e3f88f0bd420ca23fa0f9ac5fe5
Antariksh <antarikshmahajan@gmail.com>
2020-12-29 18:40:39 +0800

    fix: dedent text content

--- src/app/services/sms/sms.util.ts
+++ src/app/services/sms/sms.util.ts
@@ -1,4 +1,6 @@
-export const renderFormDeactivatedSms = (formTitle: string): string => `
+import dedent from 'dedent-js'
+
+export const renderFormDeactivatedSms = (formTitle: string): string => dedent`
   Due to responses bouncing from all recipient inboxes, your form "${formTitle}" has been automatically deactivated to prevent further response loss.
 
   Please ensure your recipient email addresses (Settings tab) have the ability to receive emailed responses from us. Invalid email addresses should be deleted, and full inboxes should be cleared.
@@ -6,6 +8,6 @@ export const renderFormDeactivatedSms = (formTitle: string): string => `
   If a systemic email issue is affecting email delivery, consider temporarily deactivating your form until email delivery is stable, or switching the form to Storage mode to continue receiving responses.
 `
 
-export const renderBouncedSubmissionSms = (formTitle: string): string => `
+export const renderBouncedSubmissionSms = (formTitle: string): string => dedent`
   A response to your form "${formTitle}" has bounced from all recipient inboxes. Bounced responses cannot be recovered. To prevent more bounces, please ensure recipient email addresses are correct, and clear any full inboxes.
 `
