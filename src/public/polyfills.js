fb9e3eabb5aa988b20b9f834d8b93c5cbfb96d5b
Yuanruo Liang <yuanruo@open.gov.sg>
2021-01-23 17:04:35 +0800

    doc: add explanations for each

--- src/public/polyfills.js
+++ src/public/polyfills.js
@@ -1,8 +1,14 @@
+// Polyfills crucial, commonly used functions from the standard library.
+// The stable option includes ES and web standards.
+// See link for more info:
+//  https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md
 require('core-js/stable')
+
+// Runtime library for async and generator functions
 require('regenerator-runtime/runtime')
 
 require('web-streams-polyfill') // Web Streams
-require('whatwg-fetch') // fetch
+require('whatwg-fetch') // fetch API
 require('abortcontroller-polyfill/dist/polyfill-patch-fetch') // AbortController
 
 // TextEncoder and TextDecoder
