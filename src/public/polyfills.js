4522c30a72b0b86d9644f7bd0720f3092ebee0eb
Yuanruo Liang <yuanruo@open.gov.sg>
2021-01-23 16:49:32 +0800

    refactor: move polyfills used in main JS thread into a single file

--- src/public/polyfills.js
+++ src/public/polyfills.js
@@ -1,4 +1,17 @@
-require('web-streams-polyfill')
-require('text-encoding')
-require('whatwg-fetch')
-require('abortcontroller-polyfill/dist/polyfill-patch-fetch')
+require('core-js/stable')
+require('regenerator-runtime/runtime')
+
+require('web-streams-polyfill') // Web Streams
+require('whatwg-fetch') // fetch
+require('abortcontroller-polyfill/dist/polyfill-patch-fetch') // AbortController
+
+// TextEncoder and TextDecoder
+const textEncoding = require('text-encoding')
+
+if (!window['TextDecoder']) {
+  window['TextDecoder'] = textEncoding.TextDecoder
+}
+
+if (!window['TextEncoder']) {
+  window['TextEncoder'] = textEncoding.TextEncoder
+}
