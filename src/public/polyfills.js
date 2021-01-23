10e05b63a9cc48fe4a895f8a6a352b8772ba47b0
Yuanruo Liang <yuanruo@open.gov.sg>
2021-01-23 17:40:13 +0800

    feat(performance): only IE11 should receive existing polyfills

--- src/public/polyfills.js
+++ src/public/polyfills.js
@@ -1,23 +1,39 @@
-// Polyfills crucial, commonly used functions from the standard library.
-// The stable option includes ES and web standards.
-// See link for more info:
-//  https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md
-require('core-js/stable')
+/**
+ * Runtime logic to inject polyfills into the global namespace based on browser detection.
+ */
 
-// Runtime library for async and generator functions
-require('regenerator-runtime/runtime')
+if (isInternetExplorer()) {
+  // Polyfills crucial, commonly used functions expected to be in the standard library
+  // e.g. Promises, Object.values, Array.prototype.includes
+  // The stable option includes ES and web standards.
+  // See link for more info:
+  // https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md
+  require('core-js/stable')
 
-require('web-streams-polyfill') // Web Streams
-require('whatwg-fetch') // fetch API
-require('abortcontroller-polyfill/dist/polyfill-patch-fetch') // AbortController
+  // Runtime library for async and generator functions
+  require('regenerator-runtime/runtime')
 
-// TextEncoder and TextDecoder
-const textEncoding = require('text-encoding')
+  require('web-streams-polyfill') // Web Streams API
+  require('whatwg-fetch') // fetch API
+  require('abortcontroller-polyfill/dist/polyfill-patch-fetch') // AbortController
 
-if (!window['TextDecoder']) {
-  window['TextDecoder'] = textEncoding.TextDecoder
+  // TextEncoder and TextDecoder
+  const textEncoding = require('text-encoding')
+
+  if (!window['TextDecoder']) {
+    window['TextDecoder'] = textEncoding.TextDecoder
+  }
+
+  if (!window['TextEncoder']) {
+    window['TextEncoder'] = textEncoding.TextEncoder
+  }
 }
 
-if (!window['TextEncoder']) {
-  window['TextEncoder'] = textEncoding.TextEncoder
+/**
+ * Detects if the runtime browser is Internet Explorer. Works for IE5 and upwards.
+ * Should be pretty safe since even IE 10 is considered ancient.
+ * See https://www.w3schools.com/jsref/prop_doc_documentmode.asp
+ */
+function isInternetExplorer() {
+  return Boolean(window.document.documentMode)
 }
