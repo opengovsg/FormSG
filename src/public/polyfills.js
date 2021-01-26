9708e8e1e97a37bcb3dfc08431aa6b579ef0294c
Yuanruo Liang <yuanruo@open.gov.sg>
2021-01-26 12:02:32 +0800

    refactor: convert from browser detection to feature detection

--- src/public/polyfills.js
+++ src/public/polyfills.js
@@ -1,45 +1,36 @@
 /**
- * Runtime logic to inject polyfills into the global namespace based on browser detection.
+ * Runtime polyfill injection with feature detection to make app work on older browsers.
  */
 
 // core-js polyfills crucial, commonly used functions expected to be in the standard library
-// e.g. Promises, Object.values, Array.prototype.includes
-// The stable option includes ES and web standards.
-// See link for more info:
+// e.g. Promise, Object.values, Array.prototype.includes
+// The stable option includes ES and web standards. See link for more info:
 // https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md
 require('core-js/stable')
 
 // Runtime library for async and generator functions. Babel transpilation expects this to be in
-// the target namespace, and it also relies on core-js, so this cannot be optimised away
+// the target namespace, and it also relies on core-js, so this cannot be removed
 // until IE11 support is dropped.
 require('regenerator-runtime/runtime')
 
-// IE-specific polyfills
-if (isInternetExplorer()) {
-  require('web-streams-polyfill') // Web Streams API
-  require('whatwg-fetch') // fetch API
-  require('abortcontroller-polyfill/dist/polyfill-patch-fetch') // AbortController
-
-  // TextEncoder and TextDecoder
-  const textEncoding = require('text-encoding')
-
-  if (!window['TextDecoder']) {
-    window['TextDecoder'] = textEncoding.TextDecoder
-  }
-
-  if (!window['TextEncoder']) {
-    window['TextEncoder'] = textEncoding.TextEncoder
-  }
+// Web Streams API has varying levels of support amongst different browsers
+// See https://caniuse.com/streams
+if (!window.ReadableStream || !window.WritableStream) {
+  require('web-streams-polyfill')
 }
 
-/**
- * Detects if the runtime browser is Internet Explorer. Works for IE5 and upwards, so
- * should be considered pretty safe since even IE 10 is ancient.
- *
- * This function is not safe to call from WebWorkers as those do not have access to the
- * window object.
- * See https://www.w3schools.com/jsref/prop_doc_documentmode.asp
- */
-function isInternetExplorer() {
-  return Boolean(window.document.documentMode)
+// For IE11, Opera Mini
+// https://caniuse.com/?search=fetch
+if (!window.fetch) {
+  require('whatwg-fetch') // fetch API
+  require('abortcontroller-polyfill/dist/polyfill-patch-fetch')
+}
+
+// For IE11, Opera Mini
+// https://caniuse.com/?search=TextEncoder
+if (!window.TextDecoder || !window.TextEncoder) {
+  // TextEncoder and TextDecoder
+  const textEncoding = require('text-encoding')
+  window['TextDecoder'] = textEncoding.TextDecoder
+  window['TextEncoder'] = textEncoding.TextEncoder
 }
