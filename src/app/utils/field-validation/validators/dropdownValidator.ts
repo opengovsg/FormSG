39b8a92797af70bdefc18f4c881933c7f4e84fef
shuli-ogp <shuli@open.gov.sg>
2021-02-02 21:07:37 +0800

    chore: inject fieldOptions for MyInfo field

--- src/app/utils/field-validation/validators/dropdownValidator.ts
+++ src/app/utils/field-validation/validators/dropdownValidator.ts
@@ -5,6 +5,8 @@ import { ProcessedSingleAnswerResponse } from 'src/app/modules/submission/submis
 import { IDropdownField } from 'src/types/field'
 import { ResponseValidator } from 'src/types/field/utils/validation'
 
+import { types as myInfoTypes } from '../../../../shared/resources/myinfo'
+
 import { notEmptySingleAnswerResponse } from './common'
 import { isOneOfOptions } from './options'
 
@@ -16,6 +18,17 @@ type DropdownValidatorConstructor = (
 const makeDropdownValidator: DropdownValidatorConstructor = (dropdownField) => (
   response,
 ) => {
+  // Inject fieldOptions for MyInfo. This is necessary because the
+  // client strips out MyInfo data to keep each form submission lightweight
+  const { myInfo } = dropdownField
+  if (myInfo && myInfo.attr) {
+    const [myInfoField] = myInfoTypes.filter(
+      (type) => type.name === myInfo.attr,
+    )
+    const { fieldOptions } = myInfoField
+    if (fieldOptions) dropdownField.fieldOptions = fieldOptions
+  }
+
   const { fieldOptions } = dropdownField
   const { answer } = response
   return isOneOfOptions(fieldOptions, answer)
