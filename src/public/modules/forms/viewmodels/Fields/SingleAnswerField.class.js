2239e4883c01b76468e32a31894f7b8c07b7dcb5
Antariksh Mahajan <antarikshmahajan@gmail.com>
2021-02-09 09:47:22 +0800

    fix: interrupt submission if field is blank (#1096)
    * fix: interrupt submission if field is blank
    
    * feat: check individual field $invalid prop
    
    * feat: add console error before throwing error
    
    * docs: add explanation for validity check
    
    * feat: suggest different browser in error message

--- src/public/modules/forms/viewmodels/Fields/SingleAnswerField.class.js
+++ src/public/modules/forms/viewmodels/Fields/SingleAnswerField.class.js
@@ -19,6 +19,22 @@ class SingleAnswerField extends AnswerField {
    */
   getResponse() {
     const response = super.getResponse()
+    // Throw error if field value is missing
+    if (
+      this.isVisible &&
+      this.required &&
+      this.fieldType !== 'section' &&
+      !this.fieldValue
+    ) {
+      console.error(
+        `Attempt to getResponse on required field with no answer:\tfieldType=${
+          this.fieldType
+        }, fieldId=${this._id}, typeof fieldValue=${typeof this.fieldValue}`,
+      )
+      throw new Error(
+        `Missing answer for required field, fieldType ${this.fieldType}.`,
+      )
+    }
     response.answer =
       this.fieldValue === undefined || this.fieldValue === null
         ? ''
