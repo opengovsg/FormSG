7fcf0da3d44cad3c2427384d9f4414cf69a018ac
Kar Rui Lau <karrui.lau@gmail.com>
2021-02-15 13:30:31 +0800

    Revert "feat(FormModel): add syncCustomLogo form method"
    This reverts commit d0963148cab05f1aa8bb0165edce8b0c166aa266.

--- src/types/form.ts
+++ src/types/form.ts
@@ -151,12 +151,6 @@ export interface IFormSchema extends IForm, Document {
   getDuplicateParams(
     overrideProps: OverrideProps,
   ): PickDuplicateForm & OverrideProps
-
-  /**
-   * @deprecated
-   * Syncs form.customLogo state with current form.startPage.logo value.
-   */
-  syncCustomLogo(): IFormDocument
 }
 
 /**
