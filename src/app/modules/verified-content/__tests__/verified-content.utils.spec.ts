8cf94ac29575562c77e6b5a0a2f6364c14b4b1a4
Kar Rui Lau <karrui.lau@gmail.com>
2021-02-17 11:55:53 +0800

    test(verifiedContentUtils): add unit tests

---
+++ src/app/modules/verified-content/__tests__/verified-content.utils.spec.ts
@@ -0,0 +1,93 @@
+import { MalformedVerifiedContentError } from '../verified-content.errors'
+import {
+  getCpVerifiedContent,
+  getSpVerifiedContent,
+} from '../verified-content.utils'
+
+describe('verified-content.utils', () => {
+  describe('getSpVerifiedContent', () => {
+    it('should successfully create SpVerifiedContent', async () => {
+      // Arrange
+      const correctDataWithExtra = {
+        uinFin: 'something',
+        extraData: 'extra',
+      }
+
+      // Act
+      const actualResult = getSpVerifiedContent(correctDataWithExtra)
+
+      // Assert
+      expect(actualResult._unsafeUnwrap()).toEqual({
+        uinFin: correctDataWithExtra.uinFin,
+      })
+    })
+    it('should return MalformedVerifiedContentError on invalid shape', async () => {
+      // Arrange
+      const incorrectData = {
+        incorrect: 'something',
+        extraData: 'extra',
+      }
+
+      // Act
+      const actualResult = getSpVerifiedContent(incorrectData)
+
+      // Assert
+      expect(actualResult._unsafeUnwrapErr()).toEqual(
+        new MalformedVerifiedContentError(),
+      )
+    })
+  })
+
+  describe('getCpVerifiedContent', () => {
+    it('should successfully create CpVerifiedContent', async () => {
+      // Arrange
+      const correctDataWithExtra = {
+        uinFin: 'something',
+        userInfo: 'another',
+        extraData: 'extra',
+      }
+
+      // Act
+      const actualResult = getCpVerifiedContent(correctDataWithExtra)
+
+      // Assert
+      expect(actualResult._unsafeUnwrap()).toEqual({
+        cpUen: correctDataWithExtra.uinFin,
+        cpUid: correctDataWithExtra.userInfo,
+      })
+    })
+
+    it('should return MalformedVerifiedContentError on partial shape', async () => {
+      // Arrange
+      // Has uinFin but not userInfo key
+      const partialData = {
+        uinFin: 'some uinFin',
+        extraData: 'extra',
+      }
+
+      // Act
+      const actualResult = getCpVerifiedContent(partialData)
+
+      // Assert
+      expect(actualResult._unsafeUnwrapErr()).toEqual(
+        new MalformedVerifiedContentError(),
+      )
+    })
+
+    it('should return MalformedVerifiedContentError on invalid shape', async () => {
+      // Arrange
+      const incorrectData = {
+        incorrect: 'something',
+        extraData: 'extra',
+      }
+
+      // Act
+      const actualResult = getCpVerifiedContent(incorrectData)
+
+      // Assert
+      expect(actualResult._unsafeUnwrapErr()).toEqual(
+        new MalformedVerifiedContentError(),
+      )
+    })
+  })
+})
