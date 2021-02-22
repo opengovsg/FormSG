722f90ee75368fa5926958840ed1e81365d3f4f5
Antariksh <antarikshmahajan@gmail.com>
2021-02-22 17:09:50 +0800

    docs: add docs for constants

--- src/app/modules/myinfo/myinfo.constants.ts
+++ src/app/modules/myinfo/myinfo.constants.ts
@@ -1,8 +1,29 @@
 import config from '../../../config/config'
 
+/**
+ * Top-level router prefix to be used for MyInfo routes.
+ * We make this a constant because it is part of the route
+ * registered with MyInfo.
+ */
 export const MYINFO_ROUTER_PREFIX = '/myinfo'
+
+/**
+ * Path to be added after MYINFO_ROUTER_PREFIX for the login
+ * route. The combination of MYINFO_ROUTER_PREFIX and
+ * MYINFO_REDIRECT_PATH is the redirect endpoint registered with
+ * MyInfo.
+ */
 export const MYINFO_REDIRECT_PATH = '/login'
+
+/**
+ * Name of cookie which contains state of MyInfo login, and access
+ * token if login was successful.
+ */
 export const MYINFO_COOKIE_NAME = 'MyInfoCookie'
+
+/**
+ * Settings for the MyInfo cookie which contains the access token
+ */
 export const MYINFO_COOKIE_OPTIONS = {
   // Important for security - access token cannot be read by client-side JS
   httpOnly: true,
