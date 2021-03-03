7c4f7bdcf146a2290a40ed108a377dbc1e439708
Kar Rui Lau <karrui.lau@gmail.com>
2021-03-03 10:06:22 +0800

    test: use test specific session memory store

--- src/loaders/express/session.ts
+++ src/loaders/express/session.ts
@@ -1,12 +1,12 @@
 import MongoStore from 'connect-mongo'
 import cookieParser from 'cookie-parser'
+import { RequestHandler } from 'express'
 import session from 'express-session'
 import { Connection } from 'mongoose'
 
-import config, { nodeEnv } from '../../config/config'
-import { Environment } from '../../types/config'
+import config from '../../config/config'
 
-const sessionMiddlewares = (connection: Connection) => {
+const sessionMiddlewares = (connection: Connection): RequestHandler[] => {
   // Configure express-session and connect to mongo
   const expressSession = session({
     saveUninitialized: false,
@@ -14,12 +14,9 @@ const sessionMiddlewares = (connection: Connection) => {
     secret: config.sessionSecret,
     cookie: config.cookieSettings,
     name: 'connect.sid',
-    store:
-      nodeEnv === Environment.Test
-        ? new session.MemoryStore()
-        : MongoStore.create({
-            clientPromise: Promise.resolve(connection.getClient()),
-          }),
+    store: MongoStore.create({
+      clientPromise: Promise.resolve(connection.getClient()),
+    }),
   })
 
   return [
