9c446557ecce8e178757dc3aa2b0c807d502a994
shuli-ogp <shuli@open.gov.sg>
2021-03-04 10:10:26 +0800

    refactor: modify helper functions and use them directly in tests

--- tests/unit/backend/utils/field-validation/attachment-validation.spec.ts
+++ tests/unit/backend/utils/field-validation/attachment-validation.spec.ts
@@ -1,9 +1,8 @@
 import { ObjectId } from 'mongodb'
 
 import { ValidateFieldError } from 'src/app/modules/submission/submission.errors'
-import { ProcessedAttachmentResponse } from 'src/app/modules/submission/submission.types'
 import { validateField } from 'src/app/utils/field-validation/'
-import { BasicField, IAttachmentFieldSchema, IField } from 'src/types'
+import { AttachmentSize, BasicField } from 'src/types'
 
 import {
   generateDefaultField,
@@ -11,37 +10,14 @@ import {
 } from '../../helpers/generate-form-data'
 
 describe('Attachment validation', () => {
-  const makeField = (
-    fieldId: string,
-    attachmentSize: string,
-    customParams?: Partial<IField>,
-  ): IAttachmentFieldSchema => {
-    return {
-      ...generateDefaultField(BasicField.Attachment, customParams),
-      _id: fieldId,
-      attachmentSize,
-    } as IAttachmentFieldSchema
-  }
-
-  const makeResponse = (
-    fieldId: string,
-    content: Buffer | undefined,
-    customParams?: Partial<ProcessedAttachmentResponse>,
-  ): ProcessedAttachmentResponse => {
-    return {
-      ...generateNewAttachmentResponse(customParams),
-      _id: fieldId,
-      content,
-    } as ProcessedAttachmentResponse
-  }
-
   const formId = new ObjectId().toHexString()
-  const fieldId = new ObjectId().toHexString()
 
   describe('Required or optional', () => {
     it('should disallow submission with no attachment if it is required', () => {
-      const formField = makeField(fieldId, '1')
-      const response = makeResponse(fieldId, undefined)
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+      })
+      const response = generateNewAttachmentResponse({ content: undefined })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isErr()).toBe(true)
       expect(validateResult._unsafeUnwrapErr()).toEqual(
@@ -50,32 +26,53 @@ describe('Attachment validation', () => {
     })
 
     it('should allow submission with attachment if it is required', () => {
-      const formField = makeField(fieldId, '1')
-      const response = makeResponse(fieldId, Buffer.alloc(1))
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(1),
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isOk()).toBe(true)
       expect(validateResult._unsafeUnwrap()).toEqual(true)
     })
 
     it('should allow submission with attachment if it is optional', () => {
-      const formField = makeField(fieldId, '1', { required: false })
-      const response = makeResponse(fieldId, Buffer.alloc(1))
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+        required: false,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(1),
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isOk()).toBe(true)
       expect(validateResult._unsafeUnwrap()).toEqual(true)
     })
 
     it('should allow submission with no attachment if it is not required', () => {
-      const formField = makeField(fieldId, '1', { required: false })
-      const response = makeResponse(fieldId, undefined, { answer: '' })
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+        required: false,
+      })
+      const response = generateNewAttachmentResponse({
+        content: undefined,
+        answer: '',
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isOk()).toBe(true)
       expect(validateResult._unsafeUnwrap()).toEqual(true)
     })
 
     it('should disallow submission with no answer if it is required', () => {
-      const formField = makeField(fieldId, '1')
-      const response = makeResponse(fieldId, Buffer.alloc(1), { answer: '' })
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+        required: true,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(1),
+        answer: '',
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isErr()).toBe(true)
       expect(validateResult._unsafeUnwrapErr()).toEqual(
@@ -84,16 +81,28 @@ describe('Attachment validation', () => {
     })
 
     it('should allow submission with no answer if it is not required', () => {
-      const formField = makeField(fieldId, '1', { required: false })
-      const response = makeResponse(fieldId, Buffer.alloc(1), { answer: '' })
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+        required: false,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(1),
+        answer: '',
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isOk()).toBe(true)
       expect(validateResult._unsafeUnwrap()).toEqual(true)
     })
 
     it('should disallow when it is not required but with answer and no attachment', () => {
-      const formField = makeField(fieldId, '1', { required: false })
-      const response = makeResponse(fieldId, undefined)
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+        required: false,
+      })
+      const response = generateNewAttachmentResponse({
+        answer: 'some answer',
+        content: undefined,
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isErr()).toBe(true)
       expect(validateResult._unsafeUnwrapErr()).toEqual(
@@ -104,16 +113,24 @@ describe('Attachment validation', () => {
 
   describe('Validation of attachment size', () => {
     it('should allow attachment with valid size', () => {
-      const formField = makeField(fieldId, '1')
-      const response = makeResponse(fieldId, Buffer.alloc(1))
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(1),
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isOk()).toBe(true)
       expect(validateResult._unsafeUnwrap()).toEqual(true)
     })
 
     it('should disallow attachment that exceeds size', () => {
-      const formField = makeField(fieldId, '1')
-      const response = makeResponse(fieldId, Buffer.alloc(2000000))
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.OneMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(2000000),
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isErr()).toBe(true)
       expect(validateResult._unsafeUnwrapErr()).toEqual(
@@ -122,8 +139,12 @@ describe('Attachment validation', () => {
     })
 
     it('should respect the attachmentSize from formField', () => {
-      const formField = makeField(fieldId, '3')
-      const response = makeResponse(fieldId, Buffer.alloc(2000000))
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.ThreeMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(2000000),
+      })
       const validateResult = validateField(formId, formField, response)
       expect(validateResult.isOk()).toBe(true)
       expect(validateResult._unsafeUnwrap()).toEqual(true)
@@ -132,8 +153,11 @@ describe('Attachment validation', () => {
 
   describe('check for responses on hidden fields', () => {
     it('should disallow responses submitted for hidden fields when response contains file content', () => {
-      const formField = makeField(fieldId, '3')
-      const response = makeResponse(fieldId, Buffer.alloc(2000000), {
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.ThreeMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: Buffer.alloc(2000000),
         answer: '',
         filename: '',
       })
@@ -148,8 +172,11 @@ describe('Attachment validation', () => {
     })
 
     it('should disallow responses submitted for hidden fields when response contains answer', () => {
-      const formField = makeField(fieldId, '3')
-      const response = makeResponse(fieldId, undefined, {
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.ThreeMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: undefined,
         answer: 'some answer',
         filename: '',
       })
@@ -164,8 +191,11 @@ describe('Attachment validation', () => {
     })
 
     it('should disallow responses submitted for hidden fields when response contains filename', () => {
-      const formField = makeField(fieldId, '3')
-      const response = makeResponse(fieldId, undefined, {
+      const formField = generateDefaultField(BasicField.Attachment, {
+        attachmentSize: AttachmentSize.ThreeMb,
+      })
+      const response = generateNewAttachmentResponse({
+        content: undefined,
         answer: '',
         filename: 'some filename',
       })
