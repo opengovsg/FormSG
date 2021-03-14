ee77134b0c42619202b0013e0be64e9c0e769663
Yuanruo Liang <yuanruo@open.gov.sg>
2021-03-14 17:42:49 +0800

    refactor(analytics.client): remove unused functions

--- src/public/modules/core/services/analytics.client.service.js
+++ src/public/modules/core/services/analytics.client.service.js
@@ -1,51 +1,9 @@
 const { default: axios } = require('axios')
 
-angular.module('core').factory('Analytics', ['$q', '$http', Analytics])
+angular.module('core').factory('Analytics', [Analytics])
 
-function Analytics($q, $http) {
+function Analytics() {
   let analyticsService = {
-    getFormCount: () => {
-      let resUrl = '/analytics/forms'
-      let deferred = $q.defer()
-      $http.get(resUrl).then(
-        function (response) {
-          deferred.resolve(response.data)
-        },
-        function (_errorResponse) {
-          deferred.reject('Failed to get form count')
-        },
-      )
-      return deferred.promise
-    },
-
-    getUserCount: () => {
-      let resUrl = '/analytics/users'
-      let deferred = $q.defer()
-      $http.get(resUrl).then(
-        function (response) {
-          deferred.resolve(response.data)
-        },
-        function (_errorResponse) {
-          deferred.reject('Failed to get user count')
-        },
-      )
-      return deferred.promise
-    },
-
-    getSubmissionCount: () => {
-      let resUrl = '/analytics/submissions'
-      let deferred = $q.defer()
-      $http.get(resUrl).then(
-        function (response) {
-          deferred.resolve(response.data)
-        },
-        function (_errorResponse) {
-          deferred.reject('Failed to get submission count')
-        },
-      )
-      return deferred.promise
-    },
-
     getStatistics: () => {
       return axios
         .get('/analytics/statistics')
