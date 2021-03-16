5b7ebd67d6e4a19dd6a84c537d3c6882fcab283a
Kar Rui Lau <karrui.lau@gmail.com>
2021-03-16 10:24:52 +0800

    feat(AdminFormRoutes): add PATCH settings route and validator

---
+++ src/app/modules/form/admin-form/admin-form.routes.ts
@@ -0,0 +1,43 @@
+import { celebrate, Joi, Segments } from 'celebrate'
+import { Router } from 'express'
+
+import { AuthType, Status } from '../../../../types'
+import { withUserAuthentication } from '../../auth/auth.middlewares'
+
+import { handleUpdateSettings } from './admin-form.controller'
+import { SettingsUpdateBody } from './admin-form.types'
+
+export const AdminRouter = Router()
+
+// All routes in this handler should be protected by authentication.
+AdminRouter.use(withUserAuthentication)
+
+/**
+ * Joi validator for PATCH /form/:formId/settings route.
+ */
+const updateSettingsValidator = celebrate({
+  [Segments.BODY]: Joi.object<SettingsUpdateBody>({
+    authType: Joi.string().valid(...Object.values(AuthType)),
+    emails: Joi.alternatives().try(
+      Joi.array().items(Joi.string()),
+      Joi.string(),
+    ),
+    esrvcId: Joi.string().allow(''),
+    hasCaptcha: Joi.boolean(),
+    inactiveMessage: Joi.string(),
+    permissionList: Joi.array().items({
+      email: Joi.string().email().required(),
+      write: Joi.boolean(),
+    }),
+    status: Joi.string().valid(...Object.values(Status)),
+    title: Joi.string(),
+    webhook: Joi.object({
+      url: Joi.string().uri().required().allow(''),
+    }),
+  }).length(1),
+})
+
+AdminRouter.route('/form/:formId([a-fA-F0-9]{24})/settings').patch(
+  updateSettingsValidator,
+  handleUpdateSettings,
+)
