caabf18d5f820eceb03c433fc5fc4c09296e0c24
Kar Rui Lau <karrui.lau@gmail.com>
2021-03-16 10:25:31 +0800

    feat: use new AdminRouter in ApiRouter /api/v1/admin route

--- src/loaders/express/router.ts
+++ src/loaders/express/router.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
 
+import { AdminRouter } from '../../app/modules/form/admin-form/admin-form.routes'
+
 export const API_ROOT_ROUTE = '/api/v1'
 
 export const ApiRouter = Router()
+
+ApiRouter.use('/admin', AdminRouter)
