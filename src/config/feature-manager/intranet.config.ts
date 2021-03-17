799404d46ebe32a530284b3d3b5007c81f163d08
Antariksh <antarikshmahajan@gmail.com>
2021-03-17 17:03:36 +0800

    docs: specify file format in config

--- src/config/feature-manager/intranet.config.ts
+++ src/config/feature-manager/intranet.config.ts
@@ -4,7 +4,8 @@ export const intranetFeature: RegisterableFeature<FeatureNames.Intranet> = {
   name: FeatureNames.Intranet,
   schema: {
     intranetIpListPath: {
-      doc: 'Path to list of intranet IP addresses',
+      doc:
+        'Path to file containing list of intranet IP addresses, separated by newlines',
       format: String,
       default: null,
       env: 'INTRANET_IP_LIST_PATH',
