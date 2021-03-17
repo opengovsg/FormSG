c909d359a2a54c8977ee6cc037a4af84a1320948
Kar Rui Lau <karrui.lau@gmail.com>
2021-03-17 17:30:08 +0800

    test: add integration tests for PATCH /admin/form/:formId/settings

--- src/app/routes/admin/form/admin-form.routes.ts
+++ src/app/routes/admin/form/admin-form.routes.ts
@@ -34,6 +34,25 @@ const updateSettingsValidator = celebrate({
   }).min(1),
 })
 
+/**
+ * Update form settings according to given subset of settings.
+ * @route PATCH /admin/form/:formId/settings
+ * @group admin
+ * @param body the subset of settings to patch
+ * @produces application/json
+ * @consumes application/json
+ * @returns 200 with latest form settings on successful update
+ * @returns 400 when given body fails Joi validation
+ * @returns 401 when current user is not logged in
+ * @returns 403 when current user does not have permissions to update form settings
+ * @returns 404 when form to update settings for cannot be found
+ * @returns 409 when saving form settings incurs a conflict in the database
+ * @returns 410 when updating settings for archived form
+ * @returns 413 when updating settings causes form to be too large to be saved in the database
+ * @returns 422 when an invalid settings update is attempted on the form
+ * @returns 422 when user in session cannot be retrieved from the database
+ * @returns 500 when database error occurs
+ */
 AdminFormRouter.route('/:formId([a-fA-F0-9]{24})/settings').patch(
   updateSettingsValidator,
   handleUpdateSettings,
