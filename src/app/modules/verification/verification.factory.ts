b294f6422064a6830a0392013fda9e440a5475d7
Antariksh <antarikshmahajan@gmail.com>
2021-03-22 10:15:07 +0800

    ref: implement factory in terms of service

--- src/app/modules/verification/verification.factory.ts
+++ src/app/modules/verification/verification.factory.ts
@@ -1,48 +1,38 @@
-import { RequestHandler } from 'express'
-import { StatusCodes } from 'http-status-codes'
+import { err, errAsync } from 'neverthrow'
 
 import featureManager, { FeatureNames } from '../../../config/feature-manager'
+import { MissingFeatureError } from '../core/core.errors'
 
-import * as verification from './verification.controller'
+import * as VerificationService from './verification.service'
 
 interface IVerifiedFieldsFactory {
-  createTransaction: RequestHandler
-  getTransactionMetadata: RequestHandler<{ transactionId: string }>
-  resetFieldInTransaction: RequestHandler<{ transactionId: string }>
-  getNewOtp: RequestHandler<{ transactionId: string }>
-  verifyOtp: RequestHandler<{ transactionId: string }>
+  createTransaction: typeof VerificationService.createTransaction
+  getTransactionMetadata: typeof VerificationService.getTransactionMetadata
+  getTransaction: typeof VerificationService.getTransaction
+  resetFieldForTransaction: typeof VerificationService.resetFieldForTransaction
+  sendNewOtp: typeof VerificationService.sendNewOtp
+  verifyOtp: typeof VerificationService.verifyOtp
 }
 
-const verifiedFieldsFactory = ({
+export const createVerificationFactory = ({
   isEnabled,
 }: {
   isEnabled: boolean
 }): IVerifiedFieldsFactory => {
   if (isEnabled) {
-    return {
-      createTransaction: verification.createTransaction,
-      getTransactionMetadata: verification.getTransactionMetadata,
-      resetFieldInTransaction: verification.resetFieldInTransaction,
-      getNewOtp: verification.getNewOtp,
-      verifyOtp: verification.verifyOtp,
-    }
-  } else {
-    const errMsg = 'Verified fields feature is not enabled'
-    return {
-      createTransaction: (req, res) =>
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errMsg }),
-      getTransactionMetadata: (req, res) =>
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errMsg }),
-      resetFieldInTransaction: (req, res) =>
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errMsg }),
-      getNewOtp: (req, res) =>
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errMsg }),
-      verifyOtp: (req, res) =>
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errMsg }),
-    }
+    return VerificationService
+  }
+  const error = new MissingFeatureError(FeatureNames.VerifiedFields)
+  return {
+    createTransaction: () => errAsync(error),
+    getTransactionMetadata: () => errAsync(error),
+    getTransaction: () => errAsync(error),
+    resetFieldForTransaction: () => errAsync(error),
+    sendNewOtp: () => Promise.resolve(err(error)),
+    verifyOtp: () => errAsync(error),
   }
 }
 
-export default verifiedFieldsFactory(
+export const VerificationFactory = createVerificationFactory(
   featureManager.get(FeatureNames.VerifiedFields),
 )
