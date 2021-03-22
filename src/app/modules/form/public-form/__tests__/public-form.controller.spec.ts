be961d080d7289aee77f80060e84948ffa2fb042
seaerchin <jer005@e.ntu.edu.sg>
2021-03-22 18:28:18 +0800

    test(public-form/controller/tests): add more tests

--- src/app/modules/form/public-form/__tests__/public-form.controller.spec.ts
+++ src/app/modules/form/public-form/__tests__/public-form.controller.spec.ts
@@ -1,17 +1,33 @@
 import { ObjectId } from 'bson-ext'
-import { merge } from 'lodash'
+import { MyInfoNoESrvcIdError } from 'dist/backend/app/modules/myinfo/myinfo.errors'
+import _, { merge } from 'lodash'
 import mongoose from 'mongoose'
 import { err, errAsync, ok, okAsync } from 'neverthrow'
 import querystring from 'querystring'
+import { MockedObject } from 'ts-jest/dist/utils/testing'
 import { mocked } from 'ts-jest/utils'
 
 import getFormFeedbackModel from 'src/app/models/form_feedback.server.model'
 import { DatabaseError } from 'src/app/modules/core/core.errors'
-import { IPopulatedForm } from 'src/types'
+import { MyInfoCookieState } from 'src/app/modules/myinfo/myinfo.types'
+import * as myInfoUtils from 'src/app/modules/myinfo/myinfo.util'
+import {
+  AuthType,
+  IPopulatedForm,
+  IPopulatedUser,
+  MyInfoAttribute,
+  PublicForm,
+} from 'src/types'
 
 import expressHandler from 'tests/unit/backend/helpers/jest-express'
 
 import * as AuthService from '../../../auth/auth.service'
+import {
+  MyInfoCookieStateError,
+  MyInfoMissingAccessTokenError,
+} from '../../../myinfo/myinfo.errors'
+import { MissingJwtError } from '../../../spcp/spcp.errors'
+import { SpcpFactory } from '../../../spcp/spcp.factory'
 import {
   FormDeletedError,
   FormNotFoundError,
@@ -26,9 +42,14 @@ import { Metatags } from '../public-form.types'
 jest.mock('../public-form.service')
 jest.mock('../../form.service')
 jest.mock('../../../auth/auth.service')
+jest.mock('../../../spcp/spcp.factory')
+jest.mock('src/app/modules/myinfo/myinfo.util')
+
 const MockFormService = mocked(FormService)
 const MockPublicFormService = mocked(PublicFormService)
 const MockAuthService = mocked(AuthService)
+const MockSpcpFactory = mocked(SpcpFactory)
+const MockMyInfoUtils = mocked(myInfoUtils)
 
 const FormFeedbackModel = getFormFeedbackModel(mongoose)
 
@@ -395,49 +416,262 @@ describe('public-form.controller', () => {
   })
 
   describe('handleGetPublicForm', () => {
+    // TODO: add tests for logging for spcp to ensure it's only called once
+    // TODO: ensure that saveMyInfoHashes is being called
     // Arrange variables that are shared throughout the test suite
     const MOCK_FORM_ID = new ObjectId().toHexString()
-    // const MOCK_SUCCESSFUL_FORM = {
-    //   _id: MOCK_FORM_ID,
-    //   title: 'mock form title',
-    //   inactiveMessage: 'This mock form is mock closed.',
-    // } as IPopulatedForm
+    const MOCK_USER_ID = new ObjectId().toHexString()
+    const MOCK_USER = {
+      _id: MOCK_USER_ID,
+      email: 'randomrandomtest@example.com',
+    } as IPopulatedUser
 
-    // Success goes here
+    const MOCK_SCRUBBED_FORM = ({
+      _id: MOCK_FORM_ID,
+      title: 'mock title',
+      admin: { _id: MOCK_USER_ID },
+    } as unknown) as PublicForm
 
-    // Failed errors
-    // TODO: this should be grouped together under the same class of errors
-    it('should return 500 if the form does not exist', async () => {
-      // Arrange
-      // 1. Mock the request
-      const MOCK_REQ = expressHandler.mockRequest({
-        params: {
-          formId: MOCK_FORM_ID,
-        },
+    const MOCK_FORM = (mocked({
+      admin: MOCK_USER,
+      _id: MOCK_FORM_ID,
+      title: 'Mock',
+      getPublicView: jest.fn().mockResolvedValue(MOCK_SCRUBBED_FORM),
+    }) as unknown) as MockedObject<IPopulatedForm>
+
+    const MOCK_REQ = expressHandler.mockRequest({
+      params: {
+        formId: MOCK_FORM_ID,
+      },
+    })
+
+    // Success
+
+    // Errors
+    describe('errors in myInfo', () => {
+      // Setup because this gets invoked at the start of the controller to decide which branch to take
+      const MOCK_MYINFO_FORM = (_(MOCK_FORM)
+        .set('authType', AuthType.MyInfo)
+        .set(
+          'getUniqueMyInfoAttrs',
+          jest.fn().mockReturnValue([MyInfoAttribute.Name]),
+        )
+        .value() as unknown) as MockedObject<IPopulatedForm>
+
+      MockAuthService.getFormIfPublic.mockReturnValue(okAsync(MOCK_MYINFO_FORM))
+      MockFormService.checkFormSubmissionLimitAndDeactivateForm.mockReturnValue(
+        okAsync(MOCK_MYINFO_FORM),
+      )
+
+      it('should return 200 but the response should have cookies cleared and myInfoError if the request has no cookie', async () => {
+        // Arrange
+        // 1. Mock the response and calls
+        const MOCK_RES = expressHandler.mockResponse()
+        const MOCK_CLEAR_COOKIE = jest.fn().mockReturnValueOnce(MOCK_RES)
+        // NOTE: This is done because the calls to .json and .clearCookie are chained
+        MOCK_RES.clearCookie = MOCK_CLEAR_COOKIE
+        MockMyInfoUtils.extractMyInfoCookie.mockReturnValueOnce(
+          err(new MyInfoMissingAccessTokenError()),
+        )
+
+        // Act
+        await PublicFormController.handleGetPublicForm(
+          _.set(MOCK_REQ, 'cookies', {}),
+          MOCK_RES,
+          jest.fn(),
+        )
+
+        // Assert
+        expect(MOCK_RES.clearCookie).toHaveBeenCalled()
+        expect(MOCK_RES.json).toHaveBeenCalledWith({
+          form: MOCK_FORM.getPublicView(),
+          myInfoError: true,
+        })
       })
 
-      const MOCK_RES = expressHandler.mockResponse()
+      it('should return 200 but the response should have cookies cleared and myInfoError if the cookie cannot be validated', async () => {
+        // Arrange
+        // 1. Mock the response and calls
+        const MOCK_RES = expressHandler.mockResponse()
+        const MOCK_CLEAR_COOKIE = jest.fn().mockReturnValueOnce(MOCK_RES)
+        MOCK_RES.clearCookie = MOCK_CLEAR_COOKIE
+        MockMyInfoUtils.extractMyInfoCookie.mockReturnValueOnce(
+          ok({ state: MyInfoCookieState.Error }),
+        )
+        MockMyInfoUtils.extractSuccessfulCookie.mockReturnValueOnce(
+          err(new MyInfoCookieStateError()),
+        )
 
-      // 2. Mock the call to retrieve the form
-      MockAuthService.getFormIfPublic.mockReturnValueOnce(
-        errAsync(new DatabaseError()),
-      )
+        // Act
+        await PublicFormController.handleGetPublicForm(
+          _.set(MOCK_REQ, 'cookies', { accessToken: 'cookie monster?' }),
+          MOCK_RES,
+          jest.fn(),
+        )
 
-      // Act
-      await PublicFormController.handleGetPublicForm(
-        MOCK_REQ,
-        MOCK_RES,
-        jest.fn(),
-      )
-      // Assert
+        // Assert
+        expect(MOCK_RES.clearCookie).toHaveBeenCalled()
+        expect(MOCK_RES.json).toHaveBeenCalledWith({
+          form: MOCK_FORM.getPublicView(),
+          myInfoError: true,
+        })
+      })
 
-      // 1. Check args of mocked services
-      expect(MockAuthService.getFormIfPublic).toHaveBeenCalledWith(MOCK_FORM_ID)
-      // 2. Check that error is correct
-      expect(
-        MockFormService.checkFormSubmissionLimitAndDeactivateForm,
-      ).not.toHaveBeenCalled()
-      expect(MOCK_RES.status).toHaveBeenCalledWith(500)
+      it('should return 200 but the response should have cookies cleared and myInfoError if the form cannot be validated', async () => {
+        // Arrange
+        // 1. Mock the response and calls
+        const MOCK_RES = expressHandler.mockResponse()
+        const MOCK_CLEAR_COOKIE = jest.fn().mockReturnValueOnce(MOCK_RES)
+        MOCK_RES.clearCookie = MOCK_CLEAR_COOKIE
+        MockMyInfoUtils.extractMyInfoCookie.mockReturnValueOnce(
+          ok({ state: MyInfoCookieState.Error }),
+        )
+        MockMyInfoUtils.extractSuccessfulCookie.mockReturnValueOnce(
+          err(new MyInfoCookieStateError()),
+        )
+        MockMyInfoUtils.validateMyInfoForm.mockReturnValueOnce(
+          err(new MyInfoNoESrvcIdError()),
+        )
+
+        // Act
+        await PublicFormController.handleGetPublicForm(
+          _.set(MOCK_REQ, 'cookies', { accessToken: 'cookie monster?' }),
+          MOCK_RES,
+          jest.fn(),
+        )
+
+        // Assert
+        expect(MOCK_RES.clearCookie).toHaveBeenCalled()
+        expect(MOCK_RES.json).toHaveBeenCalledWith({
+          form: MOCK_FORM.getPublicView(),
+          myInfoError: true,
+        })
+      })
+    })
+
+    describe('errors in spcp', () => {
+      it('should return 200 with the form but without a spcpSession', async () => {
+        // Arrange
+        // 1. Mock the response and calls
+        const MOCK_RES = expressHandler.mockResponse()
+        const MOCK_SPCP_FORM = _.set(MOCK_FORM, 'authType', AuthType.SP)
+
+        MockAuthService.getFormIfPublic.mockReturnValueOnce(
+          okAsync(MOCK_SPCP_FORM),
+        )
+        MockFormService.checkFormSubmissionLimitAndDeactivateForm.mockReturnValueOnce(
+          okAsync(MOCK_SPCP_FORM),
+        )
+        MockSpcpFactory.getSpcpSession.mockReturnValueOnce(
+          errAsync(new MissingJwtError()),
+        )
+
+        // Act
+        // 2. GET the endpoint
+        await PublicFormController.handleGetPublicForm(
+          MOCK_REQ,
+          MOCK_RES,
+          jest.fn(),
+        )
+
+        // Assert
+        // Status should be 200
+        // json object should only have form property
+        expect(MOCK_RES.json).toHaveBeenCalledWith({
+          form: MOCK_SPCP_FORM.getPublicView(),
+        })
+      })
+    })
+
+    describe('errors in form retrieval', () => {
+      it('should return 500 if a database error occurs', async () => {
+        // Arrange
+        // 1. Mock the response
+        const MOCK_RES = expressHandler.mockResponse()
+
+        // 2. Mock the call to retrieve the form
+        MockAuthService.getFormIfPublic.mockReturnValueOnce(
+          errAsync(new DatabaseError()),
+        )
+
+        // Act
+        await PublicFormController.handleGetPublicForm(
+          MOCK_REQ,
+          MOCK_RES,
+          jest.fn(),
+        )
+
+        // Assert
+        // 1. Check args of mocked services
+        expect(MockAuthService.getFormIfPublic).toHaveBeenCalledWith(
+          MOCK_FORM_ID,
+        )
+        // 2. Check that error is correct
+        expect(
+          MockFormService.checkFormSubmissionLimitAndDeactivateForm,
+        ).not.toHaveBeenCalled()
+        expect(MOCK_RES.status).toHaveBeenCalledWith(500)
+      })
+
+      it('should return 404 if the form is not found', async () => {
+        // Arrange
+        // 1. Mock the response
+        const MOCK_RES = expressHandler.mockResponse()
+
+        // 2. Mock the call to retrieve the form
+        MockAuthService.getFormIfPublic.mockReturnValueOnce(
+          errAsync(new FormNotFoundError()),
+        )
+
+        // Act
+        await PublicFormController.handleGetPublicForm(
+          MOCK_REQ,
+          MOCK_RES,
+          jest.fn(),
+        )
+
+        // Assert
+        // 1. Check args of mocked services
+        expect(MockAuthService.getFormIfPublic).toHaveBeenCalledWith(
+          MOCK_FORM_ID,
+        )
+        // 2. Check that error is correct
+        expect(
+          MockFormService.checkFormSubmissionLimitAndDeactivateForm,
+        ).not.toHaveBeenCalled()
+        expect(MOCK_RES.status).toHaveBeenCalledWith(404)
+      })
+
+      it('should return 404 if the form is private and not accessible by the public', async () => {
+        // Arrange
+        // 1. Mock the response
+        const MOCK_RES = expressHandler.mockResponse()
+
+        // 2. Mock the call to retrieve the form
+        MockAuthService.getFormIfPublic.mockReturnValueOnce(
+          errAsync(
+            new PrivateFormError('teehee this form is private', 'private form'),
+          ),
+        )
+
+        // Act
+        await PublicFormController.handleGetPublicForm(
+          MOCK_REQ,
+          MOCK_RES,
+          jest.fn(),
+        )
+
+        // Assert
+        // 1. Check args of mocked services
+        expect(MockAuthService.getFormIfPublic).toHaveBeenCalledWith(
+          MOCK_FORM_ID,
+        )
+        // 2. Check that error is correct
+        expect(
+          MockFormService.checkFormSubmissionLimitAndDeactivateForm,
+        ).not.toHaveBeenCalled()
+        expect(MOCK_RES.status).toHaveBeenCalledWith(404)
+      })
     })
   })
 })
