0315418640490ee5775d24e7d96b3a6b84fc02c3
Antariksh <antarikshmahajan@gmail.com>
2021-03-24 16:30:37 +0800

    ref: use new controller in route

--- src/app/modules/verification/verification.routes.ts
+++ src/app/modules/verification/verification.routes.ts
@@ -1,6 +1,7 @@
 import { celebrate, Joi, Segments } from 'celebrate'
 import { Router } from 'express'
 
+import * as VerificationController from './verification.controller'
 import { verificationMiddleware } from './verification.factory'
 
 export const VfnRouter = Router()
@@ -24,7 +25,7 @@ VfnRouter.get(
       transactionId: formatOfId,
     }),
   }),
-  verificationMiddleware.getTransactionMetadata,
+  VerificationController.handleGetTransactionMetadata,
 )
 
 VfnRouter.post(
