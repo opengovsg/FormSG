bee7f91e94ae9f56b6caf4bdf043ce4b06bd7ce9
seaerchin <jer005@e.ntu.edu.sg>
2021-03-31 12:48:24 +0800

    feat(public/services/exampleservice): added example service

---
+++ src/public/services/ExampleService.ts
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+import {
+  QueryPageResult,
+  QueryPageResultWithTotal,
+  SingleFormResult,
+} from '../../app/modules/examples/examples.types'
+
+const EXAMPLES_ENDPOINT = '/examples'
+
+type ExampleFormsResult = QueryPageResult | QueryPageResultWithTotal
+// NOTE: Renaming for clarity that this type refers to an example
+type ExampleSingleFormResult = SingleFormResult
+
+/**
+ * Gets example forms that matches the specified parameters for listing
+ * @param pageNo
+ * @param searchTerm
+ * @param agency
+ * @param shouldGetTotalNumResults Whether to return all the results or not
+ * @returns The list of retrieved examples if `shouldGetTotalNumResults` is false
+ * @returns The list of retrieved examples with the total results if `shouldGetTotalNumResults` is true
+ */
+export const getExampleForms = (
+  pageNo: number,
+  searchTerm?: string,
+  agency?: string,
+  shouldGetTotalNumResults?: boolean,
+): Promise<ExampleFormsResult> =>
+  axios
+    .get(EXAMPLES_ENDPOINT, {
+      params: { pageNo, searchTerm, agency, shouldGetTotalNumResults },
+      // disable IE ajax request caching (so search requests don't get cached)
+      headers: { 'If-Modified-Since': '0' },
+    })
+    .then(({ data }) => data)
+
+/**
+ * Gets a single form for examples
+ * @param formId The id of the form to search for
+ * @returns The information of the example form
+ */
+export const getSingleExampleForm = (
+  formId: string,
+): Promise<ExampleSingleFormResult> => {
+  return axios
+    .get(`${EXAMPLES_ENDPOINT}/${formId}`, {
+      // disable IE ajax request caching (so search requests don't get cached)
+      headers: { 'If-Modified-Since': '0' },
+    })
+    .then(({ data }) => data)
+}
