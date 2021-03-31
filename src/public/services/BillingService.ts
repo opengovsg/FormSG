00d592ff9d2b2a9b9acb991353849afaf577ba51
seaerchin <jer005@e.ntu.edu.sg>
2021-03-31 11:52:38 +0800

    feat(public/services/billingservice): added billing service

---
+++ src/public/services/BillingService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+import { LoginStatistic } from 'src/types/login'
+
+const BILLING_ENDPOINT = '/billing'
+type BillingResult = LoginStatistic
+
+/**
+ * Gets the billing information for the given month and year
+ * @param yr The year to get the billing information for
+ * @param mth The month to get the billing information for
+ * @param esrvcId The id of the form
+ * @returns Promise<BillingResult> The billing statistics of the given month
+ */
+export const getBillingInfo = (
+  yr: string,
+  mth: string,
+  esrvcId: string,
+): Promise<BillingResult> =>
+  axios
+    .get(BILLING_ENDPOINT, {
+      params: {
+        yr,
+        mth,
+        esrvcId,
+      },
+    })
+    .then(({ data }) => data)
