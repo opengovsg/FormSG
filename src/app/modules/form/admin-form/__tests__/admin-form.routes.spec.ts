caa3268076a626debd8ea75178cc97f500e8108d
Kar Rui Lau <karrui.lau@gmail.com>
2021-03-31 18:05:47 +0800

    test: add integration tests for DELETE /:formId/adminform

--- src/app/modules/form/admin-form/__tests__/admin-form.routes.spec.ts
+++ src/app/modules/form/admin-form/__tests__/admin-form.routes.spec.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from 'bson-ext'
 import mongoose from 'mongoose'
+import { errAsync } from 'neverthrow'
 import supertest, { Session } from 'supertest-session'
 
 import getFormModel, {
@@ -7,6 +8,7 @@ import getFormModel, {
   getEncryptedFormModel,
 } from 'src/app/models/form.server.model'
 import getUserModel from 'src/app/models/user.server.model'
+import { DatabaseError } from 'src/app/modules/core/core.errors'
 import { IUserSchema, Status } from 'src/types'
 
 import { createAuthedSession } from 'tests/integration/helpers/express-auth'
@@ -14,6 +16,7 @@ import { setupApp } from 'tests/integration/helpers/express-setup'
 import dbHandler from 'tests/unit/backend/helpers/jest-db'
 
 import { AdminFormsRouter } from '../admin-form.routes'
+import * as AdminFormService from '../admin-form.service'
 
 const UserModel = getUserModel(mongoose)
 const FormModel = getFormModel(mongoose)
@@ -154,6 +157,154 @@ describe('admin-form.routes', () => {
 
       // Act
       const response = await session.get('/adminform')
+
+      // Assert
+      expect(response.status).toEqual(500)
+      expect(response.body).toEqual({
+        message: 'Something went wrong. Please try again.',
+      })
+    })
+  })
+
+  describe('DELETE /:formId/adminform', () => {
+    it('should return 200 with success message when form is successfully archived', async () => {
+      // Arrange
+      // Log in user.
+      const session = await createAuthedSession(defaultUser.email, request)
+      const formToArchive = await EmailFormModel.create({
+        title: 'Form to archive',
+        emails: [defaultUser.email],
+        admin: defaultUser._id,
+      })
+      expect(formToArchive.status).toEqual(Status.Private)
+
+      // Act
+      const response = await session.delete(`/${formToArchive._id}/adminform`)
+
+      // Assert
+      const form = await EmailFormModel.findById(formToArchive._id)
+      expect(response.status).toEqual(200)
+      expect(response.body).toEqual({ message: 'Form has been archived' })
+      expect(form?.status).toEqual(Status.Archived)
+    })
+
+    it('should return 401 when user is not logged in', async () => {
+      // Arrange
+      const formToArchive = await EmailFormModel.create({
+        title: 'Form to archive',
+        emails: [defaultUser.email],
+        admin: defaultUser._id,
+      })
+
+      // Act
+      // Call endpoint before logging in.
+      const response = await request.delete(`/${formToArchive._id}/adminform`)
+
+      // Assert
+      expect(response.status).toEqual(401)
+      expect(response.body).toEqual({ message: 'User is unauthorized.' })
+    })
+
+    it('should return 403 when user does not have permissions to archive form', async () => {
+      // Arrange
+      // Log in user.
+      const session = await createAuthedSession(defaultUser.email, request)
+      // Create separate user
+      const collabUser = (
+        await dbHandler.insertFormCollectionReqs({
+          userId: new ObjectId(),
+          mailName: 'collab-user',
+          shortName: 'collabUser',
+        })
+      ).user
+      const randomForm = await EncryptFormModel.create({
+        title: 'form that user has no delete access to',
+        admin: collabUser._id,
+        publicKey: 'some random key',
+        // Current user only has write access.
+        permissionList: [{ email: defaultUser.email, write: true }],
+      })
+
+      // Act
+      // Call endpoint before logging in.
+      const response = await session.delete(`/${randomForm._id}/adminform`)
+
+      // Assert
+      expect(response.status).toEqual(403)
+      expect(response.body).toEqual({
+        message: `User ${defaultUser.email} not authorized to perform delete operation on Form ${randomForm._id} with title: ${randomForm.title}.`,
+      })
+    })
+
+    it('should return 404 when form to archive cannot be found', async () => {
+      // Arrange
+      // Log in user.
+      const session = await createAuthedSession(defaultUser.email, request)
+      const invalidFormId = new ObjectId()
+
+      // Act
+      const response = await session.delete(`/${invalidFormId}/adminform`)
+
+      // Assert
+      expect(response.status).toEqual(404)
+      expect(response.body).toEqual({ message: 'Form not found' })
+    })
+
+    it('should return 410 when form is already archived', async () => {
+      // Arrange
+      const session = await createAuthedSession(defaultUser.email, request)
+      // Create archived form.
+      const archivedForm = await EmailFormModel.create({
+        title: 'Form already archived',
+        emails: [defaultUser.email],
+        admin: defaultUser._id,
+        status: Status.Archived,
+      })
+
+      // Act
+      const response = await session.delete(`/${archivedForm._id}/adminform`)
+
+      // Assert
+      expect(response.status).toEqual(410)
+      expect(response.body).toEqual({ message: 'Form has been archived' })
+    })
+
+    it('should return 422 when user in session cannot be found in the database', async () => {
+      // Arrange
+      // Log in user.
+      const session = await createAuthedSession(defaultUser.email, request)
+      const formToArchive = await EmailFormModel.create({
+        title: 'Form to archive',
+        emails: [defaultUser.email],
+        admin: defaultUser._id,
+      })
+      // Delete user after login.
+      await dbHandler.clearCollection(UserModel.collection.name)
+
+      // Act
+      const response = await session.delete(`/${formToArchive._id}/adminform`)
+
+      // Assert
+      expect(response.status).toEqual(422)
+      expect(response.body).toEqual({ message: 'User not found' })
+    })
+
+    it('should return 500 when database error occurs whilst archiving form', async () => {
+      // Arrange
+      // Log in user.
+      const session = await createAuthedSession(defaultUser.email, request)
+      const formToArchive = await EmailFormModel.create({
+        title: 'Form to archive',
+        emails: [defaultUser.email],
+        admin: defaultUser._id,
+      })
+      // Mock database error during archival.
+      jest
+        .spyOn(AdminFormService, 'archiveForm')
+        .mockReturnValueOnce(errAsync(new DatabaseError()))
+
+      // Act
+      const response = await session.delete(`/${formToArchive._id}/adminform`)
 
       // Assert
       expect(response.status).toEqual(500)
