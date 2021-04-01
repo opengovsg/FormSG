6690e3217ab5517dd14df5b3397533e15e1c5524
seaerchin <jer005@e.ntu.edu.sg>
2021-04-01 11:30:33 +0800

    refactor(examples): adds dto to params and updated fe file naming to fit be

--- src/public/services/ExamplesService.ts
+++ src/public/services/ExamplesService.ts
@@ -1,35 +1,25 @@
 import axios from 'axios'
 
 import {
-  QueryPageResult,
-  QueryPageResultWithTotal,
-  SingleFormResult,
-} from '../../app/modules/examples/examples.types'
+  ExampleFormsQueryDto,
+  ExampleFormsResult,
+  ExampleSingleFormResult,
+} from '../../types/api'
 
 export const EXAMPLES_ENDPOINT = '/examples'
 
-type ExampleFormsResult = QueryPageResult | QueryPageResultWithTotal
-// NOTE: Renaming for clarity that this type refers to an example
-type ExampleSingleFormResult = SingleFormResult
-
 /**
  * Gets example forms that matches the specified parameters for listing
- * @param pageNo
- * @param searchTerm
- * @param agency
- * @param shouldGetTotalNumResults Whether to return all the results or not
+ * @param exampleFormsSearchParams The search terms to query the backend for
  * @returns The list of retrieved examples if `shouldGetTotalNumResults` is false
  * @returns The list of retrieved examples with the total results if `shouldGetTotalNumResults` is true
  */
 export const getExampleForms = (
-  pageNo: number,
-  searchTerm?: string,
-  agency?: string,
-  shouldGetTotalNumResults?: boolean,
+  exampleFormsSearchParams: ExampleFormsQueryDto,
 ): Promise<ExampleFormsResult> => {
   return axios
     .get<ExampleFormsResult>(EXAMPLES_ENDPOINT, {
-      params: { pageNo, searchTerm, agency, shouldGetTotalNumResults },
+      params: exampleFormsSearchParams,
       // disable IE ajax request caching (so search requests don't get cached)
       headers: { 'If-Modified-Since': '0' },
     })
