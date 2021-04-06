b44c63f5e0ab2f03ac1ca512b4ba191d0a63c04d
seaerchin <jer005@e.ntu.edu.sg>
2021-04-06 16:55:22 +0800

    style(types/billing): imported LoginStatistic rather than redefining

--- src/types/api/billing.ts
+++ src/types/api/billing.ts
@@ -1,4 +1,4 @@
-import { AuthType } from '../../types'
+import { LoginStatistic } from '../../types'
 
 // yr: The year to get the billing information for
 // mth: The month to get the billing information for
@@ -7,16 +7,6 @@ export type BillingQueryDto = {
   esrvcId: string
   yr: string
   mth: string
-}
-
-// NOTE: LoginStatistic is defined here and in src/types/login.
-// This is to allow the communication layer (the dto) to be agnostic of intermediate representations.
-type LoginStatistic = {
-  adminEmail: string
-  formName: string
-  total: number
-  formId: string
-  authType: AuthType
 }
 
 export type BillingInfoDto = { loginStats: LoginStatistic[] }
