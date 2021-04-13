965cb2dced16e83b253f335e7ad90e898ed8d9ec
Kar Rui Lau <karrui.lau@gmail.com>
2021-04-13 13:48:47 +0800

    feat: add FormField type that is a union for every possible field

--- src/types/api/form.ts
+++ src/types/api/form.ts
@@ -1,3 +1,6 @@
+import { FormField } from '../field'
 import { FormSettings } from '../form'
 
 export type SettingsUpdateDto = Partial<FormSettings>
+
+export type FieldUpdateDto = FormField
--- src/types/field/index.ts
+++ src/types/field/index.ts
@@ -1,3 +1,23 @@
+import { IAttachmentField } from './attachmentField'
+import { ICheckboxField } from './checkboxField'
+import { IDateField } from './dateField'
+import { IDecimalField } from './decimalField'
+import { IDropdownField } from './dropdownField'
+import { IEmailField } from './emailField'
+import { IHomenoField } from './homeNoField'
+import { IImageField } from './imageField'
+import { ILongTextField } from './longTextField'
+import { IMobileField } from './mobileField'
+import { INricField } from './nricField'
+import { INumberField } from './numberField'
+import { IRadioField } from './radioField'
+import { IRatingField } from './ratingField'
+import { ISectionField } from './sectionField'
+import { IShortTextField } from './shortTextField'
+import { IStatementField } from './statementField'
+import { ITableField } from './tableField'
+import { IYesNoField } from './yesNoField'
+
 export * from './fieldTypes'
 export * from './baseField'
 export * from './attachmentField'
@@ -19,3 +39,24 @@ export * from './shortTextField'
 export * from './statementField'
 export * from './tableField'
 export * from './yesNoField'
+
+export type FormField =
+  | IAttachmentField
+  | ICheckboxField
+  | IDateField
+  | IDecimalField
+  | IDropdownField
+  | IEmailField
+  | IHomenoField
+  | IImageField
+  | ILongTextField
+  | IMobileField
+  | INricField
+  | INumberField
+  | IRadioField
+  | IRatingField
+  | ISectionField
+  | IShortTextField
+  | IStatementField
+  | ITableField
+  | IYesNoField
