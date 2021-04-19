10f02414d1ee8b0d7a692342447ee519e6c32231
Kar Rui Lau <karrui.lau@gmail.com>
2021-04-19 10:46:25 +0800

    test: fix possibly flakey test by assigning constant to date
    flakey on travis but not on local, this is just an attempt to hopefully fix the flake

--- src/app/routes/api/v3/admin/forms/__tests__/admin-forms.submissions.routes.spec.ts
+++ src/app/routes/api/v3/admin/forms/__tests__/admin-forms.submissions.routes.spec.ts
@@ -215,7 +215,7 @@ describe('admin-form.submissions.routes', () => {
       expect(response.body).toEqual(1)
     })
 
-    it('should return 200 with counts of submissions made with same start and end dates.', async () => {
+    it('should return 200 with counts of submissions made with same start and end dates', async () => {
       // Arrange
       const expectedSubmissionCount = 3
       const newForm = (await EmailFormModel.create({
@@ -235,17 +235,17 @@ describe('admin-form.submissions.routes', () => {
         ),
       )
       // Update first submission to be 5 days ago.
-      const expectedDate = subDays(new Date(), 5)
+      const expectedDate = '2021-04-04'
       const firstSubmission = results[0]._unsafeUnwrap()
-      firstSubmission.created = expectedDate
+      firstSubmission.created = new Date(expectedDate)
       await firstSubmission.save()
 
       // Act
       const response = await request
         .get(`${ADMIN_FORMS_PREFIX}/${newForm._id}/submissions/count`)
         .query({
-          startDate: format(expectedDate, 'yyyy-MM-dd'),
-          endDate: format(expectedDate, 'yyyy-MM-dd'),
+          startDate: expectedDate,
+          endDate: expectedDate,
         })
 
       // Assert
