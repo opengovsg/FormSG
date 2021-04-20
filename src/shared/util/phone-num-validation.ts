0041fca5767150c3ba430d119e4f961a14864c02
tshuli <63710093+tshuli@users.noreply.github.com>
2021-04-20 10:42:16 +0800

    fix: loosen validation for sg home numbers (#1663)
    * fix: loose home number validation for sg numbers
    
    * chore: add test case
    
    * docs: add docs on singapore national numbering plan
    
    * refactor: reuse startsWithSgPrefix

--- src/shared/util/phone-num-validation.ts
+++ src/shared/util/phone-num-validation.ts
@@ -25,7 +25,7 @@ export const isMobilePhoneNumber = (mobileNumber: string): boolean => {
 
   if (!parsedNumber) return false
 
-  if (parsedNumber.countryCallingCode === '65') {
+  if (startsWithSgPrefix(mobileNumber)) {
     return (
       isPhoneNumber(mobileNumber) &&
       // Regex checks if the national number starts with 8 or 9, and is of
@@ -54,11 +54,25 @@ export const isMobilePhoneNumber = (mobileNumber: string): boolean => {
  */
 export const isHomePhoneNumber = (phoneNum: string): boolean => {
   const parsedNumber = parsePhoneNumberFromString(phoneNum)
+
   if (!parsedNumber) return false
 
+  // For SG numbers only check if it starts with 3 or 6 and has 8 digits
+  // Leading number 3 is for IP Telephony (IPT) service and User-Centric Data-Only (UCDO) service
+  // Leading number 6 is for  PSTN service and IP Telephony (IPT) service.
+  // In both cases the length is 8
+  // See IMDA's national numbering plan for specifications: https://www.imda.gov.sg/-/media/imda/files/regulation-licensing-and-consultations/frameworks-and-policies/numbering/national-numbering-plan-and-allocation-process/national-numbering-plan-30-nov-2017.pdf?la=en
+  if (startsWithSgPrefix(phoneNum)) {
+    return (
+      isPhoneNumber(phoneNum) &&
+      // Regex checks if the national number starts with 3 or 6, and is of
+      // length 8.
+      !!parsedNumber.nationalNumber.match(/^[36][0-9]{7}$/g)
+    )
+  }
+  // For intl numbers check number type as well
   const parsedType = parsedNumber.getType()
   if (!parsedType) return false
-
   return (
     isPhoneNumber(phoneNum) &&
     // Have to include both FIXED_LINE, FIXED_LINE_OR_MOBILE as some countries lump
@@ -67,6 +81,6 @@ export const isHomePhoneNumber = (phoneNum: string): boolean => {
   )
 }
 
-export const startsWithSgPrefix = (mobileNumber: string): boolean => {
-  return mobileNumber.startsWith('+65')
+export const startsWithSgPrefix = (phoneNumber: string): boolean => {
+  return phoneNumber.startsWith('+65')
 }
