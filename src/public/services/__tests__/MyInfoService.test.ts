ed883e30799b01e54ef93805675698c8c446a271
seaerchin <jer005@e.ntu.edu.sg>
2021-04-26 11:19:50 +0800

    chore(myinfoservice): delete unused code

--- src/public/services/__tests__/MyInfoService.test.ts
+++ src/public/services/__tests__/MyInfoService.test.ts
@@ -7,42 +7,10 @@ import * as MyInfoService from '../MyInfoService'
 jest.mock('axios')
 const MockAxios = mocked(axios, true)
 
-const MOCK_REDIRECT_URL = 'redirectURL'
 const MOCK_FORM_ID = new ObjectId().toHexString()
 
 describe('MyInfoService', () => {
   afterEach(() => jest.resetAllMocks())
-  describe('createRedirectURL', () => {
-    it('should return the redirect URL when retrieval succeeds', async () => {
-      const mockData = { redirectURL: MOCK_REDIRECT_URL }
-      MockAxios.get.mockResolvedValueOnce({ data: mockData })
-
-      const result = await MyInfoService.createRedirectURL(MOCK_FORM_ID)
-
-      expect(MockAxios.get).toHaveBeenCalledWith(
-        MyInfoService.REDIRECT_URL_ENDPOINT,
-        {
-          params: { formId: MOCK_FORM_ID },
-        },
-      )
-      expect(result).toEqual(mockData)
-    })
-
-    it('should reject with error when API call fails', async () => {
-      const error = new Error('rejected')
-      MockAxios.get.mockRejectedValueOnce(error)
-
-      const rejectFunction = () => MyInfoService.createRedirectURL(MOCK_FORM_ID)
-
-      await expect(rejectFunction).rejects.toThrowError(error)
-      expect(MockAxios.get).toHaveBeenCalledWith(
-        MyInfoService.REDIRECT_URL_ENDPOINT,
-        {
-          params: { formId: MOCK_FORM_ID },
-        },
-      )
-    })
-  })
 
   describe('validateESrvcId', () => {
     it('should return isValid as true when the e-service ID is valid', async () => {
