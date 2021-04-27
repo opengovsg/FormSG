2edf96f5dd3d984a85b3570c71a97e34e01edd1c
seaerchin <jer005@e.ntu.edu.sg>
2021-04-27 18:03:05 +0800

    feat(public/service): adds method on AdminFormService to update collaborators

--- src/public/services/AdminFormService.ts
+++ src/public/services/AdminFormService.ts
@@ -5,6 +5,7 @@ import {
   FieldCreateDto,
   FieldUpdateDto,
   FormFieldDto,
+  PermissionsUpdateDto,
   SettingsUpdateDto,
 } from '../../types/api'
 
@@ -43,6 +44,18 @@ export const createSingleFormField = async (
     .post<FormFieldDto>(
       `${ADMIN_FORM_ENDPOINT}/${formId}/fields`,
       createFieldBody,
+    )
+    .then(({ data }) => data)
+}
+
+export const updateCollaborators = async (
+  formId: string,
+  collaboratorsToUpdate: PermissionsUpdateDto,
+): Promise<PermissionsUpdateDto> => {
+  return axios
+    .put<PermissionsUpdateDto>(
+      `${ADMIN_FORM_ENDPOINT}/${formId}/collaborators`,
+      collaboratorsToUpdate,
     )
     .then(({ data }) => data)
 }
