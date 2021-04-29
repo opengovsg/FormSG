6c347090f189039f2844d3fb39213886648532f3
Kar Rui Lau <karrui.lau@gmail.com>
2021-04-29 16:24:56 +0800

    feat(client/validate-url): add non-https mode validation

--- src/shared/util/url-validation.ts
+++ src/shared/util/url-validation.ts
@@ -13,3 +13,17 @@ export const isValidHttpsUrl = (url: string): boolean => {
     require_protocol: true,
   })
 }
+
+/**
+ * Checks that string is a valid HTTP or HTTPS URL.
+ * @param url the url to check
+ * @returns true if valid, false otherwise
+ */
+export const isValidUrl = (url: string): boolean => {
+  if (typeof url !== 'string') {
+    return false
+  }
+  return validator.isURL(url, {
+    protocols: ['https', 'http'],
+  })
+}
