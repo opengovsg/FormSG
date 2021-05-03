f50f43ec1ccc59dff749f2fb840a5befea56c45e
seaerchin <jer005@e.ntu.edu.sg>
2021-05-03 12:56:33 +0800

    style(collaborator-modal/client/controller): updated error message

--- src/public/modules/forms/admin/controllers/collaborator-modal.client.controller.js
+++ src/public/modules/forms/admin/controllers/collaborator-modal.client.controller.js
@@ -241,7 +241,9 @@ function CollaboratorModalController(
       if (err) {
         // Make the alert message correspond to the error code
         if (err.response.status === StatusCodes.BAD_REQUEST) {
-          Toastr.error('Outdated admin page, please refresh.')
+          Toastr.error(
+            'Please ensure that the email entered is a valid government email. If the error still persists, refresh and try again later.',
+          )
         } else if (err.response.status === StatusCodes.UNPROCESSABLE_ENTITY) {
           Toastr.error(`${email} is not part of a whitelisted agency.`)
         } else {
