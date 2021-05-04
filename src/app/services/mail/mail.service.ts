75e0d2bfea930a355d8c42c60055f9450115a4c5
Antariksh <antarikshmahajan@gmail.com>
2021-05-04 10:11:00 +0800

    fix: add quotes around sender name

--- src/app/services/mail/mail.service.ts
+++ src/app/services/mail/mail.service.ts
@@ -253,7 +253,8 @@ export class MailService {
     return generateAutoreplyHtml(templateData).andThen((mailHtml) => {
       const mail: MailOptions = {
         to: autoReplyMailData.email,
-        from: `${emailSender} <${this.#senderMail}>`,
+        // Quotes necessary to allow for special characters in emailSender, e.g. comma
+        from: `"${emailSender}" <${this.#senderMail}>`,
         subject: emailSubject,
         // Only send attachments if the admin has the box checked for email
         // fields.
