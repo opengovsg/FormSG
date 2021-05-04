1f888c793ac25df4841f6019d94f970418c64577
Kar Rui Lau <karrui.lau@gmail.com>
2021-05-04 22:45:51 +0800

    chore: update root ts-config to ignore src/frontend

--- src/frontend/.storybook/main.js
+++ src/frontend/.storybook/main.js
@@ -3,12 +3,18 @@ const toPath = (_path) => path.join(process.cwd(), _path)
 
 module.exports = {
   // Welcome story set first so it will show up first.
-  stories: ['./Welcome/Welcome.stories.tsx','../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    './Welcome/Welcome.stories.tsx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
+  // webpackFinal setup retrieved from ChakraUI's own Storybook setup
+  // https://github.com/chakra-ui/chakra-ui/blob/main/.storybook/main.js 
   webpackFinal: async (storybookConfig) => {
     return {
       ...storybookConfig,
@@ -17,6 +23,7 @@ module.exports = {
         // Required so storybook knows where the npm package is to render ChakraUI components
         // as this is not directly installed in package.json.
         alias: {
+          ...storybookConfig.resolve.alias,
           '@emotion/core': toPath('node_modules/@emotion/react'),
           'emotion-theming': toPath('node_modules/@emotion/react'),
         },
