67d0f47fd61f7b12fcdb8f70c73969f4a74b0e94
Kar Rui Lau <karrui.lau@gmail.com>
2021-05-06 22:53:26 +0800

    feat(storybook): add withPerformance addon

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -2,13 +2,15 @@ import 'focus-visible/dist/focus-visible'
 import '../src/index.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
+import { DecoratorFn } from '@storybook/react'
+import { withPerformance } from 'storybook-addon-performance'
 
 import { theme } from '../src/theme'
 
-const withChakra = (storyFn: Function) => (
+const withChakra: DecoratorFn = (storyFn) => (
   <ChakraProvider resetCSS theme={theme}>
     {storyFn()}
   </ChakraProvider>
 )
 
-export const decorators = [withChakra]
+export const decorators = [withChakra, withPerformance]
