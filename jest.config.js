d53041695da060e41d36e7f2e6c22cbbe5004b45
Kar Rui Lau <karrui.lau@gmail.com>
2021-05-06 17:39:25 +0800

    feat: rip out frontend tests and other mentions of /public
    skipping express static files, since i'm not sure how the PDF/HTML/Email generation will be affected. Keeping it in first.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
+/* eslint-env node */
 module.exports = {
   preset: 'ts-jest',
   testMatch: ['**/?(*.)+(spec|test).[t]s?(x)'],
   modulePaths: ['<rootDir>'],
   testEnvironment: 'node',
   globalSetup: '<rootDir>/tests/jest-global-setup.js',
-  testPathIgnorePatterns: [
-    '<rootDir>/dist/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/src/public',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
   collectCoverageFrom: ['./src/**/*.{ts,js}', '!**/__tests__/**'],
   coveragePathIgnorePatterns: ['./node_modules/', './tests'],
   coverageReporters: ['lcov', 'text'],
