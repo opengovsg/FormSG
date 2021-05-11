4e9746fb69ac4e9b46d862a9489afcb583a3a896
Yong Jie <viorama@gmail.com>
2021-05-11 14:33:21 +0800

    feat(submissions): add base class with static init method

--- src/app/modules/submission/submission.service.ts
+++ src/app/modules/submission/submission.service.ts
@@ -166,6 +166,101 @@ export const getProcessedResponses = (
   return ok(processedResponses)
 }
 
+export class ParsedResponsesObject {
+  public ndiResponses: ProcessedFieldResponse[] = []
+  constructor(public responses: FieldResponse[]) {}
+
+  addNdiResponses(ndiResponses: ProcessedFieldResponse[]) {
+    this.ndiResponses = ndiResponses
+    return this
+  }
+
+  static parseResponses(
+    form: IFormDocument,
+    responses: FieldResponse[],
+  ): Result<
+    ParsedResponsesObject,
+    ProcessingError | ConflictError | ValidateFieldError
+  > {
+    const filteredResponsesResult = getFilteredResponses(form, responses)
+    if (filteredResponsesResult.isErr()) {
+      return err(filteredResponsesResult.error)
+    }
+
+    const filteredResponses = filteredResponsesResult.value
+
+    // Set of all visible fields
+    const visibleFieldIds = getVisibleFieldIds(filteredResponses, form)
+
+    // Guard against invalid form submissions that should have been prevented by
+    // logic.
+    if (
+      getLogicUnitPreventingSubmit(filteredResponses, form, visibleFieldIds)
+    ) {
+      return err(new ProcessingError('Submission prevented by form logic'))
+    }
+
+    // Create a map keyed by field._id for easier access
+
+    if (!form.form_fields) {
+      return err(new ProcessingError('Form fields are undefined'))
+    }
+
+    const fieldMap = form.form_fields.reduce<{
+      [fieldId: string]: IFieldSchema
+    }>((acc, field) => {
+      acc[field._id] = field
+      return acc
+    }, {})
+
+    // Validate each field in the form and inject metadata into the responses.
+    const processedResponses = []
+    for (const response of filteredResponses) {
+      const responseId = response._id
+      const formField = fieldMap[responseId]
+      if (!formField) {
+        return err(
+          new ProcessingError('Response ID does not match form field IDs'),
+        )
+      }
+
+      const processingResponse: ProcessedFieldResponse = {
+        ...response,
+        isVisible:
+          // Set isVisible as true for Encrypt mode if there is a response for mobile and email field
+          // Because we cannot tell if the field is unhidden by logic
+          // This prevents downstream validateField from incorrectly preventing
+          // encrypt mode submissions with responses on unhidden fields
+          // TODO(#780): Remove this once submission service is separated into
+          // Email and Encrypted services
+          form.responseMode === ResponseMode.Encrypt
+            ? 'answer' in response &&
+              typeof response.answer === 'string' &&
+              response.answer.trim() !== ''
+            : visibleFieldIds.has(responseId),
+        question: formField.getQuestion(),
+      }
+
+      if (formField.isVerifiable) {
+        processingResponse.isUserVerified = formField.isVerifiable
+      }
+
+      // Error will be returned if the processed response is not valid.
+      const validateFieldResult = validateField(
+        form._id,
+        formField,
+        processingResponse,
+      )
+      if (validateFieldResult.isErr()) {
+        return err(validateFieldResult.error)
+      }
+      processedResponses.push(processingResponse)
+    }
+
+    return ok(new ParsedResponsesObject(processedResponses))
+  }
+}
+
 /**
  * Returns number of form submissions of given form id in the given date range.
  *
