3928a2a803b2f7c911bd859c09e6e526f5155af8
Kar Rui Lau <karrui.lau@gmail.com>
2021-05-11 12:15:32 +0800

    feat: add ControllerHandler type to cast unknowns to RequestHandler

--- src/app/modules/core/core.types.ts
+++ src/app/modules/core/core.types.ts
@@ -1,6 +1,16 @@
+import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
 export type ErrorResponseData = {
   statusCode: StatusCodes
   errorMessage: string
 }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ControllerHandler<
+  P = unknown,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery = unknown,
+  Locals = Record<string, unknown>
+> extends RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals> {}
