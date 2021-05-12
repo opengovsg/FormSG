fbbdabe61d5b4234ea360530d2804b8f111a0acc
seaerchin <jer005@e.ntu.edu.sg>
2021-05-12 13:34:55 +0800

    feat(public-forms/verification/routes): add new routes for retrieving transaction metadata

--- src/app/routes/api/v3/forms/public-forms.verification.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.verification.routes.ts
@@ -4,6 +4,24 @@ import * as VerificationController from '../../../../modules/verification/verifi
 
 export const PublicFormsVerificationRouter = Router()
 
+/**
+ * Route for creating a verification transaction instance
+ * @group fieldverifications
+ * @route POST /forms/:formId/fieldverifications
+ */
 PublicFormsVerificationRouter.route(
   '/:formId([a-fA-F0-9]{24})/fieldverifications',
 ).post(VerificationController.handleCreateTransactionWithFieldId)
+
+/**
+ * Route for retrieving a transaction by its id
+ * @group fieldverifications
+import { ObjectId } from 'bson-ext'
+ * @route GET /forms/:formId/fieldverifications/:id
+ * @returns 200 with transactionId/formId and expiry time when transaction exists
+ * @returns 404 when the transaction could not be found
+ * @returns 500 when database error occurs
+ */
+PublicFormsVerificationRouter.route(
+  '/:formId([a-fA-F0-9]{24})/fieldverifications/:transactionId([a-fA-F0-9]{24})',
+).get(VerificationController.handleGetTransactionMetadata)
