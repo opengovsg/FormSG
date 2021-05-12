31c8f78def2e5ec8cd6eb27a7c53149a39e6246d
seaerchin <jer005@e.ntu.edu.sg>
2021-05-12 18:33:46 +0800

    refactor(public-forms/verification/routes): added new route (and test) for resetting verification

--- src/app/routes/api/v3/forms/public-forms.verification.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.verification.routes.ts
@@ -11,10 +11,11 @@ PublicFormsVerificationRouter.route(
 /**
  * Route for resetting the verification of a given field
  * @returns 400 when the transaction has expired
+ * @returns 404 when the form could not be found
  * @returns 404 when the transaction could not be found
  * @returns 404 when the field could not be found
  * @returns 500 when a database error occurs
  */
 PublicFormsVerificationRouter.route(
-  '/forms/:formId/fieldverification/:transactionId/fields/:fieldId/reset',
+  '/:formId([a-fA-F0-9]{24})/fieldverification/:transactionId([a-fA-F0-9]{24})/fields/:fieldId([a-fA-F0-9]{24})/reset',
 ).post(VerificationController.handleResetFieldVerification)
