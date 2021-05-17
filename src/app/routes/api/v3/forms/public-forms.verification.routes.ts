fdbf3f76a758d12700b1cc41918cedd2d0c62fd5
seaerchin <jer005@e.ntu.edu.sg>
2021-05-17 22:37:40 +0800

    docs(public-forms/verification/controller): added extra docs

--- src/app/routes/api/v3/forms/public-forms.verification.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.verification.routes.ts
@@ -21,6 +21,23 @@ PublicFormsVerificationRouter.route(
   '/:formId([a-fA-F0-9]{24})/fieldverifications/:transactionId([a-fA-F0-9]{24})/fields/:fieldId([a-fA-F0-9]{24})/reset',
 ).post(VerificationController.handleResetFieldVerification)
 
+/**
+ * Route for generating a new otp for a given field
+ * @returns 200 when otp generated successfully
+ * @returns 400 when the parameters could not be parsed
+ * @returns 400 when the transaction has expired
+ * @returns 400 when the otp data could not be retrieved from the database
+ * @returns 400 when the otp could not be sent via sms
+ * @returns 400 when the otp could not be sent via email
+ * @returns 400 when the provided phone number is not valid
+ * @returns 400 when the field type is not supported for validation
+ * @returns 404 when the requested form was not found
+ * @returns 422 when the user requested for a new otp without waiting
+ * @returns 500 when the otp could not be hashed
+ * @returns 500 when the transaction could not be found
+ * @returns 500 when the field could not be found in the transaction
+ * @returns 500 when there is a database error
+ */
 PublicFormsVerificationRouter.route(
   '/:formId([a-fA-F0-9]{24})/fieldverifications/:transactionId([a-fA-F0-9]{24})/fields/:fieldId([a-fA-F0-9]{24})/otp/generate',
 ).post(VerificationController.handleGenerateOtp)
