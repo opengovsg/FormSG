d18127a90802e9195e6728fbfbbb5bc383bf3983
Kar Rui Lau <karrui.lau@gmail.com>
2021-05-17 13:44:49 +0800

    refactor: use alias instead of aliasDangerous in cra rewire

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,14 +1,17 @@
-// Config overrides for CRA to allow importing of aliases and the /shared folder.
-const {
-  aliasDangerous,
-  configPaths,
-  aliasJest,
-} = require('react-app-rewire-alias/lib/aliasDangerous')
+/**
+ * Config overrides for CRA to allow importing of aliases.
+ * This will not work if aliasing folders that is outside of React app's `/src`
+ * folder. If aliasing outside of `/src`, then another function aliasDangerous
+ * will be needed.
+ * See https://www.npmjs.com/package/react-app-rewire-alias#outside-of-root.
+ */
+
+const { alias, configPaths, aliasJest } = require('react-app-rewire-alias')
 
 const aliasMap = configPaths('tsconfig.paths.json')
 
 module.exports = function override(config) {
-  aliasDangerous(aliasMap)(config)
+  alias(aliasMap)(config)
 
   return config
 }
