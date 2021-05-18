dc828851afadc41339ea2312f3cb9da29eec3e93
seaerchin <jer005@e.ntu.edu.sg>
2021-05-18 13:03:41 +0800

    docs(verification): updated docs for otp generation

--- src/app/routes/api/v3/forms/public-forms.verification.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.verification.routes.ts
@@ -23,19 +23,18 @@ PublicFormsVerificationRouter.route(
 
 /**
  * Route for generating a new otp for a given field
- * @returns 200 when otp generated successfully
+ * @returns 201 when otp generated successfully
  * @returns 400 when the parameters could not be parsed
  * @returns 400 when the transaction has expired
- * @returns 400 when the otp data could not be retrieved from the database
  * @returns 400 when the otp could not be sent via sms
  * @returns 400 when the otp could not be sent via email
  * @returns 400 when the provided phone number is not valid
  * @returns 400 when the field type is not supported for validation
  * @returns 404 when the requested form was not found
+ * @returns 404 when the transaction was not found
+ * @returns 404 when the field was not found
  * @returns 422 when the user requested for a new otp without waiting
  * @returns 500 when the otp could not be hashed
- * @returns 500 when the transaction could not be found
- * @returns 500 when the field could not be found in the transaction
  * @returns 500 when there is a database error
  */
 PublicFormsVerificationRouter.route(
