3ec53ab834d3758a03882e7e938740b5b8b08d5b
seaerchin <jer005@e.ntu.edu.sg>
2021-05-19 13:26:46 +0800

    docs(public-forms/verification/routes): adds docs for otp verification

--- src/app/routes/api/v3/forms/public-forms.verification.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.verification.routes.ts
@@ -21,6 +21,20 @@ PublicFormsVerificationRouter.route(
   '/:formId([a-fA-F0-9]{24})/fieldverifications/:transactionId([a-fA-F0-9]{24})/fields/:fieldId([a-fA-F0-9]{24})/reset',
 ).post(VerificationController.handleResetFieldVerification)
 
+/**
+ * Route for verifying the otp for a given field
+ * @returns 200 when the otp is correct and the parameters are valid
+ * @returns 400 when TransactionExpiredError occurs
+ * @returns 400 when MissingHashDataError occurs
+ * @returns 404 when FormNotFoundError occurs
+ * @returns 404 when TransactionNotFoundError occurs
+ * @returns 404 when FieldNotFoundInTransactionError occurs
+ * @returns 422 when OtpExpiredError occurs
+ * @returns 422 when OtpRetryExceededError occurs
+ * @returns 422 when WrongOtpError occurs
+ * @returns 500 when HashingError occurs
+ * @returns 500 when DatabaseError occurs
+ */
 PublicFormsVerificationRouter.route(
   '/:formId([a-fA-F0-9]{24})/fieldverifications/:transactionId([a-fA-F0-9]{24})/fields/:fieldId([a-fA-F0-9]{24})/otp/verify',
 ).post(VerificationController.handleOtpVerification)
