81d05719e0465f6fb931605d65cd71d5fb06b135
Antariksh <antarikshmahajan@gmail.com>
2021-05-20 10:43:47 +0800

    fix: allow URL and retry options to be updated together

--- src/app/routes/api/v3/admin/forms/admin-forms.settings.routes.ts
+++ src/app/routes/api/v3/admin/forms/admin-forms.settings.routes.ts
@@ -26,7 +26,7 @@ const updateSettingsValidator = celebrate({
     webhook: Joi.object({
       url: Joi.string().uri().allow(''),
       isRetryEnabled: Joi.boolean(),
-    }).oxor('url', 'isRetryEnabled'),
+    }),
   }).min(1),
 })
 
