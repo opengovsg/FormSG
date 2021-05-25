f56d191a83cb77068d389d949bc4ceb1cf8c0932
Kar Rui Lau <karrui.lau@gmail.com>
2021-05-25 17:43:44 +0800

    test(PublicFormAuthService): add unit tests for getStoredJwt

--- src/public/services/__tests__/PublicFormAuthService.test.ts
+++ src/public/services/__tests__/PublicFormAuthService.test.ts
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import axios from 'axios'
 import { ObjectId } from 'bson'
+import Cookies from 'js-cookie'
 import { mocked } from 'ts-jest/utils'
+
+import { AuthType } from 'src/types'
 
 import * as PublicFormAuthService from '../PublicFormAuthService'
 
+const { PublicFormAuthCookieName } = PublicFormAuthService
+
 jest.mock('axios')
+jest.mock('js-cookie')
 const MockAxios = mocked(axios, true)
+const MockCookies = mocked(Cookies, true)
 
 const MOCK_REDIRECT_URL = 'redirectURL'
 const MOCK_FORM_ID = new ObjectId().toHexString()
@@ -99,6 +107,50 @@ describe('PublicFormAuthService', () => {
       expect(MockAxios.get).toHaveBeenCalledWith(
         `${PublicFormAuthService.PUBLIC_FORMS_ENDPOINT}/${MOCK_FORM_ID}/auth/validate`,
       )
+    })
+  })
+
+  describe('getStoredJwt', () => {
+    it('should return stored jwt of AuthType.SP when available', () => {
+      // Arrange
+      const expectedJwt = 'some mock jwt'
+      // @ts-ignore
+      MockCookies.get.mockReturnValueOnce(expectedJwt)
+
+      // Act
+      const actual = PublicFormAuthService.getStoredJwt(AuthType.SP)
+
+      // Assert
+      expect(actual).toEqual(expectedJwt)
+      expect(MockCookies.get).toHaveBeenCalledWith(PublicFormAuthCookieName.SP)
+    })
+
+    it('should return stored jwt of AuthType.CP when available', () => {
+      // Arrange
+      const expectedJwt = 'some mock jwt'
+      // @ts-ignore
+      MockCookies.get.mockReturnValueOnce(expectedJwt)
+
+      // Act
+      const actual = PublicFormAuthService.getStoredJwt(AuthType.CP)
+
+      // Assert
+      expect(actual).toEqual(expectedJwt)
+      expect(MockCookies.get).toHaveBeenCalledWith(PublicFormAuthCookieName.CP)
+    })
+
+    it('should return null when jwt of valid authType cannot be found', () => {
+      // Arrange
+      // Mock no cookie retrieved.
+      // @ts-ignore
+      MockCookies.get.mockReturnValueOnce(undefined)
+
+      // Act
+      const actual = PublicFormAuthService.getStoredJwt(AuthType.SP)
+
+      // Assert
+      expect(actual).toEqual(null)
+      expect(MockCookies.get).toHaveBeenCalledWith(PublicFormAuthCookieName.SP)
     })
   })
 })
