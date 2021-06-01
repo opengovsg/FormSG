ef3fb13dbd2fa680d43143a98bfd65f63fe56265
Antariksh Mahajan <antarikshmahajan@gmail.com>
2021-06-01 15:47:49 +0800

    test(webhook-retries): add unit tests for webhook service (6) (#1943)

--- src/app/modules/webhook/__tests__/webhook.service.spec.ts
+++ src/app/modules/webhook/__tests__/webhook.service.spec.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
-import { ObjectID } from 'bson'
+import { ObjectId } from 'bson'
 import mongoose from 'mongoose'
+import { ok, okAsync } from 'neverthrow'
 import { mocked } from 'ts-jest/utils'
 
 import formsgSdk from 'src/app/config/formsg-sdk'
-import getFormModel from 'src/app/models/form.server.model'
 import { getEncryptSubmissionModel } from 'src/app/models/submission.server.model'
 import { WebhookValidationError } from 'src/app/modules/webhook/webhook.errors'
 import * as WebhookValidationModule from 'src/app/modules/webhook/webhook.validation'
@@ -12,14 +12,15 @@ import { transformMongoError } from 'src/app/utils/handle-mongo-error'
 import {
   IEncryptedSubmissionSchema,
   IWebhookResponse,
-  ResponseMode,
   WebhookView,
 } from 'src/types'
 
 import dbHandler from 'tests/unit/backend/helpers/jest-db'
 
 import { SubmissionNotFoundError } from '../../submission/submission.errors'
-import { saveWebhookRecord, sendWebhook } from '../webhook.service'
+import { WebhookQueueMessage } from '../webhook.message'
+import { WebhookProducer } from '../webhook.producer'
+import * as WebhookService from '../webhook.service'
 
 // define suite-wide mocks
 jest.mock('axios')
@@ -28,9 +29,15 @@ const MockAxios = mocked(axios, true)
 jest.mock('src/app/modules/webhook/webhook.validation')
 const MockWebhookValidationModule = mocked(WebhookValidationModule, true)
 
-// define test constants
-const FormModel = getFormModel(mongoose)
+jest.mock('src/app/config/formsg-sdk')
+const MockFormSgSdk = mocked(formsgSdk, true)
+
+jest.mock('../webhook.message.ts')
+const MockWebhookQueueMessage = mocked(WebhookQueueMessage, true)
+
 const EncryptSubmissionModel = getEncryptSubmissionModel(mongoose)
+
+// define test constants
 
 const MOCK_WEBHOOK_URL = 'https://form.gov.sg/endpoint'
 const DEFAULT_ERROR_MSG = 'a generic error has occurred'
@@ -78,6 +85,20 @@ const MOCK_WEBHOOK_DEFAULT_FORMAT_RESPONSE: Pick<IWebhookResponse, 'response'> =
   }
 
 describe('webhook.service', () => {
+  const MOCK_FORM_ID = new ObjectId().toHexString()
+  const MOCK_SUBMISSION_ID = new ObjectId().toHexString()
+  const MOCK_WEBHOOK_VIEW: WebhookView = {
+    data: {
+      created: new Date(),
+      encryptedContent: 'mockEncryptedContent',
+      formId: MOCK_FORM_ID,
+      submissionId: MOCK_SUBMISSION_ID,
+      verifiedContent: 'mockVerifiedContent',
+      version: 1,
+    },
+  }
+  const MOCK_SIGNATURE = 'mockSignature'
+
   beforeAll(async () => await dbHandler.connect())
   afterEach(async () => {
     await dbHandler.clearDatabase()
@@ -85,55 +106,23 @@ describe('webhook.service', () => {
   afterAll(async () => await dbHandler.closeDatabase())
 
   // test variables
-  let testEncryptedSubmission: IEncryptedSubmissionSchema
   let testConfig: AxiosRequestConfig
-  let testSubmissionWebhookView: WebhookView | null
-  let testSignature: string
 
   beforeEach(async () => {
     jest.restoreAllMocks()
 
-    // prepare for form creation workflow
-    const MOCK_ADMIN_OBJ_ID = new ObjectID()
     const MOCK_EPOCH = 1487076708000
-    const preloaded = await dbHandler.insertFormCollectionReqs({
-      userId: MOCK_ADMIN_OBJ_ID,
-    })
-
     jest.spyOn(Date, 'now').mockImplementation(() => MOCK_EPOCH)
 
-    // instantiate new form and save
-    const testEncryptedForm = await FormModel.create({
-      title: 'Test Form',
-      admin: preloaded.user._id,
-      responseMode: ResponseMode.Encrypt,
-      publicKey: 'fake-public-key',
-    })
-
-    // initialise encrypted submussion
-    testEncryptedSubmission = await EncryptSubmissionModel.create({
-      form: testEncryptedForm._id,
-      authType: testEncryptedForm.authType,
-      myInfoFields: [],
-      encryptedContent: 'encrypted-content',
-      verifiedContent: 'verified-content',
-      version: 1,
-      webhookResponses: [],
-    })
-
-    // initialise webhook related variables
-    testSubmissionWebhookView = testEncryptedSubmission.getWebhookView()
-
-    testSignature = formsgSdk.webhooks.generateSignature({
-      uri: MOCK_WEBHOOK_URL,
-      submissionId: testEncryptedSubmission._id,
-      formId: testEncryptedForm._id,
-      epoch: MOCK_EPOCH,
-    })
+    MockFormSgSdk.webhooks.generateSignature.mockReturnValueOnce(MOCK_SIGNATURE)
+    const mockWebhookHeader = `t=${MOCK_EPOCH},s=${MOCK_SUBMISSION_ID},f=${MOCK_FORM_ID},v1=${MOCK_SIGNATURE}`
+    MockFormSgSdk.webhooks.constructHeader.mockReturnValueOnce(
+      mockWebhookHeader,
+    )
 
     testConfig = {
       headers: {
-        'X-FormSG-Signature': `t=${MOCK_EPOCH},s=${testEncryptedSubmission._id},f=${testEncryptedForm._id},v1=${testSignature}`,
+        'X-FormSG-Signature': mockWebhookHeader,
       },
       maxRedirects: 0,
       timeout: 10000,
@@ -145,7 +134,7 @@ describe('webhook.service', () => {
       // Arrange
       const mockWebhookResponse = {
         ...MOCK_WEBHOOK_SUCCESS_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       } as IWebhookResponse
 
@@ -156,8 +145,8 @@ describe('webhook.service', () => {
         .mockRejectedValueOnce(mockDBError)
 
       // Act
-      const actual = await saveWebhookRecord(
-        testEncryptedSubmission._id,
+      const actual = await WebhookService.saveWebhookRecord(
+        MOCK_SUBMISSION_ID,
         mockWebhookResponse,
       )
 
@@ -165,7 +154,7 @@ describe('webhook.service', () => {
       const expectedError = transformMongoError(mockDBError)
 
       expect(addWebhookResponseSpy).toHaveBeenCalledWith(
-        testEncryptedSubmission._id,
+        MOCK_SUBMISSION_ID,
         mockWebhookResponse,
       )
       expect(actual._unsafeUnwrapErr()).toEqual(expectedError)
@@ -175,13 +164,13 @@ describe('webhook.service', () => {
       // Arrange
       const mockWebhookResponse = {
         ...MOCK_WEBHOOK_SUCCESS_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       } as IWebhookResponse
 
       // Act
-      const actual = await saveWebhookRecord(
-        new ObjectID(),
+      const actual = await WebhookService.saveWebhookRecord(
+        new ObjectId(),
         mockWebhookResponse,
       )
 
@@ -196,15 +185,15 @@ describe('webhook.service', () => {
     it('should return updated submission with new webhook response if the record is successfully saved', async () => {
       // Arrange
       const mockWebhookResponse = {
-        _id: testEncryptedSubmission._id,
-        created: testEncryptedSubmission.created,
+        _id: MOCK_SUBMISSION_ID,
+        created: new Date(),
         ...MOCK_WEBHOOK_SUCCESS_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       } as IWebhookResponse
 
       const expectedSubmission = new EncryptSubmissionModel({
-        ...testEncryptedSubmission,
+        _id: MOCK_SUBMISSION_ID,
       })
       expectedSubmission.webhookResponses = [mockWebhookResponse]
 
@@ -213,14 +202,14 @@ describe('webhook.service', () => {
         .mockResolvedValue(expectedSubmission)
 
       // Act
-      const actual = await saveWebhookRecord(
-        testEncryptedSubmission._id,
+      const actual = await WebhookService.saveWebhookRecord(
+        MOCK_SUBMISSION_ID,
         mockWebhookResponse,
       )
 
       // Assert
       expect(addWebhookResponseSpy).toHaveBeenCalledWith(
-        testEncryptedSubmission._id,
+        MOCK_SUBMISSION_ID,
         mockWebhookResponse,
       )
       expect(actual._unsafeUnwrap()).toEqual(expectedSubmission)
@@ -235,8 +224,8 @@ describe('webhook.service', () => {
       )
 
       // Act
-      const actual = await sendWebhook(
-        testEncryptedSubmission.getWebhookView(),
+      const actual = await WebhookService.sendWebhook(
+        MOCK_WEBHOOK_VIEW,
         MOCK_WEBHOOK_URL,
       )
 
@@ -256,8 +245,8 @@ describe('webhook.service', () => {
       )
 
       // Act
-      const actual = await sendWebhook(
-        testEncryptedSubmission.getWebhookView(),
+      const actual = await WebhookService.sendWebhook(
+        MOCK_WEBHOOK_VIEW,
         MOCK_WEBHOOK_URL,
       )
 
@@ -286,28 +275,28 @@ describe('webhook.service', () => {
         toJSON: () => jest.fn(),
       }
 
-      expect(
-        MockWebhookValidationModule.validateWebhookUrl,
-      ).toHaveBeenCalledWith(MOCK_WEBHOOK_URL)
       MockAxios.post.mockRejectedValue(MOCK_AXIOS_ERROR)
       MockAxios.isAxiosError.mockReturnValue(true)
 
       // Act
-      const actual = await sendWebhook(
-        testEncryptedSubmission.getWebhookView(),
+      const actual = await WebhookService.sendWebhook(
+        MOCK_WEBHOOK_VIEW,
         MOCK_WEBHOOK_URL,
       )
 
       // Assert
       const expectedResult = {
         ...MOCK_WEBHOOK_FAILURE_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       }
 
+      expect(
+        MockWebhookValidationModule.validateWebhookUrl,
+      ).toHaveBeenCalledWith(MOCK_WEBHOOK_URL)
       expect(MockAxios.post).toHaveBeenCalledWith(
         MOCK_WEBHOOK_URL,
-        testSubmissionWebhookView,
+        MOCK_WEBHOOK_VIEW,
         testConfig,
       )
       expect(actual._unsafeUnwrap()).toEqual(expectedResult)
@@ -321,15 +310,15 @@ describe('webhook.service', () => {
       MockAxios.isAxiosError.mockReturnValue(false)
 
       // Act
-      const actual = await sendWebhook(
-        testEncryptedSubmission.getWebhookView(),
+      const actual = await WebhookService.sendWebhook(
+        MOCK_WEBHOOK_VIEW,
         MOCK_WEBHOOK_URL,
       )
 
       // Assert
       const expectedResult = {
         ...MOCK_WEBHOOK_DEFAULT_FORMAT_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       }
 
@@ -338,7 +327,7 @@ describe('webhook.service', () => {
       ).toHaveBeenCalledWith(MOCK_WEBHOOK_URL)
       expect(MockAxios.post).toHaveBeenCalledWith(
         MOCK_WEBHOOK_URL,
-        testSubmissionWebhookView,
+        MOCK_WEBHOOK_VIEW,
         testConfig,
       )
       expect(actual._unsafeUnwrap()).toEqual(expectedResult)
@@ -354,15 +343,15 @@ describe('webhook.service', () => {
       MockAxios.isAxiosError.mockReturnValue(false)
 
       // Act
-      const actual = await sendWebhook(
-        testEncryptedSubmission.getWebhookView(),
+      const actual = await WebhookService.sendWebhook(
+        MOCK_WEBHOOK_VIEW,
         MOCK_WEBHOOK_URL,
       )
 
       // Assert
       const expectedResult = {
         ...MOCK_WEBHOOK_DEFAULT_FORMAT_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       }
 
@@ -371,7 +360,7 @@ describe('webhook.service', () => {
       ).toHaveBeenCalledWith(MOCK_WEBHOOK_URL)
       expect(MockAxios.post).toHaveBeenCalledWith(
         MOCK_WEBHOOK_URL,
-        testSubmissionWebhookView,
+        MOCK_WEBHOOK_VIEW,
         testConfig,
       )
       expect(actual._unsafeUnwrap()).toEqual(expectedResult)
@@ -384,15 +373,15 @@ describe('webhook.service', () => {
       MockAxios.post.mockResolvedValue(MOCK_AXIOS_SUCCESS_RESPONSE)
 
       // Act
-      const actual = await sendWebhook(
-        testEncryptedSubmission.getWebhookView(),
+      const actual = await WebhookService.sendWebhook(
+        MOCK_WEBHOOK_VIEW,
         MOCK_WEBHOOK_URL,
       )
 
       // Assert
       const expectedResult = {
         ...MOCK_WEBHOOK_SUCCESS_RESPONSE,
-        signature: testSignature,
+        signature: MOCK_SIGNATURE,
         webhookUrl: MOCK_WEBHOOK_URL,
       }
 
@@ -401,10 +390,85 @@ describe('webhook.service', () => {
       ).toHaveBeenCalledWith(MOCK_WEBHOOK_URL)
       expect(MockAxios.post).toHaveBeenCalledWith(
         MOCK_WEBHOOK_URL,
-        testSubmissionWebhookView,
+        MOCK_WEBHOOK_VIEW,
         testConfig,
       )
       expect(actual._unsafeUnwrap()).toEqual(expectedResult)
+    })
+  })
+
+  describe('createInitialWebhookSender', () => {
+    // This suite only checks for correct behaviour for webhook retries,
+    // since there are separate tests for sending webhooks and saving
+    // responses to the database.
+    let testSubmission: IEncryptedSubmissionSchema
+    const MOCK_PRODUCER = {
+      sendMessage: jest.fn().mockReturnValue(okAsync(true)),
+    } as unknown as WebhookProducer
+    beforeEach(() => {
+      jest.clearAllMocks()
+
+      testSubmission = new EncryptSubmissionModel({
+        _id: MOCK_SUBMISSION_ID,
+      })
+      jest
+        .spyOn(EncryptSubmissionModel, 'addWebhookResponse')
+        .mockResolvedValue(testSubmission)
+      MockWebhookValidationModule.validateWebhookUrl.mockResolvedValue()
+    })
+
+    it('should return true without retrying when webhook is successful and retries are enabled', async () => {
+      MockAxios.post.mockResolvedValue(MOCK_AXIOS_SUCCESS_RESPONSE)
+
+      const result = await WebhookService.createInitialWebhookSender(
+        MOCK_PRODUCER,
+      )(testSubmission, MOCK_WEBHOOK_URL, /* isRetryEnabled= */ true)
+
+      expect(result._unsafeUnwrap()).toBe(true)
+      expect(MockWebhookQueueMessage.fromSubmissionId).not.toHaveBeenCalled()
+    })
+
+    it('should return true without retrying when webhook fails but retries are not enabled globally', async () => {
+      MockAxios.post.mockResolvedValue(MOCK_AXIOS_SUCCESS_RESPONSE)
+
+      const result = await WebhookService
+        .createInitialWebhookSender
+        // no producer passed to createInitialWebhookSender, so retries not enabled globally
+        ()(testSubmission, MOCK_WEBHOOK_URL, true)
+
+      expect(result._unsafeUnwrap()).toBe(true)
+      expect(MockWebhookQueueMessage.fromSubmissionId).not.toHaveBeenCalled()
+    })
+
+    it('should return true without retrying when webhook fails and retries are not enabled for form', async () => {
+      MockAxios.post.mockResolvedValue(MOCK_AXIOS_FAILURE_RESPONSE)
+
+      const result = await WebhookService
+        .createInitialWebhookSender
+        // no producer passed to createInitialWebhookSender, so retries not enabled globally
+        ()(testSubmission, MOCK_WEBHOOK_URL, /* isRetryEnabled= */ false)
+
+      expect(result._unsafeUnwrap()).toBe(true)
+      expect(MockWebhookQueueMessage.fromSubmissionId).not.toHaveBeenCalled()
+    })
+
+    it('should return true and retry when webhook fails and retries are enabled', async () => {
+      const mockQueueMessage =
+        'mockQueueMessage' as unknown as WebhookQueueMessage
+      MockWebhookQueueMessage.fromSubmissionId.mockReturnValueOnce(
+        ok(mockQueueMessage),
+      )
+      MockAxios.post.mockResolvedValue(MOCK_AXIOS_FAILURE_RESPONSE)
+
+      const result = await WebhookService.createInitialWebhookSender(
+        MOCK_PRODUCER,
+      )(testSubmission, MOCK_WEBHOOK_URL, /* isRetryEnabled= */ true)
+
+      expect(result._unsafeUnwrap()).toBe(true)
+      expect(MockWebhookQueueMessage.fromSubmissionId).toHaveBeenCalledWith(
+        String(testSubmission._id),
+      )
+      expect(MOCK_PRODUCER.sendMessage).toHaveBeenCalledWith(mockQueueMessage)
     })
   })
 })
