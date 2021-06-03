c6e001b32aa13733316f01d5f519034a9c423864
Kar Rui Lau <karrui.lau@gmail.com>
2021-06-03 12:53:05 +0800

    feat(AdminAuthService): add sendLoginOtp function

--- src/public/services/AdminAuthService.ts
+++ src/public/services/AdminAuthService.ts
@@ -16,7 +16,9 @@ export const checkIsEmailAllowed = async (email = ''): Promise<string> => {
   }
 
   return axios
-    .post(`${ADMIN_AUTH_ENDPOINT}/email/validate`, { email })
+    .post(`${ADMIN_AUTH_ENDPOINT}/email/validate`, {
+      email: email.toLowerCase(),
+    })
     .then(() => email)
     .catch((error) => {
       if (axios.isAxiosError(error) && error.response?.data) {
@@ -24,6 +26,33 @@ export const checkIsEmailAllowed = async (email = ''): Promise<string> => {
       }
       throw new Error(
         'Something went wrong while validating your email. Please refresh and try again',
+      )
+    })
+}
+
+/**
+ * Sends login OTP to given email
+ * @param email email to send login OTP to
+ * @returns success string if login OTP is sent successfully
+ * @throws Error on invalid email
+ * @throws Error on non 2xx response
+ */
+export const sendLoginOtp = async (email = ''): Promise<string> => {
+  if (!validator.isEmail(email)) {
+    throw new Error('Please enter a valid email address')
+  }
+
+  return axios
+    .post<string>(`${ADMIN_AUTH_ENDPOINT}/otp/generate`, {
+      email: email.toLowerCase(),
+    })
+    .then(({ data }) => data)
+    .catch((error) => {
+      if (axios.isAxiosError(error) && error.response?.data) {
+        throw new Error(error.response.data)
+      }
+      throw new Error(
+        'Failed to send login OTP. Please try again later and if the problem persists, contact us.',
       )
     })
 }
