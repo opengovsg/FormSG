07689c63ee989c4bf18ece5706b2551e6de43a41
seaerchin <44049504+seaerchin@users.noreply.github.com>
2021-06-07 18:29:54 +0800

    refactor(submissions.client.factory): extract admin form features (#1983)
    * refactor(misc): removed unused parameter in submission factory; added typings to count submissions
    
    * refactor(adminformservice): added method (and typing) for countFormSubmissions
    
    * refactor(public): removed old Submission.count calls and replaced with countFormSubmissions
    
    * test(adminformservice): adds test for countFormSubmissions
    
    * feat(adminformservice): add new method (and typings) for retrieving form metadata
    
    * test(adminformservice/test): adds tests for new method
    
    * refactor(public): removed getMetadata and changed calls to getFormsMetadata
    
    * feat(adminformservice): add new method (and type) for retrieval of single response
    
    * test(adminformservice/test): adds test for getEncryptedResponse
    
    * refactor(public): replaced Submission.getEncryptedResponse with new version
    
    * chore(adminformservice): shifts submission related stuff to AdminSubmissionsService
    
    * chore(public): updated prev callsites for submissions related methods
    
    * fix(view-feedback.client.directive): removed duplicate services
    
    * refactor(types/api/submission): changed typing of submission count query and associated methods
    
    * refactor(getformsmetadata): split into 2 smaller methods and updated callsites
    
    * test(adminsubmissionsservice): removed MockAxios import and uses jest.mock with mocked
    
    using mockAxios impacted readability and understanding so it was changed to use jest.mock (which
    mocks the whole module) together with mocked from ts-jest to mock return values
    
    * refactor(adminsubmissionservice): renamed methods for greater clarity
    
    * test(adminsubmissionsservice): changed test structure for increased clarity

---
+++ src/public/services/__tests__/AdminSubmissionsService.test.ts
@@ -0,0 +1,165 @@
+import axios from 'axios'
+import { mocked } from 'ts-jest/utils'
+
+import { SubmissionMetadataList } from 'src/types'
+
+import { ADMIN_FORM_ENDPOINT } from '../AdminFormService'
+import {
+  countFormSubmissions,
+  getEncryptedResponse,
+  getFormMetadataById,
+  getFormsMetadataByPage,
+} from '../AdminSubmissionsService'
+
+jest.mock('axios')
+const MockAxios = mocked(axios, true)
+
+describe('AdminSubmissionsService', () => {
+  describe('countFormSubmissions', () => {
+    const MOCK_FORM_ID = 'mock–form-id'
+    const MOCK_START_DATE = new Date(2020, 11, 17)
+    const MOCK_END_DATE = new Date(2021, 1, 10)
+
+    it('should call api successfully when all parameters are provided', async () => {
+      // Arrange
+      MockAxios.get.mockResolvedValueOnce({ data: 123 })
+
+      // Act
+      const actual = await countFormSubmissions({
+        formId: MOCK_FORM_ID,
+        dates: { startDate: MOCK_START_DATE, endDate: MOCK_END_DATE },
+      })
+
+      // Assert
+      expect(actual).toEqual(123)
+      expect(MockAxios.get).toHaveBeenCalledWith(
+        `${ADMIN_FORM_ENDPOINT}/${MOCK_FORM_ID}/submissions/count`,
+        {
+          params: {
+            startDate: MOCK_START_DATE,
+            endDate: MOCK_END_DATE,
+          },
+        },
+      )
+    })
+
+    it('should call api successfully when only formId is provided', async () => {
+      // Arrange
+      MockAxios.get.mockResolvedValueOnce({ data: 123 })
+
+      // Act
+      const actual = await countFormSubmissions({
+        formId: MOCK_FORM_ID,
+      })
+
+      // Assert
+      expect(actual).toEqual(123)
+      expect(MockAxios.get).toHaveBeenCalledWith(
+        `${ADMIN_FORM_ENDPOINT}/${MOCK_FORM_ID}/submissions/count`,
+      )
+    })
+  })
+
+  describe('getFormsMetadataByPage', () => {
+    const MOCK_FORM_ID = 'mock–form-id'
+    const MOCK_PAGE_NUM = 1
+    const MOCK_RESPONSE: SubmissionMetadataList = {
+      count: 1,
+      metadata: [
+        {
+          number: 1,
+          refNo: '1234',
+          submissionTime: 'sometime',
+        },
+      ],
+    }
+
+    it('should call the api correctly', async () => {
+      // Arrange
+      MockAxios.get.mockResolvedValueOnce({ data: MOCK_RESPONSE })
+
+      // Act
+      const actual = await getFormsMetadataByPage({
+        formId: MOCK_FORM_ID,
+        pageNum: MOCK_PAGE_NUM,
+      })
+
+      // Assert
+      expect(actual).toEqual(MOCK_RESPONSE)
+      expect(MockAxios.get).toHaveBeenCalledWith(
+        `${ADMIN_FORM_ENDPOINT}/${MOCK_FORM_ID}/submissions/metadata`,
+        {
+          params: {
+            page: MOCK_PAGE_NUM,
+          },
+        },
+      )
+    })
+  })
+
+  describe('getFormMetadataById', () => {
+    const MOCK_FORM_ID = 'mock–form-id'
+    const MOCK_SUBMISSION_ID = 'fake'
+    const MOCK_RESPONSE: SubmissionMetadataList = {
+      count: 1,
+      metadata: [
+        {
+          number: 1,
+          refNo: '1234',
+          submissionTime: 'sometime',
+        },
+      ],
+    }
+
+    it('should call the api correctly', async () => {
+      // Arrange
+      MockAxios.get.mockResolvedValueOnce({ data: MOCK_RESPONSE })
+
+      // Act
+      const actual = await getFormMetadataById({
+        formId: MOCK_FORM_ID,
+        submissionId: MOCK_SUBMISSION_ID,
+      })
+
+      // Assert
+      expect(actual).toEqual(MOCK_RESPONSE)
+      expect(MockAxios.get).toHaveBeenCalledWith(
+        `${ADMIN_FORM_ENDPOINT}/${MOCK_FORM_ID}/submissions/metadata`,
+        {
+          params: {
+            submissionId: MOCK_SUBMISSION_ID,
+          },
+        },
+      )
+    })
+  })
+
+  describe('getEncryptedResponse', () => {
+    const MOCK_FORM_ID = 'mock–form-id'
+    const MOCK_SUBMISSION_ID = 'fake'
+    const MOCK_RESPONSE = {
+      refNo: '1',
+      submissionTime: 'sometime',
+      content: 'jk',
+      verified: 'whups',
+      attachmentMetadata: {},
+    }
+
+    it('should call the api correctly when the parameters are valid', async () => {
+      // Arrange
+      MockAxios.get.mockResolvedValueOnce({ data: MOCK_RESPONSE })
+
+      // Act
+      const actual = await getEncryptedResponse({
+        formId: MOCK_FORM_ID,
+        submissionId: MOCK_SUBMISSION_ID,
+      })
+
+      // Assert
+      expect(actual).toEqual(MOCK_RESPONSE)
+      expect(MockAxios.get).toHaveBeenCalledWith(
+        `${ADMIN_FORM_ENDPOINT}/${MOCK_FORM_ID}/submissions/${MOCK_SUBMISSION_ID}`,
+      )
+    })
+  })
+})
