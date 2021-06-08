e29776a3b169ae8deca773b20057674289c03a39
seaerchin <jer005@e.ntu.edu.sg>
2021-06-08 11:58:52 +0800

    refactor(formsgsdkservice): uses new injectedvariables for sdk mode

--- src/public/services/FormSgSdkService.ts
+++ src/public/services/FormSgSdkService.ts
@@ -1,15 +1,10 @@
 import formSgSdk from '@opengovsg/formsg-sdk'
-import { PackageMode } from '@opengovsg/formsg-sdk/dist/types'
 
 import { TRANSACTION_EXPIRE_AFTER_SECONDS } from '../../shared/util/verification'
-
-// Reading from process.env.FORMSG_SDK_MODE defaults to undefined
-// This is because browsers are sandboxed and hence have to manually check on window...
-const sdkMode = (((window as unknown) as { formsgSdkMode: PackageMode })
-  .formsgSdkMode || 'production') as PackageMode
+import { injectedVariables } from '../utils/injectedVariables'
 
 export const FormSgSdk: ReturnType<typeof formSgSdk> = formSgSdk({
-  mode: sdkMode,
+  mode: injectedVariables.formsgSdkMode,
   verificationOptions: {
     transactionExpiry: TRANSACTION_EXPIRE_AFTER_SECONDS,
   },
