ab2fbc8c0739f49af2073f6264dbace4f222e81c
shuli-ogp <shuli@open.gov.sg>
2021-06-08 10:44:39 +0800

    chore: script to unset customMin and customMax for text and number fields

--- scripts/20210517_validation-object/script.js
+++ scripts/20210517_validation-object/script.js
@@ -1,7 +1,11 @@
 /* eslint-disable */
 // Scripts for #408
 
-// Part 1: Verify that the database is internally consistent today for all short text, long text and number fields:
+/**
+ * Part 1: Verify that the database is internally consistent today
+ * for all short text, long text and number fields.
+ * Expect 0 records for all scripts
+ */
 
 // a. customVal === customMin when selectedValidation === 'Minimum'
 // Expect 0 records
@@ -130,4 +134,150 @@ db.getCollection('forms').aggregate([
       selectedValidation: 'Range',
     },
   },
+])
+
+/**
+ * Part 2: Unset form_fields.ValidationOptions.customMin and
+ * form_fields.ValidationOptions.customMax
+ * for all short text, long text and number fields.
+ */
+
+// ********** BEFORE **********
+
+// Count number of forms with (short text, long text and number fields) and
+// with ValidationOptions.customMin
+// Should match number of updated records later
+
+db.forms.count({
+  form_fields: {
+    $elemMatch: {
+      $and: [
+        {
+          fieldType: { $in: ['textfield', 'textarea', 'number'] },
+        },
+        { 'ValidationOptions.customMin': { $exists: true } },
+      ],
+    },
+  },
+})
+
+// Count number of forms with (short text, long text and number fields) and
+// with ValidationOptions.customMax
+// Should match number of updated records later
+
+db.forms.count({
+  form_fields: {
+    $elemMatch: {
+      $and: [
+        {
+          fieldType: { $in: ['textfield', 'textarea', 'number'] },
+        },
+        { 'ValidationOptions.customMax': { $exists: true } },
+      ],
+    },
+  },
+})
+
+// ********** UPDATE **********
+
+// Remove customMin
+
+db.forms.update(
+  { 'form_fields.ValidationOptions.customMin': { $exists: true } },
+  { $unset: { 'form_fields.$[elem].ValidationOptions.customMin': '' } },
+  {
+    arrayFilters: [
+      {
+        'elem.fieldType': { $in: ['textfield', 'textarea', 'number'] },
+        'elem.ValidationOptions.customMin': { $exists: true },
+      },
+    ],
+    multi: true,
+  },
+)
+
+// Remove customMax
+
+db.forms.update(
+  { 'form_fields.ValidationOptions.customMax': { $exists: true } },
+  { $unset: { 'form_fields.$[elem].ValidationOptions.customMax': '' } },
+  {
+    arrayFilters: [
+      {
+        'elem.fieldType': { $in: ['textfield', 'textarea', 'number'] },
+        'elem.ValidationOptions.customMax': { $exists: true },
+      },
+    ],
+    multi: true,
+  },
+)
+
+// ********** AFTER **********
+
+// Count number of forms with (short text, long text and number fields) and
+// with ValidationOptions.customMin
+// Expect 0 after running update
+
+db.forms.count({
+  form_fields: {
+    $elemMatch: {
+      $and: [
+        {
+          fieldType: { $in: ['textfield', 'textarea', 'number'] },
+        },
+        { 'ValidationOptions.customMin': { $exists: true } },
+      ],
+    },
+  },
+})
+
+// Count number of forms with (short text, long text and number fields) and
+// with ValidationOptions.customMax
+// Expect 0 after running update
+
+db.forms.count({
+  form_fields: {
+    $elemMatch: {
+      $and: [
+        {
+          fieldType: { $in: ['textfield', 'textarea', 'number'] },
+        },
+        { 'ValidationOptions.customMax': { $exists: true } },
+      ],
+    },
+  },
+})
+
+// Count number of form FIELDS with (short text, long text and number fields) and
+// with ValidationOptions.customMin
+// Expect 0 after running update
+
+db.forms.aggregate([
+  { $unwind: '$form_fields' },
+  {
+    $match: {
+      'form_fields.fieldType': { $in: ['textfield', 'textarea', 'number'] },
+    },
+  },
+  {
+    $match: { 'form_fields.ValidationOptions.customMin': { $exists: true } },
+  },
+  { $count: 'numFormFields' },
+])
+
+// Count number of form FIELDS with (short text, long text and number fields) and
+// with ValidationOptions.customMax
+// Expect 0 after running update
+
+db.forms.aggregate([
+  { $unwind: '$form_fields' },
+  {
+    $match: {
+      'form_fields.fieldType': { $in: ['textfield', 'textarea', 'number'] },
+    },
+  },
+  {
+    $match: { 'form_fields.ValidationOptions.customMax': { $exists: true } },
+  },
+  { $count: 'numFormFields' },
 ])
