a259e49e9b201c76947bbaa6b124a608cffafc78
chowyiyin <yiyin.chow@gmail.com>
2021-06-08 10:01:22 +0800

    refactor: edit count statement and remove check for number of forms

--- scripts/20210706_remove-endpage-buttons/delete-endpage-buttons.js
+++ scripts/20210706_remove-endpage-buttons/delete-endpage-buttons.js
@@ -6,12 +6,8 @@ Delete endPage.buttons key from old form documents in the database
 
 // == PRE-UPDATE CHECKS ==
 
-// Count total number of forms
-db.getCollection('forms').count() 
-
 // Count number of forms with endPage buttons key
-db.getCollection('forms').find({ "endPage.buttons": { $exists: true } }).count()
-
+db.getCollection('forms').count({ "endPage.buttons": { $exists: true } })
 
 // == UPDATE ==
 // Delete endPage buttons key
@@ -24,10 +20,6 @@ db.getCollection('forms').updateMany({}, {
 
 // == POST-UPDATE CHECKS ==
 
-// Check total forms count
-// ~ Should be same as before
-db.getCollection('forms').count()
-
 // Check number of forms with endPage buttons key
 // ~ Should be zero
-db.getCollection('forms').find({ "endPage.buttons": { $exists: true } }).count()
+db.getCollection('forms').count({ "endPage.buttons": { $exists: true } })
