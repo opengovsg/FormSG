b550ccd8150954127efc99849a5646be1ef77408
Kar Rui Lau <karrui.lau@gmail.com>
2021-06-09 16:00:52 +0800

    feat(foundation): add shadows theming

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -1,12 +1,14 @@
 import { extendTheme } from '@chakra-ui/react'
 
 import { colours } from './foundations/colours'
+import { shadows } from './foundations/shadows'
 import { components } from './components'
 import { textStyles } from './textStyles'
 
 export const theme = extendTheme({
   colors: colours,
   textStyles,
+  shadows,
   fonts: {
     body: 'Inter',
   },
