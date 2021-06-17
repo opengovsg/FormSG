7908bbc5292e59b344934986f3f73369ead65109
Antariksh Mahajan <antarikshmahajan@gmail.com>
2021-06-17 21:19:47 +0800

    feat: add and call v3 API for retrieving individual admin form (#2113) (#2201)
    * feat: add v3 API for retrieving admin form by id
    
    * feat: remove `@routes` JSDoc line as they do not add info
    
    and can be often wrong, and the real route is already lines away
    
    * feat(AdminViewFormService): call v3 API to retrieve admin form
    
    this allows us to eventually deprecate the old GET /adminform endpoints (and its ilk)
    
    Co-authored-by: Kar Rui Lau <karrui.lau@gmail.com>

--- src/public/services/AdminViewFormService.ts
+++ src/public/services/AdminViewFormService.ts
@@ -25,7 +25,9 @@ export const getDashboardView = async (): Promise<FormMetaView[]> => {
 export const getAdminFormView = async (
   formId: string,
 ): Promise<FormViewDto> => {
-  return axios.get<FormViewDto>(`/${formId}/adminform`).then(({ data }) => data)
+  return axios
+    .get<FormViewDto>(`${ADMIN_FORM_ENDPOINT}/${formId}`)
+    .then(({ data }) => data)
 }
 
 /**
