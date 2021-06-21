54b91755fd41516590650cbfaaaf61ee86ddec25
Kar Rui Lau <karrui.lau@gmail.com>
2021-06-22 00:52:47 +0800

    fix: update breakpoints again to use em due to bug in sorting
    see https://github.com/chakra-ui/chakra-ui/issues/3042

--- frontend/src/theme/foundations/breakpoints.ts
+++ frontend/src/theme/foundations/breakpoints.ts
@@ -1,9 +1,9 @@
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 export const breakpoints = createBreakpoints({
-  xs: '360px',
-  sm: '480px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1440px',
+  xs: '22.5em',
+  sm: '30em',
+  md: '48em',
+  lg: '64em',
+  xl: '90em',
 })
