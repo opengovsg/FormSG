836595a33fd1fabec60b2428a559f4dd8864590b
seaerchin <jer005@e.ntu.edu.sg>
2021-06-21 13:08:18 +0800

    feat(theme/components): adds alert styling for toasts

--- frontend/src/theme/components/index.ts
+++ frontend/src/theme/components/index.ts
@@ -1,4 +1,5 @@
+import { Alert } from './Alert'
 import { Button } from './Button'
 import { Input } from './Input'
 
-export const components = { Button, Input }
+export const components = { Button, Input, Alert }
