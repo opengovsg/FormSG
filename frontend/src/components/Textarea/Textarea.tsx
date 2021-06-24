ec96f99c7bc0f8ed1910221901c0beae45e1a1c8
Kar Rui Lau <karrui.lau@gmail.com>
2021-06-24 15:27:47 +0800

    feat: add Textarea component

---
+++ frontend/src/components/Textarea/Textarea.tsx
@@ -0,0 +1,39 @@
+import ResizeTextarea, { TextareaAutosizeProps } from 'react-textarea-autosize'
+import {
+  forwardRef,
+  Textarea as ChakraTextarea,
+  TextareaProps as ChakraTextareaProps,
+} from '@chakra-ui/react'
+
+export interface TextareaProps extends ChakraTextareaProps {
+  /**
+   * The minimum rows the textarea displays on render.
+   * Defaults to `3`.
+   */
+  minAutosizeRows?: TextareaAutosizeProps['minRows']
+  /**
+   * The maximum rows the textarea will automatically resize to.
+   * Defaults to `6`.
+   */
+  maxAutosizeRows?: TextareaAutosizeProps['maxRows']
+}
+
+export const Textarea = forwardRef<TextareaProps, 'textarea'>(
+  (
+    { minAutosizeRows: minRows = 3, maxAutosizeRows: maxRows = 6, ...props },
+    ref,
+  ) => {
+    return (
+      <ChakraTextarea
+        //Chakra sets a default minH which prevents autosize to control starting height.
+        // Set here instead of on the theme to prevent all base text areas from having no minH.
+        minH="unset"
+        ref={ref}
+        as={ResizeTextarea}
+        minRows={minRows}
+        maxRows={maxRows}
+        {...props}
+      />
+    )
+  },
+)
