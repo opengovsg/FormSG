57b542f34b9860721472f598ad11f10a77302d0f
Kar Rui Lau <karrui.lau@gmail.com>
2021-06-24 13:37:03 +0800

    ref: add JSDoc and inline success icon component

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -11,7 +11,13 @@ import {
 import { BxsCheckCircle } from '~assets/icons/BxsCheckCircle'
 
 export interface InputProps extends ChakraInputProps {
+  /**
+   * Whether the input is in a prefilled state.
+   */
   isPrefilled?: boolean
+  /**
+   * Whether the input is in a success state.
+   */
   isSuccess?: boolean
 }
 
@@ -44,20 +50,12 @@ export const Input = forwardRef<InputProps, 'input'>(
     return (
       <InputGroup>
         <ChakraInput ref={ref} {...extraInputProps} {...props} />
-        {isSuccess && <SuccessIcon />}
+        {isSuccess && (
+          <InputRightElement pointerEvents="none" fontSize="1.25rem">
+            <Icon as={BxsCheckCircle} color="success.700" />
+          </InputRightElement>
+        )}
       </InputGroup>
     )
   },
-)
-
-/**
- * @precondition Must be encapsulated inside an `InputGroup` component due to
- * using InputGroup context.
- *
- * Icon to display when input is in a success state.
- */
-const SuccessIcon = () => (
-  <InputRightElement pointerEvents="none" fontSize="1.25rem">
-    <Icon as={BxsCheckCircle} color="success.700" />
-  </InputRightElement>
 )
