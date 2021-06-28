ef0f0ef7216b5b73e98ab741d650ea8d7def5fd1
seaerchin <jer005@e.ntu.edu.sg>
2021-06-28 16:59:56 +0800

    chore(scripts): added extra checks for verifiction counts

--- scripts/20210628_create-isOnboardedAcc/create-isOnboardedAcc.js
+++ scripts/20210628_create-isOnboardedAcc/create-isOnboardedAcc.js
@@ -7,8 +7,22 @@ This script creates a new key, isOnboardedAccount, on the smscounts db which ind
 let formTwilioId = 'insert the form twilio id here'
 
 // == PRE-UPDATE CHECKS ==
-// Count the number of verifications
+// Count the number of verifications (A)
 db.getCollection('smscounts').count({ smsType: 'VERIFICATION' })
+
+// Count of forms using our twilio acc (B)
+db.getCollection('smscounts').count({
+  smsType: 'VERIFICATION',
+msgSrvcSid: {$eq: formTwilioId},
+})
+
+// Count of forms using their own twilio acc (C)
+// A === B + C
+db.getCollection('smscounts').count({
+  smsType: 'VERIFICATION',
+  msgSrvcSid: { $ne: formTwilioId },
+})
+
 
 // == UPDATE ==
 // Update verifications which have message service id equal to form twilio id
@@ -32,13 +46,15 @@ db.getCollection('smscounts').updateMany(
 )
 
 // == POST-UPDATE CHECKS ==
-
 // Check number of verifications updated
 // Sum of these two should be equal to the initial count
+// Count of forms using our twilio acc
 db.getCollection('smscounts').count({
   smsType: 'VERIFICATION',
 msgSrvcSid: {$eq: formTwilioId},
 })
+
+// Count of forms using their own twilio acc
 db.getCollection('smscounts').count({
   smsType: 'VERIFICATION',
   msgSrvcSid: { $ne: formTwilioId },
