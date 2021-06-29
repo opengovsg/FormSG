7ad271f5fdf0e527c823a5da03d4487b00282576
seaerchin <jer005@e.ntu.edu.sg>
2021-06-29 17:37:46 +0800

    fix(sms.controller): removed sms count retrieval requiring the userId as it sohuld be for the admin

--- src/app/services/sms/sms.controller.ts
+++ src/app/services/sms/sms.controller.ts
@@ -1,10 +1,10 @@
 import { StatusCodes } from 'http-status-codes'
+import { Types } from 'mongoose'
 
 import { ErrorDto } from '../../../types/api'
 import { createLoggerWithLabel } from '../../config/logger'
 import { ControllerHandler } from '../../modules/core/core.types'
 import * as FormService from '../../modules/form/form.service'
-import * as UserService from '../../modules/user/user.service'
 import * as SmsService from '../../services/sms/sms.service'
 import { createReqMeta } from '../../utils/request'
 
@@ -18,35 +18,37 @@ const logger = createLoggerWithLabel(module)
  * This is the controller for GET /:userId/:formId endpoint
  * @param formId The id of the form to retrieve the message service id for
  * @param userId The user id of the form to retrieve sms counts for
- * @returns
- * @returns
+ * @returns 200 with msgSrvcId and free sms counts when successful
+ * @returns 404 when the userId is not found in the database
+ * @returns 404 when the formId is not found in the database
+ * @returns 500 when a database error occurs during retrieval
  */
 export const handleGetFreeSmsCountForUser: ControllerHandler<
   {
-    userId: string
     formId: string
   },
   ErrorDto | SmsCountsMeta
 > = (req, res) => {
-  const { userId, formId } = req.params
+  const { formId } = req.params
   const logMeta = {
     action: 'handleGetFreeSmsCountForUser',
     ...createReqMeta(req),
     formId,
-    userId,
   }
 
-  // Step 1: Check that both user and form exist
+  // Step 1: Check that the form exists
   return (
-    UserService.findUserById(userId)
-      .andThen(() => FormService.retrieveFormById(formId))
+    FormService.retrieveFormById(formId)
       // Step 2: Retrieve the free sms count
-      .andThen(({ msgSrvcName }) =>
-        SmsService.retrieveFreeSmsCounts(userId).map((freeSmsCount) => ({
-          msgSrvcSid: msgSrvcName,
-          freeSmsCount,
-        })),
-      )
+      .andThen(({ msgSrvcName, admin }) => {
+        const adminId = (admin as Types.ObjectId).toHexString()
+        return SmsService.retrieveFreeSmsCounts(adminId).map(
+          (freeSmsCount) => ({
+            msgSrvcSid: msgSrvcName,
+            freeSmsCount,
+          }),
+        )
+      })
       // Step 3: Map/MapErr accordingly
       .map((smsCountMeta) => res.status(StatusCodes.OK).json(smsCountMeta))
       .mapErr((error) => {
