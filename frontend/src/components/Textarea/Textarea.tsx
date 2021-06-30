52ba76b4f6f8ac61dba3be490725d2d8caab0dce
Kar Rui Lau <karrui.lau@gmail.com>
2021-06-30 16:18:12 +0800

    fix(Textarea): remove extra theming props before passing into DOM

--- frontend/src/components/Textarea/Textarea.tsx
+++ frontend/src/components/Textarea/Textarea.tsx
@@ -3,7 +3,9 @@ import {
   forwardRef,
   Textarea as ChakraTextarea,
   TextareaProps as ChakraTextareaProps,
+  useStyleConfig,
 } from '@chakra-ui/react'
+import { omit } from '@chakra-ui/utils'
 
 export interface TextareaProps extends ChakraTextareaProps {
   /**
@@ -16,7 +18,6 @@ export interface TextareaProps extends ChakraTextareaProps {
    * Defaults to `6`.
    */
   maxAutosizeRows?: TextareaAutosizeProps['maxRows']
-
   /**
    * Whether the input is in a prefilled state.
    */
@@ -28,10 +29,13 @@ export interface TextareaProps extends ChakraTextareaProps {
 }
 
 export const Textarea = forwardRef<TextareaProps, 'textarea'>(
-  (
-    { minAutosizeRows: minRows = 3, maxAutosizeRows: maxRows = 6, ...props },
-    ref,
-  ) => {
+  ({ minAutosizeRows = 3, maxAutosizeRows = 6, ...props }, ref) => {
+    const inputStyles = useStyleConfig('Textarea', props)
+
+    // Omit extra props so they will not be passed into the DOM and trigger
+    // React warnings.
+    const inputProps = omit(props, ['isSuccess', 'isPrefilled'])
+
     return (
       <ChakraTextarea
         //Chakra sets a default minH which prevents autosize to control starting height.
@@ -39,9 +43,10 @@ export const Textarea = forwardRef<TextareaProps, 'textarea'>(
         minH="unset"
         ref={ref}
         as={ResizeTextarea}
-        minRows={minRows}
-        maxRows={maxRows}
-        {...props}
+        minRows={minAutosizeRows}
+        maxRows={maxAutosizeRows}
+        sx={inputStyles}
+        {...inputProps}
       />
     )
   },
