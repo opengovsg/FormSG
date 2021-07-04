46f2857b7bdc77279e2cc76633355d11dfce14f6
Yong Jie <viorama@gmail.com>
2021-07-04 22:12:24 +0800

    feat(form-model): type-safe approach to creating logic

--- src/app/models/form.server.model.ts
+++ src/app/models/form.server.model.ts
@@ -691,7 +691,9 @@ const compileFormModel = (db: Mongoose): IFormModel => {
     createLogicBody: LogicDto,
   ): Promise<IFormSchema | null> {
     const form = await this.findById(formId).exec()
-    form.form_logics?.push(createLogicBody)
+    if (!form?.form_logics) return null
+    const newLogic = form.form_logics.create(createLogicBody)
+    form.form_logics.push(newLogic)
     return form.save()
   }
 
