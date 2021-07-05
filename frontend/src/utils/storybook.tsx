f03ee0f7ef1ccb622df9194b6b01a62fe36ae891
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-05 12:15:15 +0800

    ref: add helper fn to convert breakpoint string to px width

--- frontend/src/utils/storybook.tsx
+++ frontend/src/utils/storybook.tsx
@@ -8,13 +8,26 @@ export const centerDecorator: DecoratorFn = (storyFn) => (
 )
 
 /**
+ * Helper function to convert theme breakpoint into viewport width in px for
+ * Chromatic viewpoint snapshots.
+ * @param breakpoint the theme breakpoint to convert
+ * @returns the number pixel width of the given breakpoint.
+ */
+const breakpointToViewportWidth = (
+  breakpoint: keyof typeof theme.breakpoints,
+) => {
+  const rem = 16
+  return parseInt(theme.breakpoints[breakpoint]) * rem
+}
+
+/**
  * Viewports mapping viewport key to their width in (pixel) number.
  * Used for Chromatic viewpoint snapshots which requires the numbers in pixels.
  */
 export const viewports = {
-  xs: parseInt(theme.breakpoints['xs']) * 16,
-  sm: parseInt(theme.breakpoints['sm']) * 16,
-  md: parseInt(theme.breakpoints['md']) * 16,
-  lg: parseInt(theme.breakpoints['lg']) * 16,
-  xl: parseInt(theme.breakpoints['xl']) * 16,
+  xs: breakpointToViewportWidth('xs'),
+  sm: breakpointToViewportWidth('sm'),
+  md: breakpointToViewportWidth('md'),
+  lg: breakpointToViewportWidth('lg'),
+  xl: breakpointToViewportWidth('xl'),
 }
