opengovsg/FormSG

commit f88ad8997cc1b754ef7f22e6596835d7f3da4dc1

refactor: replace set hook with validator hook in emailField model (#1971)
* refactor: replace set hook with validator hook in emailField model

* refactor: edit error message and remove repeated pre-validate hook

* fix: allow update if includeFormSummary is false

* refactor: eedit validator and add comments

src/app/models/field/emailField.ts
@@ -29,9 +29,19 @@ const createEmailFieldSchema = (): Schema<IEmailFieldSchema> => {
       includeFormSummary: {
         type: Boolean,
         default: false,
-        set: function (this: IEmailFieldSchema, v: boolean) {
-          // PDF response not allowed for encrypt forms
-          return this.parent().responseMode === ResponseMode.Encrypt ? false : v
+        validate: {
+          validator: function (this: IEmailFieldSchema, v: boolean) {
+            // always ok to set to false
+            return (
+              !v ||
+              // either true or false is okay if not in storage mode
+              this.parent().responseMode !== ResponseMode.Encrypt ||
+              // in storage mode, we can ignore this field if email confirmation is not enabled anyway
+              !this.autoReplyOptions.hasAutoReply
+            )
+          },
+          message:
+            'PDF response summaries are not allowed for email confirmations in storage mode forms',
         },
       },
     },
@@ -60,21 +70,6 @@ const createEmailFieldSchema = (): Schema<IEmailFieldSchema> => {
         message: 'There are one or more duplicate or invalid email domains.',
       },
     },
-  })
-
-  // PDF response not allowed if autoreply is set in encrypted forms. If
-  // autoreply is not set, then we don't care whether the pdf is set.
-  EmailFieldSchema.pre<IEmailFieldSchema>('validate', function (next) {
-    if (this.parent().responseMode === ResponseMode.Encrypt) {
-      const { hasAutoReply, includeFormSummary } = this.autoReplyOptions || {}
-      if (hasAutoReply && includeFormSummary) {
-        return next(
-          Error('Autoreply PDF is not allowed for storage mode forms'),
-        )
-      }
-    }
-
-    return next()
   })
 
   return EmailFieldSchema

commit 0a80a50a90dab74e71002587ef2c819f02a834fd

fix: allow duplicating email field with PDF to storage mode
Bug was due to regression caused in #1971 where the set hook was converted to a validator hook. Upon further examination, the set hook was needed to set includeFormSummary to false on encrypt-mode forms.

The pre-validate hook will always pass if storage mode forms's email.includeFormSummary is always set to true, and is thus redundant and kept deleted.

src/app/models/field/emailField.ts
@@ -29,19 +29,9 @@ const createEmailFieldSchema = (): Schema<IEmailFieldSchema> => {
       includeFormSummary: {
         type: Boolean,
         default: false,
-        validate: {
-          validator: function (this: IEmailFieldSchema, v: boolean) {
-            // always ok to set to false
-            return (
-              !v ||
-              // either true or false is okay if not in storage mode
-              this.parent().responseMode !== ResponseMode.Encrypt ||
-              // in storage mode, we can ignore this field if email confirmation is not enabled anyway
-              !this.autoReplyOptions.hasAutoReply
-            )
-          },
-          message:
-            'PDF response summaries are not allowed for email confirmations in storage mode forms',
+        set: function (this: IEmailFieldSchema, v: boolean) {
+          // Set to false if encrypt mode regardless of initial value.
+          return this.parent().responseMode === ResponseMode.Encrypt ? false : v
         },
       },
     },