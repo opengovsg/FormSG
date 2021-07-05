44fe7334c9537e4e5dc8f8c40f2dcff225b9b806
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-05 18:48:05 +0800

    feat(design-system): add Field/YesNo component and stories, update react-hook-form, add more breakpoints (#2210)
    * feat: add breakpoints according to design system
    
    * feat(YesNo): add YesNo presentation component
    
    * feat(storybook): add YesNo component story
    
    * feat(storybook): add composite field component for playground
    
    * fix(deps): upgrade react-hook-form to 7.8.8
    
    * feat: update impl of YesNo field to work with react-hook-form
    
    * feat: add storybook viewports for Chromatic snapshotting
    
    * fix: update breakpoints to return correct shape
    
    with predetermined keys
    
    * feat: update story hierarchy
    
    * feat(YesNo): allow component to be both controlled or uncontrolled
    
    also add jsdoc
    
    * feat: add isDisabled prop and styling, story
    
    YesNo now allowed for disabled state
    
    * feat: allow spacebar to select/deselect YesNo option
    
    * style: update disabled styling
    
    * feat: move YesNo styling into theme
    
    * style: update colors for YesNo for app theme
    
    * feat(story): update stories to account for colorTheme
    
    * feat: set id of yesno field to name prop
    
    allows clicking of labels with htmlFor props to focus on the yesno field
    
    * fix: update breakpoints again to use em due to bug in sorting
    
    see https://github.com/chakra-ui/chakra-ui/issues/3042
    
    * fix: update breakpoints from em to px
    
    * feat: add FieldColorScheme type for available field component colors
    
    * feat: update to new colours according to design
    
    * feat: add a11y on whether option was selected or unselected
    
    * feat: update YesNo theme according to design
    
    * feat: move option styles into theme
    
    * fix: update typing and use props.onChange
    
    input.onChange was basically props.onChange, give it the correct typings
    
    * feat: inline option container styling into theme
    
    * feat: move option icon styles into theme
    
    * ref: extract YesNoOption into its own file
    
    * fix: correctly set ref on both yes and no options
    
    * ref: rename input to inputProps, checkbox to checkboxProps
    
    * feat: update story to use Controller component instead of hook
    
    * ref: add helper fn to convert breakpoint string to px width
    
    * fix: lint merge commit
    
    * feat: add comments on why onChange is called in handleSelect

--- frontend/src/theme/components/index.ts
+++ frontend/src/theme/components/index.ts
@@ -1,6 +1,13 @@
+import { YESNO_THEME_KEY, YesNoField } from './Field/YesNo'
 import { Button } from './Button'
 import { Form } from './Form'
 import { FormError } from './FormError'
 import { Input } from './Input'
 
-export const components = { Button, Input, FormError, Form }
+export const components = {
+  Button,
+  Input,
+  FormError,
+  Form,
+  [YESNO_THEME_KEY]: YesNoField,
+}
