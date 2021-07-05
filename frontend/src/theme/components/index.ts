174134a5b4282eafb24c816133035580ec3b03ea
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-05 20:00:38 +0800

    feat(design-system): add FormLabel component and stories (#2240)
    * feat: update FormLabel default theme and add FormLabel component
    
    * feat: add Label composite components and story
    
    * feat(FormLabel): add index.ts, more a11y labels
    
    * fix: export FormLabel theme
    
    * feat: remove indent with question number on label
    
    * fix: question number alignment vs non-question alignment
    
    * feat: add description prop to FormLabel component

--- frontend/src/theme/components/index.ts
+++ frontend/src/theme/components/index.ts
@@ -2,12 +2,14 @@ import { YESNO_THEME_KEY, YesNoField } from './Field/YesNo'
 import { Button } from './Button'
 import { Form } from './Form'
 import { FormError } from './FormError'
+import { FormLabel } from './FormLabel'
 import { Input } from './Input'
 
 export const components = {
   Button,
   Input,
-  FormError,
   Form,
+  FormError,
+  FormLabel,
   [YESNO_THEME_KEY]: YesNoField,
 }
