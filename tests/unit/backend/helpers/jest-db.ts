064048c2438c6f0f3d4fdde06ebf214331d24bdd
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-07 15:27:39 +0800

    ref: remove old database.js and convert DbHandler into a class
    may also reduce flakeyness since each call to DbHandler is now encapsulated into a class

--- tests/unit/backend/helpers/jest-db.ts
+++ tests/unit/backend/helpers/jest-db.ts
@@ -1,5 +1,6 @@
 import { ObjectID } from 'bson'
-import mongoose from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server-core'
+import mongoose, { Mongoose } from 'mongoose'
 
 import getAgencyModel from 'src/app/models/agency.server.model'
 import {
@@ -18,221 +19,220 @@ import {
   ResponseMode,
 } from 'src/types'
 
-import MemoryDatabaseServer from 'tests/database'
+class DbHandler {
+  #mongod?: MongoMemoryServer
+  #connection?: Mongoose
 
-/**
- * Connect to the in-memory database
- */
-const connect = async (): Promise<typeof mongoose> => {
-  const dbUrl = await MemoryDatabaseServer.getConnectionString()
+  /**
+   * Connect to the in-memory database
+   */
+  async connect(): Promise<Mongoose> {
+    if (this.#connection) return this.#connection
 
-  const conn = await mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  return conn
-}
+    this.#mongod = await MongoMemoryServer.create({
+      binary: {
+        version: process.env.MONGO_BINARY_VERSION || '4.0.22',
+        checkMD5: true,
+      },
+      instance: {},
+    })
 
-/**
- * Disconnect all mongoose connections.
- */
-const closeDatabase = async (): Promise<void> => {
-  await mongoose.disconnect()
-  await MemoryDatabaseServer.stop()
-}
+    const dbUrl = this.#mongod.getUri()
 
-/**
- * Remove all the data for all db collections.
- */
-const clearDatabase = async (): Promise<void> => {
-  const collections = mongoose.connection.collections
+    this.#connection = await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
 
-  for (const key in collections) {
-    const collection = collections[key]
-    await collection.deleteMany({})
+    return this.#connection
   }
-}
 
-const clearCollection = async (collection: string): Promise<void> => {
-  await mongoose.connection.collections[collection].deleteMany({})
-}
+  /**
+   * Disconnect all mongoose connections.
+   */
+  async closeDatabase(): Promise<void> {
+    await mongoose.disconnect()
+    await this.#mongod?.stop()
+  }
 
-const insertAgency = async ({
-  mailDomain = 'test.gov.sg',
-  shortName = 'govtest',
-}: {
-  mailDomain?: string
-  shortName?: string
-} = {}): Promise<IAgencySchema> => {
-  const Agency = getAgencyModel(mongoose)
-  const agency = await Agency.create({
-    shortName,
-    fullName: `Government Testing Agency (${shortName})`,
-    emailDomain: [mailDomain],
-    logo: `/invalid-path/test-${shortName}.jpg`,
-  })
+  /**
+   * Remove all the data for all db collections.
+   */
+  async clearDatabase(): Promise<void> {
+    const collections = mongoose.connection.collections
 
-  return agency
-}
+    for (const key in collections) {
+      const collection = collections[key]
+      await collection.deleteMany({})
+    }
+  }
 
-const insertUser = async ({
-  agencyId,
-  userId,
-  mailDomain = 'test.gov.sg',
-  mailName = 'test',
-}: {
-  agencyId: ObjectID
-  userId?: ObjectID
-  mailName?: string
-  mailDomain?: string
-}): Promise<IUserSchema> => {
-  const User = getUserModel(mongoose)
+  async clearCollection(collection: string): Promise<void> {
+    await mongoose.connection.collections[collection].deleteMany({})
+  }
 
-  return User.create({
-    email: `${mailName}@${mailDomain}`,
-    _id: userId,
-    agency: agencyId,
-  })
-}
+  insertAgency = async ({
+    mailDomain = 'test.gov.sg',
+    shortName = 'govtest',
+  }: {
+    mailDomain?: string
+    shortName?: string
+  } = {}): Promise<IAgencySchema> => {
+    const Agency = getAgencyModel(mongoose)
+    const agency = await Agency.create({
+      shortName,
+      fullName: `Government Testing Agency (${shortName})`,
+      emailDomain: [mailDomain],
+      logo: `/invalid-path/test-${shortName}.jpg`,
+    })
 
-/**
- * Inserts a default agency and user document into their respective collections.
- * This is required to create a Form document, as Form pre-validation hook
- * requires a valid user to be found in the collection
- * @param userId if provided, the User document will be created with the given user id
- */
-const insertFormCollectionReqs = async ({
-  userId,
-  mailDomain = 'test.gov.sg',
-  mailName = 'test',
-  shortName = 'govtest',
-}: {
-  userId?: ObjectID
-  mailName?: string
-  mailDomain?: string
-  shortName?: string
-} = {}): Promise<{
-  agency: IAgencySchema
-  user: IUserSchema
-}> => {
-  const User = getUserModel(mongoose)
+    return agency
+  }
 
-  const agency = await insertAgency({ mailDomain, shortName })
-
-  const user = await User.create({
-    email: `${mailName}@${mailDomain}`,
-    _id: userId ?? new ObjectID(),
-    agency: agency.id,
-  })
-
-  return { agency, user }
-}
-
-const insertEmailForm = async ({
-  formId,
-  userId,
-  mailDomain = 'test.gov.sg',
-  mailName = 'test',
-  shortName = 'govtest',
-  formOptions = {},
-}: {
-  formId?: ObjectID
-  userId?: ObjectID
-  mailName?: string
-  mailDomain?: string
-  shortName?: string
-  formOptions?: Partial<IEmailForm>
-} = {}): Promise<{
-  form: IEmailFormSchema
-  user: IUserSchema
-  agency: IAgencySchema
-}> => {
-  const { user, agency } = await insertFormCollectionReqs({
+  insertUser = async ({
+    agencyId,
     userId,
-    mailDomain,
-    mailName,
-    shortName,
-  })
+    mailDomain = 'test.gov.sg',
+    mailName = 'test',
+  }: {
+    agencyId: ObjectID
+    userId?: ObjectID
+    mailName?: string
+    mailDomain?: string
+  }): Promise<IUserSchema> => {
+    const User = getUserModel(mongoose)
 
-  const EmailFormModel = getEmailFormModel(mongoose)
-  const form = await EmailFormModel.create({
-    title: 'example form title',
-    admin: user._id,
-    responseMode: ResponseMode.Email,
-    emails: [user.email],
-    _id: formId,
-    ...formOptions,
-  })
-
-  return {
-    form: form as IEmailFormSchema,
-    user,
-    agency,
+    return User.create({
+      email: `${mailName}@${mailDomain}`,
+      _id: userId,
+      agency: agencyId,
+    })
   }
-}
 
-const insertEncryptForm = async ({
-  formId,
-  userId,
-  mailDomain = 'test.gov.sg',
-  mailName = 'test',
-  shortName = 'govtest',
-  formOptions = {},
-}: {
-  formId?: ObjectID
-  userId?: ObjectID
-  mailName?: string
-  mailDomain?: string
-  shortName?: string
-  formOptions?: Partial<IEncryptedForm>
-} = {}): Promise<{
-  form: IEncryptedFormSchema
-  user: IUserSchema
-  agency: IAgencySchema
-}> => {
-  const { user, agency } = await insertFormCollectionReqs({
+  /**
+   * Inserts a default agency and user document into their respective collections.
+   * This is required to create a Form document, as Form pre-validation hook
+   * requires a valid user to be found in the collection
+   * @param userId if provided, the User document will be created with the given user id
+   */
+  insertFormCollectionReqs = async ({
     userId,
-    mailDomain,
-    mailName,
-    shortName,
-  })
+    mailDomain = 'test.gov.sg',
+    mailName = 'test',
+    shortName = 'govtest',
+  }: {
+    userId?: ObjectID
+    mailName?: string
+    mailDomain?: string
+    shortName?: string
+  } = {}): Promise<{
+    agency: IAgencySchema
+    user: IUserSchema
+  }> => {
+    const User = getUserModel(mongoose)
 
-  const EncryptFormModel = getEncryptedFormModel(mongoose)
+    const agency = await this.insertAgency({ mailDomain, shortName })
 
-  const form = await EncryptFormModel.create({
-    title: 'example form title',
-    admin: user._id,
-    responseMode: ResponseMode.Encrypt,
-    _id: formId,
-    publicKey: 'vuUYOfkrC7eiyqZ1OCZhMcjAvMQ7R4Z4zzDWB+og4G4=',
-    ...formOptions,
-  })
+    const user = await User.create({
+      email: `${mailName}@${mailDomain}`,
+      _id: userId ?? new ObjectID(),
+      agency: agency.id,
+    })
 
-  return {
-    form: form as IEncryptedFormSchema,
-    user,
-    agency,
+    return { agency, user }
   }
+
+  insertEmailForm = async ({
+    formId,
+    userId,
+    mailDomain = 'test.gov.sg',
+    mailName = 'test',
+    shortName = 'govtest',
+    formOptions = {},
+  }: {
+    formId?: ObjectID
+    userId?: ObjectID
+    mailName?: string
+    mailDomain?: string
+    shortName?: string
+    formOptions?: Partial<IEmailForm>
+  } = {}): Promise<{
+    form: IEmailFormSchema
+    user: IUserSchema
+    agency: IAgencySchema
+  }> => {
+    const { user, agency } = await this.insertFormCollectionReqs({
+      userId,
+      mailDomain,
+      mailName,
+      shortName,
+    })
+
+    const EmailFormModel = getEmailFormModel(mongoose)
+    const form = await EmailFormModel.create({
+      title: 'example form title',
+      admin: user._id,
+      responseMode: ResponseMode.Email,
+      emails: [user.email],
+      _id: formId,
+      ...formOptions,
+    })
+
+    return {
+      form: form as IEmailFormSchema,
+      user,
+      agency,
+    }
+  }
+
+  insertEncryptForm = async ({
+    formId,
+    userId,
+    mailDomain = 'test.gov.sg',
+    mailName = 'test',
+    shortName = 'govtest',
+    formOptions = {},
+  }: {
+    formId?: ObjectID
+    userId?: ObjectID
+    mailName?: string
+    mailDomain?: string
+    shortName?: string
+    formOptions?: Partial<IEncryptedForm>
+  } = {}): Promise<{
+    form: IEncryptedFormSchema
+    user: IUserSchema
+    agency: IAgencySchema
+  }> => {
+    const { user, agency } = await this.insertFormCollectionReqs({
+      userId,
+      mailDomain,
+      mailName,
+      shortName,
+    })
+
+    const EncryptFormModel = getEncryptedFormModel(mongoose)
+
+    const form = await EncryptFormModel.create({
+      title: 'example form title',
+      admin: user._id,
+      responseMode: ResponseMode.Encrypt,
+      _id: formId,
+      publicKey: 'vuUYOfkrC7eiyqZ1OCZhMcjAvMQ7R4Z4zzDWB+og4G4=',
+      ...formOptions,
+    })
+
+    return {
+      form: form as IEncryptedFormSchema,
+      user,
+      agency,
+    }
+  }
+
+  getFullFormById = async (formId: string): Promise<IPopulatedForm | null> =>
+    await getEmailFormModel(mongoose).getFullFormById(formId)
 }
 
-const getFullFormById = async (
-  formId: string,
-): Promise<IPopulatedForm | null> =>
-  await getEmailFormModel(mongoose).getFullFormById(formId)
-
-const dbHandler = {
-  connect,
-  closeDatabase,
-  clearDatabase,
-  insertAgency,
-  insertUser,
-  insertFormCollectionReqs,
-  clearCollection,
-  insertEmailForm,
-  insertEncryptForm,
-  getFullFormById,
-}
-
-export default dbHandler
+export default new DbHandler()
