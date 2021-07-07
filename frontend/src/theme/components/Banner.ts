d65597fcffe3224ae9b27c52158f25a68eab2a8f
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-07 16:42:40 +0800

    feat(design-system): add Banner component and stories (#2256)
    * chore: add react-markdown package to allow markdown parsing
    
    * feat(theme): add multistyle config theme for Banner component
    
    * feat(Banner): add component
    
    * feat(Banner): add Banner story
    
    * style: remove center align on banner icon and text
    
    * feat: remove usage of container from banner
    
    if we need to set maxW, we can do it in banner.styles.item
    
    * style: remove link hover color change according to design
    
    * feat: set default variant to info
    
    this prevents the edge case where undefined variant results in the rendered (info) banner not having a close button

---
+++ frontend/src/theme/components/Banner.ts
@@ -0,0 +1,85 @@
+import { ComponentMultiStyleConfig } from '@chakra-ui/theme'
+
+export type BannerVariant = 'info' | 'error' | 'warn'
+
+export const Banner: ComponentMultiStyleConfig = {
+  parts: ['banner', 'item', 'icon', 'link', 'close'],
+  baseStyle: {
+    item: {
+      display: 'flex',
+      py: ['1rem', '1rem', '0.5rem'],
+      px: '1rem',
+      justifyContent: 'space-between',
+    },
+    icon: {
+      fontSize: '1.5rem',
+      mr: '0.5rem',
+    },
+    close: {
+      padding: 0,
+      mr: '-0.75rem',
+      fontSize: '1.5rem',
+      my: '-0.25rem',
+      _focus: {
+        boxShadow: '0 0 0 2px var(--chakra-colors-primary-500)',
+      },
+    },
+  },
+  variants: {
+    info: {
+      banner: {
+        color: 'white',
+        bg: 'primary.500',
+      },
+      link: {
+        color: 'white',
+        _hover: {
+          color: 'white',
+        },
+        _focus: {
+          boxShadow: '0 0 0 2px var(--chakra-colors-white)',
+        },
+      },
+      close: {
+        _focus: {
+          boxShadow: '0 0 0 2px var(--chakra-colors-white)',
+        },
+      },
+    },
+    warn: {
+      banner: {
+        color: 'secondary.700',
+        bg: 'warning.500',
+      },
+      link: {
+        color: 'secondary.700',
+        _hover: {
+          color: 'secondary.700',
+        },
+      },
+    },
+    error: {
+      banner: {
+        color: 'white',
+        bg: 'danger.500',
+      },
+      link: {
+        color: 'white',
+        _hover: {
+          color: 'white',
+        },
+        _focus: {
+          boxShadow: '0 0 0 2px var(--chakra-colors-white)',
+        },
+      },
+      close: {
+        _focus: {
+          boxShadow: '0 0 0 2px var(--chakra-colors-white)',
+        },
+      },
+    },
+  },
+  defaultProps: {
+    variant: 'info',
+  },
+}
