47a2d35ba6c8e6c4d04322747acb52632650c5d1
chowyiyin <yiyin.chow@gmail.com>
2021-07-12 15:49:18 +0800

    feat: edit theme for tabs to add focus styles

--- frontend/src/theme/components/Tabs.ts
+++ frontend/src/theme/components/Tabs.ts
@@ -4,45 +4,50 @@ import {
   SystemStyleObjectRecord,
   ThemingPropsThunk,
 } from '@chakra-ui/react'
+import { getColor } from '@chakra-ui/theme-tools'
 import merge from 'lodash/merge'
 
-const variantLine: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> =
-  () => {
-    return {
-      root: {
-        w: 'auto',
-        p: '1.5rem',
+const variantLine: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = ({
+  theme,
+}) => {
+  return {
+    root: {
+      w: '100%',
+    },
+    tablist: {
+      overflowX: 'scroll',
+      borderColor: 'transparent',
+      /* Hide scrollbar for IE and Edge */
+      '&::MsOverflowStyle': 'none',
+      /* Hide scrollbar for Firefox */
+      '&::scrollbarWidth': 'none',
+      /* Hide scrollbar for Chrome, Safari and Opera */
+      '&::-webkit-scrollbar': {
+        display: 'none',
       },
-      tablist: {
-        overflowX: 'scroll',
-        borderColor: 'transparent',
-        /* Hide scrollbar for IE and Edge */
-        '&::MsOverflowStyle': 'none',
-        /* Hide scrollbar for Firefox */
-        '&::scrollbarWidth': 'none',
-        /* Hide scrollbar for Chrome, Safari and Opera */
-        '&::-webkit-scrollbar': {
-          display: 'none',
-        },
-      },
-      tabPanel: {
-        p: '4px',
-      },
-      tab: {
-        textTransform: 'uppercase',
-        p: '0.25rem',
-        marginBottom: '0.25rem',
+      p: '1.5625rem',
+    },
+    tab: {
+      textTransform: 'uppercase',
+      p: '0.25rem 0', // No horizontal padding to let bottom border be the same length as text
+      borderColor: 'transparent',
+      mx: '1rem',
+      transition: 'none',
+      _selected: {
         borderBottom: '0.125rem solid',
-        borderColor: 'transparent',
         _focus: {
-          boxShadow: 'none',
-        },
-        _selected: {
-          borderColor: 'currentColor',
+          // add horizontal padding and update margins to ensure text remains at the same position
+          p: '0.25rem',
+          ml: '0.75rem',
+          mr: '0.75rem',
+          mb: '0.125rem',
+          borderBottom: '0',
+          borderRadius: '0.25rem',
         },
       },
-    }
+    },
   }
+}
 
 const variantLight: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = (
   props,
@@ -57,6 +62,9 @@ const variantLight: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = (
       },
       _selected: {
         color: 'primary.500',
+        _focus: {
+          boxShadow: `0 0 0 2px ${getColor(props.theme, 'primary.500')}`,
+        },
       },
       _active: {
         bg: 'white',
@@ -83,6 +91,9 @@ const variantDark: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = (
       },
       _selected: {
         color: 'white',
+        _focus: {
+          boxShadow: '0 0 0 2px white',
+        },
       },
       transitionProperty: 'common',
       color: 'secondary.300',
@@ -94,8 +105,7 @@ export const Tabs: ComponentMultiStyleConfig = {
   parts: [],
   baseStyle: {
     tab: {
-      mr: '15%',
-      px: '4%',
+      mr: '2rem',
     },
   },
   sizes: {
