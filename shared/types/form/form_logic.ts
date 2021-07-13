f83c1ec114c87f6e39d4f847cf0e07c6244a6e7d
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-13 11:12:57 +0800

    feat: add shared base for form_logic types

---
+++ shared/types/form/form_logic.ts
@@ -0,0 +1,47 @@
+import { FormFieldDto } from '../field'
+
+export enum LogicConditionState {
+  Equal = 'is equals to',
+  Lte = 'is less than or equal to',
+  Gte = 'is more than or equal to',
+  Either = 'is either',
+}
+
+export enum LogicType {
+  ShowFields = 'showFields',
+  PreventSubmit = 'preventSubmit',
+}
+
+export enum LogicIfValue {
+  Number = 'number',
+  SingleSelect = 'single-select',
+  MultiSelect = 'multi-select',
+}
+
+export type FormCondition = {
+  field: FormFieldDto['_id']
+  state: LogicConditionState
+  value: string | number | string[] | number[]
+  ifValueType?: LogicIfValue
+}
+
+export type FormLogicBase = {
+  conditions: FormCondition[]
+}
+
+export interface ShowFieldLogic extends FormLogicBase {
+  logicType: LogicType.ShowFields
+  show: FormFieldDto['_id'][]
+}
+
+export interface PreventSubmitLogic extends FormLogicBase {
+  logicType: LogicType.ShowFields
+  preventSubmitMessage: string
+}
+
+export type FormLogic = ShowFieldLogic | PreventSubmitLogic
+
+export type ShowFieldLogicDto = ShowFieldLogic & { _id: string }
+export type PreventSubmitLogicDto = PreventSubmitLogic & { _id: string }
+
+export type LogicDto = FormLogic & { _id: string }
