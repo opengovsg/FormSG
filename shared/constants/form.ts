2fcbba3ccf1a2fb2da426dd5e6be1470e3814861
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-13 16:45:26 +0800

    feat: add shared form constants for form settings and public fields

---
+++ shared/constants/form.ts
@@ -0,0 +1,37 @@
+const PUBLIC_FORM_FIELDS = <const>[
+  'admin',
+  'authType',
+  'endPage',
+  'esrvcId',
+  'form_fields',
+  'form_logics',
+  'hasCaptcha',
+  'startPage',
+  'status',
+  'title',
+  '_id',
+  'responseMode',
+]
+
+export const EMAIL_PUBLIC_FORM_FIELDS = PUBLIC_FORM_FIELDS
+export const STORAGE_PUBLIC_FORM_FIELDS = <const>[
+  ...PUBLIC_FORM_FIELDS,
+  'publicKey',
+]
+
+const FORM_SETTINGS_FIELDS = <const>[
+  'authType',
+  'esrvcId',
+  'hasCaptcha',
+  'inactiveMessage',
+  'status',
+  'submissionLimit',
+  'title',
+  'webhook',
+]
+
+export const EMAIL_FORM_SETTINGS_FIELDS = <const>[
+  ...FORM_SETTINGS_FIELDS,
+  'emails',
+]
+export const STORAGE_FORM_SETTINGS_FIELDS = FORM_SETTINGS_FIELDS
