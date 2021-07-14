395149a7936d52d9680a93350235b080a495ba95
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-14 17:00:43 +0800

    fix: remove FieldBase props from Table column types

--- shared/types/field/tableField.ts
+++ shared/types/field/tableField.ts
@@ -2,10 +2,14 @@ import { FieldBase, BasicField } from './base'
 import { DropdownFieldBase } from './dropdownField'
 import { ShortTextFieldBase } from './shortTextField'
 
-export interface ShortTextColumnBase extends ShortTextFieldBase {
+// Column types do not have misc field base props.
+type WithFieldBasePropsRemoved<T> = Omit<T, keyof FieldBase>
+export interface ShortTextColumnBase
+  extends WithFieldBasePropsRemoved<ShortTextFieldBase> {
   columnType: BasicField.ShortText
 }
-export interface DropdownColumnBase extends DropdownFieldBase {
+export interface DropdownColumnBase
+  extends WithFieldBasePropsRemoved<DropdownFieldBase> {
   columnType: BasicField.Dropdown
 }
 
