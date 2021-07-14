4cda2d2f7397b87aac82f88c7402a0db162ea0dc
seaerchin <44049504+seaerchin@users.noreply.github.com>
2021-07-14 10:53:42 +0800

    feat(sms-limiting): sms counts retrieval (#2276)
    * feat(sms_count.server.model): adds new isOnboardedAcc key and scripts to add/delete the key
    
    * refactor(sms.types): adds property that checks if a sms is sent using formsg's credentials
    
    * refactor(sms_count.server.model): adds pre-save hook to automatically set property
    
    * feat(sms.types): updated typings of IVerificationsmscount
    
    * test(sms_count.server): fixed tests and added extra tests for isOnboardedAccount
    
    * feat(sms.service): adds new method to retrieve free sms counts of a user
    
    * test(sms.service): adds new unit tests for sms count  retrieval
    
    * test(sms_count.server.model): updated test case for a client using default credentials
    
    * feat(sms.controller): added new sms controller iwth method to retrieve counts and msgsrvcid
    
    fix(sms.controller.ts): added mapRouteError for sms controller
    
    * feat(routes): exposes new route at sms/:userId/:formId
    
    * fix(sms.controller): removed sms count retrieval requiring the userId as it sohuld be for the admin
    
    * refactor(sms): count retrieval no longer required userId
    
    * test(sms.controller): added unit tests for controller method to retrieve counts
    
    * fix(sms.controller): changed to relative imports in spec to avoid e2e tests breaking
    
    * refactor(sms_count): sms service wraps model count documents call
    
    * test(sms.service): reworked tests to account for mocking of db
    
    * refactor(sms.controller): removed extra middleware from sms controller
    
    * test(sms.controller.spec): renamed sms controller's exposed method in tests
    
    * refactor(sms.routes): removed unused sms route
    
    * refactor(sms.controller): changed counts retrieval to be solely focused on counts
    
    * feat(admin-forms.form.routes): adds new route for retrieving free sms ocunts for admin of a form
    
    * refactor(sms_count): shifts default msgsrvcid to be from config, helper method defined in test
    
    * test(sms.controller.spec): updated tests to use mocks
    
    * test(sms_count.server.model.spec): updated db tests to use smsConfig
    
    * fix(v3.routes): removed extra import statement
    
    * refactor(types): renamed smscounts meta and shifted it to api
    
    renamed to smsCountsDto because it is used to transfer data b/w fe and be. against giving it a more
    specific name because this is a type signifying a transfer of sms count data. context for what this
    sms counts actually means should be given by the caller
    
    * test(sms_count.server.model.spec): updated test to use sms_config
    
    * docs(sms): updated docs for sms counts retrieval in controller and route
    
    * test(sms.service.spec): made tests more explicit
    
    * refactor(admin-form.controller): shifts over handler for count retrieval from sms to admin-form
    
    * test(admin-form.controller.spec): shifts over associated tests
    
    * refactor(sms_count.server.model.spec): changed tests structure
    
    * chore(sms.util): removed unneeded method
    
    * refactor(sms.types): added in docs for retrieveFreeSmsCounts
    
    * fix(admin-forms.form.routes): removed erroneous import
    
    * fix(sms.service): adds error logging

--- src/app/modules/form/admin-form/admin-form.controller.ts
+++ src/app/modules/form/admin-form/admin-form.controller.ts
@@ -50,6 +50,7 @@ import {
 import { DeserializeTransform } from '../../../../types/utils'
 import { createLoggerWithLabel } from '../../../config/logger'
 import MailService from '../../../services/mail/mail.service'
+import * as SmsService from '../../../services/sms/sms.service'
 import { createReqMeta } from '../../../utils/request'
 import * as AuthService from '../../auth/auth.service'
 import {
@@ -2465,3 +2466,47 @@ export const handleUpdateStartPage = [
   }),
   _handleUpdateStartPage,
 ] as ControllerHandler[]
+
+/**
+ * Handler to retrieve the free sms counts used by a form's administrator
+ * This is the controller for GET /admin/forms/:formId/verified-sms/count/free
+ * @param formId The id of the form to retrieve the free sms counts for
+ * @returns 200 with free sms counts when successful
+ * @returns 404 when the formId is not found in the database
+ * @returns 500 when a database error occurs during retrieval
+ */
+export const handleGetFreeSmsCountForFormAdmin: ControllerHandler<
+  {
+    formId: string
+  },
+  ErrorDto | number
+> = (req, res) => {
+  const { formId } = req.params
+  const logMeta = {
+    action: 'handleGetFreeSmsCountForFormAdmin',
+    ...createReqMeta(req),
+    formId,
+  }
+
+  // Step 1: Check that the form exists
+  return (
+    FormService.retrieveFormById(formId)
+      // Step 2: Retrieve the free sms count
+      .andThen(({ admin }) => {
+        return SmsService.retrieveFreeSmsCounts(String(admin))
+      })
+      // Step 3: Map/MapErr accordingly
+      .map((freeSmsCountForAdmin) =>
+        res.status(StatusCodes.OK).json(freeSmsCountForAdmin),
+      )
+      .mapErr((error) => {
+        logger.error({
+          message: 'Error while retrieving sms counts for user',
+          meta: logMeta,
+          error,
+        })
+        const { statusCode, errorMessage } = mapRouteError(error)
+        return res.status(statusCode).json({ message: errorMessage })
+      })
+  )
+}
