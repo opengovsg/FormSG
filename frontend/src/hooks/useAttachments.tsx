df602ce990b00198c4e6ed01b21227c4c3660439
seaerchin <jer005@e.ntu.edu.sg>
2021-07-27 12:04:33 +0800

    feat(hooks): added useAttachments hook

---
+++ frontend/src/hooks/useAttachments.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react'
+import {
+  DropzoneOptions as ReactDropzoneOptions,
+  DropzoneState as ReactDropzoneState,
+  FileRejection,
+  useDropzone,
+} from 'react-dropzone'
+
+// Omit props that remove accessibility as we aim to target
+// both mobile and desktop
+type DropzoneOptions = Omit<
+  ReactDropzoneOptions,
+  'noClick' | 'noKeyboard' | 'noDrag'
+>
+
+export type DropzoneState = Pick<
+  ReactDropzoneState,
+  | 'acceptedFiles'
+  | 'fileRejections'
+  | 'getInputProps'
+  | 'getRootProps'
+  | 'inputRef'
+  | 'rootRef'
+> & {
+  reset: () => void
+  deleteFile: (file: File) => void
+}
+
+// This hook layers over the useDropzone hook.
+export const useAttachments = ({
+  maxFiles = 1,
+  ...rest
+}: DropzoneOptions): DropzoneState => {
+  const {
+    inputRef,
+    rootRef,
+    acceptedFiles: actualAcceptedFiles,
+    fileRejections: actualFileRejections,
+    getRootProps,
+    getInputProps,
+  } = useDropzone({
+    maxFiles,
+    ...rest,
+  })
+
+  const [acceptedFiles, setAcceptedFiles] =
+    useState<File[]>(actualAcceptedFiles)
+
+  const [fileRejections, setFileRejections] =
+    useState<FileRejection[]>(actualFileRejections)
+
+  const reset = () => {
+    setAcceptedFiles([])
+    setAcceptedFiles([])
+  }
+
+  const deleteFile = (file: File) => {
+    const newAcceptedFiles = acceptedFiles.filter((f) => f !== file)
+    setAcceptedFiles(newAcceptedFiles)
+  }
+
+  // Listen on file/errors so that exposed state is consistent with internal state
+  useEffect(() => {
+    setAcceptedFiles(actualAcceptedFiles)
+    setFileRejections(actualFileRejections)
+  }, [actualAcceptedFiles, actualFileRejections])
+
+  return {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps,
+    inputRef,
+    rootRef,
+    reset,
+    deleteFile,
+  }
+}
