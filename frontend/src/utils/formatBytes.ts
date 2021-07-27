8b6448e6bc96a283916542574a3a3d5c92566776
seaerchin <jer005@e.ntu.edu.sg>
2021-07-27 17:41:32 +0800

    refactor(utils/formatbytes): minor change to account for 0/1 bytes

--- frontend/src/utils/formatBytes.ts
+++ frontend/src/utils/formatBytes.ts
@@ -1,6 +1,8 @@
 // Retrieved from: https://gist.github.com/zentala/1e6f72438796d74531803cc3833c039c
-export const formatBytes = (bytes: number, precision?: number): string => {
-  if (bytes === 0) return '0 Bytes'
+export const formatBytes = (bytes?: number, precision?: number): string => {
+  // Singular form required...
+  if (bytes === 0 || bytes === 1) return `${bytes} Byte`
+  if (!bytes || bytes < 0) return '-'
   const k = 1024
   const decimalPoints = precision || 2
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
