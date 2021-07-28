cb945dbe23c2c4fbf55ab1b00d40521154c68d2d
Kar Rui Lau <karrui.lau@gmail.com>
2021-07-28 13:14:56 +0800

    feat: add component stories

--- frontend/src/components/Pagination/Pagination.stories.tsx
+++ frontend/src/components/Pagination/Pagination.stories.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { Meta, Story } from '@storybook/react'
 
+import { viewports } from '~utils/storybook'
+
 import { Pagination, PaginationProps } from './Pagination'
 
 export default {
@@ -29,5 +31,58 @@ Default.args = {
   currentPage: 5,
   totalCount: 1000,
   pageSize: 10,
+}
+
+export const SiblingCountEquals2 = Template.bind({})
+SiblingCountEquals2.args = {
+  currentPage: 31,
+  totalCount: 1000,
+  pageSize: 10,
+  siblingCount: 2,
+}
+
+export const LessThan7Pages = Template.bind({})
+LessThan7Pages.args = {
+  currentPage: 1,
+  totalCount: 60,
+  pageSize: 10,
   siblingCount: 1,
+}
+
+export const StartOf100Pages = Template.bind({})
+StartOf100Pages.args = {
+  currentPage: 1,
+  totalCount: 1000,
+  pageSize: 10,
+  siblingCount: 1,
+}
+
+export const EndOf100Pages = Template.bind({})
+EndOf100Pages.args = {
+  currentPage: 100,
+  totalCount: 1000,
+  pageSize: 10,
+  siblingCount: 1,
+}
+
+export const MiddleOf100Pages = Template.bind({})
+MiddleOf100Pages.args = {
+  currentPage: 31,
+  totalCount: 1000,
+  pageSize: 10,
+  siblingCount: 1,
+}
+
+export const Mobile = Template.bind({})
+Mobile.args = {
+  currentPage: 31,
+  totalCount: 1000,
+  pageSize: 10,
+  siblingCount: 1,
+}
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+  chromatic: { viewports: [viewports.xs] },
 }
