4e5cbd0c1dd6af9e83712bc4a82f52409172ae3b
Antariksh <antarikshmahajan@gmail.com>
2021-08-06 12:20:33 +0800

    fix(Checkbox): use forwardRef

--- frontend/src/components/Checkbox/Checkbox.tsx
+++ frontend/src/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,7 @@
 import {
   Checkbox as ChakraCheckbox,
   CheckboxProps as ChakraCheckboxProps,
+  forwardRef,
   Icon,
   useMultiStyleConfig,
 } from '@chakra-ui/react'
@@ -13,28 +14,27 @@ export interface CheckboxProps extends ChakraCheckboxProps {
   colorScheme?: FieldColorScheme
 }
 
-export const Checkbox = ({
-  children,
-  colorScheme = 'primary',
-  ...props
-}: CheckboxProps): JSX.Element => {
-  const { icon: iconStyles } = useMultiStyleConfig(CHECKBOX_THEME_KEY, {
-    size: props.size,
-  })
-  return (
-    <ChakraCheckbox
-      icon={
-        <Icon
-          as={BxCheckAnimated}
-          __css={iconStyles}
-          // This prop needs to be passed explicitly for animations
-          isChecked={props.isChecked}
-        />
-      }
-      colorScheme={colorScheme}
-      {...props}
-    >
-      {children}
-    </ChakraCheckbox>
-  )
-}
+export const Checkbox = forwardRef<CheckboxProps, 'input'>(
+  ({ children, colorScheme = 'primary', ...props }, ref) => {
+    const { icon: iconStyles } = useMultiStyleConfig(CHECKBOX_THEME_KEY, {
+      size: props.size,
+    })
+    return (
+      <ChakraCheckbox
+        icon={
+          <Icon
+            as={BxCheckAnimated}
+            __css={iconStyles}
+            // This prop needs to be passed explicitly for animations
+            isChecked={props.isChecked}
+          />
+        }
+        colorScheme={colorScheme}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </ChakraCheckbox>
+    )
+  },
+)
