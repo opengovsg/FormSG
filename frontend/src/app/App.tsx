ec655bdc5b4582925d13b2e7f3aea8c33f9bab64
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-10 14:54:22 +0800

    feat: use AnalyticsService to test API calls

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,28 @@
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { theme } from '~theme/index'
-import Button from '~components/Button'
+import { getLandingPageStatistics } from '~services/AnalyticsService'
+
+// Create a client
+const queryClient = new QueryClient()
 
 export const App = (): JSX.Element => (
-  <ChakraProvider theme={theme} resetCSS>
-    Hello world <Button>aaaa</Button>
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <ChakraProvider theme={theme} resetCSS>
+      <InnerApp />
+    </ChakraProvider>
+  </QueryClientProvider>
 )
+
+export const InnerApp = () => {
+  const { data, isLoading } = useQuery('stats', getLandingPageStatistics)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return <div>{JSON.stringify(data)}</div>
+}
