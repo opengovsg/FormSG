6055760decfdcbecf4c11905d26ef627efe10df3
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-11 12:50:54 +0800

    feat: add business logic to OtpForm component

--- frontend/src/pages/login/components/OtpForm.tsx
+++ frontend/src/pages/login/components/OtpForm.tsx
@@ -1,56 +1,59 @@
 import { useCallback } from 'react'
 import { useForm } from 'react-hook-form'
-import { FormControl } from '@chakra-ui/react'
+import { ButtonGroup, FormControl } from '@chakra-ui/react'
 
 import Button from '~components/Button'
 import FormErrorMessage from '~components/FormControl/FormErrorMessage'
 import FormLabel from '~components/FormControl/FormLabel'
 import Input from '~components/Input'
 
-type OtpFormInputs = {
+import { ResendOtpButton } from './ResendOtpButton'
+
+export type OtpFormInputs = {
   otp: string
 }
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+interface OtpFormProps {
+  onSubmit: (inputs: OtpFormInputs) => Promise<void>
+  onResendOtp: () => Promise<void>
+}
 
-// interface LoginFormProps {
-//   onLogin: (values: LoginFormInputs) => void
-// }
-
-export const OtpForm = (): JSX.Element => {
-  const { handleSubmit, register, formState } = useForm<OtpFormInputs>()
+export const OtpForm = ({
+  onSubmit,
+  onResendOtp,
+}: OtpFormProps): JSX.Element => {
+  const { handleSubmit, register, formState, setError } =
+    useForm<OtpFormInputs>()
 
   const validateOtp = useCallback(
     (value: string) => value.length === 6 || 'Please enter a 6 digit OTP.',
     [],
   )
 
-  /**
-   * Submit phone number to backend for OTP to be sent to the user.
-   */
-  const onSubmit = async ({ otp }: OtpFormInputs) => {
-    // Set mobile number for parent component so next stage is triggered.
-    await sleep(2000)
-    if (otp === '123456') {
-      alert(otp)
-    } else {
-      alert('There is an error')
-    }
+  const onSubmitForm = async (inputs: OtpFormInputs) => {
+    return onSubmit(inputs).catch((e) => {
+      setError('otp', { type: 'server', message: e.message })
+    })
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmitForm)}>
       <FormControl isInvalid={!!formState.errors.otp} mb="2.5rem">
         <FormLabel isRequired htmlFor="otp">
           Enter 6 digit OTP sent to your email
         </FormLabel>
         <Input
           type="text"
+          maxLength={6}
           inputMode="numeric"
           autoComplete="one-time-code"
           autoFocus
           {...register('otp', {
-            required: 'OTP is required',
+            required: 'OTP is required.',
+            pattern: {
+              value: /^[0-9\b]+$/,
+              message: 'Only numbers are allowed.',
+            },
             validate: validateOtp,
           })}
         />
@@ -58,9 +61,12 @@ export const OtpForm = (): JSX.Element => {
           <FormErrorMessage>{formState.errors.otp.message}</FormErrorMessage>
         )}
       </FormControl>
-      <Button isLoading={formState.isSubmitting} type="submit">
-        Sign in
-      </Button>
+      <ButtonGroup spacing="1.5rem">
+        <Button isLoading={formState.isSubmitting} type="submit">
+          Sign in
+        </Button>
+        <ResendOtpButton onResendOtp={onResendOtp} />
+      </ButtonGroup>
     </form>
   )
 }
