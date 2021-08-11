c3da64bcd93bf56b845411689043ab96fcdd5dee
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-11 12:36:14 +0800

    feat: add useLocalStorage hook

---
+++ frontend/src/constants/localStorage.ts
@@ -0,0 +1,14 @@
+/**
+ * Constants relating to localStorage.
+ */
+
+/**
+ * Key to store whether the admin is logged in in localStorage.
+ */
+export const LOGGED_IN_KEY = 'is-logged-in'
+
+/**
+ * Event name to be used when emitting event to indicate that localStorage has
+ * been modified.
+ */
+export const LOCAL_STORAGE_EVENT = 'local-storage'
