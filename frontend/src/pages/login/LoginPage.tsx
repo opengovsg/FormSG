e32bb0c6a7eb10862902de3bf7fa894ef977de1b
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-12 18:22:56 +0800

    feat: add responsive styling to fit design

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,9 +1,19 @@
-import { FC, useState } from 'react'
-import { Box, Grid, GridItem, Text, Wrap } from '@chakra-ui/react'
+import { FC, useMemo, useState } from 'react'
+import {
+  Box,
+  chakra,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+  useBreakpointValue,
+  Wrap,
+} from '@chakra-ui/react'
 
-import { ReactComponent as BrandLogo } from '~assets/svgs/brand/brand-hort-colour.svg'
-import { ReactComponent as LoginImage } from '~assets/svgs/img-buildscratch.svg'
+import { ReactComponent as BrandLogoSvg } from '~assets/svgs/brand/brand-hort-colour.svg'
+import { ReactComponent as LoginImageSvg } from '~assets/svgs/img-buildscratch.svg'
 import { useAuth } from '~contexts/AuthContext'
+import Footer from '~components/Footer'
 import Link from '~components/Link'
 
 import { LoginForm, LoginFormInputs } from './components/LoginForm'
@@ -13,9 +23,27 @@ export type LoginOtpData = {
   email: string
 }
 
+const BrandLogo = chakra(BrandLogoSvg)
+const LoginImage = chakra(LoginImageSvg)
+
 export const LoginPage: FC = () => {
   const [email, setEmail] = useState<string>()
   const { sendLoginOtp, verifyLoginOtp } = useAuth()
+
+  const currentYear = new Date().getFullYear()
+  const isDesktop = useBreakpointValue({ base: false, xs: false, lg: true })
+  const isTablet = useBreakpointValue({ base: false, xs: false, md: true })
+
+  const footerLinks = useMemo(
+    () => [
+      { label: 'Contact Us', href: '' },
+      { label: 'Guide', href: '' },
+      { label: 'Privacy', href: '' },
+      { label: 'Terms of Use', href: '' },
+      { label: 'Report Vulnerability', href: '' },
+    ],
+    [],
+  )
 
   const handleSendOtp = async ({ email }: LoginFormInputs) => {
     await sendLoginOtp(email)
@@ -41,108 +69,132 @@ export const LoginPage: FC = () => {
   }
 
   return (
-    <Box
-      px={{ base: 0, lg: '2rem' }}
-      // Used to fill in the left padding gap.
-      bg={{
-        base: 'initial',
-        lg: 'linear-gradient(90deg, var(--chakra-colors-primary-500) 50%, white 0)',
-      }}
-    >
-      <Grid
-        minH="100vh"
-        maxW="90rem"
-        margin="auto"
-        templateAreas={{
-          base: `'login' 'sidebar' 'links' 'copy'`,
-          lg: `'sidebar login' 'copy links'`,
+    <Flex flexDir="column" minH="100vh">
+      <Box
+        flexGrow={1}
+        px={{ base: '1.5rem', md: '5.5rem', lg: 0 }}
+        bg={{
+          base: 'initial',
+          md: 'linear-gradient(180deg, var(--chakra-colors-primary-500) 20.625rem, white 0)',
+          lg: 'linear-gradient(90deg, var(--chakra-colors-primary-500) 42%, white 0)',
         }}
-        templateRows={{ lg: '1fr auto' }}
-        templateColumns={{ lg: '5fr 7fr' }}
-        gap="1rem"
       >
-        <GridItem
-          gridArea="sidebar"
-          bg="primary.500"
-          px={{ base: '1.5rem', lg: '5rem' }}
-          py={{ base: '1.5rem', lg: '6rem' }}
-          d="flex"
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
+        <Grid
+          minH={{ base: 'initial', lg: '100vh' }}
+          maxW="90rem"
+          margin="auto"
+          templateAreas={{
+            base: `'login'`,
+            md: `'sidebar' 'login'`,
+            lg: `'sidebar login' 'copy links'`,
+          }}
+          templateRows={{ lg: '1fr auto' }}
+          templateColumns={{ lg: '5fr 7fr' }}
         >
-          <Text
-            display={{ base: 'none', lg: 'initial' }}
-            textStyle="display-2"
-            color="white"
-            mb="2.5rem"
-          >
-            Build secure government forms in minutes
-          </Text>
-          <Box maxW="28rem" w="100%">
-            <LoginImage aria-hidden />
-          </Box>
-        </GridItem>
-
-        <GridItem
-          h="100%"
-          bg="white"
-          gridArea="login"
-          px={{ base: '1.5rem', lg: '7.25rem' }}
-          py={{ base: '1.5rem', lg: '4rem' }}
-          d="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Box
-            maxW="28rem"
-            w="100%"
-            minH={{ base: 'auto', lg: '24rem' }}
-            pt="2rem"
-          >
-            <Box mb="1.5rem" w="10.5rem">
-              <BrandLogo title="FormSG logo" />
-            </Box>
-            {!email ? (
-              <LoginForm onSubmit={handleSendOtp} />
-            ) : (
-              <OtpForm
-                onSubmit={handleVerifyOtp}
-                onResendOtp={handleResendOtp}
+          {isTablet && (
+            <GridItem
+              d="flex"
+              gridArea="sidebar"
+              bg={{ base: 'transparent', lg: 'primary.500' }}
+              px={{ base: '1.5rem', lg: '5rem' }}
+              pt={{ base: '1.5rem', md: '4rem', lg: '6rem' }}
+              pb={{ lg: '6rem' }}
+              flexDir="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Text
+                display={{ base: 'none', lg: 'initial' }}
+                textStyle="display-2"
+                color="white"
+                mb="2.5rem"
+              >
+                Build secure government forms in minutes
+              </Text>
+              <LoginImage
+                aria-hidden
+                maxW={{ base: '22rem', lg: '28rem' }}
+                w="100%"
               />
-            )}
-          </Box>
-        </GridItem>
-        <GridItem
-          gridArea="copy"
-          bg="primary.500"
-          // Remove grid gap on the y-axis
-          mt="-1rem"
-          px={{ base: '1.5rem', lg: '5rem' }}
-          pt="0.5rem"
-          pb="4rem"
-        >
-          <Text textStyle="legal" color="white">
-            © 2021 Open Government Products, GovTech Singapore
-          </Text>
-        </GridItem>
-        <GridItem
-          bg="white"
-          mt="-1rem"
-          px={{ base: '1.5rem', lg: '7.25rem' }}
-          pt="0.5rem"
-          pb="4rem"
-          gridArea="links"
-        >
-          <Wrap shouldWrapChildren textStyle="legal" spacing="1.5rem">
-            <Link>Contact Us</Link>
-            <Link>Guide</Link>
-            <Link>Privacy</Link>
-            <Link>Terms of Use</Link>
-            <Link>Report Vulnerability</Link>
-          </Wrap>
-        </GridItem>
-      </Grid>
-    </Box>
+            </GridItem>
+          )}
+
+          <GridItem
+            h="100%"
+            gridArea="login"
+            px={{ base: 0, lg: '7.25rem' }}
+            py="4rem"
+            d="flex"
+            alignItems={{ base: 'initial', lg: 'center' }}
+          >
+            <Box
+              maxW={{ base: '100%', lg: '28rem' }}
+              w="100%"
+              minH={{ base: 'auto', lg: '24rem' }}
+            >
+              <Box pt={{ base: 0, lg: '2rem' }} mb={{ base: '2.5rem', lg: 0 }}>
+                <BrandLogo
+                  title="FormSG logo"
+                  mb={{ base: '0.75rem', lg: '1.5rem' }}
+                  h={{ base: '1.5rem', lg: '2rem' }}
+                />
+                <Text
+                  textStyle="h4"
+                  color="secondary.500"
+                  display={{ base: 'initial', lg: 'none' }}
+                >
+                  Build secure government forms in minutes
+                </Text>
+              </Box>
+              {!email ? (
+                <LoginForm onSubmit={handleSendOtp} />
+              ) : (
+                <OtpForm
+                  onSubmit={handleVerifyOtp}
+                  onResendOtp={handleResendOtp}
+                />
+              )}
+            </Box>
+          </GridItem>
+          {isDesktop && (
+            <>
+              <GridItem
+                gridArea="copy"
+                bg={{ base: 'transparent', lg: 'primary.500' }}
+                px={{ base: '1.5rem', lg: '5rem' }}
+                pt="0.5rem"
+                pb="4rem"
+              >
+                <Text textStyle="legal" color="white">
+                  © {currentYear} Open Government Products, GovTech Singapore
+                </Text>
+              </GridItem>
+              <GridItem
+                px={{ base: '1.5rem', lg: '7.25rem' }}
+                pt="0.5rem"
+                pb="4rem"
+                display={{ base: 'none', lg: 'flex' }}
+                gridArea="links"
+              >
+                <Wrap shouldWrapChildren textStyle="legal" spacing="1.5rem">
+                  {footerLinks.map(({ label, href }, index) => (
+                    <Link variant="standalone" key={index} href={href}>
+                      {label}
+                    </Link>
+                  ))}
+                </Wrap>
+              </GridItem>
+            </>
+          )}
+        </Grid>
+      </Box>
+      {!isDesktop && (
+        <Footer
+          appName="Form"
+          tagline="Build secure government forms in minutes"
+          footerLinks={footerLinks}
+        />
+      )}
+    </Flex>
   )
 }
