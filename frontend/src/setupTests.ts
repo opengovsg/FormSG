b769c699f97b7a5cececf5214a61b8a63b543c8b
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-18 16:17:29 +0800

    chore: set up test linter, setup, and add test packages

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+import { setGlobalConfig } from '@storybook/testing-react'
+
+import * as globalStorybookConfig from '../.storybook/preview' // path of your preview.js file
+
+setGlobalConfig(globalStorybookConfig)
