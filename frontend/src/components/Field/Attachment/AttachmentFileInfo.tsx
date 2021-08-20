4b5663b82fb46f2e2421b19902af92497365653b
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-20 17:56:05 +0800

    feat: extract AttachmentFileInfo to own component for cleanliness

---
+++ frontend/src/components/Field/Attachment/AttachmentFileInfo.tsx
@@ -0,0 +1,42 @@
+import { useMemo } from 'react'
+import { BiTrash } from 'react-icons/bi'
+import { Flex, Text } from '@chakra-ui/react'
+
+import IconButton from '~components/IconButton'
+
+import { getReadableFileSize } from './utils/getReadableFileSize'
+
+export interface AttachmentFileInfoProps {
+  file: File
+  handleRemoveFile: () => void
+}
+
+export const AttachmentFileInfo = ({
+  file,
+  handleRemoveFile,
+}: AttachmentFileInfoProps): JSX.Element => {
+  const readableFileSize = useMemo(
+    () => getReadableFileSize(file.size),
+    [file.size],
+  )
+
+  return (
+    <Flex justify="space-between" bg="primary.100" py="0.875rem" px="1rem">
+      <Flex flexDir="column">
+        <Text textStyle="subhead-1" color="secondary.500">
+          {file.name}
+        </Text>
+        <Text textStyle="caption-1" color="secondary.400">
+          {readableFileSize}
+        </Text>
+      </Flex>
+      <IconButton
+        variant="clear"
+        colorScheme="danger"
+        aria-label="remove file"
+        icon={<BiTrash />}
+        onClick={handleRemoveFile}
+      />
+    </Flex>
+  )
+}
