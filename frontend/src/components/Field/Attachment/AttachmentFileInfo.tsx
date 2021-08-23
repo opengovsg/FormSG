1c785e46eec1d43d474a01626abe1f8154dbb56d
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-23 15:59:33 +0800

    feat(AttachmentFileInfo): accept forwardedRef to focus after selection

--- frontend/src/components/Field/Attachment/AttachmentFileInfo.tsx
+++ frontend/src/components/Field/Attachment/AttachmentFileInfo.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { BiTrash } from 'react-icons/bi'
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, forwardRef, Text } from '@chakra-ui/react'
 
 import IconButton from '~components/IconButton'
 
@@ -11,32 +11,37 @@ export interface AttachmentFileInfoProps {
   handleRemoveFile: () => void
 }
 
-export const AttachmentFileInfo = ({
-  file,
-  handleRemoveFile,
-}: AttachmentFileInfoProps): JSX.Element => {
-  const readableFileSize = useMemo(
-    () => getReadableFileSize(file.size),
-    [file.size],
-  )
+export const AttachmentFileInfo = forwardRef<AttachmentFileInfoProps, 'div'>(
+  ({ file, handleRemoveFile }, ref) => {
+    const readableFileSize = useMemo(
+      () => getReadableFileSize(file.size),
+      [file.size],
+    )
 
-  return (
-    <Flex justify="space-between" bg="primary.100" py="0.875rem" px="1rem">
-      <Flex flexDir="column">
-        <Text textStyle="subhead-1" color="secondary.500">
-          {file.name}
-        </Text>
-        <Text textStyle="caption-1" color="secondary.400">
-          {readableFileSize}
-        </Text>
+    return (
+      <Flex
+        ref={ref}
+        justify="space-between"
+        bg="primary.100"
+        py="0.875rem"
+        px="1rem"
+      >
+        <Flex flexDir="column">
+          <Text textStyle="subhead-1" color="secondary.500">
+            {file.name}
+          </Text>
+          <Text textStyle="caption-1" color="secondary.500">
+            {readableFileSize}
+          </Text>
+        </Flex>
+        <IconButton
+          variant="clear"
+          colorScheme="danger"
+          aria-label="remove file"
+          icon={<BiTrash />}
+          onClick={handleRemoveFile}
+        />
       </Flex>
-      <IconButton
-        variant="clear"
-        colorScheme="danger"
-        aria-label="remove file"
-        icon={<BiTrash />}
-        onClick={handleRemoveFile}
-      />
-    </Flex>
-  )
-}
+    )
+  },
+)
