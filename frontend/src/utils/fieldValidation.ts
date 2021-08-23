9add238dde4405c022213fd79ea5b07494e0e02e
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-23 17:48:25 +0800

    feat(fieldValidation): add createShortTextValidationRules util fn

--- frontend/src/utils/fieldValidation.ts
+++ frontend/src/utils/fieldValidation.ts
@@ -3,8 +3,13 @@
  * to the field schema.
  */
 import { RegisterOptions } from 'react-hook-form'
+import simplur from 'simplur'
 
-import { FieldBase } from '~shared/types/field'
+import {
+  FieldBase,
+  ShortTextFieldBase,
+  TextSelectedValidation,
+} from '~shared/types/field'
 
 export const createBaseValidationRules = (
   schema: FieldBase,
@@ -13,6 +18,38 @@ export const createBaseValidationRules = (
     required: {
       value: schema.required,
       message: 'This field is required',
+    },
+  }
+}
+
+export const createShortTextValidationRules = (
+  schema: ShortTextFieldBase,
+): RegisterOptions => {
+  const { selectedValidation, customVal } = schema.ValidationOptions
+  return {
+    ...createBaseValidationRules(schema),
+    validate: (val?: string) => {
+      if (!val || !customVal) return true
+
+      const currLen = val.length
+
+      switch (selectedValidation) {
+        case TextSelectedValidation.Exact:
+          return (
+            currLen === customVal ||
+            simplur`Please enter ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+        case TextSelectedValidation.Minimum:
+          return (
+            currLen >= customVal ||
+            simplur`Please enter at least ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+        case TextSelectedValidation.Maximum:
+          return (
+            currLen <= customVal ||
+            simplur`Please enter at most ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+      }
     },
   }
 }
