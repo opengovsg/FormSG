bd8e3f68af452fc916e37a0035835052368e1029
Antariksh <antarikshmahajan@gmail.com>
2021-08-24 13:15:04 +0800

    style(Tabs): clean up CSS; enable transparent background

--- frontend/src/theme/components/Tabs.ts
+++ frontend/src/theme/components/Tabs.ts
@@ -7,51 +7,24 @@ import {
 import { getColor } from '@chakra-ui/theme-tools'
 import merge from 'lodash/merge'
 
-const variantLine: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> =
-  () => {
-    return {
-      root: {
-        w: '100%',
-      },
-      tablist: {
-        w: '100%',
-        overflowX: 'scroll',
-        borderColor: 'transparent',
-        px: '1.5rem',
-        pt: '1.25rem',
-        pb: '0.625rem',
-        whiteSpace: 'nowrap',
-      },
-      tab: {
-        textStyle: 'subhead-3',
-        textTransform: 'uppercase',
-        p: '0.25rem 0', // No horizontal padding to let bottom border be the same length as text
-        borderColor: 'transparent',
-        mx: '1rem',
-        borderBottom: '0.125rem solid transparent',
-        _selected: {
-          borderBottom: '0.125rem solid',
-          transition: 'none',
-          _focusVisible: {
-            // add horizontal padding
-            p: '0.25rem',
-            ml: '0.75rem',
-            mr: '0.75rem',
-            borderRadius: '0.25rem',
-            //  margins to ensure text remains at the same position as selected tabs
-            mb: '0.125rem',
-            borderBottom: '0',
-          },
-        },
-      },
-    }
-  }
+const parts = [
+  'root', // overall container
+  'tablist', // wrapper for tabs themselves (the clickable buttons)
+  'tab', // single clickable tab
+  'tabpanels', // wrapper for all panels, which contain the CONTENT of the tabs
+  'tabpanel', // content for a single tab
+  'indicator', // used to render an active tab indicator that animates between selected tabs
+]
 
-const scrollBarStyles = (
-  thumbColor: string,
-  trackColor: string,
-  theme: ChakraTheme,
-) => {
+const createScrollBarStyles = ({
+  thumbColor,
+  trackColor,
+  theme,
+}: {
+  thumbColor: string
+  trackColor: string
+  theme: ChakraTheme
+}) => {
   return {
     /* Scrollbar for Firefox */
     // Firefox only has these two css properties to customise scrollbar
@@ -62,40 +35,81 @@ const scrollBarStyles = (
     scrollbarWidth: 'thin',
     /* Scrollbar for Chrome, Safari, Opera and Microsoft Edge */
     '&::-webkit-scrollbar': {
-      height: '0.75rem',
-      backgroundColor: 'trackColor',
+      backgroundColor: trackColor,
     },
     '&::-webkit-scrollbar-thumb': {
       backgroundColor: thumbColor,
-      border: '0.25rem solid',
-      borderColor: trackColor,
+      // Create the effect of a spacing below the scroll bar by giving
+      // it a transparent border
+      border: '0.25rem solid rgba(0, 0, 0, 0)',
+      // No background drawn beneath border, which allows the border to
+      // retain the colour of the scrollbar's background, rather than
+      // the thumb colour
+      backgroundClip: 'padding-box',
       borderRadius: '0.5rem',
-      borderBox: 'padding-box',
     },
   }
 }
+
+const createSizes = () => ({
+  md: {
+    tab: {
+      px: '0',
+      mx: '1rem',
+      _focusVisible: {
+        p: '0.25rem',
+        // Subtract 0.25rem from mx and give it to padding so that
+        // focus ring can have padding
+        mx: '0.75rem',
+      },
+    },
+    tablist: {
+      // Leftmost and rightmost spacing comes half from tablist margin,
+      // half from tab margin
+      mx: '1rem',
+      py: '1.125rem',
+      '&::-webkit-scrollbar': {
+        height: '0.75rem',
+      },
+    },
+  },
+})
+
+const variantLine: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> =
+  () => ({
+    tab: {
+      borderBottom: '0.125rem solid transparent',
+      _focusVisible: {
+        _selected: {
+          borderColor: 'transparent',
+        },
+      },
+    },
+  })
 
 const variantLight: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = (
   props,
 ) => {
   return merge(variantLine(props), {
-    root: {
-      bg: 'white',
-    },
-    tablist: scrollBarStyles('secondary.200', 'white', props.theme),
+    tablist: createScrollBarStyles({
+      thumbColor: 'secondary.200',
+      trackColor: 'transparent',
+      theme: props.theme,
+    }),
     tab: {
-      color: 'primary.500',
+      color: 'primary.300',
       _hover: {
         color: 'primary.400',
       },
       _selected: {
         color: 'primary.500',
+        borderColor: 'primary.500',
       },
       _focusVisible: {
-        boxShadow: `0 0 0 2px ${getColor(props.theme, 'primary.500')}`,
-      },
-      _active: {
-        bg: 'white',
+        boxShadow: `inset 0 0 0 0.125rem ${getColor(
+          props.theme,
+          'primary.500',
+        )}`,
       },
     },
   })
@@ -108,8 +122,13 @@ const variantDark: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = (
     root: {
       bg: 'secondary.500',
     },
-    tablist: scrollBarStyles('secondary.400', 'secondary.500', props.theme),
+    tablist: createScrollBarStyles({
+      thumbColor: 'secondary.400',
+      trackColor: 'secondary.500',
+      theme: props.theme,
+    }),
     tab: {
+      color: 'secondary.300',
       _active: {
         bg: 'secondary.500',
       },
@@ -118,29 +137,29 @@ const variantDark: ThemingPropsThunk<SystemStyleObjectRecord, ChakraTheme> = (
       },
       _selected: {
         color: 'white',
+        borderColor: 'white',
       },
       _focusVisible: {
-        boxShadow: `0 0 0 2px white`,
+        boxShadow: `inset 0 0 0 0.125rem white`,
       },
-      color: 'secondary.300',
     },
   })
 }
 
 export const Tabs: ComponentMultiStyleConfig = {
-  parts: [],
+  parts,
   baseStyle: {
+    tablist: {
+      // overflowX and whiteSpace required for use-drag-scroll library
+      overflowX: 'scroll',
+      whiteSpace: 'nowrap',
+    },
     tab: {
-      mr: '2rem',
+      textStyle: 'subhead-3',
+      textTransform: 'uppercase',
     },
   },
-  sizes: {
-    md: {
-      tab: {
-        p: '0rem',
-      },
-    },
-  },
+  sizes: createSizes(),
   variants: {
     // Chakra UI already has a line variant, these are our custom variants
     'line-light': variantLight,
@@ -148,5 +167,6 @@ export const Tabs: ComponentMultiStyleConfig = {
   },
   defaultProps: {
     colorScheme: 'primary',
+    size: 'md',
   },
 }
