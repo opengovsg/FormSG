7c969592d46036e42b287c1561d65bf6aa88a8e9
Antariksh <antarikshmahajan@gmail.com>
2021-08-24 13:14:22 +0800

    fix(Tab.stories): fix aria-controls a11y violation

--- frontend/src/components/Tabs/Tabs.stories.tsx
+++ frontend/src/components/Tabs/Tabs.stories.tsx
@@ -1,4 +1,4 @@
-import { Tabs, TabsProps } from '@chakra-ui/react'
+import { TabPanel, TabPanels, Tabs, TabsProps } from '@chakra-ui/react'
 import { Meta, Story } from '@storybook/react'
 
 import { viewports } from '~utils/storybook'
@@ -21,6 +21,8 @@ export default {
 } as Meta
 
 const TabTemplate: Story<TabsProps> = (args) => {
+  const contentFontColour = args.variant === 'line-light' ? 'black' : 'white'
+
   return (
     <Tabs {...args}>
       <TabList>
@@ -28,6 +30,11 @@ const TabTemplate: Story<TabsProps> = (args) => {
         <Tab>Settings</Tab>
         <Tab>Results</Tab>
       </TabList>
+      <TabPanels>
+        <TabPanel color={contentFontColour}>Create tab content</TabPanel>
+        <TabPanel color={contentFontColour}>Settings tab content</TabPanel>
+        <TabPanel color={contentFontColour}>Results tab content</TabPanel>
+      </TabPanels>
     </Tabs>
   )
 }
