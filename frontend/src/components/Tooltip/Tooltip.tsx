1eef1315bcfb95d892add7daf13c0e1ee33378bf
Antariksh <antarikshmahajan@gmail.com>
2021-08-24 16:42:36 +0800

    fix(Tooltip): enable opening on mobile

--- frontend/src/components/Tooltip/Tooltip.tsx
+++ frontend/src/components/Tooltip/Tooltip.tsx
@@ -1,5 +1,21 @@
-import { Tooltip as ChakraTooltip, TooltipProps } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Box, Tooltip as ChakraTooltip, TooltipProps } from '@chakra-ui/react'
 
-export const Tooltip = (props: TooltipProps): JSX.Element => (
-  <ChakraTooltip {...props} hasArrow />
-)
+export const Tooltip = ({ children, ...props }: TooltipProps): JSX.Element => {
+  // ChakraTooltip does not work on mobile by design. (see
+  // https://github.com/chakra-ui/chakra-ui/issues/2691)
+  // Hence adapt the tooltip to open when clicked on mobile
+  const [isLabelOpen, setIsLabelOpen] = useState(!!props.isOpen)
+  return (
+    <ChakraTooltip {...props} hasArrow isOpen={isLabelOpen}>
+      <Box
+        as="span"
+        onMouseEnter={() => setIsLabelOpen(true)}
+        onMouseLeave={() => setIsLabelOpen(false)}
+        onClick={() => setIsLabelOpen((currentState) => !currentState)}
+      >
+        {children}
+      </Box>
+    </ChakraTooltip>
+  )
+}
