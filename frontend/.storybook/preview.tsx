9e067110ca9e0428b1f3396166f2bf1550dd6dc0
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-24 17:44:29 +0800

    feat: update storybook decorators

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,12 +1,27 @@
 import 'focus-visible/dist/focus-visible.min.js'
 import '../src/assets/fonts/inter.css'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 import { DecoratorFn } from '@storybook/react'
+import { initializeWorker, mswDecorator } from 'msw-storybook-addon'
+
+import { AuthProvider } from '~contexts/AuthContext'
 
 import { theme } from '../src/theme'
 
 import { StorybookTheme } from './themes'
+
+initializeWorker()
+
+const withReactQuery: DecoratorFn = (storyFn) => {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{storyFn()}</AuthProvider>
+    </QueryClientProvider>
+  )
+}
 
 const withChakra: DecoratorFn = (storyFn) => (
   <ChakraProvider resetCSS theme={theme}>
@@ -14,7 +29,7 @@ const withChakra: DecoratorFn = (storyFn) => (
   </ChakraProvider>
 )
 
-export const decorators = [withChakra]
+export const decorators = [withReactQuery, withChakra, mswDecorator]
 
 export const parameters = {
   docs: {
