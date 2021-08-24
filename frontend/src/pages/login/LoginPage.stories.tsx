ee6ac88991d1e1c863fc2c09dfc54ea5d53df9bd
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-24 17:45:56 +0800

    feat: add LoginPage responsive stories

---
+++ frontend/src/pages/login/LoginPage.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, Story } from '@storybook/react'
+
+import { authHandlers } from '~/mocks/msw/handlers/auth'
+
+import { viewports } from '~utils/storybook'
+
+import { LoginPage } from './LoginPage'
+
+export default {
+  title: 'Pages/LoginPage',
+  component: LoginPage,
+  decorators: [],
+  parameters: {
+    layout: 'fullscreen',
+    msw: authHandlers,
+  },
+} as Meta
+
+const Template: Story = () => <LoginPage />
+export const Desktop = Template.bind({})
+
+export const Tablet = Template.bind({})
+Tablet.parameters = {
+  viewport: {
+    defaultViewport: 'tablet',
+  },
+  chromatic: { viewports: [viewports.md] },
+}
+
+export const Mobile = Template.bind({})
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+  chromatic: { viewports: [viewports.xs] },
+}
