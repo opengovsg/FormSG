6b71b24f5beacea7afb4fc3461bfae58cd58bd60
Antariksh <antarikshmahajan@gmail.com>
2021-08-24 16:14:28 +0800

    refactor(Tooltip): add return type

--- frontend/src/components/Tooltip/Tooltip.tsx
+++ frontend/src/components/Tooltip/Tooltip.tsx
@@ -1,5 +1,5 @@
 import { Tooltip as ChakraTooltip, TooltipProps } from '@chakra-ui/react'
 
-export const Tooltip = (props: TooltipProps) => (
+export const Tooltip = (props: TooltipProps): JSX.Element => (
   <ChakraTooltip {...props} hasArrow />
 )
