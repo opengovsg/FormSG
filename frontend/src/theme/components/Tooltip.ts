4acf099d3b237a4dae8024b03aebed3068cc2ac9
Antariksh <antarikshmahajan@gmail.com>
2021-08-24 16:14:13 +0800

    style(Tooltip): fix text styles

--- frontend/src/theme/components/Tooltip.ts
+++ frontend/src/theme/components/Tooltip.ts
@@ -1,15 +1,22 @@
 import { ComponentStyleConfig } from '@chakra-ui/theme'
 
+import { textStyles } from '../textStyles'
+
 export const Tooltip: ComponentStyleConfig = {
   baseStyle: {
+    // overriding --tooltip-big since Chakra UI does it this way -
+    // see https://github.com/chakra-ui/chakra-ui/blob/main/packages/theme/src/components/tooltip.ts
     '--tooltip-bg': 'var(--chakra-colors-secondary-700)',
+    bg: 'secondary.700',
     px: '0.75rem',
     py: '0.5rem',
     color: 'white',
-    textStyle: 'body-2',
     borderRadius: '0.25rem',
     textAlign: 'left',
     margin: '0.25rem',
     maxWidth: '19.5rem',
+    // For some reason textStyle prop is not accepted, so just
+    // pass all required styles
+    ...textStyles['body-2'],
   },
 }
