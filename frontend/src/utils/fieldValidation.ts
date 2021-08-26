50a2948af2a2d963bcbce1b21bc0c23dcaf0f662
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-26 14:04:09 +0800

    feat: add validation rules for number field

--- frontend/src/utils/fieldValidation.ts
+++ frontend/src/utils/fieldValidation.ts
@@ -3,8 +3,13 @@
  * to the field schema.
  */
 import { RegisterOptions } from 'react-hook-form'
+import simplur from 'simplur'
 
-import { FieldBase } from '~shared/types/field'
+import {
+  FieldBase,
+  NumberFieldBase,
+  NumberSelectedValidation,
+} from '~shared/types/field'
 
 import { REQUIRED_ERROR } from '~constants/validation'
 
@@ -15,6 +20,39 @@ export const createBaseValidationRules = (
     required: {
       value: schema.required,
       message: REQUIRED_ERROR,
+    },
+  }
+}
+
+export const createNumberValidationRules = (
+  schema: NumberFieldBase,
+): RegisterOptions => {
+  const { selectedValidation, customVal } = schema.ValidationOptions
+
+  return {
+    ...createBaseValidationRules(schema),
+    validate: (val?: string) => {
+      if (!val || !customVal) return true
+
+      const currLen = val.length
+
+      switch (selectedValidation) {
+        case NumberSelectedValidation.Exact:
+          return (
+            currLen === customVal ||
+            simplur`Please enter ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+        case NumberSelectedValidation.Min:
+          return (
+            currLen >= customVal ||
+            simplur`Please enter at least ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+        case NumberSelectedValidation.Max:
+          return (
+            currLen <= customVal ||
+            simplur`Please enter at most ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+      }
     },
   }
 }
