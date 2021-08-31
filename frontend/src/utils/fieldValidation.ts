89254350fbd3d2d5d11900ab8c8163b2288115fb
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-31 16:42:29 +0800

    feat(v2): add ShortTextField component for use when rendering forms (#2665)
    * fix: allow transpilation of shared code in client folder
    
    * feat: allow YesNo component to retrieve information from FormControl
    
    if it exists
    
    * fix: prevent yesno selection if field is readonly
    
    * fix(FormLabel): do not pass isRequired as DOM property
    
    was throwing a console error
    
    * feat(shared): update FormFieldWithId type with FormField generic
    
    allows for narrowing of types
    
    * feat(FieldContainer): add layout component that all fields share
    
    when rendering on a public form
    
    * feat: add YesNoField component and fieldValidation utility
    
    * feat: add YesNoField story
    
    * fix: prevent YesNoField story from crashing
    
    * test(YesNoField): add unit tests for YesNoField component
    
    * chore: set up test linter, setup, and add test packages
    
    * chore: update CI to run new frontend tests instead of AngularJS's
    
    * chore: add alias for shared imports
    
    # Conflicts:
    #	frontend/__tests__/storyshots/jest.config.js
    #	frontend/config-overrides.js
    #	frontend/tsconfig.paths.json
    
    * chore: run chromatic github action only on PR
    
    * feat(YesNoField): add optional validation story and unit tests
    
    * fix: make root jest ignore frontend folder
    
    * feat: set inputs to readOnly iff inputs are valid and submitting
    
    * feat(fieldValidation): update required error message
    
    following figma design in DesignMaster V3 > Examples, Empty, Error
    
    * test(YesNoField): remove need for mock schema, use story's own args
    
    * chore: add simplur package for easy pluralisation of strings
    
    * feat(fieldValidation): add createShortTextValidationRules util fn
    
    * feat(ShortTextField): add component
    
    * feat(ShortTextField): add aria-label of schema.title to input
    
    * feat: add stories
    
    * chore: add more react-testing-library packages, update eslint rules
    
    * test: add unit tests for ShortTextField
    
    * ref: rhf -> react-hook-form

--- frontend/src/utils/fieldValidation.ts
+++ frontend/src/utils/fieldValidation.ts
@@ -3,8 +3,13 @@
  * to the field schema.
  */
 import { RegisterOptions } from 'react-hook-form'
+import simplur from 'simplur'
 
-import { FieldBase } from '~shared/types/field'
+import {
+  FieldBase,
+  ShortTextFieldBase,
+  TextSelectedValidation,
+} from '~shared/types/field'
 
 import { REQUIRED_ERROR } from '~constants/validation'
 
@@ -15,6 +20,38 @@ export const createBaseValidationRules = (
     required: {
       value: schema.required,
       message: REQUIRED_ERROR,
+    },
+  }
+}
+
+export const createShortTextValidationRules = (
+  schema: ShortTextFieldBase,
+): RegisterOptions => {
+  const { selectedValidation, customVal } = schema.ValidationOptions
+  return {
+    ...createBaseValidationRules(schema),
+    validate: (val?: string) => {
+      if (!val || !customVal) return true
+
+      const currLen = val.length
+
+      switch (selectedValidation) {
+        case TextSelectedValidation.Exact:
+          return (
+            currLen === customVal ||
+            simplur`Please enter ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+        case TextSelectedValidation.Minimum:
+          return (
+            currLen >= customVal ||
+            simplur`Please enter at least ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+        case TextSelectedValidation.Maximum:
+          return (
+            currLen <= customVal ||
+            simplur`Please enter at most ${customVal} character[|s] (${currLen}/${customVal})`
+          )
+      }
     },
   }
 }
