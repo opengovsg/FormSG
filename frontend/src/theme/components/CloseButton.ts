563418348e3a0eb58b2a62e5fedf08080c966c51
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-02 10:52:04 +0800

    style(theme): add clear and subtle CloseButton variants

--- frontend/src/theme/components/CloseButton.ts
+++ frontend/src/theme/components/CloseButton.ts
@@ -3,16 +3,27 @@ import {
   SystemStyleObject,
   ThemeComponentFunction,
 } from '@chakra-ui/react'
+import { getColor } from '@chakra-ui/theme-tools'
 
 import { Button, ThemeButtonVariant } from './Button'
 
 export const CloseButton: ComponentStyleConfig = {
-  baseStyle: (props) => {
-    const buttonFn = Button.variants as Record<
-      ThemeButtonVariant,
-      ThemeComponentFunction<SystemStyleObject>
-    >
-    return buttonFn['clear'](props)
+  variants: {
+    subtle: (props) => {
+      const { theme, colorScheme: c } = props
+      return {
+        _focus: {
+          boxShadow: `0 0 0 4px ${getColor(theme, `${c}.300`)}`,
+        },
+      }
+    },
+    clear: (props) => {
+      const buttonFn = Button.variants as Record<
+        ThemeButtonVariant,
+        ThemeComponentFunction<SystemStyleObject>
+      >
+      return buttonFn['clear'](props)
+    },
   },
   defaultProps: {
     colorScheme: 'neutral',
