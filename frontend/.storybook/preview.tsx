3d9ad8d9b3e8e15b47954ddf654f7861ae25feff
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-02 16:25:45 +0800

    feat: add react query decorator for storybook

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import 'focus-visible/dist/focus-visible.min.js'
 import '../src/assets/fonts/inter.css'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 import { DecoratorFn } from '@storybook/react'
 
@@ -8,13 +9,20 @@ import { theme } from '../src/theme'
 
 import { StorybookTheme } from './themes'
 
+const withReactQuery: DecoratorFn = (storyFn) => {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>{storyFn()}</QueryClientProvider>
+  )
+}
+
 const withChakra: DecoratorFn = (storyFn) => (
   <ChakraProvider resetCSS theme={theme}>
     {storyFn()}
   </ChakraProvider>
 )
 
-export const decorators = [withChakra]
+export const decorators = [withReactQuery, withChakra]
 
 export const parameters = {
   docs: {
