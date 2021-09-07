447f2bc4c84f81632b8e378f93eef7224a645f68
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-07 16:56:56 +0800

    feat: add primitive routing to admin form route from workspace

--- frontend/src/pages/workspace/WorkspacePage.tsx
+++ frontend/src/pages/workspace/WorkspacePage.tsx
@@ -1,12 +1,17 @@
 import { useCallback } from 'react'
 import { useQueryClient } from 'react-query'
+import { Link as ReactLink } from 'react-router-dom'
+import { Link, Text } from '@chakra-ui/react'
 
 import { LOGGED_IN_KEY } from '~constants/localStorage'
+import { ADMIN_FORM_ROUTE } from '~constants/routes'
 import { useLocalStorage } from '~hooks/useLocalStorage'
 import { logout } from '~services/AuthService'
 import Button from '~components/Button'
 
 import { useUser } from '~features/user/queries'
+
+import { useWorkspace } from './queries'
 
 export const WorkspacePage = (): JSX.Element => {
   const queryClient = useQueryClient()
@@ -24,9 +29,22 @@ export const WorkspacePage = (): JSX.Element => {
 
   const { user } = useUser()
 
+  const { dashboardForms } = useWorkspace()
+
   return (
     <div>
-      Logged in: {JSON.stringify(user)}
+      <Text>Logged in: {JSON.stringify(user)}</Text>
+      {dashboardForms?.map((form) => {
+        return (
+          <Link
+            key={form._id}
+            as={ReactLink}
+            to={`${ADMIN_FORM_ROUTE}/${form._id}`}
+          >
+            {form.title}
+          </Link>
+        )
+      })}
       <Button onClick={handleLogout}>Logout</Button>
     </div>
   )
