4d56388d4ede5dfc639f0c9967f77a3492836119
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-07 16:56:28 +0800

    feat(queries): add useWorkspace query to retrieve dashboard form view

---
+++ frontend/src/pages/workspace/queries.ts
@@ -0,0 +1,22 @@
+import { useQuery } from 'react-query'
+import { AsyncReturnType } from 'type-fest'
+
+import { getDashboardView } from './AdminViewFormService'
+
+const workspaceKeys = {
+  all: ['workspace'] as const,
+}
+
+type UseWorkspaceReturn = {
+  dashboardForms: AsyncReturnType<typeof getDashboardView> | undefined
+}
+
+export const useWorkspace = (): UseWorkspaceReturn => {
+  const { data: dashboardForms } = useQuery(workspaceKeys.all, () =>
+    getDashboardView(),
+  )
+
+  return {
+    dashboardForms,
+  }
+}
