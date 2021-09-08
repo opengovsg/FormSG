b04cfa5097e7357e5e157a8392e06cb8a3a59ba8
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-08 10:53:56 +0800

    feat: add AdminFormPage with scaffolding

---
+++ frontend/src/features/admin-form/AdminFormPage.tsx
@@ -0,0 +1,23 @@
+import { TabPanel, TabPanels } from '@chakra-ui/react'
+
+import { AdminFormNavbar } from './components/AdminFormNavbar'
+import { AdminFormTabProvider } from './components/AdminFormTabProvider'
+
+export const AdminFormPage = (): JSX.Element => {
+  return (
+    <AdminFormTabProvider>
+      <AdminFormNavbar />
+      <TabPanels>
+        <TabPanel>
+          <p>Insert builder page here!</p>
+        </TabPanel>
+        <TabPanel>
+          <p>Insert settings page here!</p>
+        </TabPanel>
+        <TabPanel>
+          <p>Insert results page here!</p>
+        </TabPanel>
+      </TabPanels>
+    </AdminFormTabProvider>
+  )
+}
