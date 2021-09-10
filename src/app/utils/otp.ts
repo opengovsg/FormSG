753d7a7c87cf4f1da0f63f79949a54e1484d010e
Timothee Groleau <timothee.groleau@gmail.com>
2021-09-10 10:15:36 +0800

    fix(otp): Ensure uniform distribution of digits

--- src/app/utils/otp.ts
+++ src/app/utils/otp.ts
@@ -14,11 +14,9 @@ export const generateOtp = (): string => {
   const chars = '0123456789'
   // Generates cryptographically strong pseudo-random data.
   // The size argument is a number indicating the number of bytes to generate.
-  const rnd = crypto.randomBytes(length)
-  const d = chars.length / 256
   const digits = new Array(length)
   for (let i = 0; i < length; i++) {
-    digits[i] = chars[Math.floor(rnd[i] * d)]
+    digits[i] = chars[crypto.randomInt(0, chars.length)]
   }
   return digits.join('')
 }
