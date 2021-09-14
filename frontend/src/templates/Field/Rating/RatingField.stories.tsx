a082bfbe6446747b096d8479dd3ad8405965c12d
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-14 17:52:11 +0800

    feat: add RatingField component and stories

---
+++ frontend/src/templates/Field/Rating/RatingField.stories.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { Text } from '@chakra-ui/react'
+import { Meta, Story } from '@storybook/react'
+
+import { BasicField, RatingShape } from '~shared/types/field'
+
+import Button from '~components/Button'
+
+import {
+  RatingField as RatingFieldComponent,
+  RatingFieldProps,
+  RatingFieldSchema,
+} from './RatingField'
+
+export default {
+  title: 'Templates/Field/RatingField',
+  component: RatingFieldComponent,
+  decorators: [],
+  parameters: {
+    docs: {
+      // Required in this story due to react-hook-form conflicting with
+      // Storybook somehow.
+      // See https://github.com/storybookjs/storybook/issues/12747.
+      source: {
+        type: 'code',
+      },
+    },
+  },
+} as Meta
+
+const requiredSchema: RatingFieldSchema = {
+  title: 'Do you like apples',
+  description:
+    'Not oranges, not any other fruits. I only want to know your apple preferences.',
+  required: true,
+  disabled: false,
+  fieldType: BasicField.Rating,
+  ratingOptions: {
+    steps: 10,
+    shape: RatingShape.Star,
+  },
+  _id: '611b94dfbb9e300012f702a7',
+}
+
+const Template: Story<RatingFieldProps> = (args) => {
+  const formMethods = useForm()
+
+  const [submitValues, setSubmitValues] = useState<string>()
+
+  const onSubmit = (values: Record<string, string>) => {
+    setSubmitValues(values[args.schema._id] ?? 'Nothing was selected')
+  }
+
+  useEffect(() => {
+    formMethods.trigger()
+  }, [])
+
+  return (
+    <FormProvider {...formMethods}>
+      <form onSubmit={formMethods.handleSubmit(onSubmit)} noValidate>
+        <RatingFieldComponent {...args} />
+        <Button
+          mt="1rem"
+          type="submit"
+          isLoading={formMethods.formState.isSubmitting}
+          loadingText="Submitting"
+        >
+          Submit
+        </Button>
+        {submitValues && <Text>You have submitted: {submitValues}</Text>}
+      </form>
+    </FormProvider>
+  )
+}
+
+export const ValidationRequired = Template.bind({})
+ValidationRequired.args = {
+  schema: requiredSchema,
+}
+
+export const ValidationOptional = Template.bind({})
+ValidationOptional.args = {
+  schema: { ...requiredSchema, required: false },
+}
