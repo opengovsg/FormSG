51e2142c33313ba553d74be77d62d5a3a5734203
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-29 16:11:05 +0800

    feat(FormBannerLogo): extract logic to hook

--- frontend/src/features/public-form/components/FormStartPage/FormBannerLogo.tsx
+++ frontend/src/features/public-form/components/FormStartPage/FormBannerLogo.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { Flex, Image, Skeleton } from '@chakra-ui/react'
 
 import { FormLogoState } from '~shared/types/form/form_logo'
@@ -6,32 +6,57 @@ import { FormLogoState } from '~shared/types/form/form_logo'
 import { useEnv } from '~features/env/queries'
 import { usePublicForm } from '~features/public-form/queries'
 
-export const FormBannerLogo = (): JSX.Element => {
-  const { data, isLoading } = usePublicForm()
-  const { data: { logoBucketUrl } = {} } = useEnv()
+const useFormBannerLogo = () => {
+  const [hasImageLoaded, setHasImageLoaded] = useState(false)
+
+  const { data: form } = usePublicForm()
+  const { data: { logoBucketUrl } = {} } = useEnv(
+    form?.startPage.logo.state === FormLogoState.Custom,
+  )
+
+  const onImageLoad = useCallback(() => setHasImageLoaded(true), [])
 
   const logoImgSrc = useMemo(() => {
-    if (!data || !logoBucketUrl) return ''
-    const formLogo = data.startPage.logo
+    if (!form) return undefined
+    const formLogo = form.startPage.logo
     switch (formLogo.state) {
       case FormLogoState.None:
         return ''
       case FormLogoState.Default:
-        return data.admin.agency.logo
+        return form.admin.agency.logo
       case FormLogoState.Custom:
-        return `${logoBucketUrl}/${formLogo.fileId}`
+        return logoBucketUrl ? `${logoBucketUrl}/${formLogo.fileId}` : undefined
     }
-  }, [data, logoBucketUrl])
+  }, [form, logoBucketUrl])
 
   const logoImgAlt = useMemo(
-    () => (data ? `Logo for ${data.admin.agency.fullName}` : ''),
-    [data],
+    () => (form ? `Logo for ${form.admin.agency.fullName}` : undefined),
+    [form],
   )
+
+  return {
+    hasImageLoaded,
+    onImageLoad,
+    logoImgAlt,
+    logoImgSrc,
+  }
+}
+
+export const FormBannerLogo = (): JSX.Element => {
+  const { hasImageLoaded, onImageLoad, logoImgAlt, logoImgSrc } =
+    useFormBannerLogo()
 
   return (
     <Flex justify="center" p="1rem">
-      <Skeleton isLoaded={!isLoading}>
-        <Image src={logoImgSrc} alt={logoImgAlt} maxH="4rem" />
+      <Skeleton isLoaded={hasImageLoaded}>
+        <Image
+          onLoad={onImageLoad}
+          src={logoImgSrc}
+          alt={logoImgAlt}
+          // Define minimum height and width of skeleton before image has loaded.
+          {...(hasImageLoaded ? {} : { h: '4rem', w: '4rem' })}
+          maxH="4rem"
+        />
       </Skeleton>
     </Flex>
   )
