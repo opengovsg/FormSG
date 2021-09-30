4b8818c85267bb5f5d7e69460a91d00745b765ef
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-30 11:51:21 +0800

    feat: add loading states to FormFields and FormHeader

--- frontend/src/features/public-form/components/FormFields/FormFieldsContainer.tsx
+++ frontend/src/features/public-form/components/FormFields/FormFieldsContainer.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
-import { Box, Flex, Stack, Text } from '@chakra-ui/react'
+import { Box, Flex, Skeleton, Stack, Text } from '@chakra-ui/react'
 
 import { BasicField } from '~shared/types/field'
 import { FormColorTheme } from '~shared/types/form/form'
@@ -14,7 +14,7 @@ import { FormSectionsProvider } from './FormSectionsContext'
 import { SectionSidebar } from './SectionSidebar'
 
 export const FormFieldsContainer = (): JSX.Element => {
-  const { data } = usePublicForm()
+  const { data, isLoading } = usePublicForm()
 
   // TODO: Inject default values if field is MyInfo, or prefilled.
   const formMethods = useForm()
@@ -24,7 +24,8 @@ export const FormFieldsContainer = (): JSX.Element => {
   }
 
   const bgColour = useMemo(() => {
-    if (!data) return
+    if (isLoading) return 'neutral.100'
+    if (!data) return ''
     const { colorTheme } = data.startPage
     switch (colorTheme) {
       case FormColorTheme.Blue:
@@ -32,9 +33,26 @@ export const FormFieldsContainer = (): JSX.Element => {
       default:
         return `theme-${colorTheme}.100`
     }
-  }, [data])
+  }, [data, isLoading])
 
   const renderFields = useMemo(() => {
+    // Render skeleton when no data
+    if (isLoading) {
+      return (
+        <Flex flexDir="column">
+          <Skeleton height="2rem" />
+          <Skeleton height="1.5rem" mt="2.25rem" />
+          <Skeleton height="2.75rem" mt="0.75rem" />
+          <Skeleton height="1.5rem" mt="2.25rem" />
+          <Skeleton height="2.75rem" mt="0.75rem" />
+          <Skeleton height="1.5rem" mt="2.25rem" />
+          <Skeleton height="2.75rem" mt="0.75rem" />
+          <Skeleton height="1.5rem" mt="2.25rem" />
+          <Skeleton height="2.75rem" mt="0.75rem" />
+        </Flex>
+      )
+    }
+
     return data?.form_fields.map((field) => {
       switch (field.fieldType) {
         case BasicField.Section:
@@ -60,7 +78,7 @@ export const FormFieldsContainer = (): JSX.Element => {
       <Flex bg={bgColour} flex={1} justify="center" p="1.5rem">
         <SectionSidebar />
         <FormProvider {...formMethods}>
-          <Box bg="white" p="2.5rem" minW={0} maxW="57rem">
+          <Box bg="white" p="2.5rem" w="100%" maxW="57rem">
             <form onSubmit={formMethods.handleSubmit(onSubmit)} noValidate>
               <Stack spacing="2.25rem">{renderFields}</Stack>
               <Button
