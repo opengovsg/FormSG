036ba05fdf85410cf9d62dc15719394759fbeca0
Kar Rui Lau <karrui.lau@gmail.com>
2021-09-30 16:24:32 +0800

    feat: add input validation rules for emergency contact input

--- frontend/src/features/user/emergency-contact/components/modal/EmergencyContactModal.tsx
+++ frontend/src/features/user/emergency-contact/components/modal/EmergencyContactModal.tsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { BiCheck } from 'react-icons/bi'
 import {
   Flex,
@@ -7,14 +8,17 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
   Text,
   useDisclosure,
 } from '@chakra-ui/react'
 
+import { isMobilePhoneNumber } from '~shared/utils/phone-num-validation'
+
+import { REQUIRED_ERROR } from '~constants/validation'
 import Button from '~components/Button'
+import FormErrorMessage from '~components/FormControl/FormErrorMessage'
 import FormLabel from '~components/FormControl/FormLabel'
 import PhoneNumberInput from '~components/PhoneNumberInput'
 
@@ -23,11 +27,27 @@ import { useUser } from '~features/user/queries'
 
 import { VerificationBox } from './VerificationBox'
 
+type ContactNumberFormInputs = {
+  contact: string
+}
+
 const ContactNumberInput = () => {
   const { user, isLoading } = useUser()
   const [isVfnOpen, setIsVfnOpen] = useState(false)
   const [isSuccess, setIsSuccess] = useState(false)
-  const [contact, setContact] = useState(user?.contact)
+
+  const {
+    control,
+    watch,
+    handleSubmit,
+    formState: { isValid, isSubmitting, errors },
+  } = useForm<ContactNumberFormInputs>({
+    defaultValues: {
+      contact: user?.contact,
+    },
+  })
+
+  const contact = watch('contact')
 
   const isVerified = useMemo(
     () => !!user?.contact && contact === user.contact,
@@ -36,10 +56,10 @@ const ContactNumberInput = () => {
 
   const { generateOtpMutation } = useUserMutations()
 
-  const handleVerifyMobile = () => {
-    if (!user || !contact) return
+  const handleSendOtp = (inputs: ContactNumberFormInputs) => {
+    if (!user || isVerified) return
     return generateOtpMutation.mutate(
-      { userId: user._id, contact },
+      { userId: user._id, contact: inputs.contact },
       { onSuccess: () => setIsVfnOpen(true) },
     )
   }
@@ -48,28 +68,60 @@ const ContactNumberInput = () => {
     if (isVfnOpen && nextVal !== contact) {
       setIsVfnOpen(false)
     }
-    setContact(nextVal)
   }
 
+  const contactNumberValidationRules = useMemo(() => {
+    return {
+      required: REQUIRED_ERROR,
+      validate: {
+        validPhoneNumber: (val?: string) => {
+          return (
+            !val ||
+            isMobilePhoneNumber(val) ||
+            'Please enter a valid mobile number'
+          )
+        },
+      },
+    }
+  }, [])
+
   return (
-    <FormControl isReadOnly={isLoading} mb="1.5rem" mt="2rem">
-      <FormLabel isRequired>Mobile number</FormLabel>
+    <FormControl
+      mt="1rem"
+      isRequired
+      isReadOnly={(isValid && isSubmitting) || isLoading}
+      isInvalid={!!errors.contact}
+    >
+      <FormLabel>Mobile number</FormLabel>
       <Flex>
-        <PhoneNumberInput
-          isSuccess={isSuccess}
-          value={contact}
-          onChange={handleInputChange}
+        <Controller
+          name="contact"
+          control={control}
+          rules={contactNumberValidationRules}
+          render={({ field: { onChange, ...rest } }) => (
+            <PhoneNumberInput
+              isSuccess={isSuccess}
+              {...rest}
+              onChange={(nextVal) => {
+                handleInputChange(nextVal)
+                onChange(nextVal)
+              }}
+            />
+          )}
         />
         <Button
-          isDisabled={isVerified}
+          isDisabled={isVerified || isVfnOpen}
           ml="0.5rem"
-          onClick={handleVerifyMobile}
+          onClick={handleSubmit(handleSendOtp)}
           isLoading={generateOtpMutation.isLoading}
           leftIcon={isVerified ? <BiCheck fontSize="1.5rem" /> : undefined}
         >
           {isVerified ? 'Verified' : 'Verify'}
         </Button>
       </Flex>
+      <FormErrorMessage>
+        {errors.contact && errors.contact.message}
+      </FormErrorMessage>
       {isVfnOpen && !isVerified ? (
         <VerificationBox onSuccess={() => setIsSuccess(true)} />
       ) : null}
@@ -87,7 +139,7 @@ export const EmergencyContactModal = (): JSX.Element => {
         <ModalContent>
           <ModalCloseButton />
           <ModalHeader color="secondary.700">Emergency contact</ModalHeader>
-          <ModalBody whiteSpace="pre-line">
+          <ModalBody whiteSpace="pre-line" pb="3.25rem">
             <Text textStyle="body-2" color="secondary.500">
               Update your mobile number and verify it so we can contact you in
               the unlikely case of an urgent form issue. This number can be
@@ -95,7 +147,6 @@ export const EmergencyContactModal = (): JSX.Element => {
             </Text>
             <ContactNumberInput />
           </ModalBody>
-          <ModalFooter></ModalFooter>
         </ModalContent>
       </Modal>
     </>
