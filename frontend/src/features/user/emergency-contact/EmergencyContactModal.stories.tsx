81596cf7217913eea71c711ba742727eecae8e8d
Kar Rui Lau <karrui.lau@gmail.com>
2021-10-06 12:22:46 +0800

    feat: add more states to EmergencyContactModal stories

--- frontend/src/features/user/emergency-contact/EmergencyContactModal.stories.tsx
+++ frontend/src/features/user/emergency-contact/EmergencyContactModal.stories.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
 import { useDisclosure } from '@chakra-ui/hooks'
 import { Meta, Story } from '@storybook/react'
+import { omit } from 'lodash'
 
-import { authHandlers } from '~/mocks/msw/handlers/auth'
+import {
+  getUser,
+  MOCK_USER,
+  postGenerateContactOtp,
+  postVerifyContactOtp,
+  userHandlers,
+} from '~/mocks/msw/handlers/user'
 
+import { LOGGED_IN_KEY } from '~constants/localStorage'
+import { viewports } from '~utils/storybook'
 import Button from '~components/Button'
 
 import { EmergencyContactModal } from './EmergencyContactModal'
@@ -10,10 +20,20 @@ import { EmergencyContactModal } from './EmergencyContactModal'
 export default {
   title: 'Features/User/EmergencyContactModal',
   component: EmergencyContactModal,
-  decorators: [],
+  decorators: [
+    (storyFn) => {
+      useEffect(() => {
+        window.localStorage.setItem(LOGGED_IN_KEY, JSON.stringify(true))
+
+        return () => window.localStorage.removeItem(LOGGED_IN_KEY)
+      }, [])
+
+      return storyFn()
+    },
+  ],
   parameters: {
     layout: 'fullscreen',
-    msw: authHandlers,
+    msw: userHandlers({ delay: 0 }),
   },
 } as Meta
 
@@ -27,4 +47,26 @@ const Template: Story = () => {
     </>
   )
 }
-export const Default = Template.bind({})
+export const WithContact = Template.bind({})
+
+export const NoContact = Template.bind({})
+NoContact.parameters = {
+  msw: [
+    getUser({ delay: 0, mockUser: omit(MOCK_USER, 'contact') }),
+    postGenerateContactOtp({ delay: 0 }),
+    postVerifyContactOtp({ delay: 0 }),
+  ],
+}
+
+export const Loading = Template.bind({})
+Loading.parameters = {
+  msw: userHandlers({ delay: 'infinite' }),
+}
+
+export const Mobile = Template.bind({})
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+  chromatic: { viewports: [viewports.xs] },
+}
