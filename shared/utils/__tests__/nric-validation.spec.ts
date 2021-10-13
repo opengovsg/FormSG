2e455fd4cf7ba69009f9645dce5a20fc8d60ec52
Yuanruo Liang <yuanruo@open.gov.sg>
2021-10-13 09:00:55 +0800

    refactor: add comments and rearrange some code for readability

--- shared/utils/__tests__/nric-validation.spec.ts
+++ shared/utils/__tests__/nric-validation.spec.ts
@@ -5,11 +5,11 @@ describe('isMFinSeriesValid', () => {
     expect(isMFinSeriesValid('M1234567K')).toBe(true)
   })
 
-  it('should fail invalid M-series FIN numbers', () => {
+  it('should fail invalid M-series FIN numbers with an incorrect checksum digit', () => {
     expect(isMFinSeriesValid('M1234567L')).toBe(false)
   })
 
-  it('should fail M-series FIN numbers with invalid checksum digits', () => {
+  it('should fail M-series FIN numbers with checksum digits outside of the prescribed range', () => {
     expect(isMFinSeriesValid('M1234567I')).toBe(false)
   })
 })
