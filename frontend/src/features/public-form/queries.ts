1c9d78d933fd701326f32a5e9b38ff458c5666d3
Kar Rui Lau <karrui.lau@gmail.com>
2021-10-26 15:50:33 +0800

    feat: add comment as to why query keys map to array

--- frontend/src/features/public-form/queries.ts
+++ frontend/src/features/public-form/queries.ts
@@ -10,6 +10,8 @@ import { PublicFormParam } from '~constants/routes'
 import { getPublicFormView } from './PublicFormService'
 
 const publicFormKeys = {
+  // All keys map to either an array or function returning an array for
+  // consistency
   base: ['publicForm'] as const,
   id: (formId: string) => [...publicFormKeys.base, formId] as const,
 }
