01589464d1c4e474d9f0c3f1d35964397795090e
Kar Rui Lau <karrui.lau@gmail.com>
2021-10-28 02:45:24 +0800

    chore(build): clean up storybook build config
    seems to not need special typescript config anymore, and alias seems to now work without a dedicated toPath function.

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,10 +1,4 @@
 /* eslint-env node */
-const { propNames } = require('@chakra-ui/styled-system')
-const path = require('path')
-
-const toPath = (_path) => path.join(process.cwd(), _path)
-const excludedPropNames = propNames.concat(['as', 'apply', 'sx', '__css'])
-
 module.exports = {
   stories: [
     // Introduction stories set first so stories are ordered correctly.
@@ -20,28 +14,6 @@ module.exports = {
     '@storybook/addon-a11y',
     'storybook-preset-craco',
   ],
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      shouldRemoveUndefinedFromOptional: true,
-      allowSyntheticDefaultImports: true,
-      esModuleInterop: true,
-      propFilter: (prop) => {
-        if (!prop.parent) {
-          return !prop.declarations.some((d) =>
-            d.fileName.includes('node_modules'),
-          )
-        }
-
-        const isStyledSystemProp = excludedPropNames.includes(prop.name)
-        const isHTMLElementProp = prop.parent.fileName.includes('node_modules')
-        return !(isStyledSystemProp || isHTMLElementProp)
-      },
-    },
-  },
   // webpackFinal setup retrieved from ChakraUI's own Storybook setup
   // https://github.com/chakra-ui/chakra-ui/blob/main/.storybook/main.js
   webpackFinal: async (storybookConfig) => {
@@ -54,8 +26,8 @@ module.exports = {
           ...storybookConfig.resolve.alias,
           // Required so storybook knows where the npm package is to render ChakraUI components
           // as this is not directly installed in package.json.
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
+          '@emotion/core': '@emotion/react',
+          'emotion-theming': '@emotion/react',
         },
       },
     }
