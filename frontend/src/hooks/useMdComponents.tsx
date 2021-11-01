fa95275472b9149f13994222c33fe4b6ed5305d2
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-01 21:39:53 +0800

    feat(v2): Implement ParagraphField component, extend markdown components (#3000)
    * feat: convert markdown list tags to ChakraUI versions
    
    * feat(StatementField): add components and stories
    
    * test(utils): add extended utils to search for text within nested tags
    
    * test(StatementField): add unit tests
    
    * ref: rename StatementField to ParagraphField

--- frontend/src/hooks/useMdComponents.tsx
+++ frontend/src/hooks/useMdComponents.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { Components } from 'react-markdown/src/ast-to-react'
-import { CSSObject, Text } from '@chakra-ui/react'
+import { CSSObject, ListItem, OrderedList, Text } from '@chakra-ui/react'
 
 import Link from '~components/Link'
 
@@ -24,27 +24,33 @@ export const useMdComponents = ({
   styles = {},
   overrides = {},
 }: UseMdComponentsProps = {}): Components => {
+  const textStyles = useMemo(
+    () => ({ ...(styles?.text ? { sx: styles.text } : {}) }),
+    [styles.text],
+  )
+
+  const linkStyles = useMemo(
+    () => ({ ...(styles.link ? { sx: styles.link } : {}) }),
+    [styles.link],
+  )
+
   const mdComponents: Components = useMemo(
     () => ({
+      ol: (props) => (
+        <OrderedList marginInlineStart="1.25rem" {...props} {...textStyles} />
+      ),
+      li: (props) => <ListItem {...props} {...textStyles} />,
       a: (props) => {
         const { href } = props
         const isExternal =
           typeof href === 'string' && !href.startsWith(window.location.origin)
 
-        return (
-          <Link
-            {...props}
-            isExternal={isExternal}
-            {...(styles.link ? { sx: styles.link } : {})}
-          />
-        )
+        return <Link {...props} isExternal={isExternal} {...linkStyles} />
       },
-      p: (props) => (
-        <Text {...props} {...(styles?.text ? { sx: styles.text } : {})} />
-      ),
+      p: (props) => <Text {...props} {...textStyles} />,
       ...overrides,
     }),
-    [overrides, styles],
+    [linkStyles, overrides, textStyles],
   )
 
   return mdComponents
