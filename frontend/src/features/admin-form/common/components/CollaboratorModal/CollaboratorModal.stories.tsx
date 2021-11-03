0105b441664cf41e524fa9c6e3658196bccfd6a9
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-03 23:54:37 +0800

    feat(CollaboratorModal): add initial stories

---
+++ frontend/src/features/admin-form/common/components/CollaboratorModal/CollaboratorModal.stories.tsx
@@ -0,0 +1,59 @@
+import { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import { useDisclosure } from '@chakra-ui/hooks'
+import { Meta, Story } from '@storybook/react'
+
+import {
+  fullScreenDecorator,
+  LoggedInDecorator,
+  viewports,
+} from '~utils/storybook'
+
+import { CollaboratorModal } from './CollaboratorModal'
+
+export default {
+  title: 'Features/AdminForm/CollaboratorModal',
+  component: CollaboratorModal,
+  decorators: [fullScreenDecorator, LoggedInDecorator],
+  parameters: {
+    layout: 'fullscreen',
+    // Prevent flaky tests due to modal animating in.
+    chromatic: { pauseAnimationAtEnd: true },
+  },
+} as Meta
+
+const modalRoot = document.createElement('div')
+document.body.appendChild(modalRoot)
+
+const Template: Story = () => {
+  const modalProps = useDisclosure({ defaultIsOpen: true })
+
+  const el = document.createElement('div')
+
+  useEffect(() => {
+    modalRoot.appendChild(el)
+
+    return () => {
+      modalRoot.removeChild(el)
+    }
+  })
+
+  return ReactDOM.createPortal(
+    <CollaboratorModal
+      {...modalProps}
+      onClose={() => console.log('close modal')}
+    />,
+    el,
+  )
+}
+export const Default = Template.bind({})
+
+export const Loading = Template.bind({})
+
+export const Mobile = Template.bind({})
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+  chromatic: { viewports: [viewports.xs] },
+}
