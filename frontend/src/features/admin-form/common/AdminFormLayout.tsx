998e5155096ba9f6756baa2135a356c8bd62e8bc
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-04 22:20:31 +0800

    feat: replace AdminFormPage with AdminFormLayout
    uses Outlet component to render subpages, according to v6 practices

---
+++ frontend/src/features/admin-form/common/AdminFormLayout.tsx
@@ -0,0 +1,28 @@
+import { Outlet } from 'react-router-dom'
+import { Box, Flex } from '@chakra-ui/react'
+
+import AdminFormNavbar from './components/AdminFormNavbar'
+import { AdminFormTabProvider } from './components/AdminFormTabProvider'
+
+/**
+ * Page for rendering subroutes via `Outlet` component for admin form pages.
+ */
+export const AdminFormLayout = (): JSX.Element => {
+  return (
+    <Flex flexDir="column" height="100vh" overflow="hidden" pos="relative">
+      <AdminFormTabProvider>
+        <AdminFormNavbar />
+      </AdminFormTabProvider>
+      <Box
+        overflow="auto"
+        flex={1}
+        py={{ base: '2.5rem', lg: '3.125rem' }}
+        px={{ base: '1.5rem', md: '1.75rem', lg: '2rem' }}
+        // Buffer for bottom navbar in mobile breakpoints.
+        mb={{ base: '4rem', md: 'initial' }}
+      >
+        <Outlet />
+      </Box>
+    </Flex>
+  )
+}
