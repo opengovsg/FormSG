3bbd728265c00c6542fe03afce63d774a7d444a8
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-09 15:46:11 +0800

    feat(aria): add label for others input

--- frontend/src/templates/Field/Checkbox/CheckboxField.tsx
+++ frontend/src/templates/Field/Checkbox/CheckboxField.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useMemo } from 'react'
 import { FieldError, useFormContext } from 'react-hook-form'
-import { FormControl, useMultiStyleConfig } from '@chakra-ui/react'
+import {
+  FormControl,
+  useMultiStyleConfig,
+  VisuallyHidden,
+} from '@chakra-ui/react'
 import { get } from 'lodash'
 
 import { CheckboxFieldBase, FormFieldWithId } from '~shared/types/field'
@@ -9,6 +13,7 @@ import { CHECKBOX_THEME_KEY } from '~theme/components/Checkbox'
 import { createCheckboxValidationRules } from '~utils/fieldValidation'
 import Checkbox from '~components/Checkbox'
 import FormErrorMessage from '~components/FormControl/FormErrorMessage'
+import FormLabel from '~components/FormControl/FormLabel'
 
 import { BaseFieldProps, FieldContainer } from '../FieldContainer'
 
@@ -112,7 +117,13 @@ export const CheckboxField = ({
               value={CHECKBOX_OTHERS_INPUT_VALUE}
               {...register(checkboxInputName, validationRules)}
             />
+            <VisuallyHidden>
+              <FormLabel id={`${schema._id}-others`}>
+                Enter value for "Others" option
+              </FormLabel>
+            </VisuallyHidden>
             <Checkbox.OthersInput
+              aria-labelledby={`${schema._id}-others`}
               {...register(othersInputName, othersValidationRules)}
             />
             <FormErrorMessage ml={styles.othersInput?.ml as string} mb={0}>
