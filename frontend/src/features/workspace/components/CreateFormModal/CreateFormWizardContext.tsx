70f327bcc5305513565a5a38de883bc2c49cab60
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-10 20:38:56 +0800

    feat: add initial CreateFormWizard context to store form state

---
+++ frontend/src/features/workspace/components/CreateFormModal/CreateFormWizardContext.tsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useState } from 'react'
+import { useForm, UseFormReturn } from 'react-hook-form'
+
+import { FormResponseMode } from '~shared/types/form/form'
+
+export enum CreateFormFlowStates {
+  Landing = 'landing',
+  Details = 'details',
+}
+
+type CreateFormWizardContextReturn = {
+  currentStep: CreateFormFlowStates
+  setCurrentStep: (step: CreateFormFlowStates) => void
+  formMethods: UseFormReturn<CreateFormWizardInputProps>
+}
+
+const CreateFormWizardContext = createContext<
+  CreateFormWizardContextReturn | undefined
+>(undefined)
+
+type BaseCreateFormWizardInputProps = {
+  formName: string
+  responseMode: FormResponseMode
+}
+
+interface CreateStorageFormWizardInputProps
+  extends BaseCreateFormWizardInputProps {
+  responseMode: FormResponseMode.Encrypt
+  lostAck: boolean
+}
+
+type CreateEmailFormWizardInputProps = BaseCreateFormWizardInputProps
+
+type CreateFormWizardInputProps =
+  | CreateStorageFormWizardInputProps
+  | CreateEmailFormWizardInputProps
+
+const useCreateFormWizardContext = (): CreateFormWizardContextReturn => {
+  const [currentStep, setCurrentStep] = useState(CreateFormFlowStates.Details)
+
+  const formMethods = useForm<CreateFormWizardInputProps>()
+
+  return { currentStep, setCurrentStep, formMethods }
+}
+
+export const CreateFormWizardProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element => {
+  const values = useCreateFormWizardContext()
+  return (
+    <CreateFormWizardContext.Provider value={values}>
+      {children}
+    </CreateFormWizardContext.Provider>
+  )
+}
+
+export const useCreateFormWizard = (): CreateFormWizardContextReturn => {
+  const context = useContext(CreateFormWizardContext)
+  if (!context) {
+    throw new Error(
+      `useCreateFormWizard must be used within a CreateFormWizardProvider component`,
+    )
+  }
+  return context
+}
