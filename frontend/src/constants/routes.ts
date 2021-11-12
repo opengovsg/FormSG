2328acb78575d066885156b1cf365d817043240e
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-13 02:34:08 +0800

    fix: remove invalid regex route path for public forms
    react router 6 removed regex path matching, will need to validate inside the route itself

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -3,11 +3,10 @@ export const LANDING_ROUTE = '/'
 export const ROOT_ROUTE = '/'
 export const LOGIN_ROUTE = '/login'
 
-const PUBLIC_FORM_PARAM = 'formId'
-/** Used for typing useParams in PublicFormPage. */
-export type PublicFormParam = { [PUBLIC_FORM_PARAM]: string }
-export const PUBLIC_FORM_REGEX =
-  `/:${PUBLIC_FORM_PARAM}([a-fA-F0-9]{24})` as const
+// Cannot use regex match in react-router@6, which means we need to validate
+// the regex in PublicFormPage.
+export const PUBLICFORM_ROUTE = '/:formId'
+export const PUBLICFORM_REGEX = /^([a-fA-F0-9]{24})$/
 
 export const ADMINFORM_ROUTE = '/admin/form'
 /** Build tab has no subroute, its the index admin form route. */
--- frontend/src/features/public-form/PublicFormPage.tsx
+++ frontend/src/features/public-form/PublicFormPage.tsx
@@ -1,5 +1,7 @@
+import { useParams } from 'react-router-dom'
 import { Flex } from '@chakra-ui/react'
 
+import { PUBLICFORM_REGEX } from '~constants/routes'
 import { HttpError } from '~services/ApiService'
 
 import FormFields from './components/FormFields'
@@ -8,9 +10,14 @@ import { PublicFormProvider } from './PublicFormContext'
 import { usePublicFormView } from './queries'
 
 export const PublicFormPage = (): JSX.Element => {
+  const { formId } = useParams()
   const { error } = usePublicFormView()
 
-  if (error instanceof HttpError && error.code === 404) {
+  if (
+    !formId ||
+    !PUBLICFORM_REGEX.test(formId) ||
+    (error instanceof HttpError && error.code === 404)
+  ) {
     return <div>404</div>
   }
 
