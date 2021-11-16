05e2abdf49bc29d677b1e114cae33035a82496ef
Jiayee Lim <jiayeerawr@gmail.com>
2021-11-16 17:37:35 +0800

    fix: fix Country is invalid schema type

--- src/app/models/field/countryField.ts
+++ src/app/models/field/countryField.ts
@@ -1,11 +1,10 @@
 import { Schema } from 'mongoose'
-import Country from 'shared/constants/countries'
 
 import { ICountryFieldSchema } from '../../../types'
 
 const createCountryFieldSchema = () => {
   return new Schema<ICountryFieldSchema>({
-    fieldOptions: [Country],
+    fieldOptions: [String],
   })
 }
 
