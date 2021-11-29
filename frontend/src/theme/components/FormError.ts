307f28d980cd704e3864ef8c518c7a627b1250cc
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-29 11:53:25 +0800

    style(v2): update line heights of various text styles (#2967)
    * style(textStyles): update line heights of various text styles
    
    * fix(FormErrorMessage): correct line height alignment of icon and msg
    
    * fix(FormFieldMessage): correct line height alignment of icon and msg
    
    * style(Footer): fix alignment and link margins due to new text style
    
    * feat: add mobile variants for display-1 and 2, update h1 spacing

--- frontend/src/theme/components/FormError.ts
+++ frontend/src/theme/components/FormError.ts
@@ -1,14 +1,12 @@
 import { ComponentMultiStyleConfig } from '@chakra-ui/theme'
 
-import { textStyles } from '../textStyles'
-
 export const FormError: ComponentMultiStyleConfig = {
   parts: ['text', 'icon'],
   baseStyle: {
     text: {
       color: 'danger.500',
       my: '0.5rem',
-      ...textStyles['body-2'],
+      textStyle: 'body-2',
     },
     icon: {
       marginEnd: '0.5em',
