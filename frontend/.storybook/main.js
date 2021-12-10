271a77f7d1def8ee45cd42c3aea5495d6e6a2af1
Kar Rui Lau <karrui.lau@gmail.com>
2021-12-10 10:43:34 +0800

    feat(storybook): use storyStoreV7 and emotionAlias feature flags
    no longer need to alias emotion, and we get better performance with storyStoreV7 flag
    
    (from 54s to 39s!)

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,5 +1,9 @@
 /* eslint-env node */
 module.exports = {
+  features: {
+    storyStoreV7: true,
+    emotionAlias: false,
+  },
   stories: [
     // Introduction stories set first so stories are ordered correctly.
     './introduction/Welcome/Welcome.stories.tsx',
@@ -20,22 +24,4 @@ module.exports = {
       },
     },
   ],
-  // webpackFinal setup retrieved from ChakraUI's own Storybook setup
-  // https://github.com/chakra-ui/chakra-ui/blob/main/.storybook/main.js
-  webpackFinal: async (storybookConfig) => {
-    // Required to sync aliases between storybook and overriden configs
-    return {
-      ...storybookConfig,
-      resolve: {
-        ...storybookConfig.resolve,
-        alias: {
-          ...storybookConfig.resolve.alias,
-          // Required so storybook knows where the npm package is to render ChakraUI components
-          // as this is not directly installed in package.json.
-          '@emotion/core': '@emotion/react',
-          'emotion-theming': '@emotion/react',
-        },
-      },
-    }
-  },
 }
