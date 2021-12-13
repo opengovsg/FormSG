75adee43c9b4de9a4e9bbcf6968414e9b9e377a3
Kar Rui Lau <karrui.lau@gmail.com>
2021-12-13 19:06:50 +0800

    style(Menu): update theme styling to better fit design

--- frontend/src/theme/components/Menu.ts
+++ frontend/src/theme/components/Menu.ts
@@ -1,29 +1,86 @@
 import { menuAnatomy as parts } from '@chakra-ui/anatomy'
+import { getColor } from '@chakra-ui/theme-tools'
 
 import { ComponentMultiStyleConfig } from '~theme/types'
 
+export type MenuVariant = 'outline' | 'clear'
+
 export const Menu: ComponentMultiStyleConfig<typeof parts> = {
   parts: parts.keys,
-  baseStyle: {
+  baseStyle: ({
+    colorScheme: c,
+    isStretch,
+    theme,
+    focusItemBorderColor: fc,
+  }) => ({
+    button: {
+      width: isStretch ? '100%' : undefined,
+      textAlign: 'left',
+      justifyContent: 'space-between',
+      _hover: {
+        bg: 'white',
+        color: `${c}.900`,
+      },
+      _active: {
+        bg: 'white',
+        color: `${c}.500`,
+        _hover: {
+          color: `${c}.900`,
+        },
+      },
+    },
     item: {
       padding: '0.75rem 1rem',
       fontWeight: '400',
       _hover: {
-        background: 'primary.100',
+        bg: 'primary.100',
         borderWidth: '0rem',
       },
       _focus: {
-        outlineWidth: '0.125rem',
-        outlineStyle: 'solid',
-        outlineColor: 'primary.500',
+        bg: 'primary.100',
+        boxShadow: `0 0 0 2px ${getColor(theme, fc)}`,
+        _active: {
+          bg: 'primary.200',
+        },
       },
       _active: {
-        background: 'primary.200',
-        fontWeight: '500',
+        bg: 'primary.200',
+        fontWeight: 500,
       },
     },
     list: {
       minWidth: '0rem',
     },
+  }),
+  variants: {
+    clear: {
+      button: {
+        minH: 'auto',
+        p: '0.25rem',
+        outline: 'none',
+        border: 'none',
+        boxShadow: 'none',
+      },
+    },
+    outline: ({ colorScheme: c, theme }) => {
+      return {
+        button: {
+          _hover: {
+            borderColor: `${c}.900`,
+          },
+          _active: {
+            boxShadow: `0 0 0 1px ${getColor(theme, `${c}.500`)}`,
+            _hover: {
+              boxShadow: `0 0 0 1px ${getColor(theme, `${c}.900`)}`,
+            },
+          },
+        },
+      }
+    },
+  },
+  defaultProps: {
+    colorScheme: 'secondary',
+    focusItemBorderColor: 'primary.500',
+    variant: 'outline',
   },
 }
