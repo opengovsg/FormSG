50b40b8b390b9137bcdbe8512403922783e09243
themogget <y.kaiyi@outlook.com>
2021-12-14 13:28:36 +0800

    comments

--- frontend/src/components/GovtMasthead/GovtMasthead.test.tsx
+++ frontend/src/components/GovtMasthead/GovtMasthead.test.tsx
@@ -7,21 +7,27 @@ import * as stories from './GovtMasthead.stories'
 const { GovtMastheadExpanded, GovtMastheadCollapsed } = composeStories(stories)
 
 it('should collapse if it is in expanded state', async () => {
+  // Arrange
   render(<GovtMastheadExpanded />)
 
+  // Act
   const howToIdentifyLink = screen.getByText(/how to identify/i)
   userEvent.click(howToIdentifyLink)
 
+  // Assert
   expect(
     screen.queryByTestId('govtMastheadExpandedContent'),
   ).not.toBeInTheDocument()
 })
 
 it('should expand if it is in expanded state', async () => {
+  // Arrange
   render(<GovtMastheadCollapsed />)
 
+  // Act
   const howToIdentifyLink = screen.getByText(/how to identify/i)
   userEvent.click(howToIdentifyLink)
 
+  // Assert
   expect(screen.getByTestId('govtMastheadExpandedContent')).toBeInTheDocument()
 })
