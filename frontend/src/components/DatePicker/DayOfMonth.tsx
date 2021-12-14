482ec30c7dd567f591246cb7d081be42a97039c8
Kar Rui Lau <karrui.lau@gmail.com>
2021-12-14 12:21:34 +0800

    ref(Date): move custom styling into theme styling

--- frontend/src/components/DatePicker/DayOfMonth.tsx
+++ frontend/src/components/DatePicker/DayOfMonth.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import {
   Box,
   BoxProps,
@@ -6,7 +5,6 @@ import {
   useMultiStyleConfig,
 } from '@chakra-ui/react'
 import { format } from 'date-fns'
-import { merge } from 'lodash'
 
 import { DATE_INPUT_THEME_KEY } from '~theme/components/DateInput'
 
@@ -52,43 +50,17 @@ export const DayOfMonth = forwardRef<DayOfMonthProps, 'button'>(
     },
     ref,
   ) => {
-    const styles = useMultiStyleConfig(DATE_INPUT_THEME_KEY, {})
-
-    const customStyles = useMemo(() => {
-      const todayColour =
-        // Hyphen rather than . because they will be placed inside var()
-        isOutsideCurrMonth || !isAvailable ? 'secondary-300' : 'primary-500'
-      return {
-        _hover: {
-          backgroundColor: isSelected ? 'primary.500' : 'primary.200',
-        },
-        backgroundColor: isSelected ? 'primary.500' : 'transparent',
-        color:
-          isOutsideCurrMonth || !isAvailable
-            ? 'secondary.300'
-            : isSelected
-            ? 'white'
-            : 'secondary.500',
-        // The !important for boxShadow is necessary because when Dayzed tries
-        // to focus an element when the data-focus-visible attributes are not present
-        // (e.g. when opening the datepicker by clicking rather than hitting space/enter),
-        // Chakra UI adds an overriding box-shadow: none style.
-        boxShadow: `inset 0 0 0 0.0625rem ${
-          isToday ? `var(--chakra-colors-${todayColour})` : 'transparent'
-        } !important`,
-        _focus: {
-          boxShadow: `inset 0 0 0 0.0625rem ${
-            isToday ? `var(--chakra-colors-${todayColour})` : 'transparent'
-          }, 0 0 0 0.25rem var(--chakra-colors-primary-300) !important`,
-        },
-      }
-    }, [isSelected, isAvailable, isToday, isOutsideCurrMonth])
+    const styles = useMultiStyleConfig(DATE_INPUT_THEME_KEY, {
+      isSelected,
+      isOutsideCurrMonth,
+      isToday,
+    })
 
     return (
       <Box
         as="button"
         {...props}
-        __css={merge(styles.dayOfMonth, customStyles)}
+        __css={styles.dayOfMonth}
         aria-label={format(date, "do 'of' MMMM',' EEEE")}
         disabled={!isAvailable}
         tabIndex={isFocusable ? 0 : -1}
--- frontend/src/theme/components/DateInput.ts
+++ frontend/src/theme/components/DateInput.ts
@@ -1,8 +1,10 @@
-import { ComponentMultiStyleConfig } from '@chakra-ui/theme'
+import { anatomy, getColor } from '@chakra-ui/theme-tools'
+
+import { ComponentMultiStyleConfig } from '~theme/types'
 
 export const DATE_INPUT_THEME_KEY = 'DateInput'
 
-const parts = [
+const parts = anatomy('dateinput').parts(
   'container', // overall container
   'monthYearSelectorContainer', // container for month, year dropdowns and arrows
   'monthYearDropdownContainer', // container for month, year dropdowns
@@ -12,66 +14,99 @@ const parts = [
   'dayNamesContainer', // container for names of days in the week
   'dayOfMonth', // container for single date
   'todayLinkContainer', // container for "Today" link
-]
+)
 
-export const DateInput: ComponentMultiStyleConfig = {
-  parts,
-  baseStyle: {
-    container: {
-      display: 'inline-block',
-      py: '1rem',
-    },
-    monthYearSelectorContainer: {
-      display: 'flex',
-      justifyContent: 'space-between',
-    },
-    monthYearDropdownContainer: {
-      display: 'flex',
-      justifyContent: 'flex-start',
-    },
-    monthArrowContainer: {
-      display: 'flex',
-      justifyContent: 'flex-end',
-    },
-    calendarContainer: {
-      py: '1rem',
-      borderY: '1px solid',
-      borderColor: 'neutral.300',
-    },
-    monthGrid: {
-      rowGap: '0.5rem',
-      display: 'inline-grid',
-      justifyItems: 'left',
-    },
-    dayNamesContainer: {
-      textStyle: 'subhead-2',
-      color: 'secondary.700',
-      margin: 'auto',
-    },
-    dayOfMonth: {
-      display: 'inline-block',
-      textStyle: 'body-1',
-      borderRadius: '1.5rem',
-      padding: {
-        base: 0,
-        md: 0.75,
+export const DateInput: ComponentMultiStyleConfig<typeof parts> = {
+  parts: parts.keys,
+  baseStyle: ({
+    isToday,
+    isOutsideCurrMonth,
+    isSelected,
+    colorScheme: c,
+    theme,
+  }) => {
+    return {
+      container: {
+        display: 'inline-block',
+        py: '1rem',
       },
-      w: {
-        base: '2rem',
-        md: '3rem',
+      monthYearSelectorContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
       },
-      h: {
-        base: '2rem',
-        md: '3rem',
+      monthYearDropdownContainer: {
+        display: 'flex',
+        justifyContent: 'flex-start',
       },
-      mx: '0.25rem',
-      _focus: {
+      monthArrowContainer: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+      },
+      calendarContainer: {
+        py: '1rem',
+        borderY: '1px solid',
+        borderColor: 'neutral.300',
+      },
+      monthGrid: {
+        rowGap: '0.5rem',
+        display: 'inline-grid',
+        justifyItems: 'left',
+      },
+      dayNamesContainer: {
+        textStyle: 'subhead-2',
+        color: 'secondary.700',
+        margin: 'auto',
+      },
+      dayOfMonth: {
+        transitionDuration: 'normal',
+        display: 'inline-block',
+        textStyle: 'body-1',
+        borderRadius: '1.5rem',
+        color: isSelected
+          ? 'white'
+          : isOutsideCurrMonth
+          ? 'secondary.300'
+          : 'secondary.500',
+        p: {
+          base: 0,
+          md: 0.75,
+        },
         outline: 'none',
+        border: '1px solid',
+        borderColor: isToday
+          ? isOutsideCurrMonth
+            ? 'secondary.300'
+            : `${c}.500`
+          : 'transparent',
+        bg: isSelected ? `${c}.500` : 'transparent',
+        _hover: {
+          bg: isSelected ? `${c}.500` : `${c}.200`,
+        },
+        _focus: {
+          boxShadow: `0 0 0 4px ${getColor(theme, `${c}.300`)}`,
+        },
+        _disabled: {
+          color: 'secondary.300',
+          cursor: 'not-allowed',
+          bg: 'transparent',
+        },
+        w: {
+          base: '2rem',
+          md: '3rem',
+        },
+        h: {
+          base: '2rem',
+          md: '3rem',
+        },
+        mx: '0.25rem',
       },
-    },
-    todayLinkContainer: {
-      textAlign: 'center',
-      pt: '1rem',
-    },
+      todayLinkContainer: {
+        textAlign: 'center',
+        pt: '1rem',
+      },
+    }
+  },
+  defaultProps: {
+    colorScheme: 'primary',
   },
 }
