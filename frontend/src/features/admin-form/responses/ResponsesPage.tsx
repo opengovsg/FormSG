ca4639c06718b3156c127b11314d788dfd0a7f1f
Dylan Ghee <59414467+zatkiller@users.noreply.github.com>
2021-12-15 16:39:14 +0800

    feat(v2): Set up web workers on React (#3125)
    * feat: set up web workers to receive messages
    
    * feat: update web workers teardown and typing
    
    * chore: add logging for easier debugging
    
    * chore: add finished running log
    
    * chore: remove unnecessary console log and keys
    
    * docs: Update JS docs
    
    * docs: Add comments for future implementation
    
    * fix: Update typo in comment
    
    * fix: remove await to unblock UI
    
    * fix: undo eslint rule update
    
    * feat: add worker-loader to craco
    
    * fix: revert addition of worker-loader to craco
    
    * docs: add comment on use of inline loader for worker-loader
    
    * feat: change Worker type to IWorker
    
    * feat: custom hook for decryption workers
    
    * feat: add typing for submission
    
    * feat: add useCallback and remove unneccessary comments
    
    * fix: remove workerInterface
    
    * fix: fix linting errors
    
    * fix: updated regressed npm dependencies
    
    * feat: add handleExportCsv to check for formId and title
    
    * refactor: generalize getFormSubmissionsMetadata

---
+++ frontend/src/features/admin-form/responses/ResponsesPage.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback, useState } from 'react'
+import { useParams } from 'react-router'
+import { Skeleton } from '@chakra-ui/react'
+
+import { StorageModeSubmissionMetadata } from '~shared/types/submission'
+
+import Button from '~components/Button'
+
+import { useAdminFormSettings } from '../settings/queries'
+
+import { useFormResponses } from './queries'
+import useDecryptionWorkers from './useDecryptionWorkers'
+
+const ResponsesPage = (): JSX.Element => {
+  const { data: settings } = useAdminFormSettings()
+  const { data, isLoading } = useFormResponses()
+  const { formId } = useParams()
+  const [secretKey, setSecretKey] = useState<string>('')
+  const { downloadEncryptedResponses } = useDecryptionWorkers()
+
+  const handleExportCsv = useCallback(() => {
+    console.log(formId, settings?.title)
+    if (!formId || !settings?.title) return
+    return downloadEncryptedResponses(formId, settings.title, secretKey)
+  }, [])
+
+  return (
+    <Skeleton isLoaded={!isLoading && !!data}>
+      <div>
+        Enter secret key:
+        <input
+          style={{ backgroundColor: 'grey' }}
+          type="text"
+          value={secretKey}
+          onChange={(e) => setSecretKey(e.target.value)}
+        />
+        <Button onClick={handleExportCsv}>Export csv</Button>
+        <Button>Export csv and attachments</Button>
+        {data?.metadata.map((submission: StorageModeSubmissionMetadata) => {
+          return (
+            <div key={submission.refNo}>
+              Submission Ref No: {submission.refNo}
+            </div>
+          )
+        })}
+      </div>
+    </Skeleton>
+  )
+}
+export default ResponsesPage
