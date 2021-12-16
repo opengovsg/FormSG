1d3d846c07b8bffa064ecea623ef44d3f7965732
Kar Rui Lau <karrui.lau@gmail.com>
2021-12-16 18:13:48 +0800

    feat: use table instead of grid for more semantic a11y

--- frontend/src/components/DatePicker/Calendar/CalendarPanel.tsx
+++ frontend/src/components/DatePicker/Calendar/CalendarPanel.tsx
@@ -1,4 +1,4 @@
-import { Box, forwardRef, SimpleGrid, Stack, useStyles } from '@chakra-ui/react'
+import { chakra, forwardRef, Stack, Td, useStyles } from '@chakra-ui/react'
 import { isSameDay } from 'date-fns'
 
 import { DAY_NAMES, generateClassNameForDate } from '../utils'
@@ -28,43 +28,65 @@ export const CalendarPanel = forwardRef<{}, 'button'>(
         {calendars.map((calendar, i) => (
           <Stack spacing={0} key={i}>
             <CalendarHeader monthOffset={i} />
-            <SimpleGrid
+            <chakra.table
               key={`${calendar.month}${calendar.year}`}
-              columns={DAY_NAMES.length}
               sx={styles.monthGrid}
             >
-              {DAY_NAMES.map((dayName, index) => (
-                <Box key={index} sx={styles.dayNamesContainer}>
-                  {dayName}
-                </Box>
-              ))}
-              {calendar.weeks.map((week, windex) =>
-                week.map((dateObj, index) => {
-                  if (!dateObj) {
-                    return (
-                      <Box
-                        key={`${calendar.month}${calendar.year}${windex}${index}`}
-                      />
-                    )
-                  }
+              <chakra.thead>
+                <chakra.tr>
+                  {DAY_NAMES.map(({ fullName, shortName }, index) => (
+                    <chakra.th
+                      key={index}
+                      abbr={fullName}
+                      sx={styles.dayNamesContainer}
+                    >
+                      {shortName}
+                    </chakra.th>
+                  ))}
+                </chakra.tr>
+              </chakra.thead>
+              <chakra.tbody>
+                {calendar.weeks.map((week, windex) => {
                   return (
-                    <DayOfMonth
-                      key={`${calendar.month}${calendar.year}${windex}${index}`}
-                      {...getDateProps({
-                        dateObj,
+                    <chakra.tr key={windex}>
+                      {week.map((dateObj, index) => {
+                        if (!dateObj) {
+                          return (
+                            <Td
+                              key={`${calendar.month}${calendar.year}${windex}${index}`}
+                            />
+                          )
+                        }
+                        return (
+                          <chakra.td
+                            key={`${calendar.month}${calendar.year}${windex}${index}`}
+                          >
+                            <DayOfMonth
+                              {...getDateProps({
+                                dateObj,
+                              })}
+                              dateObj={dateObj}
+                              isOutsideCurrMonth={
+                                dateObj.date.getMonth() !== calendar.month
+                              }
+                              className={generateClassNameForDate(
+                                uuid,
+                                dateObj.date,
+                              )}
+                              ref={
+                                isSameDay(dateObj.date, dateToFocus)
+                                  ? initialFocusRef
+                                  : undefined
+                              }
+                            />
+                          </chakra.td>
+                        )
                       })}
-                      dateObj={dateObj}
-                      className={generateClassNameForDate(uuid, dateObj.date)}
-                      ref={
-                        isSameDay(dateObj.date, dateToFocus)
-                          ? initialFocusRef
-                          : undefined
-                      }
-                    />
+                    </chakra.tr>
                   )
-                }),
-              )}
-            </SimpleGrid>
+                })}
+              </chakra.tbody>
+            </chakra.table>
           </Stack>
         ))}
       </Stack>
