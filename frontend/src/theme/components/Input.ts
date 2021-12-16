0a906b931d4c61efe4e013f76f85f9c334ec59b9
Kar Rui Lau <karrui.lau@gmail.com>
2021-12-17 00:13:23 +0800

    Revert "ref(Input): move some styles to baseStyle"
    This reverts commit 0b1d44a44064e371a09c00011a8b2db4e91aa1e0.

--- frontend/src/theme/components/Input.ts
+++ frontend/src/theme/components/Input.ts
@@ -12,6 +12,7 @@ const size = {
   },
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const outlineVariant = (props: StyleFunctionProps) => {
   const {
     theme,
@@ -24,43 +25,18 @@ const outlineVariant = (props: StyleFunctionProps) => {
   return {
     field: {
       bg: isPrefilled ? 'warning.100' : 'white',
+      textStyle: 'body-1',
       border: '1px solid',
       borderColor: isSuccess ? 'success.700' : 'neutral.400',
+      _placeholder: {
+        color: 'neutral.500',
+      },
       _hover: {
         borderColor: isSuccess ? 'success.700' : 'neutral.400',
       },
       _disabled: {
-        borderColor: 'neutral.400',
-      },
-      _invalid: {
-        // Remove extra 1px of outline.
-        borderColor: getColor(theme, ec),
-        boxShadow: 'none',
-      },
-      _focus: {
-        borderColor: getColor(theme, fc),
-        _hover: {
-          borderColor: getColor(theme, fc),
-        },
-        boxShadow: `0 0 0 1px ${getColor(theme, fc)}`,
-      },
-    },
-  }
-}
-
-// Additional success part.
-const parts = inputAnatomy.extend('success')
-
-export const Input = {
-  parts: parts.keys,
-  baseStyle: {
-    field: {
-      textStyle: 'body-1',
-      _placeholder: {
-        color: 'neutral.500',
-      },
-      _disabled: {
         bg: 'neutral.200',
+        borderColor: 'neutral.400',
         color: 'neutral.500',
         cursor: 'not-allowed',
         opacity: 1,
@@ -71,13 +47,29 @@ export const Input = {
           bg: 'neutral.200',
         },
       },
+      _invalid: {
+        // Remove extra 1px of outline.
+        borderColor: getColor(theme, ec),
+        boxShadow: 'none',
+      },
+      _focus: {
+        borderColor: getColor(theme, fc),
+        boxShadow: `0 0 0 1px ${getColor(theme, fc)}`,
+      },
     },
     success: {
       pointerEvents: 'none',
       fontSize: '1.25rem',
       color: 'success.700',
     },
-  },
+  }
+}
+
+// Additional success part.
+const parts = inputAnatomy.extend('success')
+
+export const Input = {
+  parts: parts.keys,
   variants: {
     outline: outlineVariant,
   },
