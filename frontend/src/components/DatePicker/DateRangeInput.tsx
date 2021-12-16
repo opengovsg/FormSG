d2b322bee433ce0d4cfd1cf7a9710c1dafa3e27c
Kar Rui Lau <karrui.lau@gmail.com>
2021-12-16 15:33:46 +0800

    feat: add logic (and styling) for handling controlled date selection

--- frontend/src/components/DatePicker/DateRangeInput.tsx
+++ frontend/src/components/DatePicker/DateRangeInput.tsx
@@ -1,5 +1,14 @@
-import { useCallback, useRef, useState } from 'react'
 import {
+  ChangeEventHandler,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import {
+  Box,
+  chakra,
+  CSSObject,
   Flex,
   forwardRef,
   Popover,
@@ -10,57 +19,63 @@ import {
   PopoverTrigger,
   Portal,
   Text,
+  useMultiStyleConfig,
 } from '@chakra-ui/react'
+import { compareAsc, format } from 'date-fns'
 
 import { BxCalendar } from '~assets/icons'
 import IconButton from '~components/IconButton'
 
-import Input, { InputProps } from '../Input'
-
 import { DateRangePicker } from './DateRangePicker'
 
-export interface DateRangeInputProps
-  extends Omit<InputProps, 'value' | 'onChange'> {
-  name: string
-  value?: string
-  onChange?: (val: string) => void
+export interface DateRangeInputProps {
+  value?: Date[]
+  onChange?: (val: Date[]) => void
 }
 
 export const DateRangeInput = forwardRef<DateRangeInputProps, 'input'>(
-  ({ onChange, value = '', ...props }, ref) => {
+  ({ onChange, value = [] }, ref) => {
     const initialFocusRef = useRef<HTMLInputElement>(null)
 
-    const [selectedDates, setSelectedDates] = useState<Date[]>([])
     const [hoveredDate, setHoveredDate] = useState<Date | null>(null)
 
-    const handleOnDateSelected = (date: Date) => {
-      const newDates = [...selectedDates]
-      if (selectedDates.length) {
-        if (selectedDates.length === 1) {
-          const firstTime = selectedDates[0]
-          if (firstTime < date) {
-            newDates.push(date)
-          } else {
-            newDates.unshift(date)
+    const inputStyles = useMultiStyleConfig('Input', {})
+
+    /**
+     * Handles date selection in calendar panel.
+     * Calls onChange prop (if provided) with sorted dates.
+     * @param date the new date selected
+     */
+    const handleOnDateSelected = useCallback(
+      (date: Date) => {
+        let newDates = value.slice()
+        if (value.length) {
+          if (value.length === 1) {
+            const firstTime = value[0]
+            if (firstTime < date) {
+              newDates.push(date)
+            } else {
+              newDates.unshift(date)
+            }
+          } else if (newDates.length === 2) {
+            newDates = [date]
           }
-          setSelectedDates(newDates)
-        } else if (newDates.length === 2) {
-          setSelectedDates([date])
+        } else {
+          newDates.push(date)
         }
-      } else {
-        newDates.push(date)
-        setSelectedDates(newDates)
-      }
-    }
+        onChange?.(newDates.sort(compareAsc))
+      },
+      [onChange, value],
+    )
 
     const isDateInRange = useCallback(
       (date: Date) => {
-        if (!selectedDates?.length) {
+        if (!value?.length) {
           return false
         }
-        const firstSelected = selectedDates[0]
-        if (selectedDates.length === 2) {
-          const secondSelected = selectedDates[1]
+        const firstSelected = value[0]
+        if (value.length === 2) {
+          const secondSelected = value[1]
           return firstSelected < date && secondSelected > date
         } else {
           return (
@@ -70,40 +85,109 @@ export const DateRangeInput = forwardRef<DateRangeInputProps, 'input'>(
           )
         }
       },
-      [hoveredDate, selectedDates],
+      [hoveredDate, value],
     )
 
     const onMouseEnterHighlight = useCallback(
       (date: Date) => {
-        if (!selectedDates?.length) {
+        if (!value?.length) {
           return
         }
         setHoveredDate(date)
       },
-      [selectedDates?.length],
+      [value?.length],
     )
 
     const onMouseLeaveCalendar = useCallback(() => {
       setHoveredDate(null)
     }, [])
 
+    const handleStartDateChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+      const dateFromValue = new Date(e.target.value)
+      let clonedValue = value.slice()
+
+      if (isNaN(dateFromValue.getTime())) {
+        if (clonedValue.length > 0) {
+          clonedValue = []
+        }
+      } else {
+        clonedValue[0] = dateFromValue
+      }
+
+      onChange?.(clonedValue)
+    }
+
+    const startDateRenderedValue = useMemo(() => {
+      return value[0] ? format(value[0], 'yyyy-MM-dd') ?? '' : ''
+    }, [value])
+
+    const handleEndDateChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+      const dateFromValue = new Date(e.target.value)
+      const clonedValue = value.slice()
+      if (isNaN(dateFromValue.getTime())) {
+        if (clonedValue.length === 2) {
+          clonedValue.pop()
+        }
+      } else {
+        clonedValue[1] = dateFromValue
+      }
+
+      onChange?.(clonedValue)
+    }
+
+    const endDateRenderedValue = useMemo(() => {
+      return value[1] ? format(value[1], 'yyyy-MM-dd') ?? '' : ''
+    }, [value])
+
     return (
       <Flex>
-        <Input
-          type="date"
-          sx={{
-            borderRadius: '4px 0 0 4px',
-            // Chrome displays a default calendar icon, which we want to hide
-            // so all browsers display our icon consistently.
-            '::-webkit-calendar-picker-indicator': {
-              display: 'none',
-            },
-          }}
-          onChange={(e) => onChange?.(e.target.value)}
-          ref={ref}
-          value={value}
-          {...props}
-        />
+        <Box
+          alignItems="center"
+          display="flex"
+          __css={inputStyles.field}
+          borderEndRadius={0}
+          _focusWithin={
+            (inputStyles.field as Record<string, CSSObject>)?._focus
+          }
+        >
+          <chakra.input
+            aria-label="Start date"
+            type="date"
+            sx={{
+              // Chrome displays a default calendar icon, which we want to hide
+              // so all browsers display our icon consistently.
+              '::-webkit-calendar-picker-indicator': {
+                display: 'none',
+              },
+            }}
+            onChange={handleStartDateChange}
+            value={startDateRenderedValue}
+            ref={ref}
+          />
+          <Text textStyle="body-1" color="secondary.400" pr="1.5rem">
+            to
+          </Text>
+          <chakra.input
+            aria-label="End date"
+            type="date"
+            _disabled={{
+              opacity: 0.5,
+              bg: 'transparent',
+              cursor: 'not-allowed',
+            }}
+            sx={{
+              // Chrome displays a default calendar icon, which we want to hide
+              // so all browsers display our icon consistently.
+              '::-webkit-calendar-picker-indicator': {
+                display: 'none',
+              },
+            }}
+            disabled={!startDateRenderedValue}
+            onChange={handleEndDateChange}
+            value={endDateRenderedValue}
+            ref={ref}
+          />
+        </Box>
         <Popover
           placement="bottom-end"
           initialFocusRef={initialFocusRef}
@@ -130,14 +214,14 @@ export const DateRangeInput = forwardRef<DateRangeInputProps, 'input'>(
                   <PopoverHeader py="1rem" px="1.5rem">
                     <Flex justifyContent="space-between" alignItems="center">
                       <Text textStyle="subhead-2" color="secondary.500">
-                        Select a date
+                        Select date range
                       </Text>
                       <PopoverCloseButton position="static" />
                     </Flex>
                   </PopoverHeader>
                   <PopoverBody p={0}>
                     <DateRangePicker
-                      selectedDates={selectedDates}
+                      selectedDates={value}
                       hoveredDate={hoveredDate}
                       isDateInRange={isDateInRange}
                       onMouseEnterHighlight={onMouseEnterHighlight}
