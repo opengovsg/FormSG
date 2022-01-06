deed4a43ed8ef71aec71c1766932866b42f0ca6e
Kar Rui Lau <karrui.lau@gmail.com>
2022-01-06 15:04:20 +0800

    feat: add query to retrieve free sms quota

--- frontend/src/features/admin-form/common/queries.ts
+++ frontend/src/features/admin-form/common/queries.ts
@@ -5,11 +5,13 @@ import { AdminFormDto } from '~shared/types/form/form'
 
 import { ApiError } from '~typings/core'
 
-import { getAdminFormView } from './AdminViewFormService'
+import { getAdminFormView, getFreeSmsQuota } from './AdminViewFormService'
 
 export const adminFormKeys = {
   base: ['adminForm'] as const,
   id: (id: string) => ['adminForm', id] as const,
+  freeSmsCount: (id: string) =>
+    [...adminFormKeys.id(id), 'freeSmsCount'] as const,
 }
 
 /**
@@ -20,4 +22,13 @@ export const useAdminForm = (): UseQueryResult<AdminFormDto, ApiError> => {
   if (!formId) throw new Error('No formId provided')
 
   return useQuery(adminFormKeys.id(formId), () => getAdminFormView(formId))
+}
+
+export const useFreeSmsQuota = () => {
+  const { formId } = useParams()
+  if (!formId) throw new Error('No formId provided')
+
+  return useQuery(adminFormKeys.freeSmsCount(formId), () =>
+    getFreeSmsQuota(formId),
+  )
 }
