2a866c2c53791f834c48517c99672f8ac5129c73
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-03 15:44:39 +0800

    feat: render placeholder logic page when logic tab is active

--- frontend/src/features/admin-form-builder/FormBuilderPage.tsx
+++ frontend/src/features/admin-form-builder/FormBuilderPage.tsx
@@ -1,15 +1,15 @@
 import { Flex } from '@chakra-ui/react'
 
+import { BuilderContent } from './common/components/BuilderContent'
 import { BuilderSidebar } from './common/components/BuilderSidebar'
 import { BuilderDrawerProvider } from './BuilderDrawerContext'
-import BuilderDesign from './design'
 
 export const FormBuilderPage = (): JSX.Element => {
   return (
     <Flex h="100%" w="100%" overflow="auto" bg="neutral.200" direction="row">
       <BuilderDrawerProvider>
         <BuilderSidebar />
-        <BuilderDesign />
+        <BuilderContent />
       </BuilderDrawerProvider>
     </Flex>
   )
