e7622ee1541e094862a3e14d023605f664c3354d
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-05 16:40:25 +0800

    feat: add a11y label to selected item removal

--- frontend/src/components/Combobox/MultiSelect/SelectedItemTag.tsx
+++ frontend/src/components/Combobox/MultiSelect/SelectedItemTag.tsx
@@ -1,16 +1,61 @@
 import { BiX } from 'react-icons/bi'
 import {
+  chakra,
   forwardRef,
+  HTMLChakraProps,
   Icon,
+  SystemStyleObject,
   Tag,
-  TagCloseButton,
   TagLabel,
   TagProps,
+  useStyles,
 } from '@chakra-ui/react'
 
 export interface SelectedItemTagProps extends TagProps {
   label: string
   onRemove: () => void
+}
+
+export interface SelectedItemTagCloseButtonProps
+  extends Omit<HTMLChakraProps<'button'>, 'disabled'> {
+  isDisabled?: boolean
+}
+
+/** Not using Chakra's TagCloseButton due to inability to override aria-label */
+const SelectedItemTagCloseButton = ({
+  isDisabled,
+  children,
+  ...rest
+}: SelectedItemTagCloseButtonProps): JSX.Element => {
+  const styles = useStyles()
+
+  const btnStyles: SystemStyleObject = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    outline: '0',
+    ...styles.closeButton,
+    opacity: 0.8,
+    color: 'secondary.500',
+    _focus: {
+      boxShadow: '0 0 0 2px var(--chakra-colors-secondary-300)',
+      bg: 'secondary.200',
+    },
+    _hover: { opacity: 0.9, color: 'secondary.600' },
+    _active: { opacity: 1, color: 'secondary.700', bg: 'secondary.200' },
+  }
+
+  return (
+    <chakra.button
+      type="button"
+      aria-label="Remove selected option"
+      disabled={isDisabled}
+      __css={btnStyles}
+      {...rest}
+    >
+      <Icon as={BiX} fontSize="1.5rem" />
+    </chakra.button>
+  )
 }
 
 export const SelectedItemTag = forwardRef<SelectedItemTagProps, 'div'>(
@@ -28,19 +73,7 @@ export const SelectedItemTag = forwardRef<SelectedItemTagProps, 'div'>(
         <TagLabel isTruncated ref={ref} textStyle="body-2">
           {label}
         </TagLabel>
-        <TagCloseButton
-          onClick={onRemove}
-          opacity={0.8}
-          color="secondary.500"
-          _focus={{
-            boxShadow: '0 0 0 2px var(--chakra-colors-secondary-300)',
-            bg: 'secondary.200',
-          }}
-          _hover={{ opacity: 0.9, color: 'secondary.600' }}
-          _active={{ opacity: 1, color: 'secondary.700', bg: 'secondary.200' }}
-        >
-          <Icon as={BiX} fontSize="1.5rem" />
-        </TagCloseButton>
+        <SelectedItemTagCloseButton onClick={onRemove} />
       </Tag>
     )
   },
