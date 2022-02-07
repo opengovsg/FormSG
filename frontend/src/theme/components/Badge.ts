d50ed01c96025c77a164fd8e957ad13bee56a9b0
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-07 22:20:30 +0800

    ref(Badge): update Badge theme to be exportable

--- frontend/src/theme/components/Badge.ts
+++ frontend/src/theme/components/Badge.ts
@@ -1,41 +1,52 @@
-import { ComponentStyleConfig } from '~theme/types'
+import { SystemStyleFunction, SystemStyleObject } from '@chakra-ui/theme-tools'
 
 import { textStyles } from '../textStyles'
 
 export type BadgeVariants = 'solid' | 'subtle'
 
-export const Badge: ComponentStyleConfig = {
-  baseStyle: {
-    ...textStyles['caption-1'],
-    textTransform: 'initial',
-  },
-  variants: {
-    solid: ({ colorScheme: c }) => {
-      const textColor = c === 'secondary' ? 'white' : 'secondary.700'
+const baseStyle: SystemStyleObject = {
+  ...textStyles['caption-1'],
+  textTransform: 'initial',
+}
 
-      return {
-        color: textColor,
-        bgColor: `${c}.400`,
-      }
-    },
-    subtle: ({ colorScheme: c }) => {
-      const textColor = ['primary', 'secondary'].includes(c ?? '')
-        ? `${c}.500`
-        : `${c}.800`
+const variantSolid: SystemStyleFunction = (props) => {
+  const { colorScheme: c } = props
+  const textColor = c === 'secondary' ? 'white' : 'secondary.700'
 
-      return {
-        bgColor: `${c}.100`,
-        color: textColor,
-      }
-    },
+  return {
+    color: textColor,
+    bgColor: `${c}.400`,
+  }
+}
+const variantSubtle: SystemStyleFunction = (props) => {
+  const { colorScheme: c } = props
+  const textColor = ['primary', 'secondary'].includes(c ?? '')
+    ? `${c}.500`
+    : `${c}.800`
+
+  return {
+    bgColor: `${c}.100`,
+    color: textColor,
+  }
+}
+
+const variants = {
+  solid: variantSolid,
+  subtle: variantSubtle,
+}
+
+const sizes: Record<string, SystemStyleObject> = {
+  md: {
+    py: '0.25rem',
+    px: '0.5rem',
+    borderRadius: '4px',
   },
-  sizes: {
-    md: {
-      py: '0.25rem',
-      px: '0.5rem',
-      borderRadius: '4px',
-    },
-  },
+}
+
+export const Badge = {
+  baseStyle,
+  variants,
+  sizes,
   defaultProps: {
     variant: 'solid',
     size: 'md',
