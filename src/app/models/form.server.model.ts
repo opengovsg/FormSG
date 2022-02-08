7d25e8d65040b8a9a6a8c8519885703b0de7842b
tshuli <shuli@open.gov.sg>
2022-02-08 14:23:07 +0800

    nit: collapse webhook length check

--- src/app/models/form.server.model.ts
+++ src/app/models/form.server.model.ts
@@ -885,8 +885,7 @@ const compileFormModel = (db: Mongoose): IFormModel => {
     // Webhooks only allowed if encrypt mode
     if (
       this.responseMode !== FormResponseMode.Encrypt &&
-      this.webhook?.url &&
-      this.webhook.url.length > 0
+      this.webhook?.url?.length > 0
     ) {
       const validationError = this.invalidate(
         'webhook',
