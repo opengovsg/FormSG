419ba9d3ecc4001bbc894ba95f7504950c420c62
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-08 12:12:15 +0800

    feat(story): update story to render logic page when there is logic

--- frontend/src/features/admin-form-builder/logic/BuilderLogic.stories.tsx
+++ frontend/src/features/admin-form-builder/logic/BuilderLogic.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from '@storybook/react'
 
+import { AttachmentSize, BasicField } from '~shared/types/field'
 import {
   AdminFormDto,
   LogicConditionState,
@@ -22,8 +23,8 @@ export default {
   title: 'Pages/AdminFormPage/Logic',
   component: BuilderLogic,
   decorators: [
-    StoryRouter({ initialEntries: ['/12345'], path: '/:formId' }),
     (storyFn) => <BuilderLogicProvider>{storyFn()}</BuilderLogicProvider>,
+    StoryRouter({ initialEntries: ['/12345'], path: '/:formId' }),
   ],
   parameters: {
     layout: 'fullscreen',
@@ -42,18 +43,83 @@ NoLogic.parameters = {
 export const WithLogic = Template.bind({})
 WithLogic.parameters = {
   msw: buildMswRoutes({
+    form_fields: [
+      {
+        title: 'Do you want to upload an attachment?',
+        description: '',
+        required: true,
+        disabled: false,
+        fieldType: BasicField.YesNo,
+        _id: '620115cf3bc125001349f9c3',
+      },
+      {
+        title: 'Are you really sure you want to upload an attachment?',
+        description: '',
+        required: true,
+        disabled: false,
+        fieldType: BasicField.YesNo,
+        _id: '620115cf3bc125001349f9c6',
+      },
+      {
+        title: 'Upload instructions',
+        description: '',
+        required: true,
+        disabled: false,
+        fieldType: BasicField.Image,
+        _id: '6200e1534ad4f00012848d65',
+        url: 'some-mock-url',
+        fileMd5Hash: 'wrjH62qBTpg0uIk4GMzOCA==',
+        name: 'Upload instructions.png',
+        size: '0.03 MB',
+      },
+      {
+        title: 'Upload attachment',
+        description: '',
+        required: true,
+        disabled: false,
+        fieldType: BasicField.Attachment,
+        _id: '61e6857c9c794b0012f1c6f7',
+        attachmentSize: AttachmentSize.OneMb,
+      },
+    ],
     form_logics: [
       {
+        show: ['6200e1534ad4f00012848d65', '61e6857c9c794b0012f1c6f7'],
+        _id: '620115f74ad4f00012900a8c',
         logicType: LogicType.ShowFields,
         conditions: [
           {
-            field: 'some-field',
+            ifValueType: LogicIfValue.SingleSelect,
+            field: '620115cf3bc125001349f9c3',
             state: LogicConditionState.Equal,
-            value: 0,
-            ifValueType: LogicIfValue.Number,
+            value: 'Yes',
+          },
+          {
+            ifValueType: LogicIfValue.SingleSelect,
+            field: '620115cf3bc125001349f9c6',
+            state: LogicConditionState.Equal,
+            value: 'Yes',
           },
         ],
-        show: ['another-field'],
+      },
+      {
+        show: ['6200e1534ad4f00012848d65', '61e6857c9c794b0012f1c6f7'],
+        _id: '620115f74ad4f00012900a8d',
+        logicType: LogicType.ShowFields,
+        conditions: [
+          {
+            ifValueType: LogicIfValue.SingleSelect,
+            field: '620115cf3bc125001349f9c3',
+            state: LogicConditionState.Equal,
+            value: 'Yes',
+          },
+          {
+            ifValueType: LogicIfValue.SingleSelect,
+            field: '620115cf3bc125001349f9c6',
+            state: LogicConditionState.Equal,
+            value: 'Yes',
+          },
+        ],
       },
     ],
   }),
