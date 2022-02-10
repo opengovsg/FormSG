c85a72308be85ce3467ac402366fea7ff8ed933c
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-10 23:37:43 +0800

    feat(SingleSelectProvider): add provider for sharing context

---
+++ frontend/src/components/Dropdown/SingleSelectProvider.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useMemo } from 'react'
+import { useCombobox } from 'downshift'
+
+import { useItems } from './hooks/useItems'
+import { itemToValue } from './utils/itemUtils'
+import { SelectContext } from './SelectContext'
+import { ComboboxItem } from './types'
+
+export interface SingleSelectProviderProps<
+  Item = ComboboxItem,
+  Value = string,
+> {
+  items: Item[]
+  selectedValue: Value
+  onValueChange: (value: Value) => void
+  children: React.ReactNode
+}
+export const SingleSelectProvider = ({
+  items: rawItems,
+  selectedValue,
+  onValueChange,
+  children,
+}: SingleSelectProviderProps): JSX.Element => {
+  const { items, getItemByValue } = useItems(rawItems)
+
+  const selectedItem = useMemo(
+    () => getItemByValue(selectedValue)?.item,
+    [getItemByValue, selectedValue],
+  )
+
+  const isItemSelected = useCallback(
+    (item: ComboboxItem) => {
+      return itemToValue(selectedItem) === itemToValue(item)
+    },
+    [selectedItem],
+  )
+
+  const { toggleMenu, isOpen } = useCombobox({
+    items,
+    selectedItem,
+    onSelectedItemChange: ({ selectedItem }) => {
+      onValueChange(itemToValue(selectedItem))
+    },
+  })
+
+  return (
+    <SelectContext.Provider
+      value={{
+        isOpen,
+        selectedItem,
+        isItemSelected,
+        toggleMenu,
+      }}
+    >
+      {children}
+    </SelectContext.Provider>
+  )
+}
