7da1b5c87540d398a72fc9f93202c7f17c2747c3
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-10 23:36:37 +0800

    feat: add useItems hook to normalize initial items

---
+++ frontend/src/components/Dropdown/hooks/useItems.ts
@@ -0,0 +1,75 @@
+// Hook to normalize item array for easy filtering and checking of state
+
+import { useCallback, useMemo } from 'react'
+
+import { ComboboxItem } from '../types'
+import { itemToValue } from '../utils/itemUtils'
+
+type ItemWithIndex<Item extends ComboboxItem = ComboboxItem> = {
+  item: Item
+  index: number
+}
+
+type UseItemsReturn<
+  Item extends ComboboxItem = ComboboxItem,
+  Value extends string = string,
+> = {
+  items: Item[]
+  normalized: {
+    allValues: string[]
+    byValue: Record<Value, ItemWithIndex<Item>>
+  }
+}
+
+export const useItems = <
+  Item extends ComboboxItem = ComboboxItem,
+  Value extends string = string,
+>(
+  rawItems: Item[],
+) => {
+  const normalizedItems = useMemo(() => {
+    const initialStore: UseItemsReturn<Item> = {
+      // All selectable items
+      items: [],
+
+      // Normalized store for filtering and retrieval of state
+      normalized: {
+        allValues: [],
+        byValue: {},
+      },
+    }
+
+    let itemIndex = 0
+
+    return rawItems.reduce((store, item) => {
+      const value = itemToValue(item)
+      // Do nothing if no value.
+      if (!value) {
+        return store
+      }
+
+      store.items.push(item)
+      store.normalized.allValues.push(value)
+      store.normalized.byValue[value] = {
+        item,
+        index: itemIndex,
+      }
+
+      // Only increment if item has a value.
+      itemIndex++
+      return store
+    }, initialStore)
+  }, [rawItems])
+
+  const getItemByValue = useCallback(
+    (value: Value): ItemWithIndex<Item> | null => {
+      return normalizedItems.normalized.byValue[value] ?? null
+    },
+    [normalizedItems.normalized.byValue],
+  )
+
+  return {
+    items: normalizedItems.items,
+    getItemByValue,
+  }
+}
---
+++ frontend/src/components/Dropdown/utils/itemUtils.ts
@@ -0,0 +1,46 @@
+import { isString } from '@chakra-ui/utils'
+
+import { ComboboxItem } from '../types'
+
+const itemIsObject = (
+  item: ComboboxItem,
+): item is Exclude<ComboboxItem, string | null> => {
+  return !!item && !isString(item)
+}
+
+export const itemToValue = <Item extends ComboboxItem>(item?: Item): string => {
+  if (!item) return ''
+  if (!itemIsObject(item)) {
+    return item ?? ''
+  }
+  return item.value
+}
+
+export const itemToLabelString = <Item extends ComboboxItem>(
+  item?: Item,
+): string => {
+  if (!item) return ''
+  if (!itemIsObject(item)) {
+    return item ?? ''
+  }
+  return item.label ?? item.value
+}
+
+export const itemToIcon = <Item extends ComboboxItem>(item?: Item) => {
+  if (!item || !itemIsObject(item)) {
+    return undefined
+  }
+  return item.icon
+}
+
+export const isItemDisabled = <Item extends ComboboxItem>(
+  item: Item,
+): boolean => {
+  return itemIsObject(item) && !!item.disabled
+}
+
+export const itemToDescriptionString = <Item extends ComboboxItem>(
+  item: Item,
+): string | undefined => {
+  return itemIsObject(item) ? item.description : undefined
+}
