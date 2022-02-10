4c8ab30e7d59d0d01a87342ab8c5322482d76498
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-10 23:38:02 +0800

    feat: add placeholder SelectCombobox/Menu components for rendering

---
+++ frontend/src/components/Dropdown/elements/SelectCombobox.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SelectComboboxProps {}
+
+export const SelectCombobox = ({}: SelectComboboxProps): JSX.Element => {
+  return <div>Combobox</div>
+}
---
+++ frontend/src/components/Dropdown/elements/SelectMenu.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SelectMenuProps {}
+
+export const SelectMenu = ({}: SelectMenuProps): JSX.Element => {
+  return <div>Select menu</div>
+}
