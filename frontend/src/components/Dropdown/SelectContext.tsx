134fe307ecc27f84034cf2a6fd6980e72f8bef4e
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-11 01:52:10 +0800

    feat: update props passed into SelectContext and SingleSelectProvider

--- frontend/src/components/Dropdown/SelectContext.tsx
+++ frontend/src/components/Dropdown/SelectContext.tsx
@@ -1,16 +1,36 @@
 import { createContext, useContext } from 'react'
+import { FormControlOptions } from '@chakra-ui/react'
 import { UseComboboxPropGetters, UseComboboxState } from 'downshift'
 
 import { ComboboxItem } from './types'
 
+export interface SharedSelectContextReturnProps<
+  Item extends ComboboxItem = ComboboxItem,
+> {
+  /** Set to true to enable search, defaults to `true` */
+  isSearchable?: boolean
+  /** Set to true to allow clearing of input, defaults to `true` */
+  isClearable?: boolean
+  /** Nothing found label. Defaults to "No matching results" */
+  nothingFoundLabel?: React.ReactNode
+  /** aria-label for clear button. Defaults to "Clear dropdown" */
+  clearButtonLabel?: string
+  /** Placeholder to show in the input field. Defaults to "Select an option". */
+  placeholder?: string
+  /** ID of input itself, for a11y purposes */
+  name: string
+  /** Item data used to render items in dropdown */
+  items: Item[]
+}
+
 interface SelectContextReturn<Item extends ComboboxItem = ComboboxItem>
   extends UseComboboxPropGetters<Item>,
-    Partial<UseComboboxState<Item>> {
-  isOpen: boolean
+    UseComboboxState<Item>,
+    Required<SharedSelectContextReturnProps<Item>>,
+    FormControlOptions {
   isItemSelected: (item: ComboboxItem) => boolean
   toggleMenu: () => void
-  items: Item[]
-  nothingFoundLabel?: React.ReactNode
+  selectItem: (item: Item) => void
 }
 
 export const SelectContext = createContext<SelectContextReturn | undefined>(
--- frontend/src/components/Dropdown/SingleSelectProvider.tsx
+++ frontend/src/components/Dropdown/SingleSelectProvider.tsx
@@ -1,27 +1,43 @@
 import { useCallback, useMemo } from 'react'
+import { FormControlOptions } from '@chakra-ui/react'
 import { useCombobox } from 'downshift'
 
 import { useItems } from './hooks/useItems'
 import { defaultFilter } from './utils/defaultFilter'
 import { itemToLabelString, itemToValue } from './utils/itemUtils'
-import { SelectContext } from './SelectContext'
+import { SelectContext, SharedSelectContextReturnProps } from './SelectContext'
 import { ComboboxItem } from './types'
 
-export interface SingleSelectProviderProps<Item = ComboboxItem> {
-  items: Item[]
+export interface SingleSelectProviderProps<
+  Item extends ComboboxItem = ComboboxItem,
+> extends SharedSelectContextReturnProps<Item>,
+    FormControlOptions {
+  /** Controlled input value */
   value: string
+  /** Controlled input onChange handler */
   onChange: (value: string) => void
+  /** Function based on which items in dropdown are filtered */
   filter?(items: Item[], value: string): Item[]
-  /** Nothing found label. Defaults to "No matching results" */
-  nothingFoundLabel?: React.ReactNode
+  /** Initial dropdown opened state */
+  defaultIsOpen?: boolean
   children: React.ReactNode
 }
 export const SingleSelectProvider = ({
   items: rawItems,
   value,
   onChange,
+  name,
   filter = defaultFilter,
   nothingFoundLabel = 'No matching results',
+  placeholder = 'Select an option',
+  clearButtonLabel = 'Clear dropdown',
+  isClearable = true,
+  isSearchable = true,
+  defaultIsOpen,
+  isInvalid,
+  isReadOnly,
+  isDisabled,
+  isRequired,
   children,
 }: SingleSelectProviderProps): JSX.Element => {
   const { items, getItemByValue } = useItems({ rawItems })
@@ -51,6 +67,7 @@ export const SingleSelectProvider = ({
   } = useCombobox({
     items: filteredItems,
     inputValue: value,
+    defaultIsOpen,
     onInputValueChange: ({ inputValue }) => {
       if (!inputValue) {
         selectItem(null)
@@ -84,10 +101,20 @@ export const SingleSelectProvider = ({
         getLabelProps,
         getMenuProps,
         getToggleButtonProps,
+        selectItem,
         highlightedIndex,
         items: filteredItems,
         nothingFoundLabel,
         inputValue: value,
+        isSearchable,
+        isClearable,
+        isInvalid,
+        isDisabled,
+        isReadOnly,
+        isRequired,
+        name,
+        clearButtonLabel,
+        placeholder,
       }}
     >
       {children}
