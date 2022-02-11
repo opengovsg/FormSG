0ed1287fc42ee49c9a418d91bed6478a687a1fd0
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-11 13:33:57 +0800

    feat: update ComboboxClearButton according to design

--- frontend/src/components/Dropdown/components/SelectCombobox/ComboboxClearButton.tsx
+++ frontend/src/components/Dropdown/components/SelectCombobox/ComboboxClearButton.tsx
@@ -1,25 +1,32 @@
 import { useCallback } from 'react'
 import { BiX } from 'react-icons/bi'
-import { Button } from '@chakra-ui/react'
+import { chakra } from '@chakra-ui/react'
 
 import { useSelectContext } from '../../SelectContext'
 
 export const ComboboxClearButton = (): JSX.Element | null => {
-  const { isClearable, isDisabled, clearButtonLabel, selectItem, styles } =
-    useSelectContext()
+  const {
+    isClearable,
+    isDisabled,
+    clearButtonLabel,
+    selectItem,
+    styles,
+    inputValue,
+  } = useSelectContext()
 
   const handleClearSelection = useCallback(() => selectItem(null), [selectItem])
 
   if (!isClearable) return null
 
   return (
-    <Button
-      isDisabled={isDisabled}
+    <chakra.button
+      disabled={isDisabled}
       aria-label={clearButtonLabel}
       onClick={handleClearSelection}
-      sx={styles.clearbutton}
+      __css={styles.clearbutton}
+      color={inputValue ? 'secondary.500' : undefined}
     >
       <BiX fontSize="1.25rem" />
-    </Button>
+    </chakra.button>
   )
 }
