22b20853124555fafd50304561beeb5a53fc7135
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-14 23:01:58 +0800

    feat: add VerifiableField utils and reexports

---
+++ frontend/src/templates/VerifiableField/index.ts
@@ -0,0 +1,12 @@
+export * from './types'
+export type { VerifiableFieldContainerProps } from './VerifiableFieldContainer'
+export { VerifiableFieldContainer } from './VerifiableFieldContainer'
+export {
+  useVerifiableField,
+  VerifiableFieldContext,
+} from './VerifiableFieldContext'
+export type {
+  VerifiableFieldProps,
+  VerifiableFieldProviderProps,
+} from './VerifiableFieldProvider'
+export { VerifiableFieldProvider } from './VerifiableFieldProvider'
