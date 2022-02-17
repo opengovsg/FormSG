14caa14de94266d593755b242f05e0b8f6669f69
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-17 18:11:01 +0800

    perf: rename FormField to FieldFactory and memoize output
    no more render lag in public form page :heart_eyes:

---
+++ frontend/src/features/public-form/components/FormFields/FieldFactory.tsx
@@ -0,0 +1,78 @@
+import { memo } from 'react'
+import { Text } from '@chakra-ui/react'
+
+import { BasicField, FormFieldDto } from '~shared/types/field'
+import { FormColorTheme } from '~shared/types/form'
+
+import {
+  AttachmentField,
+  CheckboxField,
+  DecimalField,
+  EmailField,
+  HomeNoField,
+  ImageField,
+  LongTextField,
+  MobileField,
+  NricField,
+  NumberField,
+  ParagraphField,
+  RadioField,
+  RatingField,
+  SectionField,
+  ShortTextField,
+  TableField,
+  TableFieldSchema,
+  UenField,
+  YesNoField,
+} from '~templates/Field'
+
+interface FieldFactoryProps {
+  field: FormFieldDto
+  colorTheme?: FormColorTheme
+}
+
+export const FieldFactory = memo(
+  ({ field, colorTheme }: FieldFactoryProps) => {
+    switch (field.fieldType) {
+      case BasicField.Section:
+        return <SectionField schema={field} colorTheme={colorTheme} />
+      case BasicField.Checkbox:
+        return <CheckboxField schema={field} colorTheme={colorTheme} />
+      case BasicField.Radio:
+        return <RadioField schema={field} colorTheme={colorTheme} />
+      case BasicField.Nric:
+        return <NricField schema={field} colorTheme={colorTheme} />
+      case BasicField.Number:
+        return <NumberField schema={field} colorTheme={colorTheme} />
+      case BasicField.Decimal:
+        return <DecimalField schema={field} colorTheme={colorTheme} />
+      case BasicField.ShortText:
+        return <ShortTextField schema={field} colorTheme={colorTheme} />
+      case BasicField.LongText:
+        return <LongTextField schema={field} colorTheme={colorTheme} />
+      case BasicField.YesNo:
+        return <YesNoField schema={field} colorTheme={colorTheme} />
+      case BasicField.Uen:
+        return <UenField schema={field} colorTheme={colorTheme} />
+      case BasicField.Attachment:
+        return <AttachmentField schema={field} colorTheme={colorTheme} />
+      case BasicField.HomeNo:
+        return <HomeNoField schema={field} colorTheme={colorTheme} />
+      case BasicField.Mobile:
+        return <MobileField schema={field} colorTheme={colorTheme} />
+      case BasicField.Statement:
+        return <ParagraphField schema={field} colorTheme={colorTheme} />
+      case BasicField.Rating:
+        return <RatingField schema={field} colorTheme={colorTheme} />
+      case BasicField.Email:
+        return <EmailField schema={field} colorTheme={colorTheme} />
+      case BasicField.Image:
+        return <ImageField schema={field} colorTheme={colorTheme} />
+      case BasicField.Table:
+        return <TableField schema={field as TableFieldSchema} />
+      default:
+        return <Text w="100%">{JSON.stringify(field)}</Text>
+    }
+  },
+  (prevProps, nextProps) => prevProps.field._id === nextProps.field._id,
+)
