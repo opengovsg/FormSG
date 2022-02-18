2c6ba724a6ef6462ec22cc95a030cb2761b67ad8
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-18 10:50:06 +0800

    feat: remove VerifiedFormFields context
    will be inlined into PublicFormContext in future PRs instead.

--- frontend/src/features/public-form/PublicFormPage.tsx
+++ frontend/src/features/public-form/PublicFormPage.tsx
@@ -2,18 +2,15 @@ import { Flex } from '@chakra-ui/react'
 
 import FormFields from './components/FormFields'
 import FormStartPage from './components/FormStartPage'
-import { VerifiedFormFieldsProvider } from './verified-fields/VerifiedFormFieldsProvider'
 import { PublicFormProvider } from './PublicFormProvider'
 
 export const PublicFormPage = (): JSX.Element => {
   return (
     <PublicFormProvider>
-      <VerifiedFormFieldsProvider>
-        <Flex flexDir="column" h="100%" minH="100vh">
-          <FormStartPage />
-          <FormFields />
-        </Flex>
-      </VerifiedFormFieldsProvider>
+      <Flex flexDir="column" h="100%" minH="100vh">
+        <FormStartPage />
+        <FormFields />
+      </Flex>
     </PublicFormProvider>
   )
 }
