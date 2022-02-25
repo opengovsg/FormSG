a9181143c19f4dbcca92acb973ed5ef5e3f3172e
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-25 15:21:08 +0800

    feat: render EmptyWorkspace component when admin has no forms

---
+++ frontend/src/features/workspace/components/EmptyWorkspace/EmptyWorkspace.tsx
@@ -0,0 +1,46 @@
+import { BiPlus } from 'react-icons/bi'
+import { Flex, Text } from '@chakra-ui/react'
+
+import { useIsMobile } from '~hooks/useIsMobile'
+import Button from '~components/Button'
+
+import { EmptyWorkspaceSvgr } from './EmptyWorkspaceSvgr'
+
+export interface EmptyWorkspacePage {
+  isLoading: boolean
+  handleOpenCreateFormModal: () => void
+}
+
+export const EmptyWorkspace = ({
+  isLoading,
+  handleOpenCreateFormModal,
+}: EmptyWorkspacePage): JSX.Element => {
+  const isMobile = useIsMobile()
+
+  return (
+    <Flex
+      justify="center"
+      flexDir="column"
+      align="center"
+      px="2rem"
+      py="1rem"
+      bg="neutral.100"
+    >
+      <Text as="h2" textStyle="h2" color="primary.500" mb="1rem">
+        You don't have any forms yet
+      </Text>
+      <Text textStyle="body-1" color="secondary.500" mb="2rem">
+        Get started by creating a new form.
+      </Text>
+      <Button
+        isFullWidth={isMobile}
+        isDisabled={isLoading}
+        onClick={handleOpenCreateFormModal}
+        leftIcon={<BiPlus fontSize="1.5rem" />}
+      >
+        Create form
+      </Button>
+      <EmptyWorkspaceSvgr mt="3.5rem" maxW="100%" />
+    </Flex>
+  )
+}
