4d3646144365f3b6039933c8b0c33e8f08e7f4f1
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-28 14:59:28 +0800

    feat: add `/client/env` API route for sending client env vars as JSON (#3505)

---
+++ src/app/modules/frontend/frontend.service.ts
@@ -0,0 +1,24 @@
+import { ClientEnvVars } from '../../../../shared/types/core'
+import config from '../../config/config'
+import { captchaConfig } from '../../config/features/captcha.config'
+import { googleAnalyticsConfig } from '../../config/features/google-analytics.config'
+import { sentryConfig } from '../../config/features/sentry.config'
+import { spcpMyInfoConfig } from '../../config/features/spcp-myinfo.config'
+
+export const getClientEnvVars = (): ClientEnvVars => {
+  return {
+    isGeneralMaintenance: config.isGeneralMaintenance,
+    isLoginBanner: config.isLoginBanner,
+    siteBannerContent: config.siteBannerContent,
+    adminBannerContent: config.adminBannerContent,
+    logoBucketUrl: config.aws.logoBucketUrl, // S3 bucket
+    formsgSdkMode: config.formsgSdkMode,
+    captchaPublicKey: captchaConfig.captchaPublicKey, // Recaptcha
+    sentryConfigUrl: sentryConfig.sentryConfigUrl, // Sentry.IO
+    isSPMaintenance: spcpMyInfoConfig.isSPMaintenance, // Singpass maintenance message
+    isCPMaintenance: spcpMyInfoConfig.isCPMaintenance, // Corppass maintenance message
+    myInfoBannerContent: spcpMyInfoConfig.myInfoBannerContent, // MyInfo maintenance message
+    GATrackingID: googleAnalyticsConfig.GATrackingID,
+    spcpCookieDomain: spcpMyInfoConfig.spcpCookieDomain, // Cookie domain used for removing spcp cookies
+  }
+}
