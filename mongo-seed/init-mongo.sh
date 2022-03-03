bf0c9797089dbba645a5ff13b449e529c4a46543
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-03 11:42:45 +0800

    fix: revert how we initiate mongo database in docker-compose local dev (#3541)
    * fix: revert replSet in local docker-compose
    
    since we've rolled back to v5, this is unneeded and causes fresh builds to fail
    First introduced in #3039
    
    * chore(docker-compose): update mongo image from 4.0 to 4.2
    
    keep consistent with the database version we are using in prod

--- mongo-seed/init-mongo.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f /data/mongo-init.flag ]; then
-    echo "Init replicaset"
-    mongo database:27017 --eval "rs.initiate()"
-    mongoimport --host database:27017 --db formsg --collection agencies --type json --file init.json --jsonArray
-    touch /data/mongo-init.flag
-else
-    echo "Replicaset already initialized"
-fi
