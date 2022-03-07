e01abb3f5c165408945055263642ebc5b5e912d7
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-07 19:09:07 +0800

    feat(useAdminFormLogic): return "logicable" fields in hook

--- frontend/src/features/admin-form/create/logic/hooks/useAdminFormLogic.ts
+++ frontend/src/features/admin-form/create/logic/hooks/useAdminFormLogic.ts
@@ -1,9 +1,11 @@
 import { useMemo } from 'react'
+import pickBy from 'lodash/pickBy'
 
 import { FormFieldDto } from '~shared/types/field'
 
 import { useAdminForm } from '~features/admin-form/common/queries'
 
+import { ALLOWED_LOGIC_FIELDS } from '../constants'
 import { FormFieldWithQuestionNumber } from '../types'
 
 export const useAdminFormLogic = () => {
@@ -18,9 +20,15 @@ export const useAdminFormLogic = () => {
     }, {} as Record<FormFieldDto['_id'], FormFieldWithQuestionNumber>)
   }, [form])
 
+  const logicableFields = useMemo(() => {
+    if (!mapIdToField) return null
+    return pickBy(mapIdToField, (f) => ALLOWED_LOGIC_FIELDS.has(f.fieldType))
+  }, [mapIdToField])
+
   return {
     isLoading,
     formLogics: form?.form_logics,
+    logicableFields,
     mapIdToField,
   }
 }
