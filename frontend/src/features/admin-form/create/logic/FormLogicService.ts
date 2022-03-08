403bdfc567c6166d00dbd1437a41d19d454abc77
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-08 13:47:45 +0800

    feat: add FormLogicService
    logic related functions originally in src/public/services/UpdateFormService.ts

---
+++ frontend/src/features/admin-form/create/logic/FormLogicService.ts
@@ -0,0 +1,35 @@
+import { LogicDto } from '~shared/types/form'
+
+import { ApiService } from '~services/ApiService'
+
+import { ADMIN_FORM_ENDPOINT } from '~features/admin-form/common/AdminViewFormService'
+
+export const createFormLogic = async (
+  formId: string,
+  createLogicBody: LogicDto,
+): Promise<LogicDto> => {
+  return ApiService.post<LogicDto>(
+    `${ADMIN_FORM_ENDPOINT}/${formId}/logic`,
+    createLogicBody,
+  ).then(({ data }) => data)
+}
+
+export const deleteFormLogic = async (
+  formId: string,
+  logicId: string,
+): Promise<true> => {
+  return ApiService.delete(
+    `${ADMIN_FORM_ENDPOINT}/${formId}/logic/${logicId}`,
+  ).then(() => true)
+}
+
+export const updateFormLogic = async (
+  formId: string,
+  logicId: string,
+  updatedLogic: LogicDto,
+): Promise<LogicDto> => {
+  return ApiService.put<LogicDto>(
+    `${ADMIN_FORM_ENDPOINT}/${formId}/logic/${logicId}`,
+    updatedLogic,
+  ).then(({ data }) => data)
+}
