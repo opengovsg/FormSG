65369bf030f26ec81e4987f25b0c64380a65a5e0
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-17 16:29:36 +0800

    feat(shared): add generic to FormFieldDto type

--- shared/types/field/index.ts
+++ shared/types/field/index.ts
@@ -71,14 +71,17 @@ export type FormFieldWithId<T extends FormField = FormField> =
         _id: string
       }
 
-export type PossiblyPrefilledFormField = FormFieldWithId & {
-  fieldValue?: string
-}
+export type PossiblyPrefilledFormField<T extends FormField = FormField> =
+  FormFieldWithId<T> & {
+    fieldValue?: string
+  }
 
 /**
  * Form field POJO with id
  */
-export type FormFieldDto = PossiblyPrefilledFormField | FormFieldWithId
+export type FormFieldDto<T extends FormField = FormField> =
+  | PossiblyPrefilledFormField<T>
+  | FormFieldWithId<T>
 
 export type FieldCreateDto = FormField
 export type FieldUpdateDto = FormFieldWithId
