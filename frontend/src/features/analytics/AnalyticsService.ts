4d3c9313d5113858bd61074a9c0f4f07d870d438
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-25 13:29:26 +0800

    feat(analytics): add trackVisitPublicForm
    still don't know if we should put it under page view, but that means we need to avoid double counting and use custom page_view event...

--- frontend/src/features/analytics/AnalyticsService.ts
+++ frontend/src/features/analytics/AnalyticsService.ts
@@ -1,3 +1,5 @@
+import { FormStatus, PublicFormDto } from '~shared/types/form'
+
 const gtag = window?.gtag
 
 export const trackAdminLogin = () => {
@@ -13,4 +15,22 @@ export const trackAdminLoginFailure = (error: string) => {
     event_category: 'admin_login_failure',
     message: error,
   })
+}
+
+const trackPublicFormEvent = (
+  eventCategory: string,
+  form: PublicFormDto,
+  others?: Record<string, unknown>,
+) => {
+  if (!gtag || form.status !== FormStatus.Public) return
+  gtag('event', 'public_form', {
+    event_category: eventCategory,
+    event_label: `${form.title} ${form._id}`,
+    form_id: form._id,
+    ...others,
+  })
+}
+
+export const trackVisitPublicForm = (form: PublicFormDto) => {
+  return trackPublicFormEvent('visit', form)
 }
--- frontend/src/features/public-form/PublicFormProvider.tsx
+++ frontend/src/features/public-form/PublicFormProvider.tsx
@@ -14,6 +14,7 @@ import { useToast } from '~hooks/useToast'
 import { HttpError } from '~services/ApiService'
 import Link from '~components/Link'
 
+import { trackVisitPublicForm } from '~features/analytics/AnalyticsService'
 import {
   FetchNewTransactionResponse,
   useTransactionMutations,
@@ -46,6 +47,7 @@ export const PublicFormProvider = ({
   useEffect(() => {
     if (data) {
       if (!form) {
+        trackVisitPublicForm(data.form)
         setForm(data)
       } else if (!desyncToastIdRef.current && !isEqual(data, form)) {
         desyncToastIdRef.current = toast({
