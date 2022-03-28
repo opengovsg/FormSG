00f3c1996aefda83890beda177c5c26c6f7f8c86
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-28 23:28:34 +0800

    ref(AppFooter): extract appwide footer component

--- frontend/src/constants/externalLinks.ts
+++ frontend/src/constants/externalLinks.ts
@@ -1,2 +1,15 @@
 export const FORM_GUIDE = 'https://go.gov.sg/form-guide'
 export const REPORT_VULNERABILITY = 'https://go.gov.sg/report-vulnerability'
+export const OSS_README =
+  'https://s3-ap-southeast-1.amazonaws.com/misc.form.gov.sg/OSS-Legal.pdf'
+
+export const APP_FOOTER_LINKS = [
+  { label: 'Contact Us', href: '/contact-us' },
+  { label: 'Guide', href: FORM_GUIDE },
+  { label: 'Privacy', href: '/privacy' },
+  { label: 'Terms of Use', href: '/terms' },
+  {
+    label: 'Report Vulnerability',
+    href: REPORT_VULNERABILITY,
+  },
+]
