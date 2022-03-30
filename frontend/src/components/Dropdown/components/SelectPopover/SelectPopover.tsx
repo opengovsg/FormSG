45523a9d99225b7a730dc211771ee59e865bc7aa
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-30 17:33:06 +0800

    fix: further add reliability to react-popper's positioning
    previously the usage of useMemo causes too many rerenders when viewpoint changes

--- frontend/src/components/Dropdown/components/SelectPopover/SelectPopover.tsx
+++ frontend/src/components/Dropdown/components/SelectPopover/SelectPopover.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo, useRef, useState } from 'react'
+import { FC, useRef, useState } from 'react'
 import { Modifier, usePopper } from 'react-popper'
 import { Box, useMergeRefs, useOutsideClick } from '@chakra-ui/react'
 
@@ -6,52 +6,27 @@ import { useSelectContext } from '~components/Dropdown/SelectContext'
 
 import { SelectPopoverContext } from './SelectPopoverContext'
 
+export const matchWidth: Modifier<'matchWidth'> = {
+  name: 'matchWidth',
+  enabled: true,
+  phase: 'beforeWrite',
+  requires: ['computeStyles'],
+  fn: ({ state }) => {
+    state.styles.popper.width = `${state.rects.reference.width}px`
+  },
+  effect:
+    ({ state }) =>
+    () => {
+      const reference = state.elements.reference as HTMLElement
+      state.elements.popper.style.width = `${reference.offsetWidth}px`
+    },
+}
+
 export const SelectPopoverProvider: FC = ({ children }): JSX.Element => {
   const [referenceElement, setReferenceElement] =
     useState<HTMLDivElement | null>(null)
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
     null,
-  )
-
-  /**
-   * Required so menu does not get hidden when nested in a parent container with
-   * overflow hidden properties.
-   * Modifier ensures popover is the same width as the reference element.
-   * Must be wrapped with `useMemo` since `usePopper` will recompute the
-   * instance on every render, causing infinite loop.
-   */
-  const popperModifiers = useMemo(
-    () => [
-      {
-        name: 'matchReferenceSize',
-        enabled: true,
-        phase: 'beforeWrite',
-        requires: ['computeStyles'],
-        fn: ({ state, instance }) => {
-          const widthOrHeight =
-            state.placement.startsWith('left') ||
-            state.placement.startsWith('right')
-              ? 'height'
-              : 'width'
-
-          if (!popperElement) return
-
-          const popperSize =
-            popperElement[
-              `offset${
-                widthOrHeight[0].toUpperCase() + widthOrHeight.slice(1)
-              }` as 'offsetWidth'
-            ]
-          const referenceSize = state.rects.reference[widthOrHeight]
-
-          if (Math.round(popperSize) === Math.round(referenceSize)) return
-
-          popperElement.style[widthOrHeight] = `${referenceSize}px`
-          instance.update()
-        },
-      } as Modifier<'matchReferenceSize'>,
-    ],
-    [popperElement],
   )
 
   const {
@@ -61,7 +36,7 @@ export const SelectPopoverProvider: FC = ({ children }): JSX.Element => {
   } = usePopper(referenceElement, popperElement, {
     placement: 'bottom-start',
     strategy: 'fixed',
-    modifiers: popperModifiers,
+    modifiers: [matchWidth],
   })
 
   const { styles, setIsFocused } = useSelectContext()
