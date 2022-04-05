f91c9a589e658a36c2ec5462d369d3d434574a5f
Timothee Groleau <timothee.groleau@gmail.com>
2022-04-05 16:52:00 +0800

    feat: making react route work

--- src/app/modules/react-migration/react-migration.routes.ts
+++ src/app/modules/react-migration/react-migration.routes.ts
@@ -1,24 +1,39 @@
 import { Router } from 'express'
 
+import config from '../../config/config'
+import * as HomeController from '../home/home.controller'
+
 export const ReactMigrationRouter = Router()
 
-const reactRolloutThreshold = 20 / 100
+const REACT_ROLLOUT_THRESHOLD = 20 / 100
 
-ReactMigrationRouter.get('/:formId([a-fA-F0-9]{24})', (req, res) => {
+export const SESSION_COOKIE_OPTIONS = {
+  httpOnly: true,
+  sameSite: 'lax' as const,
+  secure: !config.isDev,
+}
+
+ReactMigrationRouter.get('/:formId([a-fA-F0-9]{24})', (req, res, next) => {
+  console.log('conditional routing')
+
   // check cookies
   let serveReact
 
   if (req.cookies && 'frontend' in req.cookies) {
     serveReact = req.cookies.frontend === 'react'
   } else {
-    serveReact = Math.random() < reactRolloutThreshold
+    serveReact = Math.random() < REACT_ROLLOUT_THRESHOLD
   }
 
-  res.cookie('frontend', serveReact ? 'react' : 'angular')
+  res.cookie(
+    'frontend',
+    serveReact ? 'react' : 'angular',
+    SESSION_COOKIE_OPTIONS,
+  )
 
   if (serveReact) {
     // serve react
   } else {
-    // serve angular
+    return HomeController.home(req, res, next)
   }
 })
