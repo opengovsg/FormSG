2c1816d954a46214f812cafeafed10a92b682981
Timothee Groleau <timothee.groleau@gmail.com>
2022-04-06 17:27:42 +0800

    feat: redirect formId urls with hashes

--- src/app/modules/react-migration/react-migration.routes.ts
+++ src/app/modules/react-migration/react-migration.routes.ts
@@ -9,4 +9,8 @@ ReactMigrationRouter.get(
   ReactMigrationController.serveForm,
 )
 
+ReactMigrationRouter.get('/#!/:formId([a-fA-F0-9]{24})', (req, res) => {
+  res.redirect(`/${req.params.formId}`)
+})
+
 ReactMigrationRouter.get('*', ReactMigrationController.serveDefault)
