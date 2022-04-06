50ba5262e7622c0efa01faf8f35fa0c828d2373e
Timothee Groleau <timothee.groleau@gmail.com>
2022-04-06 10:20:43 +0800

    fix: correct priority for checking environment

--- src/app/modules/react-migration/react-migration.routes.ts
+++ src/app/modules/react-migration/react-migration.routes.ts
@@ -5,9 +5,12 @@ import * as HomeController from '../home/home.controller'
 
 export const ReactMigrationRouter = Router()
 
+const RESPONDENT_COOKIE_NAME = 'v2-respondent-ui'
+const ADMIN_COOKIE_NAME = 'v2-admin-ui'
+
 const REACT_ROLLOUT_THRESHOLD = 20 / 100
 
-export const SESSION_COOKIE_OPTIONS = {
+export const RESPONDENT_COOKIE_OPTIONS = {
   httpOnly: true,
   sameSite: 'lax' as const,
   secure: !config.isDev,
@@ -19,21 +22,27 @@ ReactMigrationRouter.get('/:formId([a-fA-F0-9]{24})', (req, res, next) => {
   // check cookies
   let serveReact
 
-  if (req.cookies && 'frontend' in req.cookies) {
-    serveReact = req.cookies.frontend === 'react'
-  } else {
-    serveReact = Math.random() < REACT_ROLLOUT_THRESHOLD
+  if (req.cookies) {
+    if (ADMIN_COOKIE_NAME in req.cookies) {
+      serveReact = req.cookies[ADMIN_COOKIE_NAME] === 'react'
+    } else if (RESPONDENT_COOKIE_NAME in req.cookies) {
+      serveReact = req.cookies[RESPONDENT_COOKIE_NAME] === 'react'
+    }
   }
 
-  res.cookie(
-    'frontend',
-    serveReact ? 'react' : 'angular',
-    SESSION_COOKIE_OPTIONS,
-  )
+  if (serveReact === undefined) {
+    serveReact = Math.random() < REACT_ROLLOUT_THRESHOLD
+
+    res.cookie(
+      RESPONDENT_COOKIE_NAME,
+      serveReact ? 'react' : 'angular',
+      RESPONDENT_COOKIE_OPTIONS,
+    )
+  }
 
   if (serveReact) {
     // serve react
   } else {
-    return HomeController.home(req, res, next)
+    // return HomeController.home(req, res, next)
   }
 })
