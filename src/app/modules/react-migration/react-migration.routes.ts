d33d15497fa09244b473c1130ef4c5926284d086
Timothee Groleau <timothee.groleau@gmail.com>
2022-04-07 16:23:09 +0800

    fix: Catch all URLs

--- src/app/modules/react-migration/react-migration.routes.ts
+++ src/app/modules/react-migration/react-migration.routes.ts
@@ -13,4 +13,4 @@ ReactMigrationRouter.get('/#!/:formId([a-fA-F0-9]{24})', (req, res) => {
   res.redirect(`/${req.params.formId}`)
 })
 
-ReactMigrationRouter.get('/', ReactMigrationController.serveDefault)
+ReactMigrationRouter.get('*', ReactMigrationController.serveDefault)
