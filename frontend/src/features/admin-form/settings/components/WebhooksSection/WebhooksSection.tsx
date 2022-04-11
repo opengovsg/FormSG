1f032bdcab3c467490813e5bc9870cd09ca5f64e
Kar Rui Lau <karrui.lau@gmail.com>
2022-04-11 10:13:31 +0800

    feat: add unsupported component to follow design

--- frontend/src/features/admin-form/settings/components/WebhooksSection/WebhooksSection.tsx
+++ frontend/src/features/admin-form/settings/components/WebhooksSection/WebhooksSection.tsx
@@ -1,25 +1,9 @@
 import { Stack } from '@chakra-ui/react'
 
-import { FormResponseMode } from '~shared/types/form'
-
-import InlineMessage from '~components/InlineMessage'
-
-import { useAdminFormSettings } from '../../queries'
-
 import { RetryToggle } from './RetryToggle'
 import { WebhookUrlInput } from './WebhookUrlInput'
 
 export const WebhooksSection = (): JSX.Element => {
-  const { data: settings, isLoading } = useAdminFormSettings()
-
-  if (!isLoading && settings?.responseMode !== FormResponseMode.Encrypt) {
-    return (
-      <InlineMessage>
-        Webhooks are only available in storage mode forms.
-      </InlineMessage>
-    )
-  }
-
   return (
     <Stack spacing="2.5rem">
       <WebhookUrlInput />
