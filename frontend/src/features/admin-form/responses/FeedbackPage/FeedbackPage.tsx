4c016c1a219fefb6343dc3365631c8eb4764b7e3
tshuli <shuli@open.gov.sg>
2022-04-14 15:24:26 +0800

    feat: header bar for feedback page

--- frontend/src/features/admin-form/responses/FeedbackPage/FeedbackPage.tsx
+++ frontend/src/features/admin-form/responses/FeedbackPage/FeedbackPage.tsx
@@ -1,4 +1,7 @@
-import { Box, chakra, Container, Text } from '@chakra-ui/react'
+import { BiDownload } from 'react-icons/bi'
+import { Box, Container, Icon, Text } from '@chakra-ui/react'
+
+import Button from '~components/Button'
 
 import { getSmileyFromScore } from './FeedbackService'
 import { useFormFeedback } from './queries'
@@ -19,7 +22,7 @@ export const FeedbackPage = (): JSX.Element => {
         Average Score
       </Text>
       <Box display="flex" flexDir="row" justifyContent="space-between">
-        <Box display="flex" justifyContent="flex-start">
+        <Box display="flex" justifyContent="flex-start" alignItems="center">
           <Text
             textStyle="display-2"
             fontWeight="semibold"
@@ -31,9 +34,32 @@ export const FeedbackPage = (): JSX.Element => {
           >
             {data?.average}
           </Text>
-          {chakra(getSmileyFromScore(averageScore))}
+          <Icon
+            as={getSmileyFromScore(averageScore)}
+            h="2.5rem"
+            w="2.5rem"
+            mr="2.5rem"
+          />
+          <Text
+            textStyle="h4"
+            fontWeight="medium"
+            fontSize="1.125rem"
+            lineHeight="1.5rem"
+            color="secondary.500"
+          >
+            <span color="primary.500">{data?.count}</span>
+          </Text>
+          <Text
+            textStyle="h4"
+            fontWeight="medium"
+            fontSize="1.125rem"
+            lineHeight="1.5rem"
+            color="secondary.500"
+          >
+            &nbsp;feedback submission(s) to date
+          </Text>
         </Box>
-        <Box>B</Box>
+        <Button leftIcon={<BiDownload />}>Export </Button>
       </Box>
       <>{isLoading ? 'Loading' : 'Finished'}</>
     </Container>
--- frontend/src/features/admin-form/responses/FeedbackPage/FeedbackService.ts
+++ frontend/src/features/admin-form/responses/FeedbackPage/FeedbackService.ts
@@ -2,11 +2,7 @@ import React from 'react'
 
 import { FormFeedbackMetaDto } from '~shared/types/form/'
 
-import { ReactComponent as AngrySvg } from '~assets/svgs/smileys/angry.svg'
-import { ReactComponent as HappySvg } from '~assets/svgs/smileys/happy.svg'
-import { ReactComponent as NeutralSvg } from '~assets/svgs/smileys/neutral.svg'
-import { ReactComponent as OkSvg } from '~assets/svgs/smileys/ok.svg'
-import { ReactComponent as SadSvg } from '~assets/svgs/smileys/sad.svg'
+import { BxsAngry, BxsHappy, BxsMeh, BxsSad, BxsSmile } from '~assets/icons'
 import { ApiService } from '~services/ApiService'
 
 import { ADMIN_FORM_ENDPOINT } from '../../common/AdminViewFormService'
@@ -25,14 +21,14 @@ export const getSmileyFromScore = (
   averageScore: number,
 ): React.FunctionComponent => {
   if (averageScore < 1) {
-    return AngrySvg
+    return BxsAngry
   } else if (averageScore < 2) {
-    return SadSvg
+    return BxsSad
   } else if (averageScore < 3) {
-    return NeutralSvg
+    return BxsMeh
   } else if (averageScore < 4) {
-    return OkSvg
+    return BxsSmile
   } else {
-    return HappySvg
+    return BxsHappy
   }
 }
