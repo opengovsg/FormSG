94d3aba652d01ed50ecd7a8349714bd72c64cb04
Kar Rui Lau <karrui.lau@gmail.com>
2022-05-09 15:05:36 +0800

    feat: add default field creation meta for attachments

--- frontend/src/features/admin-form/create/builder-and-design/utils/fieldCreation.ts
+++ frontend/src/features/admin-form/create/builder-and-design/utils/fieldCreation.ts
@@ -1,4 +1,4 @@
-import { BasicField, FieldCreateDto } from '~shared/types/field'
+import { AttachmentSize, BasicField, FieldCreateDto } from '~shared/types/field'
 
 import { BASICFIELD_TO_DRAWER_META } from '../../constants'
 
@@ -18,6 +18,13 @@ export const getFieldCreationMeta = (fieldType: BasicField): FieldCreateDto => {
   }
 
   switch (fieldType) {
+    case BasicField.Attachment: {
+      return {
+        fieldType,
+        ...baseMeta,
+        attachmentSize: AttachmentSize.OneMb,
+      }
+    }
     case BasicField.YesNo:
     case BasicField.Nric:
     case BasicField.Uen:
