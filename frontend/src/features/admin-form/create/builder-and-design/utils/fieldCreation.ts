c367a74d9b08552c607b4e3644fbb5ad14ab3ccf
Kar Rui Lau <karrui.lau@gmail.com>
2022-05-10 14:00:27 +0800

    feat: allow creation of date field in admin builder

--- frontend/src/features/admin-form/create/builder-and-design/utils/fieldCreation.ts
+++ frontend/src/features/admin-form/create/builder-and-design/utils/fieldCreation.ts
@@ -140,6 +140,17 @@ export const getFieldCreationMeta = (fieldType: BasicField): FieldCreateDto => {
         },
       }
     }
+    case BasicField.Date: {
+      return {
+        fieldType,
+        ...baseMeta,
+        dateValidation: {
+          customMaxDate: null,
+          customMinDate: null,
+          selectedDateValidation: null,
+        },
+      }
+    }
     default: {
       return {
         fieldType: BasicField.Section,
