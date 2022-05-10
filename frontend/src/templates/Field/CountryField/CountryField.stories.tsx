ec3762e00b77605f603ff57dc4654f40426cb98b
Jiayee Lim <jiayeerawr@gmail.com>
2022-05-10 17:11:33 +0800

    fix: replace BasicField.Dropdown in CountryField story

--- frontend/src/templates/Field/CountryField/CountryField.stories.tsx
+++ frontend/src/templates/Field/CountryField/CountryField.stories.tsx
@@ -18,7 +18,7 @@ const baseSchema: CountryFieldSchema = {
   description: 'Type or select your residential country',
   required: true,
   disabled: false,
-  fieldType: BasicField.Dropdown,
+  fieldType: BasicField.Country,
   fieldOptions: [],
   _id: 'random-id',
 }
