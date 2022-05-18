fcb8ca58196ea18943db54e5a6363bc4bdb9786a
Kar Rui Lau <karrui.lau@gmail.com>
2022-05-18 16:48:00 +0800

    feat: remove tagline from AppFooter

--- frontend/src/app/AppFooter.tsx
+++ frontend/src/app/AppFooter.tsx
@@ -2,11 +2,5 @@ import { APP_FOOTER_LINKS } from '~constants/externalLinks'
 import Footer from '~components/Footer'
 
 export const AppFooter = (): JSX.Element => {
-  return (
-    <Footer
-      appName="Form"
-      tagline="Build secure government forms in minutes"
-      footerLinks={APP_FOOTER_LINKS}
-    />
-  )
+  return <Footer appName="Form" footerLinks={APP_FOOTER_LINKS} />
 }
