12470e5ce1e13441bfcab97e87489ea28b3cd765
Kar Rui Lau <karrui.lau@gmail.com>
2022-05-30 16:21:04 +0800

    feat: add missing required validation on table field inputs

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/EditFieldDrawer/edit-fieldtype/EditTable/EditTableColumns.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/EditFieldDrawer/edit-fieldtype/EditTable/EditTableColumns.tsx
@@ -13,6 +13,7 @@ import { BasicField, TableFieldBase } from '~shared/types/field'
 import { createBaseValidationRules } from '~utils/fieldValidation'
 import { SingleSelect } from '~components/Dropdown'
 import { ComboboxItem } from '~components/Dropdown/types'
+import FormErrorMessage from '~components/FormControl/FormErrorMessage'
 import FormLabel from '~components/FormControl/FormLabel'
 import Input from '~components/Input'
 import Toggle from '~components/Toggle'
@@ -54,23 +55,34 @@ export const EditTableColumns = ({
   )
 
   return (
-    <Stack divider={<StackDivider />} spacing="3rem">
+    <Stack
+      divider={<StackDivider borderColor="secondary.100" />}
+      spacing="2rem"
+    >
       {fields.map((column, index) => (
         <Stack key={column.id} spacing="1rem">
-          <FormControl isRequired isReadOnly={isLoading}>
+          <FormControl
+            isRequired
+            isReadOnly={isLoading}
+            isInvalid={!!errors?.columns?.[index]?.title}
+          >
             <FormLabel>{`Column ${index + 1}`}</FormLabel>
             <Input
               {...register(`columns.${index}.title`, requiredValidationRule)}
             />
+            <FormErrorMessage>
+              {errors?.columns?.[index]?.title?.message}
+            </FormErrorMessage>
           </FormControl>
           <FormControl
             isRequired
             isReadOnly={isLoading}
-            isInvalid={!!errors.maximumRows}
+            isInvalid={!!errors.columns?.[index]?.columnType}
           >
             <Controller
               name={`columns.${index}.columnType`}
               control={control}
+              rules={requiredValidationRule}
               render={({ field }) => (
                 <SingleSelect
                   items={TABLE_COLUMN_DROPDOWN_OPTIONS}
@@ -78,6 +90,9 @@ export const EditTableColumns = ({
                 />
               )}
             />
+            <FormErrorMessage>
+              {errors?.columns?.[index]?.columnType?.message}
+            </FormErrorMessage>
           </FormControl>
           {getValues(`columns.${index}.columnType`) === BasicField.Dropdown && (
             <EditTableDropdown inputName={`columns.${index}.fieldOptions`} />
