4d651e5e6da9ed0c8a5d733f6dfbeaf983f56d16
Siddarth Nandanahosur Suresh <siddarthsuresh@Siddarths-MacBook-Pro.local>
2022-05-31 15:25:02 +0800

    fix: remove running of feature tour to not affect CreatePage stories

--- frontend/src/features/admin-form/create/CreatePage.tsx
+++ frontend/src/features/admin-form/create/CreatePage.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { Flex } from '@chakra-ui/react'
 
 import { CreatePageContent } from './common/CreatePageContent'
@@ -7,18 +6,13 @@ import { CreatePageSidebarProvider } from './common/CreatePageSidebarContext'
 import { FeatureTour } from './featureTour/FeatureTour'
 
 export const CreatePage = (): JSX.Element => {
-  const [startFeatureTour, setStartFeatureTour] = useState(false)
-
-  // TODO: need to add logic to only set startFeatureTour to be true if user
-  // has not seen feature tour before
-  useEffect(() => {
-    setStartFeatureTour(true)
-  }, [])
+  // TODO: need to add logic to only show feature tour when user has
+  // not seen it before
 
   return (
     <Flex h="100%" w="100%" overflow="auto" bg="neutral.200" direction="row">
       <CreatePageSidebarProvider>
-        <FeatureTour shouldRun={startFeatureTour} />
+        <FeatureTour shouldRun={false} />
         <CreatePageSidebar />
         <CreatePageContent />
       </CreatePageSidebarProvider>
