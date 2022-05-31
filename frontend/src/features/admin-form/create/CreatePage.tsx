11ff34a4eb63b83f0e0f89033db60e8bb811bf67
Siddarth Nandanahosur Suresh <siddarthsuresh@Siddarths-MacBook-Pro.local>
2022-05-31 18:28:48 +0800

    feat: add story for AdminFormBuilder on FeatureTour

--- frontend/src/features/admin-form/create/CreatePage.tsx
+++ frontend/src/features/admin-form/create/CreatePage.tsx
@@ -5,14 +5,20 @@ import { CreatePageSidebar } from './common/CreatePageSidebar'
 import { CreatePageSidebarProvider } from './common/CreatePageSidebarContext'
 import { FeatureTour } from './featureTour/FeatureTour'
 
-export const CreatePage = (): JSX.Element => {
+export interface CreatePageProps {
+  shouldFeatureTourRun?: boolean
+}
+
+export const CreatePage = ({
+  shouldFeatureTourRun,
+}: CreatePageProps): JSX.Element => {
   // TODO: need to add logic to only show feature tour when user has
   // not seen it before
 
   return (
     <Flex h="100%" w="100%" overflow="auto" bg="neutral.200" direction="row">
       <CreatePageSidebarProvider>
-        <FeatureTour shouldRun={false} />
+        <FeatureTour shouldRun={shouldFeatureTourRun ?? false} />
         <CreatePageSidebar />
         <CreatePageContent />
       </CreatePageSidebarProvider>
