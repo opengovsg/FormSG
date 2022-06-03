189b7589219c0e2d7f93afc5a0596d5c795962ad
Kar Rui Lau <karrui.lau@gmail.com>
2022-06-03 12:13:49 +0800

    feat: add CsvRecord class as own class from AngularJS code
    this class utility is extracted out from the existing decryption worker in AngularJS

--- frontend/src/features/admin-form/responses/ResponsesPage/storage/types.ts
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/types.ts
@@ -1,9 +1,35 @@
 import { FormField } from '@opengovsg/formsg-sdk/dist/types'
+import { SetRequired } from 'type-fest'
+
+import { CsvRecord } from './utils/CsvRecord.class'
+
+export enum CsvRecordStatus {
+  Ok = 'OK',
+  Unknown = 'UNKNOWN',
+  Error = 'ERROR',
+  AttachmentError = 'ATTACHMENT_ERROR',
+  Unverified = 'UNVERIFIED',
+}
+
+export type AttachmentsDownloadMap = Map<
+  number,
+  { url: string; filename?: string }
+>
 
 export type DecryptedSubmissionData = {
   created: string
   submissionId: string
   record: FormField[]
+}
+
+export type CsvRecordData = FormField
+
+export type MaterializedCsvRecord = SetRequired<CsvRecord, 'submissionData'>
+
+export type LineData = {
+  line: string
+  secretKey: string
+  downloadAttachments?: boolean
 }
 
 /**
