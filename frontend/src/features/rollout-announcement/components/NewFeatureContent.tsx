c4c7b2da7a59c435ff83e94b8996a091261d7783
Hans Tirtaputra <hansybastian@gmail.com>
2022-06-09 17:09:07 +0800

    feat: update animation size in mobile view

--- frontend/src/features/rollout-announcement/components/NewFeatureContent.tsx
+++ frontend/src/features/rollout-announcement/components/NewFeatureContent.tsx
@@ -2,14 +2,18 @@ import { useEffect, useRef } from 'react'
 import { Box, BoxProps, ModalBody, ModalHeader, Text } from '@chakra-ui/react'
 import lottie from 'lottie-web'
 
+import { useIsMobile } from '~hooks/useIsMobile'
+
 import { NewFeatureTag } from './NewFeatureTag'
 
 interface LottieAnimationProps extends BoxProps {
   animationData: unknown
+  preserveAspectRatio?: string
 }
 
 export const LottieAnimation = ({
   animationData,
+  preserveAspectRatio = '',
   ...boxProps
 }: LottieAnimationProps): JSX.Element => {
   const element = useRef<HTMLDivElement>(null)
@@ -21,12 +25,15 @@ export const LottieAnimation = ({
     lottieInstance.current = lottie.loadAnimation({
       animationData,
       container: element.current,
+      rendererSettings: {
+        preserveAspectRatio: preserveAspectRatio,
+      },
     })
 
     return () => {
       lottieInstance.current = lottie.destroy()
     }
-  }, [animationData])
+  }, [animationData, preserveAspectRatio])
 
   return <Box {...boxProps} ref={element} />
 }
@@ -41,13 +48,16 @@ export const NewFeatureContent = (props: {
   content: NewFeatureContentProps
 }): JSX.Element => {
   const { title, description, animationData } = props.content
+  const isMobile = useIsMobile()
 
   return (
     <>
       <LottieAnimation
         bg="primary.100"
         pt="4.5rem"
+        height={isMobile ? '33vh' : ''}
         animationData={animationData}
+        preserveAspectRatio="xMidYMax slice"
       />
       <ModalHeader>
         <NewFeatureTag />
