5b3b6e455ce9a340be3e5c088926ea525dc831f8
Kar Rui <karrui.lau@gmail.com>
2022-06-16 14:13:18 +0800

    feat: add story for ProgressModal

---
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/UnlockedResponses/ProgressModal/ProgressModal.stories.tsx
@@ -0,0 +1,33 @@
+import { useDisclosure } from '@chakra-ui/react'
+import { Meta, Story } from '@storybook/react'
+
+import { fullScreenDecorator } from '~utils/storybook'
+
+import { ProgressModal, ProgressModalProps } from './ProgressModal'
+
+export default {
+  title: 'Features/Storage/ProgressModal',
+  component: ProgressModal,
+  decorators: [fullScreenDecorator],
+  parameters: {
+    layout: 'fullscreen',
+    // Prevent flaky tests due to modal animating in.
+    chromatic: { delay: 200 },
+  },
+  args: {
+    downloadPercentage: 50,
+  },
+} as Meta<ProgressModalProps>
+
+const Template: Story<ProgressModalProps> = (args) => {
+  const modalProps = useDisclosure({ defaultIsOpen: true })
+  return (
+    <ProgressModal
+      {...modalProps}
+      {...args}
+      onClose={() => console.log('close modal')}
+    />
+  )
+}
+export const Default = Template.bind({})
+Default.storyName = 'ProgressModal'
