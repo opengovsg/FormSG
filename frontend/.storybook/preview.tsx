45fa07066ab8e2555e9e5ad259d7ea8c1b793623
Kar Rui <karrui.lau@gmail.com>
2022-06-21 12:53:27 +0800

    Revert "ref: simplify global decorators used with MainDecorator component"
    This reverts commit 970f9d1912695e7a22412c04c806b47d16128cd8.

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -6,19 +6,50 @@
 import 'focus-visible/dist/focus-visible.min.js'
 import '../src/assets/fonts/inter.css'
 
+import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ChakraProvider } from '@chakra-ui/react'
+import { DecoratorFn } from '@storybook/react'
 import { initialize, mswDecorator } from 'msw-storybook-addon'
 
+import { AuthProvider } from '~contexts/AuthContext'
 import * as dayjsUtils from '~utils/dayjs'
 
 import i18n from '../src/i18n/i18n'
+import { theme } from '../src/theme'
 
-import { MainDecorator } from './utils/MainDecorator'
 import { StorybookTheme } from './themes'
 
 initialize()
 dayjsUtils.init()
 
-export const decorators = [MainDecorator, mswDecorator]
+const withReactQuery: DecoratorFn = (storyFn) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        retry: false,
+      },
+    },
+  })
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{storyFn()}</AuthProvider>
+    </QueryClientProvider>
+  )
+}
+
+const withChakra: DecoratorFn = (storyFn) => (
+  <ChakraProvider resetCSS theme={theme}>
+    {storyFn()}
+  </ChakraProvider>
+)
+
+const withHelmet: DecoratorFn = (storyFn) => (
+  <HelmetProvider>{storyFn()}</HelmetProvider>
+)
+
+export const decorators = [withReactQuery, withChakra, withHelmet, mswDecorator]
 
 export const parameters = {
   i18n,
