d8df160f76c31831efa2266270c59f591178f429
Hans Tirtaputra <hansybastian@gmail.com>
2022-06-23 14:57:19 +0800

    fix: remove skeleton for SettingsEndPage

--- frontend/src/features/admin-form/settings/SettingsEndPage.tsx
+++ frontend/src/features/admin-form/settings/SettingsEndPage.tsx
@@ -1,19 +1,15 @@
-import { Skeleton } from '@chakra-ui/react'
-
 import { useAdminForm } from '../common/queries'
 
 import { CategoryHeader } from './components/CategoryHeader'
 import { EndPageSettingsInput } from './components/EndPageSettingsSection/EndPageSettingsInput'
 
 export const SettingsEndPage = (): JSX.Element => {
-  const { data: form, isLoading } = useAdminForm()
+  const { data: form } = useAdminForm()
 
   return (
     <>
       <CategoryHeader mb={0}>Customise Thank You page</CategoryHeader>
-      <Skeleton isLoaded={!isLoading && !!form}>
-        {form && <EndPageSettingsInput endPage={form.endPage} />}
-      </Skeleton>
+      {form && <EndPageSettingsInput endPage={form.endPage} />}
     </>
   )
 }
