77dd56133728859290903128c272acb81400ce20
wanlingt <waning@open.gov.sg>
2022-06-23 23:51:26 +0800

    feat: add preview form header

--- frontend/src/app/PrivateElement.tsx
+++ frontend/src/app/PrivateElement.tsx
@@ -1,27 +1,44 @@
 import { Navigate, NavigateProps, useLocation } from 'react-router-dom'
+import { Flex } from '@chakra-ui/react'
 
 import { useAuth } from '~contexts/AuthContext'
 import { LOGIN_ROUTE } from '~constants/routes'
+import GovtMasthead from '~components/GovtMasthead'
+
+import { PreviewFormHeader } from '~features/admin-form/common/components/PreviewFormHeader/PreviewFormHeader'
 
 interface PrivateElementProps {
   /**
    * Route to redirect to when user is not authenticated. Defaults to
    * `LOGIN_ROUTE` if not provided.
+   *
+   * If `isPreview` is true, Govt Masthead will appear.
+   * Defaults to `false`.
    */
   redirectTo?: NavigateProps['to']
   element: React.ReactElement
+  isPreview?: boolean
 }
 
 export const PrivateElement = ({
   element,
   redirectTo = LOGIN_ROUTE,
+  isPreview,
 }: PrivateElementProps): React.ReactElement => {
   const location = useLocation()
 
   const { isAuthenticated } = useAuth()
 
   return isAuthenticated ? (
-    element
+    isPreview ? (
+      <Flex flexDir="column" height="100vh" pos="relative">
+        <GovtMasthead />
+        <PreviewFormHeader />
+        {element}
+      </Flex>
+    ) : (
+      element
+    )
   ) : (
     <Navigate replace to={redirectTo} state={{ from: location }} />
   )
---
+++ frontend/src/features/admin-form/common/components/PreviewFormHeader/PreviewFormHeader.tsx
@@ -0,0 +1,34 @@
+import { Link as ReactLink, useParams } from 'react-router-dom'
+import { Box, Flex, Icon, Text } from '@chakra-ui/react'
+
+import { BxsEditAlt } from '~assets/icons/BxsEditAlt'
+import { ADMINFORM_ROUTE } from '~constants/routes'
+import Link from '~components/Link'
+
+export type PreviewHeaderMode = 'preview'
+
+export const PreviewFormHeader = (): JSX.Element => {
+  const { formId } = useParams()
+
+  return (
+    <Flex bg="success.600" py="0.5rem" px="2rem" display="flex" width="100%">
+      <Flex align="center" flex={1} justify="space-between" flexDir="row">
+        <Box>
+          <Text textStyle="h4">Preview mode</Text>
+        </Box>
+        <Link
+          variant="standalone"
+          color="secondary.700"
+          aria-label="Click to edit the form"
+          as={ReactLink}
+          to={`${ADMINFORM_ROUTE}/${formId}`}
+        >
+          <Flex align="center">
+            <Icon aria-hidden as={BxsEditAlt} fontSize="1rem" mr="0.75rem" />
+            <Text textStyle="subhead-1">Edit form</Text>
+          </Flex>
+        </Link>
+      </Flex>
+    </Flex>
+  )
+}
