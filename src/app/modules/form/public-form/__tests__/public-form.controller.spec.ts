24a809f88645bda4dd384f5bb2721ea9157d1d3d
Hans Tirtaputra <hansybastian@gmail.com>
2022-06-28 13:34:04 +0800

    fix: broken feedback test cases

--- src/app/modules/form/public-form/__tests__/public-form.controller.spec.ts
+++ src/app/modules/form/public-form/__tests__/public-form.controller.spec.ts
@@ -2,12 +2,10 @@ import { IPersonResponse } from '@opengovsg/myinfo-gov-client'
 import { ObjectId } from 'bson-ext'
 import { Request } from 'express'
 import { merge } from 'lodash'
-import mongoose from 'mongoose'
 import { err, errAsync, ok, okAsync } from 'neverthrow'
 import querystring from 'querystring'
 import { mocked } from 'ts-jest/utils'
 
-import getFormFeedbackModel from 'src/app/models/form_feedback.server.model'
 import { DatabaseError } from 'src/app/modules/core/core.errors'
 import { MyInfoData } from 'src/app/modules/myinfo/myinfo.adapter'
 import {
@@ -48,7 +46,6 @@ import { JwtName } from '../../../spcp/spcp.types'
 import {
   AuthTypeMismatchError,
   FormAuthNoEsrvcIdError,
-  FormDeletedError,
   FormNotFoundError,
   PrivateFormError,
 } from '../../form.errors'
@@ -71,206 +68,8 @@ const MockSpcpService = mocked(SpcpService, true)
 const MockSpOidcService = mocked(SpOidcService, true)
 const MockMyInfoService = mocked(MyInfoService, true)
 
-const FormFeedbackModel = getFormFeedbackModel(mongoose)
-
 describe('public-form.controller', () => {
   afterEach(() => jest.clearAllMocks())
-
-  describe('submitFormFeedback', () => {
-    const MOCK_FORM_ID = new ObjectId().toHexString()
-    const MOCK_FORM = {
-      _id: MOCK_FORM_ID,
-      title: 'mock form title',
-      inactiveMessage: 'This mock form is mock closed.',
-    } as IPopulatedForm
-    const MOCK_REQ = expressHandler.mockRequest({
-      body: {
-        rating: 4,
-        comment: 'good',
-      },
-      params: {
-        formId: MOCK_FORM_ID,
-      },
-    })
-
-    it('should return 200 when feedback is saved successfully', async () => {
-      // Arrange
-      const mockRes = expressHandler.mockResponse()
-
-      // Mock services to return success.
-      const mockFormFeedback = new FormFeedbackModel({
-        formId: new ObjectId().toHexString(),
-        rating: 5,
-        comment: 'Great test',
-      })
-      MockFormService.retrieveFullFormById.mockReturnValueOnce(
-        okAsync(MOCK_FORM as IPopulatedForm),
-      )
-      MockFormService.isFormPublic.mockReturnValueOnce(ok(true))
-      MockPublicFormService.insertFormFeedback.mockReturnValueOnce(
-        okAsync(mockFormFeedback),
-      )
-
-      // Act
-      await PublicFormController.submitFormFeedback(
-        MOCK_REQ,
-        mockRes,
-        jest.fn(),
-      )
-
-      // Assert
-      // Check args of mocked services.
-      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledWith(
-        MOCK_FORM_ID,
-      )
-      expect(MockFormService.isFormPublic).toHaveBeenCalledWith(MOCK_FORM)
-      expect(MockPublicFormService.insertFormFeedback).toHaveBeenCalledWith({
-        formId: MOCK_REQ.params.formId,
-        rating: MOCK_REQ.body.rating,
-        comment: MOCK_REQ.body.comment,
-      })
-      expect(mockRes.status).toHaveBeenCalledWith(200)
-      expect(mockRes.json).toHaveBeenCalledWith({
-        message: 'Successfully submitted feedback',
-      })
-    })
-
-    it('should return 404 when retrieving form results in FormNotFoundError', async () => {
-      // Arrange
-      const mockRes = expressHandler.mockResponse()
-
-      // Mock retrieval of form to return failure.
-      MockFormService.retrieveFullFormById.mockReturnValueOnce(
-        errAsync(new FormNotFoundError()),
-      )
-
-      // Act
-      await PublicFormController.submitFormFeedback(
-        MOCK_REQ,
-        mockRes,
-        jest.fn(),
-      )
-
-      // Assert
-      // Check args of mocked services.
-      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledWith(
-        MOCK_FORM_ID,
-      )
-      expect(MockFormService.isFormPublic).not.toHaveBeenCalled()
-      expect(MockPublicFormService.insertFormFeedback).not.toHaveBeenCalled()
-      expect(mockRes.status).toHaveBeenCalledWith(404)
-      expect(mockRes.json).toHaveBeenCalledWith({
-        message: 'Form not found',
-      })
-    })
-
-    it('should return 404 when checking form status results in PrivateFormError', async () => {
-      // Arrange
-      const mockRes = expressHandler.mockResponse()
-
-      // Mock services to return correct mock states.
-      MockFormService.retrieveFullFormById.mockReturnValueOnce(
-        okAsync(MOCK_FORM as IPopulatedForm),
-      )
-      // Mock return error.
-      MockFormService.isFormPublic.mockReturnValueOnce(
-        err(new PrivateFormError(MOCK_FORM.inactiveMessage, MOCK_FORM.title)),
-      )
-
-      // Act
-      await PublicFormController.submitFormFeedback(
-        MOCK_REQ,
-        mockRes,
-        jest.fn(),
-      )
-
-      // Assert
-      // Check args of mocked services.
-      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledWith(
-        MOCK_FORM_ID,
-      )
-      expect(MockFormService.isFormPublic).toHaveBeenCalledWith(MOCK_FORM)
-      expect(MockPublicFormService.insertFormFeedback).not.toHaveBeenCalled()
-      expect(mockRes.status).toHaveBeenCalledWith(404)
-      expect(mockRes.json).toHaveBeenCalledWith({
-        message: MOCK_FORM.inactiveMessage,
-        isPageFound: true,
-        formTitle: MOCK_FORM.title,
-      })
-    })
-
-    it('should return 410 when checking form status results in FormDeletedError', async () => {
-      // Arrange
-      const mockRes = expressHandler.mockResponse()
-
-      // Mock services to return correct mock states.
-      MockFormService.retrieveFullFormById.mockReturnValueOnce(
-        okAsync(MOCK_FORM as IPopulatedForm),
-      )
-      // Mock return error.
-      MockFormService.isFormPublic.mockReturnValueOnce(
-        err(new FormDeletedError()),
-      )
-
-      // Act
-      await PublicFormController.submitFormFeedback(
-        MOCK_REQ,
-        mockRes,
-        jest.fn(),
-      )
-
-      // Assert
-      // Check args of mocked services.
-      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledWith(
-        MOCK_FORM_ID,
-      )
-      expect(MockFormService.isFormPublic).toHaveBeenCalledWith(MOCK_FORM)
-      expect(MockPublicFormService.insertFormFeedback).not.toHaveBeenCalled()
-      expect(mockRes.status).toHaveBeenCalledWith(410)
-      expect(mockRes.json).toHaveBeenCalledWith({
-        message: 'This form is no longer active',
-      })
-    })
-
-    it('should return 500 when database errors occur', async () => {
-      // Arrange
-      const mockRes = expressHandler.mockResponse()
-      const mockErrorString = 'Form feedback could not be created'
-
-      // Mock services to return success or failure states.
-      MockFormService.retrieveFullFormById.mockReturnValueOnce(
-        okAsync(MOCK_FORM as IPopulatedForm),
-      )
-      MockFormService.isFormPublic.mockReturnValueOnce(ok(true))
-      // Mock database error.
-      MockPublicFormService.insertFormFeedback.mockReturnValueOnce(
-        errAsync(new DatabaseError(mockErrorString)),
-      )
-
-      // Act
-      await PublicFormController.submitFormFeedback(
-        MOCK_REQ,
-        mockRes,
-        jest.fn(),
-      )
-
-      // Assert
-      // Check args of mocked services.
-      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledWith(
-        MOCK_FORM_ID,
-      )
-      expect(MockFormService.isFormPublic).toHaveBeenCalledWith(MOCK_FORM)
-      expect(MockPublicFormService.insertFormFeedback).toHaveBeenCalledWith({
-        formId: MOCK_REQ.params.formId,
-        rating: MOCK_REQ.body.rating,
-        comment: MOCK_REQ.body.comment,
-      })
-      expect(mockRes.status).toHaveBeenCalledWith(500)
-      expect(mockRes.json).toHaveBeenCalledWith({
-        message: mockErrorString,
-      })
-    })
-  })
 
   describe('handleRedirect', () => {
     const MOCK_FORM_ID = new ObjectId().toHexString()
