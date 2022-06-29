bfc5e95def24d2b8b7bcf75845bf49b3bfd7d44d
Justyn Oh <justynoh@gmail.com>
2022-06-29 17:28:54 +0800

    chore: rename billing route to remove plural

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -5,7 +5,7 @@ export const LOGIN_ROUTE = '/login'
 export const TOU_ROUTE = '/terms'
 export const PRIVACY_POLICY_ROUTE = '/privacy'
 
-export const BILLING_ROUTE = '/billings'
+export const BILLING_ROUTE = '/billing'
 
 // Cannot use regex match in react-router@6, which means we need to validate
 // the regex in PublicFormPage.
