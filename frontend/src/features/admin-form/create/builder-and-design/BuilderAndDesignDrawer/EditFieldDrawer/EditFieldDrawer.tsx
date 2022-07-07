420142ea4d09b5d279317d2677ba9c701a2c297e
Hans Sebastian Tirtaputra <hansybastian@gmail.com>
2022-07-07 10:42:28 +0800

    feat(v2/end-page-builder-2) edit end page from builder (#4047)
    * feat: add end page builder in settings
    
    * feat: update settings end page validation behaviour
    
    * fix: remove endPage from settings model
    
    * refactor: create separate AdminFormPage service for endpage updates
    
    * fix: broken storybook for end page settings
    
    * fix: use createFormBuilderMocks instead of getAdminForm in storybook
    
    * feat: add thank you button in builder
    
    * feat: add end page builder store
    
    * feat: link thank you button to side drawer
    
    * feat: add edit end page side drawer input
    
    Yet to cleanup the TODO comments
    
    * fix: duplicate EndPageUpdateDto type resulting in compile error
    
    * feat: add end page builder view
    
    * refactor: builder drawer to split end page builder and field builder
    
    * refactor: EditEndPage form controls
    
    * fix: change MemoFieldDrawerContentProps field to non-optional
    
    * refactor: remove unused FormPage enum base type
    
    * refactor: use React's children prop in BuilderContentContainerProps
    
    * fix: BuilderDrawerContainer props in EditFieldDrawer
    
    * fix: form builder drag and drop fieldId error
    
    * fix: render both EndPageView and FormBuilder instead of toggling between them
    
    * fix: update FormBuilder style for thank you button

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/EditFieldDrawer/EditFieldDrawer.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/EditFieldDrawer/EditFieldDrawer.tsx
@@ -1,10 +1,6 @@
-import { memo, useCallback, useMemo } from 'react'
-import { BiLeftArrowAlt } from 'react-icons/bi'
-import { Stack, Text } from '@chakra-ui/react'
+import { memo, useMemo } from 'react'
 
 import { BasicField, FieldCreateDto } from '~shared/types/field'
-
-import IconButton from '~components/IconButton'
 
 import { BASICFIELD_TO_DRAWER_META } from '~features/admin-form/create/constants'
 import { isMyInfo } from '~features/myinfo/utils'
@@ -13,11 +9,10 @@ import { useBuilderFields } from '../../BuilderAndDesignContent/useBuilderFields
 import { MYINFO_FIELD_CONSTANTS } from '../../constants'
 import {
   BuildFieldState,
-  setToInactiveSelector,
   stateDataSelector,
   useBuilderAndDesignStore,
 } from '../../useBuilderAndDesignStore'
-import { CreatePageDrawerCloseButton } from '../CreatePageDrawerCloseButton'
+import { BuilderDrawerContainer } from '../common/BuilderDrawerContainer'
 
 import {
   EditAttachment,
@@ -44,15 +39,7 @@ import {
 } from './edit-fieldtype'
 
 export const EditFieldDrawer = (): JSX.Element | null => {
-  const { stateData, setToInactive } = useBuilderAndDesignStore(
-    useCallback(
-      (state) => ({
-        stateData: stateDataSelector(state),
-        setToInactive: setToInactiveSelector(state),
-      }),
-      [],
-    ),
-  )
+  const stateData = useBuilderAndDesignStore(stateDataSelector)
 
   const fieldToEdit: FieldCreateDto | undefined = useMemo(() => {
     if (
@@ -94,43 +81,12 @@ export const EditFieldDrawer = (): JSX.Element | null => {
   if (!fieldToEdit) return null
 
   return (
-    <>
-      <Stack
-        direction="row"
-        pos="sticky"
-        top={0}
-        px="1.5rem"
-        py="1rem"
-        align="center"
-        borderBottom="1px solid var(--chakra-colors-neutral-300)"
-        bg="white"
-      >
-        <IconButton
-          size="sm"
-          h="1.5rem"
-          w="1.5rem"
-          aria-label="Back to field selection"
-          variant="clear"
-          colorScheme="secondary"
-          onClick={setToInactive}
-          icon={<BiLeftArrowAlt />}
-        />
-        <Text
-          textStyle="h4"
-          as="h4"
-          color="secondary.500"
-          flex={1}
-          textAlign="center"
-        >
-          Edit {basicFieldText}
-        </Text>
-        <CreatePageDrawerCloseButton />
-      </Stack>
+    <BuilderDrawerContainer title={basicFieldText}>
       <MemoFieldDrawerContent
         field={fieldToEdit}
         key={`${fieldIndex}-${numFields}`}
       />
-    </>
+    </BuilderDrawerContainer>
   )
 }
 
