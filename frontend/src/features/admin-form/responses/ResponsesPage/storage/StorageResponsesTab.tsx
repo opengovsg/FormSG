06c5155009e78c9e0ca426016f67a3a903371aa1
Kar Rui <karrui.lau@gmail.com>
2022-07-07 12:06:50 +0800

    fix: remove double invocation of StorageResponsesProvider

--- frontend/src/features/admin-form/responses/ResponsesPage/storage/StorageResponsesTab.tsx
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/StorageResponsesTab.tsx
@@ -2,18 +2,9 @@ import { EmptyResponses } from '../common/EmptyResponses'
 
 import { SecretKeyVerification } from './SecretKeyVerification'
 import { useStorageResponsesContext } from './StorageResponsesContext'
-import { StorageResponsesProvider } from './StorageResponsesProvider'
 import { UnlockedResponses } from './UnlockedResponses'
 
-export const StorageResponsesTab = () => {
-  return (
-    <StorageResponsesProvider>
-      <ProvidedStorageResponsesTab />
-    </StorageResponsesProvider>
-  )
-}
-
-const ProvidedStorageResponsesTab = (): JSX.Element => {
+export const StorageResponsesTab = (): JSX.Element => {
   const { totalResponsesCount, secretKey } = useStorageResponsesContext()
 
   if (totalResponsesCount === 0) {
