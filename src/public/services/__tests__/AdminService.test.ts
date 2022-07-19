22bc749a955b8f15508834d8a9a01e024e1a6a1b
wanlingt <wanling@open.gov.sg>
2022-07-19 12:00:22 +0800

    test: AdminService test

---
+++ src/public/services/__tests__/AdminService.test.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import MockAxios from 'jest-mock-axios'
+import { UiCookieValues } from 'shared/types'
+
+import { adminChooseEnvironment } from '../AdminService'
+
+jest.mock('axios', () => MockAxios)
+
+describe('AdminService', () => {
+  describe('adminChooseEnvironment', () => {
+    const MOCK_UI = UiCookieValues.React
+    const MOCK_RESPONSE = UiCookieValues.React
+
+    it('should return environment name if GET request succeeds', async () => {
+      // Arrange
+      const mockUi = UiCookieValues.React
+      const expectedResponse = UiCookieValues.React
+
+      // Act
+      const actual = adminChooseEnvironment(MOCK_UI)
+      MockAxios.mockResponse({ data: MOCK_RESPONSE })
+
+      // Assert
+      await expect(actual).resolves.toEqual(MOCK_RESPONSE)
+      expect(MockAxios.get).toHaveBeenCalledWith(
+        `/api/v3/admin/environment/${MOCK_UI}`,
+        expectedResponse,
+        { params: { data: mockUi } },
+      )
+    })
+  })
+})
