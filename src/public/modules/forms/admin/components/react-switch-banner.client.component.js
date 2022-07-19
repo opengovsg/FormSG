77b1101b539961d1ac326b0a89cc7a1208585f0f
wanlingt <wanling@open.gov.sg>
2022-07-19 11:35:44 +0800

    fix: update admin env route param

--- src/public/modules/forms/admin/components/react-switch-banner.client.component.js
+++ src/public/modules/forms/admin/components/react-switch-banner.client.component.js
@@ -1,34 +1,24 @@
 'use strict'
 
 const AdminService = require('../../../../services/AdminService')
-// const {
-//   UiCookieValues,
-// } = require('../../../../../app/modules/react-migration/react-migration.controller')
-// const { $q } = require('angular-ui-router')
+const { UiCookieValues } = require('../../../../../../shared/types')
 angular.module('core').component('reactSwitchBannerComponent', {
   templateUrl:
     'modules/forms/admin/componentViews/react-switch-banner.client.view.html',
-  controller: ['$q', '$window', reactSwitchBannerController],
+  controller: ['$scope', '$q', '$window', reactSwitchBannerController],
   controllerAs: 'vm',
 })
 
-function reactSwitchBannerController($q, $window) {
+function reactSwitchBannerController($scope, $q, $window) {
   const vm = this
-  vm.env = 'react' //UiCookieValues.React
-  // $scope.env = 'react'//UiCookieValues.React
+  $scope.ui = UiCookieValues.React
 
-  // $scope.adminChooseEnvironment = await AdminService.adminChooseEnvironment(env)
-  // const reactSwitch = await AdminService.adminChooseEnvironment(env)
-  // vm.envSwitch = reactSwitch
-  // $scope.adminChooseEnvironment = (env) => {
-  //   return $q.when(AdminService.adminChooseEnvironment(env))
-  // }
-  vm.adminChooseEnvironment = (env) => {
+  vm.adminChooseEnvironment = (ui) => {
     return (
       $q
-        .when(AdminService.adminChooseEnvironment(env))
-        // Reload the page after changing the environment cookie
-        .then($window.location.reload())
+        .when(AdminService.adminChooseEnvironment(ui))
+        // Refresh the page after changing the environment cookie
+        .then($window.location.reload(true))
         .catch((error) => {
           console.error('switch to react failed:', error)
         })
