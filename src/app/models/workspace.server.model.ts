a92203fab8e78a93002a11070b84adba4f9c6669
Hans Tirtaputra <hansybastian@gmail.com>
2022-07-20 18:59:41 +0800

    fix: removed unused database fields renaming

--- src/app/models/workspace.server.model.ts
+++ src/app/models/workspace.server.model.ts
@@ -5,47 +5,36 @@ import { IWorkspaceModel, IWorkspaceSchema } from '../../types'
 export const WORKSPACE_SCHEMA_ID = 'Workspace'
 
 const compileWorkspaceModel = (db: Mongoose): IWorkspaceModel => {
-  const WorkspaceSchema = new Schema<IWorkspaceSchema, IWorkspaceModel>(
-    {
-      title: {
-        type: String,
-        validate: [
-          /^[a-zA-Z0-9_\-./() &`;'"]*$/,
-          'Workspace title cannot contain special characters',
-        ],
-        required: 'Workspace title cannot be blank',
-        minlength: [4, 'Workspace title must be at least 4 characters'],
-        maxlength: [
-          200,
-          'Workspace title can have a maximum of 200 characters',
-        ],
-        trim: true,
-      },
-      admin: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: 'Workspace must have an Admin',
-      },
-      formIds: {
-        type: [Schema.Types.ObjectId],
-        validate: {
-          async validator(this: IWorkspaceSchema) {
-            const areFormIdsUnique =
-              new Set(this.formIds).size === this.formIds.length
+  const WorkspaceSchema = new Schema<IWorkspaceSchema, IWorkspaceModel>({
+    title: {
+      type: String,
+      validate: [
+        /^[a-zA-Z0-9_\-./() &`;'"]*$/,
+        'Workspace title cannot contain special characters',
+      ],
+      required: 'Workspace title cannot be blank',
+      minlength: [4, 'Workspace title must be at least 4 characters'],
+      maxlength: [200, 'Workspace title can have a maximum of 200 characters'],
+      trim: true,
+    },
+    admin: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: 'Workspace must have an Admin',
+    },
+    formIds: {
+      type: [Schema.Types.ObjectId],
+      validate: {
+        async validator(this: IWorkspaceSchema) {
+          const areFormIdsUnique =
+            new Set(this.formIds).size === this.formIds.length
 
-            return areFormIdsUnique
-          },
+          return areFormIdsUnique
         },
-        message: "Failed to update workspace document's formIds",
       },
+      message: "Failed to update workspace document's formIds",
     },
-    {
-      timestamps: {
-        createdAt: 'created',
-        updatedAt: 'lastModified',
-      },
-    },
-  )
+  })
 
   WorkspaceSchema.index({
     admin: 1,
