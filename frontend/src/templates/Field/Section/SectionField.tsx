56bea57ca826d02601f82f087c1354452c1a2774
Kar Rui <karrui.lau@gmail.com>
2022-07-21 15:26:37 +0800

    ref: use BaseSectionField component as intended

--- frontend/src/templates/Field/Section/SectionField.tsx
+++ frontend/src/templates/Field/Section/SectionField.tsx
@@ -16,44 +16,14 @@ export interface SectionFieldProps extends SectionFieldContainerProps {
 
 // Used by SectionFieldContainer
 export const SectionField = forwardRef<SectionFieldProps, 'div'>(
-  (
-    { schema, colorTheme = FormColorTheme.Blue, handleSectionEnter, ...rest },
-    ref,
-  ) => {
-    const sectionColor = useMemo(() => {
-      switch (colorTheme) {
-        case FormColorTheme.Orange:
-        case FormColorTheme.Red:
-          return `theme-${colorTheme}.600` as const
-        default:
-          return `theme-${colorTheme}.500` as const
-      }
-    }, [colorTheme])
-
+  ({ handleSectionEnter, ...rest }, ref) => {
     return (
       <Box
         _notFirst={{
           mt: '3.75rem',
         }}
       >
-        {/* id given so app can scrolled to this section */}
-        <Box
-          id={schema._id}
-          ref={ref}
-          {...rest}
-          _focus={{
-            boxShadow: `0 0 0 2px var(--chakra-colors-theme-${colorTheme}-500)`,
-          }}
-        >
-          <Text textStyle="h2" color={sectionColor}>
-            {schema.title}
-          </Text>
-          {schema.description && (
-            <Text textStyle="body-1" color="secondary.700" mt="1rem">
-              {schema.description}
-            </Text>
-          )}
-        </Box>
+        <BaseSectionField {...rest} ref={ref} />
         <Waypoint
           topOffset="80px"
           bottomOffset="0%"
@@ -65,23 +35,42 @@ export const SectionField = forwardRef<SectionFieldProps, 'div'>(
 )
 
 export const BaseSectionField = forwardRef<
-  Pick<SectionFieldProps, 'schema'>,
+  Pick<SectionFieldProps, 'schema' | 'colorTheme'>,
   'div'
->(({ schema }, ref) => {
+>(({ schema, colorTheme = FormColorTheme.Blue, ...rest }, ref) => {
+  const sectionColor = useMemo(() => {
+    switch (colorTheme) {
+      case FormColorTheme.Orange:
+      case FormColorTheme.Red:
+        return `theme-${colorTheme}.600` as const
+      default:
+        return `theme-${colorTheme}.500` as const
+    }
+  }, [colorTheme])
+
   return (
     // id given so app can scrolled to this section.
-    <Box id={schema._id} ref={ref}>
-      <Text textStyle="h2" color="primary.600">
+    <Box
+      id={schema._id}
+      ref={ref}
+      _focus={{
+        boxShadow: `0 0 0 2px var(--chakra-colors-theme-${colorTheme}-500)`,
+      }}
+      {...rest}
+    >
+      <Text textStyle="h2" color={sectionColor}>
         {schema.title}
       </Text>
-      <Text
-        textStyle="body-1"
-        color="secondary.700"
-        mt="1rem"
-        whiteSpace="break-spaces"
-      >
-        {schema.description}
-      </Text>
+      {schema.description && (
+        <Text
+          textStyle="body-1"
+          color="secondary.700"
+          mt="1rem"
+          whiteSpace="break-spaces"
+        >
+          {schema.description}
+        </Text>
+      )}
     </Box>
   )
 })
