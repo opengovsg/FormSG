3825730a6d7379cd367d40efec267875476ec48a
Hans Tirtaputra <hansybastian@gmail.com>
2022-07-22 10:32:44 +0800

    feat: add timestamps for workspace db model

--- src/app/models/workspace.server.model.ts
+++ src/app/models/workspace.server.model.ts
@@ -5,29 +5,35 @@ import { IWorkspaceModel, IWorkspaceSchema } from '../../types'
 export const WORKSPACE_SCHEMA_ID = 'Workspace'
 
 const compileWorkspaceModel = (db: Mongoose): IWorkspaceModel => {
-  const WorkspaceSchema = new Schema<IWorkspaceSchema, IWorkspaceModel>({
-    title: {
-      type: String,
-      validate: [
-        /^[a-zA-Z0-9_\-./() &`;'"]*$/,
-        'Workspace title cannot contain special characters',
-      ],
-      required: 'Workspace title cannot be blank',
-      minlength: [4, 'Workspace title must be at least 4 characters'],
-      maxlength: [200, 'Workspace title can have a maximum of 200 characters'],
-      trim: true,
+  const WorkspaceSchema = new Schema<IWorkspaceSchema, IWorkspaceModel>(
+    {
+      title: {
+        type: String,
+        validate: [
+          /^[a-zA-Z0-9_\-./() &`;'"]*$/,
+          'Workspace title cannot contain special characters',
+        ],
+        required: 'Workspace title cannot be blank',
+        minlength: [4, 'Workspace title must be at least 4 characters'],
+        maxlength: [
+          200,
+          'Workspace title can have a maximum of 200 characters',
+        ],
+        trim: true,
+      },
+      admin: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: 'Workspace must have an Admin',
+      },
+      formIds: {
+        type: [Schema.Types.ObjectId],
+        validate: (v: Schema.Types.ObjectId[]) => new Set(v).size === v.length,
+        message: "Failed to update workspace document's formIds",
+      },
     },
-    admin: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: 'Workspace must have an Admin',
-    },
-    formIds: {
-      type: [Schema.Types.ObjectId],
-      validate: (v: Schema.Types.ObjectId[]) => new Set(v).size === v.length,
-      message: "Failed to update workspace document's formIds",
-    },
-  })
+    { timestamps: true },
+  )
 
   WorkspaceSchema.index({
     admin: 1,
