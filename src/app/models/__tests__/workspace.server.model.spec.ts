37a73f538be081ba2bf3fd69ea75fa1d8161bca2
Hans Tirtaputra <hansybastian@gmail.com>
2022-07-22 10:36:07 +0800

    fix: add more workspace model tests and rename test descriptions

--- src/app/models/__tests__/workspace.server.model.spec.ts
+++ src/app/models/__tests__/workspace.server.model.spec.ts
@@ -20,41 +20,17 @@ const MOCK_WORKSPACE_FIELDS = {
 }
 
 describe('Workspace Model', () => {
-  let FORM_COLLAB_READ_ONLY_USER: IUserSchema
-  let FORM_COLLAB_READ_AND_WRITE_USER: IUserSchema
   let FORM_ADMIN_USER: IUserSchema
 
   beforeAll(async () => await dbHandler.connect())
   beforeEach(async () => {
-    const { form, user: adminUser } = await dbHandler.insertEncryptForm({
+    const { user: adminUser } = await dbHandler.insertEncryptForm({
       formId: MOCK_FORM_ID,
       userId: MOCK_USER_ID,
     })
-    const { user: collabReadOnlyUser } =
-      await dbHandler.insertFormCollectionReqs({
-        userId: new ObjectId(),
-        mailName: 'readOnly',
-      })
-    const { user: collabReadAndWriteUser } =
-      await dbHandler.insertFormCollectionReqs({
-        userId: new ObjectId(),
-        mailName: 'readAndWrite',
-      })
-    await form.updateFormCollaborators([
-      {
-        email: collabReadOnlyUser.email,
-        write: false,
-      },
-      {
-        email: collabReadAndWriteUser.email,
-        write: true,
-      },
-    ])
 
     await Workspace.create(MOCK_WORKSPACE_FIELDS)
     FORM_ADMIN_USER = adminUser
-    FORM_COLLAB_READ_AND_WRITE_USER = collabReadAndWriteUser
-    FORM_COLLAB_READ_ONLY_USER = collabReadOnlyUser
   })
   afterEach(async () => await dbHandler.clearDatabase())
   afterAll(async () => await dbHandler.closeDatabase())
@@ -74,118 +50,59 @@ describe('Workspace Model', () => {
       const saved = await validWorkspace.save()
       const actualSavedObject = omit(saved.toObject(), [
         '_id',
-        'created',
-        'lastModified',
+        'createdAt',
+        'updatedAt',
         '__v',
       ])
 
       expect(saved._id).toBeDefined()
-      expect(saved.created).toBeInstanceOf(Date)
-      expect(saved.lastModified).toBeInstanceOf(Date)
+      expect(saved.createdAt).toBeInstanceOf(Date)
+      expect(saved.updatedAt).toBeInstanceOf(Date)
       expect(actualSavedObject).toEqual(expectedWorkspaceObject)
     })
 
-    it("should update workspace's formIds successfully because user is the form admin", async () => {
-      const expectedWorkspaceObject = {
-        title: 'Workspace2',
-        admin: FORM_ADMIN_USER._id,
-        formIds: [MOCK_FORM_ID],
-      }
-      const validWorkspace = new Workspace(expectedWorkspaceObject)
-      const saved = await validWorkspace.save()
-      const actualSavedObject = omit(saved.toObject(), [
-        '_id',
-        'created',
-        'lastModified',
-        '__v',
-      ])
-
-      expect(actualSavedObject).toEqual(expectedWorkspaceObject)
-    })
-
-    it("should create and save successfully because user has read and write access in the form's permissionList", async () => {
-      const expectedWorkspaceObject = {
-        title: 'Workspace2',
-        admin: FORM_COLLAB_READ_AND_WRITE_USER._id,
-        formIds: [MOCK_FORM_ID],
-      }
-      const validWorkspace = new Workspace(expectedWorkspaceObject)
-      const saved = await validWorkspace.save()
-      const actualSavedObject = omit(saved.toObject(), [
-        '_id',
-        'created',
-        'lastModified',
-        '__v',
-      ])
-
-      expect(actualSavedObject).toEqual(expectedWorkspaceObject)
-    })
-
-    it("should create and save successfully because user has read access in the form's permissionList", async () => {
-      const expectedWorkspaceObject = {
-        title: 'Workspace2',
-        admin: FORM_COLLAB_READ_ONLY_USER._id,
-        formIds: [MOCK_FORM_ID],
-      }
-      const validWorkspace = new Workspace(expectedWorkspaceObject)
-      const saved = await validWorkspace.save()
-      const actualSavedObject = omit(saved.toObject(), [
-        '_id',
-        'created',
-        'lastModified',
-        '__v',
-      ])
-
-      expect(actualSavedObject).toEqual(expectedWorkspaceObject)
-    })
-
-    it('should fail because userId does not exist', async () => {
-      const workspaceObject = {
-        title: 'Workspace2',
-        admin: new ObjectId(),
-        formIds: [MOCK_FORM_ID],
-      }
-      const invalidWorkspace = new Workspace(workspaceObject)
-      await expect(invalidWorkspace.save()).rejects.toThrowError(
-        mongoose.Error.ValidationError,
-      )
-    })
-
-    it('should fail because user has no access to form', async () => {
-      const { user: existentUser } = await dbHandler.insertFormCollectionReqs({
-        userId: new ObjectId(),
-        mailName: 'userThatExists',
-      })
-
-      const workspaceObject = {
-        title: 'Workspace2',
-        admin: existentUser,
-        formIds: [MOCK_FORM_ID],
-      }
-      const invalidWorkspace = new Workspace(workspaceObject)
-      await expect(invalidWorkspace.save()).rejects.toThrowError(
-        mongoose.Error.ValidationError,
-      )
-    })
-
-    it('should fail because formId does not exist', async () => {
-      const workspaceObject = {
-        title: 'Workspace2',
-        admin: FORM_ADMIN_USER,
-        formIds: [new ObjectId()],
-      }
-      const invalidWorkspace = new Workspace(workspaceObject)
-      await expect(invalidWorkspace.save()).rejects.toThrowError(
-        mongoose.Error.ValidationError,
-      )
-    })
-
-    it('should fail because formId is not unique', async () => {
+    it('should fail when formId in is not unique', async () => {
       const duplicateFormId = new ObjectId()
       const workspaceObject = {
         title: 'Workspace2',
         admin: FORM_ADMIN_USER,
         formIds: [duplicateFormId, duplicateFormId],
+      }
+      const invalidWorkspace = new Workspace(workspaceObject)
+      await expect(invalidWorkspace.save()).rejects.toThrowError(
+        mongoose.Error.ValidationError,
+      )
+    })
+
+    it('should fail when title length is less than 4 characters', async () => {
+      const workspaceObject = {
+        title: 'aaa',
+        admin: FORM_ADMIN_USER,
+        formIds: [],
+      }
+      const invalidWorkspace = new Workspace(workspaceObject)
+      await expect(invalidWorkspace.save()).rejects.toThrowError(
+        mongoose.Error.ValidationError,
+      )
+    })
+
+    it('should fail when title length is more than 200 characters', async () => {
+      const workspaceObject = {
+        title: new Array(202).join('a'),
+        admin: FORM_ADMIN_USER,
+        formIds: [],
+      }
+      const invalidWorkspace = new Workspace(workspaceObject)
+      await expect(invalidWorkspace.save()).rejects.toThrowError(
+        mongoose.Error.ValidationError,
+      )
+    })
+
+    it('should fail when title has special characters', async () => {
+      const workspaceObject = {
+        title: 'titleI$',
+        admin: FORM_ADMIN_USER,
+        formIds: [],
       }
       const invalidWorkspace = new Workspace(workspaceObject)
       await expect(invalidWorkspace.save()).rejects.toThrowError(
