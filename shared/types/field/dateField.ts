c8ed6a36d23a0ef6ea5445580ba8848af11e5184
Siddarth Nandanahosur Suresh <siddarthsuresh@Siddarths-MacBook-Pro.local>
2022-07-22 10:34:47 +0800

    feat: edit dateField schema to store invalidDaysOfTheWeek as a string enum

--- shared/types/field/dateField.ts
+++ shared/types/field/dateField.ts
@@ -8,13 +8,13 @@ export enum DateSelectedValidation {
 }
 
 export enum DaysOfTheWeek {
-  Sunday = 0,
-  Monday = 1,
-  Tuesday = 2,
-  Wednesday = 3,
-  Thursday = 4,
-  Friday = 5,
-  Saturday = 6,
+  Sunday = 'Sunday',
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
 }
 
 export type DateValidationOptions = {
