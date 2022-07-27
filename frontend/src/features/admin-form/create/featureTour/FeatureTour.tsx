c244f89ea6e23355035d69c863f61d662ba4823b
Siddarth Nandanahosur Suresh <48427064+siddarth2824@users.noreply.github.com>
2022-07-27 17:15:36 +0800

    fix(form-v2): add correct html titles to feature tour tooltip buttons (#4388)
    * fix: fix html title for feature tour tooltip buttons
    
    * refactor: remove unused prop from interface
    
    Co-authored-by: Siddarth Nandanahosur Suresh <siddarthsuresh@Siddarths-MacBook-Pro.local>

--- frontend/src/features/admin-form/create/featureTour/FeatureTour.tsx
+++ frontend/src/features/admin-form/create/featureTour/FeatureTour.tsx
@@ -16,16 +16,20 @@ export const FeatureTour = ({ onClose }: FeatureTourProps): JSX.Element => {
   const [isPaginationClicked, setIsPaginationClicked] = useState<boolean>(false)
 
   const handleJoyrideCallback = ({
-    action,
     index,
     status,
     type,
+    action,
   }: CallBackProps) => {
     if (!isPaginationClicked) {
       if (type === EVENTS.STEP_AFTER || type === EVENTS.TARGET_NOT_FOUND) {
         setStepIndex(index + 1)
       }
-      if (status === STATUS.FINISHED || status === STATUS.SKIPPED) {
+      if (
+        status === STATUS.FINISHED ||
+        status === STATUS.SKIPPED ||
+        action === ACTIONS.CLOSE
+      ) {
         onClose()
       }
     } else {
@@ -48,6 +52,7 @@ export const FeatureTour = ({ onClose }: FeatureTourProps): JSX.Element => {
         steps={FEATURE_STEPS}
         callback={handleJoyrideCallback}
         stepIndex={stepIndex}
+        continuous
         run
         hideBackButton
         floaterProps={{
