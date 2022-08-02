5a93fc99a3e8a9c4e25fae50ccfd9534c6044ba9
wanlingt <wanling@open.gov.sg>
2022-08-02 10:49:20 +0800

    chore: add comments

--- src/public/modules/forms/admin/components/react-switch-banner.client.component.js
+++ src/public/modules/forms/admin/components/react-switch-banner.client.component.js
@@ -17,10 +17,9 @@ function reactSwitchBannerController($q, $window) {
     return (
       $q
         .when(AdminService.adminChooseEnvironment(ui))
-        // Refresh the page after changing the environment cookie
+        // Navigate to the React workspace page after changing the environment cookie
         .then(() => {
           $window.location.assign('/workspace')
-          // $window.location.reload(true)
         })
         .catch((error) => {
           console.error('switch to react failed:', error)
