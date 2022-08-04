0787589bb2b8210e406b8d8ac3bedd6ae3931692
Hans Tirtaputra <hansybastian@gmail.com>
2022-08-04 22:49:08 +0800

    refactor: cleanup unused minLength validator in workspaceValidation

--- frontend/src/utils/workspaceValidation.ts
+++ frontend/src/utils/workspaceValidation.ts
@@ -1,14 +1,10 @@
 import { UseControllerProps } from 'react-hook-form'
 
-const MAX_TITLE_LENGTH = 200
+const MAX_TITLE_LENGTH = 50
 const MIN_TITLE_LENGTH = 4
 
 export const WORKSPACE_TITLE_VALIDATION_RULES: UseControllerProps['rules'] = {
   required: 'Workspace title is required',
-  minLength: {
-    value: MIN_TITLE_LENGTH,
-    message: `Workspace title must be at least ${MIN_TITLE_LENGTH} characters`,
-  },
   maxLength: {
     value: MAX_TITLE_LENGTH,
     message: `Workspace title must be at most ${MAX_TITLE_LENGTH} characters`,
