d0032294c3e56125eed6c18628b10c18f06bafd2
Kar Rui <karrui.lau@gmail.com>
2022-08-08 12:46:48 +0800

    feat: add responsiveness to skeleton

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignContent/FormBuilder/FormBuilderFieldsSkeleton.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignContent/FormBuilder/FormBuilderFieldsSkeleton.tsx
@@ -1,7 +1,11 @@
 import { Skeleton, Stack } from '@chakra-ui/react'
 
 export const FormBuilderFieldsSkeleton = (): JSX.Element => (
-  <Stack spacing="3rem" p={{ md: '1.5rem' }}>
+  <Stack
+    spacing="3rem"
+    px={{ base: '0.5rem', md: '1.625rem' }}
+    py={{ base: '0.5rem', md: '2.5rem' }}
+  >
     <Skeleton h="2rem" />
     <Skeleton h="4rem" />
     <Skeleton h="10rem" />
