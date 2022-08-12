8b028b42bfe44d1895554068832d184dcbc5d0bc
darrelhong <darrel.hong@gmail.com>
2022-08-12 19:21:53 +0800

    chore: remove package-lock.json from secrets check

--- secrets-check.sh
+++ secrets-check.sh
@@ -6,8 +6,8 @@ against=HEAD
 exec 1>&2
 
 # Check changed files for an AWS keys
-KEY_ID=$(git diff --cached --name-only -z $against | xargs -0 cat | perl -nle'print $& if m{(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])}')
-KEY=$(git diff --cached --name-only -z $against | xargs -0 cat | perl -nle'print $& if m{(?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=])}')
+KEY_ID=$(git diff --cached --name-only -z $against -- ':!*package-lock.json' | xargs -0 cat | perl -nle'print $& if m{(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])}')
+KEY=$(git diff --cached --name-only -z $against -- ':!*package-lock.json' | xargs -0 cat | perl -nle'print $& if m{(?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=])}')
 
 # Regex for secret check can be found here: https://aws.amazon.com/blogs/security/a-safer-way-to-distribute-aws-credentials-to-ec2/
 
