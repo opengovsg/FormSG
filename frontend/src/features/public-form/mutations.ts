f41dd52b361ef430e90e5a6aefdab0121895e9be
Kar Rui <karrui.lau@gmail.com>
2022-08-25 17:07:11 +0800

    feat: remove unused imports

--- frontend/src/features/public-form/mutations.ts
+++ frontend/src/features/public-form/mutations.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from 'react-query'
+import { useMutation } from 'react-query'
 
 import { FormAuthType, SubmitFormFeedbackBodyDto } from '~shared/types/form'
 
@@ -14,11 +14,9 @@ import {
   SubmitStorageFormArgs,
   submitStorageModeForm,
 } from './PublicFormService'
-import { publicFormKeys } from './queries'
 
 export const usePublicAuthMutations = (formId: string) => {
   const { storePrefillQuery } = useStorePrefillQuery()
-  const queryClient = useQueryClient()
 
   const toast = useToast({ status: 'success', isClosable: true })
 
