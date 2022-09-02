dc48002261348893428c30719f0d513010d16305
wanlingt <wanling@open.gov.sg>
2022-09-02 15:25:46 +0800

    fix: revert move angular form redirect

--- src/app/modules/react-migration/react-migration.routes.ts
+++ src/app/modules/react-migration/react-migration.routes.ts
@@ -5,13 +5,14 @@ import * as ReactMigrationController from './react-migration.controller'
 
 export const ReactMigrationRouter = Router()
 
-ReactMigrationRouter.get('/#!/:formId([a-fA-F0-9]{24})', (req, res) => {
-  res.redirect(`/${req.params.formId}`)
-})
 ReactMigrationRouter.get(
   '/:formId([a-fA-F0-9]{24})',
   ReactMigrationController.serveForm,
 )
+
+ReactMigrationRouter.get('/#!/:formId([a-fA-F0-9]{24})', (req, res) => {
+  res.redirect(`/${req.params.formId}`)
+})
 
 // Redirect to the landing page after setting the admin cookie
 ReactMigrationRouter.get(
