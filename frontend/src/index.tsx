f29f1c728e2b9efc885a4431d3d68e176224302e
wanlingt <wanling@open.gov.sg>
2022-09-05 10:34:21 +0800

    fix: remove debug mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -27,7 +27,7 @@ function gtag(...args: unknown[]) {
   dataLayer.push(arguments)
 }
 gtag('js', new Date())
-gtag('config', process.env.REACT_APP_GA_TRACKING_ID || '', { debug_mode: true })
+gtag('config', process.env.REACT_APP_GA_TRACKING_ID || '')
 window.gtag = gtag
 
 // Init Datadog RUM
