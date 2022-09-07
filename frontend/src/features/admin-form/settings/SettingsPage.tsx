0ed8b25e03b945c38766e3a4ecb2cbedbd4f7a6a
Justyn Oh <justynoh@gmail.com>
2022-09-07 15:23:51 +0800

    feat: settings sidebar for mobile

--- frontend/src/features/admin-form/settings/SettingsPage.tsx
+++ frontend/src/features/admin-form/settings/SettingsPage.tsx
@@ -38,28 +38,17 @@ export const SettingsPage = (): JSX.Element => {
       navigate(`${ADMINFORM_ROUTE}/${formId}/${ADMINFORM_RESULTS_SUBROUTE}`)
   }, [formId, hasEditAccess, isCollabLoading, navigate])
 
-  const tabOrientation: UseTabsProps['orientation'] = useBreakpointValue({
-    base: 'horizontal',
-    xs: 'horizontal',
-    md: 'vertical',
-  })
-
   const { ref, onMouseDown } = useDraggable<HTMLDivElement>()
 
   return (
-    <Box
-      overflow="auto"
-      flex={1}
-      // Buffer for bottom navbar in mobile breakpoints.
-      mb={{ base: '4rem', md: 'initial' }}
-    >
+    <Box overflow="auto" flex={1}>
       <Tabs
         isLazy
         isManual
-        orientation={tabOrientation}
+        orientation="vertical"
         variant="line"
-        py={{ base: '2.5rem', md: '3.5rem', lg: '4rem' }}
-        px={{ base: '1.5rem', md: '1.75rem', lg: '2rem' }}
+        py={{ base: '3.5rem', lg: '4rem' }}
+        px={{ base: '1.75rem', lg: '2rem' }}
       >
         <Flex
           h="max-content"
@@ -67,18 +56,13 @@ export const SettingsPage = (): JSX.Element => {
           flexShrink={0}
           ref={ref}
           onMouseDown={onMouseDown}
-          overflowX={{ base: 'auto', md: 'initial' }}
-          position={{ base: 'fixed', md: 'sticky' }}
-          zIndex={{ base: 'docked', md: 0 }}
-          bg={{ base: 'neutral.100', md: 'inherit' }}
+          position="sticky"
+          zIndex={0}
           // Height align text with start of tab panel.
-          mt={{ md: '-1rem', lg: '-0.875rem' }}
-          top={{ base: 'initial', md: '2.5rem', lg: '3.125rem' }}
-          bottom={{ base: 0, md: 'initial' }}
-          left={{ base: 0, md: 'initial' }}
-          borderTop={{ base: '1px solid', md: 'none' }}
+          mt={{ base: '-1rem', lg: '-0.875rem' }}
+          top={{ base: '2.5rem', lg: '3.125rem' }}
           borderTopColor="neutral.300"
-          w={{ base: '100vw', md: 'auto', lg: '21rem' }}
+          w={{ base: 'auto', lg: '21rem' }}
           __css={{
             scrollbarWidth: 0,
             /* Scrollbar for Chrome, Safari, Opera and Microsoft Edge */
@@ -92,7 +76,6 @@ export const SettingsPage = (): JSX.Element => {
             overflowX="initial"
             display="inline-flex"
             w="max-content"
-            ml={{ base: '1.5rem', md: 0 }}
             mr={{ base: '1.5rem', md: '4rem', lg: '2rem' }}
             mb="calc(0.5rem - 2px)"
           >
