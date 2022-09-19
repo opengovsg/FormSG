d6e7bca6a7271d08d3f0d392fdb500c18af54e2f
tshuli <shuli@open.gov.sg>
2022-09-19 08:21:21 +0800

    docs: add REACT_APP_FORMSG_SDK_MODE env var to docs

--- frontend/src/utils/formSdk.ts
+++ frontend/src/utils/formSdk.ts
@@ -5,7 +5,7 @@ import { TRANSACTION_EXPIRE_AFTER_SECONDS } from '~shared/utils/verification'
 
 /**
  * Typeguard to check if sdkMode is valid PackageMode
- * @param sdkMode defined in REACT_APP env var
+ * @param sdkMode defined in REACT_APP_FORMSG_SDK_MODE env var
  * @returns true if sdkMode is valid PackageMode
  */
 const isPackageMode = (sdkMode?: string): sdkMode is PackageMode => {
@@ -16,7 +16,7 @@ const isPackageMode = (sdkMode?: string): sdkMode is PackageMode => {
 }
 
 const formsgSdk = formsgPackage({
-  // Either the sdk mode is set in env var, or fall back to NODE_ENV
+  // Either the sdk mode is set in REACT_APP_FORMSG_SDK_MODE env var, or fall back to NODE_ENV
   // NODE_ENV is set automatically to development (when using npm start),
   // test (when using npm test) or production (when using npm build)
   mode: isPackageMode(process.env.REACT_APP_FORMSG_SDK_MODE)
