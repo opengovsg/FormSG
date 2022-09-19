c129b70c5214a060a5895c9b3e28614b1e05e2e5
Timothee Groleau <timothee.groleau@gmail.com>
2022-09-19 19:11:01 +0800

    ref: use simpler script to check tests

--- scripts/release_prep.sh
+++ scripts/release_prep.sh
@@ -5,6 +5,8 @@ set -x
 # pre-requisites: install github CLI
 # - github documentation: https://github.com/cli/cli#installation
 # - github is remote 'origin'
+# - PRs use test section LAST with heading "## Tests"
+# - ALL build and release PRs start with "build: "
 
 git fetch --all --tags
 git reset --hard
@@ -41,23 +43,39 @@ git push origin ${release_version}
 # TODO: group changelog into sections automatically
 pr_body_file=.pr_body_${release_version}
 pr_body_file_groupped=.pr_body_${release_version}_groupped
-awk "/#### \[${release_version}\]/{flag=1;next}/####/{flag=0}flag" CHANGELOG.md | sed '/^[[:space:]]*$/d' > ${pr_body_file}
+
+awk "/#### \[${release_version}\]/{flag=1;next}/####/{flag=0}flag" CHANGELOG.md | sed -E '/^([^-]|[[:space:]]*$)/d' > ${pr_body_file}
 
 echo "## New" > ${pr_body_file_groupped}
 echo "" >> ${pr_body_file_groupped}
-grep -v -E -- '- [a-z]+\(deps(-dev)?\)'  ${pr_body_file} >> ${pr_body_file_groupped}
+grep -v -E -- '- [a-z]+\(deps(-dev)?\)' ${pr_body_file} >> ${pr_body_file_groupped}
 
 echo "" >> ${pr_body_file_groupped}
 echo "## Dependencies" >> ${pr_body_file_groupped}
 echo "" >> ${pr_body_file_groupped}
-grep -E -- '- [a-z]+\(deps\)'  ${pr_body_file} >> ${pr_body_file_groupped}
+grep -E -- '- [a-z]+\(deps\)' ${pr_body_file} >> ${pr_body_file_groupped}
 
 echo "" >> ${pr_body_file_groupped}
 echo "## Dev-Dependencies" >> ${pr_body_file_groupped}
 echo "" >> ${pr_body_file_groupped}
-grep -E -- '- [a-z]+\(deps-dev\)'  ${pr_body_file} >> ${pr_body_file_groupped}
+grep -E -- '- [a-z]+\(deps-dev\)' ${pr_body_file} >> ${pr_body_file_groupped}
 
 gh auth login
+
+## Extract test procedures for feature PRs
+echo "" >> ${pr_body_file_groupped}
+echo "## Tests" >> ${pr_body_file_groupped}
+echo "" >> ${pr_body_file_groupped}
+grep -v -E -- '- [a-z]+\(deps(-dev)?\)' ${pr_body_file} | grep -v -E -- '- build: ' | while read line_item; do
+  pr_id=$(echo ${line_item} | grep -o -E '\[`#\d+`\]' | grep -o -E '\d+')
+  tests=$(gh pr view ${pr_id} | awk "/## Tests/{flag=1;next}/#######/{flag=0}flag" | sed "s/\[X\]/[ ]/")
+  if [[ ${tests} =~ \S ]]; then
+    echo ${line_item} | sed "s/^- /### /" >> ${pr_body_file_groupped}
+    echo "${tests}" >> ${pr_body_file_groupped}
+    echo "" >> ${pr_body_file_groupped}
+  fi
+done
+
 gh pr create \
   -w \
   -H ${release_branch} \
