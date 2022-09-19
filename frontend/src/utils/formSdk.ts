738ef6a0a344fec9cca4eac9d7f7dce0e6f09c98
tshuli <63710093+tshuli@users.noreply.github.com>
2022-09-19 15:53:00 +0800

    fix: singpass decryption bug (#4888)
    * fix: set formsg sdk mode correctly
    
    * docs: add REACT_APP_FORMSG_SDK_MODE env var to docs

--- frontend/src/utils/formSdk.ts
+++ frontend/src/utils/formSdk.ts
@@ -1,9 +1,27 @@
 import formsgPackage from '@opengovsg/formsg-sdk'
+import { PackageMode } from '@opengovsg/formsg-sdk/dist/types'
 
 import { TRANSACTION_EXPIRE_AFTER_SECONDS } from '~shared/utils/verification'
 
+/**
+ * Typeguard to check if sdkMode is valid PackageMode
+ * @param sdkMode defined in REACT_APP_FORMSG_SDK_MODE env var
+ * @returns true if sdkMode is valid PackageMode
+ */
+const isPackageMode = (sdkMode?: string): sdkMode is PackageMode => {
+  return (
+    !!sdkMode &&
+    ['staging', 'production', 'development', 'test'].includes(sdkMode)
+  )
+}
+
 const formsgSdk = formsgPackage({
-  mode: process.env.NODE_ENV,
+  // Either the sdk mode is set in REACT_APP_FORMSG_SDK_MODE env var, or fall back to NODE_ENV
+  // NODE_ENV is set automatically to development (when using npm start),
+  // test (when using npm test) or production (when using npm build)
+  mode: isPackageMode(process.env.REACT_APP_FORMSG_SDK_MODE)
+    ? process.env.REACT_APP_FORMSG_SDK_MODE
+    : process.env.NODE_ENV,
   verificationOptions: {
     transactionExpiry: TRANSACTION_EXPIRE_AFTER_SECONDS,
   },
