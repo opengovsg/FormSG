71ef941b616b42b42d281a26851b6e8cc4e69f4c
wanlingt <wanling@open.gov.sg>
2022-09-28 16:38:02 +0800

    fix: rename number regex

--- frontend/src/features/admin-form/responses/ResponsesPage/storage/utils/processFormulaInjection.ts
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/utils/processFormulaInjection.ts
@@ -1,14 +1,14 @@
-const PURE_NUMBER_REGEXP = /^[+-.\d]*$/
+const NUMBERS_ONLY_REGEXP = /^[+-.\d]+$/
 const FORMULA_INJECTION_REGEXP = /^[+=@-]/
 
 export const processFormulaInjectionText = (input: string): string => {
-  // Check if input is a pure number
-  const isPureNumber = PURE_NUMBER_REGEXP.test(input)
+  // Check if input contains only + | - | digits
+  const hasNumbersOnly = NUMBERS_ONLY_REGEXP.test(input)
   // Check if input starts with formula characters
   const hasFormulaChars = FORMULA_INJECTION_REGEXP.test(input)
   // if the input is not a pure number, and starts with formula characters,
   // prefix it with a single quote to prevent formula injection
-  if (hasFormulaChars && !isPureNumber) {
+  if (hasFormulaChars && !hasNumbersOnly) {
     return `'${input}`
   } else {
     return input
