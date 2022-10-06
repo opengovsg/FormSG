c354141491aee70f06d9d764f74335b8598515f1
wanlingt <wanling@open.gov.sg>
2022-10-06 10:18:37 +0800

    style: replace design drawer icon

---
+++ frontend/src/assets/icons/BxsDockTop.tsx
@@ -0,0 +1,15 @@
+export const BxsDockTop = (
+  props: React.SVGProps<SVGSVGElement>,
+): JSX.Element => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M3 5v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2zm2 14v-9h14.001l.001 9H5z" />
+    </svg>
+  )
+}
