31a87a632a9e72a15c882c11a22c97c475830c2d
wanlingt <wanling@open.gov.sg>
2022-10-07 10:39:16 +0800

    fix: conditionally display FieldSection label

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/FieldListDrawer/FieldListDrawer.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/FieldListDrawer/FieldListDrawer.tsx
@@ -259,19 +259,21 @@ const FieldSection = ({
 }) => {
   return (
     <Box mb="0.5rem">
-      <Text
-        px="1.5rem"
-        pt="1rem"
-        pb="0.75rem"
-        textStyle="subhead-2"
-        color="secondary.500"
-        pos="sticky"
-        top={0}
-        bg="white"
-        zIndex="docked"
-      >
-        {label}
-      </Text>
+      {label ? (
+        <Text
+          px="1.5rem"
+          pt="1rem"
+          pb="0.75rem"
+          textStyle="subhead-2"
+          color="secondary.500"
+          pos="sticky"
+          top={0}
+          bg="white"
+          zIndex="docked"
+        >
+          {label}
+        </Text>
+      ) : null}
       <Stack divider={<StackDivider />} spacing={0}>
         {children}
       </Stack>
