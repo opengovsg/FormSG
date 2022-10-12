bc8adee69cb14b065f6534895f51299251a5307d
Jiayee Lim <jylim@indeed.com>
2022-10-12 17:53:30 +0800

    fix: remove duplicate country field files and restore TODO

--- shared/types/field/countryField.ts
+++
@@ -1,7 +0,0 @@
-import { BasicField, MyInfoableFieldBase } from './base'
-import { CountryRegion } from '../../constants/countryRegion'
-
-export interface CountryRegionFieldBase extends MyInfoableFieldBase {
-  fieldType: BasicField.CountryRegion
-  fieldOptions: CountryRegion[]
-}
