bbb0eb1d298d543ba4ca95bb4ae6a5ea54b7874f
Kar Rui <karrui.lau@gmail.com>
2022-10-27 18:55:49 +0800

    feat: refresh on lazy load errors once per version

--- frontend/src/utils/lazyRetry.ts
+++ frontend/src/utils/lazyRetry.ts
@@ -13,21 +13,23 @@ import { ComponentType } from 'react'
 export const lazyRetry = <T extends ComponentType>(
   componentImport: () => Promise<{ default: T }>,
 ) => {
+  const sessionStorageKey = `retry-lazy-${process.env.REACT_APP_VERSION}-refreshed`
+
   return new Promise<{ default: T }>((resolve, reject) => {
     // check if the window has already been refreshed
     const hasRefreshed = JSON.parse(
-      window.sessionStorage.getItem('retry-lazy-refreshed') || 'false',
+      window.sessionStorage.getItem(sessionStorageKey) || 'false',
     )
     // try to import the component
     componentImport()
       .then((component) => {
-        window.sessionStorage.setItem('retry-lazy-refreshed', 'false') // success so reset the refresh
+        window.sessionStorage.setItem(sessionStorageKey, 'false') // success so reset the refresh
         resolve(component)
       })
       .catch((error) => {
         if (!hasRefreshed) {
           // not been refreshed yet
-          window.sessionStorage.setItem('retry-lazy-refreshed', 'true') // we are now going to refresh
+          window.sessionStorage.setItem(sessionStorageKey, 'true') // we are now going to refresh
           return window.location.reload() // refresh the page
         }
         reject(error) // Default error behaviour as already tried refresh
