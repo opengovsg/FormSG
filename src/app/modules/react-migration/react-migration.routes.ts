11dba040c378664b97886389e043da3ff971f85e
wanlingt <wanling@open.gov.sg>
2022-11-01 15:18:23 +0800

    feat: add rate limiting to react migration router

--- src/app/modules/react-migration/react-migration.routes.ts
+++ src/app/modules/react-migration/react-migration.routes.ts
@@ -1,12 +1,16 @@
 // TODO #4279: Remove after React rollout is complete
 import { Router } from 'express'
 
+import { rateLimitConfig } from 'src/app/config/config'
+import { limitRate } from 'src/app/utils/limit-rate'
+
 import * as ReactMigrationController from './react-migration.controller'
 
 export const ReactMigrationRouter = Router()
 
 ReactMigrationRouter.get(
   '/:formId([a-fA-F0-9]{24})',
+  limitRate({ max: rateLimitConfig.submissions }),
   ReactMigrationController.servePublicForm,
 )
 
