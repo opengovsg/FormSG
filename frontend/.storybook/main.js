a78d1ce47ff14777a6dccd1b63ac581afdf8ad0e
darrelhong <darrel.hong@gmail.com>
2022-11-19 20:50:37 +0800

    chore: update storybook config with custom craco config

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /* eslint-env node */
 module.exports = {
   staticDirs: ['../public'],
@@ -25,20 +27,52 @@ module.exports = {
   ],
   // webpackFinal setup retrieved from ChakraUI's own Storybook setup
   // https://github.com/chakra-ui/chakra-ui/blob/main/.storybook/main.js
-  webpackFinal: async (storybookConfig) => {
-    // Required to sync aliases between storybook and overriden configs
-    return {
-      ...storybookConfig,
-      module: {
-        rules: [
-          ...storybookConfig.module.rules,
-          {
-            type: 'javascript/auto',
-            test: /\.mjs$/,
-            include: /node_modules/,
-          },
-        ],
-      },
+  webpackFinal: async (storybookWebpackConfig) => {
+    // include shared folder under babel-loader
+    const oneOfRule = storybookWebpackConfig.module.rules.find(
+      (rule) => rule.oneOf,
+    )
+    if (oneOfRule) {
+      const tsxRule = oneOfRule.oneOf.find(
+        (rule) => rule.test && rule.test.toString().includes('tsx'),
+      )
+
+      const newIncludePaths = [path.resolve(__dirname, '../../shared')]
+      if (tsxRule) {
+        if (Array.isArray(tsxRule.include)) {
+          tsxRule.include = [...tsxRule.include, ...newIncludePaths]
+        } else {
+          tsxRule.include = [tsxRule.include, ...newIncludePaths]
+        }
+      }
     }
+
+    // taken from tsconfig.paths.json
+    storybookWebpackConfig.resolve.alias = {
+      ...storybookWebpackConfig.resolve.alias,
+      '~shared': path.resolve('../shared'),
+      '~assets': path.resolve('./src/assets'),
+      '~contexts': path.resolve('./src/contexts'),
+      '~constants': path.resolve('./src/constants'),
+      '~components': path.resolve('./src/components'),
+      '~templates': path.resolve('./src/templates'),
+      '~features': path.resolve('./src/features'),
+      '~hooks': path.resolve('./src/hooks'),
+      '~utils': path.resolve('./src/utils'),
+      '~pages': path.resolve('./src/pages'),
+      '~services': path.resolve('./src/services'),
+      '~theme': path.resolve('./src/theme'),
+      '~typings': path.resolve('./src/typings'),
+      '~': path.resolve('./src'),
+    }
+
+    // polyfill node built-in for csv-string dependency
+    storybookWebpackConfig.resolve.fallback = {
+      stream: require.resolve('stream-browserify'),
+    }
+    return storybookWebpackConfig
+  },
+  core: {
+    builder: 'webpack5',
   },
 }
