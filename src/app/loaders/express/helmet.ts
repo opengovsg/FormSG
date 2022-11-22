58c7f7f4d279c189b38935b694a13e9745a9f1e6
tshuli <shuli@open.gov.sg>
2022-11-22 15:10:10 +0800

    fix: add js.stripe.com/v3 to csp

--- src/app/loaders/express/helmet.ts
+++ src/app/loaders/express/helmet.ts
@@ -51,6 +51,7 @@ const helmetMiddlewares = () => {
       'https://www.google.com/recaptcha/',
       'https://www.recaptcha.net/recaptcha/',
       'https://www.gstatic.com/recaptcha/',
+      'https://js.stripe.com/v3',
       'https://www.gstatic.cn/',
       'https://*.googletagmanager.com', // GA4 https://developers.google.com/tag-platform/tag-manager/web/csp
     ],
