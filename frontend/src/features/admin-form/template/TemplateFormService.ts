46dd80a3d21120b730abe83f26bf7e79066f9900
wanlingt <wanling@open.gov.sg>
2022-11-22 14:07:23 +0800

    feat: add duplicate template endpoint

---
+++ frontend/src/features/admin-form/template/TemplateFormService.ts
@@ -0,0 +1,29 @@
+import {
+  CreateEmailFormBodyDto,
+  CreateStorageFormBodyDto,
+  FormDto,
+} from '~shared/types'
+
+import { ApiService } from '~services/ApiService'
+
+import { ADMIN_FORM_ENDPOINT } from '../common/AdminViewFormService'
+
+export const dupeEmailModeTemplateForm = async (
+  formId: string,
+  body: CreateEmailFormBodyDto,
+): Promise<FormDto> => {
+  return ApiService.post<FormDto>(
+    `${ADMIN_FORM_ENDPOINT}/${formId}/use-template/duplicate`,
+    body,
+  ).then(({ data }) => data)
+}
+
+export const dupeStorageModeTemplateForm = async (
+  formId: string,
+  body: CreateStorageFormBodyDto,
+): Promise<FormDto> => {
+  return ApiService.post<FormDto>(
+    `${ADMIN_FORM_ENDPOINT}/${formId}/use-template/duplicate`,
+    body,
+  ).then(({ data }) => data)
+}
---
+++ frontend/src/features/admin-form/template/mutation.ts
@@ -0,0 +1,85 @@
+import { useCallback } from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+
+import {
+  CreateEmailFormBodyDto,
+  CreateStorageFormBodyDto,
+  FormDto,
+} from '~shared/types'
+
+import { ApiError } from '~typings/core'
+
+import { ADMINFORM_ROUTE } from '~constants/routes'
+import { useToast } from '~hooks/useToast'
+
+import { trackCreateFormFailed } from '~features/analytics/AnalyticsService'
+import { workspaceKeys } from '~features/workspace/queries'
+import {
+  dupeEmailModeForm,
+  dupeStorageModeForm,
+} from '~features/workspace/WorkspaceService'
+
+const useCommonHooks = () => {
+  const navigate = useNavigate()
+  const queryClient = useQueryClient()
+  const toast = useToast({ status: 'danger', isClosable: true })
+
+  const handleSuccess = useCallback(
+    (data: Pick<FormDto, '_id'>) => {
+      queryClient.invalidateQueries(workspaceKeys.all)
+      navigate(`${ADMINFORM_ROUTE}/${data._id}`)
+    },
+    [navigate, queryClient],
+  )
+
+  const handleError = useCallback(
+    (error: ApiError) => {
+      toast({
+        description: error.message,
+      })
+      trackCreateFormFailed()
+    },
+    [toast],
+  )
+
+  return {
+    handleSuccess,
+    handleError,
+  }
+}
+
+export const useDuplicateFormTemplateMutations = () => {
+  const { handleSuccess, handleError } = useCommonHooks()
+
+  const dupeEmailModeFormTemplateMutation = useMutation<
+    FormDto,
+    ApiError,
+    CreateEmailFormBodyDto & { formIdToDuplicate: string }
+  >(
+    ({ formIdToDuplicate, ...params }) =>
+      dupeEmailModeForm(formIdToDuplicate, params),
+    {
+      onSuccess: handleSuccess,
+      onError: handleError,
+    },
+  )
+
+  const dupeStorageModeFormTemplateMutation = useMutation<
+    FormDto,
+    ApiError,
+    CreateStorageFormBodyDto & { formIdToDuplicate: string }
+  >(
+    ({ formIdToDuplicate, ...params }) =>
+      dupeStorageModeForm(formIdToDuplicate, params),
+    {
+      onSuccess: handleSuccess,
+      onError: handleError,
+    },
+  )
+
+  return {
+    dupeEmailModeFormTemplateMutation,
+    dupeStorageModeFormTemplateMutation,
+  }
+}
