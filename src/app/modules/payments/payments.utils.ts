bce5bfce270e7ec7bb5c3b97fac450c10c9d2bb2
Kar Rui <karrui.lau@gmail.com>
2022-11-23 22:16:28 +0800

    fix: correct app root url

--- src/app/modules/payments/payments.utils.ts
+++ src/app/modules/payments/payments.utils.ts
@@ -4,5 +4,5 @@ export const getRedirectUri = (): string => {
   if (config.isDev) {
     return `http://localhost:5001/api/v3/payments/stripe/callback`
   }
-  return `https://${config.app.appUrl}/api/v3/payments/stripe/callback`
+  return `${config.app.appUrl}/api/v3/payments/stripe/callback`
 }
