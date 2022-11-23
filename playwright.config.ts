2e93a2270e57d36dc29d4f35e9ced3d6f88ef2c6
Justyn Oh <justynoh@gmail.com>
2022-11-23 21:31:46 +0800

    test: raise test timeout to 30s, and perform tests in parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ dotenv.config({
 const config: PlaywrightTestConfig = {
   testDir: './__tests__/e2e',
   /* Maximum time one test can run for. */
-  timeout: 20 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
