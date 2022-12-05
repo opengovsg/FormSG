6da16ec681d83d33d87ea7b09f43b317aad1966a
Kar Rui <karrui.lau@gmail.com>
2022-12-05 11:54:00 +0800

    fix: remove unused field schema regression

--- src/types/field/countryField.ts
+++
@@ -1,7 +0,0 @@
-import { BasicField, CountryFieldBase } from '../../../shared/types'
-
-import { IFieldSchema } from './baseField'
-
-export interface ICountryFieldSchema extends CountryFieldBase, IFieldSchema {
-  fieldType: BasicField.Country
-}
