87570f66869d625f572366177aa509bd1991ebf0
foochifa <foochifa@gmail.com>
2023-02-24 11:51:11 +0800

    fix: omit target account id in payments update

--- shared/types/form/form.ts
+++ shared/types/form/form.ts
@@ -240,4 +240,4 @@ export type StartPageUpdateDto = FormStartPage
 export type EndPageUpdateDto = FormEndPage
 export type FormPermissionsDto = FormPermission[]
 export type PermissionsUpdateDto = FormPermission[]
-export type PaymentsUpdateDto = FormPayments
+export type PaymentsUpdateDto = Omit<FormPayments, 'target_account_id'>
