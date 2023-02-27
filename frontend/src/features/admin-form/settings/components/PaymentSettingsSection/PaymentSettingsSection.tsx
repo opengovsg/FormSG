95bad52145d0e47c7e1fd3c8a3e8d5592e53d694
foochifa <foochifa@gmail.com>
2023-02-27 14:40:19 +0800

    feat: add account id to payments settings page

--- frontend/src/features/admin-form/settings/components/PaymentSettingsSection/PaymentSettingsSection.tsx
+++ frontend/src/features/admin-form/settings/components/PaymentSettingsSection/PaymentSettingsSection.tsx
@@ -1,6 +1,8 @@
-import { Flex, Icon, Skeleton, Text } from '@chakra-ui/react'
+import { Flex, FormControl, Icon, Skeleton, Text } from '@chakra-ui/react'
 
 import { BxsCheckCircle, BxsError, BxsInfoCircle } from '~assets/icons'
+import FormLabel from '~components/FormControl/FormLabel'
+import Input from '~components/Input'
 
 import { useAdminFormPayments, useAdminFormSettings } from '../../queries'
 
@@ -70,11 +72,37 @@ const PaymentsAccountValidation = () => {
   )
 }
 
+const PaymentsAccountInformation = () => {
+  const { data: settings, isLoading } = useAdminFormSettings()
+
+  return (
+    <Skeleton isLoaded={!isLoading}>
+      <FormControl mb="2.5rem">
+        <FormLabel mb="0.75rem" isRequired>
+          <Text textStyle="subhead-1">Target Account ID</Text>
+          <Text textStyle="body-2" textColor="secondary.400">
+            This is the account ID connected to this form.
+          </Text>
+        </FormLabel>
+        <Input
+          isDisabled={true}
+          value={settings?.payments?.target_account_id}
+        ></Input>
+      </FormControl>
+    </Skeleton>
+  )
+}
+
 const PaymentsSectionText = () => {
   const { data: settings, isLoading } = useAdminFormSettings()
 
   if (settings?.payments?.enabled && settings?.payments?.target_account_id) {
-    return <PaymentsAccountValidation />
+    return (
+      <>
+        <PaymentsAccountValidation />
+        <PaymentsAccountInformation />
+      </>
+    )
   }
 
   return (
