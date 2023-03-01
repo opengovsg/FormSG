fb32d47dcf9ec2997d1c5992e2a6349b5e058da9
foochifa <foochifa@gmail.com>
2023-03-01 13:52:15 +0800

    fix: add individual payment response component

---
+++ frontend/src/features/admin-form/responses/IndividualResponsePage/IndividualPaymentResponse.tsx
@@ -0,0 +1,76 @@
+import { Stack, Text } from '@chakra-ui/react'
+
+import { useIndividualPaymentSubmission } from './queries'
+
+export const IndividualPaymentResponse = (submissionId: {
+  submissionId: string
+}): JSX.Element => {
+  if (!submissionId) throw new Error('Missing submissionId')
+
+  const {
+    data: paymentData,
+    isLoading: isPaymentLoading,
+    isError: isPaymentError,
+  } = useIndividualPaymentSubmission()
+
+  return (
+    <Stack>
+      <Text
+        textStyle="h2"
+        as="h2"
+        color="primary.500"
+        mb="0.5rem"
+        _notFirst={{ mt: '2.5rem' }}
+      >
+        Payment
+      </Text>
+      {isPaymentLoading || isPaymentError ? (
+        <Text>Payment was not enabled when this form was submitted</Text>
+      ) : paymentData ? (
+        <>
+          <Stack>
+            <Stack direction={{ base: 'column', md: 'row' }}>
+              <Text textStyle="subhead-1">Payment amount:</Text>
+              <Text>
+                S$
+                {(paymentData.amount / 100).toLocaleString('en-GB', {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </Text>
+            </Stack>
+            <Stack direction={{ base: 'column', md: 'row' }}>
+              <Text textStyle="subhead-1">Payment status:</Text>
+              <Text>{paymentData.status.toUpperCase()}</Text>
+            </Stack>
+            <Stack direction={{ base: 'column', md: 'row' }}>
+              <Text textStyle="subhead-1">Payment date:</Text>
+              <Text>
+                {new Intl.DateTimeFormat('en-GB', {
+                  year: 'numeric',
+                  month: 'numeric',
+                  day: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  second: 'numeric',
+                  timeZoneName: 'shortOffset',
+                }).format(new Date(paymentData.created))}
+              </Text>
+            </Stack>
+            <Stack direction={{ base: 'column', md: 'row' }}>
+              <Text textStyle="subhead-1">Payment intent ID:</Text>
+              <Text>{paymentData.paymentIntentId}</Text>
+            </Stack>
+            <Stack direction={{ base: 'column', md: 'row' }}>
+              <Text textStyle="subhead-1">Transaction fee:</Text>
+              {/* TODO: Change this to actual transaction fee once application fee object has been added */}
+              <Text>$0.06</Text>
+            </Stack>
+          </Stack>
+        </>
+      ) : (
+        <Text>Payment data not found</Text>
+      )}
+    </Stack>
+  )
+}
