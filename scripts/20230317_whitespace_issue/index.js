3243415b0f746c9435ad4dbab60017a164a8e190
tshuli <shuli@open.gov.sg>
2023-03-20 10:07:49 +0800

    chore: lint

--- scripts/20230317_whitespace_issue/index.js
+++ scripts/20230317_whitespace_issue/index.js
@@ -8,9 +8,9 @@ async function getStats(db) {
 
   const cursor = await collection
     .find({
-      responseMode: "encrypt",
-      status: "PUBLIC",
-      "form_logics.1": {$exists: true}
+      responseMode: 'encrypt',
+      status: 'PUBLIC',
+      'form_logics.1': { $exists: true },
     })
     .project({
       form_fields: 1,
@@ -21,56 +21,57 @@ async function getStats(db) {
       created: 1,
     })
 
-    const formsWithInvalidWhiteSpace = {};
-    const formsWhereWhiteSpaceAffectsLogic = {};
-    const formsWhereWhiteSpaceAffectsLogicWithSubmissions = {};
+  const formsWithInvalidWhiteSpace = {}
+  const formsWhereWhiteSpaceAffectsLogic = {}
+  const formsWhereWhiteSpaceAffectsLogicWithSubmissions = {}
 
   await cursor.forEach((item) => {
     const id = item._id.toString()
 
     const fieldsWithInvalidWhiteSpaces = item.form_fields
-      .filter(ff => ['dropdown', 'radiobutton', 'checkbox'].includes(ff.fieldType))
-      .filter(ff => ff.fieldOptions.some(fo => fo.trim() != fo))
+      .filter((ff) =>
+        ['dropdown', 'radiobutton', 'checkbox'].includes(ff.fieldType),
+      )
+      .filter((ff) => ff.fieldOptions.some((fo) => fo.trim() != fo))
 
-    if (fieldsWithInvalidWhiteSpaces.length <= 0) return;
+    if (fieldsWithInvalidWhiteSpaces.length <= 0) return
 
-    formsWithInvalidWhiteSpace[id] = item;
+    formsWithInvalidWhiteSpace[id] = item
 
     const fields = fieldsWithInvalidWhiteSpaces.reduce((acc, ff) => {
-      acc[ff._id.toString()] = ff;
-      ff.trimmedFieldOptions = ff.fieldOptions.map(v => v.trim())
+      acc[ff._id.toString()] = ff
+      ff.trimmedFieldOptions = ff.fieldOptions.map((v) => v.trim())
       return acc
-    },
-    {});
+    }, {})
 
     // now check if any of the conditions reference one of the invalid field AND if the condition has a whitespace mismatch
-    all_logic:
-    for (const fl of item.form_logics) {
+    all_logic: for (const fl of item.form_logics) {
       for (const flc of fl.conditions) {
         const field = fields[flc.field.toString()]
 
-        if (!field) continue; // not a target we care about
+        if (!field) continue // not a target we care about
 
-        let values;
+        let values
         try {
-          values = Array.isArray(flc.value) ? flc.value.map(v => v.trim()) : [`${flc.value}`.trim()]; // trim to repro logic from Feb 23rd fix
-        }
-        catch(err) {
-          console.error(flc);
-          console.error(field);
-          throw err;
+          values = Array.isArray(flc.value)
+            ? flc.value.map((v) => v.trim())
+            : [`${flc.value}`.trim()] // trim to repro logic from Feb 23rd fix
+        } catch (err) {
+          console.error(flc)
+          console.error(field)
+          throw err
         }
 
         for (const value of values) {
-          if (field.fieldOptions.includes(value)) continue; // the logic value matches field values - all good!
+          if (field.fieldOptions.includes(value)) continue // the logic value matches field values - all good!
           if (field.trimmedFieldOptions.includes(value)) {
             // this is the bug we are after - the logic value only matches a field value if trimmed - not good T_T
             formsWhereWhiteSpaceAffectsLogic[id] = {
               form: item,
               condition: flc,
               value,
-            };
-            break all_logic;
+            }
+            break all_logic
           }
         }
       }
@@ -79,26 +80,30 @@ async function getStats(db) {
     if (formsWhereWhiteSpaceAffectsLogic[id]) {
       console.log({
         formid: id,
-        field_with_invalid_whitespaces: fieldsWithInvalidWhiteSpaces.map(ff => ({
-          id: ff._id.toString(),
-          title: ff.title,
-          values: ff.fieldOptions,
-          bad_values: ff.fieldOptions.filter(fo => fo.trim() != fo).map(o => `"${o}"`).join(',')
-        })),
+        field_with_invalid_whitespaces: fieldsWithInvalidWhiteSpaces.map(
+          (ff) => ({
+            id: ff._id.toString(),
+            title: ff.title,
+            values: ff.fieldOptions,
+            bad_values: ff.fieldOptions
+              .filter((fo) => fo.trim() != fo)
+              .map((o) => `"${o}"`)
+              .join(','),
+          }),
+        ),
         logic: {
           condition: formsWhereWhiteSpaceAffectsLogic[id].condition,
-          value: formsWhereWhiteSpaceAffectsLogic[id].value
-        }
-      });
+          value: formsWhereWhiteSpaceAffectsLogic[id].value,
+        },
+      })
     }
-
   })
 
   console.log('')
   console.log('Number of forms with field with invalid whitespace')
-  console.log(Object.keys(formsWithInvalidWhiteSpace).length);
+  console.log(Object.keys(formsWithInvalidWhiteSpace).length)
   console.log('Number of forms where whitespace affects logic:')
-  console.log(Object.keys(formsWhereWhiteSpaceAffectsLogic).length);
+  console.log(Object.keys(formsWhereWhiteSpaceAffectsLogic).length)
 
   // TODO query submission to chedk how many forms of those forms had submissions in the past 3 weeks.
 
@@ -109,30 +114,44 @@ async function getStats(db) {
       form: new ObjectId(id),
       created: {
         $gte: new Date('2023-02-20T00:00:00.000+0800'),
-        $lt: new Date('2023-03-17T21:00:00.000+0800')
-      }
+        $lt: new Date('2023-03-17T21:00:00.000+0800'),
+      },
     })
 
     if (numSubmissions > 0) {
-      formsWhereWhiteSpaceAffectsLogicWithSubmissions[id] = formsWhereWhiteSpaceAffectsLogic[id];
-      formsWhereWhiteSpaceAffectsLogicWithSubmissions[id].numSubmissions = numSubmissions
+      formsWhereWhiteSpaceAffectsLogicWithSubmissions[id] =
+        formsWhereWhiteSpaceAffectsLogic[id]
+      formsWhereWhiteSpaceAffectsLogicWithSubmissions[id].numSubmissions =
+        numSubmissions
     }
   }
 
-  console.log('Number of forms where whitespace affects logic with submissions in the past 3 weeks:');
-  console.log(Object.keys(formsWhereWhiteSpaceAffectsLogicWithSubmissions).length);
+  console.log(
+    'Number of forms where whitespace affects logic with submissions in the past 3 weeks:',
+  )
+  console.log(
+    Object.keys(formsWhereWhiteSpaceAffectsLogicWithSubmissions).length,
+  )
 
-  const sortedForms = Object.values(formsWhereWhiteSpaceAffectsLogicWithSubmissions).sort((f1, f2) => f2.numSubmissions - f1.numSubmissions)
+  const sortedForms = Object.values(
+    formsWhereWhiteSpaceAffectsLogicWithSubmissions,
+  ).sort((f1, f2) => f2.numSubmissions - f1.numSubmissions)
 
-  console.log('Total number of potentially affected submissions:');
-  console.log(sortedForms.reduce((acc, f) => acc += f.numSubmissions, 0));
+  console.log('Total number of potentially affected submissions:')
+  console.log(sortedForms.reduce((acc, f) => (acc += f.numSubmissions), 0))
 
   console.log('-----')
-  console.log(sortedForms.map((data) => `${data.form._id} (${data.numSubmissions}): ${data.form.title} `).join('\n'));
+  console.log(
+    sortedForms
+      .map(
+        (data) =>
+          `${data.form._id} (${data.numSubmissions}): ${data.form.title} `,
+      )
+      .join('\n'),
+  )
 }
 
 ;(async function main() {
-
   const client = new MongoClient(process.env.DB_URI)
 
   await client.connect()
