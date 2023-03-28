1ffd11c6a3a94bc5c90fa830b4f7f40218045bad
foochifa <foochifa@gmail.com>
2023-03-28 09:58:46 +0800

    fix: remove payments guide as it is currently not ready

--- frontend/src/features/admin-form/settings/components/PaymentSettingsSection/PaymentsUnsupportedMsg.tsx
+++ frontend/src/features/admin-form/settings/components/PaymentSettingsSection/PaymentsUnsupportedMsg.tsx
@@ -1,8 +1,5 @@
 import { Flex, Text } from '@chakra-ui/react'
 
-import { GUIDE_PAYMENTS } from '~constants/links'
-import Link from '~components/Link'
-
 import { SettingsUnsupportedSvgr } from '~features/admin-form/settings/svgrs/SettingsUnsupportedSvgr'
 
 export const PaymentsUnsupportedMsg = (): JSX.Element => {
@@ -13,10 +10,7 @@ export const PaymentsUnsupportedMsg = (): JSX.Element => {
       </Text>
       <Text textStyle="body-1" color="secondary.500" mb="2.5rem">
         Collect payments through FormSG via integration with Stripe. This
-        feature is only available in Storage Mode.{' '}
-        <Link isExternal href={GUIDE_PAYMENTS}>
-          Read more about payments
-        </Link>
+        feature is only available in Storage Mode.
       </Text>
       <SettingsUnsupportedSvgr />
     </Flex>
