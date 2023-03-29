3631498f6171563e42cd93e36739d6c1deb478a9
foochifa <foochifa@gmail.com>
2023-03-29 16:59:32 +0800

    feat: pass in encryption and strip connection check to payment drawer

--- frontend/src/features/admin-form/create/payment/PaymentTab.tsx
+++ frontend/src/features/admin-form/create/payment/PaymentTab.tsx
@@ -4,18 +4,32 @@ import { useAdminForm } from '~features/admin-form/common/queries'
 
 import { PaymentContent } from './PaymentContent'
 import { PaymentDrawer } from './PaymentDrawer'
+import { defaultPaymentsField } from './usePaymentStore'
 
 export const PaymentTab = (): JSX.Element => {
   const { data: form } = useAdminForm()
 
-  return form?.responseMode === FormResponseMode.Encrypt &&
-    form?.payments_field ? (
+  const isEncryptMode = form?.responseMode === FormResponseMode.Encrypt
+
+  const paymentsField =
+    isEncryptMode && form?.payments_field
+      ? form.payments_field
+      : defaultPaymentsField
+
+  // Check if payment channel
+  const isStripeConnected =
+    isEncryptMode &&
+    form.payments_channel?.target_account_id !== undefined &&
+    form.payments_channel?.target_account_id !== ''
+
+  return (
     <>
-      <PaymentDrawer paymentsField={form.payments_field} />
-      <PaymentContent paymentsField={form.payments_field} />
+      <PaymentDrawer
+        isEncryptMode={isEncryptMode}
+        isStripeConnected={isStripeConnected}
+        paymentsField={paymentsField}
+      />
+      <PaymentContent paymentsField={paymentsField} />
     </>
-  ) : (
-    // TODO: Handle PaymentTab for email mode forms
-    <></>
   )
 }
