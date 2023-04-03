c59572b64c06d51d6cf3d12c851a2bb4faf35bfa
Ken <ken@open.gov.sg>
2023-04-03 18:49:30 +0800

    refactor: remove side effects, switch to throttle

--- frontend/src/hooks/payments/useBrowserStm/utils.ts
+++ frontend/src/hooks/payments/useBrowserStm/utils.ts
@@ -1,3 +1,6 @@
+import { differenceInHours } from 'date-fns'
+import throttle from 'lodash/throttle'
+
 export type StmEntryDto = {
   paymentId: string
   dateCreated: number
@@ -5,37 +8,63 @@ export type StmEntryDto = {
 export type BrowserStmDto = {
   [formId: string]: StmEntryDto
 }
-export const deserialize = (jsonString: string): BrowserStmDto => {
-  try {
-    return JSON.parse(jsonString)
-  } catch {
-    return {}
-  }
+
+const EXPIRY_TIME_IN_HRS = 24 // 1 day
+const EVICTION_DEBOUNCE_TIME = 1000 * 20 // 20 seconds
+
+const _processEviction = (entryObj: BrowserStmDto) => {
+  const curTime = Date.now()
+  const returnObj = { ...entryObj }
+  Object.keys(returnObj).forEach((formId) => {
+    const { dateCreated } = returnObj[formId]
+    const deltaHrs = differenceInHours(curTime, new Date(dateCreated))
+    if (deltaHrs > EXPIRY_TIME_IN_HRS) {
+      delete returnObj[formId]
+    }
+  })
+  return returnObj
 }
+
+export const processEviction = throttle(
+  _processEviction,
+  EVICTION_DEBOUNCE_TIME,
+  { leading: true, trailing: false },
+)
+
+export const deserialize = (jsonString: string): BrowserStmDto => {
+  let parsedObj
+  try {
+    parsedObj = JSON.parse(jsonString)
+  } catch {
+    parsedObj = {}
+  }
+  return parsedObj
+}
+
 export const serialize = (entryObj: BrowserStmDto) => {
   return JSON.stringify(entryObj)
-}
-export const processEviction = (data: BrowserStmDto) => {
-  // asd
 }
 
 export const addEntry = (
   entryObj: BrowserStmDto,
   { formId, paymentId }: { formId: string; paymentId: string },
 ): BrowserStmDto => {
-  entryObj[formId] = {
-    paymentId,
-    dateCreated: Number(new Date()),
+  return {
+    ...entryObj,
+    formId: {
+      paymentId,
+      dateCreated: Date.now(),
+    },
   }
-  return entryObj
 }
 
 export const deleteEntry = (
   entryObj: BrowserStmDto,
   { formId }: { formId: string },
 ): BrowserStmDto => {
-  delete entryObj[formId]
-  return entryObj
+  const returnObj = { ...entryObj }
+  delete returnObj[formId]
+  return returnObj
 }
 
 export const getEntry = (
