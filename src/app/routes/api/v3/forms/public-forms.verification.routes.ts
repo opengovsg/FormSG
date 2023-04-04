c155ebb021e2b40437a92085108c9f6e90893d0a
LinHuiqing <huiqing@open.gov.sg>
2023-04-04 10:13:13 +0800

    chore: remove unnecessary reset payment field

--- src/app/routes/api/v3/forms/public-forms.verification.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.verification.routes.ts
@@ -27,19 +27,6 @@ PublicFormsVerificationRouter.route(
 ).post(VerificationController.handleResetFormFieldVerification)
 
 /**
- * Route for resetting the verification of a given field
- * @returns 204 when reset is successful
- * @returns 400 when the transaction has expired
- * @returns 404 when the form could not be found
- * @returns 404 when the transaction could not be found
- * @returns 404 when the field could not be found
- * @returns 500 when a database error occurs
- */
-PublicFormsVerificationRouter.route(
-  '/:formId([a-fA-F0-9]{24})/fieldverifications/:transactionId([a-fA-F0-9]{24})/payment/reset',
-).post(VerificationController.handleResetPaymentFieldVerification)
-
-/**
  * Route for verifying the otp for a given field
  * @returns 200 when the otp is correct and the parameters are valid
  * @returns 400 when TransactionExpiredError occurs
