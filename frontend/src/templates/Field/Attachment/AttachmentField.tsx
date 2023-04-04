4c79d758b807e32bf71b406bec071cdb988cc03c
Kar Rui Lau <karrui@open.gov.sg>
2023-04-04 20:19:26 +0800

    fix(AttachmentField): clone attachment to (hopefully) fix NetworkError

--- frontend/src/templates/Field/Attachment/AttachmentField.tsx
+++ frontend/src/templates/Field/Attachment/AttachmentField.tsx
@@ -1,5 +1,9 @@
 import { useCallback, useMemo } from 'react'
-import { Controller, useFormContext } from 'react-hook-form'
+import {
+  Controller,
+  ControllerRenderProps,
+  useFormContext,
+} from 'react-hook-form'
 
 import { MB } from '~shared/constants/file'
 import { FormColorTheme } from '~shared/types'
@@ -42,6 +46,35 @@ export const AttachmentField = ({
     [fieldName, setError],
   )
 
+  const handleFileChange = useCallback(
+    (onChange: ControllerRenderProps['onChange']) =>
+      async (file: File | undefined) => {
+        clearErrors(fieldName)
+        let clone = file
+        // Clone file due to bug where attached file may be empty or corrupted if the
+        // file is a Google Drive file selected from Android's native file picker.
+        // Cloning the file ensures that the file can be read (and/or not mutated by Android)
+        // and throws an error if the file cannot be read instead of silently failing and only throw
+        // an error during form submission.
+        // See https://bugs.chromium.org/p/chromium/issues/detail?id=1063576#c79
+        // and https://stackoverflow.com/questions/62714319/attached-from-google-drivecloud-storage-in-android-file-gives-err-upload-file
+        // as possible sources of the error  (still not confirmed it is the same thing).
+        if (file) {
+          try {
+            const buffer = await file.arrayBuffer()
+            clone = new File([buffer], file.name, { type: file.type })
+          } catch (error) {
+            setErrorMessage(
+              'Error uploading file. Please try again or choose another file.',
+            )
+            console.error(error) // For RUM error tracking
+          }
+        }
+        onChange(clone)
+      },
+    [clearErrors, fieldName, setErrorMessage],
+  )
+
   return (
     <FieldContainer schema={schema}>
       <Controller
@@ -53,10 +86,7 @@ export const AttachmentField = ({
             maxSize={maxSizeInBytes}
             accept={VALID_EXTENSIONS}
             showFileSize
-            onChange={(file) => {
-              clearErrors(fieldName)
-              onChange(file)
-            }}
+            onChange={handleFileChange(onChange)}
             onError={setErrorMessage}
             title={`${schema.questionNumber}. ${schema.title}`}
           />
