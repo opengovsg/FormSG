8080ee3622af6d82efe99917d454d5b9e781adef
foochifa <foochifa@gmail.com>
2023-04-20 16:36:35 +0800

    feat: create duplicate payment modal

---
+++ frontend/src/features/public-form/components/DuplicatePaymentModal/DuplicatePaymentModal.tsx
@@ -0,0 +1,72 @@
+import { MouseEvent, MouseEventHandler } from 'react'
+import {
+  Button,
+  ButtonGroup,
+  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react'
+
+import { getPaymentPageUrl } from '~features/public-form/utils/urls'
+
+type FormPaymentModalProps = {
+  onSubmit: MouseEventHandler<HTMLButtonElement> | undefined
+  onClose: () => void
+  isSubmitting: boolean
+  formId: string
+  paymentId: string
+}
+
+export const FormPaymentModal = ({
+  onSubmit,
+  onClose,
+  isSubmitting,
+  formId,
+  paymentId,
+}: FormPaymentModalProps): JSX.Element => {
+  const paymentUrl = getPaymentPageUrl(formId, paymentId)
+
+  // We need to dismiss the Modal to release the scroll lock that affects the captcha
+  const closeAndSubmit = (event: MouseEvent<HTMLButtonElement>) => {
+    onClose()
+    if (onSubmit) {
+      onSubmit(event)
+    }
+  }
+  return (
+    <>
+      <Modal isOpen onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalHeader>Proceed to pay again?</ModalHeader>
+          <ModalBody>
+            We noticed a successful payment made on this form by your email
+            address.
+            <Link href={paymentUrl}>View your previous payment</Link>
+          </ModalBody>
+          <ModalBody>Do you wish to proceed to make another payment?</ModalBody>
+          <ModalFooter>
+            <ButtonGroup>
+              <Button variant="clear" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                isLoading={isSubmitting}
+                loadingText="Submitting"
+                onClick={closeAndSubmit}
+              >
+                Proceed to pay
+              </Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
