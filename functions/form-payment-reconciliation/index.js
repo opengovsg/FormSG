d760fb2f8c4dc5c120d6d9b804e22fed5f2c8ff8
Ken <ken@open.gov.sg>
2023-04-25 00:37:12 +0800

    add single reconcile on lambda

--- functions/form-payment-reconciliation/index.js
+++ functions/form-payment-reconciliation/index.js
@@ -22,15 +22,55 @@ function getSecrets() {
     })
 }
 
-const getPendingQueries = () => {
-  return fetch('https://staging.form.gov.sg/api/v3/payments/pendingPayments')
+const getPendingQueries = (apiSecret) => {
+  return fetch('https://staging.form.gov.sg/api/v3/payments/pendingPayments', {
+    headers: {
+      'x-cron-payment-secret': apiSecret,
+    },
+  }).then((d) => d.json())
 }
+
+const reconcileAccount = (apiSecret, stripeAccountId) => {
+  return fetch('https://staging.form.gov.sg/api/v3/payments/reconcileAccount', {
+    method: 'POST',
+    headers: {
+      'x-cron-payment-secret': apiSecret,
+    },
+    body: { stripeAccountId },
+  }).then((d) => d.json())
+}
+
 async function main() {
   const secret = await getSecrets()
 
-  const pendingQueries = await getPendingQueries()
+  const pendingQueries = await getPendingQueries(
+    secret['CRON_PAYMENT_API_SECRET'],
+  )
   console.log(secret)
+
+  if (pendingQueries.length <= 0) {
+    console.log(
+      'Exiting. No pending queries. Query length:',
+      pendingQueries.data.length,
+    )
+    return
+  }
+
+  console.log('Pending Queries Report')
   console.log(JSON.stringify(pendingQueries, null, 2))
+
+  const firstAccountToProcess = pendingQueries.data[0].stripeAccount
+  if (firstAccountToProcess) {
+    console.log('Exiting. Invalid stripeAccount:', firstAccountToProcess)
+    return
+  }
+
+  const reconcileResult = await reconcileAccount(
+    secret['CRON_PAYMENT_API_SECRET'],
+    firstAccountToProcess,
+  )
+  console.log('Reconcile Report')
+  console.log(JSON.stringify(reconcileResult, null, 2))
 }
 
 if (require.main === module) {
