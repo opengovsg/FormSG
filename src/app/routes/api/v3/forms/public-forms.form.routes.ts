cb60d3c9228cdb47a65dba1031ff74c697541f70
wanlingt <wanling@open.gov.sg>
2023-05-15 16:20:02 +0800

    docs: add comments for GET sample submission endpoint

--- src/app/routes/api/v3/forms/public-forms.form.routes.ts
+++ src/app/routes/api/v3/forms/public-forms.form.routes.ts
@@ -24,6 +24,16 @@ PublicFormsFormRouter.route('/:formId([a-fA-F0-9]{24})').get(
   PublicFormController.handleGetPublicForm,
 )
 
+/**
+ * Returns a sample submission response of the specified form to the user
+ *
+ * @route GET /:formId/sample-submission
+ *
+ * @returns 200 with form when form exists and is public
+ * @returns 404 when form is private or form with given ID does not exist
+ * @returns 410 when form is archived
+ * @returns 500 when database error occurs
+ */
 PublicFormsFormRouter.route('/:formId([a-fA-F0-9]{24})/sample-submission').get(
   PublicFormController.handleGetPublicFormSampleSubmission,
 )
