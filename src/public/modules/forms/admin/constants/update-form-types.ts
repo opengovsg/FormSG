3e6ab114ede7bfff4a4ec2c748207e83e4f60720
Justyn Oh <justynoh@gmail.com>
2023-05-17 17:51:17 +0800

    chore: delete angular code

--- src/public/modules/forms/admin/constants/update-form-types.ts
+++
@@ -1,7 +0,0 @@
-export const UPDATE_FORM_TYPES = {
-  UpdateField: 'update-field',
-  CreateField: 'create-field',
-  DeleteField: 'delete-field',
-  DuplicateField: 'duplicate-field',
-  ReorderField: 'reorder-field',
-}
