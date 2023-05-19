79cf16ad5f31f19f2921c8cbf6595be8e5f1f15d
Justyn Oh <justynoh@gmail.com>
2023-05-19 10:41:10 +0800

    chore: script to add targetAccountId to payment documents

---
+++ scripts/20230519_add_target_account_id/script.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+// BEFORE
+// Count total number of payments with no received webhooks. This should be 0.
+db.getCollection('payments').countDocuments({ webhookLog: { $size: 0 } }) === 0
+
+// Count total number of payments.
+const total = db.getCollection('payments').countDocuments({})
+
+// Count total number of payments with no targetAccountId value. This should be equal to the total number of payments.
+db.getCollection('payments').countDocuments({ targetAccountId: { $exists: false } }) === total
+
+// UPDATE
+db.getCollection('payments').update(
+  {},
+  { $set: { targetAccountId: { $first: '$webhookLog.account' } } },
+)
+
+// AFTER
+// Count total number of payments with targetAccountId value. This should be equal to the total number of payments.
+db.getCollection('payments').countDocuments({ targetAccountId: { $exists: true } }) === total
+
+// Count total number of payments with no targetAccountId value. This should be equal to 0
+db.getCollection('payments').countDocuments({ targetAccountId: { $exists: false } }) === 0
