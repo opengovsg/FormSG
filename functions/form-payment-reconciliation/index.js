cc5ba050c639d99ff946890a1bc039c772ffb14c
Justyn Oh <justynoh@gmail.com>
2023-05-25 13:03:35 +0800

    feat: cron job improvements

--- functions/form-payment-reconciliation/index.js
+++ functions/form-payment-reconciliation/index.js
@@ -1,18 +1,38 @@
+/**
+ * Main file that runs payment reconciliation CRON job.
+ * Required env vars:
+ * - AWS_REGION: the aws region which the
+ * - SSM_ENV_SITE_NAME: ['prod', 'staging', 'staging-alt', 'staging-alt2', 'uat']
+ *
+ * Required parameters in parameter store `<SSM_ENV_SITE_NAME>-cron-payment`:
+ * - CRON_PAYMENT_API_SECRET: the shared secret to validate protected routes in FormSG API
+ * - SLACK_API_SECRET: the API key for posting reports to slack
+ */
+
 const { SSMClient, GetParameterCommand } = require('@aws-sdk/client-ssm')
-const { generateLinkToCloudwatch, postToSlack } = require('./src/utils')
+const { generateLinkToCloudwatch, getPostToSlack } = require('./src/utils')
 
-const AWS_REGION = 'ap-southeast-1'
+const AWS_REGION = process.env.AWS_REGION
 
-const PARAMETER_STORE_NAME = 'staging-cron-payment'
+const ENV_SITE_NAME = process.env.SSM_ENV_SITE_NAME
+const CRON_API_PREFIX = `https://${
+  ENV_SITE_NAME === 'prod' ? '' : `${ENV_SITE_NAME}.`
+}form.gov.sg/api/v3/payments/reconcile/`
 
-const SUB_DOMAIN = process.env.ENV_SITE_SUBDOMAIN || 'staging.'
-
-const KEY_VALUE_PAIR_REGEX = /^([^\s=]+)\s*=\s*(\S+)$/ // Make use of capture groups to obtain the key and value.
+const PARAMETER_STORE_NAME = `${ENV_SITE_NAME}-cron-payment`
+const CRON_PAYMENT_API_SECRET_KEY = 'CRON_PAYMENT_API_SECRET'
+const CRON_PAYMENT_SLACK_SECRET_KEY = 'SLACK_API_SECRET'
 
 const API_AUTH_HEADER = 'x-formsg-cron-payment-secret'
 
-/** TODO: migrate to separate file */
-const getSecrets = () => {
+/**
+ * Helper function to obtain secrets map from parameter store.
+ */
+const getSSMSecrets = () => {
+  // Regex used to match key-value pairs from parameter store.
+  // Makes use of capture groups to obtain the key and value.
+  const KEY_VALUE_PAIR_REGEX = /^([^\s=]+)\s*=\s*(\S+)$/
+
   const awsSsmClient = new SSMClient({ region: AWS_REGION })
   const input = {
     Name: PARAMETER_STORE_NAME,
@@ -33,115 +53,234 @@ const getSecrets = () => {
   })
 }
 
-const getPendingQueries = async (apiSecret) => {
-  return fetch(
-    `https://${SUB_DOMAIN}form.gov.sg/api/v3/payments/pendingPayments`,
-    {
+/**
+ * Helper function to get API functions given the API secret.
+ */
+const getApi = (apiSecret) => {
+  const getIncompletePayments = async () => {
+    return fetch(`${CRON_API_PREFIX}incompletePayments`, {
       headers: { [API_AUTH_HEADER]: apiSecret },
-    },
-  ).then((d) => d.json())
-}
+    }).then(async (res) => ({ ok: res.ok, data: await res.json() }))
+  }
 
-const reconcileAccount = async (apiSecret, stripeAccountId) => {
-  return fetch(
-    `https://${SUB_DOMAIN}form.gov.sg/api/v3/payments/reconcileAccount`,
-    {
+  const reconcileAccount = async (stripeAccount, paymentIds) => {
+    return fetch(`${CRON_API_PREFIX}account/${stripeAccount}`, {
       method: 'POST',
       headers: { [API_AUTH_HEADER]: apiSecret },
-      body: { stripeAccountId },
-    },
-  ).then((d) => d.json())
+      body: { paymentIds },
+    }).then(async (res) => ({ ok: res.ok, data: await res.json() }))
+  }
+
+  return { getIncompletePayments, reconcileAccount }
 }
 
-const reportToSlack = async ({
-  env,
-  title,
-  reportContent,
-  cloudwatchLink,
-  timing,
-  functionName,
-}) => {
-  const titleContentWithHeaderMrkdwn = `*${title}* for *${env}*`
-  const reportContentWithCodeMrkdwn = '```' + reportContent + '```'
-  const linkContentInHtml = `Detailed report on <${cloudwatchLink}|cloudwatch>`
-  const runContent = `Run took(ms) ${timing}`
-  const preparedTimeContent = `Report from \`${functionName}\` prepared on \`${new Date().toLocaleString(
-    'en-SG',
-  )} SGT\``
-  const msgContent = [
-    titleContentWithHeaderMrkdwn,
-    `---`,
-    '', // spacing
-    reportContentWithCodeMrkdwn,
-    linkContentInHtml,
-    runContent,
-    preparedTimeContent,
-  ].join('\n')
-
-  return postToSlack(secret['SLACK_API_SECRET'], msgContent).then((d) =>
-    console.log(d),
-  )
-}
-
-let secret
 async function main(events, context) {
-  const startTime = Date.now()
-  secret = await getSecrets()
+  // Things will be appended as we go along.
+  const REPORT = [
+    `*Reconciliation report for ${ENV_SITE_NAME}*`,
+    '---',
+    '*Critical messages:*',
+  ]
 
-  const pendingQueries = await getPendingQueries(
-    secret['CRON_PAYMENT_API_SECRET'],
+  console.log('CRON job starting')
+  const CRON_START_TIME = Date.now()
+
+  // Step 0(a): Load secrets from SSM
+  const secret = await getSSMSecrets()
+
+  // Step 0(b): Get API functions based on secret keys
+  const { getIncompletePayments, reconcileAccount } = getApi(
+    secret[CRON_PAYMENT_API_SECRET_KEY],
   )
+  const postToSlack = getPostToSlack(secret[CRON_PAYMENT_SLACK_SECRET_KEY])
 
-  if (pendingQueries.length <= 0) {
+  // Wrap the primary running functionality in a function to take advantage of
+  // short-circuiting.
+  const run = async () => {
+    // Step 1: Get all incomplete payments from the past
+    console.log('Retrieving incomplete payments')
+
+    const getIncompletePaymentsResponse = await getIncompletePayments()
+
+    if (!getIncompletePaymentsResponse.ok) {
+      console.error(
+        'Error occurred while retrieving incomplete payments:',
+        getIncompletePaymentsResponse.data.message,
+      )
+      REPORT.push(
+        'Error occurred while retrieving incomplete payments: ' +
+          getIncompletePaymentsResponse.data.message,
+        '---',
+      )
+      return
+    }
+
+    if (getIncompletePaymentsResponse.data.length === 0) {
+      console.log('No incomplete payments to reconcile.')
+      REPORT.push('---', 'No incomplete payments to reconcile.')
+      return
+    }
+
     console.log(
-      'Exiting. No pending queries. Query length:',
-      pendingQueries.data.length,
+      'Incomplete payments\n' +
+        JSON.stringify(getIncompletePaymentsResponse.data, null, 2),
     )
-    return
+
+    // Step 2: Group all payments by stripeAccount.
+    console.log('Grouping payments by Stripe account')
+
+    const incompletePaymentsByStripeAccount = {}
+    getIncompletePaymentsResponse.data.forEach(
+      ({ stripeAccount, paymentId }) => {
+        if (
+          !Object.keys(incompletePaymentsByStripeAccount).includes(
+            stripeAccount,
+          )
+        ) {
+          incompletePaymentsByStripeAccount[stripeAccount] = []
+        }
+        incompletePaymentsByStripeAccount[stripeAccount].push(paymentId)
+      },
+    )
+
+    // Step 3: Reconcile each Stripe account individually
+    const reconciliationMeta = {
+      error: {
+        accounts: [],
+      },
+      ok: {
+        accounts: [],
+        events: {
+          success: [],
+          failed: [],
+        },
+        payments: {
+          match: [],
+          mismatch: [],
+          canceled: [],
+        },
+      },
+    }
+
+    Object.keys(incompletePaymentsByStripeAccount).forEach(
+      async (stripeAccount) => {
+        console.log('Reconciling Stripe account', stripeAccount)
+
+        const paymentIds = incompletePaymentsByStripeAccount[stripeAccount]
+        const reconcileAccountResponse = await reconcileAccount(
+          stripeAccount,
+          paymentIds,
+        )
+
+        if (!reconcileAccountResponse.ok) {
+          console.error(
+            `Error occurred while reconciling Stripe account ${stripeAccount}:`,
+            reconcileAccountResponse.data.message,
+          )
+          REPORT.push(
+            `Error occurred while reconciling Stripe account ${stripeAccount}:`,
+            reconcileAccountResponse.data.message,
+          )
+          reconciliationMeta.error.accounts.push(stripeAccount)
+          return
+        }
+
+        console.log(
+          `Reconcile attempt OK for Stripe account ${stripeAccount}`,
+          JSON.stringify(reconcileAccountResponse.data, null, 2),
+        )
+        reconciliationMeta.ok.accounts.push(stripeAccount)
+
+        // Aggregate data of resent events
+        reconcileAccountResponse.data.eventsReport.forEach(
+          ({ event, error }) => {
+            const identifier = `${stripeAccount}::${event.id}`
+            if (error) {
+              reconciliationMeta.ok.events.failed.push(
+                `${identifier} [ERROR: ${error}]`,
+              )
+            } else {
+              reconciliationMeta.ok.events.success.push(identifier)
+            }
+          },
+        )
+
+        // Aggregate data of payments
+        reconcileAccountResponse.data.reconciliationReport.forEach(
+          ({ payment, paymentIntent, mismatch, canceled }) => {
+            const identifier = `${stripeAccount}::${payment.id}::${paymentIntent.id}`
+
+            if (mismatch) {
+              reconciliationMeta.ok.payments.mismatch.push(identifier)
+              // Mismatched payments should never be canceled
+              if (canceled) {
+                console.error(
+                  `Found mismatched payment that was canceled: ${identifier}`,
+                )
+                REPORT.push(
+                  `Found mismatched payment that was canceled: ${identifier}`,
+                )
+              }
+            } else {
+              reconciliationMeta.ok.payments.match.push(identifier)
+              if (canceled) {
+                reconciliationMeta.ok.payments.canceled.push(identifier)
+              }
+            }
+          },
+        )
+      },
+    )
+
+    // Step 4: Aggregate reconciliation meta counts for reporting to Slack
+    const reconciliationMetaCounts = {
+      error: {
+        accounts: reconciliationMeta.error.accounts.length,
+      },
+      ok: {
+        accounts: reconciliationMeta.ok.accounts.length,
+        events: {
+          success: reconciliationMeta.ok.events.success.length,
+          failed: reconciliationMeta.ok.events.failed.length,
+        },
+        payments: {
+          match: reconciliationMeta.ok.payments.match.length,
+          mismatch: reconciliationMeta.ok.payments.mismatch.length,
+          canceled: reconciliationMeta.ok.payments.canceled.length,
+        },
+      },
+    }
+
+    console.log(
+      'Reconciliation result\n' + JSON.stringify(reconciliationMeta, null, 2),
+    )
+    REPORT.push(
+      '---',
+      'Reconciliation result (aggregated):',
+      '```' + JSON.stringify(reconciliationMetaCounts, null, 2) + '```',
+    )
   }
 
-  console.log(
-    'Pending Queries Report' + '\n' + JSON.stringify(pendingQueries, null, 2),
+  await run()
+
+  // Step 5: Push CRON metadata
+  console.log('Lambda event details:\n' + JSON.stringify(events, null, 2))
+  console.log('Lambda invocation context:\n' + JSON.stringify(context, null, 2))
+
+  REPORT.push(
+    'CRON job runtime (ms): ' + (Date.now() - CRON_START_TIME),
+    'Cloudwatch log: <' +
+      generateLinkToCloudwatch(AWS_REGION, context) +
+      '|cloudwatch>',
+    '---',
+    `Report from \`${
+      context.functionName
+    }\` prepared on \`${new Date().toLocaleString('en-SG')} SGT\``,
   )
 
-  const firstAccountToProcess =
-    pendingQueries.data[pendingQueries.data.length - 1].stripeAccount
-  if (!firstAccountToProcess) {
-    console.log('Exiting. Invalid stripeAccount:', firstAccountToProcess)
-    return
-  }
-
-  const reconcileStartTime = Date.now()
-  const reconcileResult = await reconcileAccount(
-    secret['CRON_PAYMENT_API_SECRET'],
-    firstAccountToProcess,
+  await postToSlack(REPORT.join('\n')).then((res) =>
+    console.log('Post to Slack response:\n' + JSON.stringify(res, null, 2)),
   )
-  const verboseReport = JSON.stringify(reconcileResult, null, 2)
-  const report = [
-    'Reconcile Report',
-    verboseReport,
-    'Reconcile took(ms): ' + (Date.now() - reconcileStartTime),
-    'Lambda Event Details: ' + JSON.stringify(events),
-    'Lambda Invocation Id: ' + JSON.stringify(context),
-    'Details: ' + generateLinkToCloudwatch(context),
-  ].join('\n')
-  console.log(report)
-
-  const summarizedReport = {
-    ...reconcileResult,
-    data: {
-      processedCount: reconcileResult.data.processed.length,
-      failedCount: reconcileResult.data.failed.length,
-    },
-  }
-  await reportToSlack({
-    env: SUB_DOMAIN,
-    title: 'Reconcilation Report',
-    reportContent: JSON.stringify(summarizedReport, null, 2),
-    cloudwatchLink: generateLinkToCloudwatch(context),
-    timing: Date.now() - startTime,
-    functionName: context.functionName,
-  })
 }
 
 if (require.main === module) {
--- functions/form-payment-reconciliation/src/utils.js
+++ functions/form-payment-reconciliation/src/utils.js
@@ -1,24 +1,22 @@
-const generateLinkToCloudwatch = (context) => {
+const generateLinkToCloudwatch = (region, context) => {
   const { logGroupName, logStreamName } = context
   return (
-    'https://ap-southeast-1.console.aws.amazon.com/cloudwatch/home?region=ap-southeast-1#logsV2:log-groups/log-group/' +
+    `https://${region}.console.aws.amazon.com/cloudwatch/home?region=${region}#logsV2:log-groups/log-group/` +
     encodeURIComponent(encodeURIComponent(logGroupName)) +
     '/log-events/' +
     encodeURIComponent(encodeURIComponent(logStreamName))
   )
 }
 
-const postToSlack = (slackApiSecret, message) => {
-  return fetch(`https://hooks.slack.com/services/${slackApiSecret}`, {
+const getPostToSlack = (apiSecret) => async (message) => {
+  return fetch(`https://hooks.slack.com/services/${apiSecret}`, {
     method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
+    headers: { 'Content-type': 'application/json' },
     body: JSON.stringify({ text: message }),
   })
 }
 
 module.exports = {
   generateLinkToCloudwatch,
-  postToSlack,
+  getPostToSlack,
 }
