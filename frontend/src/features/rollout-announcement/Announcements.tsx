210f7bf07d8a45cce749a3c743c970f82199e48f
LinHuiqing <huiqing@open.gov.sg>
2023-05-30 20:00:02 +0800

    feat: update localstorage prefix to display announcements

--- frontend/src/features/rollout-announcement/Announcements.tsx
+++ frontend/src/features/rollout-announcement/Announcements.tsx
@@ -2,8 +2,11 @@ import FirstAnnouncement from './assets/1-payments.json'
 import SecondAnnouncement from './assets/2-dnd.json'
 import ThirdAnnouncement from './assets/3-listview.json'
 
+// When updating this, remember to update the ROLLOUT_ANNOUNCEMENT_KEY_PREFIX with the new date
+// so admins will see new announcements.
 export const NEW_FEATURES = [
   {
+    // Announcement date: 2023-05-31
     title: 'Collect payments on your form',
     description:
       'Citizens can now pay for fees and services directly on your form! We integrate with Stripe to provide reliable payments and hassle-free reconciliations. Payment methods we support include credit card and PayNow.',
