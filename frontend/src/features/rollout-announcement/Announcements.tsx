fb6eddae42738879705bcf910b7aa9d86d1ec369
LinHuiqing <huiqing@open.gov.sg>
2023-05-31 08:49:58 +0800

    feat: remove old announcements

--- frontend/src/features/rollout-announcement/Announcements.tsx
+++ frontend/src/features/rollout-announcement/Announcements.tsx
@@ -1,6 +1,4 @@
-import FirstAnnouncement from './assets/1-payments.json'
-import SecondAnnouncement from './assets/2-dnd.json'
-import ThirdAnnouncement from './assets/3-listview.json'
+import PaymentsAnnouncementGraphic from './assets/payments.json'
 
 // When updating this, remember to update the ROLLOUT_ANNOUNCEMENT_KEY_PREFIX with the new date
 // so admins will see new announcements.
@@ -10,18 +8,6 @@ export const NEW_FEATURES = [
     title: 'Collect payments on your form',
     description:
       'Citizens can now pay for fees and services directly on your form! We integrate with Stripe to provide reliable payments and hassle-free reconciliations. Payment methods we support include credit card and PayNow.',
-    animationData: FirstAnnouncement,
-  },
-  {
-    title: 'Drag-and-drop form creation',
-    description:
-      'Add and rearrange fields with ease, and see a preview of your form while editing it.',
-    animationData: SecondAnnouncement,
-  },
-  {
-    title: 'List view for homepages',
-    description:
-      'See the important details of all your forms at a glance, in this improved homepage layout.',
-    animationData: ThirdAnnouncement,
+    animationData: PaymentsAnnouncementGraphic,
   },
 ]
