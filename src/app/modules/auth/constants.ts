3c596d25255bfde06a21bf52f100d9711e7c78e2
Timothee Groleau <timothee.groleau@gmail.com>
2023-06-04 21:35:09 +0800

    feat: increase number of salting round used for API token hashing

--- src/app/modules/auth/constants.ts
+++ src/app/modules/auth/constants.ts
@@ -1,4 +1,4 @@
 export const BEARER_STRING = 'Bearer'
 export const BEARER_SEPARATOR = ' '
 export const API_KEY_SEPARATOR = '_'
-export const DEFAULT_SALT_ROUNDS = 1
+export const DEFAULT_SALT_ROUNDS = 2
