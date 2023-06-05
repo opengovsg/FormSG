6c3ba804da2a618e538c6dd0fb05c0eb5b2af434
Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>
2023-06-05 10:44:21 +0800

    feat: add debit card to copy and dashboard banner for payments (#6416)
    * chore: update announcement copy to incl debit
    
    * feat: dashboard banner annoucing payments
    
    * fix: reduce width of dashboard banner
    
    * chore: add debit copy to feature list

--- frontend/src/features/whats-new/FeatureUpdateList.ts
+++ frontend/src/features/whats-new/FeatureUpdateList.ts
@@ -34,7 +34,7 @@ export const FEATURE_UPDATE_LIST: FeatureUpdateList = {
       title: 'Collect payments on your form',
       date: new Date('31 May 2023 GMT+8'),
       description:
-        'Citizens can now pay for fees and services directly on your form! We integrate with Stripe to provide reliable payments and hassle-free reconciliations. Payment methods we support include credit card and PayNow.',
+        'Citizens can now pay for fees and services directly on your form! We integrate with Stripe to provide reliable payments and hassle-free reconciliations. Payment methods we support include debit / credit cards and PayNow.',
       image: {
         animationData: Animation1,
         alt: 'Collect payments on your form',
