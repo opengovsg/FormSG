2d1f3d61f1dd19dc117e4e521a9e4355f68eecf1
mayying <mayying@open.gov.sg>
2023-06-12 16:28:02 +0800

    remove unnecessary usage of portal

--- frontend/src/features/public-form/components/FormIssueFeedback/FormIssueFeedbackButton.tsx
+++ frontend/src/features/public-form/components/FormIssueFeedback/FormIssueFeedbackButton.tsx
@@ -1,5 +1,5 @@
 import { BiMessage } from 'react-icons/bi'
-import { Flex, Portal, useDisclosure } from '@chakra-ui/react'
+import { Flex, useDisclosure } from '@chakra-ui/react'
 
 import { noPrintCss } from '~utils/noPrintCss'
 import IconButton from '~components/IconButton'
@@ -14,29 +14,27 @@ export const FormIssueFeedbackButton = (): JSX.Element => {
   const { isPreview } = usePublicFormContext()
 
   return (
-    <Portal>
-      <Flex
-        position="fixed"
-        bottom="2.75rem"
-        right="2.75rem"
-        cursor="pointer"
-        sx={noPrintCss}
-      >
-        <Tooltip placement="left" label="Report an issue">
-          <IconButton
-            variant="outline"
-            as="a"
-            aria-label="issue feedback"
-            icon={<BiMessage color="primary.500" />}
-            onClick={onOpen}
-          />
-        </Tooltip>
-        <FormIssueFeedbackModal
-          isOpen={isOpen}
-          onClose={onClose}
-          isPreview={isPreview}
+    <Flex
+      position="fixed"
+      bottom="2.625rem"
+      right="2.75rem"
+      cursor="pointer"
+      sx={noPrintCss}
+    >
+      <Tooltip placement="left" label="Report an issue">
+        <IconButton
+          variant="outline"
+          as="a"
+          aria-label="issue feedback"
+          icon={<BiMessage color="primary.500" />}
+          onClick={onOpen}
         />
-      </Flex>
-    </Portal>
+      </Tooltip>
+      <FormIssueFeedbackModal
+        isOpen={isOpen}
+        onClose={onClose}
+        isPreview={isPreview}
+      />
+    </Flex>
   )
 }
