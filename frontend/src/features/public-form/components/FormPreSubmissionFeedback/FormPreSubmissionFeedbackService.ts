3ea71e3792392f9ef80d74abc6e5385731ecbb26
mayying <mayying@open.gov.sg>
2023-06-12 09:49:45 +0800

    Add todo

--- frontend/src/features/public-form/components/FormPreSubmissionFeedback/FormPreSubmissionFeedbackService.ts
+++ frontend/src/features/public-form/components/FormPreSubmissionFeedback/FormPreSubmissionFeedbackService.ts
@@ -1,5 +1,6 @@
 import { ErrorDto, SuccessMessageDto } from '~shared/types'
 
+// TODO(FRM-999): Connects to backend service
 export const submitPreSubmissionFeedback = async (): Promise<
   SuccessMessageDto | ErrorDto | null
 > => {
