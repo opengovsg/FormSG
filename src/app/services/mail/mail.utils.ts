e5eab19ffc6f4d5f15193cb21b256dc908e2f94e
mayying <mayying@open.gov.sg>
2023-06-22 11:42:56 +0800

    chore: update issue feedback mail html

--- src/app/services/mail/mail.utils.ts
+++ src/app/services/mail/mail.utils.ts
@@ -319,11 +319,11 @@ export const generateIssueReportedNotificationHtml = ({
   return dedent`
   <p>Dear Form admin,</p>
   <p>Respondents are facing issues on your form: ${formTitle}.<p>
-  <p>These issues are reported by respondents via the ‘Report an issue’ button 
+  <p>These issues are reported by respondents via the 'Report an issue' button 
   on public forms. We encourage you to view these reports as respondents might 
-  be facing urgent issues relating to form submission. </p>
-  <p><a href="${formResultUrl}">Login to Forms</a> and view issues reported in your 
-  ‘Results’ page.</p>
-  <p>Thank you,</br>${appName}</p>
+  be facing urgent issues relating to form submission.</p>
+  <p><a href="${formResultUrl}">Login to Forms</a> and view issues reported in 
+  your results page.</p>
+  <p>Thank you,<br/>${appName}</p>
   `
 }
