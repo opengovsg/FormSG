1db99dcf3ed95ea6fb28d96da4546668f191e7fc
Foo Chi Fa <59867455+foochifa@users.noreply.github.com>
2023-06-23 18:37:12 +0800

    feat: admin feedback modal (#6465)
    * feat: set up basic modal
    
    * feat: add states and content to feedback modal
    
    * feat: add last feedback seen time to local storage
    
    * feat: add eligibility check to sessionStorage and display logic
    
    * doc: explanation for memoising time
    
    * fix: display eligibility
    
    * feat: create logic for eligibility cases
    
    * fix: leftover lints
    
    * feat: add services and mutation for create and update feedback
    
    * feat: integrate modal interaction and api calls
    
    * feat: use env vars for display frequency and field threshold
    
    * fix: Comment box content size
    
    * fix: wrap modal header and close button
    
    * fix: don't send mutation if comment is empty
    
    * fix: mobile view for comment modal
    
    * doc: correct spelling errors
    
    * feat: link feedback history to specific user
    
    * feat: remove session eligibility on logout
    
    * fix: hard remove session storage
    
    * chore: fix spelling/case errors
    
    * fix: use enum for typing on feedback rating
    
    * feat: refactor admin feedback to use flex instead of modal
    
    * chore: change name of feedback from modal to box
    
    * chore: refactor names with modal to box
    
    * fix: temporarily remove placeholder
    
    * chore: update typing of mutation and service
    
    * feat: add thumbs down flow
    
    * chore: remove default true from testing
    
    * chore: set z-index of feedback box and rounded borders
    
    * fix: disable feedback from showing on mobile and added todo comment
    
    * fix: use useRef instead of memo for current time
    
    * refactor: abstracted large if case to make flow more readable

--- frontend/src/features/workspace/WorkspacePage.tsx
+++ frontend/src/features/workspace/WorkspacePage.tsx
@@ -1,14 +1,21 @@
-import { useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Flex, useDisclosure } from '@chakra-ui/react'
 
 import { AdminNavBar } from '~/app/AdminNavBar'
 
+import { ADMIN_FEEDBACK_HISTORY_PREFIX } from '~constants/localStorage'
+import { ADMIN_FEEDBACK_SESSION_KEY } from '~constants/sessionStorage'
+import { useIsMobile } from '~hooks/useIsMobile'
+import { useLocalStorage } from '~hooks/useLocalStorage'
+import { useSessionStorage } from '~hooks/useSessionStorage'
 import { fillHeightCss } from '~utils/fillHeightCss'
 import { getBannerProps } from '~utils/getBannerProps'
 import { Banner } from '~components/Banner'
 
 import { useEnv } from '~features/env/queries'
+import { useUser } from '~features/user/queries'
 
+import AdminFeedbackBox from './components/AdminFeedbackBox'
 // TODO #4279: Remove after React rollout is complete
 import CreateFormModal from './components/CreateFormModal'
 import { WorkspacePageContent } from './components/WorkspacePageContent'
@@ -17,7 +24,28 @@ import { WorkspaceProvider } from './WorkspaceProvider'
 export const CONTAINER_MAXW = '69.5rem'
 
 export const WorkspacePage = (): JSX.Element => {
-  const { data: { siteBannerContent, adminBannerContent } = {} } = useEnv()
+  const {
+    data: {
+      siteBannerContent,
+      adminBannerContent,
+      adminFeedbackDisplayFrequency,
+    } = {},
+  } = useEnv()
+  const { user, isLoading } = useUser()
+  const [isDisplayFeedback, setIsDisplayFeedback] = useState(false)
+  const isMobile = useIsMobile()
+
+  const adminFeedbackKey = useMemo(() => {
+    return ADMIN_FEEDBACK_HISTORY_PREFIX + user?._id
+  }, [user])
+
+  const [lastFeedbackTime, setLastFeedbackTime] =
+    useLocalStorage<number>(adminFeedbackKey)
+  const [isAdminFeedbackEligible, setIsAdminFeedbackEligible] =
+    useSessionStorage<boolean>(ADMIN_FEEDBACK_SESSION_KEY, false)
+
+  // capture current time on page load to prevent re-renders from update to current time
+  const currentTime = useRef(Date.now())
 
   const bannerContent = useMemo(
     // Use || instead of ?? so that we fall through even if previous banners are empty string.
@@ -31,6 +59,46 @@ export const WorkspacePage = (): JSX.Element => {
   )
 
   const createFormModalDisclosure = useDisclosure()
+
+  // check if admin is eligible in current session
+  // and has yet to seen feedback beyond our stipulated frequency
+  const showAdminFeedback =
+    isAdminFeedbackEligible &&
+    // if feedbackTime has not been seen
+    (!lastFeedbackTime ||
+      // or if last feedback time seen is more than frequency (frequency env var must be defined)
+      (!!adminFeedbackDisplayFrequency &&
+        currentTime.current - lastFeedbackTime > adminFeedbackDisplayFrequency))
+
+  // sets display of feedback box
+  useEffect(() => {
+    if (
+      // user details is loaded
+      !isLoading &&
+      // TODO: create mobile version of admin feedback
+      !isMobile &&
+      // whether to show admin the feedback box
+      showAdminFeedback
+    ) {
+      setIsDisplayFeedback(true)
+      // reset local storage and admin feedback eligibility when admin feedback is displayed
+      setLastFeedbackTime(currentTime.current)
+      setIsAdminFeedbackEligible(false)
+    }
+  }, [
+    isLoading,
+    currentTime,
+    showAdminFeedback,
+    setIsDisplayFeedback,
+    setLastFeedbackTime,
+    setIsAdminFeedbackEligible,
+    isMobile,
+  ])
+
+  const closeAdminFeedback = useCallback(
+    () => setIsDisplayFeedback(false),
+    [setIsDisplayFeedback],
+  )
 
   return (
     <>
@@ -51,6 +119,7 @@ export const WorkspacePage = (): JSX.Element => {
           />
         </WorkspaceProvider>
       </Flex>
+      {isDisplayFeedback && <AdminFeedbackBox onClose={closeAdminFeedback} />}
     </>
   )
 }
---
+++ frontend/src/features/workspace/components/AdminFeedbackBox/AdminFeedbackBox.tsx
@@ -0,0 +1,222 @@
+import { useCallback, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { GoThumbsdown, GoThumbsup } from 'react-icons/go'
+import { Flex, Link, Stack, Text } from '@chakra-ui/react'
+
+import { AdminFeedbackRating } from '~shared/types'
+
+import { BxX } from '~assets/icons'
+import { useIsMobile } from '~hooks/useIsMobile'
+import Button from '~components/Button'
+import IconButton from '~components/IconButton'
+import Textarea from '~components/Textarea'
+
+import { useAdminFeedbackMutation } from '~features/workspace/mutations'
+
+enum FeedbackBoxContentState {
+  Rating,
+  CallForComment,
+  CommentBox,
+}
+
+type AdminFeedbackCommentForm = {
+  comment: string
+}
+
+export const AdminFeedbackBox = ({ onClose }: { onClose: () => void }) => {
+  const [contentState, setContentState] = useState(
+    FeedbackBoxContentState.Rating,
+  )
+  const [feedbackId, setFeedbackId] = useState('')
+  const [ratingValue, setRatingValue] = useState(AdminFeedbackRating.up)
+  const { createAdminFeedbackMutation, updateAdminFeedbackMutation } =
+    useAdminFeedbackMutation()
+
+  const handleRatingClick = useCallback(
+    (rating: AdminFeedbackRating) => {
+      createAdminFeedbackMutation
+        .mutateAsync(rating)
+        .then((data) => setFeedbackId(data._id))
+      setContentState(FeedbackBoxContentState.CallForComment)
+      setRatingValue(rating)
+    },
+    [createAdminFeedbackMutation, setFeedbackId, setContentState],
+  )
+
+  const handleCommentClick = useCallback(
+    (data: AdminFeedbackCommentForm) => {
+      if (feedbackId && !!data.comment) {
+        updateAdminFeedbackMutation.mutateAsync({
+          feedbackId,
+          comment: data.comment,
+        })
+      }
+      onClose()
+    },
+    [feedbackId, updateAdminFeedbackMutation, onClose],
+  )
+
+  const handleCallForCommentClick = () =>
+    setContentState(FeedbackBoxContentState.CommentBox)
+
+  return (
+    <Flex
+      position="fixed"
+      bottom="1.5rem"
+      justifyContent="center"
+      margin="auto"
+      width="100%"
+      zIndex="2"
+    >
+      <Flex
+        px="1.5rem"
+        py="1rem"
+        bgColor="white"
+        boxShadow="md"
+        borderRadius="0.25rem"
+      >
+        <AdminFeedbackBoxContentBuilder
+          state={contentState}
+          onRatingClick={handleRatingClick}
+          onCallForCommentClick={handleCallForCommentClick}
+          onCommentClick={handleCommentClick}
+          onClose={onClose}
+          ratingValue={ratingValue}
+        />
+      </Flex>
+    </Flex>
+  )
+}
+
+const AdminFeedbackRatingContent = ({
+  onRatingClick,
+}: {
+  onRatingClick: (rating: AdminFeedbackRating) => void
+}) => {
+  return (
+    <Stack direction="row" alignItems="center" gap="2.2rem">
+      <Text textStyle="h6">How was your form building experience?</Text>
+      <IconButton
+        variant="clear"
+        icon={<GoThumbsup />}
+        colorScheme="theme-blue"
+        aria-label="Good"
+        onClick={() => onRatingClick(AdminFeedbackRating.up)}
+      />
+      <IconButton
+        variant="clear"
+        icon={<GoThumbsdown />}
+        colorScheme="theme-red"
+        aria-label="Bad"
+        onClick={() => onRatingClick(AdminFeedbackRating.down)}
+      />
+    </Stack>
+  )
+}
+
+const AdminFeedbackCallForCommentContent = ({
+  onLinkClick,
+  ratingValue,
+}: {
+  onLinkClick: () => void
+  ratingValue: AdminFeedbackRating
+}) => {
+  // Thumbs down
+  if (ratingValue === AdminFeedbackRating.down)
+    return (
+      <Text textStyle="h6">
+        Thank you for your feedback.{' '}
+        <Link onClick={onLinkClick}>Tell us more so we can improve!</Link>
+      </Text>
+    )
+
+  // Thumbs Up
+  return (
+    <Text textStyle="h6">
+      Thank you, you're the best!{' '}
+      <Link onClick={onLinkClick}>Want to tell us more?</Link>
+    </Text>
+  )
+}
+
+const AdminFeedbackCommentContent = ({
+  onCommentClick,
+  onClose,
+  ratingValue,
+}: {
+  onCommentClick: (data: AdminFeedbackCommentForm) => void
+  onClose: () => void
+  ratingValue: AdminFeedbackRating
+}) => {
+  const { handleSubmit, register } = useForm<AdminFeedbackCommentForm>()
+  const isMobile = useIsMobile()
+
+  const inputPlaceholder =
+    ratingValue === AdminFeedbackRating.up
+      ? 'Form is awesome'
+      : 'How can we improve your experience?'
+
+  return (
+    <Stack w={isMobile ? undefined : '28.5rem'}>
+      <Flex justifyContent="space-between" alignItems="center" mb="1rem">
+        <Text textStyle="h2">Great!</Text>
+        <IconButton
+          aria-label="close feedback box"
+          icon={<BxX />}
+          variant="clear"
+          color="black"
+          onClick={onClose}
+        />
+      </Flex>
+      <Text textStyle="body-2">
+        Tell us about your form building experience in more detail!
+      </Text>
+      <Textarea
+        mt="1rem"
+        {...register('comment')}
+        placeholder={inputPlaceholder}
+      />
+      <Flex alignItems="flex-end" flexDirection="column">
+        <Button mt="1rem" float="right" onClick={handleSubmit(onCommentClick)}>
+          Submit
+        </Button>
+      </Flex>
+    </Stack>
+  )
+}
+
+const AdminFeedbackBoxContentBuilder = ({
+  state,
+  onRatingClick,
+  onCallForCommentClick,
+  onCommentClick,
+  onClose,
+  ratingValue,
+}: {
+  state: FeedbackBoxContentState
+  onRatingClick: (rating: AdminFeedbackRating) => void
+  onCallForCommentClick: () => void
+  onCommentClick: (data: AdminFeedbackCommentForm) => void
+  onClose: () => void
+  ratingValue: AdminFeedbackRating
+}) => {
+  switch (state) {
+    case FeedbackBoxContentState.Rating:
+      return <AdminFeedbackRatingContent onRatingClick={onRatingClick} />
+    case FeedbackBoxContentState.CallForComment:
+      return (
+        <AdminFeedbackCallForCommentContent
+          onLinkClick={onCallForCommentClick}
+          ratingValue={ratingValue}
+        />
+      )
+    case FeedbackBoxContentState.CommentBox:
+      return (
+        <AdminFeedbackCommentContent
+          onCommentClick={onCommentClick}
+          onClose={onClose}
+          ratingValue={ratingValue}
+        />
+      )
+  }
+}
