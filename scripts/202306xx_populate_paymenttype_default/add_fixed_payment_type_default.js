209c478b2625c00106d6f0762262ceb4811f708f
Ken <ken@open.gov.sg>
2023-07-03 13:21:26 +0800

    refactor: db script to use batching

--- scripts/202306xx_populate_paymenttype_default/add_fixed_payment_type_default.js
+++ scripts/202306xx_populate_paymenttype_default/add_fixed_payment_type_default.js
@@ -4,31 +4,87 @@
 Add PaymentType.Fixed to all forms with existing payments_field object
 */
 
-// Count number of forms that contains payments_field property
-db.getCollection('forms')
-  .find({ payments_field: { $exists: true } })
-  .count()
+// Batch 1 2022
+const queriesModifiedIn2022 = {
+  payments_field: { $exists: true },
+  lastModified: { $gte: new ISODate('2022-01-01T00:00:00.000+0000') },
+  lastModified: { $lt: new ISODate('2023-01-01T00:00:00.000+0000') },
+}
 
-// Count number of forms that contains payments_field.payment_type property.
-// Expected to be 0
-db.getCollection('forms')
-  .find({ "payments_field.payment_type": { $exists: true } })
-  .count()
+const queriesModifiedIn2023Q1 = {
+  lastModified: { $gte: new ISODate('2023-01-01T00:00:00.000+0000') },
+  lastModified: { $lt: new ISODate('2023-04-01T00:00:00.000+0000') },
+}
 
-/*
-Create payments_field.payment_type from existing payments_fields
+const queriesModifiedIn2023AprMay = {
+  lastModified: { $gte: new ISODate('2023-04-01T00:00:00.000+0000') },
+  lastModified: { $lt: new ISODate('2023-06-01T00:00:00.000+0000') },
+}
+
+const queriesModifiedIn2023JunOnwards = {
+  lastModified: { $gte: new ISODate('2023-06-01T00:00:00.000+0000') },
+}
+
+function executePaymentTypeInsertionOnQuery(dateQuery, dryRun = true) {
+  console.log(dateQuery)
+  // Count number of forms that contains payments_field property
+  const formsWithPaymentsField = db
+    .getCollection('forms')
+    .find({
+      payments_field: { $exists: true },
+      ...dateQuery,
+    })
+    .count()
+
+  console.log('forms with payments_field', formsWithPaymentsField)
+
+  // Count number of forms that contains payments_field.payment_type property.
+  // Expected to be 0
+  let formsWithPaymentType = db
+    .getCollection('forms')
+    .find({
+      'payments_field.payment_type': { $exists: true },
+      ...dateQuery,
+    })
+    .count()
+
+  console.log('forms with payment_type', formsWithPaymentType)
+  if (dryRun) {
+    return
+  }
+  /*
+Create payments_field.payment_type from existing payments_field
 */
-db.getCollection('forms').updateMany({ payments_field: { $exists: true } }, [
-  {
-    $set: {
-      "payments_field.payment_type": "Fixed"
+  const updatedFields = db.getCollection('forms').updateMany(
+    {
+      payments_field: { $exists: true },
+      ...dateQuery,
     },
-  },
-])
+    [
+      {
+        $set: {
+          'payments_field.payment_type': 'Fixed',
+        },
+      },
+    ],
+  )
+  console.log('updated fields', updatedFields)
 
-// Count number of forms that contains payments_field.payment_type property.
-// Expected to be equal to number of payment_fields
-db.getCollection('forms')
-  .find({ "payments_field.payment_type": { $exists: true } })
-  .count()
-  
+  // Count number of forms that contains payments_field.payment_type property.
+  // Expected to be equal to number of payment_field
+  formsWithPaymentType = db
+    .getCollection('forms')
+    .find({
+      'payments_field.payment_type': { $exists: true },
+      ...dateQuery,
+    })
+    .count()
+  console.log('forms with payment_type', formsWithPaymentType)
+}
+
+// executePaymentTypeInsertionOnQuery(queriesModifiedIn2022, false)
+// executePaymentTypeInsertionOnQuery(queriesModifiedIn2023Q1, false)
+// executePaymentTypeInsertionOnQuery(queriesModifiedIn2023AprMay, false)
+
+// must be executed last
+// executePaymentTypeInsertionOnQuery(queriesModifiedIn2023JunOnwards, false)
