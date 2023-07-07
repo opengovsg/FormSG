d4a62c3c59a885e2d55bcefd008afd0be6bc56cc
Ken <ken@open.gov.sg>
2023-07-07 16:23:20 +0800

    test: add more cases for getnetamount

--- frontend/src/features/admin-form/responses/ResponsesPage/storage/UnlockedResponses/ResponsesTable/__tests__/utils.test.ts
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/UnlockedResponses/ResponsesTable/__tests__/utils.test.ts
@@ -1,3 +1,7 @@
+import { StorageModeSubmissionMetadata } from '~shared/types'
+
+import { centsToDollars } from '~utils/payments'
+
 import { getNetAmount } from '../utils'
 
 describe('getNetAmount', () => {
@@ -12,19 +16,61 @@ describe('getNetAmount', () => {
 
   it('should return empty string when no there is no transaction fees', () => {
     // Arrange
-    const emptyObjectInput = {}
+    const emptyObjectInput = {} as StorageModeSubmissionMetadata['payments']
     // Act
     const result = getNetAmount(emptyObjectInput)
     // Assert
     expect(result).toBe('')
   })
 
-  it('should return 0 when traansaction fee is 0', () => {
+  it('should return 0 when traansaction fee is < 0', () => {
     // Arrange
-    const zeroTransactionFee = { transactionFee: 0 }
+    const zeroTransactionFee = {
+      transactionFee: -1,
+    } as StorageModeSubmissionMetadata['payments']
     // Act
     const result = getNetAmount(zeroTransactionFee)
     // Assert
     expect(result).toBe('')
+  })
+
+  it('should display as estimates when payment is not final', () => {
+    // Arrange
+    const zeroTransactionFee = {
+      transactionFee: 0,
+      paymentAmt: 100,
+      payoutDate: null,
+    } as StorageModeSubmissionMetadata['payments']
+    // Act
+    const result = getNetAmount(zeroTransactionFee)
+    // Assert
+    expect(result).toContain('Est')
+  })
+
+  it('should not display as estimate when payout is provided', () => {
+    // Arrange
+    const zeroTransactionFee = {
+      transactionFee: 0,
+      paymentAmt: 100,
+      payoutDate: Date(),
+    } as StorageModeSubmissionMetadata['payments']
+    // Act
+    const result = getNetAmount(zeroTransactionFee)
+    // Assert
+    expect(result).not.toContain('Est')
+  })
+
+  it('should return gross amount if transaction fee is 0', () => {
+    // Arrange
+    const EXPECTED_PAYMENT_AMOUNT = 123
+    const zeroTransactionFee = {
+      transactionFee: 0,
+      paymentAmt: EXPECTED_PAYMENT_AMOUNT,
+      payoutDate: Date(),
+    } as StorageModeSubmissionMetadata['payments']
+    // Act
+    const result = getNetAmount(zeroTransactionFee)
+    // Assert
+    expect(result).toContain(centsToDollars(EXPECTED_PAYMENT_AMOUNT))
   })
 })
