d6e344c798b4fa3ae5f45f09d7fbba4f251662b3
mayying <mayying@open.gov.sg>
2023-07-10 15:35:53 +0800

    fix: mishandling stripe callback when user clicks return to form

--- src/app/modules/payments/__tests__/stripe.controller.spec.ts
+++ src/app/modules/payments/__tests__/stripe.controller.spec.ts
@@ -2,9 +2,12 @@ import dbHandler from '__tests__/unit/backend/helpers/jest-db'
 import expressHandler from '__tests__/unit/backend/helpers/jest-express'
 import axios from 'axios'
 import { ObjectId } from 'bson'
+import { StatusCodes } from 'http-status-codes'
 import mongoose from 'mongoose'
-import { ok, okAsync } from 'neverthrow'
+import { errAsync, ok, okAsync } from 'neverthrow'
 import { PaymentStatus, SubmissionType } from 'shared/types'
+import Stripe from 'stripe'
+import { MarkRequired } from 'ts-essentials'
 
 import getPaymentModel from 'src/app/models/payment.server.model'
 import { getEncryptPendingSubmissionModel } from 'src/app/models/pending_submission.server.model'
@@ -15,9 +18,12 @@ import {
   IPopulatedForm,
 } from 'src/types'
 
+import config from '../../../config/config'
+import { FormNotFoundError } from '../../form/form.errors'
 import * as FormService from '../../form/form.service'
 import * as EncryptSubmissionService from '../../submission/encrypt-submission/encrypt-submission.service'
 import * as StripeController from '../stripe.controller'
+import * as StripeService from '../stripe.service'
 import * as StripeUtils from '../stripe.utils'
 
 const Payment = getPaymentModel(mongoose)
@@ -36,6 +42,9 @@ const MockEncryptSubmissionService = jest.mocked(EncryptSubmissionService)
 
 jest.mock('../../form/form.service')
 const MockFormService = jest.mocked(FormService)
+
+jest.mock('src/app/modules/payments/stripe.service')
+const MockStripeService = jest.mocked(StripeService)
 
 describe('stripe.controller', () => {
   beforeAll(async () => await dbHandler.connect())
@@ -207,6 +216,143 @@ describe('stripe.controller', () => {
       expect(mockRes.status).toHaveBeenCalledWith(200)
       expect(mockRes.send).toHaveBeenCalledOnce()
       expect(axiosSpy).toHaveBeenCalledOnce()
+    })
+  })
+
+  describe('_handleConnectOauthCallback', () => {
+    beforeEach(async () => {
+      await dbHandler.clearCollection(Payment.collection.name)
+      await dbHandler.clearCollection(EncryptPendingSubmission.collection.name)
+    })
+    const mockForm = {
+      _id: MOCK_FORM_ID,
+    } as IPopulatedForm
+    it('should return UNPROCESSABLE_ENTITY when state mismatch', async () => {
+      // Arrange
+      const mockReq = expressHandler.mockRequest({
+        query: { state: 'otherState', code: 'someCode' },
+        others: { signedCookies: { stripeState: 'anotherState' } },
+      })
+      const mockRes = expressHandler.mockResponse()
+      // Act
+      await StripeController._handleConnectOauthCallbackForTest(
+        mockReq,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      expect(mockRes.status).toHaveBeenCalledWith(
+        StatusCodes.UNPROCESSABLE_ENTITY,
+      )
+      expect(mockRes.json).toHaveBeenCalledWith({
+        message: 'Invalid state parameter',
+      })
+    })
+
+    it('should redirect back to settings/payment page when code is undefined', async () => {
+      // Arrange
+      const mockReq = expressHandler.mockRequest({
+        query: { state: 'otherState' },
+        others: { signedCookies: { stripeState: 'otherState' } },
+      })
+      const mockRes = expressHandler.mockResponse()
+      // Act
+      await StripeController._handleConnectOauthCallbackForTest(
+        mockReq,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      expect(mockRes.redirect).toHaveBeenCalledWith(
+        `${config.app.appUrl}/admin/form/otherState/settings/payments`,
+      )
+    })
+
+    it('should redirect back to settings/payment page when stripe account linking is successful', async () => {
+      // Arrange
+      const mockReq = expressHandler.mockRequest({
+        query: { state: MOCK_FORM_ID + '.otherState', code: 'someCode' },
+        others: {
+          signedCookies: { stripeState: MOCK_FORM_ID + '.otherState' },
+        },
+      })
+      MockFormService.retrieveFullFormById.mockReturnValue(okAsync(mockForm))
+      const mockStripeToken = {
+        stripe_user_id: 'user_id',
+        stripe_publishable_key: 'publishable_key',
+      } as MarkRequired<
+        Stripe.OAuthToken,
+        'stripe_user_id' | 'stripe_publishable_key'
+      >
+      MockEncryptSubmissionService.checkFormIsEncryptMode.mockReturnValue(
+        ok(mockForm as IPopulatedEncryptedForm),
+      )
+      MockStripeService.exchangeCodeForAccessToken.mockReturnValue(
+        okAsync(mockStripeToken),
+      )
+      MockStripeService.linkStripeAccountToForm.mockReturnValue(
+        okAsync('someId'),
+      )
+      const mockRes = expressHandler.mockResponse()
+      // Act
+      await StripeController._handleConnectOauthCallbackForTest(
+        mockReq,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      expect(mockRes.redirect).toHaveBeenCalledWith(
+        `${config.app.appUrl}/admin/form/${MOCK_FORM_ID}/settings/payments`,
+      )
+      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledOnceWith(
+        MOCK_FORM_ID,
+      )
+      expect(
+        MockStripeService.exchangeCodeForAccessToken,
+      ).toHaveBeenCalledOnceWith('someCode')
+      expect(
+        MockStripeService.linkStripeAccountToForm,
+      ).toHaveBeenCalledOnceWith(mockForm, {
+        accountId: mockStripeToken.stripe_user_id,
+        publishableKey: mockStripeToken.stripe_publishable_key,
+      })
+    })
+
+    it('should redirect back to settings/payment page when DatabaseError occurs', async () => {
+      // Arrange
+      const mockReq = expressHandler.mockRequest({
+        query: { state: 'formId.otherState', code: 'someCode' },
+        others: {
+          signedCookies: { stripeState: 'formId.otherState' },
+        },
+      })
+      MockFormService.retrieveFullFormById.mockReturnValueOnce(
+        errAsync(new FormNotFoundError()),
+      )
+      const mockRes = expressHandler.mockResponse()
+      // Act
+      await StripeController._handleConnectOauthCallbackForTest(
+        mockReq,
+        mockRes,
+        jest.fn(),
+      )
+
+      // Assert
+      expect(mockRes.redirect).toHaveBeenCalledWith(
+        `${config.app.appUrl}/admin/form/formId/settings/payments`,
+      )
+      expect(MockFormService.retrieveFullFormById).toHaveBeenCalledOnceWith(
+        'formId',
+      )
+      expect(
+        MockStripeService.exchangeCodeForAccessToken,
+      ).not.toHaveBeenCalledWith()
+      expect(
+        MockStripeService.linkStripeAccountToForm,
+      ).not.toHaveBeenCalledWith()
     })
   })
 })
