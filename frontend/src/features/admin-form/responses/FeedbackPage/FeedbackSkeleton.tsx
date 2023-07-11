3630378838c7e4b20c169410a12c66f7ea49332f
mayying <mayying@open.gov.sg>
2023-07-11 10:45:20 +0800

    feat: issue feedback admin page (#6517)
    * feat/issue-feedback-admin-page
    
    * add storybook
    
    * change issue to issues
    
    * pluralise issue
    
    * use shorthand and remove div
    
    * update UI according to stacey feedback
    
    * update as according to design requirement
    
    * change button to icon button in mobile view for export

--- frontend/src/features/admin-form/responses/FeedbackPage/FeedbackSkeleton.tsx
+++ frontend/src/features/admin-form/responses/FeedbackPage/FeedbackSkeleton.tsx
@@ -3,20 +3,22 @@ import { Box, Container, Skeleton } from '@chakra-ui/react'
 export const FeedbackPageSkeleton = (): JSX.Element => {
   return (
     <Container maxW="69.5rem" mt="1.5rem">
-      <Skeleton w="4rem" h="0.75rem"></Skeleton>
       <Box
         display="flex"
         flexDir="row"
         justifyContent="space-between"
         mb="1rem"
+        h="4rem"
       >
-        <Box display="flex" justifyContent="flex-start" alignItems="flex-end">
-          <Box>
-            <Skeleton mr="0.75rem" w="4.5rem" h="2rem" mt="0.5rem" />
-          </Box>
-          <Skeleton ml="2rem" w="14rem" h="2rem" />
+        <Box display="flex" justifyContent="flex-start" alignItems="center">
+          <Skeleton w="18rem" h="2rem" />
         </Box>
-        <Skeleton w="6rem" h="2rem" />
+        <Box display="flex" justifyContent="flex-end" alignItems="center">
+          <Box>
+            <Skeleton mr="0.75rem" w="10rem" h="2rem" />
+          </Box>
+          <Skeleton w="6rem" h="2rem" />
+        </Box>
       </Box>
       <Skeleton w="100%" h="10rem" />
     </Container>
@@ -26,22 +28,17 @@ export const FeedbackPageSkeleton = (): JSX.Element => {
 export const FeedbackPageSkeletonMobile = (): JSX.Element => {
   return (
     <Container maxW="69.5rem" mt="1.5rem">
-      <Skeleton w="14rem" h="2rem" mb="1rem" />
-      <Skeleton w="4rem" h="0.75rem"></Skeleton>
+      <Skeleton w="14rem" h="2rem" />
       <Box
-        display="flex"
         flexDir="row"
+        display="flex"
         justifyContent="space-between"
-        mb="1rem"
+        alignItems="flex-end"
+        mt="0.5rem"
       >
-        <Box display="flex" justifyContent="flex-start" alignItems="flex-end">
-          <Box>
-            <Skeleton mr="0.75rem" w="4.5rem" h="2rem" mt="0.5rem" />
-          </Box>
-        </Box>
-        <Skeleton w="6rem" h="2rem" />
+        <Skeleton w="8rem" h="2rem" />
+        <Skeleton w="4.5rem" h="2rem" ml="0.5rem" />
       </Box>
-      <Skeleton w="100%" h="10rem" />
     </Container>
   )
 }
