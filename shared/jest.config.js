cd89e734ee4200d57253aedde02306d0980e6fed
huuji <limhj90@gmail.com>
2023-07-12 15:05:13 +0100

    feat: updated uen validator based on stdnum, along with corresponding tests

---
+++ shared/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ["file-validation.spec.ts"] // excludede from standalone test
+};
