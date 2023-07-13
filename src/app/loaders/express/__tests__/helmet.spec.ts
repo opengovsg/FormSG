663f095bbf72dfaa0223d47faaa25147c809a7da
sebastianwzq <sebastian@open.gov.sg>
2023-07-13 23:35:11 +0800

    removed gtm and gstatic from csp headers

--- src/app/loaders/express/__tests__/helmet.spec.ts
+++ src/app/loaders/express/__tests__/helmet.spec.ts
@@ -31,17 +31,13 @@ describe('helmetMiddlewares', () => {
     fontSrc: ["'self'", 'data:', 'https://fonts.gstatic.com/'],
     scriptSrc: [
       "'self'",
-      'https://www.googletagmanager.com/',
       'https://ssl.google-analytics.com/',
       'https://www.google-analytics.com/',
       'https://www.tagmanager.google.com/',
       'https://www.google.com/recaptcha/',
       'https://www.recaptcha.net/recaptcha/',
-      'https://www.gstatic.com/recaptcha/',
       'https://challenges.cloudflare.com',
       'https://js.stripe.com/v3',
-      'https://www.gstatic.cn/',
-      'https://*.googletagmanager.com',
     ],
     connectSrc: [
       "'self'",
