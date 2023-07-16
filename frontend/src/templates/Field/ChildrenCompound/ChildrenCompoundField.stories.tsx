5097c0c9a0b1701eb6f6a0a27afa5558624ac484
Ken Jin <119096102+kenjin-work@users.noreply.github.com>
2023-07-16 19:13:11 +0800

    feat: add storybook for ChildrenCompoundField

---
+++ frontend/src/templates/Field/ChildrenCompound/ChildrenCompoundField.stories.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { Text } from '@chakra-ui/react'
+import { Meta, Story } from '@storybook/react'
+import { merge } from 'lodash'
+
+import { BasicField, MyInfoChildData } from '~shared/types/field'
+
+import Button from '~components/Button'
+
+import { ChildrenCompoundFieldSchema } from '../types'
+
+import {
+  ChildrenCompoundField as ChildrenCompoundComponent,
+  ChildrenCompoundFieldProps,
+} from './ChildrenCompoundField'
+
+export default {
+  title: 'Templates/Field/ChildrenCompoundField',
+  component: ChildrenCompoundComponent,
+  decorators: [],
+  parameters: {
+    docs: {
+      // Required in this story due to react-hook-form conflicting with
+      // Storybook somehow.
+      // See https://github.com/storybookjs/storybook/issues/12747.
+      source: {
+        type: 'code',
+      },
+    },
+  },
+} as Meta
+
+const baseSchema: ChildrenCompoundFieldSchema = {
+  childrenSubFields: [],
+  allowMultiple: false,
+  title: '',
+  description: '',
+  required: true,
+  disabled: false,
+  fieldType: BasicField.Children,
+  _id: '611b94dfbb9e300012f702a7',
+}
+
+interface StoryCheckboxFieldProps extends ChildrenCompoundFieldProps {
+  childrenBirthRecords: MyInfoChildData
+}
+
+const Template: Story<StoryCheckboxFieldProps> = ({
+  childrenBirthRecords,
+  ...args
+}) => {
+  const formMethods = useForm()
+
+  const [submitValues, setSubmitValues] = useState<string>()
+
+  const onSubmit = (values: Record<string, string | undefined>) => {
+    setSubmitValues(
+      JSON.stringify(values[`${args.schema._id}.child`]) ||
+        'Nothing was selected',
+    )
+  }
+
+  return (
+    <FormProvider {...formMethods}>
+      <form onSubmit={formMethods.handleSubmit(onSubmit)} noValidate>
+        <ChildrenCompoundComponent
+          myInfoChildrenBirthRecords={childrenBirthRecords}
+          {...args}
+        />
+        <Button
+          mt="1rem"
+          type="submit"
+          isLoading={formMethods.formState.isSubmitting}
+          loadingText="Submitting"
+        >
+          Submit
+        </Button>
+        {submitValues && <Text>You have submitted: {submitValues}</Text>}
+      </form>
+    </FormProvider>
+  )
+}
+
+export const AllowMultipleChildren = Template.bind({})
+AllowMultipleChildren.args = {
+  schema: merge({}, baseSchema, { allowMultiple: true }),
+}
