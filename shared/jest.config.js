d03216eae02f16c141112bfd1115be4020258b1b
huuji <limhj90@gmail.com>
2023-07-17 21:06:18 +0100

    deleted jest.config.js and.patch file

--- shared/jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ["file-validation.spec.ts"] // excludede from standalone test
-};
