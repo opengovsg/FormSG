9551881276c134bb8b5fb807d41df1877a03ba18
Jiayee Lim <me@jiayee.net>
2023-08-02 09:15:16 +0800

    fix: revert wrongly edited test

--- src/app/routes/api/v3/admin/forms/__tests__/admin-forms.form.routes.spec.ts
+++ src/app/routes/api/v3/admin/forms/__tests__/admin-forms.form.routes.spec.ts
@@ -110,8 +110,9 @@ describe('admin-form.form.routes', () => {
         admin: collabUser._id,
         permissionList: [{ email: defaultUser.email }],
       })
-      // Create already archived form, should still be fetched
-      const ownFormArchived = await EmailFormModel.create({
+      // Create already archived form, should not be fetched even though
+      // owner is defaultUser
+      await EmailFormModel.create({
         title: 'Archived form',
         emails: defaultUser.email,
         admin: defaultUser._id,
@@ -133,7 +134,7 @@ describe('admin-form.form.routes', () => {
       // Should only receive ownForm and collabForm
       const expected = await FormModel.find({
         _id: {
-          $in: [ownForm._id, collabForm._id, ownFormArchived._id],
+          $in: [ownForm._id, collabForm._id],
         },
       })
         .select('_id title admin lastModified status responseMode')
