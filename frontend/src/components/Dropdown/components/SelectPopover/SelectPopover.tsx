15c53ecdc5f0c725b5cccc72338d859435fc92b5
Leonard Yam <yamthesmall@gmail.com>
2023-08-04 00:23:25 +0800

    fix: use floating-ui middleware to hide dropdown menu

--- frontend/src/components/Dropdown/components/SelectPopover/SelectPopover.tsx
+++ frontend/src/components/Dropdown/components/SelectPopover/SelectPopover.tsx
@@ -4,6 +4,7 @@ import {
   autoUpdate,
   flip,
   hide,
+  Middleware,
   offset,
   size,
   useFloating,
@@ -18,35 +19,61 @@ export const SelectPopoverProvider: FC = ({ children }): JSX.Element => {
 
   const wrapperRef = useRef<HTMLDivElement | null>(null)
 
-  const { x, y, refs, reference, floating, strategy, update } = useFloating({
-    placement: 'bottom-start',
-    strategy: 'absolute',
-    open: isOpen,
-    middleware: [
-      // offset middleware should be the first middleware
-      offset(1),
-      flip(),
-      hide(),
-      // Set width to be the same as the reference element.
-      size({
-        apply({ rects, elements }) {
-          Object.assign(elements.floating.style, {
-            width: `${rects.reference.width}px`,
-          })
+  // Custom hide middleware that ensures dropdown options are hidden correctly
+  // by the sliding MiniHeader in public form pages.
+  const hideWhenCoveredByHeader: Middleware = {
+    name: 'hideWhenCoveredByHeader',
+    fn(state) {
+      const { reference } = state.elements
+      const { ownerDocument } = reference as Element
+      const { x, y, height } = reference.getBoundingClientRect()
+      // Get className of all elements that cover the bottom-left corner of the combobox
+      const elementsClassName = ownerDocument
+        .elementsFromPoint(x, y + height)
+        .map((e: Element) => e.className)
+      return {
+        data: {
+          isCovered: elementsClassName.includes('chakra-slide'),
         },
-      }),
-    ],
-  })
+      }
+    },
+  }
+
+  const { x, y, refs, reference, floating, strategy, update, middlewareData } =
+    useFloating({
+      placement: 'bottom-start',
+      strategy: 'absolute',
+      open: isOpen,
+      middleware: [
+        // offset middleware should be the first middleware
+        offset(1),
+        flip(),
+        hide(),
+        hideWhenCoveredByHeader,
+        // Set width to be the same as the reference element.
+        size({
+          apply({ rects, elements }) {
+            Object.assign(elements.floating.style, {
+              width: `${rects.reference.width}px`,
+            })
+          },
+        }),
+      ],
+    })
 
   const mergedReferenceRefs = useMergeRefs(reference, wrapperRef)
 
+  const { referenceHidden } = middlewareData.hide || {}
+  const { isCovered } = middlewareData.hideWhenCoveredByHeader || {}
+
   const floatingStyles = useMemo(
     () => ({
+      visibility: referenceHidden || isCovered ? 'hidden' : 'visible',
       position: strategy,
       top: y ?? 0,
       left: x ?? 0,
     }),
-    [strategy, x, y],
+    [referenceHidden, isCovered, strategy, x, y],
   )
 
   // Allows
