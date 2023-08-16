7ca931ad8b650f6c4d19a36a304d38219de3148b
Justyn Oh <justynoh@gmail.com>
2023-08-17 00:33:47 +0800

    chore: revert out-of-scope changes

--- __tests__/unit/backend/helpers/jest-db.ts
+++ __tests__/unit/backend/helpers/jest-db.ts
@@ -1,5 +1,5 @@
 import MemoryDatabaseServer from '__tests__/setup/database'
-import { ObjectID } from 'bson-ext'
+import { ObjectID } from 'bson'
 import mongoose from 'mongoose'
 import { FormResponseMode } from 'shared/types'
 
