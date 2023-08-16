a39863f69bce827be0fef05d7037c84e06556c21
LinHuiqing <huiqing@open.gov.sg>
2023-08-16 22:35:35 +0800

    fix: restore retrieveForm step

--- src/app/modules/submission/encrypt-submission/encrypt-submission.controller.ts
+++ src/app/modules/submission/encrypt-submission/encrypt-submission.controller.ts
@@ -587,7 +587,7 @@ export const handleStorageSubmission = [
   ReceiverMiddleware.receiveStorageSubmission,
   EncryptSubmissionMiddleware.validateStorageSubmissionParams,
   EncryptSubmissionMiddleware.createFormsgReqBody,
-  // EncryptSubmissionMiddleware.retrieveForm,
+  EncryptSubmissionMiddleware.retrieveForm,
   EncryptSubmissionMiddleware.checkNewBoundaryEnabled,
   EncryptSubmissionMiddleware.checkPublicKey,
   EncryptSubmissionMiddleware.validateSubmission,
