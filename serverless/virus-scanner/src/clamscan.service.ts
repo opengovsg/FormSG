67ca9f2b832a69a0e6dee6911ee78ebac502af46
tshuli <shuli@open.gov.sg>
2023-08-17 13:42:39 +0800

    chore: scanFileStream returns clean file buffer

--- serverless/virus-scanner/src/clamscan.service.ts
+++ serverless/virus-scanner/src/clamscan.service.ts
@@ -1,7 +1,16 @@
 import NodeClam from 'clamscan'
-import internal from 'stream'
+import internal, { Stream } from 'stream'
 
-export async function scanFileStream(s3Stream: internal.Readable) {
+type ScanFileStreamResult = {
+  isMalicious: boolean
+  virusMetadata: string[]
+  // cleanFile only if isMalicious is false
+  cleanFile?: Buffer
+}
+
+export async function scanFileStream(
+  s3Stream: internal.Readable,
+): Promise<ScanFileStreamResult> {
   const scanner = await new NodeClam().init({
     clamdscan: {
       socket: '/tmp/clamd.ctl',
@@ -11,5 +20,29 @@ export async function scanFileStream(s3Stream: internal.Readable) {
   const { isInfected: isMalicious, viruses: virusMetadata } =
     await scanner.scanStream(s3Stream)
 
-  return { isMalicious, virusMetadata }
+  // create a writable stream to memory
+  const outputStream = new Stream.Writable()
+  const outputBufferArr: Uint8Array[] = []
+  outputStream._write = function (chunk) {
+    outputBufferArr.push(chunk)
+  }
+
+  s3Stream.pipe(outputStream)
+
+  return new Promise((resolve, reject) => {
+    outputStream
+      .on('finish', () => {
+        resolve({
+          isMalicious,
+          virusMetadata,
+          // Include cleanFile only if isMalicious is false
+          ...(!isMalicious
+            ? {}
+            : { cleanFile: Buffer.concat(outputBufferArr) }),
+        })
+      })
+      .on('error', (err) => {
+        reject(err)
+      })
+  })
 }
