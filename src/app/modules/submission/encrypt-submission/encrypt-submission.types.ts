a135fa335688064c454f8c29254c1875762aa575
LinHuiqing <huiqing@open.gov.sg>
2023-08-18 20:03:59 +0800

    feat: add formId for deprecatedCheck logs
    - fix: rm storage isPreview deprecatedCheck
    - fix: rm unused EncryptSubmissionBodyAfterProcess

--- src/app/modules/submission/encrypt-submission/encrypt-submission.types.ts
+++ src/app/modules/submission/encrypt-submission/encrypt-submission.types.ts
@@ -1,14 +1,4 @@
-import { StorageModeAttachmentsMap } from '../../../../../shared/types'
 import { IPopulatedEncryptedForm } from '../../../../types'
-import { ProcessedFieldResponse } from '../submission.types'
-
-export type EncryptSubmissionBodyAfterProcess = {
-  encryptedContent: string
-  attachments?: StorageModeAttachmentsMap
-  isPreview: boolean
-  version: number
-  parsedResponses: ProcessedFieldResponse[]
-}
 
 export type AttachmentMetadata = Map<string, string>
 
