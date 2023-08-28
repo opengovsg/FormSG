d26db9e21d3d66a98975017e127362c5c18dcca1
tshuli <63710093+tshuli@users.noreply.github.com>
2023-08-29 00:31:40 +0800

    feat: virus scanner for storage mode submissions (#6642)
    * feat: add base virus scanner
    
    * chore: add buckets for virus scanner
    
    * chore: update local env vars
    
    * feat: add putS3FileStream to s3.service
    
    * chore: add virus scanning bucket env vars to convict
    
    * fix: correct eslint ref
    
    * chore: add build step to dev for update
    
    * chore: add dev env var
    
    * chore: scanFileStream returns clean file buffer
    
    * refactor: put s3 file buffer only after scan
    
    * chore: add logs to clamscan
    
    * chore: remove backend service
    
    * feat: virus scanner feature
    
    * chore: shift typings to types
    
    * chore: update serverless.yml
    
    * chore: use http-status-codes
    
    * refactor: use promise notation for clarity
    
    * chore: add case where key is invalid
    
    * chore: add virus scanner to npm run dev
    
    * feat: add scanner deploy for stg
    
    * chore: swap prefix order for virus scanner ssm
    
    * chore: use hyphen for path
    
    * chore: log test params from ssm
    
    * chore: update param parser format
    
    * chore: use /virus-scanner prefix to scope iam perms
    
    * feat: provisioned concurrency to keep lambda hot
    
    * chore: use serverlessifelse
    
    * chore: validate that key is uuid
    
    * chore(deps): install uuid types
    
    * chore: improve docs, fix validate
    
    chore: docs
    
    * chore: return virus metadata on malicious file
    
    * chore: tighten perms
    
    * fix: syntax
    
    * feat: delete malicious file from quarantine bucket after logging
    
    * chore: delete clean file from quarantine bucket
    
    * feat: move clean file on successful scan
    
    * chore: use cp for now
    
    * chore: versioning enabled for local s3 bucket
    
    * chore: add versionId to copy command
    
    * fix: do not specify versionId in delete command
    
    * chore: return malicious file metadata
    
    * fix: include objectTagging perms for copy object
    
    * fix: include s3:GetObjectVersion
    
    * chore: check for empty body
    
    * chore: remove unused utils
    
    * chore: add try catch block
    
    * chore: specify versionId when deleting
    
    * chore: add name to workflow
    
    * chore: fix concurrency config
    
    * chore: add provisionedConcurrency to npm run package
    
    * fix: extra hyphen
    
    * chore: use param for provisionedConcurrency
    
    * fix: use param directly
    
    * chore: simplify logger and update docs
    
    * chore: add tests for clamscan service
    
    * chore: add versionId when deleting object after copy
    
    * chore: add tests for s3.service
    
    * chore: wrap file scanning in try catch
    
    * chore: add tests for index handler
    
    * chore: add log tests for s3.service
    
    * chore: update test description
    
    * chore: add scripts for prod and UAT
    
    * chore: update module path for tests
    
    * chore: add iam perms to delete object version
    
    * chore: add jest dependencies
    
    * chore: remove todo
    
    * chore: check for all dev envs
    
    * chore: error out if env name is invalid
    
    * chore: combine tests
    
    * chore: concise
    
    Co-authored-by: Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>

---
+++ serverless/virus-scanner/src/index.ts
@@ -0,0 +1,187 @@
+import { APIGatewayProxyResult } from 'aws-lambda'
+import crypto from 'crypto'
+import { StatusCodes } from 'http-status-codes'
+import { validate } from 'uuid'
+
+import { scanFileStream } from './clamscan.service'
+import { config } from './config'
+import { getLambdaLogger } from './logger'
+import { S3Service } from './s3.service'
+import { isBodyWithKey } from './types'
+
+/**
+ * Handler for virus scanner lambda
+ * To invoke handle locally, send POST request to http://localhost:9999/2015-03-31/functions/function/invocations with the following request body:
+ * { "key": <object key>}
+ * @param event
+ * @returns 200 with clean file key if clean
+ * @returns 400 if malicious
+ * @returns 400 if body is missing key
+ * @returns 404 if file not found
+ */
+export const handler = async (
+  event: unknown,
+): Promise<APIGatewayProxyResult> => {
+  const logger = getLambdaLogger('virus-scanner')
+
+  logger.info('handler triggered')
+
+  if (!isBodyWithKey(event)) {
+    logger.warn({
+      message: 'Missing key in body',
+      event,
+    })
+    return {
+      statusCode: StatusCodes.BAD_REQUEST,
+      body: JSON.stringify({
+        message: 'Missing key in body',
+      }),
+    }
+  }
+
+  if (!validate(event.key)) {
+    logger.warn({
+      message: 'Invalid key',
+      event,
+    })
+    return {
+      statusCode: StatusCodes.BAD_REQUEST,
+      body: JSON.stringify({
+        message: 'Invalid key',
+      }),
+    }
+  }
+
+  const quarantineFileKey = event.key
+
+  const cleanBucket = config.virusScannerCleanS3Bucket
+  const quarantineBucket = config.virusScannerQuarantineS3Bucket
+
+  // Retrieve from S3
+  const s3Client = new S3Service(config.isTestOrDev, logger)
+
+  let s3ReadableStream
+  try {
+    s3ReadableStream = await s3Client.getS3FileStreamWithVersionId({
+      bucketName: quarantineBucket,
+      objectKey: quarantineFileKey,
+    })
+  } catch (error) {
+    logger.warn({
+      message: 'File not found',
+      error,
+      quarantineFileKey,
+    })
+    return {
+      statusCode: StatusCodes.NOT_FOUND,
+      body: JSON.stringify({
+        message: 'File not found',
+        fileKey: quarantineFileKey,
+      }),
+    }
+  }
+
+  // Scan file
+
+  const { body, versionId } = s3ReadableStream
+
+  let scanResult
+  try {
+    scanResult = await scanFileStream(body)
+  } catch (error) {
+    logger.error({
+      message: 'Failed to scan file',
+      error,
+      quarantineFileKey,
+    })
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: JSON.stringify({
+        message: 'Failed to scan file',
+        key: quarantineFileKey,
+      }),
+    }
+  }
+
+  const { isMalicious } = scanResult
+
+  // If malicious, log and delete
+  if (isMalicious) {
+    const { virusMetadata } = scanResult
+
+    logger.error({
+      message: 'Malicious file detected',
+      virusMetadata,
+      key: quarantineFileKey,
+    })
+
+    // Delete from quarantine bucket
+    try {
+      await s3Client.deleteS3File({
+        bucketName: quarantineBucket,
+        objectKey: quarantineFileKey,
+        versionId,
+      })
+    } catch (error) {
+      // Log but do not halt execution as we still want to return 400 for malicious file
+      logger.error({
+        message: 'Failed to delete file from quarantine bucket',
+        error,
+        key: quarantineFileKey,
+      })
+    }
+
+    return {
+      statusCode: StatusCodes.BAD_REQUEST,
+      body: JSON.stringify({
+        message: 'Malicious file detected',
+        key: quarantineFileKey,
+        virusMetadata,
+      }),
+    }
+    // If clean, move to clean bucket with randomised key and return
+  } else {
+    const cleanFileKey = crypto.randomUUID()
+    logger.info({
+      message: 'clean file detected',
+      key: quarantineFileKey,
+      versionId,
+    })
+    try {
+      await s3Client.moveS3File({
+        sourceBucketName: quarantineBucket,
+        sourceObjectKey: quarantineFileKey,
+        sourceObjectVersionId: versionId,
+        destinationBucketName: cleanBucket,
+        destinationObjectKey: cleanFileKey,
+      })
+    } catch (error) {
+      logger.error({
+        message: 'Failed to move file to clean bucket',
+        error,
+        bucket: quarantineBucket,
+        key: quarantineFileKey,
+      })
+      return {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        body: JSON.stringify({
+          message: 'Failed to move file to clean bucket',
+          key: quarantineFileKey,
+        }),
+      }
+    }
+
+    logger.info({
+      message: 'clean file moved to clean bucket',
+      cleanFileKey,
+    })
+
+    return {
+      statusCode: StatusCodes.OK,
+      body: JSON.stringify({
+        message: 'File scan completed',
+        cleanFileKey,
+      }),
+    }
+  }
+}
