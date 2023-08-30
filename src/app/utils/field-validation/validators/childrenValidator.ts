2b028f01f1a7232ac239a507c599c96f382183fa
wanlingt <56983748+wanlingt@users.noreply.github.com>
2023-08-30 15:50:16 +0800

    fix: allow submission when Myinfo child field is hidden by logic (#6673)
    * fix: output empty array if no child is selected
    
    * fix: remove validator for empty arrays
    
    * fix: check for empty string before validation in validChildAnswersNonEmpty
    
    * fix: failure to deploy due to outdated mirror
    
    * ref: use terser condition check
    
    * fix: add comments for case where no child is selected in validChildAnswersNonEmpty

--- src/app/utils/field-validation/validators/childrenValidator.ts
+++ src/app/utils/field-validation/validators/childrenValidator.ts
@@ -24,7 +24,9 @@ const childrenAnswerValidator: ChildrenValidator = (response) => {
   const { answerArray } = response
 
   return answerArray.length === 0
-    ? left(`ChildrenValidator:\t Answer is empty array`)
+    ? left(
+        `ChildrenValidator (childrenAnswerValidator):\t Answer is empty array`,
+      )
     : right(response)
 }
 
@@ -37,7 +39,9 @@ const validChildAnswerFirstArray: ChildrenValidator = (response) => {
   const first = answerArray[0]
   return Array.isArray(first) && first.length > 0
     ? right(response)
-    : left(`ChildrenValidator:\t first subarray length is invalid`)
+    : left(
+        `ChildrenValidator (validChildAnswerFirstArray):\t first subarray length is invalid`,
+      )
 }
 
 /**
@@ -47,22 +51,40 @@ const validChildAnswerFirstArray: ChildrenValidator = (response) => {
 const validChildAnswerConsistency: ChildrenValidator = (response) => {
   const { answerArray } = response
   const len = answerArray[0].length
+
   return answerArray.every((subArr) => subArr.length === len)
     ? right(response)
-    : left(`ChildrenValidator:\t inconsistent answer array subarrays`)
+    : left(
+        `ChildrenValidator (validChildAnswerConsistency):\t inconsistent answer array subarrays`,
+      )
 }
 
 /**
  * Returns a validation function to check if
- * all the answers are non-empty.
+ * all the answers are non-empty if first answer subarray has length > 0 and a child is selected
  */
 const validChildAnswersNonEmpty: ChildrenValidator = (response) => {
-  const { answerArray } = response
-  return answerArray.every((subArr) =>
-    subArr.every((val) => typeof val === 'string' && !!val.trim()),
-  )
-    ? right(response)
-    : left(`ChildrenValidator:\t inconsistent answer array subarrays`)
+  const { childSubFieldsArray, answerArray } = response
+  const first = answerArray[0]
+
+  // Account for the case where no child is selected
+  const noOfChildrenSubFields = childSubFieldsArray?.length ?? 1
+  const noChildSelectedAnswerArray = Array(noOfChildrenSubFields).fill('')
+  // Similar to transformToChildOutput in inputTransformation, this is a string of empty strings (which represents number of children subfields).
+  const noChildSelectedAnswer = noChildSelectedAnswerArray[0]
+
+  return Array.isArray(first) &&
+    first.length > 0 &&
+    // Check that at least 1 child is selected
+    first[0] !== noChildSelectedAnswer
+    ? answerArray.every((subArr) =>
+        subArr.every((val) => typeof val === 'string' && !!val.trim()),
+      )
+      ? right(response)
+      : left(
+          `ChildrenValidator (validChildAnswersNonEmpty):\t inconsistent answer array subarrays`,
+        )
+    : right(response)
 }
 
 /**
@@ -75,7 +97,7 @@ const validChildAnswerAndSubFields: ChildrenValidator = (response) => {
   return childSubFieldsArray?.length === answerArray[0].length
     ? right(response)
     : left(
-        `ChildrenValidator:\t inconsistent child subfield and answer array length`,
+        `ChildrenValidator (validChildAnswerAndSubFields):\t inconsistent child subfield and answer array length`,
       )
 }
 
@@ -90,7 +112,9 @@ const validChildSubFieldsValidator: ChildrenValidatorConstructor =
     const attrs = new Set(Object.values(MyInfoChildAttributes))
     return childrenSubFields.every((subfield) => attrs.has(subfield))
       ? right(response)
-      : left(`ChildrenValidator:\t one or more subfields are invalid`)
+      : left(
+          `ChildrenValidator (validChildSubFieldsValidator):\t one or more subfields are invalid`,
+        )
   }
 
 /**
@@ -103,7 +127,9 @@ const validChildSubFieldsResponseValidator: ChildrenValidator = (response) => {
   const attrs = new Set(Object.values(MyInfoChildAttributes))
   return childSubFieldsArray?.every((subfield) => attrs.has(subfield))
     ? right(response)
-    : left(`ChildrenValidator:\t one or more subfields responses are invalid`)
+    : left(
+        `ChildrenValidator (validChildSubFieldsResponseValidator):\t one or more subfields responses are invalid`,
+      )
 }
 
 /**
@@ -120,7 +146,7 @@ const validChildSubFieldsAndResponseSubFieldsMatch: ChildrenValidatorConstructor
     )
       ? right(response)
       : left(
-          `ChildrenValidator:\t one or more subfields responses do not match the field's`,
+          `ChildrenValidator (validChildSubFieldsAndResponseSubFieldsMatch):\t one or more subfields responses do not match the field's`,
         )
   }
 
