fbc17a33ea9b338b063067ad6acad14ffb34ed2f
Ken Lee Shu Ming <ken@open.gov.sg>
2023-08-30 16:45:55 +0800

    feat: payment quantity selector (#6669)
    * chore: add sticky bottom theme for modal
    
    * feat: add quantity selector modal and button
    
    * fix: payment card focus border
    
    * chore: remove unused imports
    
    * chore: remove unused utils
    
    * refactor: rename size sticky-bottom to align-bottom
    
    * fix: ui issues raised from live-review
    
    * refactor: move paymentcard into separate file
    
    * refactor: remove unused colortheme props
    
    * fix: failure to deploy due to outdated mirror
    
    * fix: incorrect default qty when min qty is not 1
    
    * fix: input field not reseting to default when reopened, validation not running when stepper buttons were clicked
    
    * fix: issue where minus button presses doesnt revalidate input
    
    * fix: payment card content not aligned to top
    
    * fix: payment card font titles
    
    * fix: quantity selection modal ui, add variant bottom, remove selector-bottom size
    
    * fix: error message margins
    
    * chore: rename paymentcard to productpaymentcard
    
    * fix: remove unnecessary null coalesce
    
    * chore: update checkbox and radio component pseudo selector bg
    
    * fix: theming and background color
    
    * fix: productpaymentcard checked background
    
    * fix: sync total payment amount with paymentpreview, update pseudo selector css for checkbox and radio

---
+++ frontend/src/features/public-form/components/FormPaymentPage/components/PaymentQuantityModal.tsx
@@ -0,0 +1,174 @@
+import { Controller, useForm } from 'react-hook-form'
+import { BiMinus, BiPlus } from 'react-icons/bi'
+import {
+  Box,
+  Button,
+  FormControl,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  UseDisclosureReturn,
+} from '@chakra-ui/react'
+
+import { useIsMobile } from '~hooks/useIsMobile'
+import FormErrorMessage from '~components/FormControl/FormErrorMessage'
+import IconButton from '~components/IconButton'
+import { ModalCloseButton } from '~components/Modal'
+
+interface PaymentQuantityModalProps
+  extends Pick<UseDisclosureReturn, 'onClose' | 'isOpen'> {
+  onCancel: () => void
+  onSubmit: (quantity: number) => void
+  itemName: string
+  initialQty: number
+  minQty: number
+  maxQty: number
+}
+
+const PaymentQuantityModal = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  itemName,
+  initialQty,
+  minQty,
+  maxQty,
+}: PaymentQuantityModalProps) => {
+  const {
+    formState: { errors },
+    control,
+    setValue,
+    watch,
+    trigger,
+    resetField,
+  } = useForm<{ quantity: number | '' }>({
+    defaultValues: { quantity: initialQty },
+    mode: 'onChange',
+  })
+
+  const quantity = watch('quantity')
+  const positiveIntegerValidationRule = {
+    validate: (val: number | '') => {
+      if (!val) {
+        return `Minimum quantity is ${minQty}`
+      }
+      if (!Number.isInteger(val)) {
+        return 'Please enter a whole number'
+      }
+      if (val < minQty) {
+        return `Minimum quantity is ${minQty}`
+      }
+      if (val > maxQty) {
+        return `Maximum quantity is ${maxQty}`
+      }
+      return true
+    },
+  }
+
+  const isMobile = useIsMobile()
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        onClose()
+        resetField('quantity')
+      }}
+      size="selector"
+      variant={isMobile ? 'bottom' : 'default'}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+        <ModalHeader>
+          <Text textStyle={isMobile ? 'h3' : 'h2'} mb="1rem">
+            Update quantity
+          </Text>
+          <Text textStyle="body-2" color="content.medium">
+            {itemName}
+          </Text>
+        </ModalHeader>
+        <ModalBody>
+          <FormControl isInvalid={Boolean(errors.quantity)}>
+            <HStack spacing={'1.5rem'} justify="center">
+              <IconButton
+                icon={<BiMinus />}
+                variant="clear"
+                aria-label="Decrement"
+                colorScheme="secondary"
+                isDisabled={quantity <= minQty}
+                onClick={() => {
+                  setValue('quantity', quantity ? quantity - 1 : minQty)
+                  trigger('quantity')
+                }}
+              />
+              <Controller
+                name="quantity"
+                control={control}
+                rules={positiveIntegerValidationRule}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    width="5rem"
+                    onChange={(e) => {
+                      if (e.target.value === '') {
+                        field.onChange('')
+                        return
+                      }
+                      field.onChange(Number(e.target.value))
+                    }}
+                    inputMode="numeric"
+                    type="number"
+                    textAlign="center"
+                  />
+                )}
+              />
+              <IconButton
+                icon={<BiPlus />}
+                variant="clear"
+                aria-label="Increment"
+                colorScheme="secondary"
+                isDisabled={quantity >= maxQty}
+                onClick={() => {
+                  setValue('quantity', quantity ? quantity + 1 : minQty)
+                  trigger('quantity')
+                }}
+              />
+            </HStack>
+            <FormErrorMessage marginTop="1rem" marginBottom="0">
+              {errors.quantity?.message}
+            </FormErrorMessage>
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Stack
+            flex={1}
+            spacing="1rem"
+            direction={{ base: 'row', md: 'row-reverse' }}
+          >
+            <Button
+              isDisabled={Boolean(errors.quantity)}
+              loadingText="Saving"
+              onClick={() => onSubmit(quantity || 1)}
+              isFullWidth={isMobile}
+            >
+              Update
+            </Button>
+            {!isMobile ? (
+              <Button variant="clear" onClick={onClose} colorScheme="secondary">
+                Cancel
+              </Button>
+            ) : null}
+          </Stack>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+export default PaymentQuantityModal
--- frontend/src/features/public-form/components/FormPaymentPage/components/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generates integer range, inclusive of start anding values
- * [start, end]
- */
-export const generateIntRange = (start: number, end: number) => {
-  const arr = new Array(end).fill(0)
-  return arr.map((_, i) => i + 1).slice(start - 1)
-}
