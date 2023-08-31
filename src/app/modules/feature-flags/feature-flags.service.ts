af594bb0a86a1986b3b0b7da910fb0c47f629ec7
Ken Lee Shu Ming <ken@open.gov.sg>
2023-08-31 17:40:51 +0800

    feat: payment security account email check (#6604)
    * test: add cases for failed whitelisting domain
    
    * feat: add agency domain check when connecting stripe account
    
    * feat: add feature flag to allow release in dark
    
    * test: add cases to test for disabled fetaure flag
    
    * chore: update comments for clarity
    
    * chore: remove unused imports
    
    * refactor: use guard clause for flag checks
    
    * refcator: rename defaultvalue to fallback as its is applied during errors
    
    * feat: add be to redirect with query params when domain check fails
    
    * feat: add fe to show alert when non-whitelisted domain error is returned
    
    * chore: fix typo on whitelisting
    
    * Update src/app/modules/payments/__tests__/stripe.service.spec.ts
    
    Co-authored-by: Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>

--- src/app/modules/feature-flags/feature-flags.service.ts
+++ src/app/modules/feature-flags/feature-flags.service.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import { okAsync, ResultAsync } from 'neverthrow'
 
-import { createLoggerWithLabel } from '../../config/logger'
+import { featureFlags } from '../../../../shared/constants'
+import { createLoggerWithLabel, CustomLoggerParams } from '../../config/logger'
 import getFeatureFlagModel from '../../models/feature_flag.server.model'
 import { DatabaseError } from '../core/core.errors'
 
@@ -22,4 +23,50 @@ export const getEnabledFlags = (): ResultAsync<string[], DatabaseError> => {
   }).andThen((enabledFlagsDocs) =>
     okAsync(enabledFlagsDocs.map((doc) => doc.name)),
   )
+}
+
+/**
+ * Wrapper over getEnabledFlags function to gracefully handle errors
+ * and returns the `options.fallbackValue` instead.
+ *
+ * @example
+ * ```
+ * const logMeta = {
+ *  action: 'linkStripeAccountToForm',
+ * }
+ * return getFeatureFlag(featureFlags.validateStripeEmailDomain, {
+ *  fallbackValue: false,
+ *  logMeta,
+ * })
+ *.andThen((shouldValidateStripeEmailDomain) => ...)
+ *```
+ *
+ * @param flag
+ * @param options.fallbackValue the value to fall back to in the event that there's an error retrieving the feature flag
+ * @returns boolean that represents the status of the feature flag or the fallback value
+ */
+export const getFeatureFlag = (
+  flag: keyof typeof featureFlags,
+  options?: {
+    fallbackValue?: boolean
+    logMeta?: CustomLoggerParams['meta']
+  },
+): ResultAsync<boolean, DatabaseError> => {
+  const _fallbackValue = options?.fallbackValue ?? false
+  return getEnabledFlags()
+    .andThen((featureFlagsListResult) =>
+      okAsync(featureFlagsListResult.includes(flag)),
+    )
+    .orElse((error) => {
+      logger.error({
+        message: 'Error occurred whilst retrieving enabled feature flags',
+        meta: options?.logMeta
+          ? options.logMeta
+          : {
+              action: 'getFeatureFlag',
+            },
+        error,
+      })
+      return okAsync(_fallbackValue)
+    })
 }
