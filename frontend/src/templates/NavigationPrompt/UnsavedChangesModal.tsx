e8f1a7e0feb19f38d17fa20f9bdb4794b59a27d8
foochifa <foochifa@gmail.com>
2023-09-12 15:13:12 +0800

    fix: mobile view of unsaved modal

--- frontend/src/templates/NavigationPrompt/UnsavedChangesModal.tsx
+++ frontend/src/templates/NavigationPrompt/UnsavedChangesModal.tsx
@@ -9,6 +9,7 @@ import {
   ModalOverlay,
   ModalProps,
   Stack,
+  useBreakpointValue,
 } from '@chakra-ui/react'
 
 import { useIsMobile } from '~hooks/useIsMobile'
@@ -41,6 +42,11 @@ export const UnsavedChangesModal = ({
   cancelButtonText = 'No, stay on page',
   ...modalProps
 }: UnsavedChangesModalProps): JSX.Element => {
+  const modalSize = useBreakpointValue({
+    base: 'mobile',
+    xs: 'mobile',
+    md: 'md',
+  })
   const isMobile = useIsMobile()
 
   return (
@@ -48,6 +54,7 @@ export const UnsavedChangesModal = ({
       isOpen={isOpen}
       onClose={onClose}
       returnFocusOnClose={returnFocusOnClose}
+      size={modalSize}
       {...modalProps}
     >
       <ModalOverlay />
