4c35b1abe44f34046f870099fbe1ed848f86ebab
Ken <ken@open.gov.sg>
2023-10-03 00:35:23 +0800

    feat: add be routes

--- src/app/routes/api/v3/auth/auth-sgid.routes.ts
+++ src/app/routes/api/v3/auth/auth-sgid.routes.ts
@@ -6,4 +6,33 @@ export const AuthSGIDRouter = Router()
 
 AuthSGIDRouter.get('/authurl', AuthSgidController.generateAuthUrl)
 
-AuthSGIDRouter.get('/login', AuthSgidController.handleLogin)
+/**
+ * Receives the selected login details from Sgid
+ * Sets the returned profiles in req.session.sgid
+ * @route POST /sgid/auth/login/callback
+ *
+ * The frontend should query the available profiles through GET /sgid/auth/profiles
+ *
+ * @returns 200 with redirect to frontend /login/callback if there are no errors
+ */
+AuthSGIDRouter.get('/login/callback', AuthSgidController.handleLoginCallback)
+
+/**
+ * Sets the selected user profile
+ * Uses post request to select the workemail from the request body
+ * @route POST /sgid/auth/profile
+ *
+ * @returns 200 with redirect to /dashboard if workemail is valid
+ * @returns 500 when database error occurs
+ */
+AuthSGIDRouter.get('/profiles', AuthSgidController.getProfiles)
+
+/**
+ * Sets the selected user profile
+ * Uses post request to select the workemail from the request body
+ * @route POST /sgid/auth/profile
+ *
+ * @returns 200 with redirect to /dashboard if workemail is valid
+ * @returns 500 when database error occurs
+ */
+AuthSGIDRouter.post('/profile', AuthSgidController.setProfile)
