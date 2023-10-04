a2289b97d4ad23a6eb73172c9fb88f184c22fef0
Ken <ken@open.gov.sg>
2023-10-05 01:31:47 +0800

    refactor: move constant external urls to shared constants

--- frontend/src/features/login/components/SgidLoginButton.tsx
+++ frontend/src/features/login/components/SgidLoginButton.tsx
@@ -2,12 +2,12 @@ import { useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
 import { Flex, Link, Text, VStack } from '@chakra-ui/react'
 
+import { SGID_VALID_ORG_PAGE } from '~shared/constants'
+
 import { SingpassFullLogoSvgr } from '~assets/svgrs/singpass/SingpassFullLogoSvgr'
 import { getSgidAuthUrl } from '~services/AuthService'
 import Button from '~components/Button'
 
-const SGID_VALID_ORG_PAGE =
-  'https://docs.id.gov.sg/faq-users#as-a-government-officer-why-am-i-not-able-to-login-to-my-work-tool-using-sgid'
 export const SgidLoginButton = (): JSX.Element => {
   const { formState } = useForm()
 
