ced775ecd5f73e993d32e7f5bf512da3a5d2b99c
LinHuiqing <huiqing@open.gov.sg>
2023-10-12 07:52:25 +0800

    feat: io for pdf parser

--- src/app/modules/pdf-parser/pdf-parser.py
+++ src/app/modules/pdf-parser/pdf-parser.py
@@ -1,20 +1,36 @@
 from unstructured.partition.pdf import partition_pdf
 import argparse
+import os
 
 
-def parse_pdf(path):
+def parse_pdf(input_path, output_path):
     output_arr = []
-    tokens = partition_pdf(path)
+    tokens = partition_pdf(input_path)
     for token in tokens:
         props = token.to_dict()
         output_arr.append(f"{props['type']} | {props['text']}")
-    return "\n".join(output_arr)
+    parsed_text = "\n".join(output_arr)
+    with open(args.output_path, "w") as f:
+        f.write(parsed_text)
+    return parsed_text
 
 
 if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.realpath(__file__))
     parser = argparse.ArgumentParser(description="Parse a PDF file.")
     parser.add_argument(
-        "path", type=str, help="Path to the PDF file.", default="input.pdf"
+        "-i",
+        "--input_path",
+        type=str,
+        help="Path to the PDF file to be parsed.",
+        default=f"{script_dir}/input.pdf",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_path",
+        type=str,
+        help="Path to the output text file parsed from input.",
+        default=f"{script_dir}/output.txt",
     )
     args = parser.parse_args()
-    print(parse_pdf(args.path))
+    print(parse_pdf(args.input_path, args.output_path))
