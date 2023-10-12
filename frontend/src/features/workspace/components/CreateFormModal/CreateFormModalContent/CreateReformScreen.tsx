709f258ab12ea45e54641ad6e165f99641f8236c
LinHuiqing <huiqing@open.gov.sg>
2023-10-12 23:46:48 +0800

    feat: cleaner copy for upload / write flow

--- frontend/src/features/workspace/components/CreateFormModal/CreateFormModalContent/CreateReformScreen.tsx
+++ frontend/src/features/workspace/components/CreateFormModal/CreateFormModalContent/CreateReformScreen.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, useCallback, useState } from 'react'
-import { BiRightArrowAlt } from 'react-icons/bi'
+import { BiFileBlank, BiPencil, BiRightArrowAlt } from 'react-icons/bi'
 import { useNavigate } from 'react-router-dom'
 import {
   Container,
@@ -14,16 +14,23 @@ import {
 } from '@chakra-ui/react'
 
 import { ADMINFORM_ROUTE } from '~constants/routes'
+import Badge from '~components/Badge'
 import Button from '~components/Button'
 import FormLabel from '~components/FormControl/FormLabel'
 import Input from '~components/Input'
 import Textarea from '~components/Textarea'
+import Tile from '~components/Tile'
 
 import { useReformMutations } from '~features/admin-form/reform/mutations'
 import { parseModelOutput } from '~features/admin-form/reform/utils'
 import { useUser } from '~features/user/queries'
 
 import { Attachment } from '../../Attachment/Attachment'
+
+enum ReformMode {
+  Upload = 'upload',
+  Write = 'write',
+}
 
 export const CreateReformScreen = ({
   setStandardFlow,
@@ -174,85 +181,133 @@ export const CreateReformScreen = ({
     })
   }
 
+  const [reformMode, setReformMode] = useState<ReformMode | false>(false)
+
   return (
     <>
       <ModalHeader color="secondary.700">
         <Container maxW="42.5rem" p={0}>
           <Text>Build your form with AI</Text>
-          <Text>This will be an email mode form.</Text>
+          <Text textStyle="subhead-1">This will be an email mode form.</Text>
         </Container>
       </ModalHeader>
       <ModalBody whiteSpace="pre-wrap">
         <Container maxW="42.5rem" p={0}>
           <Flex direction="column">
             <FormControl isRequired mb="2.25rem">
-              <FormLabel description="You can change this later.">
+              <FormLabel description="You can change this later">
                 Form name
               </FormLabel>
-              <Input value={formName} onChange={handleFormNameChange} />
+              <Input
+                value={formName}
+                onChange={handleFormNameChange}
+                isDisabled={isCreatingForm}
+              />
             </FormControl>
             <FormControl isRequired mb="2.25rem">
-              <FormLabel>I want to create a form that...</FormLabel>
-              <Stack direction="row">
-                <Input
-                  placeholder="collects personal particulars for an event"
-                  value={purpose}
-                  onChange={handlePurposeChange}
-                />
-                <Spacer />
-                <Button
-                  onClick={handlePurposeEnter}
-                  isLoading={isFetchingQuestions}
+              <FormLabel>How do you want to build?</FormLabel>
+              <Stack
+                spacing="1rem"
+                w="100%"
+                direction={{ base: 'column', md: 'row' }}
+              >
+                <Tile
+                  variant="complex"
+                  icon={BiFileBlank}
+                  badge={<Badge colorScheme="success">Recommended</Badge>}
+                  isActive={reformMode === ReformMode.Upload}
+                  onClick={() => setReformMode(ReformMode.Upload)}
+                  isFullWidth
+                  flex={1}
+                  isDisabled={
+                    isCreatingForm && reformMode !== ReformMode.Upload
+                  }
                 >
-                  Enter
-                </Button>
+                  <Tile.Title>Upload a form</Tile.Title>
+                  <Tile.Subtitle>Convert a paper or pdf form</Tile.Subtitle>
+                </Tile>
+                <Tile
+                  variant="complex"
+                  icon={BiPencil}
+                  isActive={reformMode === ReformMode.Write}
+                  onClick={() => setReformMode(ReformMode.Write)}
+                  isFullWidth
+                  flex={1}
+                  isDisabled={isCreatingForm && reformMode !== ReformMode.Write}
+                >
+                  <Tile.Title>Write a prompt</Tile.Title>
+                  <Tile.Subtitle>Build a form based on a prompt</Tile.Subtitle>
+                </Tile>
               </Stack>
             </FormControl>
-            <FormControl isRequired mb="2.25rem">
-              <FormLabel>
-                Alternatively, upload a PDF form you would like to convert
-              </FormLabel>
-              <Attachment
-                onChange={handleFileUpload}
-                onError={handleFileUploadError}
-                value={selectedFile}
-                maxSize={2000000} // 2MB
-                name="file-upload"
-                accept=".pdf"
-              ></Attachment>
-              {isProgress ? (
-                <Progress size="md" isIndeterminate></Progress>
-              ) : null}
-            </FormControl>
-            {isFetchingQuestions ? (
-              <Text>Generating list of questions, this will take ~1 min.</Text>
-            ) : null}
-            {qnsList ? (
+            {reformMode === ReformMode.Upload ? (
               <FormControl isRequired mb="2.25rem">
-                <FormLabel>
-                  These are the questions the form should have:
-                </FormLabel>
+                <FormLabel>Upload your PDF form</FormLabel>
+                <Attachment
+                  onChange={handleFileUpload}
+                  onError={handleFileUploadError}
+                  value={selectedFile}
+                  maxSize={2000000} // 2MB
+                  name="file-upload"
+                  accept=".pdf"
+                ></Attachment>
+                {isProgress ? (
+                  <Progress size="md" isIndeterminate></Progress>
+                ) : null}
+              </FormControl>
+            ) : reformMode === ReformMode.Write ? (
+              <FormControl isRequired mb="2.25rem">
+                <FormLabel>I want to create a form that...</FormLabel>
                 <Stack direction="row">
-                  <Textarea value={qnsList} onChange={handleQuestionsChange} />
+                  <Input
+                    placeholder="collects personal particulars for an event"
+                    value={purpose}
+                    onChange={handlePurposeChange}
+                    isDisabled={isCreatingForm}
+                  />
+                  <Spacer />
+                  <Button
+                    onClick={handlePurposeEnter}
+                    isLoading={isFetchingQuestions}
+                    spinnerPlacement="end"
+                    loadingText="Generating questions..."
+                    isDisabled={isCreatingForm}
+                  >
+                    Enter
+                  </Button>
                 </Stack>
               </FormControl>
             ) : null}
-            {isCreatingForm ? <Text>This will take ~3 mins...</Text> : null}
+            {qnsList ? (
+              <FormControl isRequired mb="2.25rem">
+                <FormLabel description="You may edit or add questions here">
+                  Your generated questions
+                </FormLabel>
+                <Stack direction="row">
+                  <Textarea
+                    value={qnsList}
+                    onChange={handleQuestionsChange}
+                    isDisabled={isCreatingForm}
+                  />
+                </Stack>
+              </FormControl>
+            ) : null}
             <Stack direction="row-reverse">
               <Button
                 rightIcon={<BiRightArrowAlt fontSize="1.5rem" />}
                 type="submit"
                 isLoading={isCreatingForm}
+                loadingText="Creating form..."
+                spinnerPlacement="end"
                 isDisabled={!qnsList}
                 onClick={handleCreateFormFromQnsList}
               >
-                <Text lineHeight="1.5rem">Next step</Text>
+                <Text lineHeight="1.5rem">Create form</Text>
               </Button>
               <Button
                 type="submit"
-                isLoading={isCreatingForm}
                 onClick={() => setStandardFlow(true)}
-                variant="reverse"
+                variant="clear"
               >
                 <Text lineHeight="1.5rem">Back</Text>
               </Button>
