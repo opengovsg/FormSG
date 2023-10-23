2c484e6ccfdecd679c2227b1ed7a3f956f46b73c
foochifa <foochifa@gmail.com>
2023-10-23 13:48:34 +0800

    chore: update copy of workspace validation messages

--- frontend/src/utils/workspaceValidation.ts
+++ frontend/src/utils/workspaceValidation.ts
@@ -7,17 +7,17 @@ export const WORKSPACE_TITLE_VALIDATION_RULES: UseControllerProps['rules'] = {
   required: 'Folder name is required',
   maxLength: {
     value: MAX_TITLE_LENGTH,
-    message: `Folder name must be at most ${MAX_TITLE_LENGTH} characters`,
+    message: `Folder name should contain less than ${MAX_TITLE_LENGTH} characters`,
   },
   pattern: {
     value: /^[a-zA-Z0-9_\-./() &`;'"]*$/,
-    message: 'Folder name cannot contain special characters',
+    message: 'Folder name should not contain special characters',
   },
   validate: {
     trimMinLength: (value: string) => {
       return (
         value.trim().length >= MIN_TITLE_LENGTH ||
-        `Folder name must be at least ${MIN_TITLE_LENGTH} characters`
+        `Folder name should contain more than ${MIN_TITLE_LENGTH} characters`
       )
     },
   },
