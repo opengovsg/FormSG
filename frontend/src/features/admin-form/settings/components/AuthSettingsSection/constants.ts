1351389646233b54ad24c5373ca19b99cc3c1792
wanlingt <wanling@open.gov.sg>
2023-11-07 16:52:57 +0800

    ref: refactor authtypes

--- frontend/src/features/admin-form/settings/components/AuthSettingsSection/constants.ts
+++ frontend/src/features/admin-form/settings/components/AuthSettingsSection/constants.ts
@@ -3,24 +3,19 @@ import { FormAuthType, FormResponseMode } from '~shared/types/form'
 export type EmailFormAuthType = FormAuthType
 export type StorageFormAuthType = FormAuthType
 
-export const STORAGE_MODE_AUTHTYPES: Record<StorageFormAuthType, string> = {
-  [FormAuthType.NIL]: 'None',
-  [FormAuthType.SGID]: 'Singpass App-only Login',
-  [FormAuthType.SGID_MyInfo]: 'Singpass App-only with Myinfo',
-  [FormAuthType.SP]: 'Singpass',
-  [FormAuthType.MyInfo]: 'Singpass with Myinfo',
-  [FormAuthType.CP]: 'Singpass (Corporate)',
-}
+const FORM_AUTHTYPES: Record<StorageFormAuthType | EmailFormAuthType, string> =
+  {
+    [FormAuthType.NIL]: 'None',
+    [FormAuthType.SGID]: 'Singpass App-only Login',
+    [FormAuthType.SGID_MyInfo]: 'Singpass App-only with Myinfo',
+    [FormAuthType.SP]: 'Singpass',
+    [FormAuthType.MyInfo]: 'Singpass with Myinfo',
+    [FormAuthType.CP]: 'Singpass (Corporate)',
+  }
 
-// Not using STORAGE_MODE_AUTHTYPES due to wanting a different order.
-export const EMAIL_MODE_AUTHTYPES: Record<EmailFormAuthType, string> = {
-  [FormAuthType.NIL]: 'None',
-  [FormAuthType.SGID]: 'Singpass App-only Login',
-  [FormAuthType.SGID_MyInfo]: 'Singpass App-only with Myinfo',
-  [FormAuthType.SP]: 'Singpass',
-  [FormAuthType.MyInfo]: 'Singpass with Myinfo',
-  [FormAuthType.CP]: 'Singpass (Corporate)',
-}
+export const STORAGE_MODE_AUTHTYPES = FORM_AUTHTYPES
+export const EMAIL_MODE_AUTHTYPES = FORM_AUTHTYPES
+
 export const AUTHTYPE_TO_TEXT = {
   [FormResponseMode.Email]: EMAIL_MODE_AUTHTYPES,
   [FormResponseMode.Encrypt]: STORAGE_MODE_AUTHTYPES,
