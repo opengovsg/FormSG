9cf45940d027175607799a30f7879fd112275092
Ken Lee Shu Ming <ken@open.gov.sg>
2023-11-08 19:12:07 +0800

    fix(datepicker): webkit-related stylings (#6875)
    * fix: prevent unused outer aria from affecting webkit-related styles
    
    * fix: css when disabled
    
    * chore: remove unused props
    
    * fix: calendar button having red border when input is invalid
    
    * fix: remove border glitches when button is active
    
    * fix: remove border glitches when input has error
    
    * chore: add storybook cases for error and prefilled

--- frontend/src/components/DatePicker/components/CalendarButton.tsx
+++ frontend/src/components/DatePicker/components/CalendarButton.tsx
@@ -6,18 +6,17 @@ import { useDatePicker } from '../DatePickerContext'
 export const CalendarButton = (): JSX.Element => {
   const {
     disclosureProps: { onOpen, isOpen },
-    colorScheme,
     calendarButtonAria,
     fcProps: { isDisabled, isReadOnly },
   } = useDatePicker()
   return (
     <IconButton
       onClick={onOpen}
-      colorScheme={colorScheme}
       aria-label={calendarButtonAria}
       icon={<BxCalendar />}
       variant="inputAttached"
-      borderRadius={0}
+      borderLeftColor={'transparent'}
+      borderLeftRadius={0}
       isActive={isOpen}
       isDisabled={isDisabled || isReadOnly}
     />
