c63c1c851faf5e9020050dea5bbaa9bf8ccb73ad
wanlingt <wanling@open.gov.sg>
2023-11-14 16:43:05 +0800

    ref: use common FORM_AUTHTYPES for email and storage mode forms

--- frontend/src/features/admin-form/settings/components/AuthSettingsSection/constants.ts
+++ frontend/src/features/admin-form/settings/components/AuthSettingsSection/constants.ts
@@ -3,20 +3,14 @@ import { FormAuthType, FormResponseMode } from '~shared/types/form'
 export type EmailFormAuthType = FormAuthType
 export type StorageFormAuthType = FormAuthType
 
-const FORM_AUTHTYPES: Record<StorageFormAuthType | EmailFormAuthType, string> =
-  {
-    [FormAuthType.NIL]: 'None',
-    [FormAuthType.SGID]: 'Singpass App-only Login',
-    [FormAuthType.SGID_MyInfo]: 'Singpass App-only with Myinfo',
-    [FormAuthType.SP]: 'Singpass',
-    [FormAuthType.MyInfo]: 'Singpass with Myinfo',
-    [FormAuthType.CP]: 'Singpass (Corporate)',
-  }
-
-export const STORAGE_MODE_AUTHTYPES = FORM_AUTHTYPES
-export const EMAIL_MODE_AUTHTYPES = FORM_AUTHTYPES
-
-export const AUTHTYPE_TO_TEXT = {
-  [FormResponseMode.Email]: EMAIL_MODE_AUTHTYPES,
-  [FormResponseMode.Encrypt]: STORAGE_MODE_AUTHTYPES,
+export const FORM_AUTHTYPES: Record<
+  StorageFormAuthType | EmailFormAuthType,
+  string
+> = {
+  [FormAuthType.NIL]: 'None',
+  [FormAuthType.SGID]: 'Singpass App-only Login',
+  [FormAuthType.SGID_MyInfo]: 'Singpass App-only with Myinfo',
+  [FormAuthType.SP]: 'Singpass',
+  [FormAuthType.MyInfo]: 'Singpass with Myinfo',
+  [FormAuthType.CP]: 'Singpass (Corporate)',
 }
