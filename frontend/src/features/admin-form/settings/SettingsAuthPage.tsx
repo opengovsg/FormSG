0bc1b545597e1f6334e411c52bb1a45c3e86256c
Justyn Oh <justynoh@gmail.com>
2023-11-30 13:06:06 +0800

    fix: guard against editing authtype

--- frontend/src/features/admin-form/settings/SettingsAuthPage.tsx
+++ frontend/src/features/admin-form/settings/SettingsAuthPage.tsx
@@ -1,7 +1,21 @@
+import { FormResponseMode } from '~shared/types'
+
 import AuthSettingsSection from './components/AuthSettingsSection'
+import { AuthUnsupportedMsg } from './components/AuthSettingsSection/AuthUnsupportedMsg'
 import { CategoryHeader } from './components/CategoryHeader'
+import { useAdminFormSettings } from './queries'
 
 export const SettingsAuthPage = (): JSX.Element => {
+  const { data: settings, isLoading } = useAdminFormSettings()
+
+  // Form auth is unsupported in MRF; show message.
+  if (
+    !isLoading &&
+    settings?.responseMode === FormResponseMode.Multirespondent
+  ) {
+    return <AuthUnsupportedMsg />
+  }
+
   return (
     <>
       <CategoryHeader>Enable Singpass authentication</CategoryHeader>
