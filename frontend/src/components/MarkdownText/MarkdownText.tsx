97dde9c13ff19d04a5cc8074dfb5b6c592dabe95
wanlingt <wanling@open.gov.sg>
2023-11-30 17:34:10 +0800

    fix(markdown): handle newlines after indentation groups

--- frontend/src/components/MarkdownText/MarkdownText.tsx
+++ frontend/src/components/MarkdownText/MarkdownText.tsx
@@ -23,7 +23,28 @@ export const MarkdownText = ({
   const processedRawString = useMemo(() => {
     // Create new line nodes for every new line in raw string so new lines gets rendered.
     if (multilineBreaks) {
-      return children.replace(/\n/gi, '&nbsp; \n')
+      return (
+        /**
+         * For lines that are contain a token that indents,
+         * we want to remove the whitespace before the newline
+         * so that new lines that come after these tokens
+         * can break out of the indentation group
+         *
+         *  (-|\d+\.|\*): matching character tokens that indents
+         *     -: "-"
+         *     *: "*",
+         *     \d+ : "1.", "2.", etc.
+         *
+         *   \s: whitespace following the token, indentation groups must start with token followed by a whitespace character
+         *
+         *   .*: any character, any number of times, this is the actual text content of the line
+         *
+         *   \n: new line character
+         */
+        children
+          .replace(/\n/gi, '&nbsp; \n')
+          .replace(/(\n(-|\d+\.|\*)\s.*\n)(&nbsp; \n)/gi, '$1 \n')
+      )
     }
     return children
   }, [children, multilineBreaks])
