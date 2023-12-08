a840ab916c71a40f2ee0f3041c2a3af162b53f52
sebastianwzq <sebastian@open.gov.sg>
2023-12-08 14:27:21 +0800

    fix: change wrong commit

--- shared/constants/file.ts
+++ shared/constants/file.ts
@@ -3,8 +3,8 @@
 /** File types that can be uploaded for form image/logo */
 export const VALID_UPLOAD_FILE_TYPES = ['image/jpeg', 'image/png', 'image/gif']
 
-export const KB = 1024
-export const MB = 1024 * KB
+export const KB = 1000
+export const MB = 1000 * KB
 
 // Define max file size as 2MB
 export const MAX_UPLOAD_FILE_SIZE = 2 * MB // 2 Million/Mega Bytes, or 2 MB
