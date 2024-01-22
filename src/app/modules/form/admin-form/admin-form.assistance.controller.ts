256f33334dd8f022a51ff12782eb6c32928d8cb3
sebastianwzq <sebastian@open.gov.sg>
2024-01-23 00:22:36 +0800

    feat: implemented generate questions and generate form fields endpoint

--- src/app/modules/form/admin-form/admin-form.assistance.controller.ts
+++ src/app/modules/form/admin-form/admin-form.assistance.controller.ts
@@ -3,23 +3,69 @@ import { ChatCompletionMessageParam } from 'openai/src/resources/chat/completion
 
 import { ControllerHandler } from '../../core/core.types'
 
+import { sampleFormFields } from './admin-form.assistance.constants'
+import {
+  formFieldsPromptBuilder,
+  questionListPromptBuilder,
+  schemaPromptBuilder,
+} from './admin-form.assistance.service'
+
 const openai = new OpenAI({
   apiKey: 'fake-key',
 })
-export const generateQuestions: ControllerHandler = async (req, res) => {
-  console.log(req.body)
+export const generateQuestions: ControllerHandler<
+  unknown,
+  unknown,
+  { purpose: string }
+> = async (req, res) => {
   const messages: ChatCompletionMessageParam[] = [
-    { role: 'system', content: 'test' },
+    { role: 'system', content: schemaPromptBuilder(sampleFormFields) },
     {
       role: 'user',
-      content: 'hello world!',
+      content: questionListPromptBuilder(req.body.purpose),
     },
   ]
-  const completion = await openai.chat.completions.create({
-    messages: messages,
-    model: 'gpt-3.5-turbo',
-  })
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: messages,
+      model: 'gpt-3.5-turbo',
+    })
+    return res
+      .status(200)
+      .json([...messages, chatCompletion.choices[0].message])
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: error })
+  }
+}
 
-  console.log(completion.choices[0])
-  res.send(completion.choices[0])
+export const generateFormFields: ControllerHandler<
+  unknown,
+  unknown,
+  {
+    prevMessages: { role: string; content: string }[]
+    questions: string
+  }
+> = async (req, res) => {
+  try {
+    const prevMessages = [
+      ...req.body.prevMessages,
+    ] as ChatCompletionMessageParam[]
+
+    const messages: ChatCompletionMessageParam[] = [
+      ...prevMessages,
+      {
+        role: 'user',
+        content: formFieldsPromptBuilder(req.body.questions),
+      },
+    ]
+    const chatCompletion = await openai.chat.completions.create({
+      messages: messages,
+      model: 'gpt-3.5-turbo',
+    })
+    return res.status(200).json(chatCompletion.choices[0].message)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: error })
+  }
 }
---
+++ src/app/modules/form/admin-form/admin-form.assistance.service.ts
@@ -0,0 +1,19 @@
+export const schemaPromptBuilder = (schema: any) => {
+  const prompt = `I am a form builder that has the possible form field schemas in the following list:\n
+  ${JSON.stringify(schema, null, 2)}`
+  return prompt
+}
+
+export const formFieldsPromptBuilder = (questions: string) => {
+  const prompt = `Help me generate a form with the following list of questions:\n
+  ${questions}\n\n
+  Present the questions as form fields in JSON (list of form field schemas), in the form of "---\n<JSON schema; array of form fields>\n---" without any code blocks.`
+  return prompt
+}
+
+export const questionListPromptBuilder = (purpose: string) => {
+  const prompt = `I am a public officer who wants to create a form that ${purpose}.\n
+  Give me a list of questions I should have in my form, in the form of
+  "---\n1. <question> | <answer type>\n2. <question> | <answer type>\n---".`
+  return prompt
+}
