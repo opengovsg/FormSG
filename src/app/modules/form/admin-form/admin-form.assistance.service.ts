90fd6041f983b8cd440f4f9ec0a51e9646fd8cd9
sebastianwzq <sebastian@open.gov.sg>
2024-01-23 17:45:15 +0800

    feat: added form field generation for pdf content

--- src/app/modules/form/admin-form/admin-form.assistance.service.ts
+++ src/app/modules/form/admin-form/admin-form.assistance.service.ts
@@ -11,15 +11,21 @@ export const schemaPromptBuilder = (schema: any) => {
   return prompt
 }
 
-export const formFieldsPromptBuilder = (questions: string) => {
-  const prompt = `Help me generate a form with the following list of questions: ${questions}
-  Present the questions as form fields in JSON (list of form field schemas), in the form of "${expectedFormFieldSchemaFormat}" as defined by the system, without any code blocks. Format the JSON as a single line.`
-  return prompt
-}
-
 export const questionListPromptBuilder = (purpose: string) => {
   const prompt = `I am a public officer who wants to create a form that ${purpose}.
   Give me a list of content / questions I should have in my form built with this form builder, in the form of "${expectedQuestionsListFormat}".`
+  return prompt
+}
+
+export const questionListPromptBuilderV2 = (purpose: string) => {
+  const prompt = `I am a public offer who wants to create a form for ${purpose}. Give me a list of questions and answers that I should include in my form. The questions and answers should be in the following format:\n
+   "${expectedQuestionsListFormat}".`
+  return prompt
+}
+
+export const formFieldsPromptBuilder = (questions: string) => {
+  const prompt = `Help me generate a form with the following list of questions: ${questions}
+  Present the questions as form fields in JSON (list of form field schemas), in the form of "${expectedFormFieldSchemaFormat}" as defined by the system, without any code blocks. Format the JSON as a single line.`
   return prompt
 }
 
