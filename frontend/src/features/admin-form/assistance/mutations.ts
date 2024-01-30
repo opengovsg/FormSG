9cf257fa20792a2596be72d4cd195e0b083708a4
tshuli <shuli@open.gov.sg>
2024-01-31 00:55:04 +0800

    chore: close modal and clear queries on success

--- frontend/src/features/admin-form/assistance/mutations.ts
+++ frontend/src/features/admin-form/assistance/mutations.ts
@@ -1,4 +1,6 @@
-import { useMutation } from 'react-query'
+import { useContext } from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+import { useParams } from 'react-router-dom'
 
 import { ContentTypes } from '~shared/types/assistance'
 
@@ -9,8 +11,21 @@ import {
 import { parseModelOutput } from '~features/admin-form/assistance/utils'
 import { useCreateFormField } from '~features/admin-form/create/builder-and-design/mutations/useCreateFormField'
 
+import { adminFormKeys } from '../common/queries'
+import { MagicFormBuilderModalOnCloseContext } from '../create/builder-and-design/BuilderAndDesignContent/FormBuilder'
+
 export const useAssistanceMutations = () => {
   const { createFieldsMutation } = useCreateFormField()
+  const { formId } = useParams()
+
+  if (!formId) {
+    throw new Error('Form ID is required')
+  }
+
+  const queryClient = useQueryClient()
+
+  const onCloseContext = useContext(MagicFormBuilderModalOnCloseContext)
+  const { onClose } = onCloseContext || {}
 
   const createFieldsFromPromptMutation = useMutation((prompt: string) =>
     generateQuestions(prompt)
@@ -22,7 +37,12 @@ export const useAssistanceMutations = () => {
         if (data.content) {
           formFields = JSON.parse(parseModelOutput(data.content))
         }
-        return createFieldsMutation.mutate(formFields)
+        return createFieldsMutation.mutate(formFields, {
+          onSuccess: () => {
+            queryClient.invalidateQueries(adminFormKeys.id(formId))
+            onClose()
+          },
+        })
       }),
   )
 
@@ -32,7 +52,12 @@ export const useAssistanceMutations = () => {
       if (data.content) {
         formFields = JSON.parse(parseModelOutput(data.content))
       }
-      return createFieldsMutation.mutate(formFields)
+      return createFieldsMutation.mutate(formFields, {
+        onSuccess: () => {
+          queryClient.invalidateQueries(adminFormKeys.id(formId))
+          onClose()
+        },
+      })
     }),
   )
 
