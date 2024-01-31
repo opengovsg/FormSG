c350ac092bbb7e65782b5ed9c2c0e1f0954fcb6e
sebastianwzq <sebastian@open.gov.sg>
2024-01-31 18:09:44 +0800

    refactor: refactor prompt

--- src/app/modules/form/admin-form/admin-form.assistance.utils.ts
+++ src/app/modules/form/admin-form/admin-form.assistance.utils.ts
@@ -15,30 +15,27 @@ const expectedFormFieldSchemaFormat =
 const expectedQuestionsListFormat =
   '---\n1. <question> | <answer type>\n2. ...\n---'
 
-export const schemaPromptBuilder = (schema: any) => {
+export const schemaPromptBuilder = (schema: string) => {
   const prompt = `I am a FormSG, a form builder that has the possible form field schemas in the following list:\n
-  ${JSON.stringify(schema)}
+  ${schema}
   Please keep any null values in the schema as null, and false values in the schema as false. Strictly include all keys in the schema, even if they are null or false.`
   return prompt
 }
 
 export const questionListPromptBuilder = (purpose: string) => {
-  const prompt = `I am a public officer who wants to create a form that collects ${purpose}.
+  return `I am a public officer who wants to create a form that collects ${purpose}.
   Give me a list of content / questions I should have in my form built with this form builder, in the form of "${expectedQuestionsListFormat}".`
-  return prompt
 }
 
 export const formFieldsPromptBuilder = (questions: string) => {
-  const prompt = `Help me generate a form with the following list of questions: ${questions}
+  return `Help me generate a form with the following list of questions: ${questions}
   Present the questions as FormSG form fields in JSON (list of form field schemas), in the form of "${expectedFormFieldSchemaFormat}" as defined by the system, without any code blocks. Format the JSON as a single line.`
-  return prompt
 }
 export const migratePromptBuilder = (parsedContent: string) => {
-  const prompt = `Help me generate the corresponding JSON form fields from content parsed from a PDF document.
+  return `Help me generate the corresponding JSON form fields from content parsed from a PDF document.
   Here is the parsed content from the PDF document (wrapped in triple quotes):
   """
   ${parsedContent}
   """
   Based on the parsed content, extract content that should be added to the form builder form and present them as a list, in the form of "${expectedQuestionsListFormat}".`
-  return prompt
 }
