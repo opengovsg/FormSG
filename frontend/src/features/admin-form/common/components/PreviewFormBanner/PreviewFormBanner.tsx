80eb52751bd619d266fd1ebd974154fe7165b385
Kathleen Koh <kathleen@open.gov.sg>
2024-02-08 16:36:19 +0800

    feat: add warning banner that attachments and answers are not saved

--- frontend/src/features/admin-form/common/components/PreviewFormBanner/PreviewFormBanner.tsx
+++ frontend/src/features/admin-form/common/components/PreviewFormBanner/PreviewFormBanner.tsx
@@ -188,8 +188,9 @@ export const PreviewFormBanner = ({
             </Text>
           ) : (
             <Text {...textProps}>
-              You will not be able to make a test payment in Form Preview mode.
-              Open your form to make a test payment.
+              You will not be able to make a test payment, or view submitted
+              answers or attachments in Form Preview mode. Open your form to
+              make a test payment or form submission.
             </Text>
           )}
         </Flex>
