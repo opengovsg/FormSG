84e86e1c0992cec7c11fb526d91a4d402edffb19
LeonardYam <yamthesmall@gmail.com>
2024-02-10 16:52:28 +0800

    feat: add placeholder feature and improve styling

--- frontend/src/components/RichTextEditor/RichTextEditor.tsx
+++ frontend/src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,4 +1,6 @@
+import { Box } from '@chakra-ui/react'
 import Link from '@tiptap/extension-link'
+import Placeholder from '@tiptap/extension-placeholder'
 import { EditorProvider } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
@@ -12,19 +14,32 @@ export const RichTextEditor = ({
   onChange: (value: unknown) => void
 }) => {
   return (
-    <EditorProvider
-      content={value}
-      slotBefore={<MenuBar />}
-      extensions={[
-        StarterKit.configure({
-          blockquote: false,
-        }),
-        Link,
-      ]}
-      onUpdate={({ editor }) => onChange(editor.getHTML())}
-      // EditorProvider has invalid types (children property is required)
-      // See: https://github.com/ueberdosis/tiptap/issues/4618
-      children={<></>}
-    />
+    <Box
+      border="1px solid"
+      borderColor="neutral.400"
+      borderRadius="base"
+      overflow="hidden"
+    >
+      <EditorProvider
+        content={value}
+        slotBefore={<MenuBar />}
+        extensions={[
+          StarterKit.configure({
+            blockquote: false,
+            heading: {
+              levels: [2],
+            },
+          }),
+          Link,
+          Placeholder.configure({
+            placeholder: 'Write something :)',
+          }),
+        ]}
+        onUpdate={({ editor }) => onChange(editor.getHTML())}
+        // EditorProvider has invalid types (children property is required)
+        // See: https://github.com/ueberdosis/tiptap/issues/4618
+        children={<></>}
+      />
+    </Box>
   )
 }
--- frontend/src/theme/editorStyles.ts
+++ frontend/src/theme/editorStyles.ts
@@ -1,5 +1,10 @@
 // Styles for the Tiptap rich text editor
 export const editorStyles = {
+  // Apply styles only to editable instances
+  '.tiptap[contentEditable=true]': {
+    'min-height': '10em',
+    padding: '1em',
+  },
   '.tiptap ul, ol': {
     'margin-block-start': '1em',
     'margin-block-end': '1em',
@@ -21,5 +26,14 @@ export const editorStyles = {
   '.tiptap h1': {
     fontSize: '2em', // HTML Standard
     fontWeight: 'bolder',
+  },
+  // Display placeholder for first line in an empty editor
+  // See: https://tiptap.dev/docs/editor/api/extensions/placeholder#placeholder
+  '.tiptap p.is-editor-empty:first-child::before': {
+    color: '#adb5bd',
+    content: 'attr(data-placeholder)',
+    float: 'left',
+    height: '0m',
+    'pointer-events': 'none',
   },
 }
