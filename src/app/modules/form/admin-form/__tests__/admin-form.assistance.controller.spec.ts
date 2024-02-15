ddf77513e3baf252ab20b96fb6267dbe275b0c94
Kathleen Koh <kathleen@open.gov.sg>
2024-02-15 16:25:03 +0800

    chore: finalising tests after resolving merge conflicts with develop

--- src/app/modules/form/admin-form/__tests__/admin-form.assistance.controller.spec.ts
+++ src/app/modules/form/admin-form/__tests__/admin-form.assistance.controller.spec.ts
@@ -2,7 +2,10 @@
 import expressHandler from '__tests__/unit/backend/helpers/jest-express'
 import OpenAI from 'openai'
 
-import { handleGenerateQuestions } from '../admin-form.assistance.controller'
+import {
+  handleGenerateFormFields,
+  handleGenerateQuestions,
+} from '../admin-form.assistance.controller'
 
 // Mock openai
 jest.mock('openai', () => jest.fn())
@@ -81,6 +84,63 @@ describe('admin-form.assistance.controller', () => {
 
       // Act
       await handleGenerateQuestions(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert
+      expect(mockRes.status).toHaveBeenCalledWith(500)
+      expect(mockRes.json).toHaveBeenCalledWith({
+        message: 'Error while connecting to OpenAI',
+      })
+    })
+  })
+
+  describe('handleGenerateFormFields', () => {
+    it('should return 200 when fields are successfully generated', async () => {
+      // Arrange
+      const MOCK_REQ = expressHandler.mockRequest({
+        body: {
+          content: 'mock content',
+        },
+      })
+      const mockRes = expressHandler.mockResponse({
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({
+          result: {
+            value: 'sample value',
+          },
+        }),
+      })
+
+      // Act
+      await handleGenerateFormFields(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert
+      expect(mockRes.status).toHaveBeenCalledWith(200)
+      expect(mockRes.json).toHaveBeenCalledWith(mockReturnValue)
+    })
+
+    it('should return 500 when openai server error occurs', async () => {
+      // Arrange
+      const MOCK_REQ = expressHandler.mockRequest({
+        body: {
+          content: 'mock content',
+        },
+      })
+      const mockRes = expressHandler.mockResponse({
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({
+          result: {
+            value: 'sample value',
+          },
+        }),
+      })
+
+      // Mock OpenAI API throwing an error
+      MockedOpenAIClient.prototype.chat.completions.create = jest
+        .fn()
+        .mockRejectedValue(new Error('Some random error message'))
+
+      // Act
+      await handleGenerateFormFields(MOCK_REQ, mockRes, jest.fn())
 
       // Assert
       expect(mockRes.status).toHaveBeenCalledWith(500)
