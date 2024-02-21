296d3a41e92c683fd4fde1749fdd3c1cff3eef1f
Justyn Oh <justynoh@gmail.com>
2024-02-21 08:46:15 +0800

    chore: rewrite code to better clarify logic

--- frontend/src/features/form/utils/augmentWithWorkflowDisabling.ts
+++ frontend/src/features/form/utils/augmentWithWorkflowDisabling.ts
@@ -5,14 +5,17 @@ import { NON_RESPONSE_FIELD_SET } from '../constants'
 export const isFieldEnabledByWorkflow = (
   workflowStep: FormWorkflowStep | undefined,
   field: FormFieldDto,
-) =>
-  // Field is enabled if:
-  // 1. there is no workflow OR
-  // 2. it is a non-response field OR
-  // 3. (by this point it is workflow + response field) has been explicitly set to be editable in the workflow
-  !workflowStep ||
-  NON_RESPONSE_FIELD_SET.has(field.fieldType) ||
-  workflowStep.edit.includes(field._id)
+) => {
+  // If no workflow, default to enabled
+  if (!workflowStep) return true
+
+  // There is a workflow, but enable if it's a non-response field
+  if (NON_RESPONSE_FIELD_SET.has(field.fieldType)) return true
+
+  // (By this point a workflow exists and it is a response field, so check if it
+  // has been explicitly set to be editable in the workflow
+  return workflowStep.edit.includes(field._id)
+}
 
 export const augmentWithWorkflowDisabling = (
   workflowStep: FormWorkflowStep | undefined,
