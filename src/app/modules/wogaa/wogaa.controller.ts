d47553ab47c797e07247e0f8ee84a31f0f8e40bb
Ken <ken@open.gov.sg>
2024-03-06 13:51:02 +0800

    feat: add wogaa config

--- src/app/modules/wogaa/wogaa.controller.ts
+++ src/app/modules/wogaa/wogaa.controller.ts
@@ -2,21 +2,32 @@ import Axios from 'axios'
 import * as crypto from 'crypto'
 import uuidGen from 'uuid-by-string'
 
+import { wogaaConfig } from '../../config/features/wogaa'
 import { ControllerHandler } from '../core/core.types'
-
-const FORM_SUBMISSION_ENDPOINT =
-  'https://webhook.site/a4d45a44-3a88-49c0-a358-bfd072290aa2'
-const FORM_VIEW_ENDPOINT =
-  'https://webhook.site/a4d45a44-3a88-49c0-a358-bfd072290aa2'
-const FORM_FEEDBACK_ENDPOINT =
-  'https://webhook.site/a4d45a44-3a88-49c0-a358-bfd072290aa2'
 
 const generateSignature = (payload: Record<string, unknown>) => {
   const signature = crypto
-    .createHmac('sha256', 'secret')
+    .createHmac('sha256', wogaaConfig.wogaaSecretKey)
     .update(JSON.stringify(payload))
     .digest('hex')
   return signature
+}
+
+const checkAll = () => {
+  if (!wogaaConfig.wogaaFeedbackEndpoint) {
+    return false
+  }
+  if (!wogaaConfig.wogaaStartEndpoint) {
+    return false
+  }
+  if (!wogaaConfig.wogaaSubmitEndpoint) {
+    return false
+  }
+  if (!wogaaConfig.wogaaFeedbackEndpoint) {
+    return false
+  }
+
+  return true
 }
 export const handleSubmit: ControllerHandler<{ formId: string }> = async (
   req,
@@ -25,12 +36,16 @@ export const handleSubmit: ControllerHandler<{ formId: string }> = async (
 ) => {
   const { formId } = req.params
 
+  if (!req.sessionID || !formId || !checkAll()) {
+    return next()
+  }
+
   const payload = {
     formSgId: formId,
-    transactionId: uuidGen(req.sessionID || ''),
+    transactionId: uuidGen(req.sessionID),
   }
   // fire and forget
-  void Axios.post(FORM_SUBMISSION_ENDPOINT, payload, {
+  void Axios.post(wogaaConfig.wogaaSubmitEndpoint, payload, {
     headers: {
       'WOGAA-Signature': generateSignature(payload),
     },
@@ -48,11 +63,14 @@ export const handleFormView: ControllerHandler<{ formId: string }> = async (
 ) => {
   const { formId } = req.params
 
+  if (!req.sessionID || !formId || !checkAll()) {
+    return next()
+  }
   const payload = {
     formSgId: formId,
-    transactionId: uuidGen(req.sessionID || ''),
+    transactionId: uuidGen(req.sessionID),
   }
-  void Axios.post(FORM_VIEW_ENDPOINT, payload, {
+  void Axios.post(wogaaConfig.wogaaStartEndpoint, payload, {
     headers: {
       'WOGAA-Signature': generateSignature(payload),
     },
@@ -70,14 +88,18 @@ export const handleFormFeedback: ControllerHandler<
 > = async (req, _, next) => {
   const { formId } = req.params
   const { rating, comment } = req.body
+
+  if (!req.sessionID || !formId || !checkAll()) {
+    return next()
+  }
   const payload = {
     formSgId: formId,
-    transactionId: uuidGen(req.sessionID || ''),
+    transactionId: uuidGen(req.sessionID),
     rating,
     comment,
   }
 
-  void Axios.post(FORM_FEEDBACK_ENDPOINT, payload, {
+  void Axios.post(wogaaConfig.wogaaFeedbackEndpoint, payload, {
     headers: {
       'WOGAA-Signature': generateSignature(payload),
     },
