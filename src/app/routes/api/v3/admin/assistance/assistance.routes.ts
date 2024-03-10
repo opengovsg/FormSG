3cea7dbfa66fe12d93ac5e3c2893dd905608d442
Kathleen Koh <kathleen@open.gov.sg>
2024-03-11 01:09:34 +0800

    feat: add api rate limit for azure openai

--- src/app/routes/api/v3/admin/assistance/assistance.routes.ts
+++ src/app/routes/api/v3/admin/assistance/assistance.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { limitRate } from '../../../../../../app/utils/limit-rate'
 import {
   handleGenerateFormFields,
   handleGenerateQuestions,
@@ -7,5 +8,13 @@ import {
 
 export const AssistanceRouter = Router()
 
-AssistanceRouter.post('/questions', handleGenerateQuestions)
-AssistanceRouter.post('/form-fields', handleGenerateFormFields)
+AssistanceRouter.post(
+  '/questions',
+  limitRate({ windowMs: 60 * 1000 * 60, max: 20 }),
+  handleGenerateQuestions,
+)
+AssistanceRouter.post(
+  '/form-fields',
+  limitRate({ windowMs: 60 * 1000 * 60, max: 20 }),
+  handleGenerateFormFields,
+)
