01e85d33cad29e3e7dbb6ded654b8d6cb647d9bf
LeonardYam <yamthesmall@gmail.com>
2024-03-12 21:22:20 +0800

    chore: replace BxsTrashAlt with BxsTrash

---
+++ frontend/src/assets/icons/BxsTrash.tsx
@@ -0,0 +1,12 @@
+export const BxsTrash = (props: React.SVGProps<SVGSVGElement>): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    style={{ fill: 'rgba(0, 0, 0, 1)', transform: ';msFilter:' }}
+  >
+    <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
+    <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
+  </svg>
+)
