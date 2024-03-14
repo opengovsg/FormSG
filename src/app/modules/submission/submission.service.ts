51f538d368a952bbe0479714a15145fd081c48fa
Kathleen Koh <89055608+kathleenkhy@users.noreply.github.com>
2024-03-14 14:07:49 +0800

    chore: file download duration from S3 (#7135)
    * chore: add logs for virus scanning duration
    
    * chore: adding duration logs for virus scan file download
    
    * chore: rename downloadStartTime const
    
    * chore: update downloadEndTime const name

--- src/app/modules/submission/submission.service.ts
+++ src/app/modules/submission/submission.service.ts
@@ -385,6 +385,7 @@ export const downloadCleanFile = (cleanFileKey: string, versionId: string) => {
     })
     .createReadStream()
 
+  const downloadStartTime = Date.now()
   logger.info({
     message: 'File download from S3 has started',
     meta: logMeta,
@@ -399,6 +400,12 @@ export const downloadCleanFile = (cleanFileKey: string, versionId: string) => {
           message: 'Successfully downloaded file from S3',
           meta: logMeta,
         })
+        const downloadEndTime = Date.now()
+        logger.info({
+          message: 'File download from S3 duration',
+          meta: { ...logMeta, time: downloadEndTime - downloadStartTime },
+        })
+
         resolve(buffer)
       })
 
