b0b5bf332dacdc152a519e05b609a425988f8079
Kathleen Koh <kathleen@open.gov.sg>
2024-03-20 17:13:43 +0800

    feat: specifying error message in toast according to status code

--- frontend/src/features/admin-form/assistance/mutations.ts
+++ frontend/src/features/admin-form/assistance/mutations.ts
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom'
 
 import { ContentTypes } from '~shared/types/assistance'
 
+import { HttpError } from '~services/ApiService'
+
 import {
   generateFormFields,
   generateQuestions,
@@ -70,10 +72,23 @@ export const useAssistanceMutations = () => {
         }),
     {
       onError: (error: Error) => {
-        toast({
-          description: 'Too many forms generated! Please try again later.',
-          status: 'danger',
-        })
+        if (error instanceof HttpError) {
+          let errorMessage
+          switch (error.code) {
+            case 429:
+              errorMessage =
+                'Too many forms created! Please try creating again later.'
+
+              break
+
+            default:
+              errorMessage = 'An error occured. Please try again.'
+          }
+          toast({
+            description: `${errorMessage}`,
+            status: 'danger',
+          })
+        }
       },
     },
   )
