5ecd6eae9bba5197ecaf58184eb77a3f2f8fa5ff
Ken <ken@open.gov.sg>
2024-04-18 23:02:07 +0800

    chore: update twilio settings copy

--- frontend/src/features/admin-form/settings/components/TwilioSettingsSection/TwilioSettingsSection.tsx
+++ frontend/src/features/admin-form/settings/components/TwilioSettingsSection/TwilioSettingsSection.tsx
@@ -10,18 +10,18 @@ export const TwilioSettingsSection = (): JSX.Element => {
       <InlineMessage mb="1rem" variant="warning">
         <Text>
           To comply with <strong>SNDGO Circular NO-1-2024</strong>, FormSG will
-          start using Postman to send SMSes to form respondents from{' '}
-          <strong>1 July 2024</strong>.
+          start using gov.sg secured channel to send SMSes to form respondents
+          from <strong>1 July 2024</strong>.
           <UnorderedList spacing="0.5rem" mt="1rem">
             <ListItem>
-              We have removed the 10,000 SMSes per admin limit. Given this
-              change, the capability of adding new Twilio credentials will be
-              disabled from 30 April.
+              There is no longer a limit of 10,000 SMSes per form admin. Given
+              this change, the capability of adding new Twilio credentials will
+              be disabled from 30 April.
             </ListItem>
 
             <ListItem>
-              Existing Twilio credentials will be auto-removed and all SMSes
-              will be sent out via Postman from 30 June.
+              Existing Twilio credentials will automatically be removed and all
+              SMSes will be sent out via gov.sg from 30 June.
             </ListItem>
           </UnorderedList>
         </Text>
