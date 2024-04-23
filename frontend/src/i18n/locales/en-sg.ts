c4500412cf6694b74d5b50ea5d14f7c65d2dd2cb
LoneRifle <LoneRifle@users.noreply.github.com>
2024-04-23 18:16:04 +0800

    feat(i18n): add common strings for translation (#7256)

--- frontend/src/i18n/locales/en-sg.ts
+++ frontend/src/i18n/locales/en-sg.ts
@@ -1,9 +1,11 @@
+import { enSG as common } from './features/common'
 import { enSG as login } from './features/login'
 import Translation from './types'
 
 export const enSG: Translation = {
   translation: {
     features: {
+      common,
       login,
     },
   },
