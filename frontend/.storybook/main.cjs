bf42e8eaf15b45a955200e25211374812e6c62ed
Kar Rui Lau <karrui@open.gov.sg>
2024-04-24 16:25:26 +0800

    fix: update storybook to work with vite

---
+++ frontend/.storybook/main.cjs
@@ -0,0 +1,40 @@
+/* eslint-env node */
+const tsconfigPaths = require('vite-tsconfig-paths')
+const { nodePolyfills } = require('vite-plugin-node-polyfills')
+const { mergeConfig } = require('vite')
+
+const config = {
+  staticDirs: ['../public'],
+  stories: [
+    // Introduction stories set first so stories are ordered correctly.
+    './introduction/Welcome/Welcome.stories.tsx',
+    './introduction/Principles/Principles.stories.tsx',
+    './foundations/**/*.stories.@(mdx|js|jsx|ts|tsx)',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    'storybook-react-i18next',
+    '@storybook/addon-interactions',
+  ],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      plugins: [
+        tsconfigPaths.default(),
+        nodePolyfills({
+          include: ['stream', 'util'],
+        }),
+      ],
+      // manually specify plugins to avoid conflict
+    })
+  },
+}
+
+module.exports = config
