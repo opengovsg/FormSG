3039e5bec8558c31bcb4dcd08eb9994163174c3e
Kar Rui Lau <karrui@open.gov.sg>
2024-04-26 16:33:11 +0800

    fix: revert motionbox back to old implementation
    new implementation doesn't work lol

--- frontend/src/templates/MotionBox/MotionBox.tsx
+++ frontend/src/templates/MotionBox/MotionBox.tsx
@@ -1,9 +1,6 @@
-import { FC } from 'react'
-import { BoxProps, chakra } from '@chakra-ui/react'
-import { HTMLMotionProps, isValidMotionProp, motion } from 'framer-motion'
+import { Box, BoxProps } from '@chakra-ui/react'
+import { HTMLMotionProps, motion } from 'framer-motion'
 import { Merge } from 'type-fest'
 
 export type MotionBoxProps = Merge<BoxProps, HTMLMotionProps<'div'>>
-export const MotionBox: FC<MotionBoxProps> = chakra(motion.div, {
-  shouldForwardProp: isValidMotionProp,
-})
+export const MotionBox = motion(Box)
