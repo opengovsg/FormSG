b6cb42bea5c797c2d51be04bc834fefd797ffe6c
Kar Rui Lau <karrui@open.gov.sg>
2024-04-29 18:57:34 +0800

    fix: add back missing margins

--- frontend/src/pages/Landing/Payments/LandingPaymentsPage.tsx
+++ frontend/src/pages/Landing/Payments/LandingPaymentsPage.tsx
@@ -247,7 +247,7 @@ export const LandingPaymentsPage = (): JSX.Element => {
             >
               Collect payments on your form
             </Text>
-            <SectionBodyText color={mainSectionTextColour}>
+            <SectionBodyText mt="1rem" color={mainSectionTextColour}>
               Respondents can now pay for fees and services directly on your
               form. Enter your agency email to receive our guide on how to get
               started with payments.
