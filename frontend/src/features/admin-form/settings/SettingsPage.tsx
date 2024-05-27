bd94a92ee10156bf8a5314bd2c9926ada6c1322f
g-tejas <code@gtejas.com>
2024-05-27 11:58:27 +0800

    refactor tab rendering logic

--- frontend/src/features/admin-form/settings/SettingsPage.tsx
+++ frontend/src/features/admin-form/settings/SettingsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo } from 'react'
 import {
   BiCodeBlock,
   BiCog,
@@ -29,19 +29,19 @@ import { useUser } from '~features/user/queries'
 import { useAdminFormCollaborators } from '../common/queries'
 
 import { SettingsTab } from './components/SettingsTab'
+import { isEmailOrStorageMode, useAdminFormSettings } from './queries'
 import { SettingsAuthPage } from './SettingsAuthPage'
-import { SettingsEmailPage } from './SettingsEmailPage'
+import { SettingsEmailNotificationsPage } from './SettingsEmailNotificationsPage'
 import { SettingsGeneralPage } from './SettingsGeneralPage'
 import { SettingsPaymentsPage } from './SettingsPaymentsPage'
 import { SettingsTwilioPage } from './SettingsTwilioPage'
 import { SettingsWebhooksPage } from './SettingsWebhooksPage'
 
-const settingsTabsOrder = ['general', 'singpass', 'twilio', 'webhooks']
-
 export const SettingsPage = (): JSX.Element => {
   const { formId, settingsTab } = useParams()
   const { user } = useUser()
   const { data: flags } = useFeatureFlags()
+  const { data: settings } = useAdminFormSettings()
 
   if (!formId) throw new Error('No formId provided')
 
@@ -59,26 +59,50 @@ export const SettingsPage = (): JSX.Element => {
 
   const displayPayments =
     user?.betaFlags?.payment || flags?.has(featureFlags.payment)
-
-  const [tabIndex, setTabIndex] = useState(
-    settingsTabsOrder.indexOf(settingsTab ?? ''),
-  )
+  const displayEmailNotificationPage =
+    settings && isEmailOrStorageMode(settings)
 
   // Note: Admins are not redirected to /general on invalid settings tabs as we
   // don't want to do this prematurely before displayPayments can be determined.
-  useEffect(() => {
+  const tabConfig = useMemo(() => {
+    const baseTabs = [
+      { label: 'General', icon: BiCog, component: SettingsGeneralPage },
+      { label: 'Singpass', icon: BiKey, component: SettingsAuthPage },
+      {
+        label: 'Twilio credentials',
+        icon: BiMessage,
+        component: SettingsTwilioPage,
+      },
+      { label: 'Webhooks', icon: BiCodeBlock, component: SettingsWebhooksPage },
+    ]
+
     if (displayPayments) {
-      // Dynamically push payments tab to settings tab order as needed, in case
-      // there may be multiple hidden tabs in the future.
-      settingsTabsOrder.push('payments')
-      setTabIndex(settingsTabsOrder.indexOf(settingsTab ?? ''))
+      baseTabs.push({
+        label: 'Payments',
+        icon: BiDollar,
+        component: SettingsPaymentsPage,
+      })
     }
-  }, [displayPayments, settingsTab])
+
+    if (displayEmailNotificationPage && !displayPayments) {
+      baseTabs.splice(2, 0, {
+        label: 'Email Notifications',
+        icon: BiMailSend,
+        component: SettingsEmailNotificationsPage,
+      })
+    }
+
+    return baseTabs
+  }, [displayPayments, displayEmailNotificationPage])
+
+  const tabIndex = tabConfig.findIndex(
+    (tab) =>
+      tab.label.toLowerCase() === (settingsTab ?? 'general').toLowerCase(),
+  )
 
   const handleTabChange = (index: number) => {
-    setTabIndex(index)
     navigate(
-      `${ADMINFORM_ROUTE}/${formId}/settings/${settingsTabsOrder[index]}`,
+      `${ADMINFORM_ROUTE}/${formId}/settings/${tabConfig[index].label.toLowerCase()}`,
     )
   }
 
@@ -121,14 +145,9 @@ export const SettingsPage = (): JSX.Element => {
             mr={{ base: '1.5rem', md: '4rem', lg: '2rem' }}
             mb="calc(0.5rem - 2px)"
           >
-            <SettingsTab label="General" icon={BiCog} />
-            <SettingsTab label="Singpass" icon={BiKey} />
-            <SettingsTab label="Email Notifications" icon={BiMailSend} />
-            <SettingsTab label="Twilio credentials" icon={BiMessage} />
-            <SettingsTab label="Webhooks" icon={BiCodeBlock} />
-            {displayPayments && (
-              <SettingsTab label="Payments" icon={BiDollar} />
-            )}
+            {tabConfig.map((tab) => (
+              <SettingsTab key={tab.label} label={tab.label} icon={tab.icon} />
+            ))}
           </TabList>
         </Flex>
         <TabPanels
@@ -136,26 +155,11 @@ export const SettingsPage = (): JSX.Element => {
           // Offset start of tabpanel text from tablist.
           mt={{ md: '1rem' }}
         >
-          <TabPanel>
-            <SettingsGeneralPage />
-          </TabPanel>
-          <TabPanel>
-            <SettingsAuthPage />
-          </TabPanel>
-          <TabPanel>
-            <SettingsEmailPage />
-          </TabPanel>
-          <TabPanel>
-            <SettingsTwilioPage />
-          </TabPanel>
-          <TabPanel>
-            <SettingsWebhooksPage />
-          </TabPanel>
-          {displayPayments && (
-            <TabPanel>
-              <SettingsPaymentsPage />
+          {tabConfig.map((tab) => (
+            <TabPanel key={tab.label}>
+              <tab.component />
             </TabPanel>
-          )}
+          ))}
         </TabPanels>
         <Spacer />
       </Tabs>
