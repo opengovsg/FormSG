698994f8c5be5120554872e66e1694aeb6bc867c
g-tejas <code@gtejas.com>
2024-05-27 11:50:20 +0800

    feat: update validation rules and add helper fns

--- frontend/src/features/admin-form/settings/components/EmailFormSection.tsx
+++ frontend/src/features/admin-form/settings/components/EmailFormSection.tsx
@@ -12,26 +12,34 @@ import isEmail from 'validator/lib/isEmail'
 
 import { GUIDE_FORM_MRF, GUIDE_PREVENT_EMAIL_BOUNCE } from '~constants/links'
 import { useMdComponents } from '~hooks/useMdComponents'
-import { ADMIN_EMAIL_VALIDATION_RULES } from '~utils/formValidation'
+import {
+  OPTIONAL_ADMIN_EMAIL_VALIDATION_RULES,
+  REQUIRED_ADMIN_EMAIL_VALIDATION_RULES,
+} from '~utils/formValidation'
 import FormErrorMessage from '~components/FormControl/FormErrorMessage'
 import FormLabel from '~components/FormControl/FormLabel'
 import { MarkdownText } from '~components/MarkdownText'
 import { TagInput } from '~components/TagInput'
 
 import { useMutateFormSettings } from '../mutations'
+import { isEmailMode, useAdminFormSettings } from '../queries'
 
 interface EmailFormSectionProps {
   emails: string[]
+  isRequired: boolean
 }
 
 export const EmailFormSection = ({
   emails: initialEmails,
+  isRequired: isRequiredEmails,
 }: EmailFormSectionProps): JSX.Element => {
   const initialEmailSet = useMemo(() => new Set(initialEmails), [initialEmails])
   const formMethods = useForm({
     mode: 'onChange',
     defaultValues: { emails: initialEmails },
   })
+
+  const { data: settings } = useAdminFormSettings()
 
   const {
     formState: { errors },
@@ -50,20 +58,31 @@ export const EmailFormSection = ({
 
   useEffect(() => reset({ emails: initialEmails }), [initialEmails, reset])
 
+  const emailModeDescription = `Add at least **2 recipients** to prevent loss of response.`
+  const storageModeDescription = `FormSG securely stores responses in an encrypted format and does not retain any associated emails.`
+
   return (
     <>
       <MRFAdvertisingInfobox />
       <FormProvider {...formMethods}>
         <FormControl isInvalid={!isEmpty(errors)}>
           <FormLabel
-            isRequired
+            isRequired={isRequiredEmails}
             useMarkdownForDescription
-            description={`Add at least **2 recipients** to prevent loss of response. Learn more on [how to guard against email bounces](${GUIDE_PREVENT_EMAIL_BOUNCE}).`}
+            description={
+              (isEmailMode(settings)
+                ? emailModeDescription
+                : storageModeDescription) +
+              ` Learn more on [how to guard against email bounces](${GUIDE_PREVENT_EMAIL_BOUNCE}).`
+            }
           >
-            Emails where responses will be sent
+            Send an email copy of new responses
           </FormLabel>
           <AdminEmailRecipientsInput onSubmit={handleSubmitEmails} />
           <FormErrorMessage>{get(errors, 'emails.message')}</FormErrorMessage>
+          <FormLabel.Description color="neutral.500" mt="0.5rem">
+            Separate multiple email addresses with a comma
+          </FormLabel.Description>
         </FormControl>
       </FormProvider>
     </>
@@ -92,7 +111,10 @@ const AdminEmailRecipientsInput = ({
 }: AdminEmailRecipientsInputProps): JSX.Element => {
   const { getValues, setValue, control, handleSubmit } = useFormContext<{
     emails: string[]
+    isRequired: boolean
   }>()
+
+  const { data: settings } = useAdminFormSettings()
 
   const tagValidation = useMemo(() => isEmail, [])
 
@@ -109,10 +131,16 @@ const AdminEmailRecipientsInput = ({
     <Controller
       control={control}
       name="emails"
-      rules={ADMIN_EMAIL_VALIDATION_RULES}
+      rules={
+        isEmailMode(settings)
+          ? REQUIRED_ADMIN_EMAIL_VALIDATION_RULES
+          : OPTIONAL_ADMIN_EMAIL_VALIDATION_RULES
+      }
       render={({ field }) => (
         <TagInput
-          placeholder="Separate emails with a comma"
+          {...(getValues('emails') && getValues('emails').length > 0
+            ? {}
+            : { placeholder: 'me@example.com' })}
           {...field}
           tagValidation={tagValidation}
           onBlur={handleBlur}
--- frontend/src/features/admin-form/settings/components/FormDetailsSection.tsx
+++ frontend/src/features/admin-form/settings/components/FormDetailsSection.tsx
@@ -3,15 +3,13 @@ import { Controller, useForm } from 'react-hook-form'
 import { FormControl, Skeleton, Stack } from '@chakra-ui/react'
 import { get, isEmpty } from 'lodash'
 
-import { FormResponseMode } from '~shared/types/form/form'
-
 import { FORM_TITLE_VALIDATION_RULES } from '~utils/formValidation'
 import FormErrorMessage from '~components/FormControl/FormErrorMessage'
 import FormLabel from '~components/FormControl/FormLabel'
 import Input from '~components/Input'
 
 import { useMutateFormSettings } from '../mutations'
-import { useAdminFormSettings } from '../queries'
+import { isEmailMode, useAdminFormSettings } from '../queries'
 
 import { EmailFormSection } from './EmailFormSection'
 
@@ -23,8 +21,8 @@ export const FormDetailsSection = (): JSX.Element => {
     <Skeleton isLoaded={!isLoadingSettings && !!settings}>
       <Stack spacing="2rem">
         {settings ? <FormTitleInput initialTitle={settings.title} /> : null}
-        {settings?.responseMode === FormResponseMode.Email ? (
-          <EmailFormSection emails={settings.emails} />
+        {settings && isEmailMode(settings) ? (
+          <EmailFormSection emails={settings.emails} isRequired={true} />
         ) : null}
       </Stack>
     </Skeleton>
