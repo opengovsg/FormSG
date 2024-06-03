47344b0ff12c2d8b9a21504bbeae58901bea4929
g-tejas <code@gtejas.com>
2024-06-03 16:41:34 +0800

    fix: replace deprecated opaque type with tagged

--- shared/types/form/form.ts
+++ shared/types/form/form.ts
@@ -2,7 +2,7 @@ import { PublicUserDto, UserDto } from '../user'
 import { FormField, FormFieldDto, MyInfoChildData } from '../field'
 
 import { FormLogo } from './form_logo'
-import type { Merge, Opaque, PartialDeep } from 'type-fest'
+import type { Merge, Tagged, PartialDeep } from 'type-fest'
 import {
   ADMIN_FORM_META_FIELDS,
   EMAIL_FORM_SETTINGS_FIELDS,
@@ -18,7 +18,7 @@ import { PaymentChannel, PaymentMethodType, PaymentType } from '../payment'
 import { Product } from './product'
 import { FormWorkflow, FormWorkflowDto } from './workflow'
 
-export type FormId = Opaque<string, 'FormId'>
+export type FormId = Tagged<string, 'FormId'>
 
 export enum FormColorTheme {
   Blue = 'blue',
