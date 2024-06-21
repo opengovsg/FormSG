5f45d03d4efbef08af492caa1d711688726be6c1
Kevin Foong <kevin9foong@gmail.com>
2024-06-21 17:27:42 +0800

    feat: add test for isNricMaskDisabled for email submission

--- src/app/modules/submission/email-submission/__tests__/email-submission.controller.spec.ts
+++ src/app/modules/submission/email-submission/__tests__/email-submission.controller.spec.ts
@@ -33,11 +33,33 @@ const MockSgidService = jest.mocked(SgidService)
 const MockMailService = jest.mocked(MailService)
 
 describe('email-submission.controller', () => {
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
-
   describe('maskNric', () => {
+    beforeEach(() => {
+      const MOCK_SUBMISSION_HASH = {
+        hash: 'some hash',
+        salt: 'some salt',
+      }
+      MockEmailSubmissionService.hashSubmission.mockReturnValueOnce(
+        okAsync(MOCK_SUBMISSION_HASH),
+      )
+      MockEmailSubmissionService.saveSubmissionMetadata.mockReturnValueOnce(
+        okAsync({} as IEmailSubmissionSchema),
+      )
+      MockFormService.isFormPublic.mockReturnValueOnce(ok(true))
+
+      MockMailService.sendSubmissionToAdmin.mockReturnValueOnce(okAsync(true))
+
+      MockSubmissionService.validateAttachments.mockReturnValueOnce(
+        okAsync(true),
+      )
+      MockSubmissionService.sendEmailConfirmations.mockReturnValueOnce(
+        okAsync(true),
+      )
+    })
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
     it('should mask nric if form isNricMaskEnabled is true', async () => {
       // Arrange
       const mockFormId = new ObjectId()
@@ -55,39 +77,15 @@ describe('email-submission.controller', () => {
         userName: MOCK_JWT_PAYLOAD_WITH_NRIC.userName,
         rememberMe: false,
       }
-      const MOCK_SUBMISSION_HASH: SubmissionHash = {
-        hash: 'some hash',
-        salt: 'some salt',
-      }
-
       MockFormService.retrieveFullFormById.mockReturnValueOnce(
         okAsync(mockSgidAuthTypeAndNricMaskingEnabledForm),
       )
-
       MockEmailSubmissionService.checkFormIsEmailMode.mockReturnValueOnce(
         ok(mockSgidAuthTypeAndNricMaskingEnabledForm as IPopulatedEmailForm),
       )
-      MockEmailSubmissionService.hashSubmission.mockReturnValueOnce(
-        okAsync(MOCK_SUBMISSION_HASH),
-      )
-      MockEmailSubmissionService.saveSubmissionMetadata.mockReturnValueOnce(
-        okAsync({} as IEmailSubmissionSchema),
-      )
-
-      MockFormService.isFormPublic.mockReturnValueOnce(ok(true))
       MockFormService.checkFormSubmissionLimitAndDeactivateForm.mockReturnValueOnce(
         okAsync(mockSgidAuthTypeAndNricMaskingEnabledForm),
       )
-
-      MockMailService.sendSubmissionToAdmin.mockReturnValueOnce(okAsync(true))
-
-      MockSubmissionService.validateAttachments.mockReturnValueOnce(
-        okAsync(true),
-      )
-      MockSubmissionService.sendEmailConfirmations.mockReturnValueOnce(
-        okAsync(true),
-      )
-
       MockSgidService.extractSgidSingpassJwtPayload.mockReturnValueOnce(
         ok(MOCK_VALID_SGID_PAYLOAD),
       )
@@ -115,6 +113,61 @@ describe('email-submission.controller', () => {
         MockMailService.sendSubmissionToAdmin.mock.calls[0][0].formData[0]
           .answer,
       ).toEqual('*****567A')
+    })
+
+    it('should not mask nric if form isNricMaskEnabled is false', async () => {
+      // Arrange
+      const mockFormId = new ObjectId()
+      const mockSgidAuthTypeAndNricMaskingDisabledForm = {
+        _id: mockFormId,
+        title: 'some form',
+        authType: FormAuthType.SGID,
+        isNricMaskEnabled: false,
+        form_fields: [] as FormFieldSchema[],
+      } as IPopulatedForm
+      const MOCK_JWT_PAYLOAD_WITH_NRIC = {
+        userName: 'S1234567A',
+      }
+      const MOCK_VALID_SGID_PAYLOAD = {
+        userName: MOCK_JWT_PAYLOAD_WITH_NRIC.userName,
+        rememberMe: false,
+      }
+      MockFormService.retrieveFullFormById.mockReturnValueOnce(
+        okAsync(mockSgidAuthTypeAndNricMaskingDisabledForm),
+      )
+      MockEmailSubmissionService.checkFormIsEmailMode.mockReturnValueOnce(
+        ok(mockSgidAuthTypeAndNricMaskingDisabledForm as IPopulatedEmailForm),
+      )
+      MockFormService.checkFormSubmissionLimitAndDeactivateForm.mockReturnValueOnce(
+        okAsync(mockSgidAuthTypeAndNricMaskingDisabledForm),
+      )
+      MockSgidService.extractSgidSingpassJwtPayload.mockReturnValueOnce(
+        ok(MOCK_VALID_SGID_PAYLOAD),
+      )
+
+      const MOCK_REQ = expressHandler.mockRequest({
+        params: { formId: 'some id' },
+        body: {
+          responses: [],
+        },
+      })
+      const mockRes = expressHandler.mockResponse({
+        clearCookie: jest.fn().mockReturnThis(),
+      })
+
+      // Act
+      await submitEmailModeForm(MOCK_REQ, mockRes, jest.fn())
+
+      // Assert email should be sent
+      expect(MockMailService.sendSubmissionToAdmin).toHaveBeenCalledTimes(1)
+      expect(
+        MockSubmissionService.sendEmailConfirmations,
+      ).toHaveBeenCalledTimes(1)
+      // Assert nric is masked
+      expect(
+        MockMailService.sendSubmissionToAdmin.mock.calls[0][0].formData[0]
+          .answer,
+      ).toEqual(MOCK_JWT_PAYLOAD_WITH_NRIC.userName)
     })
   })
 })
