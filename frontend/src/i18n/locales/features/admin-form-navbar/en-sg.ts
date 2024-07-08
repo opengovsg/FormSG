d749b618340a4302e00b78b96548bc212f178844
Xinyi <65388923+helloitsxinyi@users.noreply.github.com>
2024-07-08 22:19:46 +0800

    feat(i18n): replace hardcoded string values for AdminFormNavbar (#7456)
    feat(i18n): add en-sg translations and types for admin-form-navbar, use translations in AdminFormNavbar
    
    Co-authored-by: Ken Lee Shu Ming <ken@open.gov.sg>

---
+++ frontend/src/i18n/locales/features/admin-form-navbar/en-sg.ts
@@ -0,0 +1,18 @@
+import { AdminFormNavbar } from '.'
+
+export const enSG: AdminFormNavbar = {
+  tabs: {
+    create: 'Create',
+    settings: 'Settings',
+    results: 'Results',
+    routing: 'Routing',
+  },
+  manageCollaborators: 'Manage collaborators',
+  previewForm: 'Preview form',
+  formActions: 'Form actions',
+  shareBtn: {
+    text: 'Share',
+    textMobile: 'Share form link',
+    tooltip: 'Share your form link',
+  },
+}
