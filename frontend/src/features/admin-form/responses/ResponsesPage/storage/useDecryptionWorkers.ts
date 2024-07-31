8a65a6d1fdda223ed52c774d8f10a14337b2b121
g-tejas <code@gtejas.com>
2024-07-31 11:25:52 +0800

    fix: remove worker pool limitation since we have convoy algo

--- frontend/src/features/admin-form/responses/ResponsesPage/storage/useDecryptionWorkers.ts
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/useDecryptionWorkers.ts
@@ -113,12 +113,7 @@ const useDecryptionWorkers = ({
 
       if (workers.length) killWorkers(workers)
 
-      // Create a pool of decryption workers
-      // If we are downloading attachments, we restrict the number of threads
-      // to one to limit resource usage on the client's browser.
-      const numWorkers = downloadAttachments
-        ? 1
-        : window.navigator.hardwareConcurrency || 4
+      const numWorkers = window.navigator.hardwareConcurrency || 4
       let errorCount = 0
       let unverifiedCount = 0
       let attachmentErrorCount = 0
