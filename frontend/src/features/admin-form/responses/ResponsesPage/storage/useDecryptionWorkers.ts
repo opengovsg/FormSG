8a4eb23eb9c2ac032b1246b5dd10b41d4d01e1aa
g-tejas <code@gtejas.com>
2024-07-31 11:18:03 +0800

    feat: add feature flag for fasterDownloads and timeout fn

--- frontend/src/features/admin-form/responses/ResponsesPage/storage/useDecryptionWorkers.ts
+++ frontend/src/features/admin-form/responses/ResponsesPage/storage/useDecryptionWorkers.ts
@@ -55,6 +55,19 @@ interface UseDecryptionWorkersProps {
   >
 }
 
+function timeout(
+  ms: number,
+  errorMessage = 'Operation timed out',
+): Promise<never> {
+  return new Promise((_, reject) =>
+    setTimeout(() => reject(new Error(errorMessage)), ms),
+  )
+}
+
+function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
+  return Promise.race([promise, timeout(ms)])
+}
+
 const useDecryptionWorkers = ({
   onProgress,
   mutateProps,
@@ -64,6 +77,8 @@ const useDecryptionWorkers = ({
 
   const { data: adminForm } = useAdminForm()
   const { user } = useUser()
+
+  const fasterDownloads = user?.betaFlags?.fasterDownloads || false
 
   useEffect(() => {
     return () => killWorkers(workers)
@@ -351,8 +366,284 @@ const useDecryptionWorkers = ({
     [adminForm, onProgress, user?._id, workers],
   )
 
+  const downloadEncryptedResponsesFaster = useCallback(
+    async ({
+      responsesCount,
+      downloadAttachments,
+      secretKey,
+      endDate,
+      startDate,
+    }: DownloadEncryptedParams) => {
+      if (!adminForm || !responsesCount) {
+        return Promise.resolve({
+          expectedCount: 0,
+          successCount: 0,
+          errorCount: 0,
+        })
+      }
+
+      abortControllerRef.current.abort()
+      const freshAbortController = new AbortController()
+      abortControllerRef.current = freshAbortController
+
+      if (workers.length) killWorkers(workers)
+
+      const numWorkers = window.navigator.hardwareConcurrency || 4
+      let errorCount = 0
+      let unverifiedCount = 0
+      let attachmentErrorCount = 0
+      let receivedRecordCount = 0
+
+      const logMeta = {
+        action: 'downloadEncryptedReponses',
+        formId: adminForm._id,
+        formTitle: adminForm.title,
+        downloadAttachments: downloadAttachments,
+        num_workers: numWorkers,
+        expectedNumSubmissions: NUM_OF_METADATA_ROWS,
+        adminId: user?._id,
+      }
+      // Trigger analytics here before starting decryption worker
+      trackDownloadResponseStart(adminForm, numWorkers, NUM_OF_METADATA_ROWS)
+      datadogLogs.logger.info('Download response start', {
+        meta: {
+          ...logMeta,
+        },
+      })
+
+      const workerPool: CleanableDecryptionWorkerApi[] = []
+      const idleWorkers: number[] = []
+
+      for (let i = workerPool.length; i < numWorkers; i++) {
+        workerPool.push(makeWorkerApiAndCleanup())
+        idleWorkers.push(i)
+      }
+
+      setWorkers(workerPool)
+
+      const csvGenerator = new EncryptedResponseCsvGenerator(
+        responsesCount,
+        NUM_OF_METADATA_ROWS,
+      )
+
+      const stream = await getEncryptedResponsesStream(
+        adminForm._id,
+        { downloadAttachments, endDate, startDate },
+        freshAbortController,
+      )
+
+      const reader = stream.getReader()
+      let read: (result: ReadableStreamDefaultReadResult<string>) => void
+      const downloadStartTime = performance.now()
+
+      let progress = 0
+      let timeSinceLastXAttachmentDownload = 0
+
+      return new Promise<DownloadResult>((resolve, reject) => {
+        reader
+          .read()
+          .then(
+            (read = async (result) => {
+              if (result.done) return
+              try {
+                // round-robin scheduling
+                const { workerApi } =
+                  workerPool[receivedRecordCount % numWorkers]
+                const decryptResult = await workerApi.decryptIntoCsv({
+                  line: result.value,
+                  secretKey,
+                  downloadAttachments,
+                  formId: adminForm._id,
+                  hostOrigin: window.location.origin,
+                })
+                progress += 1
+                onProgress(progress)
+
+                switch (decryptResult.status) {
+                  case CsvRecordStatus.Error:
+                    errorCount++
+                    break
+                  case CsvRecordStatus.Unverified:
+                    unverifiedCount++
+                    break
+                  case CsvRecordStatus.AttachmentError:
+                    errorCount++
+                    attachmentErrorCount++
+                    break
+                  case CsvRecordStatus.Ok: {
+                    try {
+                      csvGenerator.addRecord(decryptResult.submissionData)
+                      receivedRecordCount++
+                    } catch (e) {
+                      errorCount++
+                      console.error('Error in getResponseInstance', e)
+                    }
+
+                    if (downloadAttachments && decryptResult.downloadBlob) {
+                      // Ensure attachments downloads are spaced out to avoid browser blocking downloads
+                      if (progress % ATTACHMENT_DOWNLOAD_CONVOY_SIZE === 0) {
+                        const now = performance.now()
+                        const elapsedSinceXDownloads =
+                          now - timeSinceLastXAttachmentDownload
+
+                        const waitTime = Math.max(
+                          0,
+                          ATTACHMENT_DOWNLOAD_CONVOY_MINIMUM_SEPARATION_TIME -
+                            elapsedSinceXDownloads,
+                        )
+                        if (waitTime > 0) {
+                          await waitForMs(waitTime)
+                        }
+                        timeSinceLastXAttachmentDownload = now
+                      }
+                      await downloadResponseAttachment(
+                        decryptResult.downloadBlob,
+                        decryptResult.id,
+                      )
+                    }
+                  }
+                }
+              } catch (e) {
+                console.error('Error parsing JSON', e)
+              }
+              // recurse through the stream
+              return reader.read().then(read)
+            }),
+          )
+          .catch((err) => {
+            if (!downloadStartTime) {
+              // No start time, means did not even start http request.
+              datadogLogs.logger.info('Download network failure', {
+                meta: {
+                  ...logMeta,
+                  error: {
+                    message: err.message,
+                    name: err.name,
+                    stack: err.stack,
+                  },
+                },
+              })
+              trackDownloadNetworkFailure(adminForm, err)
+            } else {
+              const downloadFailedTime = performance.now()
+              const timeDifference = downloadFailedTime - downloadStartTime
+
+              datadogLogs.logger.info('Download response failure', {
+                meta: {
+                  ...logMeta,
+                  duration: timeDifference,
+                  error: {
+                    message: err.message,
+                    name: err.name,
+                    stack: err.stack,
+                  },
+                },
+              })
+
+              trackDownloadResponseFailure(
+                adminForm,
+                numWorkers,
+                NUM_OF_METADATA_ROWS,
+                timeDifference,
+                err,
+              )
+            }
+
+            console.error(
+              'Failed to download data, is there a network issue?',
+              err,
+            )
+            killWorkers(workerPool)
+            reject(err)
+          })
+          .finally(() => {
+            const checkComplete = () => {
+              // If all the records could not be decrypted
+              if (errorCount + unverifiedCount === responsesCount) {
+                const failureEndTime = performance.now()
+                const timeDifference = failureEndTime - downloadStartTime
+
+                datadogLogs.logger.info('Partial decryption failure', {
+                  meta: {
+                    ...logMeta,
+                    duration: timeDifference,
+                    error_count: errorCount,
+                    unverified_count: unverifiedCount,
+                    attachment_error_count: attachmentErrorCount,
+                  },
+                })
+
+                trackPartialDecryptionFailure(
+                  adminForm,
+                  numWorkers,
+                  csvGenerator.length(),
+                  timeDifference,
+                  errorCount,
+                  attachmentErrorCount,
+                )
+
+                killWorkers(workerPool)
+                resolve({
+                  expectedCount: responsesCount,
+                  successCount: csvGenerator.length(),
+                  errorCount,
+                  unverifiedCount,
+                })
+              } else if (
+                // All results have been decrypted
+                csvGenerator.length() + errorCount + unverifiedCount >=
+                responsesCount
+              ) {
+                killWorkers(workerPool)
+                // Generate first three rows of meta-data before download
+                csvGenerator.addMetaDataFromSubmission(
+                  errorCount,
+                  unverifiedCount,
+                )
+                csvGenerator.downloadCsv(
+                  `${adminForm.title}-${adminForm._id}.csv`,
+                )
+
+                const downloadEndTime = performance.now()
+                const timeDifference = downloadEndTime - downloadStartTime
+
+                datadogLogs.logger.info('Download response success', {
+                  meta: {
+                    ...logMeta,
+                    duration: timeDifference,
+                  },
+                })
+
+                trackDownloadResponseSuccess(
+                  adminForm,
+                  numWorkers,
+                  NUM_OF_METADATA_ROWS,
+                  timeDifference,
+                )
+
+                resolve({
+                  expectedCount: responsesCount,
+                  successCount: csvGenerator.length(),
+                  errorCount,
+                  unverifiedCount,
+                })
+              } else {
+                setTimeout(checkComplete, 100)
+              }
+            }
+
+            checkComplete()
+          })
+      })
+    },
+    [adminForm, onProgress, user?._id, workers],
+  )
+
   const handleExportCsvMutation = useMutation(
-    (params: DownloadEncryptedParams) => downloadEncryptedResponses(params),
+    (params: DownloadEncryptedParams) =>
+      fasterDownloads
+        ? downloadEncryptedResponsesFaster(params)
+        : downloadEncryptedResponses(params),
     mutateProps,
   )
 
