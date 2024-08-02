c898a2c0a2e6f38beeb54310fe3ca69d657be8e9
Kevin Foong <kevin9foong@gmail.com>
2024-08-02 22:19:29 +0800

    feat: support last row empty to reduce potential support requests

--- frontend/src/utils/parseCsvFileToCsvString.ts
+++ frontend/src/utils/parseCsvFileToCsvString.ts
@@ -20,6 +20,18 @@ export const parseCsvFileToCsvStringWithoutChunking = (
             reject(new Error(invalidReason))
           }
         }
+        const hasLastRow = contentRows.length >= 1
+        if (hasLastRow) {
+          const lastRowCols = contentRows[contentRows.length - 1]
+          const isLastRowEmpty =
+            lastRowCols.length === 0 || lastRowCols.every((col) => col === '')
+          // What: remove last row if empty
+          // Why: On excel and text editors, the last row may be empty but cannot be seen since it is a \n character.
+          // This causes admins to submit empty rows unintentionally for the last row which we will remove.
+          if (isLastRowEmpty) {
+            contentRows.splice(-1, 1)
+          }
+        }
         const csvString = Papa.unparse(contentRows, { delimiter: ',' })
         if (!csvString) {
           reject(new Error('Your CSV file body cannot be empty.'))
