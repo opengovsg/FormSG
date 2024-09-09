4a7f8c3fc85f4596ec0f1c86750551aaf5825fcf
Ken <ken@open.gov.sg>
2024-09-09 10:44:18 +0800

    chore: fix modal title typo

--- frontend/src/features/admin-form/settings/components/SecretKeyActivationModal.tsx
+++ frontend/src/features/admin-form/settings/components/SecretKeyActivationModal.tsx
@@ -29,7 +29,7 @@ export const SecretKeyActivationModal = ({
       onClose={onClose}
       isOpen={isOpen}
       publicKey={publicKey}
-      modalActionText="Active your form"
+      modalActionText="Activate your form"
       submitButtonText="Activate form"
       onSubmit={onSubmit}
       hasAck
