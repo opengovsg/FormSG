bc974cea7110bfcec9ada25c2eea9b537113420b
Ken <ken@open.gov.sg>
2024-09-18 21:40:37 +0800

    revert test code

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -18,9 +18,7 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {
-      builder: { viteConfigPath: './vite.config.ts' },
-    },
+    options: {},
   },
   // For injecitng environment variables into Storybook runs
   // (see: https://github.com/storybookjs/storybook/issues/12270#issuecomment-1139104523)
@@ -28,24 +26,6 @@ const config: StorybookConfig = {
     ...config,
     NODE_ENV: 'test',
   }),
-  async viteFinal(config, { configType }) {
-    const { mergeConfig } = await import('vite')
-
-    if (configType === 'DEVELOPMENT') {
-      // Your development configuration goes here
-    }
-    if (configType === 'PRODUCTION') {
-      // Your production configuration goes here.
-    }
-    return mergeConfig(config, {
-      // Your environment configuration here
-      build: {
-        rollupOptions: {
-          logLevel: 'silent',
-        },
-      },
-    })
-  },
 }
 
 export default config
