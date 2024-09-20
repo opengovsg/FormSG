40717291e751b50acd739a62f237e6a4987f64ba
Ken <ken@open.gov.sg>
2024-09-20 22:35:59 +0800

    refactor: use proxy for more elegance

---
+++ frontend/src/utils/__tests__/datadog.tests.ts
@@ -0,0 +1,49 @@
+describe('datadogRum', () => {
+  describe('DD_RUM is undefined', () => {
+    beforeEach(() => {
+      window.DD_RUM = undefined
+    })
+
+    afterEach(() => {
+      jest.resetModules()
+    })
+
+    it('should return a noop function for addAction', () => {
+      // Arrange
+
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const datadogRum = require('../datadog').datadogRum
+      // Assert
+      expect(window.DD_RUM).not.toBeDefined()
+      expect(datadogRum.addAction).not.toThrow()
+    })
+  })
+
+  describe('DD_RUM is defined', () => {
+    let addActionSpy: jest.Mock
+    beforeEach(() => {
+      addActionSpy = jest.fn()
+      // @ts-expect-error mocking undefined DD_RUM
+      window.DD_RUM = {
+        addAction: addActionSpy,
+      }
+    })
+
+    afterEach(() => {
+      jest.resetModules()
+    })
+
+    it('should call addAction without throwing', () => {
+      // Arrange
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const datadogRum = require('../datadog').datadogRum
+
+      // Assert
+      expect(window.DD_RUM).toBeDefined()
+      expect(datadogRum.addAction).not.toThrow()
+      expect(addActionSpy).toBeCalledTimes(1)
+    })
+  })
+})
+
+export {}
--- frontend/src/utils/datadog.ts
+++ frontend/src/utils/datadog.ts
@@ -6,6 +6,16 @@ import type { RumGlobal } from '@datadog/browser-rum'
  * `datadogRum` imported from `'@datadog/browser-rum'` refers to a different datadog instance as the chunk is loaded separately from the js bundle.
  * Instead, we have to extract the global variable from the window object.
  * */
+const _datadogRum = window.DD_RUM as RumGlobal | undefined
 
-// @ts-expect-error extracting DD_RUM that's initialised from window
-export const datadogRum = window.DD_RUM as RumGlobal
+const noop = () => {}
+const handler = {
+  get: (target: RumGlobal, prop: keyof RumGlobal) => {
+    if (Object.keys(target).length === 0) {
+      return noop
+    }
+    return target[prop]
+  },
+}
+
+export const datadogRum = new Proxy(_datadogRum || {}, handler)
