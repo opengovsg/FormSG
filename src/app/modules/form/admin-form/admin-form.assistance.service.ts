8ab57241bf6daef6467561dd8220ae117b84dbec
Kevin Foong <kevin9foong@gmail.com>
2024-09-24 01:03:48 +0800

    feat: define prompt eng

--- src/app/modules/form/admin-form/admin-form.assistance.service.ts
+++ src/app/modules/form/admin-form/admin-form.assistance.service.ts
@@ -5,12 +5,17 @@ const generateFormCreationPrompt = (userPrompt: string) => {
     {
       role: Role.System,
       content:
+        // Provide context to model on when to use each field type
         'You will make a form for the purpose described in the next prompt.' +
-        'Your next response must be an array of objects with property fieldName and fieldType.' +
-        'The fieldTypes supported must be one of the following [short text, long text, date, checkbox, radio]' +
-        'There must be at most 5 form fields in the form.',
+        'Your next response must be an ordered array of objects with compulsory properties named "fieldName" and "fieldType" and an optional property named "description"' +
+        'The field types supported must be one of the following "short_answer", "long_answer", "radio", "checkbox", "dropdown", "country/region", "heading", "paragraph", "yes/no", "rating", "email", "mobile_number", "home_number", "date", "attachment", "number", "decimal", "nric"]' +
+        '"heading" and "paragraph" field types are used to organise and provide information to the respondent about the form and not meant to collect data. It is encouraged to use "heading" to organise the form fields into sections.' +
+        '"radio" and "checkbox" field types must have an additional property named "options" that is an array of strings for the respondent to select from.' +
+        '"nric" is used to collect the unique identity number issued to each respondent, it can be used to uniquely identify of the respondent.' +
+        '"yes/no" is used to collect a boolean response, for example, whether the respondent approves to something or agrees to a paragraph field above.',
     },
     {
+      // Provide general topic + example fields that user wants to collect.
       role: Role.User,
       content: `Create a form that collects ${userPrompt}`,
     },
