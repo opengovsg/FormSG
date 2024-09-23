2f917e683fad218264aa60a5ab39cb914bd5e792
Antariksh Mahajan <antarikshmahajan@gmail.com>
2024-09-23 16:19:57 +0800

    feat: update iframe parent on form submission

---
+++ frontend/src/features/public-form/utils/iframeMessaging.ts
@@ -0,0 +1,23 @@
+export type PublicFormIFrameMessage =
+  | { state: 'submitting' | 'submitError' }
+  | { state: 'submitted'; submissionId: string }
+
+const TRUSTED_TARGET_ORIGINS = [
+  'https://pay.gov.sg',
+  'https://exp.pay.gov.sg',
+  'https://staging.pay.gov.sg',
+].concat(
+  process.env.NODE_ENV === 'development' ? ['http://localhost:3000'] : [],
+)
+
+export const postIFrameMessage = (message: PublicFormIFrameMessage): void => {
+  // De-risk by wrapping in try-catch even though this is synchronous. This should
+  // never block form submission.
+  try {
+    TRUSTED_TARGET_ORIGINS.forEach((origin) => {
+      window.parent.postMessage(message, origin)
+    })
+  } catch (error) {
+    console.warn('Error while posting form state to iframe parent', error)
+  }
+}
