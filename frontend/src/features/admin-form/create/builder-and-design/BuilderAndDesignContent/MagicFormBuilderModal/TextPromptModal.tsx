0e02d4b84e556a4e4c3fa5e6c3b1dad5f25c4e88
Kevin Foong <kevin9foong@gmail.com>
2024-09-24 23:56:07 +0800

    feat: add prompt ideas selector

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignContent/MagicFormBuilderModal/TextPromptModal.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignContent/MagicFormBuilderModal/TextPromptModal.tsx
@@ -20,7 +20,16 @@ import Textarea from '~components/Textarea'
 
 import { useAssistanceMutations } from '~features/admin-form/assistance/mutations'
 
-const TEXT_PROMPT_PLACEHOLDER = 'This is a placeholder for text prompt'
+import { TextPromptIdeasSelector } from './TextPromptIdeasSelector'
+
+const TEXT_PROMPT_PLACEHOLDER =
+  'A good prompt includes the purpose of the form, who is responding to the form and what you wish to collect (such as specific questions or sections to include).'
+
+const TEXT_PROMPT_IDEAS = [
+  'employee feedback on workplace satisfaction, including fields on overall job satisfaction, suggestions for improvement, and comments on company culture',
+  'community issue reports from citizens, including fields for location, description of the issue, and optional photo uploads"',
+  'applications for government grants from business entities, incorporating sections for project details, budget breakdown, and applicant qualifications.',
+]
 
 interface TextPromptInputs {
   prompt: string
@@ -36,6 +45,7 @@ export const TextPromptModal = ({
     reset,
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<TextPromptInputs>()
 
@@ -83,6 +93,10 @@ export const TextPromptModal = ({
                   />
                   <FormErrorMessage>{errors.prompt?.message}</FormErrorMessage>
                 </FormControl>
+                <TextPromptIdeasSelector
+                  promptIdeas={TEXT_PROMPT_IDEAS}
+                  handleIdeaClick={(idea) => setValue('prompt', idea)}
+                />
                 <Button
                   rightIcon={<BiRightArrowAlt fontSize="1.5rem" />}
                   type="submit"
