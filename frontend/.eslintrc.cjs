dd79e78aabad5106c88cd9fc278984a53693fd3a
Ken <ken@open.gov.sg>
2024-10-15 16:41:26 +0800

    fix: update eslint rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -23,6 +23,10 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       rules: {
         '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          { allowShortCircuit: true, allowTernary: true },
+        ],
       },
     },
     {
