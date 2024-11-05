1355768931ca67471fb2d8e830c778ae2fa9e0c5
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2024-11-05 16:02:37 +0800

    fix: handle string response in DTO for date validation object

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -45,7 +45,15 @@ export const normalizeDateToUtc = (date: Date | null) => {
   return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
 }
 
-export const loadDateFromNormalizedDate = (date: Date | null) => {
+export const loadDateFromNormalizedDate = (date: string | Date | null) => {
+  if (typeof date === 'string') {
+    const parsedDate = parseISO(date)
+    return new Date(
+      parsedDate.getUTCFullYear(),
+      parsedDate.getUTCMonth(),
+      parsedDate.getUTCDate(),
+    )
+  }
   if (!date) return date
   return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
 }
