bd4ac8edbd206798b56dc5cde66e72f94581e8ee
Siddarth Nandanahosur Suresh <48427064+siddarth2824@users.noreply.github.com>
2024-11-27 21:14:00 +0800

    feat: add view for translation input (#7895)
    * feat: add view for translation input
    
    * fix: refactor code and remove console log statements
    
    * chore: clean up code
    
    * chore: fix alignment of translation input and header
    
    * chore: address comments

--- frontend/src/features/admin-form/settings/SettingsMultiLangPage.tsx
+++ frontend/src/features/admin-form/settings/SettingsMultiLangPage.tsx
@@ -1,15 +1,38 @@
 import { useSearchParams } from 'react-router-dom'
+import _ from 'lodash'
+
+import { TRANSLATION_INPUT, UNICODE_LOCALE } from '~constants/routes'
 
 import { MultiLanguageSection } from './components/MultiLanguageSection/MultiLanguageSection'
 import { TranslationListSection } from './components/MultiLanguageSection/TranslationListSection'
+import { TranslationSection } from './components/MultiLanguageSection/TranslationSection'
 
 export const SettingsMultiLangPage = (): JSX.Element => {
   const [searchParams] = useSearchParams()
-  const unicodeLocale = searchParams.get('unicodeLocale')
+  const unicodeLocale = searchParams.get(UNICODE_LOCALE)
+  const translationInput = searchParams.get(TRANSLATION_INPUT)
+  const isTranslationInput = !_.isNull(translationInput)
+  const isEndPageTranslationInput = translationInput === 'endPage'
+  const isStartPageTransltionInput = translationInput === 'startPage'
 
+  // Request user to select a language
+  if (!unicodeLocale) {
+    return <MultiLanguageSection />
+  }
+  // Request user to select fields to translate
+  if (!isTranslationInput) {
+    return <TranslationListSection language={unicodeLocale} />
+  }
   return (
-    <>
-      {unicodeLocale ? <TranslationListSection language={unicodeLocale} /> : <MultiLanguageSection />}
-    </>
+    <TranslationSection
+      language={unicodeLocale}
+      formFieldNumToBeTranslated={
+        isEndPageTranslationInput || isStartPageTransltionInput
+          ? -1
+          : _.toNumber(translationInput)
+      }
+      isEndPageTranslations={isEndPageTranslationInput}
+      isStartPageTranslations={isStartPageTransltionInput}
+    />
   )
 }
---
+++ frontend/src/features/admin-form/settings/components/MultiLanguageSection/TranslationContainer.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useFormContext } from 'react-hook-form'
+import { Flex, FormControl, Text } from '@chakra-ui/react'
+
+import Textarea from '~components/Textarea'
+
+import { TranslationInput } from './TranslationSection'
+
+interface TranslationContainerProps {
+  language: string
+  defaultString: string | undefined
+  editingTranslation: keyof TranslationInput
+  previousTranslation?: string
+}
+
+export const TranslationContainer = ({
+  language,
+  defaultString,
+  editingTranslation,
+  previousTranslation,
+}: TranslationContainerProps) => {
+  const { register } = useFormContext<TranslationInput>()
+
+  return (
+    <Flex direction="column" width="100%">
+      <Flex alignItems="flex-start" mb="2rem">
+        <Text
+          color="secondary.700"
+          fontWeight="400"
+          mr="7.5rem"
+          width="6.25rem"
+        >
+          Default
+        </Text>
+        <Textarea
+          placeholder={defaultString}
+          width="100%"
+          isDisabled={true}
+          resize="none"
+        />
+      </Flex>
+      <Flex alignItems="flex-start">
+        <Text color="secondary.700" mr="7.5rem" width="6.25rem">
+          {language}
+        </Text>
+        <FormControl>
+          <Textarea
+            defaultValue={previousTranslation}
+            {...register(editingTranslation)}
+          />
+        </FormControl>
+      </Flex>
+    </Flex>
+  )
+}
