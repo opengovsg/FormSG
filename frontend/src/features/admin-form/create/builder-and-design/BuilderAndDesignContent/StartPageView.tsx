b7808aa44d191fc368ebcd83106a7879c3bf5efb
Siddarth <siddarth2824@gmail.com>
2024-12-05 15:54:32 +0700

    chore: fix code

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignContent/StartPageView.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignContent/StartPageView.tsx
@@ -240,6 +240,7 @@ export const StartPageView = () => {
                   <FormInstructions
                     content={startPage?.paragraph}
                     colorTheme={startPage?.colorTheme}
+                    title="Instructions"
                   />
                 </Box>
                 {isMobile ? (
