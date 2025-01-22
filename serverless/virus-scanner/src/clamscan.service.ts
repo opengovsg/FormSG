8d3642424cfbb4fd7ad837cf78cacb52a9eb7ef8
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-01-22 15:14:10 +0800

    feat: add retry for clamav init

--- serverless/virus-scanner/src/clamscan.service.ts
+++ serverless/virus-scanner/src/clamscan.service.ts
@@ -4,6 +4,33 @@ import internal from 'stream'
 import { getLambdaLogger } from './logger'
 import { ScanFileStreamResult } from './types'
 
+const MAX_RETRIES = 3
+const RETRY_INTERVAL_MS = 1000
+
+const retryFunction = <T>(
+  func: () => Promise<T>,
+  retries: number,
+  interval: number,
+): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    const attempt = () => {
+      func()
+        .then(resolve)
+        .catch((error) => {
+          if (retries === 0) {
+            reject(error)
+          } else {
+            setTimeout(() => {
+              retries--
+              attempt()
+            }, interval)
+          }
+        })
+    }
+    attempt()
+  })
+}
+
 export async function scanFileStream(
   s3Stream: internal.Readable,
 ): Promise<ScanFileStreamResult> {
@@ -11,11 +38,11 @@ export async function scanFileStream(
 
   logger.info('Scanning file stream')
 
-  const scanner = await new NodeClam().init({
-    clamdscan: {
-      socket: '/tmp/clamd.ctl',
-    },
-  })
+  const scanner = await retryFunction<NodeClam>(
+    () => new NodeClam().init({ clamdscan: { socket: '/tmp/clamd.ctl' } }),
+    MAX_RETRIES,
+    RETRY_INTERVAL_MS,
+  )
 
   const { isInfected: isMalicious, viruses: virusMetadata } =
     await scanner.scanStream(s3Stream)
