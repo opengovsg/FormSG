7fe1076b27d2cd2388e203fb030d835f7646fce5
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-02-05 16:19:25 +0800

    feat: remove ai button and unused forwardRef

--- frontend/src/features/admin-form/create/builder-and-design/MagicFormBuilder/components/MagicFormBuilderSmallButton.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/MagicFormBuilder/components/MagicFormBuilderSmallButton.tsx
@@ -1,44 +1,31 @@
-import { forwardRef } from 'react'
 import { BiSolidMagicWand } from 'react-icons/bi'
-import { Box, Button, Flex, Icon, Text, Tooltip } from '@chakra-ui/react'
+import { Button, Icon, Tooltip } from '@chakra-ui/react'
 
-const MagicFormBuilderSmallButton = forwardRef(
-  (
-    {
-      isActive,
-      onClick,
-      ...styleProps
-    }: { isActive: boolean; onClick: () => void } & React.ComponentProps<
-      typeof Button
-    >,
-    ref,
-  ) => {
-    return (
-      <Tooltip openDelay={500} hasArrow label="Create fields with AI">
-        <Button
-          flexDirection="row"
-          justifyContent="center"
-          alignItems="center"
-          ref={ref} // Rationale: forward ref allows the popover placement to work.
-          variant="outline"
-          onClick={onClick}
-          padding="0"
-          borderColor="primary.200"
-          backgroundColor={isActive ? 'primary.200' : undefined}
-          _hover={{
-            backgroundColor: 'primary.200',
-          }}
-          borderWidth="1px"
-          {...styleProps}
-        >
-          <Icon as={BiSolidMagicWand} color="primary.500" fontSize="1.5rem" />
-          <Text mr="0.25rem" fontSize="0.75rem">
-            AI
-          </Text>
-        </Button>
-      </Tooltip>
-    )
-  },
-)
+const MagicFormBuilderSmallButton = ({
+  isActive,
+  onClick,
+  ...styleProps
+}: { isActive: boolean; onClick: () => void } & React.ComponentProps<
+  typeof Button
+>) => {
+  return (
+    <Tooltip openDelay={500} hasArrow label="Create fields with AI">
+      <Button
+        variant="outline"
+        onClick={onClick}
+        padding="0"
+        borderColor="primary.200"
+        backgroundColor={isActive ? 'primary.200' : undefined}
+        _hover={{
+          backgroundColor: 'primary.200',
+        }}
+        borderWidth="1px"
+        {...styleProps}
+      >
+        <Icon as={BiSolidMagicWand} color="primary.500" fontSize="1.5rem" />
+      </Button>
+    </Tooltip>
+  )
+}
 
 export default MagicFormBuilderSmallButton
