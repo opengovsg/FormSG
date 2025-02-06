e0bb2871e7a22d636c40e344f1b94f31564dcc31
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-02-06 12:24:43 +0800

    Fix typo in error message in useMagicFormBuilder hook
    Co-authored-by: Ken Lee Shu Ming <ken@open.gov.sg>

--- frontend/src/features/admin-form/create/builder-and-design/MagicFormBuilder/useMagicFormBuilder.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/MagicFormBuilder/useMagicFormBuilder.tsx
@@ -9,7 +9,7 @@ export const useMagicFormBuilder = (): MagicFormBuilderProviderProps => {
   const context = useContext(MagicFormBuilderContext)
   if (!context) {
     throw new Error(
-      `useUnlockedResponsesContext must be used within a UnlockedResponsesProvider component`,
+      `useMagicFormBuilder must be used within a MagicFormBuilderProvider component`,
     )
   }
   return context
