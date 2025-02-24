a225acde6ca49a21ae8bbaf155595f513376bc4b
scottheng96 <scottdavidheng@gmail.com>
2025-02-24 09:55:25 +0800

    updated envLoader

--- serverless/virus-scanner-guardduty/scripts/envLoader.mjs
+++ serverless/virus-scanner-guardduty/scripts/envLoader.mjs
@@ -38,7 +38,7 @@ async function saveAllParameters() {
     exit(0)
   }
   const client = new SSMClient({ region: 'ap-southeast-1' })
-  const parameterName = `/virus-scanner/${SHORT_ENV_MAP[process.env.ENV]}`
+  const parameterName = `/virus-scanner-guardduty/${SHORT_ENV_MAP[process.env.ENV]}`
 
   const res = await client.send(
     new GetParameterCommand({
