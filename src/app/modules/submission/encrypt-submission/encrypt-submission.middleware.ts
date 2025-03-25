e683ba9dcdb5293fb6ce44dc064535fb583792b7
scottheng96 <44297674+scottheng96@users.noreply.github.com>
2025-03-25 09:20:14 +0800

    fix: update clean bucket download and mrf flow of invoking guardduty lambda (#8240)
    * specify bucket to downloadCleanfile
    
    * included feature flag logic to mrf
    
    * resolve backend lint
    
    * fixed error messages for encrypt and multirespondent guardduty failings

--- src/app/modules/submission/encrypt-submission/encrypt-submission.middleware.ts
+++ src/app/modules/submission/encrypt-submission/encrypt-submission.middleware.ts
@@ -183,11 +183,12 @@ const asyncVirusScanning = (
           response,
           formId,
         ).mapErr((err) =>
-          logger.info({
-            message: `GuardDuty Scan unsuccessful:, ${err}`,
+          logger.error({
+            message: `GuardDuty Scan unsuccessful`,
             meta: {
               action: 'TriggerGuarddutyScanThenDownloadCleanFileChain',
             },
+            error: err,
           }),
         )
       }
