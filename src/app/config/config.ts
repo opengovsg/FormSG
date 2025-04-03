b49210ff06b57fcbb3dcb964e904f1c57b9908b1
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-04-03 15:36:26 +0800

    feat: enforce s3 path style

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -88,10 +88,13 @@ const hasR2Buckets = Object.values(s3BucketUrlVars).some((url) =>
 const s3 = new aws.S3({
   region: basicVars.awsConfig.region,
   // Unset and use default if not in development mode
-  // Endpoint and path style overrides are needed only in development mode
+  // Endpoint override is needed only in development mode
   // for localstack to work, or for Cloudflare R2.
   endpoint: isDevOrTest || hasR2Buckets ? s3BucketUrlVars.endPoint : undefined,
-  s3ForcePathStyle: isDevOrTest || hasR2Buckets ? true : undefined,
+  // RATIONALE: For new buckets, AWS S3 returns virtual-hosted style urls by default.
+  // However, due to backwards compatibility, some of our existing buckets and our CSP connect-url config use path style.
+  // This param ensures that the signed url uses path style.
+  s3ForcePathStyle: true,
 })
 
 // using aws-sdk v3 (FRM-993)
