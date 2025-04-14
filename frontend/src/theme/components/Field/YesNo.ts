25002618e5fd93b8d2163a1f1045734a348f3b11
g-tejas <tejas@open.gov.sg>
2025-04-14 20:40:44 +0800

    fix typo in YesNo field

--- frontend/src/theme/components/Field/YesNo.ts
+++ frontend/src/theme/components/Field/YesNo.ts
@@ -33,7 +33,7 @@ const outlineOptionStyle: SystemStyleFunction = (props) => {
     _disabled: {
       bg: 'neutral.200',
       cursor: 'not-allowed',
-      color: props.highContrast ? 'neutral.800' : 'secondary.500',
+      color: props.highContrast ? 'neutral.800' : 'neutral.500',
       _active: {
         boxShadow: 'none',
         borderColor: 'neutral.500',
