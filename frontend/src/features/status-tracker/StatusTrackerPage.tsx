f273f8c05b46e18dcd56272272aed28536934124
scottheng96 <scottdavidheng@gmail.com>
2025-05-15 12:25:53 +0800

    updated base skeleton for StatusTrackerPage

--- frontend/src/features/status-tracker/StatusTrackerPage.tsx
+++ frontend/src/features/status-tracker/StatusTrackerPage.tsx
@@ -1,7 +1,25 @@
 import { useParams } from 'react-router-dom'
-import { Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, GridItem, Text } from '@chakra-ui/react'
 
 import { useStatusTracker } from './queries'
+import {
+  BackgroundBox,
+  BaseGridLayout,
+  LoginGridArea,
+  NonMobileSidebarGridArea,
+} from '~features/login/LoginPageTemplate'
+
+// // Grid area styling for the login form.
+// export const StatusTrackerGridArea: FCC = ({ children }) => (
+//   <GridItem
+//     gridColumn={{ base: '1 / 5', md: '2 / 12', lg: '7 / 12' }}
+//     py="4rem"
+//     display="flex"
+//     alignItems={{ base: 'initial', lg: 'center' }}
+//     justifyContent="center"
+//     children={children}
+//   />
+// )
 
 export const StatusTrackerPage = (): JSX.Element => {
   const { formId, submissionId } = useParams()
@@ -14,12 +32,24 @@ export const StatusTrackerPage = (): JSX.Element => {
   if (error || !data) return <Text>"Something went wrong"</Text>
 
   return (
-    <Flex direction="column">
-      <Text>Hello World!</Text>
-      <Text>{formId}</Text>
-      <Text>{submissionId}</Text>
-      <Text>{JSON.stringify(data.submittedSteps)}</Text>
-      <Text>{JSON.stringify(data.workflow)}</Text>
-    </Flex>
+    <BackgroundBox>
+      <BaseGridLayout flex={1}>
+        <NonMobileSidebarGridArea
+          maxW="100%"
+          aria-hidden
+        ></NonMobileSidebarGridArea>
+        <LoginGridArea>
+          <Box>
+            <Flex direction="column">
+              <Text>Hello World!</Text>
+              <Text>{formId}</Text>
+              <Text>{submissionId}</Text>
+              <Text>{JSON.stringify(data.submittedSteps)}</Text>
+              <Text>{JSON.stringify(data.workflow)}</Text>
+            </Flex>
+          </Box>
+        </LoginGridArea>
+      </BaseGridLayout>
+    </BackgroundBox>
   )
 }
