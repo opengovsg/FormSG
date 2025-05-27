153b5d4be76ef2dc37796304341979b234c738fd
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-05-27 16:55:27 +0800

    feat: add error custom metrics

--- src/app/modules/datadog/datadog.utils.ts
+++ src/app/modules/datadog/datadog.utils.ts
@@ -21,10 +21,20 @@ export const setFormTags = (form: IPopulatedForm) => {
  */
 export const setErrorCode = (error: ApplicationError) => {
   const span = tracer.scope().active()
+
   if (span && error.code) {
-    span.setTag('status', 'error') // RATIONALE: ensures all errors are indexed by DD default error retention filter. This allows for Trace analytic monitors to work on error codes set below.
     span.setTag('span.error.type', error.code)
     span.setTag('span.error.message', `[${error.code}] ${error.message}`)
     if (error.stack) span.setTag('span.error.stack', `${error.stack}`)
   }
+}
+
+/**
+ * Submits an error count metric to Datadog. Used for monitoring errors and dashboard visualizations.
+ * @param errorCode The error code to submit the metric for
+ */
+export const submitErrorCountMetric = (errorCode: number) => {
+  tracer.dogstatsd.increment('error.count', 1, {
+    code: errorCode,
+  })
 }
