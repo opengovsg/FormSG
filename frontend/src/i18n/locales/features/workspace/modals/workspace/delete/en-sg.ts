b9cc900dd368fa05528ef3d55b1d5933d9fca77c
LoneRifle <LoneRifle@users.noreply.github.com>
2025-06-03 13:04:51 +0800

    fix(i18n): form(s) -> forms for plural

--- frontend/src/i18n/locales/features/workspace/modals/workspace/delete/en-sg.ts
+++ frontend/src/i18n/locales/features/workspace/modals/workspace/delete/en-sg.ts
@@ -4,7 +4,7 @@ export const enSG: DeleteWorkspaceModal = {
   title: 'Delete folder',
   confirmation: {
     removeForms:
-      'Remove {formsInActiveWorkspace} form{formsInActiveWorkspace, plural, =1 {} other {(s)}} from {activeWorkspaceTitle} and delete the folder? This action cannot be undone',
+      'Remove {formsInActiveWorkspace} form{formsInActiveWorkspace, plural, =1 {} other {s}} from {activeWorkspaceTitle} and delete the folder? This action cannot be undone',
     removeWorkspace:
       'Are you sure you want to delete this folder? This action cannot be undone.',
   },
