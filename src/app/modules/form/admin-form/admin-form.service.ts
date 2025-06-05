b5347ab881ef20eee71726efc3371e7dbb52a14b
scottheng96 <scottdavidheng@gmail.com>
2025-06-05 11:47:21 +0800

    updated copy change with contact support addition

--- src/app/modules/form/admin-form/admin-form.service.ts
+++ src/app/modules/form/admin-form/admin-form.service.ts
@@ -1873,7 +1873,7 @@ export const updateFormSettings = (
     ) {
       return errAsync(
         new MalformedParametersError(
-          'Please refresh the page (Ctrl+Shift+R) to convert your form to storage mode and reopen it',
+          'Please refresh your browser (Ctrl+Shift+R) to convert your form to storage mode before opening it to responses. If you encounter issues, please contact FormSG.',
         ),
       )
     }
