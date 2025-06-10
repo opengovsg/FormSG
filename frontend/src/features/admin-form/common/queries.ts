3e4c9575c2a4d1b7e685b31986c5401810f22aa8
scottheng96 <scottdavidheng@gmail.com>
2025-06-10 17:45:34 +0800

    add useSmsQuota to queries

--- frontend/src/features/admin-form/common/queries.ts
+++ frontend/src/features/admin-form/common/queries.ts
@@ -29,6 +29,7 @@ export const adminFormKeys = {
     [...adminFormKeys.id(id), 'viewFormTemplate'] as const,
   products: (id: string, products: Product[]) =>
     [...adminFormKeys.id(id), 'products', ...products] as const,
+  smsCount: (id: string) => [...adminFormKeys.id(id), 'smsCount'] as const,
 }
 
 /**
@@ -142,4 +143,13 @@ export const useFormTemplate = (
       enabled: MONGODB_ID_REGEX.test(formId) && enabled,
     },
   )
+}
+
+export const useSmsQuota = () => {
+  const { formId } = useParams()
+  if (!formId) throw new Error('No formId provided to useFreeSmsQuota')
+
+  return useQuery(adminFormKeys.smsCount(formId), () => getSmsQuota(formId), {
+    staleTime: 0,
+  })
 }
