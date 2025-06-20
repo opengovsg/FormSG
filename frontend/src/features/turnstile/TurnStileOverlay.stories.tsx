0423004176f1d47cfa04dab747fcc970e4a2c506
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-06-20 17:10:33 +0800

    feat: add env turnstile key

--- frontend/src/features/turnstile/TurnStileOverlay.stories.tsx
+++ frontend/src/features/turnstile/TurnStileOverlay.stories.tsx
@@ -1,12 +1,36 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Meta, StoryObj } from '@storybook/react'
+import { rest } from 'msw'
 
 import TurnstileOverlay from './TurnstileOverlay'
+
+const queryClient = new QueryClient()
+
+const DUMMY_INTERACTIVE_TURNSTILE_SITE_KEY = '3x00000000000000000000FF'
 
 const meta = {
   title: 'Features/TurnstileOverlay',
   component: TurnstileOverlay,
+  decorators: [
+    (Story) => (
+      // @ts-expect-error missing FC type in old version
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
   parameters: {
     layout: 'fullscreen',
+    msw: [
+      rest.get('/api/v3/client/env', (_req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            turnstileSiteKey: DUMMY_INTERACTIVE_TURNSTILE_SITE_KEY,
+          }),
+        )
+      }),
+    ],
   },
 } satisfies Meta<typeof TurnstileOverlay>
 
