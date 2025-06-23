1eaa8385b051cbd4f2fe9cd6f117426ebeae2af7
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-06-23 13:18:47 +0800

    feat: add ts ignore

--- frontend/src/features/turnstile/TurnstileChallengeService.ts
+++ frontend/src/features/turnstile/TurnstileChallengeService.ts
@@ -4,7 +4,9 @@ import { TurnstileOverlayHandlingProps } from './TurnstileOverlay'
 
 const CloudflareChallengeService = {
   issueChallenge: async (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     requestToReplay: AxiosRequestConfig | null,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     turnstileOverlayHandlingProps: TurnstileOverlayHandlingProps,
   ) => {
     console.warn('issueChallenge called before initialization')
