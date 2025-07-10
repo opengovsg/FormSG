10ffcc6fc6a2eba1d1ab6c1146d90b7ffdfd7858
Eliot Lim <eliotlim@users.noreply.github.com>
2025-07-10 13:51:22 +0800

    feat: support intranet cidr entries (#8536)
    * feat: support intranet cidr entries
    
    * fix: add ipv6 test case

--- src/app/modules/intranet/intranet.service.ts
+++ src/app/modules/intranet/intranet.service.ts
@@ -1,3 +1,5 @@
+import * as ipAddress from 'ip-address'
+
 import {
   IIntranet,
   intranetConfig,
@@ -14,7 +16,7 @@ class IntranetServiceClass {
   /**
    * List of IP addresses associated with intranet
    */
-  intranetIps: string[]
+  intranetIps: (ipAddress.Address4 | ipAddress.Address6)[]
 
   constructor(intranetConfig: IIntranet) {
     // In future if crucial intranet-specific functionality is implemented,
@@ -27,10 +29,29 @@ class IntranetServiceClass {
       return
     }
     try {
-      this.intranetIps = retrieveFileContent({
+      const intranetIpList = retrieveFileContent({
         preIacFilePath: intranetConfig.intranetIpListPath,
         postIacBase64EncodedString: intranetConfig.intranetIpList,
-      }).split('\n')
+      })
+        .split('\n')
+        .filter((line) => !line.startsWith('#') && line.trim() !== '')
+
+      this.intranetIps = intranetIpList
+        .map((ip) => {
+          const parsedIp = IntranetServiceClass.parseIp(ip.trim())
+          if (!parsedIp) {
+            logger.warn({
+              message: `Invalid IP address in intranet IP list: ${ip}`,
+              meta: {
+                action: 'IntranetService',
+              },
+            })
+          }
+          return parsedIp
+        })
+        .filter(
+          (ip): ip is ipAddress.Address4 | ipAddress.Address6 => ip !== null,
+        )
     } catch {
       logger.warn({
         message: 'Could not read file containing intranet IPs',
@@ -48,7 +69,32 @@ class IntranetServiceClass {
    * @returns Whether the IP address originated from the intranet
    */
   isIntranetIp(ip: string): boolean {
-    return this.intranetIps.includes(ip)
+    const parsedIp = IntranetServiceClass.parseIp(ip)
+    if (!parsedIp) {
+      return false
+    }
+
+    const intranetIpMatches = this.intranetIps.map((intranetIp) =>
+      parsedIp.isInSubnet(intranetIp),
+    )
+
+    return intranetIpMatches.includes(true)
+  }
+
+  /**
+   * Parses the given IP address string into an Address4 or Address6 object.
+   * If the IP address is invalid, returns null.
+   * @param ip IP address string to parse
+   * @return Parsed IP address or null if invalid
+   */
+  static parseIp(ip: string): ipAddress.Address4 | ipAddress.Address6 | null {
+    if (ipAddress.Address4.isValid(ip)) {
+      return new ipAddress.Address4(ip)
+    } else if (ipAddress.Address6.isValid(ip)) {
+      return new ipAddress.Address6(ip)
+    } else {
+      return null
+    }
   }
 }
 
