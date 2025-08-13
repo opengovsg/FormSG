9dca4cfa30b4404157a527072b7e56dbbfe88c2d
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-08-13 19:23:43 +0800

    feat: add base docker

---
+++ serverless/pdf-generator/index.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer-core')
+
+exports.handler = async () => {
+  const dummyHtml = '<h1>Hello World</h1>'
+  const pdfBuffer = await generatePdfFromHtml(dummyHtml)
+
+  const response = {
+    statusCode: 200,
+    body: pdfBuffer,
+  }
+  return response
+}
+
+/**
+ * Utility function to generate a PDF from HTML.
+ * Used to send autoreply emails, and to generate payment receipts
+ * @param summaryHtml HTML to generate PDF from
+ * @returns PDF Uint8Array
+ */
+const generatePdfFromHtml = async (summaryHtml) => {
+  const browser = await puppeteer.launch({
+    product: 'firefox',
+    executablePath: '/var/task/firefox/linux-stable_141.0.3/firefox',
+    headless: true,
+    args: ['--no-sandbox', '--disable-gpu'],
+  })
+  const page = await browser.newPage()
+  await page.setContent(summaryHtml, {
+    waitUntil: 'networkidle0',
+  })
+  const pdfBuffer = await page.pdf({
+    format: 'A4',
+    printBackground: true,
+    margin: {
+      top: '20px',
+      bottom: '40px',
+    },
+  })
+  await browser.close()
+  return pdfBuffer
+}
