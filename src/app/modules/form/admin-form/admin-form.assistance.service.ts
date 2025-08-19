a4d772c25d66ceada411a68f69a7916191ba1c69
scottheng96 <scottdavidheng@gmail.com>
2025-08-19 14:15:30 +0800

    omit signature field from mfb creation

--- src/app/modules/form/admin-form/admin-form.assistance.service.ts
+++ src/app/modules/form/admin-form/admin-form.assistance.service.ts
@@ -112,7 +112,9 @@ const mapSuggestedFormFieldToFieldCreateDto = (
   })
 }
 
-const BASIC_FIELD_NAMES = Object.keys(omit(BasicField, ['Children', 'Image']))
+const BASIC_FIELD_NAMES = Object.keys(
+  omit(BasicField, ['Children', 'Image', 'Signature']),
+)
   .map((fieldType) => `"${fieldType}"`)
   .toString()
 
@@ -175,7 +177,7 @@ const generateFormCreationPrompt = (userPrompt: string) => {
  * Field types supported by Mfb.
  */
 const supportedFieldTypes = Object.keys(
-  omit(BasicField, ['Children', 'Image']),
+  omit(BasicField, ['Children', 'Image', 'Signature']),
 ) as [string, ...string[]]
 
 /**
