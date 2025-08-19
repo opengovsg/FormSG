eff1f99539167bceee66de59e83b868674131c80
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-08-19 14:11:22 +0800

    feat: move to sam

--- serverless/pdf-gen-sparticuz/build-dist.sh
+++ serverless/pdf-gen-sparticuz/build-dist.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# This script is used to build a fresh dist and zip file with only the necessary files for the lambda function zip deployment.
+# This script is used to build a fresh dist directory for SAM deployment
 
 # Clean and remove any existing dist directory so we can start from scratch
 rm -rf dist/
-rm -f pdf-gen-sparticuz.zip
 
 # Create a new dist directory
 mkdir -p dist/
@@ -16,10 +15,8 @@ cp package-lock.json dist/
 # Build the ts files in src/ into the /dist directory using esbuild
 npm run build-ts-dist 
 
-# The necessary files are now in the dist directory - begin building the zip file contents
-cd dist 
 # Install dependencies in the dist directory (including @sparticuz/chromium with required .br binaries)
-npm ci --omit=dev 
+cd dist 
+npm ci --omit=dev
 
-# Zip the dist directory into a zip file called pdf-gen-sparticuz.zip
-zip -r ../pdf-gen-sparticuz.zip ./*
+echo "⚡ Build completed successfully in the /dist directory. Run 'npm run sam-build' to build the SAM package."
