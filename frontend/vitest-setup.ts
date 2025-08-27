2f00f6170407bf13f96aaee5ad19bd25407a1c8a
Ken <ken@open.gov.sg>
2025-08-27 21:15:17 +0800

    exit to track test

--- frontend/vitest-setup.ts
+++ frontend/vitest-setup.ts
@@ -38,5 +38,6 @@ Object.defineProperty(window, 'matchMedia', {
 process.on('unhandledRejection', (reason) => {
   // eslint-disable-next-line no-console
   console.log(`FAILED TO HANDLE PROMISE REJECTION`)
+  process.exit(1)
   throw reason
 })
