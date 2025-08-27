90bd685fc6ee43a992bdb9f916b788c039c4f8be
Ken <ken@open.gov.sg>
2025-08-27 20:47:19 +0800

    add test file

--- frontend/vitest-setup.ts
+++ frontend/vitest-setup.ts
@@ -33,3 +33,10 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: vi.fn(),
   })),
 })
+
+// FAIL LOUDLY on unhandled promise rejections / errors
+process.on('unhandledRejection', (reason) => {
+  // eslint-disable-next-line no-console
+  console.log(`FAILED TO HANDLE PROMISE REJECTION`)
+  throw reason
+})
