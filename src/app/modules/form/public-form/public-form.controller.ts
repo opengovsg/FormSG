33e11cb7b80e73ebd88498906f04273971bc34fe
Ken Lee Shu Ming <ken@open.gov.sg>
2025-09-05 11:13:48 +0800

    feat(singpass): hiding unselected collapsed options (#8659)
    * remove SP, Myinfo requirement for isEsrvcidRequired
    
    * hide collapsed options on fe
    
    * replace eserviceid with formsg's if not supplied
    
    * remove fe check on authtype
    
    * refactor to utils function
    
    * update singpass settings copy
    
    * fix linting issues
    
    * update tests
    
    * resolved be test case no esrvc_id
    
    * removed SP and myInfo eservcidid filling for e2e tests
    
    * resolved merge conflicts
    
    * fixed BE feature flag imports
    
    * changed default singpass option to auth type MyInfo
    
    * updated singpass options display strings, revert 'free' label from auth type=SP
    
    * corpass -> corppass
    
    * reintroduce esrvcid box for SP authtype
    
    * updated e2e test to default authtype=MyInfo during form creation
    
    ---------
    
    Co-authored-by: scottheng96 <scottdavidheng@gmail.com>

--- src/app/modules/form/public-form/public-form.controller.ts
+++ src/app/modules/form/public-form/public-form.controller.ts
@@ -3,6 +3,7 @@ import { celebrate, Joi, Segments } from 'celebrate'
 import { StatusCodes } from 'http-status-codes'
 import { err, ok, Result } from 'neverthrow'
 
+import { featureFlags } from '../../../../../shared/constants'
 import {
   ErrorCode,
   ErrorDto,
@@ -35,7 +36,7 @@ import { MyInfoService } from '../../myinfo/myinfo.service'
 import {
   createMyInfoLoginCookie,
   extractAuthCode,
-  validateMyInfoForm,
+  getMyInfoEserviceIdInForm,
 } from '../../myinfo/myinfo.util'
 import { SGIDMyInfoData } from '../../sgid/sgid.adapter'
 import {
@@ -661,15 +662,19 @@ export const _handleFormAuthRedirect: ControllerHandler<
   return FormService.retrieveFullFormById(formId)
     .andThen((form) => {
       formAuthType = form.authType
+      const useFormsgEsrvcId = req.growthbook?.isOn(
+        featureFlags.useFormsgEsrvcId,
+      )
       switch (form.authType) {
         case FormAuthType.MyInfo:
-          return validateMyInfoForm(form).andThen((form) =>
-            MyInfoService.createRedirectURL({
-              formEsrvcId: form.esrvcId,
-              formId,
-              requestedAttributes: form.getUniqueMyInfoAttrs(),
-              encodedQuery,
-            }),
+          return getMyInfoEserviceIdInForm(form, useFormsgEsrvcId).andThen(
+            ([form, eserviceId]) =>
+              MyInfoService.createRedirectURL({
+                formEsrvcId: eserviceId,
+                formId,
+                requestedAttributes: form.getUniqueMyInfoAttrs(),
+                encodedQuery,
+              }),
           )
         case FormAuthType.SP: {
           return validateSpcpForm(form).asyncAndThen((form) => {
