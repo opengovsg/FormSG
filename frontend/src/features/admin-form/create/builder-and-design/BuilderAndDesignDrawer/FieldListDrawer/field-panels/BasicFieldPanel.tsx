aa1d38e9cfca9501da2dc4829a3d9c050b9e808d
scottheng96 <scottdavidheng@gmail.com>
2025-09-09 16:17:20 +0800

    removed beta flag for categories

--- frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/FieldListDrawer/field-panels/BasicFieldPanel.tsx
+++ frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/FieldListDrawer/field-panels/BasicFieldPanel.tsx
@@ -1,28 +1,21 @@
 import { Box } from '@chakra-ui/react'
-import { useFeatureIsOn } from '@growthbook/growthbook-react'
 import { Droppable } from '@hello-pangea/dnd'
-
-import { featureFlags } from '~shared/constants'
-import { BasicField } from '~shared/types'
 
 import {
   BASIC_FIELDS_CONTENT_AND_DESCRIPTIONS,
   BASIC_FIELDS_DATES_AND_NUMBER,
   BASIC_FIELDS_FREE_TEXT,
   BASIC_FIELDS_OPTIONS,
-  BASIC_FIELDS_ORDERED,
   BASIC_FIELDS_OTHERS,
   BASIC_FIELDS_PERSONAL,
   CREATE_FIELD_BASIC_FIELDS_OPTIONS_DROP_ID,
   CREATE_FIELD_CONTENT_AND_DESCRIPTIONS_DROP_ID,
   CREATE_FIELD_DATES_AND_NUMBER_DROP_ID,
-  CREATE_FIELD_DROP_ID,
   CREATE_FIELD_FREE_TEXT_DROP_ID,
   CREATE_FIELD_OTHERS_DROP_ID,
   CREATE_FIELD_PERSONAL_DROP_ID,
 } from '~features/admin-form/create/builder-and-design/constants'
 import { BASICFIELD_TO_DRAWER_META } from '~features/admin-form/create/constants'
-import { useUser } from '~features/user/queries'
 
 import { useCreateTabForm } from '../../../../builder-and-design/useCreateTabForm'
 import { DraggableBasicFieldListOption } from '../FieldListOption'
@@ -31,16 +24,7 @@ import { FieldSection } from './FieldSection'
 import { filterFieldsBySearchValue } from './utils'
 
 export const BasicFieldPanel = ({ searchValue }: { searchValue: string }) => {
-  const { user } = useUser()
   const { isLoading } = useCreateTabForm()
-
-  const isSignatureFieldEnabled = useFeatureIsOn(featureFlags.signatureField)
-
-  const filteredCreateBasicFields = filterFieldsBySearchValue(
-    searchValue,
-    BASIC_FIELDS_ORDERED,
-    BASICFIELD_TO_DRAWER_META,
-  )
 
   const filteredCreateBasicFreeTextFields = filterFieldsBySearchValue(
     searchValue,
@@ -74,38 +58,7 @@ export const BasicFieldPanel = ({ searchValue }: { searchValue: string }) => {
     BASICFIELD_TO_DRAWER_META,
   )
 
-  // TODO: FRM-2054 remove when signature field is out of beta
-  return !(user?.betaFlags?.signatureField && isSignatureFieldEnabled) ? (
-    <Droppable isDropDisabled droppableId={CREATE_FIELD_DROP_ID}>
-      {(provided) => (
-        <Box ref={provided.innerRef} {...provided.droppableProps}>
-          <FieldSection>
-            <FieldSection label="Basic">
-              {filteredCreateBasicFields.map(({ fieldType, originalIndex }) => {
-                const shouldDisableField = isLoading
-                // TODO: FRM-2054 remove when signature field is out of beta
-                if (
-                  fieldType === BasicField.Signature &&
-                  !(user?.betaFlags?.signatureField && isSignatureFieldEnabled)
-                )
-                  return null
-
-                return (
-                  <DraggableBasicFieldListOption
-                    index={originalIndex}
-                    isDisabled={shouldDisableField}
-                    key={originalIndex}
-                    fieldType={fieldType}
-                  />
-                )
-              })}
-            </FieldSection>
-            <Box display="none">{provided.placeholder}</Box>
-          </FieldSection>
-        </Box>
-      )}
-    </Droppable>
-  ) : (
+  return (
     <>
       <Droppable isDropDisabled droppableId={CREATE_FIELD_FREE_TEXT_DROP_ID}>
         {(provided) => (
