17b541d631194c9fd64df721e875e9d9b1ee98d2
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-09-16 13:18:41 +0800

    feat: save draft v1.0 (#8091)
    * feat: basic save draft
    
    * feat: hoist state to provider
    
    * fix: specify ipv4
    
    * feat: implement draft restore and save
    
    * feat: add date formatting and add last saved
    
    * feat: update typings
    
    * feat: save null if empty
    
    * fix: bug with mrf augmented disabling, add save draft button
    
    * feat: update key to support mrf
    
    * feat: remove draft values for deleted field ids
    
    * feat: simplify and remove myinfo from saved draft
    
    * feat: remove console logs
    
    * feat: encapsulate draft submission object
    
    * fix: save draft mobile button height issue
    
    * feat: update save draft message
    
    * feat: add form toggle and setting for save draft
    
    * feat: connect save draft toggle
    
    * fix: bug with save draft
    
    * fix: add props for template and preview providers
    
    * feat: restore only after singpass login
    
    * feat: avoid storing signature for verifiable fields in draft
    
    * feat: make ratings a controlled component
    
    * feat: exclude headings and section from save draft
    
    * feat: handle form definition changes by not restoring when field checksum changes
    
    * fix: bug with form def updates
    
    * feat: add gb flag and use stack for settings general page
    
    * feat: add dd logs for save draft usage tracking
    
    * feat: adopt i18n translation
    
    * chore: fix lint formatting
    
    * chore: fix import formatting
    
    * fix: memo dependency issues
    
    * feat: carry over save draft when duplicating
    
    * feat: add ut to cover isSaveDraftEnabled setting update
    
    * feat: add tc and fix implementation of restore
    
    * feat: fix infinite render
    
    * fix: lint issues
    
    * feat: add floating tool bar stories
    
    * feat: add stories for settings and public form
    
    * fix: memo default vals
    
    * fix: lint issues
    
    * fix: floating button radius and add i18n
    
    * feat: make the selector for button click more selective
    
    * fix: lint issues
    
    * feat: add toast when draft exists
    
    * fix: toast infinite renders and double toast bug
    
    * fix: dont display toast if no fields to restore
    
    * fix: support toast when has draft
    
    * fix: lint and useToast type errors
    
    * feat: add gb attributes for admin settings page
    
    * feat: add i18n and floating toolbar to template preview
    
    * fix: lint formatting
    
    * feat: simplify chromatic tc
    
    * feat: use save draft specific store
    
    * feat: include story steps
    
    * fix: lint formatting
    
    * feat: increase timeout to reduce flakiness
    
    * fix: use mrf submission snapshot for drafts
    
    * fix: update the rating stories to use the controlled value
    
    * fix: update form def
    
    * fix: lint
    
    * fix: add memo and use jsdocs
    
    * fix: lint
    
    * chore: rename mrfConsistentFormData to data
    
    ---------
    
    Co-authored-by: Eliot Lim <eliot@open.gov.sg>

--- src/types/form.ts
+++ src/types/form.ts
@@ -59,6 +59,7 @@ type FormDefaultableKey =
   | 'permissionList'
   | 'startPage'
   | 'endPage'
+  | 'isSaveDraftEnabled'
   | 'hasCaptcha'
   | 'hasIssueNotification'
   | 'hasRespondentCopy'
@@ -105,6 +106,7 @@ export type PickDuplicateForm = Pick<
   | 'authType'
   | 'isSubmitterIdCollectionEnabled'
   | 'isSingleSubmission'
+  | 'isSaveDraftEnabled'
   | 'inactiveMessage'
   | 'submissionLimit'
   | 'responseMode'
@@ -289,6 +291,7 @@ interface IFormBaseDocument<T extends IFormSchema> {
   form_fields: NonNullable<T['form_fields']>
   form_logics: NonNullable<T['form_logics']>
   permissionList: NonNullable<T['permissionList']>
+  isSaveDraftEnabled: NonNullable<T['isSaveDraftEnabled']>
   hasCaptcha: NonNullable<T['hasCaptcha']>
   hasIssueNotification: NonNullable<T['hasIssueNotification']>
   hasRespondentCopy: NonNullable<T['hasRespondentCopy']>
