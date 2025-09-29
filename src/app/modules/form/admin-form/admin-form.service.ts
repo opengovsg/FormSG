e8acdeb3a922ce07a3c77ddc12650164c263c57b
scottheng96 <scottdavidheng@gmail.com>
2025-09-29 10:19:01 +0800

    fixed lint issues

--- src/app/modules/form/admin-form/admin-form.service.ts
+++ src/app/modules/form/admin-form/admin-form.service.ts
@@ -693,8 +693,7 @@ export const duplicateForm = (
     duplicateParams.form_fields.forEach((field) => {
       if (field.fieldType === BasicField.Dropdown) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (field as any).optionsToRecipientsMap = undefined
-        console.log(field)
+        ; (field as any).optionsToRecipientsMap = undefined
       }
     })
   }
