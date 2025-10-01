210766abad60baa3424b754c5ca9feb2c241464d
Eliot Lim <eliot@open.gov.sg>
2025-10-01 15:28:09 +0800

    chore: FloatingToolbar stories refetch return type

--- frontend/src/features/public-form/components/FloatingToolBar/FloatingToolbar.stories.tsx
+++ frontend/src/features/public-form/components/FloatingToolBar/FloatingToolbar.stories.tsx
@@ -65,7 +65,7 @@ const createMockContext = (
     isPreviousData: false,
     isRefetchError: false,
     isStale: false,
-    refetch: () => Promise.resolve({}),
+    refetch: () => Promise.resolve({} as unknown),
     remove: () => {},
     status: 'success' as const,
   } as PublicFormContextProps
