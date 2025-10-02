23b4a9baf7e060fb522f9dd8ec84a0b08dccbf64
scottheng96 <scottdavidheng@gmail.com>
2025-10-02 08:30:50 +0800

    updated shared import with right path

--- src/app/modules/submission/multirespondent-submission/multirespondent-submission.utils.ts
+++ src/app/modules/submission/multirespondent-submission/multirespondent-submission.utils.ts
@@ -14,7 +14,6 @@ import {
 } from '../../../../../shared/types'
 import { handleAddressResponseDisplay } from '../../../../../shared/utils/address'
 import { SIGNATURE_CAPTURED_STRING } from '../../../../../shared/utils/signature'
-import { getSignatureFileName } from '../../../../../shared/utils/signature'
 import { VerifiedKeys } from '../../../../../shared/utils/verified-content'
 import {
   FormFieldSchema,
