e6edbdfc8af7b4cce53d31301fe4fa9f6d1221af
scottheng96 <scottdavidheng@gmail.com>
2025-10-31 11:36:04 +0800

    show respondent copy helper text on email field conditionally

--- frontend/src/templates/Field/Email/EmailFieldInput.tsx
+++ frontend/src/templates/Field/Email/EmailFieldInput.tsx
@@ -12,6 +12,7 @@ import Input, { InputProps } from '~components/Input'
 
 import { EmailFieldSchema, VerifiableFieldInput } from '../types'
 
+import { Text } from '@chakra-ui/react'
 export interface EmailFieldInputProps {
   schema: EmailFieldSchema
   disableRequiredValidation?: boolean
@@ -59,20 +60,34 @@ export const EmailFieldInput = ({
       name={schema._id}
       defaultValue={{ value: '' }}
       render={({ field: { onChange, value, ...field } }) => (
-        <Input
-          autoComplete="email"
-          value={value?.value ?? ''}
-          onChange={(event) => {
-            const value = event.target.value.trim().toLowerCase()
-            return handleInputChange
-              ? handleInputChange(onChange)(value)
-              : onChange({ value })
-          }}
-          isHighContrast={isHighContrast}
-          preventDefaultOnEnter
-          {...field}
-          {...inputProps}
-        />
+        <>
+          <Input
+            autoComplete="email"
+            value={value?.value ?? ''}
+            onChange={(event) => {
+              const value = event.target.value.trim().toLowerCase()
+              return handleInputChange
+                ? handleInputChange(onChange)(value)
+                : onChange({ value })
+            }}
+            isHighContrast={isHighContrast}
+            preventDefaultOnEnter
+            {...field}
+            {...inputProps}
+          />
+          {schema.autoReplyOptions?.includeFormSummary ? 
+          <Text
+            color="secondary.400"
+            textStyle="body-2"
+            aria-hidden
+            my="0.5rem"
+          >
+            {t(
+              'features.publicForm.components.fields.email.respondentCopyHelperText',
+            )}
+            </Text>
+            : null}
+        </>
       )}
     />
   )
