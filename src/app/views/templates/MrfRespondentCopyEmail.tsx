14de202a23d097f36b538b2d0c47741c9684e3d0
scottheng96 <scottdavidheng@gmail.com>
2025-11-05 15:07:43 +0800

    created MrfRespondentCopyTemplate

---
+++ src/app/views/templates/MrfRespondentCopyEmail.tsx
@@ -0,0 +1,95 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components'
+import { FORMSG_LOGO_URL } from '../../constants/formsg-logo'
+
+import {
+  secondaryTextStyle,
+  containerStyle,
+  headingTextStyle,
+  mainStyle,
+  sectionStyle,
+  primaryTextStyle,
+  questionMargin,
+  answerMargin,
+} from './mrfWorkflowCompletionEmailStyle'
+import { DEFAULT_RESPONDENT_COPY_EMAIL } from '../../../../shared/constants/mail'
+
+export type QuestionAnswer = {
+  question: string
+  answer: string
+}
+
+export type RespondentCopyEmilData = {
+    formTitle: string
+    responseId: string
+    formQuestionAnswers: QuestionAnswer[] | undefined
+    body?: string
+}
+
+export const MrfRespondentCopyEmail = ({
+    formTitle,
+    responseId,
+    formQuestionAnswers,
+    body,
+}: RespondentCopyEmilData): JSX.Element => {
+    let headingText = 'Thank you for submitting this form'
+    let emailBody = body || DEFAULT_RESPONDENT_COPY_EMAIL.content.replace('{agencyName}', 'FormSG') //TODO: add agency name
+  
+    const renderQuestionAnswer = (qa: QuestionAnswer) => (
+        <>
+          <Text style={{ ...primaryTextStyle, ...questionMargin }}>{qa.question}</Text>
+          <Text style={{ ...secondaryTextStyle, ...answerMargin }}>{qa.answer}</Text>
+        </>
+    )
+
+    const renderResponseId = () => (
+        <>
+          <Text style={{ ...secondaryTextStyle, marginBottom: '40px' }}>{'---'}</Text>
+          <Text style={{ ...primaryTextStyle, ...questionMargin }}>Response ID</Text>
+          <Text style={{ ...secondaryTextStyle,  marginBottom: '40px' }}>{responseId}</Text>
+          <Text style={{ ...secondaryTextStyle, marginBottom: '40px' }}>{'---'}</Text>
+        </>
+    )
+
+    return (
+    <Html>
+      <Head /> 
+      <Preview>{headingText}</Preview>
+      <Body style={mainStyle}>
+        <Container style={containerStyle}>
+          <Section style={sectionStyle}>
+            <Img style={{ height: '24px', marginBottom: '40px' }} src={FORMSG_LOGO_URL} alt="FormSG" />
+            <Heading style={{ ...headingTextStyle, marginBottom: '40px' }}>
+              {headingText}
+            </Heading>
+            <Text style={{ ...secondaryTextStyle, marginBottom: '40px', whiteSpace: 'pre-line' }}>
+              {emailBody}
+            </Text>
+            {renderResponseId()}
+              {formQuestionAnswers ? (
+                <>
+                  <Text style={{ ...primaryTextStyle, ...answerMargin }}>Responses for {formTitle}</Text>
+                  <>
+                    {formQuestionAnswers.map(renderQuestionAnswer)}
+                  </>
+              </>
+            ) : null}
+            <Text style={{ ...secondaryTextStyle, marginTop: '24px' }}>
+              For more details, please contact the respondent(s) or form administrator.
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>        
+    )
+}
