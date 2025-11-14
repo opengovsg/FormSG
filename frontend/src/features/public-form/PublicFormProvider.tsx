ba627bc19804751849fc4b12ff9006db55d21287
Eliot Lim <eliotlim@users.noreply.github.com>
2025-11-14 11:02:23 +0800

    fix: remove unnecessary optional chaining operator

--- frontend/src/features/public-form/PublicFormProvider.tsx
+++ frontend/src/features/public-form/PublicFormProvider.tsx
@@ -876,7 +876,7 @@ export const PublicFormProvider = ({
             'features.publicForm.components.saveDraft.toast.restoredOnlyUnchangedFields',
           )
         : t('features.publicForm.components.saveDraft.toast.restoredAllFields')
-      datadogLogs.logger?.log(restoreDraftMessage, {
+      datadogLogs.logger.log(restoreDraftMessage, {
         meta: {
           action: 'restoreDraft',
           formId,
