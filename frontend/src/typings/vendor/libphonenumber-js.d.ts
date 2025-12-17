7030ac0af7f94518154d32ed3005a15847c2a68e
Eliot Lim <eliot@open.gov.sg>
2025-12-17 16:29:39 +0800

    chore: implement suggestions from code review

--- frontend/src/typings/vendor/libphonenumber-js.d.ts
+++ frontend/src/typings/vendor/libphonenumber-js.d.ts
@@ -1,13 +1,6 @@
 declare module 'libphonenumber-js/examples.mobile.json' {
-  import { CountryCode } from 'libphonenumber-js'
+  import { CountryCode, NationalNumber } from 'libphonenumber-js'
 
-  const defaultExport: { [country in CountryCode]: string }
-  export default defaultExport
-}
-
-declare module '~components/PhoneNumberInput/resources/examples.landline.json' {
-  import { CountryCode } from 'libphonenumber-js'
-
-  const defaultExport: { [country in CountryCode]: string }
+  const defaultExport: { [country in CountryCode]: NationalNumber }
   export default defaultExport
 }
