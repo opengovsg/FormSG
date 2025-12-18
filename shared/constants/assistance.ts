7987ea188d34d437fb06ac4608a877768575f5aa
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2025-12-18 17:45:10 +0800

    chore: move vision prompt to assistance const file

--- shared/constants/assistance.ts
+++ shared/constants/assistance.ts
@@ -1,4 +1,4 @@
-// Constants relating to AI assistance features (Magic Form Builder)
-
 /** Maximum character limit for text prompts in Magic Form Builder */
 export const MFB_TEXT_PROMPT_MAX_CHAR = 500
+/** Maximum number of images / pages in a single PDF that can be uploaded for MFB Vision. */
+export const MFB_VISION_MAX_IMAGES_COUNT = 10
