9abace5e6193bb96423958e8f039ba93589d9f40
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-01-28 11:48:35 +0800

    feat: add wogad button component to login page

---
+++ frontend/src/features/login/components/WogadLoginButton.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { useMutation } from 'react-query'
+import { Flex, Text, VStack } from '@chakra-ui/react'
+import { delay } from 'lodash'
+
+import { getWogadAuthUrl } from '~services/AuthService'
+import Button from '~components/Button'
+
+export const WogadLoginButton = (): JSX.Element | null => {
+  const [isRetryDelayWindow, setRetryDelayWindow] = useState(false)
+
+  const handleWogadLogin = () => {
+    setRetryDelayWindow(true)
+    delay(setRetryDelayWindow, 3000, false)
+    return wogadLoginMutation.mutate()
+  }
+
+  const wogadLoginMutation = useMutation(getWogadAuthUrl, {
+    onSuccess: ({ authUrl }) => {
+      window.location.assign(authUrl)
+    },
+  })
+
+  return (
+    <VStack alignItems="start">
+      <Button
+        isFullWidth
+        isLoading={wogadLoginMutation.isLoading || isRetryDelayWindow}
+        type="submit"
+        color="primary"
+        onClick={handleWogadLogin}
+        variant="outline"
+        aria-label="Log in with WOG AD"
+      >
+        <Flex align="center" flexDirection="row" aria-hidden>
+          <Text color="primary.500">Log in with WOG AD</Text>
+        </Flex>
+      </Button>
+    </VStack>
+  )
+}
