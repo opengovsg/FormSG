5d1dbdcc6182c7fafc39c5bf2f2ea734bc362e16
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-02-04 16:33:08 +0800

    Merge pull request #9071 from opengovsg/feat/change-gd-timeout
    feat: move guardduty scan check timings to env config

--- serverless/virus-scanner-guardduty/src/config.ts
+++ serverless/virus-scanner-guardduty/src/config.ts
@@ -7,7 +7,16 @@ const isTest = process.env.NODE_ENV === 'test'
 export const config = convict({
   environment: {
     env: 'NODE_ENV',
-    format: ['development', 'stg', 'stg-alt', 'stg-alt2', 'stg-alt3', 'uat', 'production', 'test'],
+    format: [
+      'development',
+      'stg',
+      'stg-alt',
+      'stg-alt2',
+      'stg-alt3',
+      'uat',
+      'production',
+      'test',
+    ],
     default: 'development',
   },
   isTestOrDev: {
@@ -22,6 +31,27 @@ export const config = convict({
     env: 'GUARDDUTY_CLEAN_S3_BUCKET',
     format: String,
     default: '',
+  },
+  guarddutyScanCheckTimeout: {
+    env: 'GUARDDUTY_SCAN_AWAIT_TIMEOUT',
+    format: Number,
+    description:
+      'Total amount of time to repeatedly check if GuardDuty has scanned and tagged the file before timing out and marking as failure.',
+    default: 40 * 1000, // 40 seconds.
+  },
+  guarddutyScanCheckDelay: {
+    env: 'GUARDDUTY_SCAN_CHECK_DELAY',
+    format: Number,
+    description:
+      'Initial amount of time to wait before retrying checking if Guardduty has scanned and tagged the file.',
+    default: 200, // 0.2 seconds.
+  },
+  guarddutyScanCheckMaxBackoff: {
+    env: 'GUARDDUTY_SCAN_CHECK_MAX_BACKOFF',
+    format: Number,
+    description:
+      'Maximum amount of time for backoff to for retries for checking if Guardduty has scanned and tagged the file.',
+    default: 2 * 1000, // 2 seconds.
   },
 })
   .validate()
