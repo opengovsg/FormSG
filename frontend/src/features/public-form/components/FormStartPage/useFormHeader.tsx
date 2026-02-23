9c4299ff4ebc454d820d2bf45e855ee9ec938613
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-02-23 17:17:42 +0800

    fix: ensure color theme is returning colorScheme

--- frontend/src/features/public-form/components/FormStartPage/useFormHeader.tsx
+++ frontend/src/features/public-form/components/FormStartPage/useFormHeader.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { FormColorTheme, FormStartPage } from '~shared/types'
 
 import { useDesignColorTheme } from '~features/admin-form/create/builder-and-design/utils/useDesignColorTheme'
+import { ThemeColorScheme } from '~theme/foundations/colours'
 
 interface UseFormHeaderProps {
   startPage?: FormStartPage
@@ -15,26 +16,28 @@ export const getTitleBg = (colorTheme?: FormColorTheme, hover?: boolean) =>
 
 export const useFormHeader = ({ startPage, hover }: UseFormHeaderProps) => {
   const { t } = useTranslation()
-  const colorScheme = useDesignColorTheme()
+  const colorTheme = useDesignColorTheme()
   const titleColor = useMemo(() => {
-    if (colorScheme === FormColorTheme.Orange) {
+    if (colorTheme === FormColorTheme.Orange) {
       return 'secondary.700'
     }
     return 'white'
-  }, [colorScheme])
+  }, [colorTheme])
 
   const titleBg = useMemo(
-    () => getTitleBg(colorScheme, hover),
-    [hover, colorScheme],
+    () => getTitleBg(colorTheme, hover),
+    [hover, colorTheme],
   )
 
   const estTimeString = useMemo(() => {
     return startPage?.estTimeTaken
       ? t('features.publicForm.components.header.estTime', {
-          estTime: startPage.estTimeTaken,
-        })
+        estTime: startPage.estTimeTaken,
+      })
       : ''
   }, [t, startPage?.estTimeTaken])
+
+  const colorScheme: ThemeColorScheme | undefined = useMemo(() => colorTheme ? `theme-${colorTheme}` as const : undefined, [colorTheme])
 
   return {
     titleColor,
