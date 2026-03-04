7cb90d66cba23ca62d0ef1c57f1419e67aa37ec7
Eliot Lim <eliot@open.gov.sg>
2026-03-04 15:35:48 +0800

    feat: migrate react-email-preview to pnpm

--- react-email-preview/stories/EmailAddressVerificationOtp.stories.tsx
+++ react-email-preview/stories/EmailAddressVerificationOtp.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryFn } from '@storybook/react'
 
 import EmailAddressVerificationOtp, {
   type EmailAddressVerificationOtpHtmlData,
-} from './EmailAddressVerificationOtp'
+} from '../emails/EmailAddressVerificationOtp'
 
 export default {
   title: 'EmailPreview/EmailAddressVerificationOtp',
