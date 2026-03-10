94f3943073458525d12e58b94de9af2f3a8fa000
Eliot Lim <eliotlim@users.noreply.github.com>
2026-03-10 17:47:42 +0800

    feat: auxiliary pnpm workspaces (#9183)
    * feat: auxiliary pnpm workspaces
    
    * chore: update package builtDependencies
    
    * fix: pnpm compatibility for pdf-gen build-dist script
    
    * chore: move workspaces into packages and services
    
    * fix: correct react-email-preview imports

--- react-email-preview/emails/MrfWorkflowEmail.tsx
+++
@@ -1,5 +0,0 @@
-import { MrfWorkflowEmail } from '../../src/app/views/templates/MrfWorkflowEmail'
-
-export type { WorkflowEmailData } from '../../src/app/views/templates/MrfWorkflowEmail'
-
-export default MrfWorkflowEmail
