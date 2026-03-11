7d35590f735b2de13445c7e83fda7af29ff848ed
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-03-11 17:39:37 +0800

    feat: move shared test resources to root since they are used by multi packages

--- packages/shared/types/field/attachmentField.ts
+++ packages/shared/types/field/attachmentField.ts
@@ -1,6 +1,6 @@
 import { BasicField, FieldBase } from './base'
-import { PresignedPost } from 'aws-sdk/clients/s3'
-
+import type { PresignedPost } from 'aws-sdk/clients/s3'
+  
 export enum AttachmentSize {
   OneMb = '1',
   TwoMb = '2',
