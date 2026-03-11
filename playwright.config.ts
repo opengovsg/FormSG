bbf0c2e004b2f82550ca61db5b41c1444dc8b998
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-03-11 15:46:34 +0800

    feat: update e2e to use the new backend directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config({
-  path: path.resolve(__dirname, './__tests__/setup/.test-env'),
+  path: path.resolve(__dirname, './apps/backend/__tests__/setup/.test-env'),
 })
 
 /**
