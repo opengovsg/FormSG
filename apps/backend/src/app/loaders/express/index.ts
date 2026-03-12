9d9af678f828cb1387e0bcae027f49d2f9eb0dd9
scottheng96 <scottdavidheng@gmail.com>
2026-03-12 11:31:21 +0800

    feat: update fe build path from /dist/apps/frontend to /apps/frontend/dist

--- apps/backend/src/app/loaders/express/index.ts
+++ apps/backend/src/app/loaders/express/index.ts
@@ -129,7 +129,7 @@ const loadExpressApp = async (connection: Connection) => {
   // API routes
   app.use('/api', ApiRouter)
 
-  // serve static assets. `dist/apps/frontend` contains the root files as well as a `/static` folder
+  // serve static assets. `apps/frontend/dist` contains the root files as well as a `/static` folder
   // express.static calls next() if the file is not found
   app.use(express.static(path.resolve('apps/frontend/dist'), { index: false }))
 
