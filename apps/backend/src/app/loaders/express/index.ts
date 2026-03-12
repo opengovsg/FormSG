eee2f0ee0fa5c9ea00d8ab71ea88774dd0ebac7c
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-03-12 11:08:13 +0800

    feat: update build dist for fe and be

--- apps/backend/src/app/loaders/express/index.ts
+++ apps/backend/src/app/loaders/express/index.ts
@@ -131,7 +131,7 @@ const loadExpressApp = async (connection: Connection) => {
 
   // serve static assets. `dist/frontend` contains the root files as well as a `/static` folder
   // express.static calls next() if the file is not found
-  app.use(express.static(path.resolve('dist/frontend'), { index: false }))
+  app.use(express.static(path.resolve('../frontend/dist'), { index: false }))
 
   // If requests for known static asset patterns were not served by
   // the static handlers above, middleware should try to fetch from s3 static bucket or else return 404s
