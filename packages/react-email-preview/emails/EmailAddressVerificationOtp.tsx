4845049c6b2c45f21285448883acbacb1bf975ea
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-03-17 10:54:38 +0800

    feat: use backend package

--- packages/react-email-preview/emails/EmailAddressVerificationOtp.tsx
+++ packages/react-email-preview/emails/EmailAddressVerificationOtp.tsx
@@ -1,5 +1,5 @@
-import { EmailAddressVerificationOtp } from '../../../apps/backend/src/app/views/templates/EmailAddressVerificationOtp'
+import { EmailAddressVerificationOtp } from 'formsg-backend/src/app/views/templates/EmailAddressVerificationOtp'
 
-export type { EmailAddressVerificationOtpHtmlData } from '../../../apps/backend/src/app/views/templates/EmailAddressVerificationOtp'
+export type { EmailAddressVerificationOtpHtmlData } from 'formsg-backend/src/app/views/templates/EmailAddressVerificationOtp'
 
 export default EmailAddressVerificationOtp
--- packages/react-email-preview/emails/MrfWorkflowEmail.tsx
+++ packages/react-email-preview/emails/MrfWorkflowEmail.tsx
@@ -1,5 +1,5 @@
-import { MrfWorkflowEmail } from '../../../apps/backend/src/app/views/templates/MrfWorkflowEmail'
+import { MrfWorkflowEmail } from 'formsg-backend/src/app/views/templates/MrfWorkflowEmail'
 
-export type { WorkflowEmailData } from '../../../apps/backend/src/app/views/templates/MrfWorkflowEmail'
+export type { WorkflowEmailData } from 'formsg-backend/src/app/views/templates/MrfWorkflowEmail'
 
 export default MrfWorkflowEmail
