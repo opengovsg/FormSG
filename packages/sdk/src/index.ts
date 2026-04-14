56c99e2fd11f5b3284e1aa26f74bbcc17b912f29
scottheng96 <scottdavidheng@gmail.com>
2026-04-14 14:35:58 +0800

    feat: add v4 to v3 schema

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -6,6 +6,7 @@ import Verification from './verification'
 import Webhooks from './webhooks'
 
 export { adaptV3ToV4 } from './adapt-v3-to-v4'
+export { adaptV4ToV3 } from './adapt-v4-to-v3'
 export type {
   DecryptedContent,
   DecryptedContentV3,
