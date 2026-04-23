788e1d7fb4d342bb4158cbb1c1a1f72e769e0999
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-04-23 15:01:05 +0800

    feat: add whitelistedSubmitterIds to mrf type

--- packages/shared/constants/form.ts
+++ packages/shared/constants/form.ts
@@ -75,6 +75,7 @@ export const MULTIRESPONDENT_FORM_SETTINGS_FIELDS = [
   'stepsToNotify',
   'stepOneEmailNotificationFieldId',
   'hasStatusTracker',
+  'whitelistedSubmitterIds',
 ] as const
 
 // Fields that are necessary for decrypting the cipherTexts given peer's private key
