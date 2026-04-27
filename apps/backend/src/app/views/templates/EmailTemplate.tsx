d0c40758baf1a7f768a718774a7278dfb402ec1a
scottheng96 <scottdavidheng@gmail.com>
2026-04-27 16:57:43 +0800

    feat: show headers with different styles in email template

--- apps/backend/src/app/views/templates/EmailTemplate.tsx
+++ apps/backend/src/app/views/templates/EmailTemplate.tsx
@@ -24,6 +24,7 @@ import {
 } from '@react-email/components'
 import { BasicField } from 'formsg-shared/types'
 import React from 'react'
+import { BasicField } from 'formsg-shared/types'
 
 import { FORMSG_LOGO_URL } from '../../constants/formsg-logo'
 
