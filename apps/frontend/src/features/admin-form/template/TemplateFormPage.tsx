432ffa4c8f6044f02a87d1dbfbc23a799b3548a0
Eliot Lim <eliot@open.gov.sg>
2026-04-28 14:01:09 +0800

    feat(frontend): restore use-template-frame

--- apps/frontend/src/features/admin-form/template/TemplateFormPage.tsx
+++ apps/frontend/src/features/admin-form/template/TemplateFormPage.tsx
@@ -20,12 +20,17 @@ import { PublicFormWrapper } from '~features/public-form/components/PublicFormWr
 import { PreviewFormBannerContainer } from '../common/components/PreviewFormBanner'
 
 import { TemplateFormProvider } from './TemplateFormProvider'
-import { UseTemplateTour, UseTemplateWall } from './UseTemplateNudges'
+import {
+  UseTemplateFrame,
+  UseTemplateTour,
+  UseTemplateWall,
+} from './UseTemplateNudges'
 
 export const TemplateFormPage = (): JSX.Element => {
   const { formId } = useParams()
   if (!formId) throw new Error('No formId provided')
 
+  const isFrameOn = useFeatureIsOn(featureFlags.useTemplateFrame)
   const isTourOn = useFeatureIsOn(featureFlags.useTemplateTour)
   const isWallOn = useFeatureIsOn(featureFlags.useTemplateWall)
 
@@ -37,6 +42,7 @@ export const TemplateFormPage = (): JSX.Element => {
 
   return (
     <Flex flexDir="column" css={fillHeightCss} pos="relative">
+      {isFrameOn && <UseTemplateFrame />}
       <TemplateFormProvider
         formId={formId}
         onTemplatePreviewSubmitClick={isWallOn ? onWallOpen : undefined}
