seriouslee13-beep/formsgimport

feat: extract text for admin form settings i18n

apps/frontend/src/features/admin-form/settings/hooks/useSecretKeyForm.ts
@@ -1,5 +1,6 @@
 import { useCallback, useRef, useState } from 'react'
 import { useForm, UseFormSetError, UseFormSetValue } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
 
 import { isKeypairValid, SECRET_KEY_REGEX } from '~utils/secretKeyValidation'
 
@@ -27,6 +28,8 @@ export const useSecretKeyForm = ({
   onSubmit,
   hasAck = false,
 }: UseSecretKeyFormProps) => {
+  const { t } = useTranslation()
+
   const {
     formState: { errors },
     setError,
@@ -44,31 +47,36 @@ export const useSecretKeyForm = ({
     e.stopPropagation()
   }
 
-  const processFile = (
-    file: File,
-    setError: UseFormSetError<SecretKeyFormInputs>,
-    setValue: UseFormSetValue<SecretKeyFormInputs>,
-  ) => {
-    const reader = new FileReader()
-    reader.onload = async (e) => {
-      if (!e.target) return
-      const text = e.target.result?.toString()
+  const processFile = useCallback(
+    (
+      file: File,
+      setError: UseFormSetError<SecretKeyFormInputs>,
+      setValue: UseFormSetValue<SecretKeyFormInputs>,
+    ) => {
+      const reader = new FileReader()
+      reader.onload = async (e) => {
+        if (!e.target) return
+        const text = e.target.result?.toString()
 
-      if (!text || !SECRET_KEY_REGEX.test(text)) {
-        return setError(
-          SECRET_KEY_NAME,
-          {
-            type: 'invalidFile',
-            message: 'Selected file seems to be invalid',
-          },
-          { shouldFocus: true },
-        )
+        if (!text || !SECRET_KEY_REGEX.test(text)) {
+          return setError(
+            SECRET_KEY_NAME,
+            {
+              type: 'invalidFile',
+              message: t(
+                'features.adminForm.settings.secretKey.errors.invalidFile',
+              ),
+            },
+            { shouldFocus: true },
+          )
+        }
+
+        setValue(SECRET_KEY_NAME, text, { shouldValidate: true })
       }
-
-      setValue(SECRET_KEY_NAME, text, { shouldValidate: true })
-    }
-    reader.readAsText(file)
-  }
+      reader.readAsText(file)
+    },
+    [t],
+  )
 
   const handleDrop = useCallback(
     (e: React.DragEvent) => {
@@ -80,7 +88,7 @@ export const useSecretKeyForm = ({
 
       processFile(file, setError, setValue)
     },
-    [setError, setValue],
+    [processFile, setError, setValue],
   )
 
   const handleDragEnter = (e: React.DragEvent) => {
@@ -114,7 +122,9 @@ export const useSecretKeyForm = ({
           SECRET_KEY_NAME,
           {
             type: 'invalidKey',
-            message: 'The secret key provided is invalid',
+            message: t(
+              'features.adminForm.settings.secretKey.errors.invalidKey',
+            ),
           },
           { shouldFocus: true },
         )
@@ -137,7 +147,7 @@ export const useSecretKeyForm = ({
 
       processFile(file, setError, setValue)
     },
-    [setError, setValue],
+    [processFile, setError, setValue],
   )
 
   // Reset form before closing.

apps/frontend/src/i18n/locales/features/admin-form/settings/index.ts
@@ -9,9 +9,82 @@ export type HasTitle = {
   title: string
 }
 
+export interface SecretKeyFormStrings {
+  errors: {
+    invalidFile: string
+    invalidKey: string
+  }
+}
+
+export interface SettingsMutationsStrings {
+  missingFormId: string
+  formStatus: {
+    openStorageMode: string
+    open: string
+    closed: string
+  }
+  submissionLimit: {
+    withOrdinal: string
+    removed: string
+  }
+  multiLang: {
+    enabled: string
+    disabled: string
+  }
+  supportedLanguages: {
+    selectable: string
+    hidden: string
+  }
+  saveDraft: {
+    enabled: string
+    disabled: string
+  }
+  captcha: {
+    enabled: string
+    disabled: string
+  }
+  issueNotification: {
+    enabled: string
+    disabled: string
+  }
+  formTitleUpdated: string
+  inactiveMessageUpdated: string
+  emailsUpdated: string
+  esrvcIdUpdated: string
+  authType: {
+    enabled: string
+    disabled: string
+    updated: string
+  }
+  submitterId: {
+    enabled: string
+    disabled: string
+  }
+  singleSubmission: {
+    enabled: string
+    disabled: string
+  }
+  whitelist: {
+    uploaded: string
+    removed: string
+  }
+  webhookUrl: {
+    updated: string
+    removed: string
+  }
+  webhookRetries: {
+    enabled: string
+    disabled: string
+  }
+  businessInfoUpdated: string
+  gstUpdated: string
+}
+
 export interface Settings {
   general: General
   singpass: HasTitle
+  secretKey: SecretKeyFormStrings
+  mutations: SettingsMutationsStrings
   emailNotifications: EmailNotifications
   webhooks: Webhooks
   payments: Payments

opengovsg/FormSG

apps/frontend/src/features/admin-form/settings/hooks/useSecretKeyForm.ts
@@ -1,5 +1,6 @@
 import { useCallback, useRef, useState } from 'react'
 import { useForm, UseFormSetError, UseFormSetValue } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
 
 import { isKeypairValid, SECRET_KEY_REGEX } from '~utils/secretKeyValidation'
 
@@ -27,6 +28,8 @@ export const useSecretKeyForm = ({
   onSubmit,
   hasAck = false,
 }: UseSecretKeyFormProps) => {
+  const { t } = useTranslation()
+
   const {
     formState: { errors },
     setError,
@@ -44,31 +47,36 @@ export const useSecretKeyForm = ({
     e.stopPropagation()
   }
 
-  const processFile = (
-    file: File,
-    setError: UseFormSetError<SecretKeyFormInputs>,
-    setValue: UseFormSetValue<SecretKeyFormInputs>,
-  ) => {
-    const reader = new FileReader()
-    reader.onload = async (e) => {
-      if (!e.target) return
-      const text = e.target.result?.toString()
+  const processFile = useCallback(
+    (
+      file: File,
+      setError: UseFormSetError<SecretKeyFormInputs>,
+      setValue: UseFormSetValue<SecretKeyFormInputs>,
+    ) => {
+      const reader = new FileReader()
+      reader.onload = async (e) => {
+        if (!e.target) return
+        const text = e.target.result?.toString()
 
-      if (!text || !SECRET_KEY_REGEX.test(text)) {
-        return setError(
-          SECRET_KEY_NAME,
-          {
-            type: 'invalidFile',
-            message: 'Selected file seems to be invalid',
-          },
-          { shouldFocus: true },
-        )
+        if (!text || !SECRET_KEY_REGEX.test(text)) {
+          return setError(
+            SECRET_KEY_NAME,
+            {
+              type: 'invalidFile',
+              message: t(
+                'features.adminForm.settings.secretKeyVerification.errors.invalidFile',
+              ),
+            },
+            { shouldFocus: true },
+          )
+        }
+
+        setValue(SECRET_KEY_NAME, text, { shouldValidate: true })
       }
-
-      setValue(SECRET_KEY_NAME, text, { shouldValidate: true })
-    }
-    reader.readAsText(file)
-  }
+      reader.readAsText(file)
+    },
+    [t],
+  )
 
   const handleDrop = useCallback(
     (e: React.DragEvent) => {
@@ -80,7 +88,7 @@ export const useSecretKeyForm = ({
 
       processFile(file, setError, setValue)
     },
-    [setError, setValue],
+    [processFile, setError, setValue],
   )
 
   const handleDragEnter = (e: React.DragEvent) => {
@@ -114,7 +122,9 @@ export const useSecretKeyForm = ({
           SECRET_KEY_NAME,
           {
             type: 'invalidKey',
-            message: 'The secret key provided is invalid',
+            message: t(
+              'features.adminForm.settings.secretKeyVerification.errors.invalidKey',
+            ),
           },
           { shouldFocus: true },
         )
@@ -137,7 +147,7 @@ export const useSecretKeyForm = ({
 
       processFile(file, setError, setValue)
     },
-    [setError, setValue],
+    [processFile, setError, setValue],
   )
 
   // Reset form before closing.

apps/frontend/src/i18n/locales/features/admin-form/settings/index.ts
@@ -36,11 +36,80 @@ export interface SecretKeyModalStrings {
   }
 }
 
+export interface SecretKeyVerificationStrings {
+  errors: {
+    invalidFile: string
+    invalidKey: string
+  }
+}
+
+export interface SettingsMutationsStrings {
+  missingFormId: string
+  formStatus: {
+    openStorageMode: string
+    open: string
+    closed: string
+  }
+  multiLang: {
+    enabled: string
+    disabled: string
+  }
+  supportedLanguages: {
+    selectable: string
+    hidden: string
+  }
+  saveDraft: {
+    enabled: string
+    disabled: string
+  }
+  captcha: {
+    enabled: string
+    disabled: string
+  }
+  issueNotification: {
+    enabled: string
+    disabled: string
+  }
+  formTitleUpdated: string
+  inactiveMessageUpdated: string
+  emailsUpdated: string
+  esrvcIdUpdated: string
+  authType: {
+    enabled: string
+    disabled: string
+    updated: string
+  }
+  submitterId: {
+    enabled: string
+    disabled: string
+  }
+  singleSubmission: {
+    enabled: string
+    disabled: string
+  }
+  whitelist: {
+    uploaded: string
+    removed: string
+  }
+  webhookUrl: {
+    updated: string
+    removed: string
+  }
+  webhookRetries: {
+    enabled: string
+    disabled: string
+  }
+  businessInfoUpdated: string
+  gstUpdated: string
+}
+
 export interface Settings {
   general: General
   singpass: HasTitle
   tabs: SettingsTabsStrings
   secretKeyModal: SecretKeyModalStrings
+  secretKeyVerification: SecretKeyVerificationStrings
+  mutations: SettingsMutationsStrings
   emailNotifications: EmailNotifications
   webhooks: Webhooks
   payments: Payments