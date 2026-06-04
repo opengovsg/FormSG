32ebf32118d63b915f6ae17fd90619848592e12b
scottheng96 <scottdavidheng@gmail.com>
2026-06-04 10:14:07 +0800

    fix: fix conditional render of signature canvas based on answerArray[1] instead of length of answerArray

--- apps/frontend/src/features/admin-form/responses/IndividualResponsePage/PrintableResponse.tsx
+++ apps/frontend/src/features/admin-form/responses/IndividualResponsePage/PrintableResponse.tsx
@@ -109,7 +109,7 @@ const PrintableDecryptedRow = ({
         </>
       )
     case BasicField.Signature: {
-      if (row.answerArray && row.answerArray.length > 1) {
+      if (row.answerArray && row.answerArray[1]) {
         const signatureVectorArray = JSON.parse(row.answerArray[1] as string)
         const signatureSvg = convertToSignatureSvgString(signatureVectorArray)
         return (
@@ -125,8 +125,9 @@ const PrintableDecryptedRow = ({
           </TableRow>
         )
       }
-      return <></>
+      return <StandardPrintableRow question={row.question} answer="" />
     }
+    // falls through to default
     default:
       return (
         <StandardPrintableRow
