22cbc7c4127ce7963a0182e15c2fb9b3522902a3
Kevin Foong <55353265+kevin9foong@users.noreply.github.com>
2026-09-16 14:13:47 +0800

    fix: arg type for unsupported type error

--- packages/shared/utils/response-value-rules.ts
+++ packages/shared/utils/response-value-rules.ts
@@ -6,7 +6,7 @@ import {
   CLIENT_CHECKBOX_OTHERS_INPUT_VALUE,
   CLIENT_RADIO_OTHERS_INPUT_VALUE,
 } from '../constants/form'
-import { AddressAttributes, SignatureVectorArray } from '../types/field'
+import { AddressAttributes, FormFieldDto, SignatureVectorArray } from '../types/field'
 import { TableRow } from '../types/response'
 
 import { removeAt } from './immutable-array-fns'
@@ -250,6 +250,6 @@ export const computeAddressAnswerValue = (
   }
 }
 
-export const throwUnsupportedFieldType = (fieldType: never): never => {
-  throw new Error(`Unsupported field type: ${fieldType}`)
+export const throwUnsupportedFieldType = (field: FormFieldDto): never => {
+  throw new Error(`Unsupported field type: ${field.fieldType} for field id: ${field._id}`)
 }
